84b143d3f3a5f6ebe37c4f9accb52ea80fe9221f
rupertbates <rupert.bates@guardian.co.uk>
2017-04-06 17:41:22 +0100

    Tweak

--- monthlycontributions/src/main/scala/com/gu/support/lambda/CreatePaymentMethod.scala
+++ monthlycontributions/src/main/scala/com/gu/support/lambda/CreatePaymentMethod.scala
@@ -5,9 +5,10 @@ import java.util.{ Map => JMap }
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.typesafe.scalalogging.LazyLogging
 
-class CreatePaymentMethod extends RequestHandler[JMap[String, Object], Unit] with LazyLogging {
+class CreatePaymentMethod extends RequestHandler[JMap[String, Object], String] with LazyLogging {
 
-  override def handleRequest(event: JMap[String, Object], context: Context): Unit = {
+  override def handleRequest(event: JMap[String, Object], context: Context): String = {
+    "Success"
   }
 
 }
