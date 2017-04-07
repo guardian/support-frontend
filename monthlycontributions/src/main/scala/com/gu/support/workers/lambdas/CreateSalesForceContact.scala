9f8c8b08dd30906c304a67b125217e5753a1f82b
rupertbates <rupert.bates@guardian.co.uk>
2017-04-07 16:16:46 +0100

    Add a create Zuora subscription step

--- monthlycontributions/src/main/scala/com/gu/support/workers/lambdas/CreateSalesForceContact.scala
+++ monthlycontributions/src/main/scala/com/gu/support/workers/lambdas/CreateSalesForceContact.scala
@@ -1,11 +1,15 @@
 package com.gu.support.workers.lambdas
 
 import com.amazonaws.services.lambda.runtime.Context
+import com.gu.memsub.Address
 import com.gu.support.workers.helpers.Handler
 import com.gu.support.workers.model.{PaymentMethod, SalesForceContact}
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
 
 class CreateSalesForceContact extends Handler[PaymentMethod, SalesForceContact] with LazyLogging {
-  override protected def handler(input: PaymentMethod, context: Context) = SalesForceContact("789")
+  override protected def handler(input: PaymentMethod, context: Context) = {
+    val address = Address("", "", "", "", "", "")
+    SalesForceContact("789")
+  }
 }
