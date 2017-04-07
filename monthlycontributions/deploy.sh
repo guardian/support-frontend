d50876b9d2225684da755309ca74e9903dd25797
rupertbates <rupert.bates@guardian.co.uk>
2017-04-07 16:18:25 +0100

    Add a deploy script to avoid having to work out a full deployment story

---
+++ monthlycontributions/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sbt assembly
+aws s3 cp target/scala-2.11/guardian-support-monthly-contributions-lambdas-assembly-0.1-SNAPSHOT.jar s3://support-workers-dist/lambdas/bin/guardian-support-monthly-contributions-lambdas-assembly-0.1-SNAPSHOT.jar
+#aws --region eu-west-1 cloudformation update-stack --stack-name "support-workers" --template-body file://cloud-formation/monthly-contribution-step-functions.yaml --capabilities CAPABILITY_NAMED_IAM --profile membership
