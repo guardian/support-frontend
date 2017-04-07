247fbdb86bf07dd518e04be5a170aece76ae9f81
rupertbates <rupert.bates@guardian.co.uk>
2017-04-07 19:41:25 +0100

    Extract json conversion for tests into implicit classes

--- monthlycontributions/src/test/scala/com/gu/support/workers/CreatePaymentMethodSpec.scala
+++ monthlycontributions/src/test/scala/com/gu/support/workers/CreatePaymentMethodSpec.scala
@@ -1,35 +1,31 @@
 package com.gu.support.workers
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import java.io.ByteArrayOutputStream
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.gu.support.workers.lambdas.CreatePaymentMethod
-import com.gu.support.workers.model.User
+import com.gu.support.workers.model.{PaymentMethod, User}
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FlatSpec, Matchers}
 
 
+
 class CreatePaymentMethodSpec extends FlatSpec with Matchers with MockitoSugar with LazyLogging {
 
-  import io.circe.Encoder
+  import com.gu.support.workers.Conversions._
   import io.circe.generic.auto._
-  import io.circe.syntax._
 
   "CreatePaymentMethod lambda" should "accept and return valid json" in {
     val createPaymentMethod = new CreatePaymentMethod()
 
     val outStream = new ByteArrayOutputStream()
 
-    createPaymentMethod.handleRequest(asInputStream(User("123", "Test user")), outStream, mock[Context])
+    val inStream = User("123", "Test user").asInputStream()
 
-    logger.info(s"Output: ${outStream.toString}")
+    createPaymentMethod.handleRequest(inStream, outStream, mock[Context])
+
+    val p = outStream.toClass[PaymentMethod]()
+    logger.info(s"Output: $p")
   }
-
-  def asInputStream[T](input: T)(implicit encoder: Encoder[T]) = {
-    val convertStream = new ByteArrayOutputStream()
-    convertStream.write(input.asJson.noSpaces.getBytes("UTF-8"))
-    new ByteArrayInputStream(convertStream.toByteArray)
-  }
-
 }
