1fbbb8a79443c93185953cde64953ee98ec21346
Jamie Byers <james.byers@guardian.co.uk>
2017-04-10 15:46:50 +0100

    Eslint is driving me crazy, this brings it more into line with dotcom.

--- webpack.config.js
+++ webpack.config.js
@@ -3,52 +3,52 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    helloWorldPage: 'pages/hello-world/helloWorld.jsx',
-    bundlesLandingPage: 'pages/bundles-landing/bundlesLanding.jsx',
-  },
-
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    chunkFilename: 'webpack/[chunkhash].js',
-    filename: 'javascripts/[name].js',
-    publicPath: '/assets/',
-  },
-
-  resolve: {
-    alias: {
-      react: 'preact-compat',
-      'react-dom': 'preact-compat',
+    entry: {
+        helloWorldPage: 'pages/hello-world/helloWorld.jsx',
+        bundlesLandingPage: 'pages/bundles-landing/bundlesLanding.jsx',
     },
-    modules: [
-      path.resolve(__dirname, 'assets'),
-      path.resolve(__dirname, 'node_modules'),
-    ],
-    extensions: ['.js'],
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'es2015'],
-          cacheDirectory: '',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        chunkFilename: 'webpack/[chunkhash].js',
+        filename: 'javascripts/[name].js',
+        publicPath: '/assets/',
+    },
+
+    resolve: {
+        alias: {
+            react: 'preact-compat',
+            'react-dom': 'preact-compat',
         },
-      },
-    ],
-  },
-
-  devtool: 'source-map',
-
-  devServer: {
-    proxy: {
-      '**': {
-        target: 'http://localhost:9000',
-        secure: false,
-      },
+        modules: [
+            path.resolve(__dirname, 'assets'),
+            path.resolve(__dirname, 'node_modules'),
+        ],
+        extensions: ['.js', '.jsx'],
     },
-  },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react', 'es2015'],
+                    cacheDirectory: '',
+                },
+            },
+        ],
+    },
+
+    devtool: 'source-map',
+
+    devServer: {
+        proxy: {
+            '**': {
+                target: 'http://localhost:9000',
+                secure: false,
+            },
+        },
+    },
 };
