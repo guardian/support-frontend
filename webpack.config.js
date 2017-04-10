3165a4b216c87f31b74fcca65b0d332833c4d978
Jamie Byers <james.byers@guardian.co.uk>
2017-04-10 14:50:29 +0100

    Added a placeholder bundles landing page.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 module.exports = {
   entry: {
     helloWorldPage: 'pages/hello-world/helloWorld.jsx',
+    bundlesLandingPage: 'pages/bundles-landing/bundlesLanding.jsx',
   },
 
   output: {
