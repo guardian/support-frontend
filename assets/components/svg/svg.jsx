64a48a64267d8688e1fd0457b79350042a1e78b1
Jamie Byers <james.byers@guardian.co.uk>
2017-04-25 10:52:20 +0100

    Added some type safety to svg names.

--- assets/components/svg/svg.jsx
+++ assets/components/svg/svg.jsx
@@ -5,29 +5,30 @@
 import React from 'react';
 
 
-// ----- Types ----- //
+// ----- Catalogue ----- //
 
 type svg = {
   path: string,
   viewBox: string,
 };
 
-type svgCatalog = {
-  [string]: svg,
-};
-
-type PropTypes = {
-  svgName: string,
-};
-
-
-// ----- Catalogue ----- //
-
-const svgsCatalog: svgCatalog = {
+const svgsCatalog: {
+  'arrow-right-straight': svg,
+} = {
   'arrow-right-straight': {
     path: 'M20 9.35l-9.08 8.54-.86-.81 6.54-7.31H0V8.12h16.6L10.06.81l.86-.81L20 8.51v.84z',
     viewBox: '0 0 20 17.89',
   },
+};
+
+
+// ----- Types ----- //
+
+// Utility type: https://flow.org/en/docs/types/utilities/#toc-keys
+export type svgName = $Keys<typeof svgsCatalog>;
+
+type PropTypes = {
+  svgName: svgName,
 };
 
 
