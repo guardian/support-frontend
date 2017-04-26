5bb39ed5e7d66cedb4351ebd71385a56ec0526d3
David Furey <david.furey@guardian.co.uk>
2017-04-26 11:13:12 +0100

    Add cache headers to errors

--- app/wiring/AppComponents.scala
+++ app/wiring/AppComponents.scala
@@ -5,10 +5,12 @@ import play.api.routing.Router
 import router.Routes
 import controllers.{Application, Assets}
 import filters.CheckCacheHeadersFilter
+import lib.CustomHttpErrorHandler
 import play.api.mvc.EssentialFilter
 
 trait AppComponents extends PlayComponents {
 
+  override lazy val httpErrorHandler = new CustomHttpErrorHandler(environment, configuration, sourceMapper, Some(router))
   implicit lazy val assetsResolver = new AssetsResolver("/assets/", "assets.map", environment)
   lazy val assetController = new Assets(httpErrorHandler)
   lazy val applicationController = new Application()
