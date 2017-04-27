712eb49d36af89751150817572fbbcd30fda1f70
Jamie Byers <james.byers@guardian.co.uk>
2017-04-27 18:21:49 +0100

    Added introduction section to landing page.

--- assets/pages/bundles-landing/bundlesLanding.jsx
+++ assets/pages/bundles-landing/bundlesLanding.jsx
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import Introduction from './components/Introduction';
 import Bundles from './components/Bundles';
 import WaysOfSupport from './components/WaysOfSupport';
 import SimpleFooter from '../../components/footers/simpleFooter/simpleFooter';
@@ -23,6 +24,7 @@ const store = createStore(reducer);
 const content = (
   <Provider store={store}>
     <div>
+      <Introduction />
       <Bundles />
       <br style={{ clear: 'both' }} />
       <WaysOfSupport />
