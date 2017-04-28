af06e1bcbd95999ef5cf1cfce8afba20fb2467f1
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-04-28 11:13:06 +0100

    [adding-tests] Added the first basic test for reducers.

---
+++ assets/pages/bundles-landing/reducers/__tests__/reducersTest.js
@@ -0,0 +1,29 @@
+import reducer from '../reducers'
+import { changePaperBundle, changeContribType, changeContribAmountRecurring, changeContribAmountOneOff } from '../../actions/bundlesLandingActions';
+
+describe('reducer tests', () => {
+
+  const initialContrib: ContribState = {
+    type: 'RECURRING',
+    error: null,
+    amount: {
+      recurring: {
+        value: '5',
+        userDefined: false,
+      },
+      oneOff: {
+        value: '25',
+        userDefined: false,
+      },
+    },
+  };
+  const intialPaperBundle: PaperBundle = 'PAPER+DIGITAL';
+  const intialState = { paperBundle: intialPaperBundle, contribution: initialContrib };
+
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, {})
+    ).toEqual(intialState)
+  })
+  
+});
