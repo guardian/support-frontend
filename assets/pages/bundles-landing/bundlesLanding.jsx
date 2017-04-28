4987edee207988a26e229424e33fd1fe2fa86bc4
Jamie Byers <james.byers@guardian.co.uk>
2017-04-28 18:18:56 +0100

    Added the new simple header to the bundles landing page.

--- assets/pages/bundles-landing/bundlesLanding.jsx
+++ assets/pages/bundles-landing/bundlesLanding.jsx
@@ -7,10 +7,12 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
+import SimpleFooter from 'components/footers/simpleFooter/simpleFooter';
+
 import Introduction from './components/Introduction';
 import Bundles from './components/Bundles';
 import WaysOfSupport from './components/WaysOfSupport';
-import SimpleFooter from '../../components/footers/simpleFooter/simpleFooter';
 import reducer from './reducers/reducers';
 
 
@@ -24,6 +26,7 @@ const store = createStore(reducer);
 const content = (
   <Provider store={store}>
     <div>
+      <SimpleHeader />
       <Introduction />
       <Bundles />
       <br style={{ clear: 'both' }} />
