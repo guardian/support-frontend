b3017cef0a377519ce3706ac4bb5a9b79ca9dc3a
rupertbates <rupert.bates@guardian.co.uk>
2017-05-09 18:06:14 +0100

    Implement the CreateZuoraSubscription lambda

--- common/src/main/scala/com/gu/zuora/model/PaymentGateway.scala
+++ common/src/main/scala/com/gu/zuora/model/PaymentGateway.scala
@@ -1,13 +1,21 @@
 package com.gu.zuora.model
 
 sealed trait PaymentGateway {
-  def name : String
+  def name: String
+}
+
+object PaymentGateway {
+  def forPaymentMethod(paymentMethod: PaymentMethod): PaymentGateway = paymentMethod match {
+    case _: PayPalReferenceTransaction => PayPalGateway
+    case _: CreditCardReferenceTransaction => StripeGateway
+  }
 }
 
 case object StripeGateway extends PaymentGateway {
   val name = "Stripe Gateway 1"
 }
-case object PayPalGateway extends PaymentGateway{
+
+case object PayPalGateway extends PaymentGateway {
   val name = "PayPal Express"
 }
 
--- monthly-contributions/src/main/scala/com/gu/support/workers/lambdas/CreateZuoraSubscription.scala
+++ monthly-contributions/src/main/scala/com/gu/support/workers/lambdas/CreateZuoraSubscription.scala
@@ -1,13 +1,60 @@
 package com.gu.support.workers.lambdas
 
 import com.amazonaws.services.lambda.runtime.Context
+import com.gu.config.Configuration
+import com.gu.i18n.Country
+import com.gu.i18n.Currency.GBP
 import com.gu.support.workers.model.CreateZuoraSubscriptionState
+import com.gu.zuora.ZuoraService
+import com.gu.zuora.model._
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
+import org.joda.time.{DateTimeZone, LocalDate}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
-class CreateZuoraSubscription extends FutureHandler[CreateZuoraSubscriptionState, String] with LazyLogging {
-  override protected def handlerFuture(state: CreateZuoraSubscriptionState, context: Context) = Future.successful("success")
+class CreateZuoraSubscription(zuoraService: ZuoraService)
+  extends FutureHandler[CreateZuoraSubscriptionState, String] with LazyLogging {
+
+  override protected def handlerFuture(state: CreateZuoraSubscriptionState, context: Context) =
+    zuoraService.subscribe(buildSubscribeRequest(state)).map(_.head.accountNumber)
+
+  private def buildSubscribeRequest(state: CreateZuoraSubscriptionState) = {
+    //Documentation for this request is here: https://www.zuora.com/developer/api-reference/#operation/Action_POSTsubscribe
+
+    val account = Account(
+      state.salesForceContact.AccountId, //We store the Salesforce Account id in the name field
+      GBP, //Hard coded to GBP for now
+      state.salesForceContact.AccountId, //Somewhere else we store the Salesforce Account id
+      state.salesForceContact.AccountId, //Hello, this looks familiar
+      state.user.id,
+      PaymentGateway.forPaymentMethod(state.paymentMethod))
+
+    val contactDetails = ContactDetails(
+      state.user.firstName,
+      state.user.lastName,
+      state.user.primaryEmailAddress,
+      Country.UK) //Country is hardcoded for now
+
+    val date = LocalDate.now(DateTimeZone.UTC)
+
+    val subscriptionData = SubscriptionData(List(
+      RatePlanData(
+        RatePlan(Configuration.zuoraConfig.productRatePlanId),
+        List(RatePlanChargeData(
+          RatePlanCharge(Configuration.zuoraConfig.productRatePlanChargeId, Some(state.amount)) //Pass the amount the user selected into Zuora
+        )),
+        Nil
+      )),
+      Subscription(date, date, date)
+    )
+
+    SubscribeRequest(List(
+      SubscribeItem(
+        account,
+        contactDetails,
+        state.paymentMethod,
+        subscriptionData,
+        SubscribeOptions())))
+  }
 }
