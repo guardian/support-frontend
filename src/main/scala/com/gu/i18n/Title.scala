74c2d689c38d91d13f01b9fb3b296c9dc86a8abf
Alex Ware <alex.ware@guardian.co.uk>
2017-05-17 12:30:06 +0100

    Re-add Mx title.

--- src/main/scala/com/gu/i18n/Title.scala
+++ src/main/scala/com/gu/i18n/Title.scala
@@ -7,9 +7,10 @@ object Title{
   val Mrs = Title("Mrs")
   val Ms = Title("Ms")
   val Miss = Title("Miss")
+  val Mx = Title("Mx")
   val Dr = Title("Dr")
   val Prof = Title("Prof")
   val Rev = Title("Rev")
-  val all = Seq(Mr,Mrs,Ms,Miss,Dr,Prof,Rev)
+  val all = Seq(Mr,Mrs,Ms,Miss,Mx,Dr,Prof,Rev)
   def fromString(str: String): Option[Title] = all.find {_.title == str}
 }
