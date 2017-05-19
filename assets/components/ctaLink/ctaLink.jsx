803244588a0610b9c8a2399f5ef4382b0adbf35f
Jamie Byers <james.byers@guardian.co.uk>
2017-05-19 15:00:32 +0100

    Added the right arrow to the cta links.

--- assets/components/ctaLink/ctaLink.jsx
+++ assets/components/ctaLink/ctaLink.jsx
@@ -3,6 +3,7 @@
 // ----- Imports ----- //
 
 import React from 'react';
+import Svg from 'components/svg/svg';
 
 
 // ----- Types ----- //
@@ -19,7 +20,8 @@ export default function CtaLink(props: PropTypes) {
 
   return (
     <a className="component-cta-link" href={props.url}>
-      {props.text}
+      <span>{props.text}</span>
+      <Svg svgName="arrow-right-straight" />
     </a>
   );
 
