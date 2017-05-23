7bb48a5d710303c8ffb7ab874fbb9b28fbc75551
rupertbates <rupert.bates@guardian.co.uk>
2017-05-23 14:23:57 +0100

    Provide a no args constructor for the SendThankYouEmail lambda

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val circeVersion = "0.7.0"
-  val awsVersion = "1.11.95"
+  val awsVersion = "1.11.131"
   val config = "com.typesafe" % "config" % "1.3.1"
   val joda = "org.joda" % "joda-convert" % "1.8.1"
   val cats = "org.typelevel" %% "cats" % "0.9.0"
@@ -11,7 +11,6 @@ object Dependencies {
   val supportInternationalisation = "com.gu" %% "support-internationalisation" % "0.1"
   val okhttp = "com.squareup.okhttp3" % "okhttp" % "3.4.1"
   val scalaUri = "com.netaporter" %% "scala-uri" % "0.4.16"
-  val awsSdk = "com.amazonaws" % "aws-java-sdk" % awsVersion
   val awsLambdas = "com.amazonaws" % "aws-lambda-java-core" % "1.1.0"
   val awsS3 = "com.amazonaws" % "aws-java-sdk-s3" % awsVersion
   val awsSQS = "com.amazonaws" % "aws-java-sdk-sqs" % awsVersion
@@ -26,7 +25,7 @@ object Dependencies {
   val stm = "org.scala-stm" %% "scala-stm" % "0.8"
 
   val commonDependencies: Seq[ModuleID] = Seq(config, logback, scalaLogging, joda, dispatch,
-    supportInternationalisation, awsSdk, awsCloudwatch, awsS3, awsSQS, awsLambdas, okhttp, scalaUri, cats, circeCore,
+    supportInternationalisation, awsCloudwatch, awsS3, awsSQS, awsLambdas, okhttp, scalaUri, cats, circeCore,
     circeGeneric, circeGenericExtras, circeParser, stm, scalaTest)
   val monthlyContributionsDependencies: Seq[ModuleID] = Seq(mokito, scalaTest)
 
