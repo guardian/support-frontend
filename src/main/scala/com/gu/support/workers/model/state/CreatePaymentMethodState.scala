5de6895257dc3b9a457ccc4d73c8a4d6e25e994d
davidfurey <david.furey@guardian.co.uk>
2017-06-01 16:27:10 +0100

    Add requestId to states

--- src/main/scala/com/gu/support/workers/model/state/CreatePaymentMethodState.scala
+++ src/main/scala/com/gu/support/workers/model/state/CreatePaymentMethodState.scala
@@ -1,8 +1,11 @@
 package com.gu.support.workers.model.state
 
+import java.util.UUID
+
 import com.gu.support.workers.model.{Contribution, PayPalPaymentFields, StripePaymentFields, User}
 
 case class CreatePaymentMethodState(
+  requestId: UUID,
   user: User,
   contribution: Contribution,
   paymentFields: Either[StripePaymentFields, PayPalPaymentFields]
