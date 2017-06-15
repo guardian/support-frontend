dbce157f88e12928e8df17198b74b20abf8b0559
davidfurey <david.furey@guardian.co.uk>
2017-06-15 17:04:12 +0100

    Make user fields available to JS on authenticated pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,8 +3,17 @@ package controllers
 import assets.AssetsResolver
 import lib.actions.{ActionRefiners, CachedAction}
 import play.api.mvc.{Action, AnyContent, Controller}
+import services.IdentityService
+import scala.concurrent.ExecutionContext
+import cats.implicits._
 
-class Application(implicit actionRefiners: ActionRefiners, val assets: AssetsResolver) extends Controller {
+class Application(
+    implicit
+    actionRefiners: ActionRefiners,
+    val assets: AssetsResolver,
+    identityService: IdentityService,
+    ec: ExecutionContext
+) extends Controller {
 
   import actionRefiners._
 
@@ -14,6 +23,12 @@ class Application(implicit actionRefiners: ActionRefiners, val assets: AssetsRes
 
   def authenticatedReactTemplate(title: String, id: String, js: String): Action[AnyContent] = AuthenticatedAction { request =>
     Ok(views.html.authenticatedReact(title, id, js))
+  }
+
+  def authenticatedFullUserReactTemplate(title: String, id: String, js: String): Action[AnyContent] = AuthenticatedAction.async { implicit request =>
+    identityService.getUser(request.user).map { user =>
+      Ok(views.html.authenticatedFullUserReactTemplate(title, id, js, user))
+    } getOrElse InternalServerError
   }
 
   def healthcheck: Action[AnyContent] = PrivateAction {
--- test/controllers/ApplicationTest.scala
+++ test/controllers/ApplicationTest.scala
@@ -9,6 +9,9 @@ import assets.AssetsResolver
 import com.gu.identity.play.AuthenticatedIdUser
 import lib.actions.ActionRefiners
 import org.scalatest.mockito.MockitoSugar.mock
+import services.IdentityService
+
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -20,12 +23,12 @@ class ApplicationTest extends WordSpec with MustMatchers {
 
   "/healthcheck" should {
     "return healthy" in {
-      val result = new Application()(actionRefiner, mock[AssetsResolver]).healthcheck.apply(FakeRequest())
+      val result = new Application()(actionRefiner, mock[AssetsResolver], mock[IdentityService], mock[ExecutionContext]).healthcheck.apply(FakeRequest())
       contentAsString(result) mustBe "healthy"
     }
 
     "not be cached" in {
-      val result = new Application()(actionRefiner, mock[AssetsResolver]).healthcheck.apply(FakeRequest())
+      val result = new Application()(actionRefiner, mock[AssetsResolver], mock[IdentityService], mock[ExecutionContext]).healthcheck.apply(FakeRequest())
       header("Cache-Control", result) mustBe Some("private")
     }
   }
