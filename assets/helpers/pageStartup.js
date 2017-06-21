e3e418047095259fa7d4c205fce0079a4a2719b3
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-06-21 16:42:58 +0100

    Created a pageStartup helper.

---
+++ assets/helpers/pageStartup.js
@@ -0,0 +1,25 @@
+import 'ophan';
+import * as ga from 'helpers/ga';
+import * as abTest from 'helpers/abtest';
+import * as logger from 'helpers/logger';
+
+const start = () => {
+  // ----- AB Tests ----- //
+
+  const participation = abTest.init();
+
+
+  // ----- Tracking ----- //
+
+  ga.init();
+  ga.setDimension('experience', abTest.getVariantsAsString(participation));
+  ga.trackPageview();
+
+// ----- Logging ----- //
+
+  logger.init();
+
+  return participation;
+};
+
+export  { start };
