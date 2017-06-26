34f27324e4c1eb56975c2e617e90350bf2822d0e
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-06-26 10:11:15 +0100

    Added an onclick function for ctaLink.

--- assets/components/ctaLink/ctaLink.jsx
+++ assets/components/ctaLink/ctaLink.jsx
@@ -10,16 +10,16 @@ import Svg from 'components/svg/svg';
 
 type PropTypes = {
   text: string,
-  url: string,
+  url?: string,
+  onClick?: () => void,
 };
 
 
 // ----- Component ----- //
 
 export default function CtaLink(props: PropTypes) {
-
   return (
-    <a className="component-cta-link" href={props.url}>
+    <a className="component-cta-link" href={props.url} onClick={props.onClick}>
       <span>{props.text}</span>
       <Svg svgName="arrow-right-straight" />
     </a>
