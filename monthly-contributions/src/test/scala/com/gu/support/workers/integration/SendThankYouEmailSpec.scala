6dedc8d0464636481bc95714d3a3f870ce7a07b1
davidfurey <david.furey@guardian.co.uk>
2017-06-28 14:39:00 +0100

    Move integration tests into separate package

--- monthly-contributions/src/test/scala/com/gu/support/workers/integration/SendThankYouEmailSpec.scala
+++ monthly-contributions/src/test/scala/com/gu/support/workers/integration/SendThankYouEmailSpec.scala
@@ -1,10 +1,11 @@
-package com.gu.support.workers.lambdas
+package com.gu.support.workers.integration
 
 import java.io.ByteArrayOutputStream
 
 import com.gu.support.workers.Conversions.{FromOutputStream, StringInputStreamConversions}
 import com.gu.support.workers.Fixtures.thankYouEmailJson
 import com.gu.support.workers.LambdaSpec
+import com.gu.support.workers.lambdas.SendThankYouEmail
 import com.gu.test.tags.annotations.IntegrationTest
 
 @IntegrationTest
