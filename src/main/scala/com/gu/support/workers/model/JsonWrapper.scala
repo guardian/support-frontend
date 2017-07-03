4f4715e73ea481b0e0aaf6e3a3170aecde69d0dc
rupertbates <rupert.bates@guardian.co.uk>
2017-07-03 15:40:47 +0100

    Add a wrapper object for the state models to enable encryption

---
+++ src/main/scala/com/gu/support/workers/model/JsonWrapper.scala
@@ -0,0 +1,7 @@
+package com.gu.support.workers.model
+
+/**
+ * AWS Step Functions expect to be passed valid Json, as we want to encrypt the whole of the
+ * state, we need to wrap it in a Json 'wrapper' object as a Base64 encoded String
+ */
+case class JsonWrapper(state : String)
