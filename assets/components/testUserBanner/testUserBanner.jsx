38cf381a4cd5956f8d3913cd05724605278b5084
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-06 13:27:18 +0100

    [test-user-banner] Implememntation of the testUserBanner component.

---
+++ assets/components/testUserBanner/testUserBanner.jsx
@@ -0,0 +1,23 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+
+// ----- Component ----- //
+
+const TestUserBanner = () => {
+
+  return (
+    <div className="component-test-user-banner">
+      Using test backend: <strong>UAT</strong> because you are signed in as a user
+      with a valid Test Username.
+    </div>
+  );
+}
+
+
+// ----- Exports ----- //
+
+export default TestUserBanner;
