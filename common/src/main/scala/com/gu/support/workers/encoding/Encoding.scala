c656d83f504911a37b67fa6401d524c368a32862
rupertbates <rupert.bates@guardian.co.uk>
2017-07-06 15:32:40 +0100

    Remove some debugging

--- common/src/main/scala/com/gu/support/workers/encoding/Encoding.scala
+++ common/src/main/scala/com/gu/support/workers/encoding/Encoding.scala
@@ -6,11 +6,10 @@ import java.util.Base64
 import cats.syntax.either._
 import com.gu.support.workers.encoding.Encryption._
 import com.gu.support.workers.encoding.Wrapper._
-import com.typesafe.scalalogging.LazyLogging
 
 import scala.util.Try
 
-private[workers] object Encoding extends LazyLogging{
+private[workers] object Encoding {
 
   import io.circe._
   import io.circe.parser._
@@ -21,7 +20,7 @@ private[workers] object Encoding extends LazyLogging{
       wrapper <- unWrap(is)
       state <- Try(Base64.getDecoder.decode(wrapper.state))
       decrypted <- Try(decrypt(state))
-      result <- {logger.info(s"decrypted state:\n$decrypted"); decode[T](decrypted).toTry}
+      result <- decode[T](decrypted).toTry
     } yield result
 
   def out[T](value: T, os: OutputStream)(implicit encoder: Encoder[T]): Try[Unit] = {
