67051228fb043c4f3667e3afd8a177c2df7e2b8f
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-06 15:45:59 +0100

    [test-user-banner] Adding redux state to TestUserBanner compnent.

--- assets/components/testUserBanner/testUserBanner.jsx
+++ assets/components/testUserBanner/testUserBanner.jsx
@@ -3,14 +3,23 @@
 // ----- Imports ----- //
 
 import React from 'react';
+import { connect } from 'react-redux';
 
+
+// ----- Types ----- //
+
+type PropTypes = {
+  isTestUser: boolean,
+};
 
 // ----- Component ----- //
 
-const TestUserBanner = () => {
+const TestUserBanner = (props: PropTypes) => {
+  const suffix = !props.isTestUser? '--disabled' : '';
+  const className = `component-test-user-banner${suffix}`;
 
   return (
-    <div className="component-test-user-banner">
+    <div className={className}>
       Using test backend: <strong>UAT</strong> because you are signed in as a user
       with a valid Test Username.
     </div>
@@ -18,6 +27,16 @@ const TestUserBanner = () => {
 }
 
 
+// ----- Map State/Props ----- //
+
+function mapStateToProps(state) {
+
+  return {
+    isTestUser: state.user.isTestUser,
+  };
+
+}
+
 // ----- Exports ----- //
 
-export default TestUserBanner;
+export default connect(mapStateToProps)(TestUserBanner);
