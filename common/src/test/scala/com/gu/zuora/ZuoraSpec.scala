87491cd0cb2224cb0236db80ff1980121d603ee0
rupertbates <rupert.bates@guardian.co.uk>
2017-07-12 10:53:34 +0100

    Remove unnecessary type definition

--- common/src/test/scala/com/gu/zuora/ZuoraSpec.scala
+++ common/src/test/scala/com/gu/zuora/ZuoraSpec.scala
@@ -4,7 +4,6 @@ import com.gu.config.Configuration.zuoraConfigProvider
 import com.gu.okhttp.RequestRunners
 import com.gu.test.tags.annotations.IntegrationTest
 import com.gu.zuora.Fixtures._
-import com.gu.zuora.model.response.Subscription
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.{AsyncFlatSpec, Matchers}
 
@@ -44,7 +43,7 @@ class ZuoraSpec extends AsyncFlatSpec with Matchers with LazyLogging {
 
   it should "be able to find a monthly recurring subscription" in {
     uatService.getMonthlyRecurringSubscription("30000701").map {
-      response: Option[Subscription] =>
+      response =>
         response.isDefined should be(true)
         response.get.ratePlans.head.productName should be("Contributor")
     }
