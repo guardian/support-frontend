ab28f61fc7fec10f82f89d8cba48472455c166cc
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-13 12:07:25 +0100

    [monthly-contribution-error-message] Added a fix to prioritise the fill error.

--- assets/pages/monthly-contributions/components/paymentMethods.jsx
+++ assets/pages/monthly-contributions/components/paymentMethods.jsx
@@ -26,12 +26,10 @@ function PaymentMethods(props: PropTypes) {
 
   let errorMessage = '';
   let stripeButton = <StripePopUpButton email={props.email} callback={postCheckout} />;
-  if ((props.firstName === '' || props.lastName === '') && props.error === null) {
+  if (props.firstName === '' || props.lastName === '') {
     errorMessage = <ErrorMessage message={'Please fill in all the fields above.'} />;
     stripeButton = '';
-  }
-
-  if (props.error !== null) {
+  } else if (props.error !== null) {
     errorMessage = <ErrorMessage message={'There was an error processing your payment. Please\u00a0try\u00a0again\u00a0later.'} />;
   }
 
