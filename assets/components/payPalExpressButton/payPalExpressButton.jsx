ba539b77bc9809cb92305b108828846b5225ad3f
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-14 16:11:26 +0100

    [paypal] Added first approach to PayPal's button.

---
+++ assets/components/payPalExpressButton/payPalExpressButton.jsx
@@ -0,0 +1,60 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+import {
+  setupPayPalExpressCheckout,
+} from 'helpers/payPalExpressCheckout/payPalExpressCheckoutActions';
+
+
+// ---- Types ----- //
+
+type PropTypes = {
+  payPalLoaded: boolean,
+  setupPayPalCheckout: Function,
+  amount: number,
+  email: string,
+  callback: Function,
+};
+
+
+// ----- Component ----- //
+
+const PayPalExpressButton = (props: PropTypes) => {
+
+  if (!props.payPalLoaded) {
+    props.setupPayPalCheckout(props.callback);
+  }
+  return (<div id="component-paypal-button-checkout" className="component-paypal-button-checkout" />);
+};
+
+
+// ----- Map State/Props ----- //
+
+function mapStateToProps(state) {
+
+  return {
+    // overlayOpen: state.stripeCheckout.overlay,
+    payPalLoaded: state.payPalExpressCheckout.loaded,
+    amount: state.payPalExpressCheckout.amount,
+  };
+
+}
+
+function mapDispatchToProps(dispatch) {
+
+  return {
+    setupPayPalCheckout: (callback: Function) => {
+      dispatch(setupPayPalExpressCheckout(callback));
+    },
+  };
+
+}
+
+
+// ----- Exports ----- //
+
+export default connect(mapStateToProps, mapDispatchToProps)(PayPalExpressButton);
