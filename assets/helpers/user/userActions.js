b01d1d177871c0062c3b47a4acebba1d7fd60f08
Jamie Byers <Ap0c@users.noreply.github.com>
2017-07-17 17:48:01 +0100

    Added additional fields to shared user helper/reducer.

--- assets/helpers/user/userActions.js
+++ assets/helpers/user/userActions.js
@@ -5,6 +5,9 @@
 export type Action =
   | { type: 'SET_FIRST_NAME', name: string }
   | { type: 'SET_LAST_NAME', name: string }
+  | { type: 'SET_FULL_NAME', name: string }
+  | { type: 'SET_EMAIL', email: string }
+  | { type: 'SET_POSTCODE', postcode: string }
   ;
 
 
@@ -18,3 +21,14 @@ export function setLastName(name: string): Action {
   return { type: 'SET_LAST_NAME', name };
 }
 
+export function setFullName(name: string): Action {
+  return { type: 'SET_FULL_NAME', name };
+}
+
+export function setEmail(email: string): Action {
+  return { type: 'SET_EMAIL', email };
+}
+
+export function setPostcode(postcode: string): Action {
+  return { type: 'SET_POSTCODE', postcode };
+}
--- assets/helpers/user/userReducer.js
+++ assets/helpers/user/userReducer.js
@@ -13,6 +13,8 @@ type User = {
   firstName: ?string,
   lastName: ?string,
   isTestUser: ?string,
+  fullName?: string,
+  postcode?: string,
 };
 
 
@@ -46,6 +48,15 @@ export default function userReducer(
 
     case 'SET_LAST_NAME':
       return Object.assign({}, state, { lastName: action.name });
+
+    case 'SET_FULL_NAME':
+      return Object.assign({}, state, { fullName: action.name });
+
+    case 'SET_EMAIL':
+      return Object.assign({}, state, { email: action.email });
+
+    case 'SET_POSTCODE':
+      return Object.assign({}, state, { postcode: action.postcode });
 
     default:
       return state;
