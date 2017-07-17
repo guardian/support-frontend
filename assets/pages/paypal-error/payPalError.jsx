01d227e3702ffb954b2b3083381512b4e5ee6a54
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-17 10:38:09 +0100

    [paypal] Added PayPal Error pages.

---
+++ assets/pages/paypal-error/payPalError.jsx
@@ -0,0 +1,66 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
+import SimpleFooter from 'components/footers/simpleFooter/simpleFooter';
+import CtaLink from 'components/ctaLink/ctaLink';
+import InfoSection from 'components/infoSection/infoSection';
+import SocialShare from 'components/socialShare/socialShare';
+
+import pageStartup from 'helpers/pageStartup';
+
+
+// ----- Page Startup ----- //
+
+pageStartup.start();
+
+
+// ----- Render ----- //
+
+const content = (
+  <div className="gu-content">
+    <SimpleHeader />
+    <section className="payPal-error gu-content">
+      <div className="payPal-error__content gu-content-margin">
+        <div className="payPal-error__wrapper">
+          <h1 className="payPal-error__heading">PayPal Error! :-(</h1>
+          <h2 className="payPal-error__subheading">
+            <p>Sorry, there was a problem completing your PayPal payment. Please try again:</p>
+          </h2>
+          <CtaLink
+            text="Become a Supporter"
+            url="https://support.theguardian.com/uk"
+          />
+        </div>
+        <InfoSection heading="Questions?" className="payPal-error__questions">
+          <p>
+            If you have any questions about contributing to the Guardian,
+            please <a href="mailto:contribution.support@theguardian.com">
+            contact us</a>
+          </p>
+        </InfoSection>
+        <InfoSection
+          heading="Spread the word"
+          className="payPal-error__spread-the-word"
+        >
+          <p>
+            We report for everyone. Let your friends and followers know that
+            you support independent journalism.
+          </p>
+          <SocialShare name="facebook" />
+          <SocialShare name="twitter" />
+        </InfoSection>
+      </div>
+    </section>
+    <SimpleFooter />
+  </div>
+);
+
+ReactDOM.render(
+  content,
+  document.getElementById('paypal-error-page'),
+);
