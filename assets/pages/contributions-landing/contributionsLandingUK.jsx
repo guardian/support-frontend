8ec08b026d1ad60371a0f88c989a56fd43526e27
Jamie Byers <Ap0c@users.noreply.github.com>
2017-07-20 19:24:07 +0100

    Added the introduction bleed and legal copy.

--- assets/pages/contributions-landing/contributionsLandingUK.jsx
+++ assets/pages/contributions-landing/contributionsLandingUK.jsx
@@ -51,9 +51,32 @@ const content = (
     <div className="gu-content">
       <SimpleHeader />
       <IntroductionText messages={introductionCopy} />
-      <div className="contributions-landing gu-content-margin">
-        <ContributionsBundle />
-      </div>
+      <section className="contributions-bundle">
+        <div className="introduction-bleed-margins" />
+        <div className="contributions-bundle__content gu-content-margin">
+          <div className="introduction-bleed" />
+          <ContributionsBundle />
+        </div>
+      </section>
+      <section className="contributions-legal">
+        <div className="contributions-legal__content gu-content-margin">
+          <p className="contributions-legal__segment">
+            The ultimate owner of the Guardian is The Scott Trust Limited,
+            whose role it is to secure the editorial and financial independence
+            of the Guardian in perpetuity. Reader contributions support the
+            Guardian’s journalism.&nbsp;
+          </p>
+          <p className="contributions-legal__segment">
+            Please note that your support of the Guardian’s journalism does not
+            constitute a charitable donation, as such your contribution is not
+            eligible for Gift Aid in the UK nor a tax-deduction elsewhere.&nbsp;
+          </p>
+          <p className="contributions-legal__segment">
+            If you have any questions about contributing to the Guardian,
+            please contact us here.&nbsp;
+          </p>
+        </div>
+      </section>
       <SimpleFooter />
     </div>
   </Provider>
