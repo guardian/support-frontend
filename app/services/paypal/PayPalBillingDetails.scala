9f9a0ec41b1d1c0b55b83ee3031f8874716cf75c
rupertbates <rupert.bates@guardian.co.uk>
2017-07-20 11:27:06 +0100

    Use Currency class in PayPalBillingDetails

--- app/services/paypal/PayPalBillingDetails.scala
+++ app/services/paypal/PayPalBillingDetails.scala
@@ -2,9 +2,11 @@ package services.paypal
 
 import codecs.Codec
 import codecs.CirceDecoders.deriveCodec
+import com.gu.i18n.Currency
 
-case class PayPalBillingDetails(amount: Float, billingPeriod: String, currency: String)
+case class PayPalBillingDetails(amount: Float, billingPeriod: String, currency: Currency)
 
 object PayPalBillingDetails {
+  import codecs.CirceDecoders.{encodeCurrency, decodeCurrency}
   implicit val codec: Codec[PayPalBillingDetails] = deriveCodec
 }
