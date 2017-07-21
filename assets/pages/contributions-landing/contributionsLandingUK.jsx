61542c0b9e1ccd4ddee92322d55a715e07594582
Jamie Byers <Ap0c@users.noreply.github.com>
2017-07-21 16:18:13 +0100

    Sorted out widths at wider breakpoints.

--- assets/pages/contributions-landing/contributionsLandingUK.jsx
+++ assets/pages/contributions-landing/contributionsLandingUK.jsx
@@ -60,21 +60,23 @@ const content = (
       </section>
       <section className="contributions-legal gu-content-filler">
         <div className="contributions-legal__content gu-content-filler__inner">
-          <p className="contributions-legal__segment">
-            The ultimate owner of the Guardian is The Scott Trust Limited,
-            whose role it is to secure the editorial and financial independence
-            of the Guardian in perpetuity. Reader contributions support the
-            Guardian’s journalism.&nbsp;
-          </p>
-          <p className="contributions-legal__segment">
-            Please note that your support of the Guardian’s journalism does not
-            constitute a charitable donation, as such your contribution is not
-            eligible for Gift Aid in the UK nor a tax-deduction elsewhere.&nbsp;
-          </p>
-          <p className="contributions-legal__segment">
-            If you have any questions about contributing to the Guardian,
-            please contact us here.&nbsp;
-          </p>
+          <div className="contributions-legal__copy">
+            <p className="contributions-legal__segment">
+              The ultimate owner of the Guardian is The Scott Trust Limited,
+              whose role it is to secure the editorial and financial independence
+              of the Guardian in perpetuity. Reader contributions support the
+              Guardian’s journalism.&nbsp;
+            </p>
+            <p className="contributions-legal__segment">
+              Please note that your support of the Guardian’s journalism does not
+              constitute a charitable donation, as such your contribution is not
+              eligible for Gift Aid in the UK nor a tax-deduction elsewhere.&nbsp;
+            </p>
+            <p className="contributions-legal__segment">
+              If you have any questions about contributing to the Guardian,
+              please contact us here.&nbsp;
+            </p>
+          </div>
         </div>
       </section>
       <SimpleFooter />
