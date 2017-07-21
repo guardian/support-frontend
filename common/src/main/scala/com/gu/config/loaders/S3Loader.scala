6444c1badcb119c051f2fa685a41ca1ac3c295ad
rupertbates <rupert.bates@guardian.co.uk>
2017-07-21 11:04:25 +0100

    Extra logging for config

--- common/src/main/scala/com/gu/config/loaders/S3Loader.scala
+++ common/src/main/scala/com/gu/config/loaders/S3Loader.scala
@@ -11,12 +11,13 @@ import scala.io.{BufferedSource, Source}
 
 class S3Loader extends PrivateConfigLoader with LazyLogging {
   override def load(stage: Stage, public: Config): Config = {
-    logger.info(s"Loading config from S3 for stage: $stage")
+    val uri: AmazonS3URI = new AmazonS3URI(public.getString(s"config.private.s3.$stage"))
+    logger.info(s"Loading config from S3 for stage: $stage from $uri")
     val s3Client = AmazonS3ClientBuilder
       .standard()
       .withCredentials(CredentialsProvider)
       .build()
-    val uri: AmazonS3URI = new AmazonS3URI(public.getString(s"config.private.s3.$stage"))
+
     val s3Object: S3Object = s3Client.getObject(uri.getBucket, uri.getKey)
 
     val source: BufferedSource = Source.fromInputStream(s3Object.getObjectContent)
