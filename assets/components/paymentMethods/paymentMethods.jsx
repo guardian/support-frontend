049337251437bcd4a8d2261f48b99bb6cde0694d
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-24 14:42:42 +0100

    [oneoff-contributions-checkout-server-side] (Refactor) Extracted paymentMethods and validation into a shared component and helper.

---
+++ assets/components/paymentMethods/paymentMethods.jsx
@@ -0,0 +1,72 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+import StripePopUpButton from 'components/stripePopUpButton/stripePopUpButton';
+import PaypalExpressButton from 'components/payPalExpressButton/payPalExpressButton';
+import ErrorMessage from 'components/errorMessage/errorMessage';
+
+
+// ----- Types ----- //
+
+type PropTypes = {
+  email: string,
+  firstName: string,
+  lastName: string,
+  error: ?string,
+  payPalButtonExists: boolean,
+  stripeCallback: Function,
+  paypalCallback: Function,
+};
+
+
+// ----- Component ----- //
+
+function PaymentMethods(props: PropTypes) {
+
+  let errorMessage = '';
+  let stripeButton = <StripePopUpButton email={props.email} callback={props.stripeCallback} />;
+  let payPalButton = '';
+
+  if (props.payPalButtonExists) {
+    payPalButton = <PaypalExpressButton callback={props.paypalCallback} />;
+  }
+
+  if (props.firstName === '' || props.lastName === '') {
+    errorMessage = <ErrorMessage message={'Please fill in all the fields above.'} />;
+    stripeButton = '';
+    payPalButton = '';
+  } else if (props.error !== null) {
+    errorMessage = <ErrorMessage message={'There was an error processing your payment. Please\u00a0try\u00a0again\u00a0later.'} />;
+  }
+
+  return (
+    <section className="payment-methods">
+      {errorMessage}
+      {stripeButton}
+      {payPalButton}
+    </section>
+  );
+
+}
+
+
+// ----- Map State/Props ----- //
+
+function mapStateToProps(state) {
+
+  return {
+    email: state.user.email,
+    firstName: state.user.firstName,
+    lastName: state.user.lastName,
+  };
+
+}
+
+
+// ----- Exports ----- //
+
+export default connect(mapStateToProps)(PaymentMethods);
---
+++ assets/helpers/validation.js
@@ -0,0 +1,30 @@
+// @flow
+
+// ----- Imports ----- //
+
+import { roundDp } from 'helpers/utilities';
+
+
+// ----- Functions ----- //
+
+function validateContribution(amount: string, lowBound: number, upperBound: number, defaultValue: number): number {
+
+  const numericAmount = Number(amount);
+
+  if (
+    isNaN(numericAmount) ||
+    numericAmount < lowBound ||
+    numericAmount > upperBound
+  ) {
+    return defaultValue;
+  }
+
+  // Converts to 2.d.p.
+  return roundDp(numericAmount);
+
+}
+
+
+// ----- Exports ----- //
+
+export default validateContribution;
