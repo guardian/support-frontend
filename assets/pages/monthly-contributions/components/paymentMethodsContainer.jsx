b0cbadf552a3e83a86cd89f750debe850c9f325e
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-26 11:39:27 +0100

    [payment-methods-refator] Added two PaymentMethods container in order to link the redux state with the shared PaymentMethods.

---
+++ assets/pages/monthly-contributions/components/paymentMethodsContainer.jsx
@@ -0,0 +1,39 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+import { connect } from 'react-redux';
+import PaymentMethods from 'components/paymentMethods/paymentMethods';
+
+
+// ----- Types ----- //
+
+type PropTypes = {
+  email: string,
+  firstName: string,
+  lastName: string,
+  error: ?string,
+  payPalButtonExists: boolean,
+  stripeCallback: Function,
+  payPalCallback: Function,
+};
+
+
+// ----- Map State/Props ----- //
+
+function mapStateToProps(state) {
+
+  return {
+    email: state.user.email,
+    firstName: state.user.firstName,
+    lastName: state.user.lastName,
+    error: state.monthlyContrib.error,
+  };
+
+}
+
+
+// ----- Exports ----- //
+
+export default connect(mapStateToProps)(PaymentMethods);
