6157b8f7786a5b2cde98133d59396cc2422b1bb7
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-27 19:02:03 +0100

    [tracking-query-param] Refactor of the addQueryParamsURL

--- assets/helpers/url.js
+++ assets/helpers/url.js
@@ -10,15 +10,29 @@ const getQueryParameter = (paramName: string, defaultValue?: string): ?string =>
 
 };
 
-const addQueryParamToURL = (urlStr: string, paramsKey: string, paramsValue: ?string): string => {
-  const url = new URL(urlStr);
-  const params = new URLSearchParams(url.search);
+
+const addQueryParamToURL = (str: string, paramsKey: string, paramsValue: ?string): string => {
+  // str can be a full URL (i.e. https://...?q1=a) or a path (i.e. '/...?q1=a).
+  // Whatever is the case, I am interested in the query params i.e. the part after the '?'
+  const strParts = str.split('?');
+
+  // Save the first part of the str since is my protocol://host:port or path.
+  const strInit = strParts[0];
+
+  // Drop the URL.
+  strParts.splice(0, 1);
+
+  // I concatenate the rest of the array's values since all of them are query params.
+  const params = strParts.reduce((a,b) => `${a}?${b}`, '');
+
+  // Add the new param to the list of params.
+  const paramsObj = new URLSearchParams(params);
 
   if (paramsValue !== undefined && paramsValue !== null) {
-    params.set(paramsKey, paramsValue);
+    paramsObj.set(paramsKey, paramsValue);
   }
 
-  return `${url.toString()}?${params.toString()}`;
+  return `${strInit}?${paramsObj.toString()}`;
 };
 
 // ----- Exports ----- //
