e1498fdb705a395a59d04831661e5be8574d137c
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-27 17:22:41 +0100

    [tracking-query-param] Change the init state of intcmp

--- assets/helpers/intCmp.js
+++ assets/helpers/intCmp.js
@@ -3,7 +3,7 @@
 // ----- Reducer ----- //
 
 // Since nothing change the INTCMP, this reducer does not handle any action.
-const intCmpReducer = (state: string = ''): string => state;
+const intCmpReducer = (state: ?string = null): ?string => state;
 
 export {
   intCmpReducer, // eslint-disable-line import/prefer-default-export
