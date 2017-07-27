bc26d3dd869328e1d517958991c7053639cdd673
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-07-27 17:22:18 +0100

    [tracking-query-param] Added a new function to url helper.

--- assets/helpers/url.js
+++ assets/helpers/url.js
@@ -2,7 +2,7 @@
 
 // ----- Functions ----- //
 
-const getQueryParameter = (paramName: string, defaultValue: string): string => {
+const getQueryParameter = (paramName: string, defaultValue?: string): ?string => {
 
   const params = new URLSearchParams(window.location.search);
 
@@ -10,6 +10,19 @@ const getQueryParameter = (paramName: string, defaultValue: string): string => {
 
 };
 
-// ----- Exports ----- //
+const addQueryParamToURL = (urlStr: string, paramsKey: string, paramsValue: ?string): string => {
+  const url = new URL(urlStr);
+  const params = new URLSearchParams(url.search);
 
-export default getQueryParameter;
+  if (paramsValue !== undefined && paramsValue !== null) {
+    params.set(paramsKey, paramsValue);
+  }
+
+  return `${url.toString()}?${params.toString()}`;
+};
+
+// ----- Exports ----- //
+export {
+  getQueryParameter,
+  addQueryParamToURL,
+};
