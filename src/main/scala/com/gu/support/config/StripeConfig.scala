f0315de463f709f4330ca1bb64bd6c9768c8e501
rupertbates <rupert.bates@guardian.co.uk>
2017-07-27 16:02:53 +0100

    Enable multiple Stripe config sections
    To support separate Stripe configs for one-off and recurring

--- src/main/scala/com/gu/support/config/StripeConfig.scala
+++ src/main/scala/com/gu/support/config/StripeConfig.scala
@@ -4,9 +4,9 @@ import com.typesafe.config.Config
 
 case class StripeConfig(secretKey: String, publicKey: String) extends TouchpointConfig
 
-class StripeConfigProvider(config: Config, defaultStage: Stage) extends TouchpointConfigProvider[StripeConfig](config, defaultStage) {
+class StripeConfigProvider(config: Config, defaultStage: Stage, prefix: String = "stripe") extends TouchpointConfigProvider[StripeConfig](config, defaultStage) {
   def fromConfig(config: Config): StripeConfig = StripeConfig(
-    secretKey = config.getString(s"stripe.api.key.secret"),
-    publicKey = config.getString(s"stripe.api.key.public")
+    secretKey = config.getString(s"$prefix.api.key.secret"),
+    publicKey = config.getString(s"$prefix.api.key.public")
   )
 }
