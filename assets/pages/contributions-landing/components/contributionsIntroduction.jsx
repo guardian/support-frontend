c60df3b6e69772b7830ff2faef1012e8b1ad5670
Jamie Byers <Ap0c@users.noreply.github.com>
2017-08-03 18:24:54 +0100

    Styled the in-between breakpoints.

--- assets/pages/contributions-landing/components/contributionsIntroduction.jsx
+++ assets/pages/contributions-landing/components/contributionsIntroduction.jsx
@@ -12,13 +12,13 @@ export default function ContributionsIntroduction() {
   return (
     <div className="contributions-introduction">
       <div className="contributions-introduction__primary">
-        <h1>support the Guardian&#39;s</h1>
-        <h1>quality journalism</h1>
+        <h1>support the&nbsp;Guardian&#39;s</h1>
+        <h1>quality&nbsp;journalism</h1>
         <h1>and independent voice</h1>
       </div>
       <div className="contributions-introduction__secondary">
         <h1>make a one-off</h1>
-        <h1>contribution today</h1>
+        <h1>contribution <span className="contributions-introduction__indent">today</span></h1>
       </div>
     </div>
   );
