648e9f854e690adc00c3247d8d2281f7c5f28ae4
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-08-09 14:41:56 +0100

    [payment-logos] Fixing linting errors.

--- assets/components/inlinePaymentLogos/inlinePaymentLogos.jsx
+++ assets/components/inlinePaymentLogos/inlinePaymentLogos.jsx
@@ -5,18 +5,16 @@
 import React from 'react';
 import Svg from 'components/svg/svg';
 
-import type { Children } from 'react';
-
 // ----- Component ----- //
 
 export default function InlinePaymentLogos() {
 
   return (
     <div className={'inline-payment-logos'}>
-      <Svg svgName='visa-logo' />
-      <Svg svgName='mastercard-logo' />
-      <Svg svgName='paypal-logo' />
-      <Svg svgName='amex-logo' />
+      <Svg svgName="visa-logo" />
+      <Svg svgName="mastercard-logo" />
+      <Svg svgName="paypal-logo" />
+      <Svg svgName="amex-logo" />
     </div>
   );
 }
