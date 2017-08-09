c9f178230042be4997a69c1ee555624a6fba2c2e
rupertbates <rupert.bates@guardian.co.uk>
2017-08-09 09:56:08 +0100

    Use one parameterised template for lambdas rather than one for each

--- cloud-formation/src/build-cfn.js
+++ cloud-formation/src/build-cfn.js
@@ -5,28 +5,28 @@ const Mustache = require('mustache'),
 
 //We need to deal with the statemachine and its retries separately because they are
 //converted to Json before inclusion in the final cfn output
-const stateMachine = readFile('state-machine.yaml');
-const stateMachineYaml = Mustache.render(stateMachine, {}, {stateMachineRetries: readFile('state-machine-retries.yaml')});
+const stateMachine = loadTemplate('state-machine.yaml');
+const stateMachineYaml = Mustache.render(stateMachine, {}, {stateMachineRetries: loadTemplate('state-machine-retries.yaml')});
 const stateMachineJson = JSON.stringify(yaml.load(stateMachineYaml));
 
 const partials = {
-    contributionCompletedLambda: readFile('lambdas/contributionCompletedLambda.yaml'),
-    createPaymentMethodLambda: readFile('lambdas/createPaymentMethodLambda.yaml'),
-    createSalesforceContactLambda: readFile('lambdas/createSalesforceContactLambda.yaml'),
-    createZuoraSubscriptionLambda: readFile('lambdas/createZuoraSubscriptionLambda.yaml'),
-    failureHandlerLambda: readFile('lambdas/failureHandlerLambda.yaml'),
-    sendThankYouEmailLambda: readFile('lambdas/sendThankYouEmailLambda.yaml'),
-    updateMembersDataAPILambda: readFile('lambdas/updateMembersDataAPILambda.yaml'),
-    environmentVariables: readFile('environment-variables.yaml'),
-    stateMachine: "           " + stateMachineJson
+    lambda: loadTemplate('lambda.yaml'),
+    environmentVariables: loadTemplate('environment-variables.yaml'),
+    stateMachine: indent(11, stateMachineJson)
 };
 
-const main = readFile('cfn-template.yaml');
-const output = Mustache.render(main, {}, partials);
-console.log(output);
+const main = loadTemplate('cfn-template.yaml');
+const output = Mustache.render(main, JSON.parse(readFile('cfn.json')), partials);
 fs.writeFileSync('../target/cfn.yaml', output, 'utf-8');
 
 
+function loadTemplate(filename) {
+    return readFile('template/' + filename);
+}
 function readFile(filename) {
     return fs.readFileSync(filename, 'utf-8');
+}
+
+function indent(number, text) {
+    return ' '.repeat(number) + text;
 }
