366f0d7ace49895581c3768ab4fc5ac5c5477580
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-08-09 16:56:51 +0100

    [one-off-paypal-button] Adding PayPal P logo.

--- assets/components/payPalContributionButton/payPalContributionButton.jsx
+++ assets/components/payPalContributionButton/payPalContributionButton.jsx
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import Svg from 'components/svg/svg';
 
 import { payPalContributionsButtonClicked, paypalContributionsRedirect } from 'helpers/payPalContributionsCheckout/payPalContributionsCheckoutActions';
 
@@ -27,10 +28,9 @@ const PayPalContributionButton = (props: PropTypes) => {
 
   return (
     <button className={'component-paypal-contribution-button'} onClick={props.payWithPayPal}>
-      PayPal checkout
-      <svg className="component-paypal-contribution-button__arrow" xmlns="http://www.w3.org/2000/svg" width="20" height="17.89" viewBox="0 0 20 17.89">
-        <path d="M20 9.35l-9.08 8.54-.86-.81 6.54-7.31H0V8.12h16.6L10.06.81l.86-.81L20 8.51v.84zm20-.81L49.08 0l.86.81-6.54 7.31H60v1.65H43.4l6.54 7.31-.86.81L40 9.39v-.85z" />
-      </svg>
+      <Svg svgName="paypal-p-logo" />
+      <span>PayPal checkout</span>
+      <Svg svgName="arrow-right-straight" />
     </button>);
 };
 
