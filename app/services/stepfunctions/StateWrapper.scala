adc5fd53879c81ee52f0373464e30011a060726e
davidfurey <david.furey@guardian.co.uk>
2017-08-18 12:26:02 +0100

    Add execution error codec

--- app/services/stepfunctions/StateWrapper.scala
+++ app/services/stepfunctions/StateWrapper.scala
@@ -2,16 +2,18 @@ package services.stepfunctions
 
 import java.util.Base64
 
-import com.gu.support.workers.model.JsonWrapper
+import com.gu.support.workers.model.{ExecutionError, JsonWrapper}
 import io.circe.Encoder
 import io.circe.generic.semiauto.deriveEncoder
 import io.circe.syntax._
 
 class StateWrapper(encryption: EncryptionProvider) {
+  implicit private val executionErrorEncoder = deriveEncoder[ExecutionError]
+
   implicit private val wrapperEncoder = deriveEncoder[JsonWrapper]
 
   def wrap[T](state: T)(implicit encoder: Encoder[T]): String = {
-    JsonWrapper(encodeState(state)).asJson.noSpaces
+    JsonWrapper(encodeState(state), None).asJson.noSpaces
   }
 
   private def encodeState[T](state: T)(implicit encoder: Encoder[T]): String = encodeToBase64String(encryption.encrypt(state.asJson.noSpaces))
