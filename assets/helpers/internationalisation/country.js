c2af5d38ede14092889795e3893fd6ad3e029587
Jamie Byers <Ap0c@users.noreply.github.com>
2017-08-23 11:59:01 +0100

    Added US states to the country helper.

--- assets/helpers/internationalisation/country.js
+++ assets/helpers/internationalisation/country.js
@@ -1,11 +1,87 @@
 // @flow
 
+// ----- Imports ----- //
+
 import { getQueryParameter } from 'helpers/url';
 import * as cookie from './../cookie';
+
+
+// ----- Types ----- //
 
 export type IsoCountry =
   | 'GB'
   | 'US';
+
+type usState = {
+  code: string,
+  name: string,
+};
+
+
+// ----- Setup ----- //
+
+const usStates: usState[] = [
+  { code: 'AA', name: 'Armed Forces America' },
+  { code: 'AE', name: 'Armed Forces' },
+  { code: 'AP', name: 'Armed Forces Pacific' },
+  { code: 'AK', name: 'Alaska' },
+  { code: 'AL', name: 'Alabama' },
+  { code: 'AR', name: 'Arkansas' },
+  { code: 'AZ', name: 'Arizona' },
+  { code: 'CA', name: 'California' },
+  { code: 'CO', name: 'Colorado' },
+  { code: 'CT', name: 'Connecticut' },
+  { code: 'DC', name: 'Washington DC (District of Columbia)' },
+  { code: 'DE', name: 'Delaware' },
+  { code: 'FL', name: 'Florida' },
+  { code: 'GA', name: 'Georgia' },
+  { code: 'GU', name: 'Guam' },
+  { code: 'HI', name: 'Hawaii' },
+  { code: 'IA', name: 'Iowa' },
+  { code: 'ID', name: 'Idaho' },
+  { code: 'IL', name: 'Illinois' },
+  { code: 'IN', name: 'Indiana' },
+  { code: 'KS', name: 'Kansas' },
+  { code: 'KY', name: 'Kentucky' },
+  { code: 'LA', name: 'Louisiana' },
+  { code: 'MA', name: 'Massachusetts' },
+  { code: 'MD', name: 'Maryland' },
+  { code: 'ME', name: 'Maine' },
+  { code: 'MI', name: 'Michigan' },
+  { code: 'MN', name: 'Minnesota' },
+  { code: 'MO', name: 'Missouri' },
+  { code: 'MS', name: 'Mississippi' },
+  { code: 'MT', name: 'Montana' },
+  { code: 'NC', name: 'North Carolina' },
+  { code: 'ND', name: 'North Dakota' },
+  { code: 'NE', name: 'Nebraska' },
+  { code: 'NH', name: 'New Hampshire' },
+  { code: 'NJ', name: 'New Jersey' },
+  { code: 'NM', name: 'New Mexico' },
+  { code: 'NV', name: 'Nevada' },
+  { code: 'NY', name: 'New York' },
+  { code: 'OH', name: 'Ohio' },
+  { code: 'OK', name: 'Oklahoma' },
+  { code: 'OR', name: 'Oregon' },
+  { code: 'PA', name: 'Pennsylvania' },
+  { code: 'PR', name: 'Puerto Rico' },
+  { code: 'RI', name: 'Rhode Island' },
+  { code: 'SC', name: 'South Carolina' },
+  { code: 'SD', name: 'South Dakota' },
+  { code: 'TN', name: 'Tennessee' },
+  { code: 'TX', name: 'Texas' },
+  { code: 'UT', name: 'Utah' },
+  { code: 'VA', name: 'Virginia' },
+  { code: 'VI', name: 'Virgin Islands' },
+  { code: 'VT', name: 'Vermont' },
+  { code: 'WA', name: 'Washington' },
+  { code: 'WI', name: 'Wisconsin' },
+  { code: 'WV', name: 'West Virginia' },
+  { code: 'WY', name: 'Wyoming' },
+];
+
+
+// ----- Functions ----- /
 
 function fromString(s: string): ?IsoCountry {
   switch (s.toLowerCase()) {
@@ -49,10 +125,17 @@ function fromGeolocation(): ?IsoCountry {
   return null;
 }
 
-export function detect(): IsoCountry {
+function detect(): IsoCountry {
   const country = fromPath() || fromQueryParameter() || fromCookie() || fromGeolocation() || 'GB';
   // cookie.set('GU_country', country, 7);
   // Always return GB because we aren't ready to support US quite yet
-  return 'GB' || country;
+  return country;
 }
 
+
+// ----- Exports ----- //
+
+export {
+  detect,
+  usStates,
+};
