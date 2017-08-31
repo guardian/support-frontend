b485f5a383637e0a09da8a50d7851a8ce9b3364b
rupertbates <rupert.bates@guardian.co.uk>
2017-08-31 14:03:35 +0100

    Add BillingPeriods to the model

--- src/main/scala/com/gu/support/workers/model/monthlyContributions/Contribution.scala
+++ src/main/scala/com/gu/support/workers/model/monthlyContributions/Contribution.scala
@@ -1,8 +1,10 @@
 package com.gu.support.workers.model.monthlyContributions
 
 import com.gu.i18n.Currency
+import com.gu.support.workers.model.BillingPeriod
 
 case class Contribution(
   amount: BigDecimal,
-  currency: Currency
+  currency: Currency,
+  billingPeriod: BillingPeriod
 )
