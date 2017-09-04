c4b066548b09ba9b76ddd304a900ac41a1fd5ff9
Sam Desborough <sam.desborough@gmail.com>
2017-09-04 11:32:11 +0100

    Remove redundant test

--- src/test/scala/services/OphanServiceSpec.scala
+++ src/test/scala/services/OphanServiceSpec.scala
@@ -22,32 +22,5 @@ class OphanServiceSpec extends WordSpec with MustMatchers {
       tests.asAbTestInfo.asJson.noSpaces mustBe
         """{"test1":{"variantName":"control1"},"test2":{"variantName":"control2"}}"""
     }
-
-//    "encode query parameters correctly" in {
-//      import com.netaporter.uri.Uri.parse
-//
-//      val acquisition = Acquisition(
-//        product = Contribution,
-//        paymentFrequency = PaymentFrequency.OneOff,
-//        currency = "GBP",
-//        amount = 10.12,
-//        amountInGBP = Some(10.12),
-//        paymentProvider = Some(PaymentProvider.Stripe),
-//        campaignCode = Some(Set("woot", "poot")),
-//        abTests = Some(Set(ExampleABTest("test1", "control")).asAbTestInfo),
-//        countryCode = Some("US"),
-//        referrerPageViewId = Some("refpivd"),
-//        referrerUrl = Some("refurl"),
-//        componentId = Some("my-great-component"),
-//        componentTypeV2 = Some(AcquisitionsEpic),
-//        source = Some(GuardianWeb)
-//      )
-//
-//      val paramsFromEvent: Map[String, String] = AcquisitionUtils.queryParamsFor(acquisition)
-//      val url: Uri = parse(ophanService.url(paramsFromEvent).toString)
-//      val paramsFromUrl: Map[String, String] = url.query.params.toMap.collect { case (k, Some(v)) => k -> v }
-//
-//      paramsFromEvent mustEqual paramsFromUrl
-//    }
   }
 }
