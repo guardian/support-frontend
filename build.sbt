894874cb3e8172c5f32434f0a78a3b2393266f9e
Sam Desborough <sam.desborough@gmail.com>
2017-09-05 14:04:47 +0100

    Add AbTestConverter.instance

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 
 name := "acquisition-event-producer"
 
-version := "0.2.0"
+version := "0.2.1"
 
 scalaVersion := "2.11.11"
 
--- src/main/scala/utils/AbTestUtils.scala
+++ src/main/scala/utils/AbTestUtils.scala
@@ -7,3 +7,9 @@ import simulacrum._
 @typeclass trait AbTestConverter[A] {
   def asAbTest(a: A): AbTest
 }
+
+object AbTestConverter {
+  def instance[A](f: A => AbTest): AbTestConverter[A] = new AbTestConverter[A] {
+    override def asAbTest(a: A): AbTest = f(a)
+  }
+}
