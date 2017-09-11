b06fdb54b6719e9697d3b83dda5af071720a2c3f
James Byers <Ap0c@users.noreply.github.com>
2017-09-11 18:34:36 +0100

    Added a support-landing variant of the bundles landing page.

--- assets/pages/bundles-landing/bundlesLanding.jsx
+++ assets/pages/bundles-landing/bundlesLanding.jsx
@@ -9,6 +9,7 @@ import { Provider } from 'react-redux';
 import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
 import SimpleFooter from 'components/footers/simpleFooter/simpleFooter';
 
+import { getQueryParameter } from 'helpers/url';
 import { init as pageInit } from 'helpers/page/page';
 import { setIntCmp } from 'helpers/page/pageActions';
 
@@ -18,6 +19,9 @@ import WhySupport from './components/WhySupport';
 import WaysOfSupport from './components/WaysOfSupport';
 import reducer from './bundlesLandingReducers';
 import { trackOphan } from '../../helpers/abtest';
+
+// New Design Test
+import supportLanding from './supportLanding';
 
 
 // ----- Redux Store ----- //
@@ -42,17 +46,24 @@ if (participations.addAnnualContributions) {
 
 // ----- Render ----- //
 
-const content = (
-  <Provider store={store}>
-    <div>
-      <SimpleHeader />
-      <Introduction />
-      <Bundles />
-      <WhySupport />
-      {store.getState().common.campaign === 'baseline_test' ? '' : <WaysOfSupport />}
-      <SimpleFooter />
-    </div>
-  </Provider>
-);
+let content = null;
+
+// New Design Test: Replace with check for variant when test goes live.
+if (getQueryParameter('newDesigns', 'false') === 'true') {
+  content = supportLanding(store);
+} else {
+  content = (
+    <Provider store={store}>
+      <div>
+        <SimpleHeader />
+        <Introduction />
+        <Bundles />
+        <WhySupport />
+        {store.getState().common.campaign === 'baseline_test' ? '' : <WaysOfSupport />}
+        <SimpleFooter />
+      </div>
+    </Provider>
+  );
+}
 
 ReactDOM.render(content, document.getElementById('bundles-landing-page'));
