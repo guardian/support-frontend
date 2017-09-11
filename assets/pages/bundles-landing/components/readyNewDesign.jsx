cda63d1a1006f02db620bc94814cb14a3501d045
James Byers <Ap0c@users.noreply.github.com>
2017-09-11 18:35:41 +0100

    Added components for the support landing page.

---
+++ assets/pages/bundles-landing/components/readyNewDesign.jsx
@@ -0,0 +1,22 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+import InfoSection from 'components/infoSection/infoSection';
+
+
+// ----- Component ----- //
+
+export default function Subscribe() {
+
+  return (
+    <div className="subscribe-new-design gu-content-filler">
+      <InfoSection className="subscribe-new-design__content gu-content-filler__inner">
+        <h2 className="subscribe-new-design__heading">ready to support the Guardian?</h2>
+      </InfoSection>
+    </div>
+  );
+
+}
---
+++ assets/pages/bundles-landing/components/whySupportNewDesign.jsx
@@ -0,0 +1,64 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+import InfoSection from 'components/infoSection/infoSection';
+import CtaCircle from 'components/ctaCircle/ctaCircle';
+
+
+// ----- Component ----- //
+
+export default function WhySupport() {
+
+  return (
+    <div className="why-support-new-design">
+      <InfoSection heading="why support?" className="why-support-new-design__content gu-content-margin">
+        <div className="why-support-new-design__wrapper">
+          <h1 className="why-support-new-design__heading why-support-new-design__heading--edits">
+            <span>no one edits</span>
+            <span>our editor</span>
+          </h1>
+          <p className="why-support-new-design__copy">
+            With no billionaire owner pulling our strings, nobody, be they
+            shareholders or advertisers, can tell us to censor or drop a story.
+          </p>
+          <CtaCircle text="Find out more about our independence" />
+          <h1 className="why-support-new-design__heading why-support-new-design__heading--advertising">
+            <span>advertising revenues</span>
+            <span>are falling</span>
+          </h1>
+          <p className="why-support-new-design__copy">
+            Our quality, investigative journalism takes a lot of time and money
+            to produce. And while the Scott Trust safeguards our independence, its
+            funds are limited.
+          </p>
+          <p className="why-support-new-design__copy why-support-new-design__copy--no-border">
+            With ad revenues falling across the media, we need our readers&#39;
+            support to secure our future.
+          </p>
+          <CtaCircle text="Find out more about the Scott Trust" />
+          <h1 className="why-support-new-design__heading why-support-new-design__heading--paywall">
+            <span>we haven&#39;t put up</span>
+            <span>a paywall</span>
+          </h1>
+          <p className="why-support-new-design__copy">
+            We believe the truth should be open to everyone, regardless of their
+            income or background
+          </p>
+          <h1 className="why-support-new-design__heading why-support-new-design__heading--perspective">
+            <span>we believe</span>
+            <span>our perspective matters</span>
+          </h1>
+          <p className="why-support-new-design__copy">
+            Independent, progressive journalism is for everyone&#39;s benefit. And
+            if that&#39;s a view you share, then join us today and help ensure our
+            voice continues to be heard.
+          </p>
+        </div>
+      </InfoSection>
+    </div>
+  );
+
+}
