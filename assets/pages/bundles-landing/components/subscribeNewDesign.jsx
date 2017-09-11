a99b9fac1dd6933665875bb561508762ef26e430
James Byers <Ap0c@users.noreply.github.com>
2017-09-11 18:35:41 +0100

    Added components for the support landing page.

---
+++ assets/pages/bundles-landing/components/subscribeNewDesign.jsx
@@ -0,0 +1,21 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+import InfoSection from 'components/infoSection/infoSection';
+
+
+// ----- Component ----- //
+
+export default function Subscribe() {
+
+  return (
+    <div className="subscribe-new-design">
+      <InfoSection heading="subscribe" className="subscribe-new-design__content gu-content-margin">
+      </InfoSection>
+    </div>
+  );
+
+}
