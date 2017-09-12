3aed006550b1f256035f6db5e20a9ac8563881f0
James Byers <Ap0c@users.noreply.github.com>
2017-09-12 18:38:29 +0100

    Added the subscriptions bundles.

---
+++ assets/pages/bundles-landing/components/subscriptionBundleNewDesign.jsx
@@ -0,0 +1,38 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+import CtaCircle from 'components/ctaCircle/ctaCircle';
+
+
+// ----- Types ----- //
+
+type PropTypes = {
+  heading: string,
+  price: string,
+  from: boolean,
+  copy: string,
+};
+
+
+// ----- Component ----- //
+
+export default function SubscriptionBundle(props: PropTypes) {
+
+  return (
+    <div className="subscription-bundle">
+      <figure></figure>
+      <h3 className="subscription-bundle__heading">{props.heading}</h3>
+      <h4 className="subscription-bundle__price">
+        {props.from ? 'from ' : ''}
+        <span className="subscription-bundle__price-amount">£{props.price}</span>
+        <span className="subscription-bundle__price-period"> /&nbsp;month</span>
+      </h4>
+      <p className="subscription-bundle__copy">{props.copy}</p>
+      <CtaCircle text="Find out more" />
+    </div>
+  );
+
+}
