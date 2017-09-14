fb12977c65db3720771292d1ffafeda51c167249
James Byers <Ap0c@users.noreply.github.com>
2017-09-14 16:48:59 +0100

    Added a couple of semicircles.

--- assets/pages/bundles-landing/components/readyNewDesign.jsx
+++ assets/pages/bundles-landing/components/readyNewDesign.jsx
@@ -15,7 +15,7 @@ export default function Ready() {
   return (
     <div className="ready-new-design gu-content-filler">
       <InfoSection className="ready-new-design__content gu-content-filler__inner">
-        <h2 className="ready-new-design__heading">ready to support the Guardian?</h2>
+        <h2 className="ready-new-design__heading">ready to support the&nbsp;Guardian?</h2>
         <CtaLink text="see supporter options" url="#" svgName="chevron-up" />
       </InfoSection>
     </div>
