f2344a33d4232de0b1a53868b1509804e96786d3
guy.dawson <guy.dawson@theguardian.com>
2017-09-19 15:36:16 +0100

    correctly format ab tests

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 
 name := "acquisition-event-producer"
 
-version := "1.0.0"
+version := "1.0.1"
 
 scalaVersion := "2.11.11"
 
--- src/main/scala/instances/abTestInfo.scala
+++ src/main/scala/instances/abTestInfo.scala
@@ -8,6 +8,6 @@ trait AbTestInfoInstances {
 
   implicit val abTestInfoEncoder: Encoder[AbTestInfo] = Encoder.instance { testInfo =>
     import io.circe.syntax._
-    testInfo.tests.map(t => t.name -> Map("variantName" -> t.variant)).toMap.asJson
+    testInfo.tests.map(test => Map("name" -> test.name, "variant" -> test.variant)).asJson
   }
 }
