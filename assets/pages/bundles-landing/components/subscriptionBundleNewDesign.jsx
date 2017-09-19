9d18508c0ed87a3b037f6268554f87bff82a0c06
James Byers <Ap0c@users.noreply.github.com>
2017-09-19 12:12:33 +0100

    Added images to the subscriptions bundles.

--- assets/pages/bundles-landing/components/subscriptionBundleNewDesign.jsx
+++ assets/pages/bundles-landing/components/subscriptionBundleNewDesign.jsx
@@ -5,6 +5,7 @@
 import React from 'react';
 
 import CtaCircle from 'components/ctaCircle/ctaCircle';
+import GridImage from 'components/gridImage/gridImage';
 
 
 // ----- Types ----- //
@@ -15,6 +16,7 @@ type PropTypes = {
   from: boolean,
   copy: string,
   ctaText: string,
+  image: string,
 };
 
 
@@ -24,7 +26,13 @@ export default function SubscriptionBundle(props: PropTypes) {
 
   return (
     <div className="subscription-bundle">
-      <figure></figure>
+      <GridImage
+        gridId={props.image}
+        srcSizes={[600]}
+        sizes="600px"
+        altText={`${props.heading} bundle`}
+        imgType="png"
+      />
       <h3 className="subscription-bundle__heading">{props.heading}</h3>
       <h4 className="subscription-bundle__price">
         {props.from ? 'from ' : ''}
