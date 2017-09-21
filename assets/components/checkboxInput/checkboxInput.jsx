8bdafeeaaf4b2f58d21816b7145bd7d005770fc7
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-09-21 17:10:07 +0100

    [permissions-parity] Added checkbox input component

---
+++ assets/components/checkboxInput/checkboxInput.jsx
@@ -0,0 +1,35 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+
+// ----- Types ----- //
+
+type PropTypes = {
+  id: string,
+  onChange: (event: Object) => void,
+  labelText?: string,
+};
+
+
+// ----- Component ----- //
+
+export default function CheckboxInput(props: PropTypes) {
+  let label = '';
+
+  if (props.labelText) {
+    label = <label For={props.id}>{props.labelText}</label>;
+  }
+  return (
+    <div className={`component-checkbox`}>
+      <input
+        id={props.id}
+        type="checkbox"
+        onChange={e => props.onChange(e.target.value)}
+      />
+      {label}
+    </div>
+  );
+}
