95debf76e69155fea77ca1b9e3cca4bb5c076d55
rupertbates <rupert.bates@guardian.co.uk>
2017-09-28 16:35:28 +0100

    Add contribution value to the datalayer

--- assets/helpers/tracking/googleTagManager.js
+++ assets/helpers/tracking/googleTagManager.js
@@ -1,26 +1,35 @@
 import uuidv4 from 'uuid';
 import { forCountry } from '../internationalisation/currency';
 import { get as getCookie } from '../cookie';
+import { getQueryParameter } from '../url';
 
-// Generates a uuid to anonymously identify this user in this session.
-// We need this to prevent page refreshes on conversion pages being
-// treated as new conversions
-export function getOrderId() {
-  let orderId = sessionStorage.getItem('orderId');
-  if (orderId === null) {
-    orderId = uuidv4();
-    sessionStorage.setItem('orderId', orderId);
+function getDataValue(name, generator) {
+  let value = sessionStorage.getItem(name);
+  if (value === null) {
+    value = generator();
+    sessionStorage.setItem(name, value);
   }
-  return orderId;
+  return value;
 }
 
-export function getCurrency() {
+function getCurrency() {
   const country = getCookie('GU_country');
   return forCountry(country).iso;
 }
 
+function getContributionValue() {
+  const param = getQueryParameter('contributionValue');
+  if (param) {
+    sessionStorage.setItem('contributionValue', parseInt(param, 10));
+  }
+  return sessionStorage.getItem('contributionValue') || 0;
+}
+
 window.googleTagManagerDataLayer = [{
-  orderId: getOrderId(),
-  currency: getCurrency(),
-  value: 1.00,
+  // orderId anonymously identifies this user in this session.
+  // We need this to prevent page refreshes on conversion pages being
+  // treated as new conversions
+  orderId: getDataValue('orderId', uuidv4),
+  currency: getDataValue('currency', getCurrency),
+  value: getContributionValue(),
 }];
