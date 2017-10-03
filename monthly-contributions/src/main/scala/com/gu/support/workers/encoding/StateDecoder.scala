5cc63874231b668bcf9cbe2aa5ae8689b3d1b0e0
rupertbates <rupert.bates@guardian.co.uk>
2017-10-03 17:16:34 +0100

    Refactor

--- monthly-contributions/src/main/scala/com/gu/support/workers/encoding/StateDecoder.scala
+++ monthly-contributions/src/main/scala/com/gu/support/workers/encoding/StateDecoder.scala
@@ -5,18 +5,29 @@ import java.util.Base64
 import com.gu.support.config.AwsConfig
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.util.Try
+
 object StateDecoder extends App with LazyLogging {
 
   if (args.length < 2) {
     println("Please pass in the AWS encryptionKeyId and state")
   }
 
-  def decodeState(key: String, state: String): Unit = {
-    val config = new AwsConfig(true, key)
+  val awsEncryptionKeyId = args(0)
+  val stepFunctionState = args(1)
+  val useEncryption = Try(args(2)).getOrElse("true").toBoolean
+
+  val decoded = Base64.getDecoder.decode(stepFunctionState)
+
+  if (useEncryption)
+    decryptState(decoded)
+  else
+    println(new String(decoded, utf8))
+
+  def decryptState(state: Array[Byte]): Unit = {
+    val config = new AwsConfig(true, awsEncryptionKeyId)
     val encoder = new AwsEncryptionProvider(config)
-    val decoded = Base64.getDecoder.decode(state)
-    println(encoder.decrypt(decoded))
+    println(encoder.decrypt(state))
   }
 
-  decodeState(args(0), args(1))
 }
