625fa729770e9b7b6030495844165d114f586815
Sam Desborough <sam.desborough@gmail.com>
2017-10-04 16:10:59 +0100

    Update CloudFormation templates to include SendAcquisitionEvent function
    I've also added a friendlier confirmation message to the validate
    cloudformation script, as it wasn't immediately obvious that the normal output
    of the `aws` command meant the template was valid.

--- scripts/validate-cloudformation.sh
+++ scripts/validate-cloudformation.sh
@@ -3,3 +3,8 @@
 ./build-cloudformation.sh
 
 aws --region eu-west-1 cloudformation validate-template --template-body file://../cloud-formation/target/cfn.yaml --profile membership
+
+if [[ $? == 0 ]]; then
+  echo -e "\nYour CloudFormation template is valid.";
+  exit 0;
+fi
