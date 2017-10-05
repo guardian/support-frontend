8ffde16d8d3b17d7a15a4ba0cdc69ddc9fa2ebd7
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-10-05 12:20:03 +0100

    [post-support-frontend-data] Renamed acquisition type to match aquisition tracking and defined ophanIds type

--- assets/helpers/tracking/acquisitions.js
+++ assets/helpers/tracking/acquisitions.js
@@ -4,24 +4,31 @@
 
 import { getQueryParameter } from 'helpers/url';
 import { deserialiseJsonObject } from 'helpers/utilities';
+import type { Participations } from 'helpers/abtest';
 import * as storage from 'helpers/storage';
 
 
 // ----- Types ----- //
 
-type AcquisitionABTest = {
+export type AcquisitionABTest = {
   name: string,
   variant: string,
 };
 
-export type Acquisition = {|
-  referrerPageviewId: ?string,
+export type OphanIds = {
+  pageviewId: ?string,
+  visitId: ?string,
+  browserId: ?string,
+};
+
+export type ReferrerAcquisitionData = {|
   campaignCode: ?string,
+  referrerPageviewId: ?string,
   referrerUrl: ?string,
   componentId: ?string,
   componentType: ?string,
   source: ?string,
-  abTests: AcquisitionABTest[],
+  abTest: AcquisitionABTest[],
 |};
 
 
@@ -55,7 +62,7 @@ export type Campaign = $Keys<typeof campaigns>;
 // ----- Functions ----- //
 
 // Retrieves the user's campaign, if known, from the campaign code.
-function getCampaign(acquisition: Acquisition): ?Campaign {
+function getCampaign(acquisition: ReferrerAcquisitionData): ?Campaign {
 
   const campaignCode = acquisition.campaignCode;
 
@@ -70,11 +77,11 @@ function getCampaign(acquisition: Acquisition): ?Campaign {
 }
 
 // Stores the acquisition data in sessionStorage.
-function storeAcquisition(acquisition: Acquisition): boolean {
+function storeAcquisition(referrerAcquisitionData: ReferrerAcquisitionData): boolean {
 
   try {
 
-    const serialised = JSON.stringify(acquisition);
+    const serialised = JSON.stringify(referrerAcquisitionData);
     storage.setSession(ACQUISITIONS_STORAGE_KEY, serialised);
 
     return true;
@@ -86,7 +93,7 @@ function storeAcquisition(acquisition: Acquisition): boolean {
 }
 
 // Reads the acquisition data from sessionStorage.
-function readAcquisition(): ?Acquisition {
+function readAcquisition(): ?ReferrerAcquisitionData {
 
   const stored = storage.getSession(ACQUISITIONS_STORAGE_KEY);
   return stored ? deserialiseJsonObject(stored) : null;
@@ -94,7 +101,7 @@ function readAcquisition(): ?Acquisition {
 }
 
 // Builds the acquisition object from data and other sources.
-function buildAcquisition(acquisitionData: Object = {}): Acquisition {
+function buildAcquisition(acquisitionData: Object = {}): ReferrerAcquisitionData {
 
   const referrerPageviewId = acquisitionData.referrerPageviewId ||
     getQueryParameter('REFPVID') ||
@@ -103,7 +110,7 @@ function buildAcquisition(acquisitionData: Object = {}): Acquisition {
   const campaignCode = acquisitionData.campaignCode ||
     getQueryParameter('INTCMP') ||
     null;
-
+  debugger
   return {
     referrerPageviewId,
     campaignCode,
@@ -111,22 +118,35 @@ function buildAcquisition(acquisitionData: Object = {}): Acquisition {
     componentId: acquisitionData.componentId || null,
     componentType: acquisitionData.componentType || null,
     source: acquisitionData.source || null,
-    abTests: acquisitionData.abTests || [],
+    abTest: acquisitionData.abTests || [],
   };
 
 }
 
+const participationsToAcquisitionABTest = (participations: Participations): AcquisitionABTest[] => {
+  const response: AcquisitionABTest[] = [];
+
+  Object.keys(participations).forEach((participation) => {
+    response.push({
+      name: participation,
+      variant: participations[(participation: any)],
+    });
+  });
+
+  return response;
+};
+
 // Returns the acquisition metadata, either from query param or sessionStorage.
 // Also stores in sessionStorage if not present or new from param.
-function getAcquisition(): Acquisition {
+function getAcquisition(): ReferrerAcquisitionData {
 
   const paramData = deserialiseJsonObject(getQueryParameter(ACQUISITIONS_PARAM) || '');
 
   // Read from param, or read from sessionStorage, or build minimal version.
-  const acquisition = buildAcquisition(paramData || readAcquisition() || undefined);
-  storeAcquisition(acquisition);
+  const referrerAcquisitionData = buildAcquisition(paramData || readAcquisition() || undefined);
+  storeAcquisition(referrerAcquisitionData);
 
-  return acquisition;
+  return referrerAcquisitionData;
 
 }
 
@@ -136,4 +156,5 @@ function getAcquisition(): Acquisition {
 export {
   getCampaign,
   getAcquisition,
+  participationsToAcquisitionABTest,
 };
