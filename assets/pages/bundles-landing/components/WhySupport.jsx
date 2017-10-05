f8d2d911addd54b578646f33f87a070b5d386a0d
James Byers <Ap0c@users.noreply.github.com>
2017-10-05 17:54:55 +0100

    Stripped out why support hero image.

--- assets/pages/bundles-landing/components/WhySupport.jsx
+++ assets/pages/bundles-landing/components/WhySupport.jsx
@@ -7,30 +7,10 @@ import React from 'react';
 import Svg from 'components/svg/svg';
 
 import BodyCopy from 'components/bodyCopy/bodyCopy';
-import GridPicture from 'components/gridPicture/gridPicture';
 import Video from 'components/video/video';
 
 
 // ----- Copy ----- //
-
-const heroImage = {
-  sources: [
-    {
-      gridId: 'bce7d14f7f837a4f6c854d95efc4b1eab93a8c65/0_0_5200_720',
-      srcSizes: [2000, 1000],
-      media: '(min-width: 740px)',
-      sizes: '(max-width: 980px) 1734px, 2600px',
-    },
-    {
-      gridId: 'd1a7088f8f2a367b0321528f081777c9b5618412/0_0_3578_2013',
-      srcSizes: [2000, 1000, 500],
-      media: '(max-width: 740px)',
-      sizes: '100vw',
-    },
-  ],
-  fallback: 'bce7d14f7f837a4f6c854d95efc4b1eab93a8c65/0_0_5200_720/1000.jpg',
-  altText: 'Guardian supporters',
-};
 
 const copy = {
   top: [
@@ -51,9 +31,6 @@ export default function WhySupport() {
 
   return (
     <section className="why-support">
-      <div className="why-support__image">
-        <GridPicture {...heroImage} />
-      </div>
       <div className="why-support__content gu-content-margin">
         <div className="why-support__top-content">
           <div className="why-support__top-copy">
