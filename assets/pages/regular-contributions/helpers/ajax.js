ecddde70aa62f75c43880a934a842c069cf8b3d7
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-10-09 13:41:09 +0100

    [post-support-frontend-data] Update the acquisition field to referreracquisition

--- assets/pages/regular-contributions/helpers/ajax.js
+++ assets/pages/regular-contributions/helpers/ajax.js
@@ -120,7 +120,7 @@ function statusPoll(dispatch: Function, getState: Function) {
   const state = getState();
 
   if (state.page.regularContrib.pollCount >= MAX_POLLS) {
-    const url: string = addQueryParamToURL(routes.recurringContribPending, 'INTCMP', state.common.acquisition.campaignCode);
+    const url: string = addQueryParamToURL(routes.recurringContribPending, 'INTCMP', state.common.referrerAcquisitionData.campaignCode);
     window.location.assign(url);
   }
 
@@ -154,7 +154,7 @@ function handleStatus(response: Response, dispatch: Function, getState: Function
           dispatch(checkoutError(status.message));
           break;
         case 'success':
-          window.location.assign(addQueryParamToURL(routes.recurringContribThankyou, 'INTCMP', state.common.acquisition.campaignCode));
+          window.location.assign(addQueryParamToURL(routes.recurringContribThankyou, 'INTCMP', state.common.referrerAcquisitionData.campaignCode));
           break;
         default:
           delayedStatusPoll(dispatch, getState);
