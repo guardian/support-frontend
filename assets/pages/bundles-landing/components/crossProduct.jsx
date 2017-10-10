7d3871431ba8ca26b13015b1a64fdd54b3e9ed01
James Byers <Ap0c@users.noreply.github.com>
2017-10-10 12:11:15 +0100

    Updated the cross-product copy.

--- assets/pages/bundles-landing/components/crossProduct.jsx
+++ assets/pages/bundles-landing/components/crossProduct.jsx
@@ -12,13 +12,15 @@ export default function CrossProduct() {
   return (
     <div className="bundles__cross-product">
       <h1 className="bundles__cross-product-header">
-        <span className="bundles__cross-product-heading">...and take a look</span>
-        <span className="bundles__cross-product-heading">behind the scenes</span>
+        <span className="bundles__cross-product-heading">...and go</span>
+        <span className="bundles__cross-product-heading">
+          behind the scenes
+        </span>
       </h1>
       <p className="bundles__cross-product-copy">
-        Everyone who supports the Guardian can experience our journalism up
-        close with behind the scenes emails from our newsroom and the
-        opportunity to take part in our podcast for supporters.
+        Everyone who supports the Guardian can experience it up close with
+        emails from our newsroom and the opportunity to participate in our
+        journalism
       </p>
     </div>
   );
