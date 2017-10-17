6cfad3adc9826ed7295e80c44f16e09b03e9a3b5
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-10-17 14:34:27 +0100

    [us-launch-copy-changes] Adding a nbsp for the Guardian

--- assets/pages/contributions-landing/components/contributionsIntroduction.jsx
+++ assets/pages/contributions-landing/components/contributionsIntroduction.jsx
@@ -17,7 +17,7 @@ export default function ContributionsIntroduction() {
         <h1>the world needs</h1>
       </div>
       <div className="contributions-introduction__secondary">
-        <h1>support the Guardian </h1>
+        <h1>support the&nbsp;Guardian </h1>
         <h1><span className="contributions-introduction__indent">make a contribution </span></h1>
       </div>
     </div>
