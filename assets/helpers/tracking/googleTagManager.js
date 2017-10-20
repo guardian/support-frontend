bf60e8c6bc6c78632b93ef2f7325a045e7061fb3
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-10-20 13:26:21 +0100

    [dimensions-as-datalayer-variables] Removed the GTM script form googleTagManagerHerlper

--- assets/helpers/tracking/googleTagManager.js
+++ assets/helpers/tracking/googleTagManager.js
@@ -1,7 +1,5 @@
 // @flow
 
-// ----- Imports ----- //
-
 import uuidv4 from 'uuid';
 import * as storage from 'helpers/storage';
 import { forCountry } from 'helpers/internationalisation/currency';
@@ -37,6 +35,17 @@ function getContributionValue() {
   return storage.getSession('contributionValue') || 0;
 }
 
+function pushBasicDimensions() {
+  window.googleTagManagerDataLayer = [{
+    // orderId anonymously identifies this user in this session.
+    // We need this to prevent page refreshes on conversion pages being
+    // treated as new conversions
+    orderId: getDataValue('orderId', uuidv4),
+    currency: getDataValue('currency', getCurrency),
+    value: getContributionValue(),
+  }];
+}
+
 // ----- Exports ---//
 
 export function pushDimensions(dimensions: Dimensions) {
@@ -44,24 +53,6 @@ export function pushDimensions(dimensions: Dimensions) {
   dataLayer.push(dimensions);
 }
 
-// ----- Run ----- //
-
-window.googleTagManagerDataLayer = [{
-  // orderId anonymously identifies this user in this session.
-  // We need this to prevent page refreshes on conversion pages being
-  // treated as new conversions
-  orderId: getDataValue('orderId', uuidv4),
-  currency: getDataValue('currency', getCurrency),
-  value: getContributionValue(),
-}];
-
-// Google Tag Manager
-/* eslint-disable */
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-// $FlowFixMe
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','googleTagManagerDataLayer','GTM-5PKPPQZ');
-/* eslint-enable */
-// End Google Tag Manager
+export function init() {
+  pushBasicDimensions();
+}
