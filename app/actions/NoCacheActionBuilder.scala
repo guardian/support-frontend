88a2d29022757fe0a29958279608e88d99815225
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-10-25 16:09:29 +0100

    add expired -> 0 to request header

--- app/actions/NoCacheActionBuilder.scala
+++ app/actions/NoCacheActionBuilder.scala
@@ -12,6 +12,6 @@ class NoCacheActionBuilder(
   implicit private val ec = executionContext
 
   override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] =
-    block(request).map(_.withHeaders("Cache-Control" -> "no-cache, private", "Pragma" -> "no-cache"))
+    block(request).map(_.withHeaders("Cache-Control" -> "no-cache, private", "Pragma" -> "no-cache", "Expires" -> "0"))
 
 }
