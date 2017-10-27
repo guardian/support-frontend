ad6c1e7686088fdd58d5b20d6034c3cacd8f83ec
James Byers <Ap0c@users.noreply.github.com>
2017-10-27 11:59:40 +0100

    First iteration of the mothership circle at widest breakpoint.

--- assets/pages/bundles-landing/support-landing-ab-test/components/mothershipNewDesign.jsx
+++ assets/pages/bundles-landing/support-landing-ab-test/components/mothershipNewDesign.jsx
@@ -15,6 +15,18 @@ export default function Mothership() {
     <div className="mothership-new-design">
       <div className="mothership-new-design__content gu-content-margin">
         <SvgGasParticles />
+        <div className="mothership-new-design__circle">
+          <h1 className="mothership-new-design__heading">
+            join over
+            <strong className="mothership-new-design__supporter-number">300,432</strong>
+            others and help secure the future of independent journalism
+          </h1>
+          <p className="mothership-new-design__copy">
+            We have no billionaire owner pulling our strings.
+            Fund quality, independent journalism and hold power to account
+            by making a contribution or by getting a subscription.
+          </p>
+        </div>
       </div>
     </div>
   );
