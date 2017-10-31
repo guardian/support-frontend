f000a113d81916e1160ff3465270873749913993
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-10-31 07:49:07 +0000

    [refactor-stripe-regular-contributions] Delete stripe checkout from the reducers and delete postcheckout action from regular contributions reducers.

--- assets/pages/regular-contributions/regularContributionsActions.js
+++ assets/pages/regular-contributions/regularContributionsActions.js
@@ -10,9 +10,6 @@ import type { PayPalButtonType } from './components/regularContributionsPayment'
 export type Action =
   | { type: 'CHECKOUT_ERROR', message: string }
   | { type: 'SET_PAYPAL_BUTTON', value: PayPalButtonType }
-  | { type: 'SET_STATUS_URI', uri: string }
-  | { type: 'INCREMENT_POLL_COUNT' }
-  | { type: 'RESET_POLL_COUNT' }
   | { type: 'CREATING_CONTRIBUTOR' };
 
 
@@ -24,18 +21,6 @@ export function checkoutError(message: string): Action {
 
 export function setPayPalButton(value: PayPalButtonType): Action {
   return { type: 'SET_PAYPAL_BUTTON', value };
-}
-
-export function setStatusUri(uri: string): Action {
-  return { type: 'SET_STATUS_URI', uri };
-}
-
-export function incrementPollCount(): Action {
-  return { type: 'INCREMENT_POLL_COUNT' };
-}
-
-export function resetPollCount(): Action {
-  return { type: 'RESET_POLL_COUNT' };
 }
 
 export function creatingContributor(): Action {
