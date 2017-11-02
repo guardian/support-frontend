4bf371e448d0a0960db15eb08c238dd0184e57e8
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-11-02 09:16:19 +0000

    update promo banenr codes

--- assets/helpers/tracking/acquisitions.js
+++ assets/helpers/tracking/acquisitions.js
@@ -69,14 +69,14 @@ const campaigns : {
   seven_fifty_email: [
     'gdnwb_copts_email_memco_seven_fifty',
   ],
-  big_long_banner_control_2: [
-    'banner_big_long_control_2',
+  big_long_banner_two_control: [
+    'banner_big_long_two_control',
   ],
-  big_long_banner_big_2: [
-    'banner_big_long_big_2',
+  big_long_banner_two_big: [
+    'banner_big_long_two_big',
   ],
-  big_long_banner_long_2: [
-    'banner_big_long_long_2',
+  big_long_banner_two_long: [
+    'banner_big_long_two_long',
   ],
 };
 
