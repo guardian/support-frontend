9c9c6e171c7ca4e4e111c50b29d0bf806bc1813f
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-11-02 16:46:14 +0000

    [meta-description] Removed strings prefix

--- app/config/StringsConfig.scala
+++ app/config/StringsConfig.scala
@@ -5,5 +5,5 @@ import com.typesafe.config.ConfigFactory
 class StringsConfig {
   val config = ConfigFactory.load("strings.conf")
 
-  val bundleLandingDescription = config.getString("strings.bundleLanding.description")
+  val bundleLandingDescription = config.getString("bundleLanding.description")
 }
