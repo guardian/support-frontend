528239a0a3946784ccdedebd6cd0ce7a1dd67d51
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-11-09 13:54:43 +0000

    add header test

--- assets/pages/bundles-landing/components/Introduction.jsx
+++ assets/pages/bundles-landing/components/Introduction.jsx
@@ -3,17 +3,61 @@
 // ----- Imports ----- //
 
 import React from 'react';
+import type { Contrib, Amounts, ContribError } from 'helpers/contributions';
+import type { IsoCountry } from 'helpers/internationalisation/country';
+import type { Currency } from 'helpers/internationalisation/currency';
+import type { Campaign } from 'helpers/tracking/acquisitions';
+import type { Participations } from 'helpers/abtest';
+import type { ReferrerAcquisitionData } from 'helpers/tracking/acquisitions';
 
+/* eslint-disable react/no-unused-prop-types */
+type PropTypes = {
+    contribType: Contrib,
+    contribAmount: Amounts,
+    contribError: ContribError,
+    intCmp: ?string,
+    campaign: ?Campaign,
+    otherQueryParams: Array<[string, string]>,
+    toggleContribType: (string) => void,
+    changeContribAnnualAmount: (string) => void,
+    changeContribMonthlyAmount: (string) => void,
+    changeContribOneOffAmount: (string) => void,
+    changeContribAmount: (string) => void,
+    isoCountry: IsoCountry,
+    currency: Currency,
+    abTests: Participations,
+    referrerAcquisitionData: ReferrerAcquisitionData,
+}
 
+type HeaderCopy = {
+    line1: string,
+    line2: string
+}
 // ----- Component ----- //
 
-export default function Introduction() {
+export default function Introduction(props: PropTypes) {
+
+    const headerCopyOptions: {[variant: string] : HeaderCopy} = {
+        control: {
+            line1: `help us deliver the`,
+            line2: `independent journalism the world&nbsp;needs`,
+        },
+        numberOfSupporters: {
+            line1: `Over 800,000 readers now help fund the Guardian`,
+            line2: `join them today and support the independent journalism the world needs
+`
+        }
+    };
+    
+    const headerCopy = props.abTests.headerCopyTest === 'numberOfSupporters' 
+        ? headerCopyOptions['numberOfSupporters']
+        : headerCopyOptions['control'];
 
   return (
     <section className="introduction-text">
       <div className="introduction-text__content gu-content-margin">
-        <h1 className="introduction-text__heading">help us deliver the</h1>
-        <p>independent journalism the world&nbsp;needs</p>
+        <h1 className="introduction-text__heading">headerCopy['line1']</h1>
+        <p>headerCopy['line2']</p>
         <h1 className="introduction-text__heading">support the Guardian</h1>
         <p>contribute or subscribe</p>
       </div>
