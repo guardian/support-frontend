23831296c3592daa40bde4c3c60f7a2b7f2f22bb
James Byers <Ap0c@users.noreply.github.com>
2017-11-17 18:25:13 +0000

    Added the other ways section.

---
+++ assets/pages/bundles-landing/support-landing-ab-test/components/otherWaysNewDesign.jsx
@@ -0,0 +1,33 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+import InfoSection from 'components/infoSection/infoSection';
+
+import OtherWaysCard from './otherWaysCardNewDesign';
+
+
+// ----- Component ----- //
+
+export default function OtherWays() {
+
+  return (
+    <div className="other-ways-new-design gu-content-filler">
+      <InfoSection
+        heading="other ways you can support us"
+        className="other-ways-new-design__content gu-content-filler__inner"
+      >
+        <OtherWaysCard
+          heading="patrons"
+          copy="The Patron tier is for those who want a deeper relationship with the Guardian and its journalists"
+          ctaText="Find out more"
+          gridImg="guardianObserverOffice"
+          imgAlt="the Guardian and the Observer"
+        />
+      </InfoSection>
+    </div>
+  );
+
+}
