24adfbc33889c970f95df5855f92f7b5032cb541
James Byers <Ap0c@users.noreply.github.com>
2017-11-17 14:06:53 +0000

    Added a moving circle svg.

--- assets/components/svg/svg.jsx
+++ assets/components/svg/svg.jsx
@@ -632,6 +632,26 @@ function SvgCross() {
 
 }
 
+function SvgMovingCircle() {
+
+  return (
+    <svg
+      className="svg-moving-circle"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 75 30"
+      preserveAspectRatio="xMinYMid"
+    >
+      <g fill="#FB0" filRule="evenodd">
+        <circle fillOpacity=".1" cx="60" cy="15" r="15" />
+        <circle fillOpacity=".4" cx="30" cy="15" r="15" />
+        <circle fillOpacity=".2" cx="46" cy="15" r="15" />
+        <circle cx="15" cy="15" r="15" />
+      </g>
+    </svg>
+  );
+
+}
+
 
 // ----- Exports ----- //
 
@@ -658,4 +678,5 @@ export {
   SvgWallMobile,
   SvgGasParticles,
   SvgCross,
+  SvgMovingCircle,
 };
