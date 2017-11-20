7c842804078cabf7d8054358c8ec9801e0734b42
rupertbates <rupert.bates@guardian.co.uk>
2017-11-20 17:35:47 +0000

    addMessage -> appendMessage

--- src/main/scala/com/gu/support/workers/model/RequestInfo.scala
+++ src/main/scala/com/gu/support/workers/model/RequestInfo.scala
@@ -1,5 +1,5 @@
 package com.gu.support.workers.model
 
 case class RequestInfo(encrypted: Boolean, testUser: Boolean, failed: Boolean, messages: List[String]){
-  def addMessage(message: String) = copy(messages = messages :+ message)
+  def appendMessage(message: String) = copy(messages = messages :+ message)
 }
