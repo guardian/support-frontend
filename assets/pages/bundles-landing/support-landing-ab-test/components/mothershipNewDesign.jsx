33cee4ca3c02235c099984c6c98e24efa5f55447
James Byers <Ap0c@users.noreply.github.com>
2017-12-01 12:30:58 +0000

    Shortened the copy in the mothership.

--- assets/pages/bundles-landing/support-landing-ab-test/components/mothershipNewDesign.jsx
+++ assets/pages/bundles-landing/support-landing-ab-test/components/mothershipNewDesign.jsx
@@ -22,10 +22,9 @@ export default function Mothership() {
             readers and help us deliver the independent journalism the world needs
           </h1>
           <p className="mothership-new-design__copy">
-           Your support helps to make our journalism possible. We don&#39;t have a
-           billionaire owner pulling our strings and we havent put up a
-           paywall. Fund independent, quality journalism and help us keep it
-           open for all by making a contribution or getting a subscription.
+           Your support helps to make our journalism possible, without paywalls
+           or billionaire owners. Fund us today by making a contribution or
+           getting a subscription.
           </p>
         </div>
       </div>
