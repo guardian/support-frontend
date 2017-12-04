27b08913871bc76e432a8d156b8cd7dcc6c13f53
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2017-12-04 17:08:14 +0000

    Let Stripe config optionally have a version

--- src/main/scala/com/gu/support/config/StripeConfig.scala
+++ src/main/scala/com/gu/support/config/StripeConfig.scala
@@ -1,12 +1,20 @@
 package com.gu.support.config
 
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigException}
 
-case class StripeConfig(secretKey: String, publicKey: String) extends TouchpointConfig
+import scala.util.control.Exception.catching
 
-class StripeConfigProvider(config: Config, defaultStage: Stage, prefix: String = "stripe") extends TouchpointConfigProvider[StripeConfig](config, defaultStage) {
+case class StripeConfig(secretKey: String, publicKey: String, version: Option[String]) extends TouchpointConfig
+
+class StripeConfigProvider(config: Config, defaultStage: Stage, prefix: String = "stripe", version: Option[String] = None) extends TouchpointConfigProvider[StripeConfig](config, defaultStage) {
   def fromConfig(config: Config): StripeConfig = StripeConfig(
     secretKey = config.getString(s"$prefix.api.key.secret"),
-    publicKey = config.getString(s"$prefix.api.key.public")
+    publicKey = config.getString(s"$prefix.api.key.public"),
+    version = stripeVersion(config)
   )
+
+  def stripeVersion(config: Config): Option[String] = {
+    val catchMissing = catching(classOf[ConfigException.Missing])
+    catchMissing opt config.getString(s"stripe.api.version")
+  }
 }
