cd35cb2917623cd4d7f0be041fabb0e72fefc30b
Joseph Smith <joseph.smith@guardian.co.uk>
2017-12-20 15:50:31 +0000

    Attempting to get Flow to typecheck

--- assets/components/secure/secure.jsx
+++ assets/components/secure/secure.jsx
@@ -6,16 +6,27 @@ import React from 'react';
 
 import { SvgLock } from 'components/svg/svg';
 
+// ----- Types ----- //
+
+type PropTypes = {
+  style?: Object
+}
 
 // ----- Component ----- //
 
-export default function Secure(props: {style: Object}) {
+export default function Secure(props: PropTypes) {
 
   return (
-    <div className="component-secure" style={props.style}>
+    <div className="component-secure" style={props && props.style}>
       <SvgLock />
       <span className="component-secure__text">Secure</span>
     </div>
   );
 
 }
+
+// ----- Default Props ----- //
+
+Secure.defaultProps = {
+  style: null,
+};
