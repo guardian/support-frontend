3583f41fd844f337d06873f3a4fd17f90788dcb3
James Byers <Ap0c@users.noreply.github.com>
2018-01-18 17:44:03 +0000

    Added a component for the three subscriptions bundles.

---
+++ assets/components/threeSubscriptions/threeSubscriptions.jsx
@@ -0,0 +1,20 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+import PageSection from 'components/pageSection/pageSection';
+
+
+// ----- Component ----- //
+
+export default function ThreeSubscriptions() {
+
+  return (
+    <PageSection heading="Subscribe">
+      Hello
+    </PageSection>
+  );
+
+}
