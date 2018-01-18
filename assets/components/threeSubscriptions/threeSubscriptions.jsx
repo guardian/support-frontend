4ca31bb0fbe0fbf85f0d9815a43df707366a80c8
James Byers <Ap0c@users.noreply.github.com>
2018-01-18 17:58:11 +0000

    Dropped the subs bundles into the page.

--- assets/components/threeSubscriptions/threeSubscriptions.jsx
+++ assets/components/threeSubscriptions/threeSubscriptions.jsx
@@ -5,6 +5,7 @@
 import React from 'react';
 
 import PageSection from 'components/pageSection/pageSection';
+import SubscriptionBundle from 'components/subscriptionBundle/subscriptionBundle';
 
 
 // ----- Component ----- //
@@ -12,8 +13,61 @@ import PageSection from 'components/pageSection/pageSection';
 export default function ThreeSubscriptions() {
 
   return (
-    <PageSection heading="Subscribe">
-      Hello
+    <PageSection heading="Subscribe" modifierClass="three-subscriptions">
+      <SubscriptionBundle
+        modifierClass="digital"
+        heading="Digital subscription"
+        subheading="£11.99/month"
+        benefits={[
+          {
+            heading: 'Premium experience on the Guardian app',
+            text: `No adverts means faster loading pages and a clearer reading experience.
+              Play our daily crosswords offline wherever you are`,
+          },
+          {
+            heading: 'Daily Tablet Edition app',
+            text: `Read the Guardian, the Observer and all the Weekend supplements in an
+              optimised tablet app; available on iPad`,
+          },
+        ]}
+        ctaText="Start your 14 day trial"
+        ctaLink="https://subscribe.theguardian.com/p/DXX83X"
+      />
+      <SubscriptionBundle
+        modifierClass="paper"
+        heading="Paper subscription"
+        subheading="£10.79/month"
+        benefits={[
+          {
+            heading: 'Choose your package and delivery method',
+            text: 'Everyday, Sixday, Weekend and Sunday; redeem paper vouchers or get home delivery',
+          },
+          {
+            heading: 'Save money on the retail price',
+          },
+        ]}
+        ctaText="Get a paper subscription"
+        ctaLink="https://subscribe.theguardian.com/p/GXX83P"
+      />
+      <SubscriptionBundle
+        modifierClass="paper-digital"
+        heading="Paper+digital"
+        subheading="£22.06/month"
+        benefits={[
+          {
+            heading: 'Choose your package and delivery method',
+            text: 'Everyday, Sixday, Weekend and Sunday; redeem paper vouchers or get home delivery',
+          },
+          {
+            heading: 'Save money on the retail price',
+          },
+          {
+            heading: 'Get all the benefits of a digital subscription with paper + digital',
+          },
+        ]}
+        ctaText="Get a paper + digital subscription"
+        ctaLink="https://subscribe.theguardian.com/p/GXX83X"
+      />
     </PageSection>
   );
 
