777ec8cd03795d935f79f3f13655c3e2bc2c11fe
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-01-19 18:04:08 +0000

    [direct-debit-phase-2] Defined the checkAccount

--- assets/components/directDebit/helpers/ajax.js
+++ assets/components/directDebit/helpers/ajax.js
@@ -2,9 +2,26 @@
 
 // ----- Imports ----- //
 
-const checkAccount = () => {
+import { routes } from 'helpers/routes';
+import type { Csrf as CsrfState } from 'helpers/csrf/csrfReducer';
 
-}
+const checkAccount = (sortCode: string, accountNumber: string, accountHolderName: string, isTestUser: boolean, csrf: CsrfState) => {
+
+  const bankAccountInformation = {
+    sortCodeValue: sortCode,
+    accountNumber,
+    accountHolderName,
+  };
+
+  const requestData = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', 'Csrf-Token': csrf.token || '' },
+    credentials: 'same-origin',
+    body: JSON.stringify(bankAccountInformation),
+  };
+
+  return fetch(routes.directDebitCheckAccount, requestData);
+};
 
 export {
   checkAccount,
