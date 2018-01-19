28938e311eb267ca21531d011165cab1a23dd329
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-01-19 10:56:03 +0000

    [direct-debit-phase-2] Added firstApproach of directDebit controller

---
+++ app/controllers/DirectDebit.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import actions.CustomActionBuilders
+import com.typesafe.scalalogging.LazyLogging
+import io.circe.syntax._
+import models.DirectDebitData
+
+import play.api.libs.circe.Circe
+import play.api.mvc._
+import services.paypal.PayPalBillingDetails.codec
+import services.{GoCardlessService}
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+class DirectDebit(
+  actionBuilders: CustomActionBuilders,
+  components: ControllerComponents
+)(implicit val ec: ExecutionContext) extends AbstractController(components) with Circe with LazyLogging {
+
+  import actionBuilders._
+
+  def checkAccount: Action[AnyContent]  =
+    CachedAction().async(circe.json[DirectDebitData]) { implicit request =>
+      for {
+        isAccountValid <- GoCardlessService.checkBankDetails(request)
+      } yield Ok(("accountValid" -> isAccountValid).asJson)
+    }
+}
