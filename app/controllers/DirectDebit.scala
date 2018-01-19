62c942bb5e11bd3c5afeb20b71b0a318129875dc
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-01-19 11:31:26 +0000

    [direct-debit-phase-2] Refactor of the directDebit controller

--- app/controllers/DirectDebit.scala
+++ app/controllers/DirectDebit.scala
@@ -4,11 +4,10 @@ import actions.CustomActionBuilders
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.syntax._
 import models.DirectDebitData
-
 import play.api.libs.circe.Circe
 import play.api.mvc._
 import services.paypal.PayPalBillingDetails.codec
-import services.{GoCardlessService}
+import services.GoCardlessService
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -16,15 +15,16 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class DirectDebit(
   actionBuilders: CustomActionBuilders,
-  components: ControllerComponents
+  components: ControllerComponents,
+  goCardlessService: GoCardlessService
 )(implicit val ec: ExecutionContext) extends AbstractController(components) with Circe with LazyLogging {
 
   import actionBuilders._
 
-  def checkAccount: Action[AnyContent]  =
-    CachedAction().async(circe.json[DirectDebitData]) { implicit request =>
-      for {
-        isAccountValid <- GoCardlessService.checkBankDetails(request)
-      } yield Ok(("accountValid" -> isAccountValid).asJson)
+  def checkAccount: Action[DirectDebitData]  =
+    PrivateAction.async(circe.json[DirectDebitData]) { implicit request =>
+      goCardlessService.checkBankDetails(request.body).map { isAccountValid =>
+        Ok(("accountValid" -> isAccountValid).asJson)
+      }
     }
 }
