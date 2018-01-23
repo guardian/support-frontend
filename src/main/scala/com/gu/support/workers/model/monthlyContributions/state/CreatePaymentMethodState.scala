5943a7d17d326cfc31163148e32e844835dc7151
rupertbates <rupert.bates@guardian.co.uk>
2018-01-23 11:55:15 +0000

    Change CreatePaymentMethodState

--- src/main/scala/com/gu/support/workers/model/monthlyContributions/state/CreatePaymentMethodState.scala
+++ src/main/scala/com/gu/support/workers/model/monthlyContributions/state/CreatePaymentMethodState.scala
@@ -3,12 +3,12 @@ package com.gu.support.workers.model.monthlyContributions.state
 import java.util.UUID
 
 import com.gu.support.workers.model.monthlyContributions.Contribution
-import com.gu.support.workers.model.{AcquisitionData, PayPalPaymentFields, StripePaymentFields, User}
+import com.gu.support.workers.model._
 
 case class CreatePaymentMethodState(
   requestId: UUID,
   user: User,
   contribution: Contribution,
-  paymentFields: Either[StripePaymentFields, PayPalPaymentFields],
+  paymentFields: PaymentFields,
   acquisitionData: Option[AcquisitionData]
 ) extends StepFunctionUserState
