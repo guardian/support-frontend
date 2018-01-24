540bb51522971d8d6fabf2fe122eda00f1bdc6f5
James Byers <Ap0c@users.noreply.github.com>
2018-01-24 16:34:06 +0000

    Styled the ReadyToSupport component.

--- assets/components/readyToSupport/readyToSupport.jsx
+++ assets/components/readyToSupport/readyToSupport.jsx
@@ -21,9 +21,10 @@ export default function ReadyToSupport(props: PropTypes) {
 
   return (
     <section className="component-ready-to-support">
-      <div className="component-ready-to-support__content">
+      <div className="component-ready-to-support__content gu-content-margin">
         <h1 className="component-ready-to-support__heading">
-          Ready to support The&nbsp;Guardian
+          <span className="component-ready-to-support__heading-line">Ready to support</span>
+          <span className="component-ready-to-support__heading-line">The&nbsp;Guardian?</span>
         </h1>
         <CtaLink
           text="See supporter options"
