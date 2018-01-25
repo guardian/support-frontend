a2266a4aeac1ee3ca68f5d13cebb4970c43c2f30
James Byers <Ap0c@users.noreply.github.com>
2018-01-25 11:13:02 +0000

    Added a redux store to the support landing page.

--- assets/pages/support-landing/supportLanding.jsx
+++ assets/pages/support-landing/supportLanding.jsx
@@ -3,6 +3,7 @@
 // ----- Imports ----- //
 
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
 import Footer from 'components/footer/footer';
@@ -12,21 +13,29 @@ import WhySupport from 'components/whySupport/whySupport';
 import ReadyToSupport from 'components/readyToSupport/readyToSupport';
 import PatronsEvents from 'components/patronsEvents/patronsEvents';
 
+import { init as pageInit } from 'helpers/page/page';
 import { renderPage } from 'helpers/render';
+
+
+// ----- Redux Store ----- //
+
+const store = pageInit({});
 
 
 // ----- Render ----- //
 
 const content = (
-  <div>
-    <SimpleHeader />
-    <CirclesIntroduction />
-    <ThreeSubscriptions />
-    <WhySupport />
-    <ReadyToSupport ctaUrl="#" />
-    <PatronsEvents />
-    <Footer />
-  </div>
+  <Provider store={store}>
+    <div>
+      <SimpleHeader />
+      <CirclesIntroduction />
+      <ThreeSubscriptions />
+      <WhySupport />
+      <ReadyToSupport ctaUrl="#" />
+      <PatronsEvents />
+      <Footer />
+    </div>
+  </Provider>
 );
 
 renderPage(content, 'support-landing-page');
