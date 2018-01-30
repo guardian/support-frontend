db282c9c2e51659abd51a36e0eccd52e3ad28b8a
jranks123 <jonathan.rankin@guardian.co.uk>
2018-01-30 14:32:40 +0000

    add useful logging for paypal payments

--- app/controllers/PayPal.scala
+++ app/controllers/PayPal.scala
@@ -53,7 +53,7 @@ class PayPal(
   def resultFromEmailOption(email: Option[Email]): Result = {
     val redirect = Redirect("/contribute/one-off/thankyou")
     email.fold(redirect)(e => {
-      logger.info("email: " + e)
+      logger.info("Redirecting to thank you page with email in flash session")
       redirect.flashing("email" -> e.value)
     })
   }
