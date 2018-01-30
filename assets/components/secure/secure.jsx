5cea9c4e82e4e4c8262b6a60ffae499bef4ff2e9
James Byers <Ap0c@users.noreply.github.com>
2018-01-30 14:02:22 +0000

    Added modifierClass prop to Secure.

--- assets/components/secure/secure.jsx
+++ assets/components/secure/secure.jsx
@@ -6,16 +6,32 @@ import React from 'react';
 
 import { SvgLock } from 'components/svg/svg';
 
+import { generateClassName } from 'helpers/utilities';
+
+
+// ----- Props ----- //
+
+type PropTypes = {
+  modifierClass?: string,
+};
+
 
 // ----- Component ----- //
 
-export default function Secure() {
+export default function Secure(props: PropTypes) {
 
   return (
-    <div className="component-secure">
+    <div className={generateClassName('component-secure', props.modifierClass)}>
       <SvgLock />
       <span className="component-secure__text">Secure</span>
     </div>
   );
 
 }
+
+
+// ----- Default Props ----- //
+
+Secure.defaultProps = {
+  modifierClass: '',
+};
