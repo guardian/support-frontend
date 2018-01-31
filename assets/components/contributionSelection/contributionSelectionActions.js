2efd3f72a39b5b5306870c51fcc316c6bacce19d
James Byers <Ap0c@users.noreply.github.com>
2018-01-31 18:21:00 +0000

    Added scoped actions for contribution selection.

---
+++ assets/components/contributionSelection/contributionSelectionActions.js
@@ -0,0 +1,32 @@
+// @flow
+
+// ----- Imports ----- //
+
+import type { Contrib as ContributionType } from 'helpers/contributions';
+
+
+// ----- Types ----- //
+
+export type Action =
+  | { type: 'SET_CONTRIBUTION_TYPE', scope: string, contributionType: ContributionType }
+  | { type: 'SET_AMOUNT', scope: string, amount: number }
+  | { type: 'SET_CUSTOM_AMOUNT', scope: string, amount: string };
+
+
+// ----- Action Creators ----- //
+
+export default function contributionSelectionActionsFor(scope: string): Object {
+
+  return {
+    setContributionType(contributionType: ContributionType): Action {
+      return { type: 'SET_CONTRIBUTION_TYPE', scope, contributionType };
+    },
+    setAmount(amount: number): Action {
+      return { type: 'SET_AMOUNT', scope, amount };
+    },
+    setCustomAmount(amount: string): Action {
+      return { type: 'SET_CUSTOM_AMOUNT', scope, amount };
+    },
+  };
+
+}
