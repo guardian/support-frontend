7c85fbf843bd113b6a0f21fc16c0b76ad66f0468
rupertbates <rupert.bates@guardian.co.uk>
2018-01-31 15:08:45 +0000

    Move version to the top level stripe config object & make country and Option

--- src/main/scala/com/gu/support/config/StripeConfig.scala
+++ src/main/scala/com/gu/support/config/StripeConfig.scala
@@ -1,34 +1,38 @@
 package com.gu.support.config
 
 import com.gu.i18n.Country
+import com.gu.i18n.Country.Australia
 import com.typesafe.config.Config
 
-case class StripeConfig(config: Config, prefix: String = "stripe") extends TouchpointConfig {
-  private val australia = fromConfig(config, prefix, "AU")
-  private val default = fromConfig(config, prefix, "default")
 
-  def forCountry(country: Country) =
+case class StripeConfig(defaultAccount: StripeAccountConfig, australiaAccount: StripeAccountConfig, version: Option[String] = None)
+  extends TouchpointConfig {
+  def forCountry(country: Option[Country]) =
     country match {
-      case Country.Australia => australia
-      case _ => default
+      case Some(Australia) => australiaAccount
+      case _ => defaultAccount
     }
+}
 
-  private def fromConfig(config: Config, prefix: String, country: String): StripeAccountConfig =
+case class StripeAccountConfig(secretKey: String, publicKey: String)
+
+
+class StripeConfigProvider(config: Config, defaultStage: Stage, prefix: String = "stripe", version: Option[String] = None)
+  extends TouchpointConfigProvider[StripeConfig](config, defaultStage) {
+  def fromConfig(config: Config): StripeConfig = StripeConfig(
+    accountFromConfig(config, prefix, "AU"),
+    accountFromConfig(config, prefix, "default"),
+    version = version
+  )
+
+  private def accountFromConfig(config: Config, prefix: String, country: String) =
     StripeAccountConfig(
       secretKey = config.getString(s"$prefix.$country.api.key.secret"),
-      publicKey = config.getString(s"$prefix.$country.api.key.public"),
-      version = stripeVersion(config)
+      publicKey = config.getString(s"$prefix.$country.api.key.public")
     )
 
   private def stripeVersion(config: Config): Option[String] = {
     val stripeVersion = "stripe.api.version"
     if (config.hasPath(stripeVersion)) Some(config.getString(stripeVersion)) else None
   }
-}
-
-case class StripeAccountConfig(secretKey: String, publicKey: String, version: Option[String])
-
-
-class StripeConfigProvider(config: Config, defaultStage: Stage, prefix: String = "stripe", version: Option[String] = None) extends TouchpointConfigProvider[StripeConfig](config, defaultStage) {
-  def fromConfig(config: Config): StripeConfig = StripeConfig(config)
 }
