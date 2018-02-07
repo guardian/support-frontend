82aeb5fdd74a8fe1f4b7ed803cf6bd83950c5080
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-02-07 17:33:51 +0000

    [adding-europe] Adding extra tests for AUD and EUR cases

--- assets/helpers/internationalisation/__tests__/currencyTest.js
+++ assets/helpers/internationalisation/__tests__/currencyTest.js
@@ -2,7 +2,7 @@
 
 // ----- Imports ----- //
 
-import { detect, GBP, USD, IsoCurrency } from '../currency';
+import { detect, GBP, USD, EUR, AUD, IsoCurrency } from '../currency';
 
 let mockCurrency: ?IsoCurrency = null;
 
@@ -37,6 +37,26 @@ describe('detect currency', () => {
   it('should return the currency from the query parameter (GBP)', () => {
     mockCurrency = 'GBP';
     expect(detect('US')).toEqual(GBP);
+  });
+
+  it('should return the currency from the query parameter (AUD)', () => {
+    mockCurrency = 'AUD';
+    expect(detect('US')).toEqual(AUD);
+  });
+
+  it('should return the currency for the supplied country if there is no query parameter set (AU)', () => {
+    mockCurrency = null;
+    expect(detect('AU')).toEqual(AUD);
+  });
+
+  it('should return the currency from the query parameter (EUR)', () => {
+    mockCurrency = 'EUR';
+    expect(detect('US')).toEqual(EUR);
+  });
+
+  it('should return the currency for the supplied country if there is no query parameter set (FR)', () => {
+    mockCurrency = null;
+    expect(detect('FR')).toEqual(EUR);
   });
 
 });
