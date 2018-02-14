356138de92be9c7c0fbca3c0ad1631415df186cd
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-02-14 11:12:19 +0000

    [adding-country-group] Updated currency tests

--- assets/helpers/internationalisation/__tests__/currencyTest.js
+++ assets/helpers/internationalisation/__tests__/currencyTest.js
@@ -2,7 +2,7 @@
 
 // ----- Imports ----- //
 
-import { detect, GBP, USD, EUR, AUD, IsoCurrency, isEuroCountry } from '../currency';
+import { detect, GBP, USD, EUR, AUD, IsoCurrency } from '../currency';
 
 let mockCurrency: ?IsoCurrency = null;
 
@@ -14,70 +14,49 @@ jest.mock('helpers/url', () => ({
 
 describe('detect currency', () => {
 
-  it('should return the currency for the supplied country if there is no query parameter set (GB)', () => {
+  it('should return the currency for the supplied country group if there is no query parameter set (GBP)', () => {
     mockCurrency = null;
-    expect(detect('GB')).toEqual(GBP);
+    expect(detect('GBPCountries')).toEqual(GBP);
   });
 
-  it('should return the currency for the supplied country if there is no query parameter set (US)', () => {
+  it('should return the currency for the supplied country group if there is no query parameter set (USD)', () => {
     mockCurrency = null;
-    expect(detect('US')).toEqual(USD);
+    expect(detect('UnitedStates')).toEqual(USD);
   });
 
-  it('should return GBP if the country is not recognised', () => {
+  it('should return GBP if the country group is not recognised', () => {
     mockCurrency = null;
     expect(detect('ZZ')).toEqual(GBP);
   });
 
   it('should return the currency from the query parameter (USD)', () => {
     mockCurrency = 'USD';
-    expect(detect('GB')).toEqual(USD);
+    expect(detect('GBPCountries')).toEqual(USD);
   });
 
   it('should return the currency from the query parameter (GBP)', () => {
     mockCurrency = 'GBP';
-    expect(detect('US')).toEqual(GBP);
+    expect(detect('UnitedStates')).toEqual(GBP);
   });
 
   it('should return the currency from the query parameter (AUD)', () => {
     mockCurrency = 'AUD';
-    expect(detect('US')).toEqual(AUD);
+    expect(detect('UnitedStates')).toEqual(AUD);
   });
 
-  it('should return the currency for the supplied country if there is no query parameter set (AU)', () => {
+  it('should return the currency for the supplied country group if there is no query parameter set (AUDCountries)', () => {
     mockCurrency = null;
-    expect(detect('AU')).toEqual(AUD);
+    expect(detect('AUDCountries')).toEqual(AUD);
   });
 
   it('should return the currency from the query parameter (EUR)', () => {
     mockCurrency = 'EUR';
-    expect(detect('US')).toEqual(EUR);
+    expect(detect('UnitedStates')).toEqual(EUR);
   });
 
-  it('should return the currency for the supplied country if there is no query parameter set (FR)', () => {
+  it('should return the currency for the supplied country group if there is no query parameter set (EURCountries)', () => {
     mockCurrency = null;
-    expect(detect('FR')).toEqual(EUR);
+    expect(detect('EURCountries')).toEqual(EUR);
   });
 
 });
-
-describe('isEuroCountry', () => {
-
-  it('should return true is the country process EUR (FR)', () => {
-    mockCurrency = null;
-    expect(isEuroCountry('FR')).toEqual(true);
-    expect(isEuroCountry('DE')).toEqual(true);
-    expect(isEuroCountry('NL')).toEqual(true);
-    expect(isEuroCountry('NO')).toEqual(true);
-    expect(isEuroCountry('IE')).toEqual(true);
-  });
-
-  it('should return true is the country process EUR (US)', () => {
-    mockCurrency = null;
-    expect(isEuroCountry('US')).toEqual(false);
-    expect(isEuroCountry('UK')).toEqual(false);
-    expect(isEuroCountry('AU')).toEqual(false);
-  });
-
-});
-
