82c6767fefdba13b879fd99d5510f49c70e1e037
James Byers <Ap0c@users.noreply.github.com>
2018-02-15 16:30:54 +0000

    Added copy informing the user about the confirmation email.

--- assets/pages/regular-contributions-thank-you/regularContributionsThankYou.jsx
+++ assets/pages/regular-contributions-thank-you/regularContributionsThankYou.jsx
@@ -14,6 +14,8 @@ import SpreadTheWord from 'components/spreadTheWord/spreadTheWord';
 import { statelessInit as pageInit } from 'helpers/page/page';
 import { renderPage } from 'helpers/render';
 
+import EmailConfirmation from './components/emailConfirmation';
+
 
 // ----- Page Startup ----- //
 
@@ -29,8 +31,9 @@ const content = (
       highlights={['Thank you']}
       headings={['for a valuable', 'contribution']}
     />
-    <QuestionsContact />
+    <EmailConfirmation />
     <DotcomCta />
+    <QuestionsContact />
     <SpreadTheWord />
     <Footer />
   </div>
