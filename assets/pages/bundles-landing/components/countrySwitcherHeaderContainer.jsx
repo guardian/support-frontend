9b7532fef291d33ddfe51e49a64ec4954d431a96
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-02-16 16:40:24 +0000

    [country-group-switcher] Updated countrySwitcherHeader Container

---
+++ assets/pages/bundles-landing/components/countrySwitcherHeaderContainer.jsx
@@ -0,0 +1,48 @@
+// @flow
+
+// ----- Imports ----- //
+
+import { connect } from 'react-redux';
+
+import HeaderWitchCountrySwitcher from 'components/headers/countrySwitcherHeader/countrySwitcherHeader';
+
+import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
+import type { State } from '../bundlesLandingReducers';
+
+
+const availableCountriesGroups: CountryGroupId[] =
+  ['GBPCountries', 'UnitedStates', 'AUDCountries', 'EURCountries'];
+
+// ----- Functions ----- //
+
+const handleCountryGroupChange = (value: string) => {
+  switch (value) {
+    case 'UnitedStates':
+      window.location.href = '/us';
+      break;
+    case 'AUDCountries':
+      window.location.href = '/au';
+      break;
+    case 'EURCountries':
+      window.location.href = '/eu';
+      break;
+    default:
+  }
+};
+
+
+// ----- State Maps ----- //
+
+function mapStateToProps(state: State) {
+
+  return {
+    countryGroupIds: availableCountriesGroups,
+    selectedCountryGroup: state.common.countryGroup,
+    onCountryGroupSelect: handleCountryGroupChange,
+  };
+}
+
+
+// ----- Exports ----- //
+
+export default connect(mapStateToProps)(HeaderWitchCountrySwitcher);
