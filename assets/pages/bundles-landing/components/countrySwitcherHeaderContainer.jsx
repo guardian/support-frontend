36e4e169fe4ec57230d74fd327918bdf03275b0f
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-02-19 14:05:58 +0000

    [country-group-switcher] restricting the countries that a user can see

--- assets/pages/bundles-landing/components/countrySwitcherHeaderContainer.jsx
+++ assets/pages/bundles-landing/components/countrySwitcherHeaderContainer.jsx
@@ -11,7 +11,7 @@ import type { State } from '../bundlesLandingReducers';
 
 
 const availableCountriesGroups: CountryGroupId[] =
-  ['GBPCountries', 'UnitedStates', 'AUDCountries', 'EURCountries'];
+  ['GBPCountries', 'UnitedStates'];
 
 // ----- Functions ----- //
 
