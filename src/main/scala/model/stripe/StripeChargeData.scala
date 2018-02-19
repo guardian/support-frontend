99e79ca07def005ffcfead04e53e39e4292e9f3d
guy.dawson <guy.dawson@theguardian.com>
2018-02-19 11:50:52 +0000

    add composite decoder; consider some acquisition fields in acquisition-event-producer pull request

--- src/main/scala/model/stripe/StripeChargeData.scala
+++ src/main/scala/model/stripe/StripeChargeData.scala
@@ -2,6 +2,7 @@ package model.stripe
 
 import io.circe.Decoder
 import io.circe.generic.JsonCodec
+import io.circe.generic.semiauto._
 
 import model.Currency
 
@@ -14,9 +15,6 @@ import model.Currency
 )
 
 // TODO: can this be used for both Stripe and Paypal payments?
-// TODO: correct types
-// TODO: use acquisition event producer models to build this?
-// TODO: model this case class as composite case classes?
 @JsonCodec case class AcquisitionData(
     browserId: Option[String],
     visitId: Option[String],
@@ -25,23 +23,24 @@ import model.Currency
     referrerUrl: Option[String],
     // TODO: look into whether circe can handle empty sets without them being wrapped in an option
     campaignCodes: Option[Set[String]],
-    platform: Option[String],
-    componentId: Option[String],
-    componentType: Option[String],
-    source: Option[String],
-    abTests: Option[Set[String]]
+    // TODO: add these fields when dependency on acquisition-event-producer is added
+    //platform: Option[String],
+    //componentId: Option[String],
+    //componentType: Option[String],
+    //source: Option[String],
+    //abTests: Option[Set[String]]
 )
 
-// TODO: can this be used for both Stripe and Paypal payments
+// TODO: can this be used for both Stripe and Paypal payments?
 @JsonCodec case class IdentityData(
     identityId: Option[String]
 )
 
-// Fields are either used to:
-// - chargeData - required to create a Stripe charge
+// Fields are grouped by what they're used for:
+// - paymentData - required to create a Stripe charge
 // - acquisitionData - required to create an acquisition event (used for analytics)
 // - identityData - TODO: note about payment-api as an API to see if a user has contributed
-@JsonCodec case class StripeChargeData(
+case class StripeChargeData(
     paymentData: StripePaymentData,
     acquisitionData: AcquisitionData,
     identityData: IdentityData
@@ -49,57 +48,55 @@ import model.Currency
 
 object StripeChargeData {
 
-  object legacy {
-
-    implicit val legacyStripeChargeDataDecoder: Decoder[StripeChargeData] = Decoder.instance { cursor =>
-      import cursor._
-      for {
-        currency <- downField("currency").as[Currency]
-        amount <- downField("amount").as[Int]
-        token <- downField("token").as[String]
-        email <- downField("email").as[String]
-        browserId <- downField("ophanBrowserId").as[Option[String]]
-        visitId <- downField("ophanVisitId").as[Option[String]]
-        pageviewId <- downField("ophanPageviewId").as[Option[String]]
-        // Intentional typo in down field
-        referrerPageviewId <- downField("refererPageviewId").as[Option[String]]
-        // Intentional typo in down field
-        referrerUrl <- downField("refererUrl").as[Option[String]]
-        cmp <- downField("cmp").as[Option[String]]
-        intcmp <- downField("intcmp").as[Option[String]]
-        platform <- downField("platform").as[Option[String]]
-        componentId <- downField("componentId").as[Option[String]]
-        componentType <- downField("componentType").as[Option[String]]
-        source <- downField("source").as[Option[String]]
-        // TODO: ab tests field
-        // TODO: support field
-        identityId <- downField("idUser").as[Option[String]]
-      } yield {
-        StripeChargeData(
-          paymentData = StripePaymentData(
-            currency = currency,
-            amount = amount,
-            source = token,
-            email = email
-          ),
-          acquisitionData = AcquisitionData(
-            browserId = browserId,
-            visitId = visitId,
-            pageviewId = pageviewId,
-            referrerPageviewId = referrerPageviewId,
-            referrerUrl = referrerUrl,
-            campaignCodes = Some(Set(cmp, intcmp).flatten),
-            platform = platform,
-            componentId = componentId,
-            componentType = componentType,
-            source = source,
-            abTests = None
-          ),
-          identityData = IdentityData(
-            identityId = identityId
-          )
+  // This will decode Stripe charge data in the format expected by the old contributions-frontend API.
+  private val legacyStripeChargeDataDecoder: Decoder[StripeChargeData] = Decoder.instance { cursor =>
+    import cursor._
+    for {
+      currency <- downField("currency").as[Currency]
+      amount <- downField("amount").as[Int]
+      token <- downField("token").as[String]
+      email <- downField("email").as[String]
+      browserId <- downField("ophanBrowserId").as[Option[String]]
+      visitId <- downField("ophanVisitId").as[Option[String]]
+      pageviewId <- downField("ophanPageviewId").as[Option[String]]
+      // Intentional typo in down field
+      referrerPageviewId <- downField("refererPageviewId").as[Option[String]]
+      // Intentional typo in down field
+      referrerUrl <- downField("refererUrl").as[Option[String]]
+      cmp <- downField("cmp").as[Option[String]]
+      intcmp <- downField("intcmp").as[Option[String]]
+      // TODO: add these fields when dependency on acquisition-event-producer is added
+      //platform <- downField("platform").as[Option[String]]
+      //componentId <- downField("componentId").as[Option[String]]
+      //componentType <- downField("componentType").as[Option[String]]
+      //source <- downField("source").as[Option[String]]
+      identityId <- downField("idUser").as[Option[String]]
+    } yield {
+      StripeChargeData(
+        paymentData = StripePaymentData(
+          currency = currency,
+          amount = amount,
+          source = token,
+          email = email
+        ),
+        acquisitionData = AcquisitionData(
+          browserId = browserId,
+          visitId = visitId,
+          pageviewId = pageviewId,
+          referrerPageviewId = referrerPageviewId,
+          referrerUrl = referrerUrl,
+          campaignCodes = Some(Set(cmp, intcmp).flatten)
+        ),
+        identityData = IdentityData(
+          identityId = identityId
         )
-      }
+      )
     }
   }
+
+  // Use a decoder which falls back to the old format expected by the contribution-frontend API,
+  // Useful if the client hasn't sent Stripe charge data in the new format -
+  // e.g. if a user is making a contribution through an old version of the native App.
+  implicit val stripeChargeDataDecoder: Decoder[StripeChargeData] =
+    deriveDecoder[StripeChargeData].or(legacyStripeChargeDataDecoder)
 }
