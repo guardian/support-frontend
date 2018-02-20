f77f0ff30b23b3f274cd5bf579f85b80967ff2bb
James Byers <Ap0c@users.noreply.github.com>
2018-02-20 10:50:59 +0000

    Added the country switcher to the US contributions page.

---
+++ assets/pages/contributions-landing-us/components/countrySwitcherHeaderContainer.js
@@ -0,0 +1,51 @@
+// @flow
+
+// ----- Imports ----- //
+
+import { connect } from 'react-redux';
+
+import CountrySwitcherHeader from 'components/headers/countrySwitcherHeader/countrySwitcherHeader';
+
+import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
+import type { State } from '../contributionsLandingUSReducer';
+
+
+// ----- Setup ----- //
+
+const availableCountriesGroups: CountryGroupId[] =
+  ['GBPCountries', 'UnitedStates'];
+
+
+// ----- Functions ----- //
+
+function handleCountryGroupChange(value: string): void {
+  switch (value) {
+    case 'GBPCountries':
+      window.location.pathname = '/uk/contribute';
+      break;
+    case 'AUDCountries':
+      window.location.pathname = '/au';
+      break;
+    case 'EURCountries':
+      window.location.pathname = '/eu';
+      break;
+    default:
+  }
+}
+
+
+// ----- State Maps ----- //
+
+function mapStateToProps(state: State) {
+
+  return {
+    countryGroupIds: availableCountriesGroups,
+    selectedCountryGroup: state.common.countryGroup,
+    onCountryGroupSelect: handleCountryGroupChange,
+  };
+}
+
+
+// ----- Exports ----- //
+
+export default connect(mapStateToProps)(CountrySwitcherHeader);
--- assets/pages/contributions-landing-us/contributionsLandingUS.jsx
+++ assets/pages/contributions-landing-us/contributionsLandingUS.jsx
@@ -5,7 +5,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
 import Footer from 'components/footer/footer';
 import CirclesIntroduction from 'components/circlesIntroduction/circlesIntroduction';
 import Contribute from 'components/contribute/contribute';
@@ -14,6 +13,7 @@ import { init as pageInit } from 'helpers/page/page';
 import { renderPage } from 'helpers/render';
 
 import pageReducer from './contributionsLandingUSReducer';
+import CountrySwitcherHeaderContainer from './components/countrySwitcherHeaderContainer';
 import ContributionSelectionContainer from './components/contributionSelectionContainer';
 import ContributionPaymentCtasContainer from './components/contributionPaymentCtasContainer';
 import PayPalContributionButtonContainer from './components/payPalContributionButtonContainer';
@@ -35,7 +35,7 @@ const store = pageInit(
 const content = (
   <Provider store={store}>
     <div className="gu-content">
-      <SimpleHeader />
+      <CountrySwitcherHeaderContainer />
       <CirclesIntroduction
         headings={['Help us deliver', 'the independent', 'journalism the', 'world needs']}
         highlights={['Support', 'The Guardian']}
