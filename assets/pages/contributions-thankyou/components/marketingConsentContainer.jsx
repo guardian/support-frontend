e22110aba4b75a2bac2d15442964c71c4dd4739e
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-02-26 13:59:29 +0000

    [opt-in-for-recurring-contributions] Modified marketing consent as cantainer

---
+++ assets/pages/contributions-thankyou/components/marketingConsentContainer.jsx
@@ -0,0 +1,43 @@
+// @flow
+
+// ----- Imports ----- //
+
+import { connect } from 'react-redux';
+
+import { setGnmMarketing } from 'helpers/user/userActions';
+import { sendMarketingPreferencesToIdentity } from 'containerisableComponents/marketingConsent/helpers';
+import MarketingConsent from 'containerisableComponents/marketingConsent/marketingConsent';
+
+import type { Csrf as CsrfState } from 'helpers/csrf/csrfReducer';
+
+
+// ----- Component ----- //
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick: (marketingPreferencesOptIn: boolean, email: string, csfr: CsrfState) => {
+      sendMarketingPreferencesToIdentity(
+        marketingPreferencesOptIn,
+        email,
+        dispatch,
+        csfr,
+        'CONTRIBUTIONS_THANK_YOU',
+      );
+    },
+    marketingPreferenceUpdate: (preference: boolean) => {
+      dispatch(setGnmMarketing(preference));
+    },
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    email: state.page.user.email,
+    marketingPreferencesOptIn: state.page.user.gnmMarketing,
+    consentApiError: state.page.marketingConsent.error,
+    csrf: state.page.csrf,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MarketingConsent);
