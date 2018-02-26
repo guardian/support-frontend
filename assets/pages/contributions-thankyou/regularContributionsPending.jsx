ad4731ec125121fe93a3ac24ad93064a9e32b0d4
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-02-26 15:49:14 +0000

    [opt-in-for-recurring-contributions] Adding the marketing consent container to the pending thank you page and the regular contribution page.

--- assets/pages/contributions-thankyou/regularContributionsPending.jsx
+++ assets/pages/contributions-thankyou/regularContributionsPending.jsx
@@ -3,17 +3,49 @@
 // ----- Imports ----- //
 
 import React from 'react';
-import { statelessInit as pageInit } from 'helpers/page/page';
+import thunkMiddleware from 'redux-thunk';
+import { applyMiddleware, compose } from 'redux';
+import { init as pageInit } from 'helpers/page/page';
 import { renderPage } from 'helpers/render';
+import * as user from 'helpers/user/user';
 import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
 import Footer from 'components/footer/footer';
+import CtaLink from 'components/ctaLink/ctaLink';
+import MarketingConsentContainer from './components/marketingConsentContainer';
 import ThankYouIntroduction from './components/thankYouIntroduction';
 import QuestionsAndSocial from './components/questionsAndSocial';
+import reducer from './contributionsThankYouReducer';
+
 
 // ----- Page Startup ----- //
 
-pageInit();
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
 
+const store = pageInit(
+  reducer,
+  undefined,
+  composeEnhancers(applyMiddleware(thunkMiddleware)),
+);
+
+user.init(store.dispatch);
+
+const marketing = () => {
+  if (store.getState().page.user.email) {
+    return <MarketingConsentContainer />;
+  }
+  return (
+    <div className="thankyou__wrapper">
+      <CtaLink
+        ctaId="return-to-the-guardian"
+        text="Return to the Guardian"
+        url="https://theguardian.com"
+        accessibilityHint="Go to the guardian dot com front page"
+      />
+    </div>
+  );
+};
 
 // ----- Render ----- //
 
@@ -26,6 +58,7 @@ const content = (
             Look out for an email confirming your recurring
             payment."
         />
+        {marketing()}
         <QuestionsAndSocial />
       </div>
     </section>
--- assets/pages/contributions-thankyou/regularContributionsThankyouOld.jsx
+++ assets/pages/contributions-thankyou/regularContributionsThankyouOld.jsx
@@ -3,16 +3,50 @@
 // ----- Imports ----- //
 
 import React from 'react';
-import { statelessInit as pageInit } from 'helpers/page/page';
+import { applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import * as user from 'helpers/user/user';
+import { init as pageInit } from 'helpers/page/page';
 import { renderPage } from 'helpers/render';
 import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
+import CtaLink from 'components/ctaLink/ctaLink';
 import Footer from 'components/footer/footer';
 import ThankYouIntroduction from './components/thankYouIntroduction';
 import QuestionsAndSocial from './components/questionsAndSocial';
+import reducer from './contributionsThankYouReducer';
+import MarketingConsentContainer from './components/marketingConsentContainer';
+
 
 // ----- Page Startup ----- //
 
-pageInit();
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
+
+const store = pageInit(
+  reducer,
+  undefined,
+  composeEnhancers(applyMiddleware(thunkMiddleware)),
+);
+
+user.init(store.dispatch);
+
+const marketing = () => {
+  if (store.getState().page.user.email) {
+    return <MarketingConsentContainer />;
+  }
+  return (
+    <div className="thankyou__wrapper">
+      <CtaLink
+        ctaId="return-to-the-guardian"
+        text="Return to the Guardian"
+        url="https://theguardian.com"
+        accessibilityHint="Go to the guardian dot com front page"
+      />
+    </div>
+  );
+};
 
 
 // ----- Render ----- //
@@ -26,6 +60,7 @@ const content = (
             Look out for an email confirming your recurring
             payment."
         />
+        {marketing()}
         <QuestionsAndSocial />
       </div>
     </section>
