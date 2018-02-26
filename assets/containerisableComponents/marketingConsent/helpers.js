ddcbaec4b8ff4286da1d36872fa43a26c96d18ca
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-02-26 18:20:58 +0000

    [opt-in-for-recurring-contributions] We are dispatching action instead of redirects

--- assets/containerisableComponents/marketingConsent/helpers.js
+++ assets/containerisableComponents/marketingConsent/helpers.js
@@ -9,13 +9,6 @@ import { marketingConsentActionsFor } from './marketingConsentActions';
 
 // ----- Functions ----- //
 
-const marketingConfirmUrl = (marketingPreferencesOptIn: boolean) => {
-  const params = new URLSearchParams();
-  const optInParam = marketingPreferencesOptIn === true ? 'yes' : 'no';
-  params.append('optIn', optInParam);
-  return `${routes.contributionsMarketingConfirm}?${params.toString()}`;
-};
-
 const requestData = (email: string, csrf: CsrfState) => ({
   method: 'POST',
   headers: { 'Content-Type': 'application/json', 'Csrf-Token': csrf.token || '' },
@@ -30,25 +23,27 @@ function sendMarketingPreferencesToIdentity(
   dispatch: Function,
   csrf: CsrfState,
   scope: string,
-) {
+): void {
 
   const setConsentApiError = marketingConsentActionsFor(scope).setAPIError;
+  const { setConfirmMarketingConsent } = marketingConsentActionsFor(scope);
 
-  if (optIn) {
-    fetch(`${routes.contributionsSendMarketing}`, requestData(email, csrf))
-      .then((response) => {
-        if (response.status === 200) {
-          window.location = marketingConfirmUrl(optIn);
-        } else {
-          dispatch(setConsentApiError(true));
-        }
-      })
-      .catch(() => {
-        dispatch(setConsentApiError(true));
-      });
-  } else {
-    window.location = marketingConfirmUrl(optIn);
+  if (!optIn) {
+    dispatch(setConfirmMarketingConsent(false));
+    return;
   }
+
+  fetch(`${routes.contributionsSendMarketing}`, requestData(email, csrf))
+    .then((response) => {
+      if (response.status === 200) {
+        dispatch(setConfirmMarketingConsent(optIn));
+      } else {
+        dispatch(setConsentApiError(true));
+      }
+    })
+    .catch(() => {
+      dispatch(setConsentApiError(true));
+    });
 }
 
 // ----- Exports ----- //
