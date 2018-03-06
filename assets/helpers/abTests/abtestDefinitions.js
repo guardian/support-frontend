d2fbca79363a3f3ca411b85d4a49c3a4b21f17fa
rupertbates <rupert.bates@guardian.co.uk>
2018-03-06 13:59:04 +0000

    Switch the DD test off

--- assets/helpers/abTests/abtestDefinitions.js
+++ assets/helpers/abTests/abtestDefinitions.js
@@ -4,16 +4,4 @@ import type { Tests } from './abtest';
 // ----- Tests ----- //
 
 export const tests: Tests = {
-  directDebitTest: {
-    variants: ['control', 'directDebit'],
-    audiences: {
-      GB: {
-        offset: 0,
-        size: 1,
-      },
-    },
-    isActive: true,
-    independent: true,
-    seed: 0,
-  },
 };
