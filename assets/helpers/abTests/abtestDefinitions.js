fec9fd4240dabd1e4ef8ec68fd240b7019751f6a
rupertbates <rupert.bates@guardian.co.uk>
2018-03-06 14:07:56 +0000

    Switch the DD test enabled to false rather than removing altogether

--- assets/helpers/abTests/abtestDefinitions.js
+++ assets/helpers/abTests/abtestDefinitions.js
@@ -4,4 +4,16 @@ import type { Tests } from './abtest';
 // ----- Tests ----- //
 
 export const tests: Tests = {
+  directDebitTest: {
+    variants: ['control', 'directDebit'],
+    audiences: {
+      GB: {
+        offset: 0,
+        size: 1,
+      },
+    },
+    isActive: false,
+    independent: true,
+    seed: 0,
+  },
 };
