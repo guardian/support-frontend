d2a5b3dfb4127fcec687d1c62da98622d83d5416
davidfurey <david.furey@guardian.co.uk>
2018-03-13 15:07:57 +0000

    PR feedback
    Make the connection between state machine failure and cloudwatch metric
    more clear

--- app/monitoring/StateMachineMonitor.scala
+++ app/monitoring/StateMachineMonitor.scala
@@ -8,13 +8,17 @@ import SafeLogger._
 
 class StateMachineMonitor(client: RegularContributionsClient, actorSystem: ActorSystem) {
 
+  val cloudwatchMetricsPattern = "regular-contributions-state-machine-unavailable"
+
   def start(): Unit = {
     implicit val ec = actorSystem.dispatcher
     actorSystem.scheduler.schedule(5.seconds, 60.seconds) {
-      client.healthy.onComplete {
+      client.healthy().onComplete {
         case Success(true) => SafeLogger.debug("Regular contributions state machine is healthy")
-        case Success(false) => SafeLogger.error(scrub"Regular contributions state machine is not available")
-        case Failure(exception) => SafeLogger.error(scrub"Regular contributions state machine is not available", exception)
+        case Success(false) => SafeLogger.error(scrub"Regular contributions state machine is not available [$cloudwatchMetricsPattern]")
+        case Failure(exception) => SafeLogger.error(
+          scrub"Exception while fetching regular contributions state machine status [$cloudwatchMetricsPattern]", exception
+        )
       }
     }
   }
