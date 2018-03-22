5365dae5fa47264aa3ce6fc86b3ceda22cf7df8d
James Byers <Ap0c@users.noreply.github.com>
2018-03-22 12:27:24 +0000

    Set the correct image sizes.

--- assets/components/threeSubscriptions/threeSubscriptions.jsx
+++ assets/components/threeSubscriptions/threeSubscriptions.jsx
@@ -12,7 +12,7 @@ import SubscriptionBundle from 'components/subscriptionBundle/subscriptionBundle
 
 const gridImageProperties = {
   srcSizes: [825, 500, 140],
-  sizes: '(max-width: 480px) 90vw, (max-width: 660px) 400px, 165px',
+  sizes: '(max-width: 660px) 165px, (max-width: 740px) 174px, (max-width: 980px) 196px, (max-width: 1140px) 205px, 165px',
   imgType: 'png',
 };
 
