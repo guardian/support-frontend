250a0a919876705d9e20c3b1cbf743f19d444761
rupertbates <rupert.bates@guardian.co.uk>
2018-03-23 16:16:55 +0000

    Fix asset paths for dev-server

--- app/wiring/Assets.scala
+++ app/wiring/Assets.scala
@@ -4,5 +4,5 @@ import assets.AssetsResolver
 import play.api.BuiltInComponentsFromContext
 
 trait Assets { self: BuiltInComponentsFromContext =>
-  val assetsResolver = new AssetsResolver("/assets/", "assets.map", environment)
+  val assetsResolver = new AssetsResolver("", "assets.map", environment)
 }
