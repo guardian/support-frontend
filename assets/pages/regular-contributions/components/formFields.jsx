37938d4d5f36eadb1f1b21472e0674c826a5abb0
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-04-03 12:46:53 +0100

    [dropdown-for-aud-gbp] Adding country dropdown to checkout flows

--- assets/pages/regular-contributions/components/formFields.jsx
+++ assets/pages/regular-contributions/components/formFields.jsx
@@ -94,7 +94,9 @@ function countriesDropdown(
   country: IsoCountry,
 ) {
 
-  if (countryGroup !== 'EURCountries' && countryGroup !== 'International' && countryGroup !== 'NZDCountries') {
+  const askForCountryCountryGroups = ['EURCountries', 'International', 'NZDCountries', 'GBPCountries', 'AUDCountries'];
+
+  if (!askForCountryCountryGroups.includes(countryGroup)) {
     return null;
   }
 
