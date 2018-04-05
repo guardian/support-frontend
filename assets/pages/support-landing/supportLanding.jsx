261604f366068d992c7d422f0210399c1179d000
James Byers <Ap0c@users.noreply.github.com>
2018-04-05 14:09:22 +0100

    Built the subs links using the externalLinks helper.

--- assets/pages/support-landing/supportLanding.jsx
+++ assets/pages/support-landing/supportLanding.jsx
@@ -9,7 +9,6 @@ import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
 import Footer from 'components/footer/footer';
 import CirclesIntroduction from 'components/circlesIntroduction/circlesIntroduction';
 import Contribute from 'components/contribute/contribute';
-import ThreeSubscriptions from 'components/threeSubscriptions/threeSubscriptions';
 import WhySupport from 'components/whySupport/whySupport';
 import ReadyToSupport from 'components/readyToSupport/readyToSupport';
 
@@ -20,6 +19,7 @@ import pageReducer from './supportLandingReducer';
 import ContributionSelectionContainer from './components/contributionSelectionContainer';
 import ContributionPaymentCtasContainer from './components/contributionPaymentCtasContainer';
 import PayPalContributionButtonContainer from './components/payPalContributionButtonContainer';
+import ThreeSubscriptionsContainer from './components/threeSubscriptionsContainer';
 import PatronsEventsContainer from './components/patronsEventsContainer';
 
 
@@ -58,7 +58,7 @@ const content = (
             PayPalButton={PayPalContributionButtonContainer}
           />
         </Contribute>
-        <ThreeSubscriptions />
+        <ThreeSubscriptionsContainer />
       </section>
       <WhySupport />
       <ReadyToSupport ctaUrl={`#${supporterSectionId}`} />
