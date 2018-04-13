4c86881eb2b9728b8b5331b7836b7ab8124170c9
rupertbates <rupert.bates@guardian.co.uk>
2018-04-13 14:25:35 +0100

    Refactor the direct debit guarantee expanding link so that it can be reused on the thank you page

--- assets/components/directDebit/directDebitForm/directDebitForm.jsx
+++ assets/components/directDebit/directDebitForm/directDebitForm.jsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ErrorMessage from 'components/errorMessage/errorMessage';
 import SortCodeInput from 'components/directDebit/directDebitForm/sortCodeInput';
+import DirectDebitGuarantee from 'components/directDebit/directDebitForm/directDebitGuarantee';
 import {
   updateSortCode,
   updateAccountNumber,
@@ -24,6 +25,7 @@ import {
   SvgArrowRightStraight,
   SvgExclamationAlternate,
 } from 'components/svg/svg';
+
 
 
 // ---- Types ----- //
@@ -144,7 +146,9 @@ const DirectDebitForm = (props: PropTypes) => (
       svg={<SvgExclamationAlternate />}
     />
 
-    <LegalNotice
+    <LegalNotice />
+
+    <DirectDebitGuarantee
       isDDGuaranteeOpen={props.isDDGuaranteeOpen}
       openDDGuaranteeClicked={props.openDDGuaranteeClicked}
       closeDDGuaranteeClicked={props.closeDDGuaranteeClicked}
@@ -308,10 +312,7 @@ function PaymentButton(props: {
   }
 }
 
-function LegalNotice(props: {
-  isDDGuaranteeOpen: boolean,
-  openDDGuaranteeClicked: () => void,
-  closeDDGuaranteeClicked: () => void}) {
+function LegalNotice() {
   return (
     <div className="component-direct-debit-form__legal-notice">
       <p><strong>Advance notice</strong> The details of your Direct Debit instruction including
@@ -327,42 +328,6 @@ function LegalNotice(props: {
         <a href="mailto:contribution.support@theguardian.com">contribution.support@theguardian.com</a>
       </p>
       <SvgDirectDebitSymbolAndText />
-      <p>
-        Your payments are protected by the&nbsp;
-        <button className="component-direct-debit-form__open-link" onClick={props.isDDGuaranteeOpen ? props.closeDDGuaranteeClicked : props.openDDGuaranteeClicked}>
-          Direct Debit guarantee
-        </button>.
-        <div>
-          <ul className={`component-direct-debit-form__guarantee-list ${props.isDDGuaranteeOpen ? '' : 'component-direct-debit-form__guarantee-list--closed'}`}>
-            <li>
-              The Guarantee is offered by all banks and building societies that accept instructions
-              to pay Direct Debits
-            </li>
-            <li>
-              If there are any changes to the amount, date or frequency of your Direct Debit
-              Guardian News & Media Ltd will notify you at least three working days in advance of
-              your account being debited or as otherwise agreed.
-            </li>
-            <li>
-              If you ask Guardian News & Media Ltd to collect a payment, confirmation of the amount
-              and date will be given to you at the time of the request.
-            </li>
-            <li>
-              If an error is made in the payment of your Direct Debit by Guardian News & Media Ltd
-              or your bank or building society, you are entitled to a full and immediate refund of
-              the amount paid from your bank or building society.
-            </li>
-            <li>
-              If you receive a refund you are not entitled to, you must pay it back when Guardian
-              News & Media Ltd asks you to.
-            </li>
-            <li>
-              You can cancel a Direct Debit at any time by contacting your bank or building society.
-              Written confirmation may be required. Please also notify us.
-            </li>
-          </ul>
-        </div>
-      </p>
     </div>
   );
 }
---
+++ assets/components/directDebit/directDebitForm/directDebitGuarantee.jsx
@@ -0,0 +1,52 @@
+// @flow
+
+// ----- Imports ----- //
+import React from 'react';
+
+function DirectDebitGuarantee(props: {
+  isDDGuaranteeOpen: boolean,
+  openDDGuaranteeClicked: () => void,
+  closeDDGuaranteeClicked: () => void}) {
+  return (
+    <div className="component-direct-debit-form__guarantee">
+      <p>
+        Your payments are protected by the&nbsp;
+        <button className="component-direct-debit-form__open-link" onClick={props.isDDGuaranteeOpen ? props.closeDDGuaranteeClicked : props.openDDGuaranteeClicked}>
+          Direct Debit guarantee
+        </button>.
+        <div>
+          <ul className={`component-direct-debit-form__guarantee-list ${props.isDDGuaranteeOpen ? '' : 'component-direct-debit-form__guarantee-list--closed'}`}>
+            <li>
+              The Guarantee is offered by all banks and building societies that accept instructions
+              to pay Direct Debits
+            </li>
+            <li>
+              If there are any changes to the amount, date or frequency of your Direct Debit
+              Guardian News & Media Ltd will notify you at least three working days in advance of
+              your account being debited or as otherwise agreed.
+            </li>
+            <li>
+              If you ask Guardian News & Media Ltd to collect a payment, confirmation of the amount
+              and date will be given to you at the time of the request.
+            </li>
+            <li>
+              If an error is made in the payment of your Direct Debit by Guardian News & Media Ltd
+              or your bank or building society, you are entitled to a full and immediate refund of
+              the amount paid from your bank or building society.
+            </li>
+            <li>
+              If you receive a refund you are not entitled to, you must pay it back when Guardian
+              News & Media Ltd asks you to.
+            </li>
+            <li>
+              You can cancel a Direct Debit at any time by contacting your bank or building society.
+              Written confirmation may be required. Please also notify us.
+            </li>
+          </ul>
+        </div>
+      </p>
+    </div>
+  );
+}
+
+export default DirectDebitGuarantee;
