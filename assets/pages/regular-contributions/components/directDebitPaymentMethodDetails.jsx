e05b9f22aa13e7226fb988690b2c3a942a8b9dfd
rupertbates <rupert.bates@guardian.co.uk>
2018-04-18 11:07:49 +0100

    PR feedback and Jest snapshot update

--- assets/pages/regular-contributions/components/directDebitPaymentMethodDetails.jsx
+++ assets/pages/regular-contributions/components/directDebitPaymentMethodDetails.jsx
@@ -6,11 +6,17 @@ import React from 'react';
 
 import PageSection from 'components/pageSection/pageSection';
 
+type PropTypes = {
+  accountHolderName: string,
+  accountNumber: string,
+  sortCodeArray: string[],
+}
+
 function mask(s: string): string {
   return `******${s.substring(6)}`;
 }
 
-export default function DirectDebitPaymentMethodDetails(props: Object) {
+export default function DirectDebitPaymentMethodDetails(props: PropTypes) {
   return (
     <div className="component-direct-debit-confirmation">
       <PageSection modifierClass="email-confirmation" heading="Your contribution">
@@ -18,21 +24,21 @@ export default function DirectDebitPaymentMethodDetails(props: Object) {
           Look out for an email confirming your recurring payment.
         </p>
         <ul className="component-direct-debit-confirmation__details">
-          <li className="component-direct-debit-confirmation__details__item">
-            <div className="component-direct-debit-confirmation__details__item-name">Payment Method:</div>
-            <div className="component-direct-debit-confirmation__details__item-value">Direct Debit</div>
+          <li className="component-direct-debit-confirmation__item">
+            <div className="component-direct-debit-confirmation__item-name">Payment Method:</div>
+            <div className="component-direct-debit-confirmation__item-value">Direct Debit</div>
           </li>
-          <li className="component-direct-debit-confirmation__details__item">
-            <div className="component-direct-debit-confirmation__details__item-name">Account Name:</div>
-            <div className="component-direct-debit-confirmation__details__item-value">{props.accountHolderName}</div>
+          <li className="component-direct-debit-confirmation__item">
+            <div className="component-direct-debit-confirmation__item-name">Account Name:</div>
+            <div className="component-direct-debit-confirmation__item-value">{props.accountHolderName}</div>
           </li>
-          <li className="component-direct-debit-confirmation__details__item">
-            <div className="component-direct-debit-confirmation__details__item-name">Account number:</div>
-            <div className="component-direct-debit-confirmation__details__item-value">{mask(props.accountNumber)}</div>
+          <li className="component-direct-debit-confirmation__item">
+            <div className="component-direct-debit-confirmation__item-name">Account number:</div>
+            <div className="component-direct-debit-confirmation__item-value">{mask(props.accountNumber)}</div>
           </li>
-          <li className="component-direct-debit-confirmation__details__item">
-            <div className="component-direct-debit-confirmation__details__item-name">Sort Code:</div>
-            <div className="component-direct-debit-confirmation__details__item-value">{props.sortCodeArray[0]}-{props.sortCodeArray[1]}-{props.sortCodeArray[2]}</div>
+          <li className="component-direct-debit-confirmation__item">
+            <div className="component-direct-debit-confirmation__item-name">Sort Code:</div>
+            <div className="component-direct-debit-confirmation__item-value">{props.sortCodeArray[0]}-{props.sortCodeArray[1]}-{props.sortCodeArray[2]}</div>
           </li>
         </ul>
       </PageSection>
