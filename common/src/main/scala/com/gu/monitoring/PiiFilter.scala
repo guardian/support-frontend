b9dcfe6a6b10f46d59089ac4721394d2865cc799
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2018-05-22 11:45:07 +0100

    SentryFilters -> PiiFilter

--- common/src/main/scala/com/gu/monitoring/PiiFilter.scala
+++ common/src/main/scala/com/gu/monitoring/PiiFilter.scala
@@ -4,15 +4,15 @@ import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.filter.Filter
 import ch.qos.logback.core.spi.FilterReply
 
-object SentryFilters {
+//object SentryFilters {
 
   class PiiFilter extends Filter[ILoggingEvent] {
     override def decide(event: ILoggingEvent): FilterReply = if (event.getMarker.contains(SafeLogger.sanitizedLogMessage)) FilterReply.ACCEPT
     else FilterReply.DENY
   }
 
-  val piiFilter = new PiiFilter
-
-  piiFilter.start()
-
-}
+//  val piiFilter = new PiiFilter
+//
+//  piiFilter.start()
+//
+//}
