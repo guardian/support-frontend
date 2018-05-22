46ddff185888c85716c12f13428164997ba692b1
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-05-22 13:56:21 +0100

    [applying-payment-logos] Making Payment logos bigger

--- assets/containerisableComponents/contributionAwarePaymentLogos/contributionAwarePaymentLogos.jsx
+++ assets/containerisableComponents/contributionAwarePaymentLogos/contributionAwarePaymentLogos.jsx
@@ -4,17 +4,25 @@
 
 import React from 'react';
 
+import { classNameWithModifiers } from 'helpers/utilities';
+
 import SvgVisaLogo from 'components/svgs/visaLogo';
 import SvgMastercardLogo from 'components/svgs/mastercardLogo';
 import SvgPaypalLogo from 'components/svgs/payPalLogo';
 import SvgAmexLogo from 'components/svgs/amexLogo';
 
+import type { Contrib as ContributionType } from 'helpers/contributions';
+
 // ----- Component ----- //
 
-export default function ContributionAwarePaymentLogos() {
+export default function ContributionAwarePaymentLogos(props: { contributionType: ContributionType }) {
+  let modifierClass = '';
+  if (props.contributionType === 'ONE_OFF') {
+    modifierClass = 'one-off';
+  }
 
   return (
-    <div className="contribution-aware-payment-logos">
+    <div className={classNameWithModifiers('contribution-aware-payment-logos', [modifierClass])}>
       <SvgVisaLogo />
       <SvgMastercardLogo />
       <SvgAmexLogo />
