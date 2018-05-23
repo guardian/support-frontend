4b5e07f78a0cddd4f6d4de0a8979b8fe4af7b1ef
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-05-23 17:56:59 +0100

    [desktop-only-test-2] Added test definition for above the fold

--- assets/helpers/abTests/abtestDefinitions.js
+++ assets/helpers/abTests/abtestDefinitions.js
@@ -4,4 +4,23 @@ import type { Tests } from './abtest';
 // ----- Tests ----- //
 
 export const tests: Tests = {
+  desktopAboveTheFold: {
+    variants: ['control', 'variant'],
+    audiences: {
+      GB: {
+        offset: 0,
+        size: 1,
+      },
+      US: {
+        offset: 0,
+        size: 1,
+      },
+    },
+    customSegmentCondition: () => {
+      return window.matchMedia('(min-width: 980px)').matches && (window.location.pathname.indexOf('contribute') > 0);
+    }, // matches the 'desktop' breakpoint defined in breakpoints.scss and contribute page
+    isActive: false,
+    independent: true,
+    seed: 0,
+  },
 };
