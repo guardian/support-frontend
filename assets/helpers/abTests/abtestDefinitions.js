4a13f79a5449260a08d4891a6da5e751f5ad1f16
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-05-24 13:48:40 +0100

    initial commit

--- assets/helpers/abTests/abtestDefinitions.js
+++ assets/helpers/abTests/abtestDefinitions.js
@@ -17,4 +17,17 @@ export const tests: Tests = {
     independent: true,
     seed: 0,
   },
+  oneOffOnlyTest: {
+      variants: ['control', 'oneOffOnly'],
+      audiences: {
+        ALL: {
+            offset: 0,
+            size: 1,
+        },
+      },
+      customSegmentCondition: () => window.matchMedia('(max-width: 320px)').matches, // matches the 'mobile' breakpoint defined in breakpoints.scss
+      isActive: true,
+      independent: true,
+      seed: 1,
+  },
 };
