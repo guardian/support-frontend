d001c22a73521ef59ad7b2362cb1e03bf4d11097
James Byers <Ap0c@users.noreply.github.com>
2018-05-29 18:30:19 +0100

    Created multinational versions of digital landing page.

--- assets/pages/digital-subscription-landing/digitalSubscriptionLanding.jsx
+++ assets/pages/digital-subscription-landing/digitalSubscriptionLanding.jsx
@@ -3,20 +3,50 @@
 // ----- Imports ----- //
 
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import { renderPage } from 'helpers/render';
+import { detect, type CountryGroupId } from 'helpers/internationalisation/countryGroup';
 
-import SimpleHeader from 'components/headers/simpleHeader/simpleHeader';
+import countrySwitcherHeaderContainer from 'components/headers/countrySwitcherHeader/countrySwitcherHeaderContainer';
 import Footer from 'components/footer/footer';
+
+import { init as pageInit } from 'helpers/page/page';
+
+
+// ----- Redux Store ----- //
+
+const store = pageInit();
+
+
+// ----- Internationalisation ----- //
+
+const countryGroupId: CountryGroupId = detect();
+
+const reactElementId: {
+  [CountryGroupId]: string,
+} = {
+  GBPCountries: 'digital-subscription-landing-page-uk',
+  UnitedStates: 'digital-subscription-landing-page-us',
+  AUDCountries: 'digital-subscription-landing-page-au',
+  International: 'digital-subscription-landing-page-int',
+};
+
+const CountrySwitcherHeader = countrySwitcherHeaderContainer(
+  '/subscribe/digital',
+  ['GBPCountries', 'UnitedStates', 'AUDCountries', 'International'],
+);
 
 
 // ----- Render ----- //
 
 const content = (
-  <div>
-    <SimpleHeader />
-    <Footer />
-  </div>
+  <Provider store={store}>
+    <div>
+      <CountrySwitcherHeader />
+      <Footer />
+    </div>
+  </Provider>
 );
 
-renderPage(content, 'digital-subscription-landing-page');
+renderPage(content, reactElementId[countryGroupId]);
