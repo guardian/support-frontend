c8b6a335f08645ea766009613304241e55b4897f
James Byers <Ap0c@users.noreply.github.com>
2018-05-31 11:30:07 +0100

    Better a11y descriptions for social share.

--- assets/components/socialShare/socialShare.jsx
+++ assets/components/socialShare/socialShare.jsx
@@ -21,6 +21,7 @@ type PropTypes = {
 type SocialMedia = {
   link: string,
   svg: Node,
+  a11yHint: string,
 };
 
 
@@ -32,10 +33,12 @@ const socialMedia: {
   facebook: {
     link: 'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fsupport.theguardian.com%2F%3FINTCMP%3Dsocial&t=',
     svg: <SvgFacebook />,
+    a11yHint: 'Share on facebook',
   },
   twitter: {
     link: 'https://twitter.com/intent/tweet?text=I%27ve+just+contributed+to+the+Guardian.+Join+me+in+supporting+independent+journalism+https%3A%2F%2Fsupport.theguardian.com&amp;related=guardian',
     svg: <SvgTwitter />,
+    a11yHint: 'Share on twitter',
   },
 };
 
@@ -44,15 +47,17 @@ const socialMedia: {
 
 export default function SocialShare(props: PropTypes) {
 
+  const a11yId = `component-social-share-a11y-hint-${props.name}`;
+
   return (
     <a
       className="component-social-share"
       href={socialMedia[props.name].link}
-      aria-labelledby="component-social-share-a11y-hint"
+      aria-labelledby={a11yId}
     >
       {socialMedia[props.name].svg}
-      <p id="component-social-share-a11y-hint" className="accessibility-hint">
-        Share on social media
+      <p id={a11yId} className="accessibility-hint">
+        {socialMedia[props.name].a11yHint}
       </p>
     </a>
   );
