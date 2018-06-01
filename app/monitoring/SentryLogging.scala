04b629886410f3d6a972973b40d84b005bc79e7c
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2018-06-01 14:58:55 +0100

    Remove junk logging for testing

--- app/monitoring/SentryLogging.scala
+++ app/monitoring/SentryLogging.scala
@@ -16,16 +16,13 @@ object SentryLogging {
         SafeLogger.info(s"Initialising Sentry logging with $sentryDSN")
         Try {
           val sentryClient = Sentry.init(sentryDSN)
-          SafeLogger.info("Initialised! Adding tags.")
           val buildInfo: Map[String, String] = app.BuildInfo.toMap.mapValues(_.toString)
           val tags = Map("stage" -> config.stage.toString) ++ buildInfo
-          SafeLogger.info(s"tagmap $tags")
           sentryClient.setTags(tags.asJava)
         } match {
           case Success(_) => SafeLogger.debug("Sentry logging configured.")
           case Failure(e) => SafeLogger.error(scrub"Something went wrong when setting up Sentry logging ${e.getStackTrace}")
         }
     }
-    SafeLogger.error(scrub"*TEST* more spam from Leigh-Anne. Why, you look radiant today! ^-^ ")
   }
 }
