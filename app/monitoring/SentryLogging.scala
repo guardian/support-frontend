f4ff6837372986ea040e41f179fd3921c036869a
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2018-06-04 18:05:29 +0100

    Move creating sentry client and appender out of logback.xml

--- app/monitoring/SentryLogging.scala
+++ app/monitoring/SentryLogging.scala
@@ -1,12 +1,17 @@
 package monitoring
 
+import ch.qos.logback.classic.filter.ThresholdFilter
 import monitoring.SafeLogger._
-
 import config.Configuration
 import io.sentry.Sentry
+import io.sentry.logback.SentryAppender
 
 import scala.collection.JavaConverters._
 import scala.util.{Failure, Success, Try}
+import SentryFilters._
+import ch.qos.logback.classic.Logger
+import org.slf4j.Logger.ROOT_LOGGER_NAME
+import org.slf4j.LoggerFactory
 
 object SentryLogging {
   def init(config: Configuration) {
@@ -16,13 +21,34 @@ object SentryLogging {
         SafeLogger.info(s"Initialising Sentry logging")
         Try {
           val sentryClient = Sentry.init(sentryDSN)
+          val sentryAppender = new SentryAppender {
+            addFilter(errorLevelFilter)
+            addFilter(piiFilter)
+          }
+
+          sentryAppender.start()
+
           val buildInfo: Map[String, String] = app.BuildInfo.toMap.mapValues(_.toString)
           val tags = Map("stage" -> config.stage.toString) ++ buildInfo
           sentryClient.setTags(tags.asJava)
+
+          LoggerFactory.getLogger(ROOT_LOGGER_NAME).asInstanceOf[Logger].addAppender(sentryAppender)
         } match {
           case Success(_) => SafeLogger.debug("Sentry logging configured.")
           case Failure(e) => SafeLogger.error(scrub"Something went wrong when setting up Sentry logging ${e.getStackTrace}")
         }
     }
+    val compliment = "bee's knees"
+    SafeLogger.error(scrub"Why, you are just the bee's knees! again, $compliment!")
   }
+}
+
+object SentryFilters {
+
+  val errorLevelFilter = new ThresholdFilter { setLevel("ERROR") }
+  val piiFilter = new PiiFilter
+
+  errorLevelFilter.start()
+  piiFilter.start()
+
 }
