ec528faa37fced5c7190a4d8401fb1c5ecb85d84
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-06-04 14:36:26 +0100

    [dropping-intro-text] Added "drop the intro" test

--- assets/helpers/abTests/abtestDefinitions.js
+++ assets/helpers/abTests/abtestDefinitions.js
@@ -4,5 +4,17 @@ import type { Tests } from './abtest';
 // ----- Tests ----- //
 
 export const tests: Tests = {
-
+  dropIntroText: {
+    variants: ['control', 'variant'],
+    audiences: {
+      ALL: {
+        offset: 0,
+        size: 1,
+      },
+    },
+    customSegmentCondition: () => window.location.pathname.indexOf('contribute') > 0, // matches any contribute page
+    isActive: true,
+    independent: true,
+    seed: 0,
+  },
 };
