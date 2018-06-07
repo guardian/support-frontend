44fe3b156ab5f541d177584c9fed287c28dadc95
rupertbates <rupert.bates@guardian.co.uk>
2018-06-07 17:33:42 +0100

    Add in Amber Rudd

---
+++ assets/components/svgs/amberRudd.jsx
@@ -0,0 +1,18 @@
+// @flow
+
+import React from 'react';
+
+export default function AmberRudd() {
+  return (
+    <svg
+      className="amber-rudd"
+      height="412"
+      width="404"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="273" cy="35" fill="#FFABDB" r="26" />
+      <circle cx="349.5" cy="53.5" fill="#FFE501" r="52.5" />
+      <image xlinkHref="https://media.guim.co.uk/33d55e682e496b355baed45c0d92ca7dedbc6eff/0_0_370_370/370.png" y="42" height="370" width="370" />
+    </svg>
+  );
+}
--- assets/pages/digital-subscription-landing/components/independentJournalismSection.jsx
+++ assets/pages/digital-subscription-landing/components/independentJournalismSection.jsx
@@ -2,19 +2,26 @@
 
 // ----- Imports ----- //
 import React from 'react';
+import AmberRudd from 'components/svgs/amberRudd';
 import PriceCtaContainer from './priceCtaContainer';
 
 export default function IndependentJournalismSection() {
+
   return (
-    <div className="component-independent-journalism__header">
-      <h2>Your subscription helps support independent investigative journalism</h2>
-      <p className="component-independent-journalism__copy">
-        Independence means we can pursue a story without fear of where it might take us.
-        We are dedicated to holding power
-        to account, to reporting the truth,
-        and exposing corruption wherever
-        we find it.
-      </p>
+    <div className="component-independent-journalism">
+      <div className="component-independent-journalism__content">
+        <h2 className="component-independent-journalism__header">Your subscription helps support independent investigative journalism</h2>
+        <div className="middle">
+          <p className="component-independent-journalism__copy">
+            Independence means we can pursue a story without fear of where it might take us.
+            We are dedicated to holding power
+            to account, to reporting the truth,
+            and exposing corruption wherever
+            we find it.
+          </p>
+          <AmberRudd />
+        </div>
+      </div>
       <PriceCtaContainer />
     </div>
   );
