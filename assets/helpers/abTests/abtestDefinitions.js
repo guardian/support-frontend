c7c88bbc87b1bf56d75eefd5836338e50c249de7
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-06-08 14:41:14 +0100

    [one-off-vs-one-time-vs-single] Defined the test in all places except US

--- assets/helpers/abTests/abtestDefinitions.js
+++ assets/helpers/abTests/abtestDefinitions.js
@@ -17,4 +17,36 @@ export const tests: Tests = {
     independent: true,
     seed: 0,
   },
+  oneOffOneTimeSingle: {
+    variants: ['control', 'single', 'once', 'oneTime'],
+    audiences: {
+      GBPCountries: {
+        offset: 0,
+        size: 1,
+      },
+      AUDCountries: {
+        offset: 0,
+        size: 1,
+      },
+      Canada: {
+        offset: 0,
+        size: 1,
+      },
+      NZDCountries: {
+        offset: 0,
+        size: 1,
+      },
+      International: {
+        offset: 0,
+        size: 1,
+      },
+      EURCountries: {
+        offset: 0,
+        size: 1,
+      },
+    },
+    isActive: true,
+    independent: true,
+    seed: 0,
+  },
 };
