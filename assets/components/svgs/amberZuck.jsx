41d42ff84a5c5249d12689ea9350f725a657427f
rupertbates <rupert.bates@guardian.co.uk>
2018-06-13 17:15:20 +0100

    Use a slightly larger image in the combined svg

--- assets/components/svgs/amberZuck.jsx
+++ assets/components/svgs/amberZuck.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 import { gridUrl } from 'helpers/theGrid';
 
 export default function AmberZuck() {
-  const amberUrl = gridUrl('amberRudd', 140, 'png');
-  const zuckUrl = gridUrl('zuck', 140, 'png');
+  const amberUrl = gridUrl('amberRudd', 370, 'png');
+  const zuckUrl = gridUrl('zuck', 374, 'png');
   return (
     <svg className="svg-amber-zuck" xmlns="http://www.w3.org/2000/svg" width="576" height="220" viewBox="0 0 576 220">
       <path fill="#E2E2E2" d="M465.552 219C405.077 219 357 169.975 357 109.5S406.025 0 466.5 0 576 49.025 576 109.5 526.027 219 465.552 219z" />
