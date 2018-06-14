72611ab9d9b2cb6791ea268a03c9c2e79980fa8e
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-06-14 11:16:52 +0100

    [stripe-elements-test] Tidied up stripeInlineForm

--- assets/components/stripeInlineForm/stripeInlineForm.jsx
+++ assets/components/stripeInlineForm/stripeInlineForm.jsx
@@ -7,40 +7,15 @@ import React from 'react';
 import { StripeProvider, Elements, CardElement, injectStripe } from 'react-stripe-elements';
 import { getStripeKey } from 'helpers/paymentIntegrations/stripeCheckout';
 import { type Currency } from 'helpers/internationalisation/currency';
-import { type  Status } from 'helpers/switch';
+import { type Status } from 'helpers/switch';
 import SvgArrowRightStraight from 'components/svgs/arrowRightStraight';
 import Switchable from 'components/switchable/switchable';
 import PaymentError from 'components/switchable/errorComponents/paymentError';
 
 
-// ----- Component ----- //
+// ----- Types -----//
 
-function checkoutForm(props: {stripe: Object, callback: (token: string) => mixed}) {
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    props
-      .stripe
-      .createToken()
-      .then(({ token }) => {
-        props.callback(token.id);
-      });
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <span className="component-stripe-inline-form__label-content">Enter credit/debit card details</span>
-        <CardElement className="component-stripe-inline-form__card-element" hidePostalCode style={{ base: { fontSize: '14px', fontFamily: 'Guardian Text Sans Web\', \'Helvetica Neue\', Helvetica, Arial, \'Lucida Grande', lineHeight: '40px' } }} />
-      </label>
-      <button className="component-stripe-inline-form__submit-payment">Confirm card payment <SvgArrowRightStraight /></button>
-    </form>);
-}
-
-const InjectedCheckoutForm = injectStripe(checkoutForm);
-
-
+/* eslint-disable react/no-unused-prop-types */
 type PropTypes = {
   dispatch: () => mixed,
   stripeIsLoaded: () => mixed,
@@ -50,6 +25,10 @@ type PropTypes = {
   callback: ()=> mixed,
   switchStatus: Status,
 };
+/* eslint-enable react/no-unused-prop-types */
+
+
+// ---- Auxiliary functions ----- //
 
 const setupStripeInlineForm = (dispatch: Function, stripeIsLoaded: () => mixed) => {
   const htmlElement = document.querySelector('#stripe-js');
@@ -63,7 +42,11 @@ const setupStripeInlineForm = (dispatch: Function, stripeIsLoaded: () => mixed) 
     );
   }
 };
-export default function StripeInlineFormComp(props: PropTypes) {
+
+
+// ----- Component ----- //
+
+function StripeInlineFormComp(props: PropTypes) {
 
   if (props.isStripeLoaded === false && window.Stripe === undefined) {
     setupStripeInlineForm(props.dispatch, props.stripeIsLoaded);
@@ -86,3 +69,41 @@ const StripeInlineForm = (props: PropTypes) => (
     fallback={() => <PaymentError paymentMethod="credit/debit card" />}
   />
 );
+
+// ----- Auxiliary components ----- //
+
+const stripeElementsStyle = {
+  base:
+    {
+      fontSize: '14px',
+      fontFamily: 'Guardian Text Sans Web\', \'Helvetica Neue\', Helvetica, Arial, \'Lucida Grande',
+      lineHeight: '40px',
+    },
+};
+
+function checkoutForm(props: {stripe: Object, callback: (token: string) => mixed}) {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    props
+      .stripe
+      .createToken()
+      .then(({ token }) => {
+        props.callback(token.id);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <span className="component-stripe-inline-form__label-content">Enter credit/debit card details</span>
+        <CardElement className="component-stripe-inline-form__card-element" hidePostalCode style={stripeElementsStyle} />
+      </label>
+      <button className="component-stripe-inline-form__submit-payment">Confirm card payment <SvgArrowRightStraight /></button>
+    </form>);
+}
+
+const InjectedCheckoutForm = injectStripe(checkoutForm);
+
+export default StripeInlineForm;
