0a28bd64a0395b97151130cda5675f77948b75d4
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-06-15 17:08:25 +0100

    [stripe-inline-test-part-2] Adding set and reset error functions

--- assets/components/stripeInlineForm/stripeInlineFormActions.js
+++ assets/components/stripeInlineForm/stripeInlineFormActions.js
@@ -3,7 +3,9 @@
 // ----- Types ----- //
 
 export type Action =
-  | { type: 'STRIPE_IS_LOADED', scope: string };
+  | { type: 'STRIPE_IS_LOADED', scope: string }
+  | { type: 'SET_ERROR', scope: string, message: string }
+  | { type: 'RESET_ERROR', scope: string };
 
 
 // ----- Action Creators ----- //
@@ -14,6 +16,12 @@ function stripeInlineFormActionsFor(scope: string): Object {
     stripeIsLoaded(): Action {
       return { type: 'STRIPE_IS_LOADED', scope };
     },
+    setError(message: string): Action {
+      return { type: 'SET_ERROR', scope, message };
+    },
+    resetError(): Action {
+      return { type: 'RESET_ERROR', scope };
+    }
   };
 }
 
