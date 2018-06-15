17b6b2063094dfab919c8a66e35532dc1005b34e
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-06-15 17:14:26 +0100

    [stripe-inline-test-part-2] Added postdploytestuser logic, error handling and can proceed function.

--- assets/components/stripeInlineForm/stripeInlineForm.jsx
+++ assets/components/stripeInlineForm/stripeInlineForm.jsx
@@ -9,8 +9,10 @@ import { getStripeKey } from 'helpers/paymentIntegrations/stripeCheckout';
 import { type Currency } from 'helpers/internationalisation/currency';
 import { type Status } from 'helpers/switch';
 import SvgArrowRightStraight from 'components/svgs/arrowRightStraight';
+import ErrorMessage from 'components/errorMessage/errorMessage';
 import Switchable from 'components/switchable/switchable';
 import PaymentError from 'components/switchable/errorComponents/paymentError';
+import * as storage from 'helpers/storage';
 
 
 // ----- Types -----//
@@ -23,6 +25,11 @@ type PropTypes = {
   isTestUser: boolean,
   callback: () => mixed,
   switchStatus: Status,
+  canOpen: () => boolean,
+  errorMessage: ?string,
+  isPostDeploymentTestUser: boolean,
+  setError: (message: string) =>void,
+  resetError: () =>void,
 };
 /* eslint-enable react/no-unused-prop-types */
 
@@ -53,7 +60,15 @@ function StripeInlineFormComp(props: PropTypes) {
   return (
     <StripeProvider apiKey={getStripeKey(props.currency.iso, props.isTestUser)}>
       <Elements>
-        <InjectedCheckoutForm callback={props.callback} currency={props.currency} />
+        <InjectedCheckoutForm
+          callback={props.callback}
+          currency={props.currency}
+          isPostDeploymentTestUser={props.isPostDeploymentTestUser}
+          canOpen={props.canOpen}
+          errorMessage={props.errorMessage}
+          setError={props.setError}
+          resetError={props.resetError}
+        />
       </Elements>
     </StripeProvider>
   );
@@ -78,18 +93,41 @@ const stripeElementsStyle = {
     },
 };
 
-function checkoutForm(props: {stripe: Object, callback: (token: string) => mixed}) {
+function checkoutForm(props: {
+  stripe: Object,
+  callback: (token: string) => mixed,
+  isPostDeploymentTestUser: boolean,
+  canOpen: () => boolean,
+  errorMessage: ?string,
+  setError: (string) => void,
+  resetError: () => void,
+}) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    props
-      .stripe
-      .createToken()
-      .then(({ token }) => {
-        props.callback(token.id);
-      });
+    // Don't open Stripe Checkout for automated tests, call the backend immediately
+    if (props.isPostDeploymentTestUser) {
+      const testTokenId = 'tok_visa';
+      props.callback(testTokenId);
+    } else if (props.canOpen && props.canOpen()) {
+      storage.setSession('paymentMethod', 'Stripe');
+      props
+        .stripe
+        .createToken()
+        .then(({ token, error }) => {
+          if (error !== undefined) {
+            props.setError(error.message);
+          } else {
+            props.resetError();
+            props.callback(token.id);
+          }
+
+        });
+    }
   };
+
+  const errorComponent = props.inlineStripeErrorMessage !== null ? <ErrorMessage message={props.errorMessage} /> : null;
 
   return (
     <form className="component-stripe-inline-form" onSubmit={handleSubmit}>
@@ -97,10 +135,18 @@ function checkoutForm(props: {stripe: Object, callback: (token: string) => mixed
         <span className="component-stripe-inline-form__label-content">Enter credit/debit card details</span>
         <CardElement className="component-stripe-inline-form__card-element" hidePostalCode style={stripeElementsStyle} />
       </label>
+      {errorComponent}
       <button className="component-stripe-inline-form__submit-payment">Confirm card payment <SvgArrowRightStraight /></button>
     </form>);
 }
 
 const InjectedCheckoutForm = injectStripe(checkoutForm);
+
+// ----- Default props----- //
+
+StripeInlineForm.defaultProps = {
+  canOpen: () => true,
+  switchStatus: 'ON',
+};
 
 export default StripeInlineForm;
