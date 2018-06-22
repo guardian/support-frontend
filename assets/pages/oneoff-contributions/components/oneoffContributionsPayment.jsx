37728cdc57552c04445d33287a2bc93652c799cb
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2018-06-22 12:30:40 +0100

    Remove option to pay with paypal after already selecting to contribute by card.

--- assets/pages/oneoff-contributions/components/oneoffContributionsPayment.jsx
+++ assets/pages/oneoff-contributions/components/oneoffContributionsPayment.jsx
@@ -7,16 +7,13 @@ import { connect } from 'react-redux';
 import type { Dispatch } from 'redux';
 
 import StripePopUpButton from 'components/paymentButtons/stripePopUpButton/stripePopUpButton';
-import PayPalContributionButton from 'containerisableComponents/payPalContributionButton/payPalContributionButton';
 import ErrorMessage from 'components/errorMessage/errorMessage';
 
 import { validateEmailAddress } from 'helpers/utilities';
 
-import type { IsoCountry } from 'helpers/internationalisation/country';
 import type { ReferrerAcquisitionData } from 'helpers/tracking/acquisitions';
 import type { Participations } from 'helpers/abTests/abtest';
 import type { Currency } from 'helpers/internationalisation/currency';
-import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
 import type { Status } from 'helpers/switch';
 
 import { checkoutError, type Action } from '../oneoffContributionsActions';
@@ -32,15 +29,12 @@ type PropTypes = {
   isFormEmpty: boolean,
   amount: number,
   referrerAcquisitionData: ReferrerAcquisitionData,
-  isoCountry: IsoCountry,
-  countryGroupId: CountryGroupId,
   checkoutError: (?string) => void,
   abParticipations: Participations,
   currency: Currency,
   isTestUser: boolean,
   isPostDeploymentTestUser: boolean,
   stripeSwitchStatus: Status,
-  payPalSwitchStatus: Status,
 };
 
 // ----- Map State/Props ----- //
@@ -54,12 +48,9 @@ function mapStateToProps(state) {
     isFormEmpty: state.page.user.email === '' || state.page.user.fullName === '',
     amount: state.page.oneoffContrib.amount,
     referrerAcquisitionData: state.common.referrerAcquisitionData,
-    isoCountry: state.common.country,
-    countryGroupId: state.common.countryGroup,
     abParticipations: state.common.abParticipations,
     currency: state.common.currency,
     stripeSwitchStatus: state.common.switches.oneOffPaymentMethods.stripe,
-    payPalSwitchStatus: state.common.switches.oneOffPaymentMethods.payPal,
   };
 }
 
@@ -117,15 +108,6 @@ function OneoffContributionsPayment(props: PropTypes, context) {
   return (
     <section className="oneoff-contribution-payment">
       <ErrorMessage message={props.error} />
-      <PayPalContributionButton
-        amount={props.amount}
-        referrerAcquisitionData={props.referrerAcquisitionData}
-        isoCountry={props.isoCountry}
-        countryGroupId={props.countryGroupId}
-        errorHandler={props.checkoutError}
-        abParticipations={props.abParticipations}
-        switchStatus={props.payPalSwitchStatus}
-      />
       <StripePopUpButton
         email={props.email}
         callback={postCheckout(
