29e12842e5f57c271d1c0d840b1876507464ee40
Michael McNamara <michael.mcnamara@theguardian.com>
2018-07-03 13:49:51 +0100

    Add more error detail to stripe card errors (#110)
    * add more error detail to stripe card errors
    
    * tiny fix
    
    * remove request id from stripe
    
    * tidy up

--- src/main/scala/model/stripe/StripeApiError.scala
+++ src/main/scala/model/stripe/StripeApiError.scala
@@ -5,8 +5,19 @@ import io.circe.generic.JsonCodec
 
 import PartialFunction.condOpt
 
+// exceptionType: The type of exception - eg CardException, ApiException etc
+//
+// responseCode: The http status code returned by the Stripe API: eg 400, 403, 500 etc.
+// See here for more details on exception types and http status codes: https://stripe.com/docs/api/java#errors
+//
+// errorName: For some exception types, such as CardException, there is an extra field which gives a more detailed name
+// for the error - this can normally be displayed to the user.
+// See here for more information on errorNames (NB: Stripe calls these error codes):
+// https://stripe.com/docs/error-codes
+//
+// requestID: This field contains an identifying key for the request which can be used for debugging and investigating any failures
 
-@JsonCodec case class StripeApiError private (exceptionType: Option[String], responseCode: Option[Int], requestId: Option[String], message: String) extends Exception {
+@JsonCodec case class StripeApiError private (exceptionType: Option[String], responseCode: Option[Int], errorName: Option[String], message: String) extends Exception {
   override val getMessage: String = message
 }
 
@@ -22,7 +33,7 @@ object StripeApiError {
   }
 
   def fromException(err: StripeException): StripeApiError = {
-    val exceptionType: Option[String] =  {
+    val exceptionType: Option[String] = {
       condOpt(err) {
         case _: APIConnectionException => "APIConnectionException"
         case _: APIException => "APIException"
@@ -32,7 +43,9 @@ object StripeApiError {
       }
     }
 
-    StripeApiError(exceptionType, Option(err.getStatusCode), Option(err.getRequestId), err.getMessage)
-    }
+    val errorName: Option[String] = condOpt(err) { case e: CardException => e.getCode }
 
+    StripeApiError(exceptionType, Option(err.getStatusCode), errorName, err.getMessage)
+
+  }
 }
