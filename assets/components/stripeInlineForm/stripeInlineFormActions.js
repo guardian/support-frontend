68f97184aa7716b03d6658db2100fe55409dd17f
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2018-07-04 15:22:39 +0100

    [adding-spinner-to-stripe] Added the action to enable/disable stripe button

--- assets/components/stripeInlineForm/stripeInlineFormActions.js
+++ assets/components/stripeInlineForm/stripeInlineFormActions.js
@@ -5,7 +5,8 @@
 export type Action =
   | { type: 'STRIPE_IS_LOADED', scope: string }
   | { type: 'SET_ERROR', scope: string, message: string }
-  | { type: 'RESET_ERROR', scope: string };
+  | { type: 'RESET_ERROR', scope: string }
+  | { type: 'DISABLE_SUBMIT_BUTTON', scope: string };
 
 
 // ----- Action Creators ----- //
@@ -21,6 +22,12 @@ function stripeInlineFormActionsFor(scope: string): Object {
     },
     resetError(): Action {
       return { type: 'RESET_ERROR', scope };
+    },
+    disableSubmitButton(): Action {
+      return { type: 'DISABLE_SUBMIT_BUTTON', scope };
+    },
+    enableSubmitButton(): Action {
+      return { type: 'ENABLE_SUBMIT_BUTTON', scope };
     },
   };
 }
