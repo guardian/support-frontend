83876b52007caa0bade88e6ac06434edd08d8371
Emma Milner <ekmilner@gmail.com>
2018-07-06 15:09:04 +0100

    common var

--- assets/helpers/legal.js
+++ assets/helpers/legal.js
@@ -6,19 +6,22 @@ import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
 
 // ----- Terms & Conditions ----- //
 
+const defaultTermsLink: string = 'https://www.theguardian.com/info/2016/apr/04/contribution-terms-and-conditions';
+
 const termsLinks: {
   [CountryGroupId]: string,
 } = {
-  GBPCountries: 'https://www.theguardian.com/info/2016/apr/04/contribution-terms-and-conditions',
+  GBPCountries: defaultTermsLink,
   UnitedStates: 'https://www.theguardian.com/info/2016/apr/07/us-contribution-terms-and-conditions',
-  AUDCountries: 'https://www.theguardian.com/info/2016/apr/04/contribution-terms-and-conditions',
-  EURCountries: 'https://www.theguardian.com/info/2016/apr/04/contribution-terms-and-conditions',
-  International: 'https://www.theguardian.com/info/2016/apr/04/contribution-terms-and-conditions',
-  NZDCountries: 'https://www.theguardian.com/info/2016/apr/04/contribution-terms-and-conditions',
-  Canada: 'https://www.theguardian.com/info/2016/apr/04/contribution-terms-and-conditions',
+  AUDCountries: defaultTermsLink,
+  EURCountries: defaultTermsLink,
+  International: defaultTermsLink,
+  NZDCountries: defaultTermsLink,
+  Canada: defaultTermsLink,
 };
 
 const privacyLink = 'https://www.theguardian.com/help/privacy-policy';
+const defaultContributionEmail = 'mailto:contribution.support@theguardian.com';
 
 const copyrightNotice = `\u00A9 ${(new Date()).getFullYear()} Guardian News and Media Limited or its
   affiliated companies. All rights reserved.`;
@@ -26,13 +29,13 @@ const copyrightNotice = `\u00A9 ${(new Date()).getFullYear()} Guardian News and 
 const contributionsEmail: {
     [CountryGroupId]: string,
 } = {
-  GBPCountries: 'mailto:contribution.support@theguardian.com',
-  UnitedStates: 'mailto:contribution.support@theguardian.com',
   AUDCountries: 'mailto:apac.help@theguardian.com',
-  EURCountries: 'mailto:contribution.support@theguardian.com',
-  International: 'mailto:contribution.support@theguardian.com',
-  NZDCountries: 'mailto:contribution.support@theguardian.com',
-  Canada: 'mailto:contribution.support@theguardian.com',
+  GBPCountries: defaultContributionEmail,
+  UnitedStates: defaultContributionEmail,
+  EURCountries: defaultContributionEmail,
+  International: defaultContributionEmail,
+  NZDCountries: defaultContributionEmail,
+  Canada: defaultContributionEmail,
 };
 
 
