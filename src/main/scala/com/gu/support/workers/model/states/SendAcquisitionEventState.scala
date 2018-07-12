91b7b83d07d42553ef4e54022799f6f51f4cf612
rupertbates <rupert.bates@guardian.co.uk>
2018-07-12 12:12:01 +0100

    Update the SendAcquisitionEventState to expect a `ProductType` rather than a `Contribution`

--- src/main/scala/com/gu/support/workers/model/states/SendAcquisitionEventState.scala
+++ src/main/scala/com/gu/support/workers/model/states/SendAcquisitionEventState.scala
@@ -1,10 +1,10 @@
 package com.gu.support.workers.model.states
 
-import com.gu.support.workers.model.{AcquisitionData, Contribution, PaymentMethod, User}
+import com.gu.support.workers.model._
 
 case class SendAcquisitionEventState(
   user: User,
-  contribution: Contribution,
+  product: ProductType,
   paymentMethod: PaymentMethod,
   acquisitionData: Option[AcquisitionData]
 ) extends StepFunctionUserState
