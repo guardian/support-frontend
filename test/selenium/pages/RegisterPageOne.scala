71866b30c6c28f19a0e27cbfe135a83b7f634426
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-07-17 16:21:36 +0100

    update selenium tests

--- test/selenium/pages/RegisterPageOne.scala
+++ test/selenium/pages/RegisterPageOne.scala
@@ -1,14 +1,14 @@
 package selenium.pages
 
-import org.scalatest.selenium.Page
 import java.net.URLEncoder
 
 import org.openqa.selenium.WebDriver
+import org.scalatest.selenium.Page
 import selenium.util.{Browser, Config, TestUser}
 
-case class Register(testUser: TestUser, amount: Int)(implicit val webDriver: WebDriver) extends Page with Browser {
+case class RegisterPageOne(testUser: TestUser, amount: Int)(implicit val webDriver: WebDriver) extends Page with Browser {
   private val returnUrlParam = URLEncoder.encode(s"${Config.supportFrontendUrl}/contribute/recurring?contributionValue%3D${amount}", "UTF-8")
-  val url = s"${Config.identityFrontendUrl}/register?returnUrl=${returnUrlParam}&skipConfirmation=true&clientId=members"
+  val url = s"${Config.identityFrontendUrl}/signin/start?returnUrl=${returnUrlParam}&skipConfirmation=true&clientId=recurringContributions"
 
   def fillInPersonalDetails() { RegisterFields.fillIn() }
 
@@ -17,18 +17,13 @@ case class Register(testUser: TestUser, amount: Int)(implicit val webDriver: Web
   def pageHasLoaded: Boolean = pageHasElement(submitButton)
 
   private object RegisterFields {
-    val firstName = id("register_field_firstname")
-    val lastName = id("register_field_lastname")
-    val email = id("register_field_email")
-    val password = id("register_field_password")
+    val email = id("tssf-email")
 
     def fillIn() {
-      setValue(firstName, testUser.username, clear = true)
-      setValue(lastName, testUser.username, clear = true)
       setValue(email, s"${testUser.username}@gu.com", clear = true)
-      setValue(password, testUser.username, clear = true)
     }
   }
 
-  private val submitButton = id("register_submit")
+  private val submitButton = id("tssf-submit")
 }
+
--- test/selenium/pages/RegisterPageTwo.scala
+++ test/selenium/pages/RegisterPageTwo.scala
@@ -6,7 +6,7 @@ import java.net.URLEncoder
 import org.openqa.selenium.WebDriver
 import selenium.util.{Browser, Config, TestUser}
 
-case class RegisterPageOne(testUser: TestUser, amount: Int)(implicit val webDriver: WebDriver) extends Page with Browser {
+case class RegisterPageTwo(testUser: TestUser, amount: Int)(implicit val webDriver: WebDriver) extends Page with Browser {
   private val returnUrlParam = URLEncoder.encode(s"${Config.supportFrontendUrl}/contribute/recurring?contributionValue%3D${amount}", "UTF-8")
   val url = s"${Config.identityFrontendUrl}/signin/start?returnUrl=${returnUrlParam}&skipConfirmation=true&clientId=recurringContributions"
 
@@ -17,16 +17,16 @@ case class RegisterPageOne(testUser: TestUser, amount: Int)(implicit val webDriv
   def pageHasLoaded: Boolean = pageHasElement(submitButton)
 
   private object RegisterFields {
-    val email = id("register_field_email")
-    val password = id("register_field_password")
+    val firstName = id("register_field_firstname")
+    val lastName = id("register_field_lastname")
+    val password = name("password")
 
     def fillIn() {
       setValue(firstName, testUser.username, clear = true)
       setValue(lastName, testUser.username, clear = true)
-      setValue(email, s"${testUser.username}@gu.com", clear = true)
       setValue(password, testUser.username, clear = true)
     }
   }
 
-  private val submitButton = id("register_submit")
+  private val submitButton = id("tssf-create-account")
 }
