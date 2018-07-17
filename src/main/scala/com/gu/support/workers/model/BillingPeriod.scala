4dc90d5b72e937da48daf58b6876c1381ead5f44
rupertbates <rupert.bates@guardian.co.uk>
2018-07-17 17:55:16 +0100

    Refactor BillingPeriods to avoid inexhaustive match errors

--- src/main/scala/com/gu/support/workers/model/BillingPeriod.scala
+++ src/main/scala/com/gu/support/workers/model/BillingPeriod.scala
@@ -1,11 +1,11 @@
 package com.gu.support.workers.model
 
 
-sealed trait BillingPeriod {
+sealed trait BillingPeriod
+
+object BillingPeriod {
   def fromString(code: String): Option[BillingPeriod] = List(Monthly, Annual).find(_.getClass.getSimpleName == s"$code$$")
 }
-
-object BillingPeriod extends BillingPeriod
 
 case object Monthly extends BillingPeriod
 
