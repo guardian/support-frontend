f0bd52207726dae33a00726d61492abb4c61cafc
Michael McNamara <michael.mcnamara@guardian.co.uk>
2018-08-02 18:00:54 +0100

    change unsupported user agent to info level and add logging for unsupported user agent page

--- app/utils/BrowserCheck.scala
+++ app/utils/BrowserCheck.scala
@@ -5,7 +5,7 @@ import play.api.mvc.RequestHeader
 
 object BrowserCheck {
   def logUserAgent(implicit request: RequestHeader): Unit = {
-    SafeLogger.warn(s"Unsupported user agent: ${
+    SafeLogger.info(s"Unsupported user agent: ${
       request.headers.get("User-Agent").getOrElse("No User-Agent available in request.headers")
     }")
   }
