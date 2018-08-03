9c67a27da39eab098a437ab49d59a239bf811a2f
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-08-03 12:07:30 +0100

    drop cookie for recurring

---
+++ app/cookies/RecurringContributionCookie.scala
@@ -0,0 +1,19 @@
+package cookies
+
+import java.time.Instant
+
+import com.gu.support.workers.model.BillingPeriod
+import play.api.mvc.Cookie
+
+object RecurringContributionCookie {
+
+  val currentTime = Instant.ofEpochSecond(System.currentTimeMillis / 1000).toString
+
+  def create(domain: String, billingPeriod: BillingPeriod): Cookie = Cookie(
+    name = s"gu.contributions.recurring.contrib-timestamp.$billingPeriod",
+    value = currentTime,
+    secure = true,
+    httpOnly = false,
+    domain = Some(domain)
+  )
+}
