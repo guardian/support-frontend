a088a4bd1ce22a1056a1c448497f57e91a130223
James Byers <Ap0c@users.noreply.github.com>
2018-08-08 15:06:03 +0100

    Added the mobile SVG and restructured the page to accomodate.

--- assets/components/subscriptionsThankYou/subscriptionsThankYou.jsx
+++ assets/components/subscriptionsThankYou/subscriptionsThankYou.jsx
@@ -13,6 +13,7 @@ import {
 import LeftMarginSection from 'components/leftMarginSection/leftMarginSection';
 import HeadingBlock from 'components/headingBlock/headingBlock';
 import CtaLink from 'components/ctaLink/ctaLink';
+import SvgSubsThankYouHeroMobile from 'components/svgs/subsThankYouHeroMobile';
 
 
 // ----- Component ----- //
@@ -22,6 +23,7 @@ function SubscriptionsThankYou() {
   return (
     <div className="component-subscriptions-thank-you">
       <LeftMarginSection>
+        <SvgSubsThankYouHeroMobile />
         <HeadingBlock heading="Thank you, for helping to support our journalism">
           <p className="component-subscriptions-thank-you__heading-banner">
             <strong className="component-subscriptions-thank-you__congrats">
@@ -29,44 +31,44 @@ function SubscriptionsThankYou() {
             </strong>, you are now subscribed!
           </p>
         </HeadingBlock>
-        <div className="component-subscriptions-thank-you__content">
-          <h2 className="component-subscriptions-thank-you__heading">
-            Have you downloaded the app?
-          </h2>
-          <p className="component-subscriptions-thank-you__copy">
-            <strong className="component-subscriptions-thank-you__strong">
-              Your digital subscription is now live
-            </strong>, and we&#39;ve sent you an email to explain everything you need to know.
-            Log in to the app with the email you used to sign up
-          </p>
-          <div className="component-subscriptions-thank-you__ctas">
-            <CtaLink
-              text="Download on the App Store"
-              accessibilityHint="Click to download the app on the Apple App Store"
-              url={iOSAppUrl}
-            />
-            <CtaLink
-              text="Get it on Google Play"
-              accessibilityHint="Click to download the app on the Google Play store"
-              url={androidAppUrl}
-            />
-          </div>
-          <h2 className="component-subscriptions-thank-you__heading">
-            and the iPad Daily Edition..?
-          </h2>
-          <p className="component-subscriptions-thank-you__copy">
-            <strong className="component-subscriptions-thank-you__strong">
-              If you&#39;re and iPad user
-            </strong> you can now access our award winning journalism in a
-            format tailored for the iPad available to read offline, and updated daily.
-          </p>
-          <div className="component-subscriptions-thank-you__ctas">
-            <CtaLink
-              text="Download the iPad Edition"
-              accessibilityHint="Click to download the Daily Tablet Edition app on the Apple App Store"
-              url={dailyEditionUrl}
-            />
-          </div>
+      </LeftMarginSection>
+      <LeftMarginSection modifierClasses={['apps']}>
+        <h2 className="component-subscriptions-thank-you__heading">
+          Have you downloaded the app?
+        </h2>
+        <p className="component-subscriptions-thank-you__copy">
+          <strong className="component-subscriptions-thank-you__strong">
+            Your digital subscription is now live
+          </strong>, and we&#39;ve sent you an email to explain everything you need to know.
+          Log in to the app with the email you used to sign up
+        </p>
+        <div className="component-subscriptions-thank-you__ctas">
+          <CtaLink
+            text="Download on the App Store"
+            accessibilityHint="Click to download the app on the Apple App Store"
+            url={iOSAppUrl}
+          />
+          <CtaLink
+            text="Get it on Google Play"
+            accessibilityHint="Click to download the app on the Google Play store"
+            url={androidAppUrl}
+          />
+        </div>
+        <h2 className="component-subscriptions-thank-you__heading">
+          and the iPad Daily Edition..?
+        </h2>
+        <p className="component-subscriptions-thank-you__copy">
+          <strong className="component-subscriptions-thank-you__strong">
+            If you&#39;re and iPad user
+          </strong> you can now access our award winning journalism in a
+          format tailored for the iPad available to read offline, and updated daily.
+        </p>
+        <div className="component-subscriptions-thank-you__ctas">
+          <CtaLink
+            text="Download the iPad Edition"
+            accessibilityHint="Click to download the Daily Tablet Edition app on the Apple App Store"
+            url={dailyEditionUrl}
+          />
         </div>
       </LeftMarginSection>
     </div>
---
+++ assets/components/svgs/subsThankYouHeroMobile.jsx
@@ -0,0 +1,84 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+
+// ----- SVG ----- //
+
+export default function SvgSubsThankYouHeroMobile() {
+
+  return (
+    <svg
+      className="svg-subs-thank-you-hero-mobile"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 593 369"
+      aria-labelledby="svg-subs-thank-you-hero-mobile-title"
+    >
+      <title id="svg-subs-thank-you-hero-mobile-title">Thank You</title>
+      <defs>
+        <path d="M0 0h607v370H0z" id="a" />
+        <circle cx="45" cy="45" id="c" r="45" />
+        <circle cx="90" cy="90" id="d" r="90" />
+        <ellipse cx="60" cy="59.718" id="e" rx="60" ry="59.718" />
+        <circle cx="90" cy="90" id="f" r="90" />
+        <circle cx="64" cy="96" id="g" r="30" />
+        <circle cx="-254.346" cy="40.918" id="h" r="30" />
+        <circle cx="-200" cy="203" id="i" r="30" />
+      </defs>
+      <g fill="none" fillRule="evenodd">
+        <g transform="translate(-7)">
+          <mask fill="#fff" id="b">
+            <use xlinkHref="#a" />
+          </mask>
+          <use fill="#F6F6F6" xlinkHref="#a" />
+          <g mask="url(#b)">
+            <use fill="#FF7F0F" transform="scale(-1 1) rotate(23 -145.643 -489.498)" xlinkHref="#c" />
+          </g>
+          <g mask="url(#b)">
+            <g transform="translate(96 -82)">
+              <circle cx="257" cy="392" fill="#BB3B80" r="15" transform="matrix(-1 0 0 1 514 0)" />
+              <circle cx="40" cy="448" fill="#ECECEC" r="40" transform="matrix(-1 0 0 1 80 0)" />
+              <circle cx="342" cy="249" fill="#DCDCDC" r="15" transform="matrix(-1 0 0 1 684 0)" />
+              <circle cx="174" cy="110" fill="#DCDCDC" r="15" transform="matrix(-1 0 0 1 348 0)" />
+              <circle cx="360" cy="307" fill="#EACCA0" r="15" transform="matrix(-1 0 0 1 720 0)" />
+              <circle cx="84" cy="415" fill="#00B2FF" r="15" transform="matrix(-1 0 0 1 168 0)" />
+              <circle cx="73" cy="278" fill="#DCDCDC" r="15" transform="matrix(-1 0 0 1 146 0)" />
+              <circle cx="26.5" cy="195.5" fill="#FFE500" r="22.5" transform="matrix(-1 0 0 1 53 0)" />
+              <ellipse cx="119" cy="59.718" fill="#EACCA0" rx="60" ry="59.718" transform="matrix(-1 0 0 1 238 0)" />
+              <circle cx="395.5" cy="216.5" fill="#FF7F0F" r="22.5" transform="matrix(-1 0 0 1 791 0)" />
+            </g>
+          </g>
+          <g mask="url(#b)">
+            <use fill="#FFABDB" transform="scale(-1 1) rotate(27 -1.938 -1090.706)" xlinkHref="#d" />
+          </g>
+          <g mask="url(#b)">
+            <use fill="#FF4E36" transform="matrix(-1 0 0 1 204 207)" xlinkHref="#e" />
+          </g>
+          <g fill="#00B2FF" mask="url(#b)">
+            <circle cx="15" cy="15" r="15" transform="matrix(-1 0 0 1 419 79)" />
+          </g>
+          <g mask="url(#b)">
+            <use fill="#00B2FF" transform="scale(-1 1) rotate(-26 334.611 1391.993)" xlinkHref="#f" />
+          </g>
+          <g mask="url(#b)">
+            <g transform="rotate(-4 1552.558 -5795.703)">
+              <circle cx="30" cy="30" fill="#FF4E36" r="30" transform="matrix(-1 0 0 1 60 0)" />
+              <use fill="#ECECEC" transform="matrix(-1 0 0 1 128 0)" xlinkHref="#g" />
+              <use fill="#00B2FF" transform="matrix(-1 0 0 1 -508.693 0)" xlinkHref="#h" />
+              <use fill="#EACCA0" transform="matrix(-1 0 0 1 -400 0)" xlinkHref="#i" />
+            </g>
+          </g>
+        </g>
+        <g transform="translate(177 64)">
+          <circle cx="120" cy="120" fill="#FFE500" r="120" />
+          <path d="M36.44 122.733v-1.02l4.44-.9v-40.2h-3.42l-8.4 13.86h-1.02l.54-15h37.2l.48 15h-1.02l-8.16-13.86h-3.54v40.2l4.44.9v1.02H36.44zm31.5-1.02l3.42-.96v-41.04l-3.6-1.44v-1.02l13.02-1.92 1.38.18v18.06h.36c2.82-2.88 7.02-4.5 11.16-4.5 5.7 0 8.22 2.7 8.22 8.7v22.98l3.42.96v1.02h-17.4v-1.02l3.06-.78v-22.56c0-3.24-1.44-4.68-4.26-4.68-1.74 0-3.12.42-4.38 1.08v26.16l3 .78v1.02h-17.4v-1.02zm49.26-16.92l7.2-1.38v-4.38c0-6.66-1.44-8.82-5.1-8.82-.42 0-.78.06-1.2.12l-8.1 10.98h-1.02l.24-10.2c3.12-.96 7.02-2.04 12.18-2.04 8.88 0 14.04 2.46 14.04 9.9v21.36l3.18.84v.84c-1.26.78-3.78 1.5-6.54 1.5-4.38 0-6.48-1.44-7.44-3.84h-.3c-1.86 2.52-4.5 3.96-8.64 3.96-5.28 0-8.88-3.3-8.88-9 0-5.52 3.42-8.52 10.38-9.84zm4.44 14.52c1.38 0 2.16-.42 2.76-1.14v-13.2l-2.22.18c-3.48.3-4.74 2.52-4.74 7.44 0 5.34 1.74 6.72 4.2 6.72zm18.6 2.4l3.42-.96v-27.12l-3.6-1.44v-1.02l13.08-1.92 1.32.12v4.2h.36c2.82-2.88 7.02-4.5 11.16-4.5 5.7 0 8.22 2.7 8.22 8.7v22.98l3.42.96v1.02h-17.4v-1.02l3.06-.78v-22.56c0-3.24-1.44-4.68-4.26-4.68-1.74 0-3.12.42-4.38 1.08v26.16l3 .78v1.02h-17.4v-1.02zm53.34-46.2v30.3l13.32-12.24-5.1-2.7v-1.02h12.54v1.02l-5.64 2.58-7.14 6.78 12.42 20.4 2.88 1.08v1.02h-14.34l-8.94-15.72v13.68l3.24.96v1.08h-17.64v-1.02l3.42-.96v-41.1l-3.6-1.38v-1.02l13.02-1.92 1.56.18zm-105.84 94.1c-1.92 5.7-5.58 9.48-11.52 10.74l-3.54-6.48v-.9H85.4l-14.82-36.72-2.88-1.38v-1.02h18.48v1.02l-3.72 1.56 7.98 21.9 6.9-20.88-4.68-2.58v-1.02h9.54v1.02l-3.6 2.4-10.86 32.34zm32.46-1.98c-10.02 0-16.86-6.78-16.86-17.46 0-10.56 6.72-17.1 16.8-17.1 10.08 0 16.92 6.54 16.92 17.16 0 10.68-6.72 17.4-16.86 17.4zm0-33.48c-3.3 0-5.04 3.18-5.04 16.08 0 12.96 1.74 16.32 5.1 16.32 3.3 0 4.98-3.42 4.98-16.38 0-12.9-1.62-16.02-5.04-16.02zm21.72 3.48l-3.6-1.44v-1.02l13.14-1.92 1.44.12v24.84c0 3.66 1.56 4.68 4.2 4.68 1.5 0 2.82-.42 4.08-.96v-24.3l-3.6-1.44v-1.02l13.14-1.92 1.32.12v29.58l3.54 1.44v1.02l-12.96 2.04-1.32-.12v-4.2h-.36c-2.46 2.46-5.76 4.44-9.84 4.44-6.3 0-9.18-3.72-9.18-9.36v-20.58z" fill="#121212" />
+        </g>
+        <circle cx="237.5" cy="55.5" fill="#ECECEC" r="22.5" transform="matrix(-1 0 0 1 475 0)" />
+      </g>
+    </svg>
+
+  );
+
+}
