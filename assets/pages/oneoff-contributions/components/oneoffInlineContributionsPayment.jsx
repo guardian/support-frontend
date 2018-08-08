d6b99e04680e19bae3e0bfee6d26b95bdcb09680
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-08-08 13:52:18 +0100

    fix validation error

--- assets/pages/oneoff-contributions/components/oneoffInlineContributionsPayment.jsx
+++ assets/pages/oneoff-contributions/components/oneoffInlineContributionsPayment.jsx
@@ -28,7 +28,7 @@ type PropTypes = {|
   dispatch: Dispatch<*>,
   email: string,
   error: ?string,
-  isFormEmpty: boolean,
+  areAnyRequiredFieldsEmpty: boolean,
   amount: number,
   referrerAcquisitionData: ReferrerAcquisitionData,
   checkoutError: (?string) => void,
@@ -52,7 +52,7 @@ function mapStateToProps(state) {
     isPostDeploymentTestUser: state.page.user.isPostDeploymentTestUser,
     email: state.page.user.email,
     error: state.page.oneoffContrib.error,
-    isFormEmpty: state.page.user.email === '' || state.page.user.fullName === '',
+    areAnyRequiredFieldsEmpty: !state.page.user.email || !state.page.user.fullName,
     amount: state.page.oneoffContrib.amount,
     referrerAcquisitionData: state.common.referrerAcquisitionData,
     abParticipations: state.common.abParticipations,
@@ -85,14 +85,14 @@ function mapDispatchToProps(dispatch: Dispatch<OneOffCheckoutAction | StripeInli
 
 // If the form is valid, calls the given callback, otherwise sets an error.
 function formValidation(
-  isFormEmpty: boolean,
+  areAnyRequiredFieldsEmpty: boolean,
   validEmail: boolean,
   error: ?string => void,
 ): () => boolean {
 
   return (): boolean => {
 
-    if (!isFormEmpty && validEmail) {
+    if (!areAnyRequiredFieldsEmpty && validEmail) {
       if (error) {
         error(null);
       }
@@ -100,7 +100,7 @@ function formValidation(
     }
 
     if (error) {
-      if (isFormEmpty) {
+      if (areAnyRequiredFieldsEmpty) {
         error('Please fill in all the fields above.');
       } else {
         error('Please fill in a valid email address.');
@@ -136,7 +136,7 @@ function OneoffContributionsPayment(props: PropTypes, context) {
           context.store.getState,
         )}
         canProceed={formValidation(
-          props.isFormEmpty,
+          props.areAnyRequiredFieldsEmpty,
           validateEmailAddress(props.email),
           props.checkoutError,
         )}
