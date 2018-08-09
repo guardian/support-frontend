475bde91539254d055453054c4ada5a835e9cd53
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-08-09 12:00:40 +0100

    Fix test

--- test/controllers/RegularContributionsTest.scala
+++ test/controllers/RegularContributionsTest.scala
@@ -111,7 +111,7 @@ class RegularContributionsTest extends WordSpec with MustMatchers with TestCSRFC
         stripeConfigProvider,
         payPalConfigProvider,
         stubControllerComponents(),
-        Switches(PaymentMethodsSwitch(On, On, None), PaymentMethodsSwitch(On, On, Some(On)), On),
+        Switches(PaymentMethodsSwitch(On, On, None), PaymentMethodsSwitch(On, On, Some(On)), Map.empty, On),
         guardianDomain = ".thegulocal.com"
       )
     }
