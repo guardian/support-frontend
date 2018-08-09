33e9378bc3b1c790b75682d0a916fe17fb5df3de
Joseph Smith <joseph.lochlann@gmail.com>
2018-08-09 16:37:31 +0100

    Revert "Add server-side AB test infrastructure"

--- app/switchboard/Group.scala
+++
@@ -1,8 +0,0 @@
-package switchboard
-
-sealed abstract class Group(val name: String)
-object Group {
-  case object Variant extends Group("variant")
-  case object Control extends Group("control")
-  case object Unknown extends Group("unknown")
-}
--- app/switchboard/Switches.scala
+++ app/switchboard/Switches.scala
@@ -1,13 +1,10 @@
 package switchboard
 
 import com.typesafe.config.Config
-import collection.JavaConverters._
-import play.api.mvc.RequestHeader
 
 case class Switches(
     oneOffPaymentMethods: PaymentMethodsSwitch,
     recurringPaymentMethods: PaymentMethodsSwitch,
-    experiments: Map[String, ExperimentSwitch],
     optimize: SwitchState,
     internationalSubscribePages: SwitchState
 )
@@ -17,34 +14,13 @@ object Switches {
     Switches(
       PaymentMethodsSwitch.fromConfig(config.getConfig("oneOff")),
       PaymentMethodsSwitch.fromConfig(config.getConfig("recurring")),
-      experimentsFromConfig(config, "abtests"),
       SwitchState.fromConfig(config, "optimize"),
       SwitchState.fromConfig(config, "internationalSubscribePages")
     )
 
-  def experimentsFromConfig(config: Config, rootKey: String): Map[String, ExperimentSwitch] =
-    config.getConfigList(rootKey).asScala.map(ExperimentSwitch.fromConfig).map { x => x.name -> x }.toMap
 }
 
 case class PaymentMethodsSwitch(stripe: SwitchState, payPal: SwitchState, directDebit: Option[SwitchState])
-case class ExperimentSwitch(name: String, description: String, segment: Segment, state: SwitchState) {
-  def isOn: Boolean = state == SwitchState.On
-
-  private def checkHeader(headerName: String, predicate: String => Boolean)(implicit request: RequestHeader): Boolean =
-    request.headers.get(headerName).exists(predicate)
-
-  private def inVariant(implicit request: RequestHeader): Boolean = checkHeader(segment.headerName, _ == Group.Variant.name)
-  private def inControl(implicit request: RequestHeader): Boolean = checkHeader(segment.headerName, _ == Group.Control.name)
-
-  def canRun(implicit request: RequestHeader): Boolean = isOn
-  def isParticipating(implicit request: RequestHeader): Boolean = canRun && inVariant
-  def isControl(implicit request: RequestHeader): Boolean = canRun && inControl
-  def value(implicit request: RequestHeader): Group = (isParticipating, isControl) match {
-    case (true, _) => Group.Variant
-    case (_, true) => Group.Control
-    case _ => Group.Unknown
-  }
-}
 
 object PaymentMethodsSwitch {
   def fromConfig(config: Config): PaymentMethodsSwitch =
@@ -58,28 +34,16 @@ object PaymentMethodsSwitch {
     )
 }
 
-object ExperimentSwitch {
-  def fromConfig(config: Config): ExperimentSwitch =
-    ExperimentSwitch(
-      config.getString("name"),
-      config.getString("description"),
-      Segment.fromConfig(config, "segment"),
-      SwitchState.fromConfig(config, "state")
-    )
-}
-
-sealed trait SwitchState {
-  def isOn: Boolean
-}
+sealed trait SwitchState
 
 object SwitchState {
   def fromConfig(config: Config, path: String): SwitchState = fromString(config.getString(path))
 
   def fromString(s: String): SwitchState = if (s.toLowerCase == "on") On else Off
 
-  case object On extends SwitchState { val isOn = true }
+  case object On extends SwitchState
 
-  case object Off extends SwitchState { val isOn = false }
+  case object Off extends SwitchState
 
 }
 
