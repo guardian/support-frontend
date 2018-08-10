e278a4db632db8eaedf744e1f45c15ab19e946a3
Jacob Winch <jacob.winch@guardian.co.uk>
2018-08-10 14:19:54 +0100

    Add strings to reasons

--- src/main/scala/com/gu/support/workers/model/CheckoutFailureReasons.scala
+++ src/main/scala/com/gu/support/workers/model/CheckoutFailureReasons.scala
@@ -2,15 +2,32 @@ package com.gu.support.workers.model
 
 object CheckoutFailureReasons {
 
-  val all = List(PaymentMethodUnacceptable, PaymentMethodDetailsIncorrect, PaymentAlreadyTaken, Unknown)
+  val all = List(PaymentMethodUnacceptable, PaymentMethodDetailsIncorrect, PaymentRecentlyTaken, Unknown)
   def fromString(string: String): Option[CheckoutFailureReason] = all.find(_.toString == string)
 
-  sealed trait CheckoutFailureReason
-  case object PaymentMethodUnacceptable extends CheckoutFailureReason
-  case object PaymentMethodDetailsIncorrect extends CheckoutFailureReason
-  case object PaymentAlreadyTaken extends CheckoutFailureReason
-  case object PaymentProviderUnavailable extends CheckoutFailureReason
-  case object Unknown extends CheckoutFailureReason
+  sealed trait CheckoutFailureReason {
+    def asString: String
+  }
+
+  case object PaymentMethodUnacceptable extends CheckoutFailureReason {
+    override def asString: String = "payment_method_unacceptable"
+  }
+
+  case object PaymentMethodDetailsIncorrect extends CheckoutFailureReason {
+    override def asString: String = "payment_details_incorrect"
+  }
+
+  case object PaymentRecentlyTaken extends CheckoutFailureReason {
+    override def asString: String = "payment_recently_taken"
+  }
+
+  case object PaymentProviderUnavailable extends CheckoutFailureReason {
+    override def asString: String = "payment_provider_unavailable"
+  }
+
+  case object Unknown extends CheckoutFailureReason {
+    override def asString: String = "unknown"
+  }
 
 }
 
