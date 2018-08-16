f5d910035d807072d53266d758b800b77ffb23c8
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-08-16 10:17:59 +0100

    Let's just do that for now

--- assets/pages/contributions-landing/pagesVersions/newPaymentPage.jsx
+++ assets/pages/contributions-landing/pagesVersions/newPaymentPage.jsx
@@ -6,8 +6,8 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import type { Store } from 'redux';
 
-import { type CountryGroupId, countryGroups } from 'helpers/internationalisation/countryGroup';
-import { getOrigin } from 'helpers/url';
+// import { type CountryGroupId, countryGroups } from 'helpers/internationalisation/countryGroup';
+// import { getOrigin } from 'helpers/url';
 import Page from 'components/page/page';
 
 // React components connected to redux store
@@ -19,16 +19,19 @@ import Page from 'components/page/page';
 
 type PropTypes = {
   store: Store<*, *, *>,
-  countryGroupId: CountryGroupId,
+  countryGroupId: CountryGroupId
 };
 
 // ----- Internationalisation ----- //
 
-const defaultHeaderCopy = ['Help us deliver the', 'independent journalism', 'the world needs'];
-const defaultContributeCopy = 'Make a monthly commitment to support The Guardian long term or a one-off contribution as and when you feel like it – choose the option that suits you best.';
+const defaultHeaderCopy = 'Help us deliver the independent journalism the world needs';
+const defaultContributeCopy = `
+  Make a monthly commitment to support The Guardian long term or a one-off contribution 
+  as and when you feel like it – choose the option that suits you best.
+`;
 
 const countryGroupSpecificDetails: {
-  [CountryGroupId]: {headerCopy: string[], contributeCopy: string}
+  [CountryGroupId]: {headerCopy: string, contributeCopy: string}
 } = {
   GBPCountries: {
     headerCopy: defaultHeaderCopy,
@@ -43,7 +46,7 @@ const countryGroupSpecificDetails: {
     contributeCopy: defaultContributeCopy,
   },
   AUDCountries: {
-    headerCopy: ['Help us deliver', 'the independent', 'journalism', 'Australia needs'],
+    headerCopy: 'Help us deliver the independent journalism Australia needs',
     contributeCopy: defaultContributeCopy,
   },
   International: {
@@ -60,18 +63,19 @@ const countryGroupSpecificDetails: {
   },
 };
 
-function payPalCancelUrl(cgId: CountryGroupId): string {
-  return `${getOrigin()}/${countryGroups[cgId].supportInternationalisationId}/contribute`;
-}
+// function payPalCancelUrl(cgId: CountryGroupId): string {
+//   return `${getOrigin()}/${countryGroups[cgId].supportInternationalisationId}/contribute`;
+// }
 
 
 // ----- Render ----- //
 
-const NewPaymentPage: (PropTypes) => React.Node = (props: PropTypes) => 
+const NewPaymentPage: (PropTypes) => React.Node = (props: PropTypes) =>
   (
     <Provider store={props.store}>
       <Page>
-        <h1>Hello!</h1>
+        <h1>{countryGroupSpecificDetails[props.countryGroupId].headerCopy}</h1>
+        <p className="blurb">{countryGroupSpecificDetails[props.countryGroupId].contributeCopy}</p>
       </Page>
     </Provider>
   );
