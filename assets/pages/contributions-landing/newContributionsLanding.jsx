67fc62d93c63d3db86b8b857278a3238d8a75318
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-08-16 09:54:19 +0100

    Bootstrap React infrastructure

---
+++ assets/pages/contributions-landing/newContributionsLanding.jsx
@@ -0,0 +1,43 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+
+import { init as pageInit } from 'helpers/page/page';
+import { renderPage } from 'helpers/render';
+import { detect } from 'helpers/internationalisation/countryGroup';
+
+import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
+
+
+import NewPaymentPage from './pagesVersions/newPaymentPage';
+
+import { createPageReducerFor } from './contributionsLandingReducer';
+
+
+// ----- Redux Store ----- //
+
+const countryGroupId: CountryGroupId = detect();
+
+const store = pageInit(createPageReducerFor(countryGroupId));
+
+
+const reactElementId: {
+  [CountryGroupId]: string
+} = {
+  GBPCountries: 'contributions-landing-page-uk',
+  EURCountries: 'contributions-landing-page-eu',
+  UnitedStates: 'contributions-landing-page-us',
+  AUDCountries: 'contributions-landing-page-au',
+  International: 'contributions-landing-page-int',
+  NZDCountries: 'contributions-landing-page-nz',
+  Canada: 'contributions-landing-page-ca',
+};
+
+
+// ----- Render ----- //
+
+const content = <NewPaymentPage store={store} countryGroupId={countryGroupId} />;
+
+renderPage(content, reactElementId[countryGroupId]);
