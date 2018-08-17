7811950624915f53acd424f3ddd198f46d150222
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-08-17 13:26:01 +0100

    Remove navigation

--- assets/pages/contributions-landing/newContributionsLanding.jsx
+++ assets/pages/contributions-landing/newContributionsLanding.jsx
@@ -114,17 +114,8 @@ const content = (
         <footer role="contentinfo" className="gu-content__footer">
           <div className="license">
             <small className="license__txt">© 2018 Guardian News and Media Limited or its affiliated companies. All rights reserved.</small>
+            <p>The ultimate owner of the Guardian is The Scott Trust Limited, whose role it is to secure the editorial and financial independence of the Guardian in perpetuity. Reader contributions support the Guardian’s journalism. Please note that your support of the Guardian’s journalism does not constitute a charitable donation, as such your contribution is not eligible for Gift Aid in the UK nor a tax-deduction elsewhere. If you have any questions about contributing to the Guardian, please <a href="mailto:contribution.support@theguardian.com">contact us here</a>.</p>
           </div>
-          <nav className="support">
-            <ul className="support__menu">
-              <li className="support__item"><a className="support__link" href="#">Help</a></li>
-              <li className="support__item"><a className="support__link" href="#">Terms &amp; Conditions</a></li>
-              <li className="support__item"><a className="support__link" href="mailto:contribution.support@theguardian.com">Contact us</a></li>
-              <li className="support__item"><a className="support__link" href="#">Privacy Policy</a></li>
-              <li className="support__item"><a className="support__link" href="#">Report technical issue</a></li>
-              <li className="support__item"><a className="support__link" href="#">Cookie Policy</a></li>
-            </ul>
-          </nav>
         </footer>
       }
     >
