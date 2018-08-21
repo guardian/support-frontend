38a69ccbeaf6ea346aa998a5a617acc70ec92bd0
rupertbates <rupert.bates@guardian.co.uk>
2018-08-21 11:29:13 +0100

    Try 2 containers

--- assets/helpers/tracking/optimize.js
+++ assets/helpers/tracking/optimize.js
@@ -11,7 +11,7 @@ try {
       h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
       (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
     })(window,document.documentElement,'async-hide','dataLayer',4000,
-      {'GTM-KGKKPS4':true});
+      {'GTM-KGKKPS4':true, 'GTM-NZGXNBL':true});
 
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -19,7 +19,7 @@ try {
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
     ga('create', 'UA-51507017-5', 'auto', {cookieDomain: 'auto', anonymizeIp: true});
-    //ga('require', 'GTM-NZGXNBL');
+    ga('require', 'GTM-NZGXNBL');
     ga('require', 'GTM-KGKKPS4');
   //};
 } catch (e) {
