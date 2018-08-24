fbee18931f1633a38a5e8b61b57911bd14737147
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-08-24 12:22:33 +0100

    Use temporary state

--- assets/pages/contributions-landing/new_components/ContributionType.jsx
+++ assets/pages/contributions-landing/new_components/ContributionType.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { connect } from 'react-redux';
 
 function ContributionType(props) {
@@ -6,19 +7,26 @@ function ContributionType(props) {
       <legend className="form__legend form__legend--radio-group">Recurrence</legend>
       <ul className="form__radio-group__list">
         <li className="form__radio-group__item">
+          <input id="contributionType-oneoff" className="form__radio-group__input" type="radio" name="contributionType" value="ONE_OFF" checked={props.contributionType === 'ONE_OFF'} />
+          <label htmlFor="contributionType-oneoff" className="form__radio-group__label">{props.labels.ONE_OFF}</label>
+        </li>
+        <li className="form__radio-group__item">
           <input id="contributionType-monthly" className="form__radio-group__input" type="radio" name="contributionType" value="MONTHLY" checked={props.contributionType === 'MONTHLY'} />
           <label htmlFor="contributionType-monthly" className="form__radio-group__label">{props.labels.MONTHLY}</label>
         </li>
         <li className="form__radio-group__item">
-          <input id="contributionType-oneoff" className="form__radio-group__input" type="radio" name="contributionType" value="ONE_OFF" checked={props.contributionType === 'ONE_OFF'} />
-          <label htmlFor="contributionType-oneoff" className="form__radio-group__label">{props.labels.ONE_OFF}</label>
+          <input id="contributionType-annual" className="form__radio-group__input" type="radio" name="contributionType" value="ANNUAL" checked={props.contributionType === 'ANNUAL'} />
+          <label htmlFor="contributionType-annual" className="form__radio-group__label">{props.labels.ANNUAL}</label>
         </li>
       </ul>
     </fieldset>
   );
 };
 
-const s2p = state => { contributionType: state.newPaymentUI.contributionType };
+const s2p = state => ({ 
+  contributionType: state.common.newPaymentUI.contributionType, 
+  labels: state.common.newPaymentUI.labels 
+});
 
 const NewContributionType = connect(s2p)(ContributionType);
 
