6dc8ced4b54907a07eb84bcf017202dca885e3bd
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-08-29 10:17:36 +0100

    Add user icon

--- assets/pages/contributions-landing/newContributionsLanding.jsx
+++ assets/pages/contributions-landing/newContributionsLanding.jsx
@@ -25,6 +25,7 @@ import SvgCheckmark from 'components/svgs/checkmark';
 import SvgChevron from 'components/svgs/chevron';
 import SvgEnveloppe from 'components/svgs/enveloppe';
 import SvgGlobe from 'components/svgs/globe';
+import SvgUser from 'components/svgs/user';
 
 // ----- Redux Store ----- //
 
@@ -92,7 +93,7 @@ const content = (
           <span className="form__input-with-icon">
             <input id="contributionFirstName" className="form__input" type="text" autoCapitalize="words" required />
             <span className="form__icon">
-              <svg width="14" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M6.99 7.381c1.433 0 3.121-1.66 3.121-3.683C10.111 1.674 8.953.5 6.991.5 5.026.5 3.888 1.674 3.888 3.698c0 2.023 1.825 3.683 3.101 3.683zM2.333 9.789l-.797.83L0 16.69l.758.81h12.445l.797-.81-1.556-6.071-.777-.83C10.11 9.283 8.692 9 7 9c-1.711 0-3.111.243-4.667.79z" fillRule="evenodd" /></svg>
+              <SvgUser />
             </span>
           </span>
         </div>
@@ -101,7 +102,7 @@ const content = (
           <span className="form__input-with-icon">
             <input id="contributionLastName" className="form__input" aria-describedby="error-contributionLastName" autoCapitalize="words" type="text" required />
             <span className="form__icon">
-              <svg width="14" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M6.99 7.381c1.433 0 3.121-1.66 3.121-3.683C10.111 1.674 8.953.5 6.991.5 5.026.5 3.888 1.674 3.888 3.698c0 2.023 1.825 3.683 3.101 3.683zM2.333 9.789l-.797.83L0 16.69l.758.81h12.445l.797-.81-1.556-6.071-.777-.83C10.11 9.283 8.692 9 7 9c-1.711 0-3.111.243-4.667.79z" /></svg>
+              <SvgUser />
             </span>
           </span>
         </div>
