b65b91166aa4885f75e7e3109557adf511d38f39
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-09-06 20:53:26 +0100

    Rewrite Stripe logic

--- assets/helpers/paymentIntegrations/stripeCheckout.js
+++ assets/helpers/paymentIntegrations/stripeCheckout.js
@@ -32,6 +32,8 @@ import { type OptimizeExperiments } from 'helpers/tracking/optimize';
 import * as cookie from 'helpers/cookie';
 import { logException } from 'helpers/logger';
 
+import { createPaymentCallback, type PaymentCallback, type PaymentResult } from './paymentApi';
+
 // ----- Setup ----- //
 
 let stripeHandler = null;
@@ -58,163 +60,97 @@ type CheckoutData = {|
 
 // ----- Functions ----- //
 
-function requestData(
-  abParticipations: Participations,
-  paymentToken: string,
-  currency: IsoCurrency,
-  referrerAcquisitionData: ReferrerAcquisitionData,
-  optimizeExperiments: OptimizeExperiments,
-  getData: () => CheckoutData,
-) {
-  const { user, amount } = getData();
+// function requestData(
+//   abParticipations: Participations,
+//   paymentToken: string,
+//   currency: IsoCurrency,
+//   referrerAcquisitionData: ReferrerAcquisitionData,
+//   optimizeExperiments: OptimizeExperiments,
+//   getData: () => CheckoutData,
+// ) {
+//   const { user, amount } = getData();
 
-  const oneOffContribFields: PaymentApiStripeExecutePaymentBody = {
-    paymentData: {
-      currency,
-      amount,
-      token: paymentToken,
-      email: user.email,
-    },
-    acquisitionData: derivePaymentApiAcquisitionData(referrerAcquisitionData, abParticipations, optimizeExperiments),
-  };
+//   const oneOffContribFields: PaymentApiStripeExecutePaymentBody = {
+//     paymentData: {
+//       currency,
+//       amount,
+//       token: paymentToken,
+//       email: user.email,
+//     },
+//     acquisitionData: derivePaymentApiAcquisitionData(referrerAcquisitionData, abParticipations, optimizeExperiments),
+//   };
 
-  return {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(oneOffContribFields),
-    credentials: 'include',
-  };
-}
-
-function postToEndpoint(request: Object, onSuccess: Function, onError: Function): Promise<*> {
-  return Promise.resolve();
-  // fetch(stripeOneOffContributionEndpoint(cookie.get('_test_username')), request)
-  //   .then((response) => {
-  //     if (response.ok) {
-  //       onSuccess();
-  //       return Promise.resolve();
-  //     }
-  //     return response.json().then((responseJson) => {
-  //       if (responseJson.error.exceptionType === 'CardException') {
-  //         onError('Your card has been declined.');
-  //       } else {
-  //         const errorHttpCode = responseJson.error.errorCode || 'unknown';
-  //         const exceptionType = responseJson.error.exceptionType || 'unknown';
-  //         const errorName = responseJson.error.errorName || 'unknown';
-  //         logException(`Stripe payment attempt failed with following error: code: ${errorHttpCode} type: ${exceptionType} error-name: ${errorName}.`);
-  //         onError('There was an error processing your payment. Please\u00a0try\u00a0again\u00a0later.');
-  //       }
-  //     });
-
-  //   }).catch(() => {
-  //     logException('Stripe payment attempt failed with unexpected error while attempting to process payment response');
-  //     onError('There was an error processing your payment. Please\u00a0try\u00a0again\u00a0later.');
-  //   });
-}
-
-function createTokenCallback({
-  abParticipations, currencyId, referrerAcquisitionData,
-  optimizeExperiments, getData, onSuccess, onError,
-}: {
-  abParticipations: Participations,
-  currencyId: IsoCurrency,
-  referrerAcquisitionData: ReferrerAcquisitionData,
-  optimizeExperiments: OptimizeExperiments,
-  getData: () => CheckoutData,
-  onSuccess: () => void,
-  onError: string => void,
-}): (string) => Promise<*> {
-  return (paymentToken: string) => {
-    const request = requestData(
-      abParticipations,
-      paymentToken,
-      currencyId,
-      referrerAcquisitionData,
-      optimizeExperiments,
-      getData,
-    );
-
-    return postToEndpoint(request, onSuccess, onError);
-  };
-}
-
-const loadStripe = () => new Promise((resolve) => {
-
+function loadStripe(): Promise<void> {
   if (!window.StripeCheckout) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
 
-    const script = document.createElement('script');
+      script.onload = resolve;
+      script.onerror = reject;
+      script.src = 'https://checkout.stripe.com/checkout.js';
 
-    script.onload = resolve;
-    script.src = 'https://checkout.stripe.com/checkout.js';
-
-    if (document.head) {
-      document.head.appendChild(script);
-    }
-
+      if (document.head) {
+        document.head.appendChild(script);
+      }
+    });
   } else {
-    resolve();
+    return Promise.resolve();
   }
+}
 
-});
-
-const getStripeKey = (currency: string, isTestUser: boolean) => {
-
-  let stripeKey = null;
-
+function getStripeKey(currency: IsoCurrency, isTestUser: boolean): string {
   switch (currency) {
     case 'AUD':
-      stripeKey = isTestUser ?
+      return isTestUser ?
         window.guardian.stripeKeyAustralia.uat : window.guardian.stripeKeyAustralia.default;
-      break;
     default:
-      stripeKey = isTestUser ?
+      return isTestUser ?
         window.guardian.stripeKeyDefaultCurrencies.uat :
         window.guardian.stripeKeyDefaultCurrencies.default;
-      break;
   }
+}
 
-  return stripeKey;
-};
-
-const setupStripeCheckout = (
-  callback: (token: string) => Promise<*>,
-  closeHandler: ?() => void,
-  currency: string,
+function setupStripeCheckout(
+  callback: PaymentCallback,
+  currency: IsoCurrency,
   isTestUser: boolean,
-): Promise<void> => loadStripe().then(() => {
+): Promise<[Object, Promise<PaymentResult>]> {
+  return loadStripe().then(() => new Promise((resolve1) => {
+    const innerPromise = new Promise((resolve2, reject2) => {
+      const handleToken = (token) => {
+        callback({ tag: 'Stripe', token: token.id }).then(resolve2, reject2);
+      };
 
-  const handleToken = (token) => {
-    callback(token.id);
-  };
-  const defaultCloseHandler: () => void = () => {};
+      const stripeKey = getStripeKey(currency, isTestUser);
 
-  const stripeKey = getStripeKey(currency, isTestUser);
+      resolve1([
+        window.StripeCheckout.configure({
+          name: 'Guardian',
+          description: 'Please enter your card details.',
+          allowRememberMe: false,
+          key: stripeKey,
+          image: 'https://uploads.guim.co.uk/2018/01/15/gu.png',
+          locale: 'auto',
+          currency,
+          token: handleToken,
+        }),
+        innerPromise
+      ]);
+    });
+  }));
+}
 
-  stripeHandler = window.StripeCheckout.configure({
-    name: 'Guardian',
-    description: 'Please enter your card details.',
-    allowRememberMe: false,
-    key: stripeKey,
-    image: 'https://uploads.guim.co.uk/2018/01/15/gu.png',
-    locale: 'auto',
-    currency,
-    token: handleToken,
-    closed: closeHandler || defaultCloseHandler,
-  });
-});
-
-const openDialogBox = (amount: number, email: string) => {
-  if (stripeHandler) {
+function openDialogBox(stripeHandler: Object) {
+  return (amount: number, email: string) => {
     stripeHandler.open({
       // Must be passed in pence.
       amount: amount * 100,
       email,
     });
   }
-};
+}
 
 export {
-  createTokenCallback,
   setupStripeCheckout,
   openDialogBox,
   getStripeKey,
