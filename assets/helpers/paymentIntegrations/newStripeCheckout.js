faeab250c86944d47070ab7a214f04ff04fe16c9
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-09-07 15:06:08 +0100

    Restore previous stripeCheckout and fork new version

---
+++ assets/helpers/paymentIntegrations/newStripeCheckout.js
@@ -0,0 +1,115 @@
+// @flow
+
+/**
+ * This module contains a copy of pages/oneoff-contributions/helpers/ajax so that
+ * the new payment flow can use this code without any impact on the existing
+ * infrastructure.
+ *
+ * There are a couple of differences though:
+ * - the amount: instead of fixing it when the callback is created, it should
+ *   be provided by the `getState` function
+ * - the actions: what happens upon error/success is view-dependent, so the
+ *   actions are provided as parameters too. As a result, `dispatch` can
+ *   go away (which makes the module completely independant of React)
+ * - tracking conversions: this should be decoupled from the payment itself and
+ *   moved upstream, in the success handler
+ *
+ * The latter module can be removed entirely once the new payment flow becomes the one
+ * and only contribution endpoint.
+ */
+
+import {
+  derivePaymentApiAcquisitionData,
+  type ReferrerAcquisitionData,
+  type PaymentAPIAcquisitionData,
+} from 'helpers/tracking/acquisitions';
+import { type Participations } from 'helpers/abTests/abtest';
+import { type IsoCurrency } from 'helpers/internationalisation/currency';
+import { type OptimizeExperiments } from 'helpers/tracking/optimize';
+import { type PaymentCallback, type PaymentResult } from './paymentApi';
+
+// ----- Functions ----- //
+
+function loadStripe(): Promise<void> {
+  if (!window.StripeCheckout) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+
+      script.onload = resolve;
+      script.onerror = reject;
+      script.src = 'https://checkout.stripe.com/checkout.js';
+
+      if (document.head) {
+        document.head.appendChild(script);
+      }
+    });
+  } else {
+    return Promise.resolve();
+  }
+}
+
+function getStripeKey(currency: IsoCurrency, isTestUser: boolean): string {
+  switch (currency) {
+    case 'AUD':
+      return isTestUser ?
+        window.guardian.stripeKeyAustralia.uat : window.guardian.stripeKeyAustralia.default;
+    default:
+      return isTestUser ?
+        window.guardian.stripeKeyDefaultCurrencies.uat :
+        window.guardian.stripeKeyDefaultCurrencies.default;
+  }
+}
+
+function setupStripeCheckout(
+  callback: PaymentCallback,
+  currency: IsoCurrency,
+  isTestUser: boolean,
+): Promise<[Object, Promise<PaymentResult>]> {
+  return loadStripe().then(() => new Promise((resolve1) => {
+    const deferred = {
+      promise: (null: any),
+      resolve: (null: any),
+      reject: (null: any),
+    };
+
+    deferred.promise = new Promise((resolve2, reject2) => {
+      deferred.resolve = resolve2;
+      deferred.reject = reject2;
+    });
+
+    const handleToken = (token) => {
+      console.log('urioew')
+      callback({ tag: 'Stripe', token: token.id }).then(deferred.resolve, deferred.reject);
+    };
+
+    const stripeKey = getStripeKey(currency, isTestUser);
+
+    resolve1([
+      window.StripeCheckout.configure({
+        name: 'Guardian',
+        description: 'Please enter your card details.',
+        allowRememberMe: false,
+        key: stripeKey,
+        image: 'https://uploads.guim.co.uk/2018/01/15/gu.png',
+        locale: 'auto',
+        currency,
+        token: handleToken,
+      }),
+      deferred.promise
+    ]);
+  }));
+}
+
+function openDialogBox(stripeHandler: Object, amount: number, email: string) {
+  stripeHandler.open({
+    // Must be passed in pence.
+    amount: amount * 100,
+    email,
+  });
+}
+
+export {
+  setupStripeCheckout,
+  openDialogBox,
+  getStripeKey,
+};
--- assets/helpers/paymentIntegrations/stripeCheckout.js
+++ assets/helpers/paymentIntegrations/stripeCheckout.js
@@ -1,107 +1,89 @@
 // @flow
 
-/**
- * This module contains a copy of pages/oneoff-contributions/helpers/ajax so that
- * the new payment flow can use this code without any impact on the existing
- * infrastructure.
- *
- * There are a couple of differences though:
- * - the amount: instead of fixing it when the callback is created, it should
- *   be provided by the `getState` function
- * - the actions: what happens upon error/success is view-dependent, so the
- *   actions are provided as parameters too. As a result, `dispatch` can
- *   go away (which makes the module completely independant of React)
- * - tracking conversions: this should be decoupled from the payment itself and
- *   moved upstream, in the success handler
- *
- * The latter module can be removed entirely once the new payment flow becomes the one
- * and only contribution endpoint.
- */
 
-import {
-  derivePaymentApiAcquisitionData,
-  type ReferrerAcquisitionData,
-  type PaymentAPIAcquisitionData,
-} from 'helpers/tracking/acquisitions';
-import { type Participations } from 'helpers/abTests/abtest';
-import { type IsoCurrency } from 'helpers/internationalisation/currency';
-import { type OptimizeExperiments } from 'helpers/tracking/optimize';
-import { type PaymentCallback, type PaymentResult } from './paymentApi';
+// ----- Setup ----- //
+
+let stripeHandler = null;
+
 
 // ----- Functions ----- //
 
-function loadStripe(): Promise<void> {
+const loadStripe = () => new Promise((resolve) => {
+
   if (!window.StripeCheckout) {
-    return new Promise((resolve, reject) => {
-      const script = document.createElement('script');
 
-      script.onload = resolve;
-      script.onerror = reject;
-      script.src = 'https://checkout.stripe.com/checkout.js';
+    const script = document.createElement('script');
 
-      if (document.head) {
-        document.head.appendChild(script);
-      }
-    });
+    script.onload = resolve;
+    script.src = 'https://checkout.stripe.com/checkout.js';
+
+    if (document.head) {
+      document.head.appendChild(script);
+    }
+
   } else {
-    return Promise.resolve();
+    resolve();
   }
-}
 
-function getStripeKey(currency: IsoCurrency, isTestUser: boolean): string {
+});
+
+const getStripeKey = (currency: string, isTestUser: boolean) => {
+
+  let stripeKey = null;
+
   switch (currency) {
     case 'AUD':
-      return isTestUser ?
+      stripeKey = isTestUser ?
         window.guardian.stripeKeyAustralia.uat : window.guardian.stripeKeyAustralia.default;
+      break;
     default:
-      return isTestUser ?
+      stripeKey = isTestUser ?
         window.guardian.stripeKeyDefaultCurrencies.uat :
         window.guardian.stripeKeyDefaultCurrencies.default;
+      break;
   }
-}
 
-function setupStripeCheckout(
-  callback: PaymentCallback,
-  currency: IsoCurrency,
+  return stripeKey;
+};
+
+export const setupStripeCheckout = (
+  callback: (token: string) => Promise<*>,
+  closeHandler: ?() => void,
+  currency: string,
   isTestUser: boolean,
-): Promise<[Object, Promise<PaymentResult>]> {
-  return loadStripe().then(() => new Promise((resolve1) => {
-    const innerPromise = new Promise((resolve2, reject2) => {
-      const handleToken = (token) => {
-        callback({ tag: 'Stripe', token: token.id }).then(resolve2, reject2);
-      };
+): Promise<void> => loadStripe().then(() => {
 
-      const stripeKey = getStripeKey(currency, isTestUser);
+  const handleToken = (token) => {
+    callback(token.id);
+  };
+  const defaultCloseHandler: () => void = () => {};
 
-      resolve1([
-        window.StripeCheckout.configure({
-          name: 'Guardian',
-          description: 'Please enter your card details.',
-          allowRememberMe: false,
-          key: stripeKey,
-          image: 'https://uploads.guim.co.uk/2018/01/15/gu.png',
-          locale: 'auto',
-          currency,
-          token: handleToken,
-        }),
-        innerPromise
-      ]);
-    });
-  }));
-}
+  const stripeKey = getStripeKey(currency, isTestUser);
 
-function openDialogBox(stripeHandler: Object) {
-  return (amount: number, email: string) => {
+  stripeHandler = window.StripeCheckout.configure({
+    name: 'Guardian',
+    description: 'Please enter your card details.',
+    allowRememberMe: false,
+    key: stripeKey,
+    image: 'https://uploads.guim.co.uk/2018/01/15/gu.png',
+    locale: 'auto',
+    currency,
+    token: handleToken,
+    closed: closeHandler || defaultCloseHandler,
+  });
+});
+
+const openDialogBox = (amount: number, email: string) => {
+  if (stripeHandler) {
     stripeHandler.open({
       // Must be passed in pence.
       amount: amount * 100,
       email,
     });
   }
-}
+};
 
 export {
-  setupStripeCheckout,
   openDialogBox,
   getStripeKey,
 };
