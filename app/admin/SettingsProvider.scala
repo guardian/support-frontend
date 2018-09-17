8ec7a4fc8faa41a33890ac6779ad830e0482c99f
guy.dawson <guy.dawson@theguardian.com>
2018-09-17 18:20:41 +0100

    fastly service implemented

--- app/admin/SettingsProvider.scala
+++ app/admin/SettingsProvider.scala
@@ -4,14 +4,17 @@ import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicReference
 
 import akka.actor.ActorSystem
+import cats.data.EitherT
+import cats.instances.future._
 import com.amazonaws.services.s3.AmazonS3
 import config.{Configuration, FastlyConfig}
 import monitoring.SafeLogger
+import play.api.libs.ws.WSClient
 import play.api.mvc.Result
-import services.fastly.FastlyService
 
+import services.fastly.FastlyService
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 abstract class SettingsProvider {
 
@@ -41,13 +44,14 @@ class S3SettingsProvider private (
 
   private val cachedSettings = new AtomicReference[Settings](initialSettings)
 
-  def getAndSetSettings(): Either[Throwable, SettingsUpdate] =
-    Settings.fromS3(source).map { settings =>
-      SettingsUpdate(cachedSettings.getAndSet(settings), settings)
-    }
+  def getAndSetSettings(): EitherT[Future, Throwable, SettingsUpdate] =
+    EitherT.fromEither(Settings.fromS3(source))
+      .map(settings => SettingsUpdate(cachedSettings.getAndSet(settings), settings))
 
-  def purgeIfChanged(diff: SettingsUpdate): Either[Throwable, SettingsUpdate] = {
-    val purgeResult = if (diff.isChange) fastlyService.purgeSurrogateKey(SettingsSurrogateKey.settingsSurrogateKey) else Right(())
+  def purgeIfChanged(diff: SettingsUpdate): EitherT[Future, Throwable, SettingsUpdate] = {
+    val purgeResult =
+      if (diff.isChange) fastlyService.purgeSurrogateKey(SettingsSurrogateKey.settingsSurrogateKey)
+      else EitherT.pure[Future, Throwable](())
     purgeResult.map(_ => diff)
   }
 
@@ -55,14 +59,13 @@ class S3SettingsProvider private (
   private def pollS3(): Unit =
     // TODO: should duration be configurable?
     system.scheduler.scheduleOnce(1.minute) {
-      (for {
-        update <- getAndSetSettings()
-        result <- purgeIfChanged(update)
-      } yield result).fold(
-        err => SafeLogger.error(scrub"error occurred getting settings from S3", err),
-        update => if (update.isChange) SafeLogger.info(s"settings changed from ${update.old} to ${update.current}")
-      )
-      pollS3()
+      getAndSetSettings()
+        .flatMap(purgeIfChanged)
+        .fold(
+          err => SafeLogger.error(scrub"error occurred getting settings from S3", err),
+          update => if (update.isChange) SafeLogger.info(s"settings changed from ${update.old} to ${update.current}")
+        )
+        .map(_ => pollS3())
     }
 
   override def settings(): Settings = cachedSettings.get
@@ -70,18 +73,27 @@ class S3SettingsProvider private (
 
 object S3SettingsProvider {
 
-  def fromS3(s3: SettingsSource.S3, fastlyConfig: FastlyConfig)(implicit client: AmazonS3, system: ActorSystem): Either[Throwable, SettingsProvider] = {
+  def fromS3(s3: SettingsSource.S3, fastlyConfig: FastlyConfig)(
+    implicit
+    client: AmazonS3,
+    system: ActorSystem,
+    wsClient: WSClient
+  ): Either[Throwable, SettingsProvider] = {
     // Ok using a single threaded execution context,
     // since the only one task is getting executed periodically (pollS3())
     implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(1))
     val fastlyService = new FastlyService(fastlyConfig)
-    Settings.fromS3(s3).map(new S3SettingsProvider(_, s3, fastlyService))
+    Settings.fromS3(s3).map { settings =>
+      val service = new S3SettingsProvider(settings, s3, fastlyService)
+      service.pollS3()
+      service
+    }
   }
 }
 
 object SettingsProvider {
 
-  def fromAppConfig(config: Configuration)(implicit client: AmazonS3, system: ActorSystem): Either[Throwable, SettingsProvider] = {
+  def fromAppConfig(config: Configuration)(implicit client: AmazonS3, system: ActorSystem, wsClient: WSClient): Either[Throwable, SettingsProvider] = {
     SafeLogger.info(s"loading settings from ${config.settingsSource}")
     config.settingsSource match {
       case s3: SettingsSource.S3 => S3SettingsProvider.fromS3(s3, config.fastlyConfig)
