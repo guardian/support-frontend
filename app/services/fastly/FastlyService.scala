7d22ab281f6be3e41000a0c6ba5a423fdc03f241
guy.dawson <guy.dawson@theguardian.com>
2018-09-17 17:36:31 +0100

    start to implement the fastly service

--- app/services/fastly/FastlyService.scala
+++ app/services/fastly/FastlyService.scala
@@ -1,8 +1,24 @@
 package services.fastly
 
+import cats.data.EitherT
+import cats.instances.future._
+import cats.syntax.applicativeError._
 import config.FastlyConfig
+import play.api.libs.ws.WSClient
 
-class FastlyService(config: FastlyConfig) {
+import scala.concurrent.{ExecutionContext, Future}
 
-  def purgeSurrogateKey(key: String): Either[Throwable, Unit] = ???
+class FastlyService(config: FastlyConfig)(implicit client: WSClient) {
+
+  // https://docs.fastly.com/api/purge#purge_d8b8e8be84c350dd92492453a3df3230
+  def purgeSurrogateKey(key: String)(implicit ec: ExecutionContext): EitherT[Future, Throwable, Unit] = {
+    client.url(s"https://api.fastly.com/service/${config.serviceId}/purge/$key")
+      .withHttpHeaders(
+        "Fastly-Key" -> config.apiToken,
+        "Accept" -> "application/json"
+      )
+      .execute("POST")
+      .attemptT
+      .map(_ => ()) // TODO: return type
+  }
 }
