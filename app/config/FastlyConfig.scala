38334b243bbeb42d9a36d48d097e29a0a538e117
guy.dawson <guy.dawson@theguardian.com>
2018-09-18 18:31:00 +0100

    implement fromConfig method for fastly config

--- app/config/FastlyConfig.scala
+++ app/config/FastlyConfig.scala
@@ -1,10 +1,17 @@
 package config
 
+import cats.syntax.either._
 import com.typesafe.config.Config
 
 case class FastlyConfig private (serviceId: String, apiToken: String)
 
 object FastlyConfig {
-  // TODO: implement with Lauren
-  def fromConfig(config: Config): Either[Throwable, FastlyConfig] = ??? // FIXME
+
+  def fromConfig(config: Config): Either[Throwable, FastlyConfig] =
+    Either.catchNonFatal {
+      FastlyConfig(
+        config.getString("fastly.serviceId"),
+        config.getString("fastly.apiToken")
+      )
+    }
 }
