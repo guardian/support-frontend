c1ca6565925f4b46cccd724b0e4bffec0cf7ae2d
rupertbates <rupert.bates@guardian.co.uk>
2018-09-24 16:26:21 +0100

    Add correct tracking code

--- assets/pages/digital-subscription-landing/helpers/promotions.js
+++ assets/pages/digital-subscription-landing/helpers/promotions.js
@@ -3,9 +3,10 @@
 import { getQueryParameter } from 'helpers/url';
 
 function showPromotion(): boolean {
-  return getQueryParameter('promoCode') === 'the_code'; // TODO: need the actual code here
+  return getQueryParameter('utm_source') === 'eml' &&
+    getQueryParameter('utm_medium') === 'emlf' &&
+    getQueryParameter('utm_campaign') === 'SC_AD_Print_Upsell_270918_ID2';
 }
-
 
 function getPageTitle(): string {
   if (showPromotion()) {
