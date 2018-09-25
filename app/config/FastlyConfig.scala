662d7e3f8c9ebe96911900d2d8ab38a66ceb68d9
guy.dawson <guy.dawson@theguardian.com>
2018-09-25 14:01:13 +0100

    some documentationon the fromConfig method

--- app/config/FastlyConfig.scala
+++ app/config/FastlyConfig.scala
@@ -9,6 +9,9 @@ case class FastlyConfig private (serviceId: String, apiToken: String)
 
 object FastlyConfig {
 
+  // Tries to load a FastlyConfig instance from the fastly object in the configuration.
+  // This object should only be specified if we want to purge the Fastly cache.
+  // Typically this should be the case if and only if the app is running in CODE or PROD mode.
   def fromConfig(config: Config): Either[Throwable, Option[FastlyConfig]] = {
     type Result[A] = Either[Throwable, A]
     Try(config.getConfig("fastly"))
