85c852c13cfc12daa9af1cd3e7081b410cc45839
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-09-27 14:56:37 +0100

    initialise form data from user (#998)

--- assets/pages/new-contributions-landing/contributionsLandingInit.js
+++ assets/pages/new-contributions-landing/contributionsLandingInit.js
@@ -3,16 +3,17 @@
 // ----- Imports ----- //
 import { type Store, type Dispatch } from 'redux';
 import { getValidPaymentMethods } from 'helpers/checkouts';
-import { updatePaymentMethod } from './contributionsLandingActions';
+import {
+  updatePaymentMethod,
+  updateUserFormData,
+} from './contributionsLandingActions';
 import { type State } from './contributionsLandingReducer';
 import { type Action } from './contributionsLandingActions';
 
 // ----- Functions ----- //
 
-const init = (store: Store<State, Action, Dispatch<Action>>) => {
-  const { dispatch } = store;
-  const state = store.getState();
 
+function initialisePaymentMethod(state, dispatch) {
   const { contributionType } = state.page.form;
   const { countryId } = state.common.internationalisation;
   const { switches } = state.common.settings;
@@ -24,6 +25,18 @@ const init = (store: Store<State, Action, Dispatch<Action>>) => {
   } else {
     dispatch(updatePaymentMethod('None'));
   }
+}
+
+
+const init = (store: Store<State, Action, Dispatch<Action>>) => {
+  const { dispatch } = store;
+  const state = store.getState();
+  initialisePaymentMethod(state, dispatch);
+
+  const { firstName, lastName, email } = state.page.user;
+  dispatch(updateUserFormData({ firstName, lastName, email }));
+
+
 };
 
 
