98547babfe82223c410bfcdb463c365636ee58ee
Michael McNamara <michael.mcnamara@guardian.co.uk>
2018-10-01 13:37:08 +0100

    update max line length as Controllers definition cant be split between lines

--- app/wiring/Controllers.scala
+++ app/wiring/Controllers.scala
@@ -4,8 +4,7 @@ import controllers._
 import play.api.BuiltInComponentsFromContext
 
 trait Controllers {
-  self: AssetsComponents with Services with BuiltInComponentsFromContext with ApplicationConfiguration
-    with ActionBuilders with Assets with GoogleAuth with Monitoring =>
+  self: AssetsComponents with Services with BuiltInComponentsFromContext with ApplicationConfiguration with ActionBuilders with Assets with GoogleAuth with Monitoring =>
 
   lazy val assetController = new controllers.Assets(httpErrorHandler, assetsMetadata)
 
