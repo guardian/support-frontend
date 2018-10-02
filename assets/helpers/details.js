61c00f508cfcdd97b1e7374827df90e3ce633402
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-10-02 15:03:40 +0100

    Lint

--- assets/helpers/details.js
+++ assets/helpers/details.js
@@ -3,37 +3,36 @@
 const isDetailsSupported: boolean = ('open' in document.createElement('details'));
 
 function toggleDetailsState(details: HTMLDetailsElement) {
-    if (details.hasAttribute('open')) {
-        details.removeAttribute('open');
-    } else {
-        details.setAttribute('open', 'open');
-    }
+  if (details.hasAttribute('open')) {
+    details.removeAttribute('open');
+  } else {
+    details.setAttribute('open', 'open');
+  }
 }
 
 function handleEvent(event: UIEvent) {
-    var targetNode: Element = (event.target: any);
+  let targetNode: Element = (event.target: any);
 
-    while (targetNode.nodeName.toLowerCase() !== 'summary' && targetNode !== document && targetNode.parentElement) {
-        targetNode = targetNode.parentElement;
-    }
-    if (targetNode.nodeName.toLowerCase() === 'summary' && targetNode.parentElement instanceof HTMLDetailsElement) {
-        toggleDetailsState(targetNode.parentElement);
-    }
+  while (targetNode.nodeName.toLowerCase() !== 'summary' && targetNode !== document && targetNode.parentElement) {
+    targetNode = targetNode.parentElement;
+  }
+  if (targetNode.nodeName.toLowerCase() === 'summary' && targetNode.parentElement instanceof HTMLDetailsElement) {
+    toggleDetailsState(targetNode.parentElement);
+  }
 }
 
 function polyfillDetails() {
-    document.addEventListener('click', handleEvent, true);
-    document.addEventListener('keypress', function (event: KeyboardEvent) {
-        if (event.key && (event.key === ' ' || event.key === 'Enter')) {
-            handleEvent(event);
-        }
-        else if (event.keyCode === 0x20 || event.keyCode === 0x0D) {
-            handleEvent(event);
-        }
-    }, true);
+  document.addEventListener('click', handleEvent, true);
+  document.addEventListener('keypress', (event: KeyboardEvent) => {
+    if (event.key && (event.key === ' ' || event.key === 'Enter')) {
+      handleEvent(event);
+    } else if (event.keyCode === 0x20 || event.keyCode === 0x0D) {
+      handleEvent(event);
+    }
+  }, true);
 }
 
 export {
   isDetailsSupported,
-  polyfillDetails
+  polyfillDetails,
 };
