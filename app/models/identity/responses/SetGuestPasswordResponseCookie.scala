e2521ab529c483f890c39309e2e8812e68b5d8d2
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-10-04 12:25:42 +0100

    .

--- app/models/identity/responses/SetGuestPasswordResponseCookie.scala
+++ app/models/identity/responses/SetGuestPasswordResponseCookie.scala
@@ -21,7 +21,8 @@ object SetGuestPasswordResponseCookies {
   implicit val readsCookieResponse: Reads[SetGuestPasswordResponseCookie] = Json.reads[SetGuestPasswordResponseCookie]
   implicit val readsCookiesResponse: Reads[SetGuestPasswordResponseCookies] = Json.reads[SetGuestPasswordResponseCookies]
 
-  def getCookies(setPasswordResponse: SetGuestPasswordResponseCookies, guardianDomain: String)(implicit executionContext: ExecutionContext): List[PlayCookie] = {
+  def getCookies(setPasswordResponse: SetGuestPasswordResponseCookies, guardianDomain: String)
+                (implicit executionContext: ExecutionContext): List[PlayCookie] = {
     setPasswordResponse.values.map { cookie =>
       val maxAge = Some(Seconds.secondsBetween(DateTime.now, setPasswordResponse.expiresAt).getSeconds)
       val secureHttpOnly = cookie.key.startsWith("SC_")
