7142655dddfea424feeb6d7e56a3e2e1d2ae1459
Joseph Smith <joseph.smith@guardian.co.uk>
2018-10-05 13:42:41 +0100

    Comment

--- assets/helpers/contributions.js
+++ assets/helpers/contributions.js
@@ -20,6 +20,9 @@ export type AllPaymentMethods<T> = {
   None: T,
 }
 
+// This lets us create a union type from the object keys,
+// avoiding the need to specify them separately and keep them in sync!
+// https://flow.org/en/docs/types/utilities/#toc-keys
 // We need to supply the type parameter, but we're only using the keys
 // so it's irrelevant - so we supply null
 export type PaymentMethod = $Keys<AllPaymentMethods<null>>;
