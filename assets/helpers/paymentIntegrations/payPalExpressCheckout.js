6a86b776cfd02049dcad8a106fb92235f5efb6b8
Joseph Smith <joseph.smith@guardian.co.uk>
2018-10-15 13:25:35 +0100

    Restore old payPalExpressCheckout file

--- assets/helpers/paymentIntegrations/payPalExpressCheckout.js
+++ assets/helpers/paymentIntegrations/payPalExpressCheckout.js
@@ -5,8 +5,11 @@
 import { logException } from 'helpers/logger';
 import { routes } from 'helpers/routes';
 import * as storage from 'helpers/storage';
+import { formInputs } from 'helpers/checkoutForm/checkoutForm';
 import type { Csrf as CsrfState } from 'helpers/csrf/csrfReducer';
 import type { IsoCurrency } from 'helpers/internationalisation/currency';
+import { formClassName } from '../../pages/regular-contributions/components/formFields';
+
 
 // ----- Functions ----- //
 
@@ -52,7 +55,6 @@ function setupPayment(
   const csrfToken = csrf.token;
 
   return (resolve, reject) => {
-    console.log('setupPayment');
     storage.setSession('paymentMethod', 'PayPal');
     const requestBody = {
       amount: amountToPay,
@@ -69,14 +71,10 @@ function setupPayment(
           logException('PayPal token came back blank');
         }
       }).catch((err) => {
-        logException(err.message);
-        reject(err);
-      });
+      logException(err.message);
+      reject(err);
+    });
   };
-}
-
-function getPayPalEnvironment(isTestUser: boolean): string {
-  return isTestUser ? window.guardian.payPalEnvironment.uat : window.guardian.payPalEnvironment.default;
 }
 
 function createAgreement(payPalData: Object, csrf: CsrfState) {
@@ -94,8 +92,6 @@ function setup(
   onPaymentAuthorisation: string => void,
   canOpen: () => boolean,
   whenUnableToOpen: () => void,
-  formClassName: string,
-  isTestUser: boolean,
 ): Promise<Object> {
 
   const handleBaId = (baid: Object) => {
@@ -110,16 +106,8 @@ function setup(
       });
   };
 
-  function addFormChangeListener(handler: () => void) {
-    const form = document.querySelector(`.${formClassName}`);
-    if (form instanceof HTMLElement) {
-      // Thanks to event bubbling, we can just listen on the form element.
-      // This means that we'll get change events even from form elements which
-      // aren't in the DOM at the time this handler is bound.
-      form.addEventListener('change', handler);
-    } else {
-      logException(`No form found with class ${formClassName}`);
-    }
+  function addFormChangeListeners(handler: () => void) {
+    formInputs(formClassName).forEach(input => input.addEventListener('change', handler));
   }
 
   function toggleButton(actions): void {
@@ -127,22 +115,16 @@ function setup(
   }
 
   const payPalOptions: Object = {
-    // TODO: default value is just to not break existing flow.
-    // maybe just make a copy of this file instead?
-    env: getPayPalEnvironment(isTestUser) || window.guardian.payPalEnvironment,
-
-    // TODO: I think Iona also wanted a different colour for this, so another reason to copy-paste
+    env: window.guardian.payPalEnvironment,
     style: { color: 'blue', size: 'responsive', label: 'pay' },
 
     // Defines whether user sees 'Agree and Continue' or 'Agree and Pay now' in overlay.
     commit: true,
 
     validate(actions) {
-      // TODO: if we stored actions.enable and actions.disable in the state somewhere,
-      // we could trigger the enable/disable from a redux action rather than bypassing
-      // things with our own change handler.
       toggleButton(actions);
-      addFormChangeListener(() => toggleButton(actions));
+
+      addFormChangeListeners(() => toggleButton(actions));
     },
 
     onClick() {
