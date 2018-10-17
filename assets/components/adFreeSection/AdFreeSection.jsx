3e12daeb052b3fec00a3e642d52bfade8a9610e8
Laura <laura.gonzalez@guardian.co.uk>
2018-10-17 15:28:38 +0100

    shorter para

--- assets/components/adFreeSection/AdFreeSection.jsx
+++ assets/components/adFreeSection/AdFreeSection.jsx
@@ -13,9 +13,9 @@ export default function AdFreeSection() {
         <div className="component-ad-free__content">
           <h3 className="component-ad-free__badge">New</h3>
           <div className="component-ad-free__wrapper">
-            <div className="component-ad-free__copy">
+            <div className="component-ad-free__col">
               <h2 className="component-ad-free__header">Ad-free on all your devices</h2>
-              <p>
+              <p className="component-ad-free__copy">
                 {`Avoid the adverts and read without interruptions when you're signed in on your apps and theguardian.com`}
               </p>
             </div>
