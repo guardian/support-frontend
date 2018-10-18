3c0581f5cd02278816594dbf795f963f12bfd312
rupertbates <rupert.bates@guardian.co.uk>
2018-10-18 14:21:27 +0100

    Add the new dates

--- assets/helpers/flashSale.js
+++ assets/helpers/flashSale.js
@@ -3,31 +3,23 @@
 import { getQueryParameter } from 'helpers/url';
 import type { SubscriptionProduct } from './subscriptions';
 
+// Which products are included in the current sale?
+const includedProducts: SubscriptionProduct[] = ['DigitalPack'];
 
 function inOfferPeriod(product: SubscriptionProduct): boolean {
   // Days are 1 based, months are 0 based
-  const startTime = new Date(2018, 7, 20, 0, 0).getTime(); // 20th August 2018
-  const endTime = new Date(2018, 8, 3, 0, 0).getTime(); // include all of 2nd September 2018
-
-  // The current sale is paper and paper+digital only; everything else is unaffected
-  const included = {
-    DigitalPack: false,
-    Paper: true,
-    PaperAndDigital: true,
-    DailyEdition: false,
-    GuardianWeekly: false,
-    PremiumTier: false,
-  };
-
+  const startTime = new Date(2018, 9, 22, 0, 0).getTime(); // 22nd Oct 2018
+  const endTime = new Date(2018, 10, 5, 0, 0).getTime(); // include all of 4th Nov 2018
   const now = Date.now();
-  return (now > startTime && now < endTime && included[product]) ||
-    (included[product] && getQueryParameter('flash_sale') === 'true');
+  const included = includedProducts.includes(product);
+
+  return (now > startTime && now < endTime && included) || (included && getQueryParameter('flash_sale') === 'true');
 }
 
 // Promo codes
 const promoCodes = {
   DigitalPack: {
-    promoCode: '',
+    promoCode: 'DDPO80X',
     price: 11.99,
     intcmp: '',
   },
@@ -79,51 +71,8 @@ function getDiscountedPrice(product: SubscriptionProduct, defaultPrice: number):
   return defaultPrice;
 }
 
-// Copy text
-const offerItem = { heading: 'Subscribe today and save an extra 50% for three months' };
-const saveMoneyOnRetailPrice = { heading: 'Save money on the retail price' };
-const getAllBenefits = { heading: 'Get all the benefits of a digital subscription with paper + digital' };
-const chooseYourPackage = {
-  heading: 'Choose your package and delivery method',
-  text: 'Everyday, Sixday, Weekend, Saturday and Sunday; redeem paper vouchers or get home delivery',
-};
-
-function getDigitalBenefits() {
-  const items = [
-    {
-      heading: 'Premium experience on the Guardian app',
-      text: 'No adverts means faster loading pages and a clearer reading experience. Play our daily crosswords offline wherever you are',
-    },
-    {
-      heading: 'Daily Tablet Edition app',
-      text: 'Read the Guardian, the Observer and all the Weekend supplements in an optimised tablet app; available on iPad',
-    },
-  ];
-
-  if (inOfferPeriod('DigitalPack')) {
-    return [offerItem, ...items];
-  }
-  return items;
-}
-
-function getPaperBenefits() {
-  if (inOfferPeriod('Paper')) {
-    return [offerItem, chooseYourPackage, saveMoneyOnRetailPrice];
-  }
-  return [chooseYourPackage, saveMoneyOnRetailPrice];
-}
-
-function getPaperDigitalBenefits() {
-  if (inOfferPeriod('PaperAndDigital')) {
-    return [offerItem, chooseYourPackage, saveMoneyOnRetailPrice, getAllBenefits];
-  }
-  return [chooseYourPackage, saveMoneyOnRetailPrice, getAllBenefits];
-}
-
 export {
-  getDigitalBenefits,
-  getPaperBenefits,
-  getPaperDigitalBenefits,
+  inOfferPeriod,
   getPromoCode,
   getIntcmp,
   getDiscountedPrice,
