af3853a8a45929a9e3a68ba60d6b47b476e00b5d
Emma Milner <ekmilner@gmail.com>
2018-10-22 14:50:28 +0100

    indentation

--- assets/components/directDebit/directDebitForm/directDebitForm.jsx
+++ assets/components/directDebit/directDebitForm/directDebitForm.jsx
@@ -317,7 +317,9 @@ function PaymentButton(props: {
 function LegalNotice(props: { countryGroupId: CountryGroupId }) {
   return (
     <div className="component-direct-debit-form__legal-notice">
-      <p><strong>Payments by GoCardless</strong> <a href="https://gocardless.com/legal/privacy/" target="_blank" rel="noopener noreferrer">read the GoCardless privacy notice.</a>
+      <p>
+        <strong>Payments by GoCardless </strong>
+        <a href="https://gocardless.com/legal/privacy/" target="_blank" rel="noopener noreferrer">read the GoCardless privacy notice.</a>
       </p>
       <p><strong>Advance notice</strong> The details of your Direct Debit instruction including
         payment schedule, due date, frequency and amount will be sent to you within three working
