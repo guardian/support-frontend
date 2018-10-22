59a4e3528f7b99364aae18252f020191c5c394aa
tomrf1 <tom.forbes@theguardian.com>
2018-10-22 10:08:59 +0100

    Update signup info message

---
+++ assets/components/svgs/information.jsx
@@ -0,0 +1,17 @@
+// @flow
+
+import React from 'react';
+
+export default function SvgInformation() {
+
+  return (
+    <svg width="17" height="17" xmlns="http://www.w3.org/2000/svg">
+      <g fill="none" fillRule="evenodd">
+        <circle fill="#00B2FF" cx="8.5" cy="8.5" r="8.5" />
+        <text fontFamily="GHGuardianHeadline-Bold, GH Guardian Headline" fontSize="12" fontWeight="bold" fill="#FFF">
+          <tspan x="6.6" y="12.9">i</tspan>
+        </text>
+      </g>
+    </svg>
+  );
+}
