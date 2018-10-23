6b08f247c90ad5392a6d0a1fe34a56aace674dd8
John Duffell <john.duffell@guardian.co.uk>
2018-10-23 17:05:23 +0100

    use released models library

--- src/test/scala/com/gu/support/workers/AccessScopeCodecTest.scala
+++ src/test/scala/com/gu/support/workers/AccessScopeCodecTest.scala
@@ -7,12 +7,12 @@ import org.scalatest.{FlatSpec, Matchers}
 class AccountAccessScopeCodecTest extends FlatSpec with Matchers {
 
   it should "for backwards compatibility, deserialise when it's not specified to no restriction" in {
-    val actual = AccountAccessScope.fromWire(None)
+    val actual = AccountAccessScope.fromInput(None)
     actual should be(AuthenticatedAccess)
   }
 
   it should "deserialise when it's got a value to token scope" in {
-    val actual = AccountAccessScope.fromWire(Some("hello"))
+    val actual = AccountAccessScope.fromInput(Some("hello"))
     actual should be(SessionAccess(SessionId("hello")))
   }
 
