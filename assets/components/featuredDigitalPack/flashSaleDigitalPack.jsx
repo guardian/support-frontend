2f033cebf7ecaf381ab1f4a3e007803e35d9a95e
Laura <laura.gonzalez@guardian.co.uk>
2018-10-24 16:10:11 +0100

    add types

--- assets/components/featuredDigitalPack/flashSaleDigitalPack.jsx
+++ assets/components/featuredDigitalPack/flashSaleDigitalPack.jsx
@@ -8,6 +8,7 @@ import Heading from 'components/heading/heading';
 import CtaLink from 'components/ctaLink/ctaLink';
 import GridPicture from 'components/gridPicture/gridPicture';
 import { currencies, detect } from 'helpers/internationalisation/currency';
+import { type CountryGroupId } from 'helpers/internationalisation/countryGroup';
 import { getDiscountedPrice } from 'helpers/flashSale';
 import { sendTrackingEventsOnClick } from 'helpers/subscriptions';
 import type { ComponentAbTest } from 'helpers/subscriptions';
@@ -18,11 +19,12 @@ import type { HeadingSize } from 'components/heading/heading';
 type PropTypes = {
   headingSize: HeadingSize,
   url: string,
+  countryGroupId: CountryGroupId,
   abTest: ComponentAbTest | null,
 };
 
 
-export default function FlashSaleDigitalPack(props: PropTypes) {
+function FlashSaleDigitalPack(props: PropTypes) {
   const currency = currencies[detect(props.countryGroupId)].glyph;
   return (
     <section className="component-flash-sale-featured-digital-pack">
@@ -81,3 +83,14 @@ export default function FlashSaleDigitalPack(props: PropTypes) {
   );
 }
 
+
+// ----- Default Props ----- //
+
+FlashSaleDigitalPack.defaultProps = {
+  abTest: null,
+};
+
+
+// ----- Export ----- //
+
+export default FlashSaleDigitalPack;
