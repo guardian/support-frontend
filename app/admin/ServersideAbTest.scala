b01ad7b32e9027202e6a28dacab704c304b28553
Joseph Smith <joseph.smith@guardian.co.uk>
2018-10-25 15:25:20 +0100

    Handle invalid cookie value

--- app/admin/ServersideAbTest.scala
+++ app/admin/ServersideAbTest.scala
@@ -8,10 +8,11 @@ object ServersideAbTest {
   // Serverside A/B tests currently only support a single concurrent test
   // running to 100% audience with a 50%/50% split
   def getParticipation(implicit request: RequestHeader): Participation = {
-    ServersideAbTestCookie.get.fold(computeParticipation)(_.value match {
-      case "Control" => Control
-      case "Variant" => Variant
-    })
+    ServersideAbTestCookie.get.flatMap(_.value match {
+      case "Control" => Some(Control)
+      case "Variant" => Some(Variant)
+      case _ => None
+    }).getOrElse(computeParticipation)
   }
 
   private def computeParticipation: Participation = if (Random.nextBoolean) Control else Variant
