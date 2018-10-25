3b5b2eb15b050bc8590d06f042ed7d24464c2294
James Byers <Ap0c@users.noreply.github.com>
2018-10-25 16:37:06 +0100

    Added email section SVGs.

--- assets/components/svgs/information.jsx
+++ assets/components/svgs/information.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function SvgInformation() {
 
   return (
-    <svg width="17" height="17" xmlns="http://www.w3.org/2000/svg">
+    <svg width="17" height="17" className="svg-information" xmlns="http://www.w3.org/2000/svg">
       <g fill="none" fillRule="evenodd">
         <circle fill="#00B2FF" cx="8.5" cy="8.5" r="8.5" />
         <text fontFamily="GHGuardianHeadline-Bold, GH Guardian Headline" fontSize="12" fontWeight="bold" fill="#FFF">
--- assets/pages/digital-subscription-checkout/components/thankYouContent.jsx
+++ assets/pages/digital-subscription-checkout/components/thankYouContent.jsx
@@ -16,6 +16,8 @@ import { classNameWithModifiers } from 'helpers/utilities';
 
 import LeftMarginSection from 'components/leftMarginSection/leftMarginSection';
 import CtaLink from 'components/ctaLink/ctaLink';
+import SvgNewsletters from 'components/svgs/newsletters';
+import SvgInformation from 'components/svgs/information';
 
 
 // ----- Types ----- //
@@ -90,18 +92,22 @@ function EmailsSection() {
 
   return (
     <LeftMarginSection modifierClasses={['thank-you-emails']}>
-      <h2 className="thank-you-content__heading">
-        Stay in the know
-      </h2>
-      <p className="thank-you-content__copy">
-        Opt in below to receive news and offers from The Guardian, The Observer
-        and Guardian Weekly on the ways to read and support our journalism.
-      </p>
-      <p className="thank-you-content__copy thank-you-content__copy--small">
-        <small>
-          You can unsubscribe at any time by going to the <a className="thank-you-content__link" href={emailPreferencesUrl}>preference centre</a>
-        </small>
-      </p>
+      <div className="thank-you-content__email-copy">
+        <h2 className="thank-you-content__heading">
+          Stay in the know
+        </h2>
+        <p className="thank-you-content__copy">
+          Opt in below to receive news and offers from The Guardian, The Observer
+          and Guardian Weekly on the ways to read and support our journalism.
+        </p>
+        <p className="thank-you-content__copy thank-you-content__copy--small">
+          <small>
+            <SvgInformation />
+            You can unsubscribe at any time by going to the <a className="thank-you-content__link" href={emailPreferencesUrl}>preference centre</a>
+          </small>
+        </p>
+      </div>
+      <SvgNewsletters />
     </LeftMarginSection>
   );
 
