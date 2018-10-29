a357d104a0746fdf11c6c2ff9109bd27edc87a45
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-10-29 16:27:55 +0000

    user hypen-seperated string for endpoint

--- assets/helpers/routes.js
+++ assets/helpers/routes.js
@@ -16,7 +16,7 @@ const routes: {
   recurringContribPending: '/contribute/recurring/pending',
   contributionsSendMarketing: '/contribute/send-marketing',
   contributionsSetPasswordGuest: '/identity/set-password-guest',
-  getUserType: '/identity/getUserType',
+  getUserType: '/identity/get-user-type',
   oneOffContribCheckout: '/contribute/one-off',
   oneOffContribThankyou: '/contribute/one-off/thankyou',
   oneOffContribAutofill: '/contribute/one-off/autofill',
