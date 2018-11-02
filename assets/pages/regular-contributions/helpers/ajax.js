5ed348fd885adbbc85c72d62c63c2fbe72d48122
Joseph Smith <joseph.smith@guardian.co.uk>
2018-11-02 17:40:23 +0000

    Send sessionId with recurring requests on old flow

--- assets/pages/regular-contributions/helpers/ajax.js
+++ assets/pages/regular-contributions/helpers/ajax.js
@@ -21,6 +21,7 @@ import type { Csrf as CsrfState } from 'helpers/csrf/csrfReducer';
 import type { PaymentMethod } from 'helpers/contributions';
 import type { OptimizeExperiments } from 'helpers/tracking/optimize';
 import { checkoutPending, checkoutSuccess, checkoutError, creatingContributor, setGuestAccountCreationToken } from '../regularContributionsActions';
+import uuidv4 from "uuid";
 
 // ----- Setup ----- //
 
@@ -62,7 +63,8 @@ type RegularContribFields = {|
   ophanIds: OphanIds,
   referrerAcquisitionData: ReferrerAcquisitionData,
   supportAbTests: AcquisitionABTest[],
-  email: ?string
+  email: ?string,
+  sessionId: string,
 |};
 
 // https://github.com/guardian/support-models/blob/master/src/main/scala/com/gu/support/workers/model/Status.scala
@@ -165,6 +167,7 @@ function requestData(
     referrerAcquisitionData,
     supportAbTests,
     email: user.email,
+    sessionId: uuidv4(),
   };
 
   if (user.stateField) {
