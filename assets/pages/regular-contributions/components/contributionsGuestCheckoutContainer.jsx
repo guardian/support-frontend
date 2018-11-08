d4b5b93dad5abbc4eac4372b2c29a56a96f13adf
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-11-08 15:13:29 +0000

    add to old flow

--- assets/pages/regular-contributions/components/contributionsGuestCheckoutContainer.jsx
+++ assets/pages/regular-contributions/components/contributionsGuestCheckoutContainer.jsx
@@ -4,9 +4,9 @@
 
 import { connect } from 'react-redux';
 import type { Dispatch } from 'redux';
-import type { Contrib } from 'helpers/contributions';
+import type { Contrib, PaymentMethod } from 'helpers/contributions';
 import type { UserTypeFromIdentityResponse } from 'helpers/identityApis';
-import { formIsValid, getForm, invalidReason } from 'helpers/checkoutForm/checkoutForm';
+import { formIsValid, getForm, invalidReason, onFormSubmit } from 'helpers/checkoutForm/checkoutForm';
 import { canContributeWithoutSigningIn } from 'helpers/identityApis';
 import { trackCheckoutSubmitAttempt } from 'helpers/tracking/ophanComponentEventTracking';
 import ContributionsGuestCheckout from './contributionsGuestCheckout';
@@ -32,29 +32,24 @@ const mapDispatchToProps = (dispatch: Dispatch<CheckoutAction>) => ({
   onBackClick: () => {
     dispatch(setStage('checkout'));
   },
+
   onNextButtonClick: (
     contributionType: Contrib,
     isSignedIn: boolean,
     userTypeFromIdentityResponse: UserTypeFromIdentityResponse,
+    paymentMethod: PaymentMethod,
   ) => {
-    const canContribute =
-      canContributeWithoutSigningIn(contributionType, isSignedIn, userTypeFromIdentityResponse)
-      || isSignedIn;
-
-    const componentId = `opf-${contributionType}-submit`;
     const formName = 'regular-contrib__checkout-form';
-    if (formIsValid(formName)) {
-      if (canContribute) {
-        dispatch(setStage('payment'));
-        trackCheckoutSubmitAttempt(componentId, `opf-allowed-for-user-type-${userTypeFromIdentityResponse}`);
-      } else {
-        trackCheckoutSubmitAttempt(componentId, `opf-blocked-because-user-type-is-${userTypeFromIdentityResponse}`);
-      }
-    } else {
-      trackCheckoutSubmitAttempt(componentId, `opf-blocked-because-form-not-valid${invalidReason(getForm(formName))}`);
-    }
-
-    dispatch(setCheckoutFormHasBeenSubmitted());
+    onFormSubmit(
+      paymentMethod,
+      contributionType,
+      getForm(formName),
+      isSignedIn,
+      userTypeFromIdentityResponse,
+      () => {}, // we don't need this for the old flow
+      () => { dispatch(setCheckoutFormHasBeenSubmitted()); },
+      () => { dispatch(setStage('payment')); },
+    );
   },
 });
 
