efd0bcc13e268355dacbd8b9c09242a8b8a59c75
Laura <laura.gonzalez@guardian.co.uk>
2018-11-15 12:50:45 +0000

    action types

--- assets/components/productPage/productPagePeriodForm/productPagePeriodFormActions.js
+++ assets/components/productPage/productPagePeriodForm/productPagePeriodFormActions.js
@@ -1,10 +1,11 @@
 // @flow
+import { type Dispatch } from 'redux';
 import { sendTrackingEventsOnClick, type SubscriptionProduct } from 'helpers/subscriptions';
 
 export type Action<P> = { type: 'SET_PERIOD', period: P, scope: string };
 
-function productPagePeriodFormActionsFor<P>(scope: string, product: SubscriptionProduct | null) {
-  function setPeriod(period: string): (dispatch: Function) => Action<P> {
+function productPagePeriodFormActionsFor<P:string>(scope: string, product: SubscriptionProduct | null) {
+  function setPeriod(period: P): (dispatch: Dispatch<Action<P>>) => Action<P> {
     return (dispatch) => {
       if (product) {
         sendTrackingEventsOnClick(`toggle_period_${period}`, product, null)();
