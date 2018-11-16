42a95800e8b187fcab9f18d08d7bb72e9c98ac07
James Byers <Ap0c@users.noreply.github.com>
2018-11-16 12:05:50 +0000

    Folded a few similar test cases into one test.

--- assets/helpers/types/__tests__/optionTest.js
+++ assets/helpers/types/__tests__/optionTest.js
@@ -13,22 +13,13 @@ describe('option', () => {
 
     it('retrieves the first item from an array', () => {
       expect(headOption([1, 2, 3])).toBe(1);
+      expect(headOption([null, 2, 3])).toBeNull();
+      expect(headOption(['', 2, 3])).toBe('');
+      expect(headOption([false, true, false])).toBe(false);
     });
 
     it('returns null when the array is empty', () => {
       expect(headOption([])).toBeNull();
-    });
-
-    it('returns null when the array contains null', () => {
-      expect(headOption([null, 2, 3])).toBeNull();
-    });
-
-    it('returns empty string when the array contains empty string', () => {
-      expect(headOption(['', 2, 3])).toBe('');
-    });
-
-    it('returns false when the array contains false', () => {
-      expect(headOption([false, true, false])).toBe(false);
     });
 
   });
