e520a0289d27a85f00f4556655d9c8cea6c25206
Joseph Smith <joseph.smith@guardian.co.uk>
2018-11-16 16:18:27 +0000

    Check session storage too for consistent banner experience

--- assets/helpers/abTests/abtestDefinitions.js
+++ assets/helpers/abTests/abtestDefinitions.js
@@ -1,4 +1,5 @@
 // @flow
+import { getSession } from 'helpers/storage';
 import type { Tests } from './abtest';
 
 // ----- Tests ----- //
@@ -33,6 +34,12 @@ export const tests: Tests = {
     canRun: () => [
       'ACQUISITIONS_EPIC',
       'ACQUISITIONS_ENGAGEMENT_BANNER',
-    ].some(componentType => window.location.href.includes(componentType)),
+    ].some((componentType: string) => {
+      // Try from session storage first. This is so that we get the correct header
+      // on subsequent pageviews which don't have the componentType in the URL, e.g.
+      // thank you page after PayPal one-off, or after changing country dropdown.
+      const searchString = getSession('acquisitionData') || window.location.href;
+      return searchString.includes(componentType);
+    }),
   },
 };
