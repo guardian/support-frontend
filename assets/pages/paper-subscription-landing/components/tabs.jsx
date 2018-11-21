cbc0e32df194f2240027519a6c28ce591c4efbda
Laura <laura.gonzalez@guardian.co.uk>
2018-11-21 16:23:46 +0000

    cleaner tabs

--- assets/pages/paper-subscription-landing/components/tabs.jsx
+++ assets/pages/paper-subscription-landing/components/tabs.jsx
@@ -4,18 +4,19 @@
 
 import React, { Component } from 'react';
 
+import { outsetClassName } from 'components/productPage/productPageContentBlock/productPageContentBlock';
 import ProductPageTabs from 'components/productPage/productPageTabs/productPageTabs';
 
 class Tabs extends Component<any, any> {
   state = {
-    active: 1,
+    active: 0,
   }
   onChange(active: any) {
     this.setState({ active });
   }
   render() {
     return (
-      <div style={{ marginLeft: -11 }}>
+      <div className={outsetClassName}>
         <ProductPageTabs
           active={this.state.active}
           onChange={(t) => { this.onChange(t); }}
