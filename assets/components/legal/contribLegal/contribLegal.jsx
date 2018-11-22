7f8c92d0081088a67b3eb43c5068a89dfa97180c
Robert Barbour <42343800+rjbarbour-g@users.noreply.github.com>
2018-11-22 16:18:55 +0000

    WIP added Terms and Conditions and Privacy Policy links to NPF, taken from OPF. Need AUS version too.

--- assets/components/legal/contribLegal/contribLegal.jsx
+++ assets/components/legal/contribLegal/contribLegal.jsx
@@ -22,6 +22,9 @@ export default function ContribLegal(props: PropTypes) {
 
   return (
     <p className="component-contrib-legal">
+      By proceeding, you are agreeing to our <a href="https://www.theguardian.com/info/2016/apr/04/contribution-terms-and-conditions">Terms and Conditions</a>.
+      To find out what personal data we collect and how we use it, please visit our <a href="https://www.theguardian.com/help/privacy-policy">Privacy Policy</a>.
+
       The ultimate owner of the Guardian is The Scott Trust Limited,
       whose role it is to secure the editorial and financial independence
       of the Guardian in perpetuity. Reader contributions support the
