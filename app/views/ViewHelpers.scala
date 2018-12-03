0c2ba3362e885536b43af5bad2057d048471444f
Laura <laura.gonzalez@guardian.co.uk>
2018-12-03 12:35:10 +0000

    move mapToDataset to viewhelpers

--- app/views/ViewHelpers.scala
+++ app/views/ViewHelpers.scala
@@ -8,6 +8,11 @@ import io.circe.syntax._
 
 object ViewHelpers {
   def doNotTrack(implicit request: RequestHeader): Boolean = request.headers.get("DNT").contains("1")
+  def mapToDataset(data: Map[String, String]): Html =
+    data.map {
+      case (key, value) =>
+        Html(s"""data-${HtmlFormat.escape(key.replaceAll("[^a-zA-Z0-9\\-]", ""))}="${HtmlFormat.escape(value)}" """)
+    }
   def printSettings(settings: Settings): String =
     settings
       .asJson
