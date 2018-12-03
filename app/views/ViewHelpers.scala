cc12c2346648bc805dacc17c8edc3d2564134d9c
Laura <laura.gonzalez@guardian.co.uk>
2018-12-03 12:39:05 +0000

    fix typing

--- app/views/ViewHelpers.scala
+++ app/views/ViewHelpers.scala
@@ -1,6 +1,7 @@
 package views
 
 import play.api.mvc.RequestHeader
+import play.twirl.api.{HtmlFormat, Html}
 import admin.Settings
 import codecs.CirceDecoders._
 import io.circe.Printer
@@ -8,7 +9,7 @@ import io.circe.syntax._
 
 object ViewHelpers {
   def doNotTrack(implicit request: RequestHeader): Boolean = request.headers.get("DNT").contains("1")
-  def mapToDataset(data: Map[String, String]): Html =
+  def mapToDataset(data: Map[String, String]): Iterable[Html] =
     data.map {
       case (key, value) =>
         Html(s"""data-${HtmlFormat.escape(key.replaceAll("[^a-zA-Z0-9\\-]", ""))}="${HtmlFormat.escape(value)}" """)
