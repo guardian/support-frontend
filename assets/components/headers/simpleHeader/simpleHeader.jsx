34f4293dcc3c270b18bdf1850584c46c5466e04a
Laura <lawr@lauragonzalez.cc>
2018-12-12 12:36:04 +0000

    brekkypoints

--- assets/components/headers/simpleHeader/simpleHeader.jsx
+++ assets/components/headers/simpleHeader/simpleHeader.jsx
@@ -6,6 +6,7 @@ import React, { type Node } from 'react';
 
 import { type Option } from 'helpers/types/option';
 import SvgGuardianLogo from 'components/svgs/guardianLogo';
+import LeftMarginSection from 'components/leftMarginSection/leftMarginSection';
 
 import './simpleHeader.scss';
 
@@ -19,13 +20,15 @@ export default function SimpleHeader({ utility }: PropTypes) {
 
   return (
     <header className="component-simple-header">
-      <div className="component-simple-header__content">
-        {utility && <div className="component-simple-header__utility">{utility}</div>}
-        <a className="component-simple-header__link" href="https://www.theguardian.com">
-          <div className="accessibility-hint">The guardian logo</div>
-          <SvgGuardianLogo />
-        </a>
-      </div>
+      <LeftMarginSection>
+        <div className="component-simple-header__content">
+          {utility && <div className="component-simple-header__utility">{utility}</div>}
+          <a className="component-simple-header__link" href="https://www.theguardian.com">
+            <div className="accessibility-hint">The guardian logo</div>
+            <SvgGuardianLogo />
+          </a>
+        </div>
+      </LeftMarginSection>
     </header>
   );
 
