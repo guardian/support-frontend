56b339bc762f44ad34cbd00bf5d99268b804e7cd
Justin Pinner <justinpinner@gmail.com>
2018-12-21 11:47:26 +0000

    Change 'Introductory offer' to say 'Save 10%' instead

--- assets/pages/weekly-subscription-landing/components/weeklyForm.jsx
+++ assets/pages/weekly-subscription-landing/components/weeklyForm.jsx
@@ -43,7 +43,7 @@ export const billingPeriods = {
   },
   year: {
     title: 'Annually',
-    offer: 'Introductory offer',
+    offer: 'Save 10%',
     copy: (countryGroupId: CountryGroupId) => `${getPromotionPrice(countryGroupId, 'year', '10ANNUAL')} for 1 year, then standard rate (${getPrice(countryGroupId, 'year')} every year)`,
   },
 };
