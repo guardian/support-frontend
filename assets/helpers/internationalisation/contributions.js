cb1e2d36092ccc3feac01047cc9641028d7482bb
tomrf1 <tom.forbes@theguardian.com>
2019-01-08 10:11:48 +0000

    Remove US end-of-year campaign from landing page

--- assets/helpers/internationalisation/contributions.js
+++ assets/helpers/internationalisation/contributions.js
@@ -28,9 +28,8 @@ const countryGroupSpecificDetails: {
     contributeCopy: defaultContributeCopy,
   },
   UnitedStates: {
-    headerCopy: 'Invest in our independent journalism for 2019 and beyond',
-    headerClasses: 'header__us-campaign',
-    tickerJsonUrl: '/ticker.json',
+    headerCopy: defaultHeaderCopy,
+    headerClasses: defaultContributeCopy,
   },
   AUDCountries: {
     headerCopy: 'Help us deliver the independent journalism Australia needs',
