6de5a3f963d5e9b93783d57fe11cc060d1be7fe9
rupertbates <rupert.bates@guardian.co.uk>
2019-01-09 18:38:13 +0000

    Strong typing for Product classes

--- support-models/src/main/scala/com/gu/support/catalog/Catalog.scala
+++ support-models/src/main/scala/com/gu/support/catalog/Catalog.scala
@@ -10,9 +10,8 @@ case class Catalog(
 
 object Catalog {
   val supportedProducts: List[ProductId] = List(
-    "2c92a0fb4edd70c8014edeaa4ddb21e7", //DP
-    "2c92a0fe5aacfabe015ad24bf6e15ff6", //Contributor
-
+    DigitalPack.productId,
+    Contribution.productId,
   )
   implicit val encoder: Encoder[Catalog] = deriveEncoder
   implicit val decoder: Decoder[Catalog] = deriveDecoder[Catalog].prepare(mapFields)
--- support-models/src/main/scala/com/gu/support/catalog/Product.scala
+++ support-models/src/main/scala/com/gu/support/catalog/Product.scala
@@ -1,14 +1,64 @@
 package com.gu.support.catalog
 
+import com.gu.support.catalog.Catalog.mapFields
 import com.gu.support.encoding.Codec
 import com.gu.support.encoding.Codec._
+import com.gu.support.workers.Contribution
+import io.circe.{Decoder, DecodingFailure, Encoder, HCursor}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 
-case class Product(
-  id: ProductId,
-  name: String,
-  productRatePlans: List[ProductRatePlan]
-)
+sealed trait Product {
+  def id: ProductId
+
+  def name: String
+
+  def productRatePlans: List[ProductRatePlan]
+}
+
+case class DigitalPack(id: ProductId, name: String, productRatePlans: List[ProductRatePlan]) extends Product
+
+object DigitalPack {
+  val productId = "2c92a0fb4edd70c8014edeaa4ddb21e7"
+}
+
+case class Contribution(id: ProductId, name: String, productRatePlans: List[ProductRatePlan]) extends Product
+
+object Contribution {
+  val productId = "2c92a0fe5aacfabe015ad24bf6e15ff6"
+}
 
 object Product {
-  implicit val codec: Codec[Product] = deriveCodec
+  implicit val digitalPackDecoder: Decoder[DigitalPack] = deriveDecoder
+  implicit val contributionDecoder: Decoder[Contribution] = deriveDecoder
+
+  implicit val encoder: Encoder[Product] = deriveEncoder
+  implicit val decoder: Decoder[Product] = Decoder.instance {
+    cursor =>
+      val product = for {
+        id <- cursor.downField("id").as[String].toOption
+        decoder <- decoderForProduct(id)
+      } yield decoder.decodeJson(cursor.value)
+
+      product.getOrElse(Left(decodingFailure(cursor)))
+  }
+
+  private def decodingFailure(cursor: HCursor) = {
+    val id = cursor.downField("id").as[String].toOption.getOrElse("in this product")
+    DecodingFailure(
+      s"""
+        The product id $id does not have a corresponding case class.
+        Supported ids are defined in the companion objects of subclasses
+        of the Product trait.
+        Unsupported product ids should be filtered out by the Circe decoder of
+        the Catalog class
+        """, Nil
+    )
+  }
+
+
+  private def decoderForProduct(id: String) = id match {
+    case DigitalPack.productId => Some(digitalPackDecoder)
+    case Contribution.productId => Some(contributionDecoder)
+    case _ => None
+  }
 }
