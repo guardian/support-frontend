97f511fd8f396cba58a7cde372e05e61009b6889
Laura <lawr@lauragonzalez.cc>
2019-01-15 11:39:08 +0000

    Update digitalSubscriptionLandingActions.js

--- assets/pages/digital-subscription-landing/digitalSubscriptionLandingActions.js
+++ assets/pages/digital-subscription-landing/digitalSubscriptionLandingActions.js
@@ -1,10 +1,13 @@
 // @flow
 
 // ----- Imports ----- //
+import { type Dispatch } from 'redux';
 
 import { type DigitalBillingPeriod } from 'helpers/billingPeriods';
 import { sendTrackingEventsOnClick } from 'helpers/subscriptions';
 import { ProductPagePlanFormActionsFor } from 'components/productPage/productPagePlanForm/productPagePlanFormActions';
+import { getDigitalCheckout } from 'helpers/externalLinks';
+import { type State } from './digitalSubscriptionLandingReducer';
 
 
 // ----- Action Creators ----- //
@@ -12,8 +15,20 @@ import { ProductPagePlanFormActionsFor } from 'components/productPage/productPag
 const { setPlan } = ProductPagePlanFormActionsFor<DigitalBillingPeriod>('GuardianWeekly', 'GuardianWeekly');
 
 function redirectToDigitalPage() {
-  return () => {
-    const location = null;
+  return (dispatch: Dispatch<any>, getState: () => State) => {
+
+    const state = getState();
+
+    const { countryGroupId } = state.common.internationalisation;
+    const { referrerAcquisitionData, abParticipations, optimizeExperiments } = state.common;
+
+    const location = getDigitalCheckout(
+      referrerAcquisitionData,
+      countryGroupId,
+      null,
+      abParticipations,
+      optimizeExperiments,
+    );
 
     if (location) {
       sendTrackingEventsOnClick('main_cta_click', 'DigitalPack', null)();
