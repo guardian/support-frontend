dbb9e766d96d74ff2c815b0fcb4bae7bb6a03067
Laura <lawr@lauragonzalez.cc>
2019-01-21 11:30:34 +0000

    add nav

--- assets/components/headers/simpleHeader/simpleHeader.jsx
+++ assets/components/headers/simpleHeader/simpleHeader.jsx
@@ -6,7 +6,6 @@ import React, { type Node } from 'react';
 
 import { type Option } from 'helpers/types/option';
 import SvgGuardianLogo from 'components/svgs/guardianLogo';
-import LeftMarginSection from 'components/leftMarginSection/leftMarginSection';
 
 import './simpleHeader.scss';
 
@@ -14,21 +13,57 @@ export type PropTypes = {|
   utility: Option<Node>,
 |};
 
+const links = [
+  {
+    href: '/subscribe/digital',
+    text: 'Subscriptions',
+  },
+  {
+    href: '/subscribe/digital',
+    text: 'Bananas',
+  },
+  {
+    href: '/subscribe/digital',
+    text: 'Donuts',
+  },
+  {
+    href: '/subscribe/digital',
+    text: 'Pretzels',
+  },
+  {
+    href: '/subscribe/digital',
+    text: 'Pears',
+  },
+  {
+    href: '/subscribe/digital',
+    text: 'Apples',
+  },
+];
+
 // ----- Component ----- //
 
 export default function SimpleHeader({ utility }: PropTypes) {
 
   return (
     <header className="component-simple-header">
-      <LeftMarginSection>
-        <div className="component-simple-header__content">
+      <div className="component-simple-header__content">
+        <div className="component-simple-header__logo">
           <div className="component-simple-header__utility">{utility}</div>
           <a className="component-simple-header__link" href="https://www.theguardian.com">
-            <div className="accessibility-hint">The guardian logo</div>
+            <div className="accessibility-hint">The Guardian logo</div>
             <SvgGuardianLogo />
           </a>
         </div>
-      </LeftMarginSection>
+        <nav className="component-simple-header-nav">
+          <ul className="component-simple-header-nav__ul">
+            {links.map(({ href, text }) => (
+              <li className="component-simple-header-nav__li">
+                <a className="component-simple-header-nav__link" href={href}>{text}</a>
+              </li>
+              ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 
