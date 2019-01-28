fdfb4207976b758c7095daf10f6f6c71d71d72d4
rupertbates <rupert.bates@guardian.co.uk>
2019-01-28 18:12:55 +0000

    Tag an integration test

--- support-services/src/test/scala/com/gu/support/promotions/dynamo/DynamoServiceSpec.scala
+++ support-services/src/test/scala/com/gu/support/promotions/dynamo/DynamoServiceSpec.scala
@@ -3,10 +3,12 @@ package com.gu.support.promotions.dynamo
 import com.gu.support.config.PromotionsConfigProvider
 import com.gu.support.promotions.DynamoPromotionCollection
 import com.gu.support.config.Stages.PROD
+import com.gu.test.tags.annotations.IntegrationTest
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.{FlatSpec, Matchers}
 
+@IntegrationTest
 class DynamoServiceSpec extends FlatSpec with Matchers with LazyLogging {
 
   "DynamoService" should "work" ignore {
@@ -14,7 +16,7 @@ class DynamoServiceSpec extends FlatSpec with Matchers with LazyLogging {
     val items = new DynamoPromotionCollection(config.tables).all.toList
     val discounts = items.filter(_.discount.isDefined)
     logger.info(s"There are ${discounts.length} discounts")
-    discounts.filter(_.discount.get.durationMonths.isEmpty).foreach(p => logger.info(s"${p}"))
+    discounts.filter(_.discount.get.durationMonths.isEmpty).foreach(p => logger.info(s"$p"))
     val landingPages = items.filter(_.expires.isDefined)
     landingPages.nonEmpty should be(true)
   }
