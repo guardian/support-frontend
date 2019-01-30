277d4ae1209322ed5573948c7ce3714a43b5c72e
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2019-01-30 16:40:38 +0000

    put back accidental change.

--- assets/helpers/flashSale.js
+++ assets/helpers/flashSale.js
@@ -193,7 +193,7 @@ const Sales: Sale[] = [
     subscriptionProduct: 'PaperAndDigital',
     activeRegions: ['GBPCountries'],
     startTime: new Date(2019, 0, 4).getTime(), // 4 Jan 2019
-    endTime: new Date(2019, 0, 4).getTime(), // 3 Feb 2019 (to finish at 0:00 in the morning)
+    endTime: new Date(2019, 1, 4).getTime(), // 3 Feb 2019 (to finish at 0:00 in the morning)
     saleDetails: {
       GBPCountries: {
         promoCode: 'GCB56X',
@@ -223,7 +223,7 @@ const Sales: Sale[] = [
   {
     subscriptionProduct: 'Paper',
     activeRegions: ['GBPCountries'],
-    startTime: new Date(2019, 1, 4).getTime(), // 4 Jan 2019
+    startTime: new Date(2019, 0, 4).getTime(), // 4 Jan 2019
     endTime: new Date(2019, 1, 18).getTime(), // 17 Feb 2019 (to finish at 0:00 in the morning)
     duration: '3 months',
     saleDetails: {
