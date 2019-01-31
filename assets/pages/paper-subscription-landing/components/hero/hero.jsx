d0ad3f2803689ba49b45c0fd3ac33f33d1fce2b4
Sarah Laughton <45856485+SarahLaughton@users.noreply.github.com>
2019-01-31 17:06:08 +0000

    Update hero.jsx

--- assets/pages/paper-subscription-landing/components/hero/hero.jsx
+++ assets/pages/paper-subscription-landing/components/hero/hero.jsx
@@ -78,7 +78,7 @@ const SaleHeader = () => (
           <div className="sale-joy-of-print-badge">
             <span>Save up to</span>
             <span>52%</span>
-            <span>For 3 months</span>
+            <span>for a year</span>
           </div>
           <div className="sale-joy-of-print-graphic">
             <GridImage
