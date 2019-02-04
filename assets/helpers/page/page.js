c1d034392cf5c2b3628d5cab6542c1c08ddeccc3
Laura <lawr@lauragonzalez.cc>
2019-02-04 14:17:37 +0000

    update implementation
    Co-Authored-By: Jacob Winch <jacobwinch@users.noreply.github.com>

--- assets/helpers/page/page.js
+++ assets/helpers/page/page.js
@@ -132,7 +132,7 @@ function init<S, A>(
   const commonReducer = createCommonReducer(initialState);
 
   const store = createStore(
-    combineReducers({ page: pageReducer, common: commonReducer }),
+    combineReducers({ page: pageReducer ? pageReducer(initialState) : null, common: commonReducer }),
     storeEnhancer(thunk),
   );
 
