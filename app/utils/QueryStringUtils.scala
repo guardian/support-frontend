04fc625ba0859d8597e6dd7b7acf31fa1d5eef2f
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2019-02-04 14:09:11 +0000

    fix typo, use Random.nextBoolean()

--- app/utils/QueryStringUtils.scala
+++ app/utils/QueryStringUtils.scala
@@ -1,8 +1,10 @@
 package utils
 
+import scala.util.Random
+
 object QueryStringUtils {
   def addServerSideRenderingTestParameterQueryString(queryString: Map[String, Seq[String]]): Map[String, Seq[String]] = {
-    val ssrParamterValue = if (math.random() < 0.5) "on" else "off"
-    queryString ++ List("ssr" -> Seq(ssrParamterValue))
+    val ssrParameterValue = if (Random.nextBoolean()) "on" else "off"
+    queryString ++ List("ssr" -> Seq(ssrParameterValue))
   }
 }
