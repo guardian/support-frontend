0c8fcd6e8dccbc9117aabc1f7363956bf85a5071
Laura <lawr@lauragonzalez.cc>
2019-02-06 17:10:19 +0000

    fast sass

--- webpack.common.js
+++ webpack.common.js
@@ -114,7 +114,13 @@ module.exports = (cssFilename, outputFilename, minimizeCss) => ({
             },
           },
           {
-            loader: 'sass-loader',
+            loader: 'fast-sass-loader',
+            options: {
+              includePaths: [
+                path.resolve(__dirname, 'assets'),
+                path.resolve(__dirname),
+              ],
+            },
           },
         ],
       },
