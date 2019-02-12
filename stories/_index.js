1d897a67dfd6e18c79991dacec017fcd9838f685
Laura <lawr@lauragonzalez.cc>
2019-02-12 11:57:38 +0000

    prettier stories

---
+++ stories/_index.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+
+module.exports = () => {
+  require('stylesheets/skeleton/skeleton.scss');
+  require('./button.jsx');
+  require('./dialog.jsx');
+  require('./form.jsx');
+  require('./header.jsx');
+  require('./list.jsx');
+  require('./hero.jsx');
+};
