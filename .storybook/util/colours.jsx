cf48674028b020309e36596539e3a5a5ff66761e
Laura <lawr@lauragonzalez.cc>
2019-02-14 12:08:58 +0000

    make them look like design.gu but uglier to not outshine design.gu

--- .storybook/util/colours.jsx
+++ .storybook/util/colours.jsx
@@ -1,6 +1,8 @@
 import React, {type Node, Component} from 'react';
 
 import { colours as allColours } from 'stylesheets/gu-sass/colours.json';
+import WithState from './withState.jsx';
+import './colours.scss';
 
 const copyStringToClipboard = (str) => {
   const el = document.createElement('textarea');
@@ -16,20 +18,14 @@ const copyStringToClipboard = (str) => {
   }
 }
 
-const getCategories = (colours) => {
-  const categories = {
-    brand: ['brand-dark', 'brand-main', 'brand-pastel'],
-    neutrals: ['brightness-7', 'brightness-20', 'brightness-46', 'brightness-60', 'brightness-86', 'brightness-93', 'brightness-97', 'brightness-100'],
-    state: ['highlight-main', 'highlight-dark', 'green-main', 'green-dark', 'error', 'success'],
-    news: ['news-dark', 'news-main', 'news-bright', 'news-pastel', 'news-faded'],
-    opinion: ['opinion-dark', 'opinion-main', 'opinion-bright', 'opinion-pastel', 'opinion-faded'],
-    sport: ['sport-dark', 'sport-main', 'sport-bright', 'sport-pastel', 'sport-faded'],
-    culture: ['culture-dark', 'culture-main', 'culture-bright', 'culture-pastel', 'culture-faded'],
-    lifestyle: ['lifestyle-dark', 'lifestyle-main', 'lifestyle-bright', 'lifestyle-pastel', 'lifestyle-faded'],
-  };
-  
+const colourIsBright = colour => (
+  parseInt(colour.substring(1, 3), 16) +
+  parseInt(colour.substring(3, 5), 16) +
+  parseInt(colour.substring(5, 7), 16)
+) > (128 * 3);
+
+const addMissingColoursToCategories = (categories, colours) => {
   const listed = Object.values(categories).reduce((prev, cur)=>[...prev, ...cur],[]);
-    
   return {
     ...categories,
     others: Object.keys(colours)
@@ -39,50 +35,37 @@ const getCategories = (colours) => {
 }
 
 const Colour = ({name, colour, copyHex}) => (
-  colour && <button 
-  onClick ={() => {
-    copyStringToClipboard(copyHex?colour:`gu-colour(${name})`)
-  }}
-  style={{
-    background: colour,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    width: '10rem',
-    fontFamily: 'monospace',
-    height: '10rem',
-    borderRadius:'100%',
-    border: `1px solid ${allColours['brightness-93']}`,
-  }}>
-    <div style={{
-      color: parseInt(colour.substring(1,3), 16) > 126 ? 'black' : 'white',
-      fontSize: '1em',
-    }}>
-      <strong>{colour}</strong>
-      <div>{name}</div>
-    </div>
-  </button>
+  <WithState initialState={{clicked: false}}>
+    {({clicked}, setState) =>
+      <button 
+        onClick ={() => {
+          copyStringToClipboard(copyHex?colour:`gu-colour(${name})`)
+          setState({clicked: true})
+        }}
+        onMouseLeave={()=>{
+          setState({clicked: false})
+        }}
+        className="story-colours__colour"
+        data-clicked={clicked}
+        style={{
+          background: colour,
+          color: colourIsBright(colour) ? 'black' : 'white',
+        }}
+      >
+        {colour}
+        <div>{clicked?'copied':name}</div>
+      </button>
+    }
+  </WithState>
 );
 
-const Colours = ({copyHex}) => (
+const Colours = ({copyHex, categories}) => (
   <div>
     {
-      Object.entries(getCategories(allColours)).map(([name, colours])=>(
-        <div style={{
-          padding: '2rem'
-        }}>
-          <h2 style={{
-            marginBottom: '1rem',
-            fontFamily: '"Guardian Headline", Georgia, serif',
-            fontWeight: 700,
-            fontSize: '1.2rem'
-          }}>{name}</h2>
-          <div style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(auto-fit, 10rem)',
-            gap: '1rem',
-          }}>
+      Object.entries(addMissingColoursToCategories(categories, allColours)).map(([name, colours])=>(
+        <div className="story-colours">
+          <h2 className="story-colours__title">{name}</h2>
+          <div className="story-colours__row">
           {
             colours.map(colour => (
               <Colour copyHex={copyHex} name={colour} colour={allColours[colour]} />
--- stories/colours.jsx
+++ stories/colours.jsx
@@ -7,11 +7,22 @@ import { withKnobs, radios } from '@storybook/addon-knobs';
 
 import Colours from '../.storybook/util/colours';
 
+const categories = {
+  brand: ['brand-dark', 'brand-main', 'brand-pastel'],
+  neutrals: ['brightness-7', 'brightness-20', 'brightness-46', 'brightness-60', 'brightness-86', 'brightness-93', 'brightness-97', 'brightness-100'],
+  state: ['highlight-main', 'highlight-dark', 'green-main', 'green-dark', 'error', 'success'],
+  news: ['news-dark', 'news-main', 'news-bright', 'news-pastel', 'news-faded'],
+  opinion: ['opinion-dark', 'opinion-main', 'opinion-bright', 'opinion-pastel', 'opinion-faded'],
+  sport: ['sport-dark', 'sport-main', 'sport-bright', 'sport-pastel', 'sport-faded'],
+  culture: ['culture-dark', 'culture-main', 'culture-bright', 'culture-pastel', 'culture-faded'],
+  lifestyle: ['lifestyle-dark', 'lifestyle-main', 'lifestyle-bright', 'lifestyle-pastel', 'lifestyle-faded'],
+};
+
 const stories = storiesOf('Colour palette', module)
   .addDecorator(withKnobs);
 
 const copyHexKnob = () => radios('Copy on click', ['variable', 'hex'], 'variable');
 
 stories.add('Colours', () => (
-  <Colours copyHex={copyHexKnob() === 'hex'} />
+  <Colours categories={categories} copyHex={copyHexKnob() === 'hex'} />
 ));
