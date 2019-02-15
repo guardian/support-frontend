49adb49c0bda5c2de9cff479b6b628435ab6735a
Laura <lawr@lauragonzalez.cc>
2019-02-15 14:27:54 +0000

    use test users

--- app/controllers/DigitalSubscription.scala
+++ app/controllers/DigitalSubscription.scala
@@ -62,13 +62,15 @@ class DigitalSubscription(
     val description = stringsConfig.digitalPackLandingDescription
     val canonicalLink = Some(buildCanonicalDigitalSubscriptionLink("uk"))
     val promoCode = request.queryString.get("promoCode").flatMap(_.headOption)
+    val testUsername = request.cookies.get("_test_username")
+    val isTestUser = testUsers.isTestUser(testUsername.map(_.value))
     val hrefLangLinks = Map(
       "en-us" -> buildCanonicalDigitalSubscriptionLink("us"),
       "en-gb" -> buildCanonicalDigitalSubscriptionLink("uk"),
       "en-au" -> buildCanonicalDigitalSubscriptionLink("au"),
       "en" -> buildCanonicalDigitalSubscriptionLink("int")
     )
-    val productPrices = priceSummaryServiceProvider.forUser(false).getPrices(DigitalPack, promoCode)
+    val productPrices = priceSummaryServiceProvider.forUser(isTestUser).getPrices(DigitalPack, promoCode)
 
     Ok(views.html.main(
       title, id, js, css, description, canonicalLink, hrefLangLinks, productPrices = Some(productPrices)
