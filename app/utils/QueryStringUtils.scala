34a2f05ae7d32820299b3a66409a5f46411bcf9f
damien-shaw-guardian <damien.shaw.freelancer@guardian.co.uk>
2019-02-19 16:04:15 +0000

    fix: added variant to query string
    fix: added variant to query string - when the query string is randomly created, it will now be created with “control” or “variant”

--- app/utils/QueryStringUtils.scala
+++ app/utils/QueryStringUtils.scala
@@ -4,7 +4,7 @@ import scala.util.Random
 object QueryStringUtils {
   def addServerSideRenderingTestParameterQueryString(queryString: Map[String, Seq[String]]): Map[String, Seq[String]] = {
     val ssrParameterValue = if (Random.nextBoolean()) "on" else "off"
-    val formDesignTestParameterValue = if (Random.nextBoolean()) "on" else "off"
+    val formDesignTestParameterValue = if (Random.nextBoolean()) "control" else "variant"
    queryString + ("ssrTwo" -> Seq(ssrParameterValue)) + ("formDesignTest" -> Seq(formDesignTestParameterValue))
   }
 }
