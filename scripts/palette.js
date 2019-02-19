32313c2c2b52cfff2e18b61ca57b739a9dbdf11d
Laura <lawr@lauragonzalez.cc>
2019-02-19 15:45:16 +0000

    document

--- scripts/palette.js
+++ scripts/palette.js
@@ -1,3 +1,9 @@
+/*
+Given the pasteup palette,
+this function flattens all colours into a
+child free map where the keys are named as
+parent-child.
+*/
 const flatten = (obj, prefixes = []) => Object.entries(obj)
   .reduce((prev, [key, val]) => ((typeof val === 'string') ?
     {
