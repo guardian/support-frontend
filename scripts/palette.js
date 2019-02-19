1b320088e17b55a7a4148ac8ddfe9797ac029c38
Laura <lawr@lauragonzalez.cc>
2019-02-19 15:31:52 +0000

    unify colours

--- scripts/palette.js
+++ scripts/palette.js
@@ -2,7 +2,7 @@ const flatten = (obj, prefixes = []) => Object.entries(obj)
   .reduce((prev, [key, val]) => ((typeof val === 'string') ?
     {
       ...prev,
-      [[...prefixes, key].join('-')]: val,
+      [[...prefixes, key].join('-').replace('neutral-', 'brightness-')]: val,
     }
     : {
       ...prev,
--- stories/colours.jsx
+++ stories/colours.jsx
@@ -9,8 +9,10 @@ import Colours from '../.storybook/util/colours';
 
 const categories = {
   brand: ['brand-dark', 'brand-main', 'brand-pastel'],
-  neutrals: ['neutral-7', 'neutral-20', 'neutral-46', 'neutral-60', 'neutral-86', 'neutral-93', 'neutral-97', 'neutral-100'],
-  state: ['highlight-main', 'highlight-dark', 'green-main', 'green-dark', 'error', 'success'],
+  neutrals: ['brightness-7', 'brightness-20', 'brightness-46', 'brightness-60', 'brightness-86', 'brightness-93', 'brightness-97', 'brightness-100'],
+  highlight: ['highlight-main', 'highlight-dark'],
+  green: ['green-main', 'green-dark'],
+  state: ['state-error', 'state-success'],
   news: ['news-dark', 'news-main', 'news-bright', 'news-pastel', 'news-faded'],
   opinion: ['opinion-dark', 'opinion-main', 'opinion-bright', 'opinion-pastel', 'opinion-faded'],
   sport: ['sport-dark', 'sport-main', 'sport-bright', 'sport-pastel', 'sport-faded'],
