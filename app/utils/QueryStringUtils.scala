1538859d0199a607997aa05c629f846e6bcdde38
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2019-02-21 09:35:36 +0000

    put test to 100%

--- app/utils/QueryStringUtils.scala
+++ app/utils/QueryStringUtils.scala
@@ -3,7 +3,6 @@ package utils
 
 object QueryStringUtils {
   def addFormDesignTestParameterQueryString(queryString: Map[String, Seq[String]]): Map[String, Seq[String]] = {
-    val formDesignTestParameterValue = if (Random.nextFloat <= 0.2) "control" else "variant"
-   queryString + ("formDesignTest" -> Seq(formDesignTestParameterValue))
+   queryString + ("formDesignTest" -> Seq("variant"))
   }
 }
