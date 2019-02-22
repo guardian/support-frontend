f6e7d2a0b80c068b33315884dc0370b5a8ca8569
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2019-02-22 10:13:28 +0000

    Revert "When creating digital pack subscriptions, get rate plan id from the Product mapping"

--- src/main/scala/com/gu/zuora/ProductSubscriptionBuilders.scala
+++ src/main/scala/com/gu/zuora/ProductSubscriptionBuilders.scala
@@ -1,33 +1,14 @@
 package com.gu.zuora
 
-import com.gu.config.Configuration
 import com.gu.i18n.Country
-import com.gu.support.catalog
-import com.gu.support.catalog.{Product, ProductRatePlan, ProductRatePlanId}
-import com.gu.support.config.{TouchPointEnvironments, ZuoraConfig}
+import com.gu.support.catalog.ProductRatePlanId
+import com.gu.support.config.ZuoraConfig
 import com.gu.support.promotions.{PromoCode, PromotionService}
-import com.gu.support.workers.exceptions.CatalogDataNotFoundException
-import com.gu.support.workers.{Contribution, DigitalPack, ProductType}
+import com.gu.support.workers.{Contribution, DigitalPack}
 import com.gu.support.zuora.api._
 import org.joda.time.{DateTimeZone, LocalDate}
 
-import scala.util.{Failure, Success, Try}
-
 object ProductSubscriptionBuilders {
-
-  def getProductRatePlanId[PT <: ProductType, P <: Product](product: P, productType: PT): ProductRatePlanId = {
-    val touchpointEnvironment = TouchPointEnvironments.fromStage(Configuration.stage)
-    def getRatePlans[T <: Product](product: T): Seq[ProductRatePlan[Product]] = product.ratePlans.getOrElse(touchpointEnvironment, Nil)
-
-    val ratePlans: Seq[ProductRatePlan[Product]] = getRatePlans(catalog.DigitalPack)
-
-    val maybeProductRatePlanId: Option[ProductRatePlanId] = ratePlans.find(p => p.billingPeriod == productType.billingPeriod) map (_.id)
-
-    Try(maybeProductRatePlanId.get) match {
-      case Success(value) => value
-      case Failure(e) => throw new CatalogDataNotFoundException(s"RatePlanId not found for ${productType.toString}", e)
-    }
-  }
 
   implicit class ContributionSubscriptionBuilder(val contribution: Contribution) extends ProductSubscriptionBuilder {
     def build(config: ZuoraConfig): SubscriptionData = {
@@ -45,19 +26,19 @@ object ProductSubscriptionBuilders {
 
   implicit class DigitalPackSubscriptionBuilder(val digitalPack: DigitalPack) extends ProductSubscriptionBuilder {
     def build(config: ZuoraConfig, country: Country, maybePromoCode: Option[PromoCode], promotionService: PromotionService): SubscriptionData = {
-
       val contractEffectiveDate = LocalDate.now(DateTimeZone.UTC)
       val contractAcceptanceDate = contractEffectiveDate
         .plusDays(config.digitalPack.defaultFreeTrialPeriod)
         .plusDays(config.digitalPack.paymentGracePeriod)
 
-      val productRatePlanId = getProductRatePlanId(catalog.DigitalPack, digitalPack)
+      val productRatePlanId = config.digitalPackRatePlan(digitalPack.billingPeriod)
 
       val subscriptionData = buildProductSubscription(
         productRatePlanId,
         contractAcceptanceDate = contractAcceptanceDate,
         contractEffectiveDate = contractEffectiveDate
       )
+
       maybePromoCode
         .map(promotionService.applyPromotion(_, country, productRatePlanId, subscriptionData, isRenewal = false))
         .getOrElse(subscriptionData)
