2a6fbf08ff85ee3cb0c5cc2b671d23c544a56dd0
rupertbates <rupert.bates@guardian.co.uk>
2019-02-27 17:36:46 +0000

    Pass Address objects back to the client

--- support-frontend/app/controllers/GetAddress.scala
+++ support-frontend/app/controllers/GetAddress.scala
@@ -4,21 +4,21 @@ import actions.CustomActionBuilders
 import com.gu.monitoring.SafeLogger
 import com.gu.monitoring.SafeLogger._
 import com.gu.support.getaddressio.GetAddressIOService
-import play.api.libs.json.Json
+import io.circe.syntax._
+import play.api.libs.circe.Circe
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class GetAddress(
         components: ControllerComponents,
         getAddressService: GetAddressIOService,
         actionRefiners: CustomActionBuilders
-      ) extends AbstractController(components) {
+      ) extends AbstractController(components) with Circe {
   import actionRefiners._
   def findAddress(postCode: String): Action[AnyContent] = NoCacheAction().async { implicit request =>
     getAddressService.find(postCode).map { result =>
-      // Capital A 'Addresses' is for compatibility with the https://api.getaddress.io/v2/uk/ response,
-      // should a client want not to proxy via this server.
-      Ok(Json.obj("Addresses" -> result.Addresses))
+      Ok(result.asJson)
     } recover {
       case error if error.getMessage == "Bad Request" || error.getMessage == "Not Found" =>
         BadRequest //The postcode was invalid
