036d9452c2d4b537345666fbf2d7a2d27dbccf33
rupertbates <rupert.bates@guardian.co.uk>
2019-02-27 17:19:13 +0000

    Return Address objects from the postcode lookup

--- support-services/src/main/scala/com/gu/support/getaddressio/GetAddressIOService.scala
+++ support-services/src/main/scala/com/gu/support/getaddressio/GetAddressIOService.scala
@@ -1,23 +1,27 @@
 package com.gu.support.getaddressio
 
 import com.gu.helpers.WebServiceHelper
+import com.gu.i18n.{Country, PostalCode}
 import com.gu.okhttp.RequestRunners
 import com.gu.okhttp.RequestRunners.FutureHttpClient
 import com.gu.support.config.GetAddressIOConfig
 import com.typesafe.scalalogging.LazyLogging
 import com.gu.support.encoding.Codec
 import com.gu.support.encoding.Codec._
+import com.gu.support.workers.Address
 import okhttp3.{Request, Response}
+import com.gu.support.encoding.StringExtensions._
 
 import scala.concurrent.{ExecutionContext, Future}
 
 
-case class FindAddressResult(Latitude: Float, Longitude: Float, Addresses: Seq[String])
+case class FindAddressAPIResult(Latitude: Float, Longitude: Float, Addresses: List[String])
 
-object FindAddressResult {
-  implicit val codec: Codec[FindAddressResult] = deriveCodec
+object FindAddressAPIResult {
+  implicit val codec: Codec[FindAddressAPIResult] = deriveCodec
 }
-case class FindAddressResultError(Message: String) extends RuntimeException(s"$Message")
+
+case class FindAddressResultError(Message: String) extends Throwable(s"$Message")
 
 object FindAddressResultError {
   implicit val codec: Codec[FindAddressResultError] = deriveCodec
@@ -29,6 +33,20 @@ class GetAddressIOService(config: GetAddressIOConfig, client: FutureHttpClient)(
   override val wsUrl: String = config.apiUrl
   override val httpClient: FutureHttpClient = client
 
-  def find(postcode: String): Future[FindAddressResult] =
-    get[FindAddressResult](postcode, Map("api-key" -> config.apiKey)).transform(x => x, y => y)
+  def find(postalCode: String): Future[List[Address]] =
+    get[FindAddressAPIResult](postalCode, Map("api-key" -> config.apiKey))
+      .map(_.Addresses.map(s => addressFromString(postalCode, s)))
+
+
+  def addressFromString(postalCode: String, addressString: String): Address = {
+    val splitAddress = addressString.split(",").map(_.trim)
+    Address(
+      splitAddress(0).toOption,
+      splitAddress(1).toOption,
+      splitAddress(5).toOption,
+      splitAddress(6).toOption,
+      Some(postalCode),
+      Country.UK
+    )
+  }
 }
--- support-services/src/test/scala/com/gu/support/getaddressio/GetAddressIOServiceSpec.scala
+++ support-services/src/test/scala/com/gu/support/getaddressio/GetAddressIOServiceSpec.scala
@@ -1,5 +1,6 @@
 package com.gu.support.getaddressio
 
+import com.gu.i18n.Country
 import com.gu.okhttp.RequestRunners
 import com.gu.support.config.GetAddressIOConfig
 import com.gu.test.tags.annotations.IntegrationTest
@@ -8,8 +9,23 @@ import org.scalatest.{AsyncFlatSpec, Matchers}
 
 @IntegrationTest
 class GetAddressIOServiceSpec extends AsyncFlatSpec with Matchers {
+  val service = new GetAddressIOService(GetAddressIOConfig.fromConfig(ConfigFactory.load()), RequestRunners.futureRunner)
+
   "GetAddressService" should "be able to find a postcode" in {
-    val service = new GetAddressIOService(GetAddressIOConfig.fromConfig(ConfigFactory.load()), RequestRunners.futureRunner)
-    service.find("N19GU").map(_.Addresses.nonEmpty shouldBe true)
+    service.find("N19GU").map {
+      result =>
+        result.nonEmpty shouldBe true
+        val address = result.head
+        address.postCode shouldBe Some("N19GU")
+        address.lineOne shouldBe Some("The Guardian")
+        address.lineTwo shouldBe Some("Kings Place")
+        address.city shouldBe Some("London")
+        address.state shouldBe  None
+        address.country shouldBe Country.UK
+    }
+
+    service.find("NN13ER").map {
+      result => result.head.state shouldBe Some("Northamptonshire")
+    }
   }
 }
