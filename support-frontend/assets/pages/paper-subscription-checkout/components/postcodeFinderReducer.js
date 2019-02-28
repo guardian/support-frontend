b1ab9e815c6c33c80be205e5bccb283e2311ba3b
Laura <laura.gonzalez@guardian.co.uk>
2019-02-28 16:27:23 +0000

    renamez

--- support-frontend/assets/pages/paper-subscription-checkout/components/postcodeFinderReducer.js
+++ support-frontend/assets/pages/paper-subscription-checkout/components/postcodeFinderReducer.js
@@ -15,17 +15,17 @@ export type PostcodeFinderState = {|
 |}
 
 export type PostcodeFinderActions =
-  {|type: 'FILL_POSTCODEFINDER_RESULTS', results: Address[]|} |
-  {|type: 'SET_POSTCODEFINDER_ERROR', error: Option<string>|} |
-  {|type: 'START_POSTCODEFINDER_FETCH_RESULTS' |} |
-  {|type: 'SET_POSTCODEFINDER_POSTCODE', postcode: Option<string>|};
+  {|type: 'FILL_POSTCODE_FINDER_RESULTS', results: Address[]|} |
+  {|type: 'SET_POSTCODE_FINDER_ERROR', error: Option<string>|} |
+  {|type: 'START_POSTCODE_FINDER_FETCH_RESULTS' |} |
+  {|type: 'SET_POSTCODE_FINDER_POSTCODE', postcode: Option<string>|};
 
 export const postcodeFinderActionCreators = {
-  setPostcode: (postcode: string) => ({ type: 'SET_POSTCODEFINDER_POSTCODE', postcode }),
+  setPostcode: (postcode: string) => ({ type: 'SET_POSTCODE_FINDER_POSTCODE', postcode }),
   fetchResults: () => (dispatch: Dispatch<PostcodeFinderActions>) => {
-    dispatch({ type: 'START_POSTCODEFINDER_FETCH_RESULTS' });
+    dispatch({ type: 'START_POSTCODE_FINDER_FETCH_RESULTS' });
     setTimeout(() => {
-      dispatch({ type: 'SET_POSTCODEFINDER_ERROR', error: 'This failed (predictably) because it\'s not linked to the backend yet' });
+      dispatch({ type: 'SET_POSTCODE_FINDER_ERROR', error: 'This failed (predictably) because it\'s not linked to the backend yet' });
     }, 2000);
   },
 };
@@ -45,21 +45,21 @@ const postcodeFinderReducer = (
 ): PostcodeFinderState => {
 
   switch (action.type) {
-    case 'FILL_POSTCODEFINDER_RESULTS':
+    case 'FILL_POSTCODE_FINDER_RESULTS':
       return {
         ...state,
       };
-    case 'SET_POSTCODEFINDER_POSTCODE':
+    case 'SET_POSTCODE_FINDER_POSTCODE':
       return {
         ...state,
         postcode: action.postcode,
       };
-    case 'SET_POSTCODEFINDER_ERROR':
+    case 'SET_POSTCODE_FINDER_ERROR':
       return {
         ...state,
         error: action.error,
       };
-    case 'START_POSTCODEFINDER_FETCH_RESULTS':
+    case 'START_POSTCODE_FINDER_FETCH_RESULTS':
       return {
         ...initialState,
         postcode: state.postcode,
