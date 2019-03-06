0705bb9211a2f5150c929eb848cde8166689b563
Jesse Yuen <jesse.yuen.casual@theguardian.com>
2019-03-06 09:09:03 +0000

    Addding new headers to Storybook wip

--- support-frontend/stories/layout.jsx
+++ support-frontend/stories/layout.jsx
@@ -74,11 +74,22 @@ stories.add('Content', () => (
 stories.add('Hero', () => (
   <ProductPageHero
     overheading="This overheading is the H1"
-    heading="Lemon drizzle waffle confit"
+    heading="Lemon drizzle waffle confit lorem ipsu dolet"
     appearance="feature"
     content="Hero blocks showcase a product and really help set the mood of a page. They work great right under the header!"
   >
-    <div style={{ height: '300px' }} />
+    <div />
+  </ProductPageHero>
+));
+
+stories.add('Hero (campaign)', () => (
+  <ProductPageHero
+    overheading="This overheading is the H1"
+    heading="Lemon drizzle waffle confit lorem ipsu dolet"
+    appearance="campaign"
+    content="Hero blocks showcase a product and really help set the mood of a page. They work great right under the header!"
+  >
+    <div />
   </ProductPageHero>
 ));
 
