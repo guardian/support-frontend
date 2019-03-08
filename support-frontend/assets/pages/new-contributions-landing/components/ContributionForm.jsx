103f7e41d0d81b7b3d7a98bd13255f28c390f778
Tom Forbes <tom.forbes@theguardian.com>
2019-03-08 10:02:51 +0000

    Enable ticker for AU (#1597)
    * Enable ticker for AU
    
    * Move ticker to above form
    
    * Update ticker styling
    
    * fix styling
    
    * Add form message
    
    * Remove form message from the actual form
    
    * Style the message
    
    * Use headline font, better spacing
    
    * Only allow one-off in aus
    
    * use query param
    
    * Frontline campaign param

--- support-frontend/assets/pages/new-contributions-landing/components/ContributionForm.jsx
+++ support-frontend/assets/pages/new-contributions-landing/components/ContributionForm.jsx
@@ -78,7 +78,6 @@ type PropTypes = {|
   isTestUser: boolean,
   country: IsoCountry,
   stripePaymentRequestButtonMethod: StripePaymentRequestButtonMethod,
-  message: ?React$Element<string>,
 |};
 
 // We only want to use the user state value if the form state value has not been changed since it was initialised,
@@ -210,7 +209,6 @@ function ContributionForm(props: PropTypes) {
   return (
     <form onSubmit={onSubmit(props)} className={classNameWithModifiers('form', ['contribution'])} noValidate>
       <div>
-        {props.message ? <div className={classNameWithModifiers('form', ['message'])}>{props.message}</div> : null}
         <ContributionTypeTabs />
         <NewContributionAmount
           checkOtherAmount={checkAmount}
