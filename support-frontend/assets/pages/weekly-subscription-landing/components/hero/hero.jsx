d6af97799470ea3a87795dc7333398c317a98ac7
Jesse Yuen <jesse.yuen.casual@theguardian.com>
2019-03-11 16:57:58 +0000

    Move banner out of main page and into compnents

---
+++ support-frontend/assets/pages/weekly-subscription-landing/components/hero/hero.jsx
@@ -0,0 +1,84 @@
+// @flow
+
+// ----- Imports ----- //
+
+import React from 'react';
+import GridPicture from 'components/gridPicture/gridPicture';
+import AnchorButton from 'components/button/anchorButton';
+import SvgChevron from 'components/svgs/chevron';
+import GridImage from 'components/gridImage/gridImage';
+import { sendTrackingEventsOnClick } from 'helpers/subscriptions';
+import ProductPagehero from 'components/productPage/productPageHero/productPageHero';
+
+import './weeklyCampaign.scss';
+
+const DefaultHeader = () => (
+  <header>
+    <ProductPagehero
+      appearance="feature"
+      overheading="Guardian Weekly subscriptions"
+      heading="Get a clearer, global perspective on the issues that matter, in one magazine."
+      modifierClasses={['weekly']}
+      content={<AnchorButton aria-label={null} onClick={sendTrackingEventsOnClick('options_cta_click', 'GuardianWeekly', null)} icon={<SvgChevron />} href="#subscribe">See Subscription options</AnchorButton>}
+      hasCampaign={false}
+    >
+      <GridPicture
+        sources={[
+            {
+              gridId: 'weeklyLandingHero',
+              srcSizes: [500, 1000],
+              imgType: 'png',
+              sizes: '100vw',
+              media: '(max-width: 739px)',
+            },
+            {
+              gridId: 'weeklyLandingHero',
+              srcSizes: [1000, 2000],
+              imgType: 'png',
+              sizes: '(min-width: 1000px) 2000px, 1000px',
+              media: '(min-width: 740px)',
+            },
+          ]}
+        fallback="weeklyLandingHero"
+        fallbackSize={1000}
+        altText=""
+        fallbackImgType="png"
+      />
+    </ProductPagehero>
+  </header>
+);
+
+const SaleHeader = () => (
+  <ProductPagehero
+    appearance="campaign"
+    overheading="Guardian Weekly subscriptions"
+    heading="The Guardian's essential news magazine"
+    modifierClasses={['weekly-campaign']}
+    content={<AnchorButton aria-label={null} onClick={sendTrackingEventsOnClick('options_cta_click', 'GuardianWeekly', null)} icon={<SvgChevron />} href="#subscribe">See Subscription options</AnchorButton>}
+    hasCampaign
+  >
+
+    <div className="weekly-campaign-hero">
+      <div className="weekly-campaign-hero__copy">
+        <h2>Think globally.<br />Read Weekly.</h2>
+        <p>An independent perspective on the issues affecting the world.</p>
+      </div>
+
+      <div className="weekly-campaign-hero__graphic">
+        <GridImage
+          gridId="weeklyCampaignHeroImg"
+          srcSizes={[1000, 1358]}
+          sizes="(max-width: 740px) 1000px, 1358px"
+          imgType="png"
+          altText="A mobile device"
+        />
+      </div>
+
+    </div>
+
+
+  </ProductPagehero>
+);
+
+
+export { DefaultHeader, SaleHeader, HeroPicture };
