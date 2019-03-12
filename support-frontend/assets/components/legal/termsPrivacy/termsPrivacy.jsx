bcd8c7633d98c45e5c3fe3113efb22214ef3452e
tomrf1 <tom.forbes@theguardian.com>
2019-03-12 12:32:57 +0000

    Custom T&Cs for Frontline campaign

--- support-frontend/assets/components/legal/termsPrivacy/termsPrivacy.jsx
+++ support-frontend/assets/components/legal/termsPrivacy/termsPrivacy.jsx
@@ -8,6 +8,7 @@ import { privacyLink, contributionsTermsLinks } from 'helpers/legal';
 import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
 
 import type { ContributionType } from 'helpers/contributions';
+import { isFrontlineCampaign } from 'helpers/url';
 
 // ---- Types ----- //
 
@@ -22,6 +23,21 @@ type PropTypes = {|
 function TermsPrivacy(props: PropTypes) {
   const terms = <a href={contributionsTermsLinks[props.countryGroupId]}>Terms and Conditions</a>;
   const privacy = <a href={privacyLink}>Privacy Policy</a>;
+
+  if (isFrontlineCampaign()) {
+    return (
+      <div className="component-terms-privacy">
+        By proceeding, you’re agreeing to our Terms and Conditions.
+        If we hit our goal of $100,000, The Guardian will allocate this amount to its core operations
+        which will help fund the completion of the ‘The Frontline: Australia and the climate emergency’ series,
+        including editing, reporting, graphics and new commissions. If we exceed our fundraising goal,
+        The Guardian will allocate the additional funds towards its core operations that fund our in-depth
+        environmental reporting. If we fall short of the goal, The Guardian will allocate the funds for a scaled
+        back project on climate change. Contributions will not be returned. Your contribution is also governed by
+        our standard contribution terms and conditions.
+      </div>
+    );
+  }
 
   return (
     <div className="component-terms-privacy">
