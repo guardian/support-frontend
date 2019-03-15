53289bdec60f925f70f4396680c0113f05769581
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-03-15 16:03:01 +0000

    Set tracking consent cookie on acceptance

--- support-frontend/assets/helpers/tracking/thirdPartyTrackingConsent.js
+++ support-frontend/assets/helpers/tracking/thirdPartyTrackingConsent.js
@@ -1,8 +1,10 @@
 // @flow
 
-import { get as getCookie } from '../cookie';
+import { get as getCookie, set as setCookie } from '../cookie';
 
 const ConsentCookieName = 'GU_TK';
+const DaysToLive = 30 * 18;
+
 const OptedIn: 'OptedIn' = 'OptedIn';
 const OptedOut: 'OptedOut' = 'OptedOut';
 const Unset: 'Unset' = 'Unset';
@@ -18,4 +20,9 @@ const getTrackingConsent = (): ThirdPartyTrackingConsent => {
   return Unset;
 };
 
-export { getTrackingConsent, OptedIn, OptedOut, Unset, ConsentCookieName };
+const setTrackingConsent = (optedIn: boolean) => {
+  const cookie = [optedIn ? '1' : '0', Date.now()].join('.');
+  setCookie(ConsentCookieName, cookie, DaysToLive);
+};
+
+export { getTrackingConsent, setTrackingConsent, OptedIn, OptedOut, Unset, ConsentCookieName };
