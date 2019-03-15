d511befdd70e1a77f991c4b7eb8c129565322f2f
Joshua <joshuathomaslieberman@gmail.com>
2019-03-15 16:08:23 +0000

    made fontLoader a local helper lib rather than node package bc it was not getting transpiled, causing errors on IE11

---
+++ support-frontend/assets/helpers/fontLoader.js
@@ -0,0 +1,47 @@
+// @flow
+
+const fetchFonts = (window: Object, document: Document): void => {
+  const head: null | HTMLHeadElement = document.querySelector('head');
+
+  const useFont = (font: Object): void => {
+    if (head && font.css) {
+      const style: HTMLStyleElement = document.createElement('style');
+      style.innerHTML = font.css;
+      head.appendChild(style);
+    }
+  };
+
+  const loadFonts = (): void => {
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://theguardian.com/font-loader';
+    // add iframe and wait for message
+    iframe.style.display = 'none';
+    window.addEventListener('message', (e: MessageEvent) => {
+      if (
+        e &&
+        e.data &&
+        e.data.name &&
+        e.data.name === 'guardianFonts' &&
+        e.data.fonts &&
+        e.source === iframe.contentWindow
+      ) {
+        if (Array.isArray(e.data.fonts)) {
+          e.data.fonts.forEach(useFont);
+        }
+      }
+    });
+    if (document.body) {
+      document.body.appendChild(iframe);
+    }
+  };
+
+  if (document.readyState === 'loading') {
+    // Loading hasn't finished yet
+    document.addEventListener('DOMContentLoaded', loadFonts);
+  } else {
+    // DOMContentLoaded has already fired
+    loadFonts();
+  }
+};
+
+export default fetchFonts;
