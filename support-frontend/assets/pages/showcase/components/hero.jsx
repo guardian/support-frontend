dba51f52c720f451034e1685422644421a994857
Jesse Yuen <jesse.yuen.casual@theguardian.com>
2019-03-19 11:57:07 +0000

    Removed js state toggle and rever to original component structure

--- support-frontend/assets/pages/showcase/components/hero.jsx
+++ support-frontend/assets/pages/showcase/components/hero.jsx
@@ -9,80 +9,61 @@ import HeroImg from './hero.svg';
 
 import './hero.scss';
 
-class BreakingHeadlines extends React.Component {
+export default function BreakingHeadlines() {
 
-  state = { isAnimated: false }
-
-  componentDidMount() {
-    const startAnimation = () => {
-      this.setState({
-        isAnimated: !this.state.isAnimated,
-      });
-    };
-    setTimeout(() => {
-      startAnimation();
-    }, 800);
-  }
-
-  render() {
-    const isLoaded = this.state.isAnimated ? 'loaded' : '';
-    return (
-      <div className={`showcase-hero  ${isLoaded}`}>
-        <HeroWrapper appearance="custom">
-          <div className="showcase-hero-heading">
-            <h1 className="accessibility-hint">Support the guardian</h1>
-            <h2 className="accessibility-hint">Help us support investigative independent journalism</h2>
-            <HeroImg />
+  return (
+    <div className="showcase-hero">
+      <HeroWrapper appearance="custom">
+        <div className="showcase-hero-heading">
+          <h1 className="accessibility-hint">Support the guardian</h1>
+          <h2 className="accessibility-hint">Help us support investigative independent journalism</h2>
+          <HeroImg />
+        </div>
+        <figure className="showcase-hero__figure">
+          <div className="showcase-hero__image">
+            <GridImage
+              gridId="showcaseChris"
+              srcSizes={[1000, 500]}
+              sizes="(max-width: 740px) 100vw, 400px"
+              imgType="jpg"
+            />
           </div>
-          <figure className="showcase-hero__figure">
-            <div className="showcase-hero__image">
-              <GridImage
-                gridId="showcaseChris"
-                srcSizes={[1000, 500]}
-                sizes="(max-width: 740px) 100vw, 400px"
-                imgType="jpg"
-              />
-            </div>
-            <div className="showcase-hero__image">
-              <GridImage
-                gridId="showcaseZuckEyes"
-                srcSizes={[1000, 500]}
-                sizes="(max-width: 740px) 100vw, 400px"
-                imgType="jpg"
-              />
-            </div>
-            <div className="showcase-hero__image">
-              <GridImage
-                gridId="showcaseZuck"
-                srcSizes={[1000, 500]}
-                sizes="(max-width: 740px) 100vw, 400px"
-                imgType="jpg"
-              />
-            </div>
-            <div className="showcase-hero__image">
-              <GridImage
-                gridId="showcaseBrit"
-                srcSizes={[1000, 500]}
-                sizes="(max-width: 740px) 100vw, 600px"
-                imgType="jpg"
-              />
-            </div>
-            <figcaption className="showcase-hero__caption">
-              <strong>Cambridge Analytica</strong>
-              <p>A year-long investigation in which we revealed how the data
+          <div className="showcase-hero__image">
+            <GridImage
+              gridId="showcaseZuckGlass"
+              srcSizes={[1000, 500]}
+              sizes="(max-width: 740px) 100vw, 400px"
+              imgType="jpg"
+            />
+          </div>
+          <div className="showcase-hero__image">
+            <GridImage
+              gridId="showcaseNix"
+              srcSizes={[1000, 500]}
+              sizes="(max-width: 740px) 100vw, 400px"
+              imgType="jpg"
+            />
+          </div>
+          <div className="showcase-hero__image">
+            <GridImage
+              gridId="showcaseBrit"
+              srcSizes={[1000, 500]}
+              sizes="(max-width: 740px) 100vw, 600px"
+              imgType="jpg"
+            />
+          </div>
+          <figcaption className="showcase-hero__caption">
+            <strong>Cambridge Analytica</strong>
+            <p>A year-long investigation in which we revealed how the data
               analytics firm Cambridge Analytica that was behind Trump’s 2016
               campaign and played a role in Brexit, had used the data harvested
               from 87 million Facebook users without their consent.
               This reporting led to a public apology from Facebook’s
               Mark Zuckerberg who was forced to testify before Congress.
-              </p>
-            </figcaption>
-          </figure>
-        </HeroWrapper>
-      </div>
-    );
-  }
-
+            </p>
+          </figcaption>
+        </figure>
+      </HeroWrapper>
+    </div>
+  );
 }
-
-export default BreakingHeadlines;
