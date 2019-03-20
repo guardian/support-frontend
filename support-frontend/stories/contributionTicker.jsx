0667c2e5c3689b47479b446d282b608c4fde61af
Joseph Smith <joseph.smith@guardian.co.uk>
2019-03-20 18:03:09 +0000

    Add ticker to storybook

---
+++ support-frontend/stories/contributionTicker.jsx
@@ -0,0 +1,26 @@
+// @flow
+
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+
+import ContributionTicker from 'components/ticker/contributionTicker';
+import { text, withKnobs } from '@storybook/addon-knobs/src/index';
+import { withCenterAlignment } from '../.storybook/decorators/withCenterAlignment';
+
+
+const tickerJsonUrlInput = () => text('Ticker JSON URL', 'https://support.theguardian.com/ticker.json');
+
+const stories = storiesOf('Tickers', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withCenterAlignment);
+
+stories.add('Contribution ticker', () => (
+  <div style={{width: '100%', maxWidth: '500px'}}>
+    <ContributionTicker
+      tickerJsonUrl={tickerJsonUrlInput()}
+      onGoalReached={() => {}}
+    />
+  </div>
+));
