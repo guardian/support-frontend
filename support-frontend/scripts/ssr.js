ab64ba9fe1695ee9fe1ac89efcec1d3227a713fc
Laura buns <lawr@lauragonzalez.cc>
2019-03-21 05:49:36 +0000

    clust

--- support-frontend/scripts/ssr.js
+++ support-frontend/scripts/ssr.js
@@ -1,28 +1,45 @@
-const puppeteer = require('puppeteer');
+const chalk = require('chalk');
 const { writeFile } = require('fs');
+const { Cluster } = require('puppeteer-cluster');
+const { resolve } = require('path');
 
 const pages = ['uk/support', 'uk/subscribe/paper'];
 
-const pg = pages[1];
-
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const url = `http://localhost:9211/${pg}`;
-  page.goto(url);
-  await page.setCookie({
-    name: 'GU_TK',
-    value: '1.1553079258164',
-    domain: 'localhost',
+
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: require('os').cpus().length || 2,
   });
 
-  await page.waitFor('.gu-render-to > *');
-  const bodyHTML = await page.evaluate(() => document.documentElement.querySelector('.gu-render-to').innerHTML);
+  await cluster.task(async ({ page, data }) => {
+    const { path } = data;
+    console.log(`started  ${path}`);
 
-  writeFile(`${__dirname}/../conf/ssr-cache/${pg.replace(/\//g, '-')}.html`, bodyHTML, 'utf8', (err) => {
-    if (err) {
-      throw err;
-    }
-    console.info(`Wrote ${pg}`);
+    const url = `http://localhost:9211/${path}`;
+    await page.goto(url);
+    await page.setCookie({
+      name: 'GU_TK',
+      value: '1.1553079258164',
+      domain: 'localhost',
+    });
+
+    await page.waitFor('.gu-render-to > *');
+    const bodyHTML = await page.evaluate(() => document.documentElement.querySelector('.gu-render-to').innerHTML);
+
+    writeFile(
+      resolve(__dirname, '..', 'conf/ssr-cache/', `${path.replace(/\//g, '-')}.html`),
+      bodyHTML, 'utf8', (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log(chalk.green(`done     ${path}`));
+      },
+    );
   });
+
+  await Promise.all(pages.map(path => cluster.queue({ path })));
+  await cluster.idle();
+  await cluster.close();
+
 })();
