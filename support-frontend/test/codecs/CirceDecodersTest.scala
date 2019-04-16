0c8878d96e7727d27ee29507478c9c8032eaf160
Tom Forbes <tom.forbes@theguardian.com>
2019-04-16 16:58:39 +0100

    fix test

--- support-frontend/test/codecs/CirceDecodersTest.scala
+++ support-frontend/test/codecs/CirceDecodersTest.scala
@@ -192,7 +192,51 @@ class CirceDecodersTest extends WordSpec with MustMatchers {
           |            { "value": "25", "isDefault": true }
           |        ]
           |    }
-          |  }
+          |  },
+          |  "contributionTypes": {
+          |"GBPCountries": [
+          |{
+          |"contributionType": "ONE_OFF",
+          |"isDefault": true
+          |}
+          |],
+          |"UnitedStates": [
+          |{
+          |"contributionType": "ONE_OFF",
+          |"isDefault": true
+          |}
+          |],
+          |"EURCountries": [
+          |{
+          |"contributionType": "ONE_OFF",
+          |"isDefault": true
+          |}
+          |],
+          |"International": [
+          |{
+          |"contributionType": "ONE_OFF",
+          |"isDefault": true
+          |}
+          |],
+          |"Canada": [
+          |{
+          |"contributionType": "ONE_OFF",
+          |"isDefault": true
+          |}
+          |],
+          |"AUDCountries": [
+          |{
+          |"contributionType": "ONE_OFF",
+          |"isDefault": true
+          |}
+          |],
+          |"NZDCountries": [
+          |{
+          |"contributionType": "ONE_OFF",
+          |"isDefault": true
+          |}
+          |]
+          |}
           |}""".stripMargin
 
       val amount = Amount(value = "25", isDefault = Some(true))
