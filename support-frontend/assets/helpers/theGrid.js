33f98819b258cffb3ae52c1d592be6f820f2854e
Jesse Yuen <jesse.yuen.casual@theguardian.com>
2019-04-17 10:31:54 +0100

    Packshot image updated

--- support-frontend/assets/helpers/theGrid.js
+++ support-frontend/assets/helpers/theGrid.js
@@ -70,7 +70,7 @@ export const imageCatalogue: {
   theMomentDigiHero3: 'f73e9336a0fb62916921c2956d12dd29a0f43d30/0_0_486_772',
   weeklyCampaignHeroImg: '3d4bf412afdf3c91faed55c9507a6d741575e3c5/0_0_1358_954',
   checkoutPackshotPaperGraunVoucher: '5aa8702d1de22589ec5dd1a20a6cf4bc4b7c9674/0_0_696_400',
-  checkoutPackshotDigitalPack: '9810210a3535768abef1921b293b1959967f09d7/0_0_696_400',
+  checkoutPackshotDigitalPack: 'd68d6e6f276eae28d18851548e59bd7918a23ffc/0_0_1392_800',
 };
 
 // Utility type: https://flow.org/en/docs/types/utilities/#toc-keys
