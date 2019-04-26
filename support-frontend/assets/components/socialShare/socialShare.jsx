dd4c560986191577fa29a7d9990d74c9b4487681
Joshua <joshuathomaslieberman@gmail.com>
2019-04-26 12:45:44 +0100

    integrated PR feedback to branch

--- support-frontend/assets/components/socialShare/socialShare.jsx
+++ support-frontend/assets/components/socialShare/socialShare.jsx
@@ -14,42 +14,47 @@ import { trackComponentClick } from 'helpers/tracking/ophanComponentEventTrackin
 
 // ---- Types ----- //
 
-type Platform = 'facebook' | 'twitter' | 'linkedin' | 'email';
+type SharePlatform = 'facebook' | 'twitter' | 'linkedin' | 'email';
 
-type PropTypes = {| name: Platform |};
+type PropTypes = {| name: SharePlatform |};
 
 type SocialMedia = {
   link: string,
   svg: Node,
   a11yHint: string,
+  windowFeatures: string
 };
 
-type WindowFeatures = '' | 'menubar=no, toolbar=no, resizable=yes, scrollbars=yes, width=500, height=400';
+const SocialWindowFeatures = 'menubar=no, toolbar=no, resizable=yes, scrollbars=yes, width=500, height=400';
 
 // ----- Setup ----- //
 
 const socialMedia: {
-  [Platform]: SocialMedia,
+  [SharePlatform]: SocialMedia,
 } = {
   facebook: {
-    link: 'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fsupport.theguardian.com%2Fuk%2Fcontribute',
+    link: 'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fsupport.theguardian.com%2Fcontribute',
     svg: <SvgFacebook />,
     a11yHint: 'Share on facebook',
+    windowFeatures: SocialWindowFeatures,
   },
   twitter: {
-    link: 'https://twitter.com/intent/tweet?url=https%3A%2F%2Fsupport.theguardian.com%2Fuk%2Fcontribute&text=Join%20me%20and%20over%20one%20million%20others%20in%20supporting%20a%20different%20model%20for%20open%2C%20independent%20journalism.%20Together%20we%20can%20help%20safeguard%20The%20Guardian%E2%80%99s%20future%20%E2%80%93%20so%20more%20people%2C%20across%20the%20world%2C%20can%20keep%20accessing%20factual%20information%20for%20free',
+    link: 'https://twitter.com/intent/tweet?url=https%3A%2F%2Fsupport.theguardian.com%2Fcontribute&text=Join%20me%20and%20over%20one%20million%20others%20in%20supporting%20a%20different%20model%20for%20open%2C%20independent%20journalism.%20Together%20we%20can%20help%20safeguard%20The%20Guardian%E2%80%99s%20future%20%E2%80%93%20so%20more%20people%2C%20across%20the%20world%2C%20can%20keep%20accessing%20factual%20information%20for%20free',
     svg: <SvgTwitter />,
     a11yHint: 'Share on twitter',
+    windowFeatures: SocialWindowFeatures,
   },
   linkedin: {
-    link: 'http://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fsupport.theguardian.com%2Fuk%2Fcontribute&title=Join%20me%20and%20over%20one%20million%20others%20in%20supporting%20a%20different%20model%20for%20open%2C%20independent%20journalism.%20Together%20we%20can%20help%20safeguard%20The%20Guardian%E2%80%99s%20future%20%E2%80%93%20so%20more%20people%2C%20across%20the%20world%2C%20can%20keep%20accessing%20factual%20information%20for%20free',
+    link: 'http://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fsupport.theguardian.com%2Fcontribute&title=Join%20me%20and%20over%20one%20million%20others%20in%20supporting%20a%20different%20model%20for%20open%2C%20independent%20journalism.%20Together%20we%20can%20help%20safeguard%20The%20Guardian%E2%80%99s%20future%20%E2%80%93%20so%20more%20people%2C%20across%20the%20world%2C%20can%20keep%20accessing%20factual%20information%20for%20free',
     svg: <SvgLinkedin />,
     a11yHint: 'Share on linkedin',
+    windowFeatures: SocialWindowFeatures,
   },
   email: {
-    link: 'mailto:?subject=Join%20me%20in%20supporting%20open%2C%20independent%20journalism&body=Join%20me%20and%20over%20one%20million%20others%20in%20supporting%20a%20different%20model%20for%20open%2C%20independent%20journalism.%20Together%20we%20can%20help%20safeguard%20The%20Guardian%E2%80%99s%20future%20%E2%80%93%20so%20more%20people%2C%20across%20the%20world%2C%20can%20keep%20accessing%20factual%20information%20for%20free%3A%20https%3A%2F%2Fsupport.theguardian.com%2Fuk%2Fcontribute',
+    link: 'mailto:?subject=Join%20me%20in%20supporting%20open%2C%20independent%20journalism&body=Join%20me%20and%20over%20one%20million%20others%20in%20supporting%20a%20different%20model%20for%20open%2C%20independent%20journalism.%20Together%20we%20can%20help%20safeguard%20The%20Guardian%E2%80%99s%20future%20%E2%80%93%20so%20more%20people%2C%20across%20the%20world%2C%20can%20keep%20accessing%20factual%20information%20for%20free%3A%20https%3A%2F%2Fsupport.theguardian.com%2Fcontribute',
     svg: <SvgEmail />,
     a11yHint: 'Share by email',
+    windowFeatures: '',
   },
 };
 
@@ -60,17 +65,13 @@ export default function SocialShare(props: PropTypes) {
 
   const a11yId = `component-social-share-a11y-hint-${props.name}`;
 
-  function onShare(eventName: string, eventLink: string): () => void {
+  function onShare(eventName: string, eventLink: string, windowFeatures: string): () => void {
     return (): void => {
-      let features: WindowFeatures = '';
-      if (eventName === 'contributions-share-facebook' || eventName === 'contributions-share-linkedin' || eventName === 'contributions-share-twitter') {
-        features = 'menubar=no, toolbar=no, resizable=yes, scrollbars=yes, width=500, height=400';
-      }
       trackComponentClick(eventName);
       window.open(
         eventLink,
         '',
-        features,
+        windowFeatures,
       );
     };
   }
@@ -80,7 +81,7 @@ export default function SocialShare(props: PropTypes) {
     <button
       className="component-social-share"
       onClick={
-          onShare(`contributions-share-${props.name}`, socialMedia[props.name].link)
+          onShare(`contributions-share-${props.name}`, socialMedia[props.name].link, socialMedia[props.name].windowFeatures)
       }
       aria-labelledby={a11yId}
     >
