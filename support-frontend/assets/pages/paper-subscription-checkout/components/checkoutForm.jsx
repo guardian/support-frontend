10723f80c1f0d259ee0948c2961319036c405c7f
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2019-05-02 13:24:01 +0100

    Adds digital pack rollout for order summary and tweaks suggested by Rupert

--- support-frontend/assets/pages/paper-subscription-checkout/components/checkoutForm.jsx
+++ support-frontend/assets/pages/paper-subscription-checkout/components/checkoutForm.jsx
@@ -25,7 +25,7 @@ import { withError } from 'hocs/withError';
 import { asControlled } from 'hocs/asControlled';
 import Form, { FormSection } from 'components/checkoutForm/checkoutForm';
 import Layout, { Content } from 'components/subscriptionCheckouts/layout';
-import Summary from './summary';
+import Summary from 'components/subscriptionCheckouts/summary';
 import DirectDebitPopUpForm from 'components/directDebit/directDebitPopUpForm/directDebitPopUpForm';
 import type { PaymentAuthorisation } from 'helpers/paymentIntegrations/readerRevenueApis';
 import type { ErrorReason } from 'helpers/errorReasons';
