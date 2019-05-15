cd3d0decfbbac71afb8f44f56036bc9657fa4945
damien-shaw-guardian <damien.shaw.freelancer@guardian.co.uk>
2019-05-15 10:24:41 +0100

    refactor: changed name of variant
    refactor: changed name of variant
    - Also added `disableHostCheck: true` to disable error "Invalid Host/Origin Header" warning
    - issue https://github.com/webpack/webpack-dev-server/issues/1604

--- support-frontend/assets/pages/paper-subscription-landing/components/hero/discountCopy.js
+++ support-frontend/assets/pages/paper-subscription-landing/components/hero/discountCopy.js
@@ -3,5 +3,5 @@ export const discountCopyChoices = {
     control: ['Save up to','37%'],
     save: ['Save up to', '£27.78', 'per month'],
     from: ['from', '£1.57', 'per issue'],
-    getup: ['Get up to', '11', 'issues free per mth']
+    getupto: ['Get up to', '11', 'issues free per mth']
 }
