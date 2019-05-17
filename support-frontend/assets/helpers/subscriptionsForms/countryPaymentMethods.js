769a147779ca5e2128f9b63d2ec3621415fc6e5f
Jon Soul <jfsoul@users.noreply.github.com>
2019-05-17 14:51:43 +0100

    Remove direct debit payment method

--- support-frontend/assets/helpers/subscriptionsForms/countryPaymentMethods.js
+++ support-frontend/assets/helpers/subscriptionsForms/countryPaymentMethods.js
@@ -9,7 +9,7 @@ import type { Option } from 'helpers/types/option';
 
 function supportedPaymentMethods(country: IsoCountry, product: SubscriptionProduct): PaymentMethod[] {
   const productSpecific: PaymentMethod[] = product === DigitalPack ? [PayPal] : [];
-  const countrySpecific: PaymentMethod[] = country === 'GB' ? [DirectDebit, Stripe] : [Stripe];
+  const countrySpecific: PaymentMethod[] = country === 'NotACountry' ? [DirectDebit, Stripe] : [Stripe];
 
   return countrySpecific.concat(productSpecific);
 }
