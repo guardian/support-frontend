cb741f4122da45244693ba6a9eca63e2fac17bba
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2019-06-05 16:51:33 +0100

    Marks the gift email field as optional

--- support-frontend/assets/components/subscriptionCheckouts/personalDetailsGift.jsx
+++ support-frontend/assets/components/subscriptionCheckouts/personalDetailsGift.jsx
@@ -48,6 +48,7 @@ export default function PersonalDetailsGift(props: PropTypes) {
         type="emailGiftRecipient"
         value={props.emailGiftRecipient}
         setValue={props.setEmailGift}
+        optional
         error={firstError('emailGiftRecipient', props.formErrors)}
       />
     </div>
