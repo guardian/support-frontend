1d24c0b7cefde31165b1d500584ecd3ea407a9b5
Joseph Smith <joseph.lochlann@gmail.com>
2019-06-24 11:01:36 +0100

    Single setup script

--- support-frontend/setup.sh
+++ support-frontend/setup.sh
@@ -98,41 +98,38 @@ install_node() {
   fi
 }
 
-install_nginx() {
-  if ! installed nginx; then
-    brew install nginx
-    EXTRA_STEPS+=("nginx has been installed. Ensure you have 'include sites-enabled/*' in your nginx configuration ${NGINX_ROOT}/nginx.conf and add '127.0.0.1 support.thegulocal.com' and '127.0.0.1 support-ui.thegulocal.com' to /etc/hosts")
-  else
-    EXTRA_STEPS+=("nginx is installed. Ensure you have 'include sites-enabled/*' in your nginx configuration ${NGINX_ROOT}/nginx.conf and add '127.0.0.1 support.thegulocal.com' and '127.0.0.1 support-ui.thegulocal.com' to /etc/hosts")
-  fi
+setup_nginx() {
+  DOMAIN=support.thegulocal.com
+  DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  SITE_CONFIG=${DIR}/nginx/support.conf
 
-  ./nginx/setup.sh
+  DOMAINS=(
+    "support.thegulocal.com"
+    "support-ui.thegulocal.com"
+  )
+
+  for domain in ${DOMAINS[@]}; do
+    dev-nginx setup-cert $domain
+  done
+
+  dev-nginx link-config ${SITE_CONFIG}
+  dev-nginx restart-nginx
 }
 
-install_awscli() {
-  if ! installed aws; then
-    brew install awscli
-  fi
-}
-
-install_sbt() {
-  if ! installed sbt; then
-    brew install sbt@1
-  fi
-}
-
-install_yarn() {
-  if ! installed yarn; then
-    if linux; then
-      sudo apt-get install yarn
-    elif mac; then
-      brew install yarn
-    fi
+install_yarn_if_linux() {
+  # This will be installed by 'install_brew_deps' if on mac
+  if linux && ! installed yarn; then
+    sudo apt-get install yarn
   fi
 }
 
 install_js_deps() {
   yarn install
+}
+
+install_brew_deps() {
+  # Installs dependencies defined in Brewfile
+  brew bundle
 }
 
 report() {
@@ -148,12 +145,13 @@ main () {
   create_aws_config
   install_node
   install_homebrew
-  install_nginx
-  install_awscli
+  install_brew_deps
+  setup_nginx
   install_jdk
-  install_sbt
-  install_yarn
+  install_yarn_if_linux
   install_js_deps
+
+  EXTRA_STEPS+=("nginx is installed. Ensure you have 'include sites-enabled/*' in your nginx configuration ${NGINX_ROOT}/nginx.conf and add '127.0.0.1 support.thegulocal.com' and '127.0.0.1 support-ui.thegulocal.com' to /etc/hosts")
   report
 }
 
