fbed83865de9f5d8534b39b26615fde03d8ff6ae
Joseph Smith <joseph.smith@guardian.co.uk>
2019-06-24 15:39:26 +0100

    Remove editorialiseAmountsRoundTwo test (#1917)

--- support-frontend/assets/helpers/abTests/abtestDefinitions.js
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.js
@@ -4,29 +4,7 @@ import type { Tests } from './abtest';
 // ----- Tests ----- //
 
 
-export type EditorialiseAmountsRoundTwoVariant = 'control' | 'weeklyBreakdownMonthlyAsWell' | 'notintest';
-
 export const tests: Tests = {
 
-  editorialiseAmountsRoundTwo: {
-    type: 'OTHER',
-    variants: [
-      {
-        id: 'control',
-      },
-      {
-        id: 'weeklyBreakdownMonthlyAsWell',
-      },
-    ],
-    audiences: {
-      ALL: {
-        offset: 0,
-        size: 1,
-      },
-    },
-    isActive: true,
-    independent: true,
-    seed: 3,
-  },
 };
 
