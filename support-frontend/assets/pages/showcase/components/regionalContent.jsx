03cd2c380402ca8e97c6143ab559d5311703d1d3
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2019-07-02 15:40:11 +0100

    Updates images and animations to match designs

--- support-frontend/assets/pages/showcase/components/regionalContent.jsx
+++ support-frontend/assets/pages/showcase/components/regionalContent.jsx
@@ -6,42 +6,42 @@ export const regionalContent = {
     caption: (
       <span>
         In 2018, we revealed that the British Home Office had wrongly detained and threatened with deportation&#32;
-        hundreds of UK residents. Among their number were members of the Windrush Generation &mdash; named after&#32;
-        the <i className="showcase-hero__caption--italics">Empire Windrush</i>, the ship that brought one of the&#32;
+        hundreds of UK residents. Among their number were members of the Windrush Generation &mdash; so named after&#32;
+        the &quot;Empire Windrush&quot;, the ship that brought one of the&#32;
         first groups of West Indian migrants to the UK. The scandal prompted a wider debate about British&#32;
         immigration policy.
       </span>),
     images: {
-      first: 'showcaseChrisSquare',
-      second: 'showcaseZuckGlass',
-      third: 'showcaseNix',
-      fourth: 'showcaseBrit',
+      first: 'windrushPortrait',
+      second: 'windrushWelcome',
+      third: 'windrushLammy',
+      fourth: 'showcaseWindrush',
     },
   },
   auContent: {
     title: 'Climate crisis',
     caption: (
       <span>
-        <i className="showcase-hero__caption--italics">Guardian Australia</i> gives climate crisis&#32;
-        reporting the prominence it deserves, stories that often go unreported in the mainstream media.&#32;
-        At this critical time for our planet, we are determined to inform readers about threats,&#32;
-        consequences and solutions based on scientific facts, not political prejudice or business interests.
+        <i>Guardian Australia</i> gives climate crisis reporting the prominence it deserves,&#32;
+        stories that often go unreported in the mainstream media. At this critical time for our planet,&#32;
+        we are determined to inform readers about threats, consequences and solutions based on scientific&#32;
+        facts, not political prejudice or business interests.
       </span>),
     images: {
-      first: 'newsroom',
-      second: 'showcaseZuckGlass',
-      third: 'protestorsWide',
-      fourth: 'showcaseBrit',
+      first: 'showcaseGreta',
+      second: 'showcaseProtest',
+      third: 'showcaseProtestKids',
+      fourth: 'showcaseIndustry',
     },
   },
   usContent: {
     title: 'Cambridge Analytica',
     caption: 'A year-long investigation in which we revealed that 50 million Facebook profiles had been harvested without their consent by data analytics firm Cambridge Analytica, which worked with Trump\'s election team and the Brexit campaign.',
     images: {
-      first: 'showcaseChrisSquare',
-      second: 'showcaseZuckGlass',
-      third: 'showcaseNix',
-      fourth: 'showcaseBrit',
+      first: 'showcaseImpeach',
+      second: 'showcaseTrump',
+      third: 'showcaseNancyP',
+      fourth: 'showcaseMueller',
     },
   },
 };
