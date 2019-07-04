b954ada8aec60edbc2d293cf5920daf586f20355
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-07-04 15:44:40 +0100

    Extract the error message from failed executions

--- support-workers/scripts/show-failed.sh
+++ support-workers/scripts/show-failed.sh
@@ -4,7 +4,7 @@ grep executionArn | \
 awk '{print $2;}' | \
 sed 's=^"\(.*\)",$=\1=' | \
 xargs -n1 aws stepfunctions get-execution-history --profile membership --region eu-west-1 --execution-arn | \
-jq '.events[] | select(.type == "LambdaFunctionFailed")' | jq -s '. | .[0].lambdaFunctionFailedEventDetails.cause' | \
+jq '[.events[] | select(.type == "LambdaFunctionFailed")] | .[0].lambdaFunctionFailedEventDetails.cause' | \
 sed 's=\\\\=\\=g' | \
 sed 's=\\"="=g' | \
 sed 's=^"\(.*\)"$=\1=' | \
