30b4ae7c014fc5a00d509cd8318a627c13d53f54
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-07-04 16:52:04 +0100

    Set the default number of failed executions to show to 1

--- support-workers/scripts/show-failed.sh
+++ support-workers/scripts/show-failed.sh
@@ -1,4 +1,4 @@
-MAX_EXECUTIONS=${1:-5}   
+MAX_EXECUTIONS=${1:-1}   
 aws stepfunctions list-executions --status-filter FAILED --state-machine-arn arn:aws:states:eu-west-1:865473395570:stateMachine:support-workers-PROD --max-items $MAX_EXECUTIONS --profile membership --region eu-west-1 | \
 grep executionArn | \
 awk '{print $2;}' | \
