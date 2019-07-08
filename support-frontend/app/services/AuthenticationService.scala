68f0d57fc8df1ae3052985aaf07071678967a963
Mullefa <gccdawson@gmail.com>
2019-07-09 00:00:20 +0100

    introduce identity-play-auth

--- support-frontend/app/services/AuthenticationService.scala
+++ support-frontend/app/services/AuthenticationService.scala
@@ -1,38 +1,45 @@
 package services
 
-import com.gu.identity.cookie.IdentityKeys
-import com.gu.identity.play.AccessCredentials.{Cookies, Token}
-import com.gu.identity.play.AuthenticatedIdUser
-import com.gu.identity.play.AuthenticatedIdUser._
+import cats.implicits._
+import com.gu.identity.auth.UserCredentials
+import com.gu.identity.play.IdentityPlayAuthService
+import config.Identity
+import org.http4s.Uri
 import play.api.mvc.RequestHeader
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class AuthenticationService(override val identityKeys: IdentityKeys) extends com.gu.identity.play.AuthenticationService {
-  override lazy val authenticatedIdUserProvider: Provider =
-    Cookies.authProvider(identityKeys).withDisplayNameProvider(Token.authProvider(identityKeys, "membership"))
-}
+// Removed from identity-play-auth
+case class IdMinimalUser(id: String, displayName: Option[String])
+case class AuthenticatedIdUser(credentials: UserCredentials, user: IdMinimalUser)
 
 // TODO: consider porting this to identity-play-auth.
 class AsyncAuthenticationService(
-  authenticationService: AuthenticationService,
-  testUserService: TestUserService
+    identityPlayAuthService: IdentityPlayAuthService,
+    testUserService: TestUserService
 )(implicit ec: ExecutionContext) {
 
   // TODO in follow-up PR:
   // authenticate the user by making a call to identity API rather than using underyling authentication service.
-  def authenticateUser(requestHeader: RequestHeader): Future[Option[AuthenticatedIdUser]] =
-    Future.successful(authenticationService.authenticatedUserFor(requestHeader))
+  def authenticateUser(requestHeader: RequestHeader): Future[AuthenticatedIdUser] =
+    identityPlayAuthService.getUserFromRequest(requestHeader)
+      .map { case (credentials, user) =>
+        AuthenticatedIdUser(credentials, IdMinimalUser(user.id, user.publicFields.displayName))
+      }
+      .unsafeToFuture()
 
   // TODO in follow-up PR:
   // authenticate the user by making a call to identity API rather than using underyling authentication service.
-  def authenticateTestUser(requestHeader: RequestHeader): Future[Option[AuthenticatedIdUser]] =
-    authenticateUser(requestHeader).map(_.filter(user => testUserService.isTestUser(user.user.displayName)))
+  def authenticateTestUser(requestHeader: RequestHeader): Future[AuthenticatedIdUser] =
+    authenticateUser(requestHeader).ensure(new RuntimeException("user is not a test user")) { user =>
+      testUserService.isTestUser(user.user.displayName)
+    }
 }
 
 object AsyncAuthenticationService {
-  def apply(identityKeys: IdentityKeys, testUserService: TestUserService)(implicit ec: ExecutionContext): AsyncAuthenticationService = {
-    val authenticationService = new AuthenticationService(identityKeys)
-    new AsyncAuthenticationService(authenticationService, testUserService)
+  def apply(config: Identity, testUserService: TestUserService)(implicit ec: ExecutionContext): AsyncAuthenticationService = {
+    val apiUrl = Uri.unsafeFromString(config.apiUrl)
+    val identityPlayAuthService = IdentityPlayAuthService(apiUrl, config.apiClientToken)
+    new AsyncAuthenticationService(identityPlayAuthService, testUserService)
   }
 }
