a2e5b206740f6a6e26812b7cd107ae4ac2b323a2
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2019-07-10 14:29:43 +0100

    Add GA tracking for contribute cta on support page

--- support-frontend/assets/pages/showcase/components/ctaContribute.jsx
+++ support-frontend/assets/pages/showcase/components/ctaContribute.jsx
@@ -9,6 +9,7 @@ import ArrowRightStraight from 'components/svgs/arrowRightStraight';
 
 import WithSupport from 'components/svgs/withSupport';
 import OneMillionCircles from 'components/svgs/oneMillionCircles';
+import { sendClickedEvent } from 'helpers/tracking/clickTracking';
 
 export default function CtaContribute() {
   return (
@@ -43,6 +44,7 @@ export default function CtaContribute() {
               aria-label={null}
               appearance="secondary"
               href="/contribute"
+              onClick={() => sendClickedEvent('support_page_cta_contribute')()}
             >
             Make a Contribution
             </AnchorButton>
