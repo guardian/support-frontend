443ba361396b1701e72fcb35c6e27c37c9319821
Joshua <joshuathomaslieberman@gmail.com>
2019-07-25 13:30:20 +0100

    deploying new survey

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYou.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYou.jsx
@@ -186,7 +186,7 @@ function ContributionThankYou(props: PropTypes) {
         ) : null}
         { renderIdentityCTA() }
         <MarketingConsent />
-        <ContributionSurvey isRunning={false} contributionType={props.contributionType} />
+        <ContributionSurvey isRunning={true} />
         <SpreadTheWord />
         <div className="gu-content__return-link">
           <AnchorButton
