217ac046e5cb550c294abbf3a700bf2429c79a53
Tom Forbes <tom.forbes@theguardian.com>
2019-07-30 16:31:50 +0100

    remove log

--- support-frontend/assets/helpers/cookie.js
+++ support-frontend/assets/helpers/cookie.js
@@ -45,8 +45,6 @@ export function set(name: string, value: string, daysToLive: ?number): void {
     expires.setDate(1);
   }
 
-  console.log("date:", expires.getMonth(), expires.getDate())
-
   document.cookie = `${name}=${value}; path=/; secure; expires=${expires.toUTCString()};${getDomainAttribute()}`;
 
 }
