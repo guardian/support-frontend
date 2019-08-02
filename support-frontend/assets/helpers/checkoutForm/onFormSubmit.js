9065df9b2082f9ff5c7b0444013cd0fd70fc48a7
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-08-02 14:44:32 +0100

    Revert "Revert "Add additional tracking for the product checkout""

--- support-frontend/assets/helpers/checkoutForm/onFormSubmit.js
+++ support-frontend/assets/helpers/checkoutForm/onFormSubmit.js
@@ -29,12 +29,12 @@ export const onFormSubmit = (params: FormSubmitParameters) => {
       if (params.handlePayment) {
         params.handlePayment();
       }
-      trackCheckoutSubmitAttempt(componentId, `${params.flowPrefix}-allowed-for-user-type-${userType}`, params.paymentMethod);
+      trackCheckoutSubmitAttempt(componentId, `${params.flowPrefix}-allowed-for-user-type-${userType}`, params.paymentMethod, 'Contribution');
     } else {
-      trackCheckoutSubmitAttempt(componentId, `${params.flowPrefix}-blocked-because-user-type-is-${userType}`, params.paymentMethod);
+      trackCheckoutSubmitAttempt(componentId, `${params.flowPrefix}-blocked-because-user-type-is-${userType}`, params.paymentMethod, 'Contribution');
     }
   } else {
-    trackCheckoutSubmitAttempt(componentId, `${params.flowPrefix}-blocked-because-form-not-valid${invalidReason(params.form)}`, params.paymentMethod);
+    trackCheckoutSubmitAttempt(componentId, `${params.flowPrefix}-blocked-because-form-not-valid${invalidReason(params.form)}`, params.paymentMethod, 'Contribution');
   }
   params.setCheckoutFormHasBeenSubmitted();
 };
