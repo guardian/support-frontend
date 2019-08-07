abc6173dcc1bb833d5531dd9e887acc13b146bab
damien-shaw-guardian <damien.shaw.freelancer@guardian.co.uk>
2019-08-07 11:45:26 +0100

    feat: add new experiment id

--- support-frontend/assets/pages/digital-subscription-landing/digitalSubscriptionLanding.jsx
+++ support-frontend/assets/pages/digital-subscription-landing/digitalSubscriptionLanding.jsx
@@ -68,7 +68,7 @@ const CountrySwitcherHeader = headerWithCountrySwitcherContainer({
 
 const mapStateToProps = (state) => {
   const { optimizeExperiments } = state.common;
-  const dailyEditionsExperimentId = 'xOzjpaFDQlO5L6_ORotRWA';
+  const dailyEditionsExperimentId = 'IscBgfXeTmyDoga1IGKcOQ';
   const dailyEditionsVariant = optimizeExperiments
     .filter(exp => exp.id === dailyEditionsExperimentId && exp.variant === '1').length !== 0
     && !isPostDeployUser();
