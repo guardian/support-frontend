a302b4245a172865ce1c62d4b909de5ae9a6399d
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-09-17 10:22:26 +0100

    Remove extra space

--- support-frontend/assets/pages/digital-subscription-landing/components/paymentSelection/paymentSelection.jsx
+++ support-frontend/assets/pages/digital-subscription-landing/components/paymentSelection/paymentSelection.jsx
@@ -44,7 +44,7 @@ const PaymentSelection = ({ paymentOptions }: PropTypes) => (
                 aria-label="Subscribe-button"
                 salesCopy={paymentOption.salesCopy}
               >
-                {' Start free trial now'}
+                {'Start free trial now'}
               </ProductOptionButton>
             </ProductOption>
           </div>
