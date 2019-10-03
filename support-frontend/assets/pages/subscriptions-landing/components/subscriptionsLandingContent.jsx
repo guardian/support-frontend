cee8a97e9774256fd5e847ab850e0904563478e2
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-10-03 15:07:04 +0100

    Revert "Revert "New subs showcase page 2""

---
+++ support-frontend/assets/pages/subscriptions-landing/components/subscriptionsLandingContent.jsx
@@ -0,0 +1,33 @@
+// @flow
+
+import React from 'react';
+
+// components
+import SubscriptionsProduct from './subscriptionsProduct';
+import FeatureHeader from './featureHeader';
+
+import { subscriptionCopy } from '../copy/subscriptionCopy'; // make the first card a feature
+
+const isFeature = index => index === 0;
+
+const SubscriptionsLandingContent = () => (
+  <div className="subscriptions-landing-page">
+    <FeatureHeader />
+    <div className="subscriptions__product-container">
+      {subscriptionCopy.map((product, index) => (
+        <SubscriptionsProduct
+          title={product.title}
+          subtitle={product.subtitle}
+          description={product.description}
+          productImage={product.productImage}
+          buttons={product.buttons}
+          offer={product.offer || null}
+          isFeature={isFeature(index)}
+          classModifier={product.classModifier}
+        />
+      ))}
+    </div>
+  </div>
+);
+
+export default SubscriptionsLandingContent;
