319ab98ba8a964e4fdf9a9ea3ee05418faf6bbc2
John Duffell <john.duffell@guardian.co.uk>
2019-10-04 11:53:19 +0100

    newer AWS library

--- project/LibraryVersions.scala
+++ project/LibraryVersions.scala
@@ -1,7 +1,7 @@
 
 object LibraryVersions {
   val circeVersion = "0.11.1"
-  val awsClientVersion = "1.11.226"
+  val awsClientVersion = "1.11.642"
   val catsVersion = "1.4.0"
   val jacksonVersion = "2.10.0"
   val okhttpVersion = "3.10.0"
