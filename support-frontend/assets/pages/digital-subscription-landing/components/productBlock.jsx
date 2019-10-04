ff521c4dfc05a8829bbdb275b1441860eef3ef87
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2019-10-04 15:14:41 +0100

    Add plus labels between products

--- support-frontend/assets/pages/digital-subscription-landing/components/productBlock.jsx
+++ support-frontend/assets/pages/digital-subscription-landing/components/productBlock.jsx
@@ -5,6 +5,8 @@ import AdFreeSectionC from 'components/adFreeSectionC/adFreeSectionC';
 // styles
 import './digitalSubscriptionLanding.scss';
 
+const Plus = () => <div className="product-block__plus">+ Plus</div>;
+
 const ProductBlock = () => (
   <div className="hope-is-power__products">
     <div className="product-block__container hope-is-power--centered">
@@ -12,9 +14,11 @@ const ProductBlock = () => (
       <div className="product-block__item">
         <div className="product-block__item__title">The Guardian Daily</div>
       </div>
+      <Plus />
       <div className="product-block__item">
         <div className="product-block__item__title">Premium access to the Live app</div>
       </div>
+      <Plus />
       <AdFreeSectionC />
     </div>
   </div>
