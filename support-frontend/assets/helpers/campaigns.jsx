9ab7edec4f1f06be92a6dd5b029857b1470cb56a
Joshua <joshuathomaslieberman@gmail.com>
2019-10-09 11:03:50 +0100

    updating url and code names for the climate moment

--- support-frontend/assets/helpers/campaigns.jsx
+++ support-frontend/assets/helpers/campaigns.jsx
@@ -64,6 +64,7 @@ export const campaigns: Campaigns = {
           To find out what personal data we collect and how we use it, please visit
           our <a href="https://www.theguardian.com/help/privacy-policy">Privacy Policy</a>.
         </p>
+        
         {isUK ? (
           <span>
             <p>
