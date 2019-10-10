49dc762d6baf5e90d82710022e95c9620252e713
Joe Griffiths <menai.software@gmail.com>
2019-10-10 14:49:55 +0100

    Safer creation of payload

--- support-frontend/app/services/RemindMeService.scala
+++ support-frontend/app/services/RemindMeService.scala
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.AWSLambdaClientBuilder
 import com.amazonaws.services.lambda.model.InvokeRequest
 import com.gu.monitoring.SafeLogger
 import com.gu.support.config.{Stage, Stages}
+import play.api.libs.json.Json
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -24,11 +25,13 @@ class RemindMeService(stage: Stage) {
   private val expectedResponse = """{"statusCode":200,"body":"\"OK\""}"""
 
   def apply(email: String)(implicit ec: ExecutionContext): Future[Boolean] = {
-    val payload = s"""{"ReminderCreatedEvent": {"email": "$email"}}"""
+
+    val emailJson = Json.obj("email" -> email)
+    val payload = Json.obj("ReminderCreatedEvent" -> emailJson)
 
     val request = new InvokeRequest()
       .withFunctionName(functionName)
-      .withPayload(payload)
+      .withPayload(payload.toString)
 
     val res = lambdaClient.invoke(request)
     val responseBody = new String(res.getPayload.array())
