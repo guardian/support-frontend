3d990ae6459549f471f4ad421bee9428351411f3
jesse yuen <contactjesseyuen@gmail.com>
2019-10-14 17:26:13 +0100

    Copy update

--- support-frontend/assets/components/adFreeSectionB/adFreeSectionB.jsx
+++ support-frontend/assets/components/adFreeSectionB/adFreeSectionB.jsx
@@ -13,9 +13,7 @@ const AdFreeSectionB = () => (
       <div className="ad-free-section__content">
         <h2 className="ad-free-section-b__sub-header">Ad-free web</h2>
         <p className="ad-free-section__copy">
-          Read The Guardian&apos;s quality, independent journalism without interruptions.
-          Enjoy an ad-free experience across all of your devices when you&apos;re
-          signed in on your apps and theguardian.com
+          Read The Guardian&apos;s quality, independent journalism without interruptions. Enjoy an ad-free experience across all of your devices when you&apos;re signed in on your apps and theguardian.com
         </p>
       </div>
 
