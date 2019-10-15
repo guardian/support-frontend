459d33a9b10a8b7fe506f3bea77e002eda8e77ed
jesse yuen <contactjesseyuen@gmail.com>
2019-10-15 08:59:11 +0100

    Footer email and copy updates

--- support-frontend/assets/pages/digital-subscription-landing/components/faqsAndHelp.jsx
+++ support-frontend/assets/pages/digital-subscription-landing/components/faqsAndHelp.jsx
@@ -45,11 +45,11 @@ function productAndCountrySpecificEmail(
     return 'apac.help@theguardian.com';
   }
 
-  return 'digitalpack@theguardian.com';
+  return 'customer.help@theguardian.com';
 }
 
 Email.defaultProps = {
-  email: 'digitalpack@theguardian.com',
+  email: 'customer.help@theguardian.com',
 };
 
 // ----- Component ----- //
