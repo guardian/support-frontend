e9eafef887984112de7f30a4cf8e9fa5a3e2c5a1
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-10-21 11:32:08 +0100

    Add 'Get this offer' button

--- support-frontend/assets/pages/promotion-terms/promoDetails.jsx
+++ support-frontend/assets/pages/promotion-terms/promoDetails.jsx
@@ -6,6 +6,21 @@ import { LargeParagraph, Title } from 'components/text/text';
 import Content from 'components/content/content';
 import { formatUserDate } from 'helpers/dateConversions';
 import UnorderedList from 'components/list/unorderedList';
+import AnchorButton from 'components/button/anchorButton';
+import type { SubscriptionProduct } from 'helpers/subscriptions';
+import { DigitalPack, Paper } from 'helpers/subscriptions';
+import { routes } from 'helpers/routes';
+
+const landingPageForProduct = (product: SubscriptionProduct) => {
+  switch (product) {
+    case DigitalPack:
+      return routes.digitalSubscriptionLanding;
+    case Paper:
+      return routes.paperSubscriptionLanding;
+    default:
+      return routes.guardianWeeklySubscriptionLanding;
+  }
+};
 
 export default function PromoDetails(props: PromotionTerms) {
   const validUntil = props.expires ? (
@@ -28,6 +43,8 @@ export default function PromoDetails(props: PromotionTerms) {
         }
         />
       </LargeParagraph>
+      <AnchorButton href={landingPageForProduct(props.product)}>Get this offer</AnchorButton>
     </Content>
   );
 }
+
