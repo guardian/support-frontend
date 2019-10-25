6ce4fc91720dee2d5dc39a7178eb7f72ba706910
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-10-25 14:13:21 +0100

    Add a /p/[promoCode] endpoint which redirects to the appropriate product page.

--- support-frontend/app/controllers/Promotions.scala
+++ support-frontend/app/controllers/Promotions.scala
@@ -3,10 +3,12 @@ package controllers
 import actions.CustomActionBuilders
 import admin.settings.{AllSettings, AllSettingsProvider}
 import assets.{AssetsResolver, RefPath, StyleContent}
+import com.gu.support.catalog.{DigitalPack, GuardianWeekly, Paper}
 import com.gu.support.config.Stage
 import com.gu.support.encoding.CustomCodecs._
 import com.gu.support.pricing.PriceSummaryServiceProvider
 import com.gu.support.promotions.{PromoCode, PromotionServiceProvider, PromotionTerms}
+import lib.RedirectWithEncodedQueryString
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import play.twirl.api.Html
 import services.TestUserService
@@ -27,6 +29,22 @@ class Promotions(
   import actionRefiners._
 
   implicit val a: AssetsResolver = assets
+
+  def promo(promoCode: String): Action[AnyContent] = CachedAction() { implicit request =>
+    val promotionService = promotionServiceProvider.forUser(false)
+    val maybePromotionTerms = PromotionTerms.fromPromoCode(promotionService, stage, promoCode)
+
+    maybePromotionTerms.fold(NotFound("Invalid promo code")
+    ) { promotionTerms =>
+      val productLandingPage = promotionTerms.product match {
+        case GuardianWeekly => routes.Subscriptions.weeklyGeoRedirect().url
+        case DigitalPack => routes.DigitalSubscription.digitalGeoRedirect().url
+        case Paper => routes.PaperSubscription.paper(false).url
+      }
+      val queryString = request.queryString + ("promoCode" -> Seq(promoCode))
+      RedirectWithEncodedQueryString(productLandingPage, queryString, MOVED_PERMANENTLY)
+    }
+  }
 
   def terms(promoCode: String): Action[AnyContent] = CachedAction() { implicit request =>
     implicit val settings: AllSettings = settingsProvider.getAllSettings()
