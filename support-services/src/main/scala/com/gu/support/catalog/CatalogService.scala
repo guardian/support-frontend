1058043e451ad98d900b7f1c1b21422055bed7fc
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-10-28 21:20:16 +0000

    Adjust 6 weekly pricing to the same as quarterly in the catalog

--- support-services/src/main/scala/com/gu/support/catalog/CatalogService.scala
+++ support-services/src/main/scala/com/gu/support/catalog/CatalogService.scala
@@ -3,9 +3,9 @@ package com.gu.support.catalog
 import com.gu.aws.AwsCloudWatchMetricPut
 import com.gu.aws.AwsCloudWatchMetricPut.{catalogFailureRequest, client}
 import com.gu.i18n.Currency
-import com.gu.support.catalog.FulfilmentOptions._
-import com.gu.support.config.{Stages, TouchPointEnvironment}
-import com.gu.support.workers.BillingPeriod
+import com.gu.support.catalog.GuardianWeekly.getProductRatePlan
+import com.gu.support.config.TouchPointEnvironment
+import com.gu.support.workers.{BillingPeriod, Quarterly, SixWeekly}
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
 
@@ -14,6 +14,34 @@ object CatalogService {
 }
 
 class CatalogService(val environment: TouchPointEnvironment, jsonProvider: CatalogJsonProvider) extends LazyLogging {
+
+  private def getRatePlanId(billingPeriod: BillingPeriod, fulfilmentOptions: FulfilmentOptions)
+    = getProductRatePlan(environment, billingPeriod,fulfilmentOptions, NoProductOptions).map(_.id).getOrElse("")
+
+  private def fetchQuarterlyPrice(quarterlyId: ProductRatePlanId, sixWeeklyPriceList: Pricelist, catalogPrices: List[Pricelist]) = {
+    Pricelist(sixWeeklyPriceList.productRatePlanId,
+      catalogPrices.find(_.productRatePlanId == quarterlyId).map(_.prices).getOrElse(sixWeeklyPriceList.prices)
+    )
+  }
+
+  def adjustSixWeeklyPriceList(c: Catalog) = {
+    // The price stored for the 6 weekly billing period in Zuora is £6 - the price of the
+    // promotion. It is much more use from the point of view of the site to have the subscription
+    // price, ie. the quarterly price as the £6 is available through the introductory promotion object
+    val ratePlanIdsToSwap = Map(
+      getRatePlanId(SixWeekly, Domestic) -> getRatePlanId(Quarterly, Domestic),
+      getRatePlanId(SixWeekly, RestOfWorld) -> getRatePlanId(Quarterly, RestOfWorld)
+    )
+
+    Catalog(
+      c.prices.map(priceList =>
+        ratePlanIdsToSwap.get(priceList.productRatePlanId)
+          .map(quarterlyId => fetchQuarterlyPrice(quarterlyId, priceList, c.prices))
+          .getOrElse(priceList)
+      )
+    )
+  }
+
   private lazy val catalog = {
 
     jsonProvider.get.flatMap { json =>
@@ -26,7 +54,7 @@ class CatalogService(val environment: TouchPointEnvironment, jsonProvider: Catal
         },
         c => {
           logger.info(s"Successfully loaded the catalog")
-          Some(c)
+          Some(adjustSixWeeklyPriceList(c))
         }
       )
     }
