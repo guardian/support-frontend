86f77af13f62423c9f10b9100bd15296033ec5b0
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-11-08 15:00:26 +0000

    Remove old version of DP product page

--- support-frontend/assets/pages/digital-subscription-landing/components/cta.jsx
+++ support-frontend/assets/pages/digital-subscription-landing/components/cta.jsx
@@ -1,23 +1,19 @@
 // @flow
 import React from 'react';
 
-import PaymentSelection from 'pages/digital-subscription-landing/components/paymentSelection/paymentSelection';
+import PaymentSelection
+  from 'pages/digital-subscription-landing/components/paymentSelection/paymentSelection';
 
 import './digitalSubscriptionLanding.scss';
 
-
-type PropTypes = {
-  dailyEditionsVariant: boolean,
-}
-
-const CallToAction = (props: PropTypes) => (
+const CallToAction = () => (
   <div className="call-to-action__container">
     <div className="hope-is-power--centered">
       <h2>Choose one of our special offers and subscribe today</h2>
       <h3>After your <strong>14-day free trial</strong>, your
       subscription will begin automatically and you can cancel any time
       </h3>
-      <PaymentSelection dailyEditionsVariant={props.dailyEditionsVariant} />
+      <PaymentSelection />
     </div>
   </div>
 );
