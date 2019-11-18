84b140d037ace6955cad3c1ef29a6bb8af70b9a1
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2019-11-18 12:23:10 +0000

    Fix flow issue

--- support-frontend/assets/components/priceLabel/priceLabel.jsx
+++ support-frontend/assets/components/priceLabel/priceLabel.jsx
@@ -10,12 +10,11 @@ import {
 import type { BillingPeriod } from 'helpers/billingPeriods';
 import { getPriceDescription } from 'helpers/productPrice/priceDescriptions';
 import { getAppliedPromo, hasDiscount } from 'helpers/productPrice/promotions';
-import type { Option } from 'helpers/types/option';
 
 export type PropTypes = {
   productPrice: ProductPrice,
   billingPeriod: BillingPeriod,
-  orderIsAGift: Option<boolean>,
+  orderIsAGift?: boolean,
   giftStyles?: Object,
 }
 
@@ -42,6 +41,7 @@ function PriceLabel({
 
 PriceLabel.defaultProps = {
   giftStyles: {},
+  orderIsAGift: false,
 };
 
 export { PriceLabel };
