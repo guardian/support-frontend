68ab0047630108c94fbdc056ebe1c4ebd655ce9f
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-11-20 14:09:17 +0000

    New display names for Quarterly and Annual gifts

--- support-frontend/assets/helpers/billingPeriods.js
+++ support-frontend/assets/helpers/billingPeriods.js
@@ -5,6 +5,7 @@ const Monthly: 'Monthly' = 'Monthly';
 const Quarterly: 'Quarterly' = 'Quarterly';
 const SixWeekly: 'SixWeekly' = 'SixWeekly';
 const weeklyBillingPeriods = [SixWeekly, Quarterly, Annual];
+const weeklyGiftPeriods = [Quarterly, Annual];
 export type BillingPeriod = typeof SixWeekly | typeof Annual | typeof Monthly | typeof Quarterly;
 export type DigitalBillingPeriod = typeof Monthly | typeof Annual;
 export type WeeklyBillingPeriod = typeof SixWeekly | typeof Quarterly | typeof Annual;
@@ -23,9 +24,26 @@ function billingPeriodNoun(billingPeriod: BillingPeriod, fixedTerm: boolean = fa
   }
 }
 
-function billingPeriodTitle(billingPeriod: BillingPeriod) {
-  if (billingPeriod === SixWeekly) { return '6 for 6'; }
-  return billingPeriod;
+function billingPeriodTitle(billingPeriod: BillingPeriod, fixedTerm: boolean = false) {
+  switch (billingPeriod) {
+    case Annual:
+      return fixedTerm ? '12 months' : billingPeriod;
+    case Quarterly:
+      return fixedTerm ? '3 months' : billingPeriod;
+    case SixWeekly:
+      return '6 for 6';
+    default:
+      return billingPeriod;
+  }
 }
 
-export { Annual, Monthly, Quarterly, SixWeekly, billingPeriodNoun, billingPeriodTitle, weeklyBillingPeriods };
+export {
+  Annual,
+  Monthly,
+  Quarterly,
+  SixWeekly,
+  billingPeriodNoun,
+  billingPeriodTitle,
+  weeklyBillingPeriods,
+  weeklyGiftPeriods,
+};
