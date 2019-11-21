7fc553aa6f813aa68aa9f5b371478e8fa150c85f
Rupert Bates <Rupert.Bates@guardian.co.uk>
2019-11-21 17:49:31 +0000

    Read full state in FailureHandler lambda

--- support-models/src/main/scala/com/gu/support/workers/states/FailureHandlerState.scala
+++ support-models/src/main/scala/com/gu/support/workers/states/FailureHandlerState.scala
@@ -2,16 +2,24 @@ package com.gu.support.workers.states
 
 import java.util.UUID
 
+import com.gu.support.promotions.PromoCode
 import com.gu.support.workers.{User, _}
+import org.joda.time.LocalDate
 
 case class FailureHandlerState(
   requestId: UUID,
   user: User,
-  product: ProductType
+  giftRecipient: Option[GiftRecipient],
+  product: ProductType,
+  paymentFields: Option[PaymentFields], //Will be present if CreatePaymentMethod failed
+  paymentMethod: Option[PaymentMethod], //Will be present if anything after CreatePaymentMethod failed
+  firstDeliveryDate: Option[LocalDate],
+  promoCode: Option[PromoCode]
 ) extends StepFunctionUserState
 
 import com.gu.support.encoding.Codec
 import com.gu.support.encoding.Codec._
+import com.gu.support.encoding.CustomCodecs.{decodeLocalTime, encodeLocalTime}
 
 object FailureHandlerState {
   implicit val codec: Codec[FailureHandlerState] = deriveCodec
