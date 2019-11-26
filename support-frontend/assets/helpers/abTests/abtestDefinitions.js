4c1e5d6f0e36212d6ab173947b884c33217dc4df
TJ Silver <tjsilver@protonmail.com>
2019-11-26 16:55:50 +0000

    capitalise variant type

--- support-frontend/assets/helpers/abTests/abtestDefinitions.js
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.js
@@ -7,7 +7,7 @@ import {
 
 // ----- Tests ----- //
 export type RecurringStripePaymentRequestButtonTestVariants = 'control' | 'paymentRequestButton' | 'notintest';
-export type paymentSecuritySecureTransactionGreyNonUKVariants = 'control' | 'V1_securetransactiongrey' | 'notintest';
+export type PaymentSecuritySecureTransactionGreyNonUKVariants = 'control' | 'V1_securetransactiongrey' | 'notintest';
 export type LandingPageReverseAmountsTestVariant = 'control' | 'reversedAmounts' | 'notintest';
 export type NewLandingPageTemplateTestVariants = 'control' | 'new_template' | 'notintest';
 
