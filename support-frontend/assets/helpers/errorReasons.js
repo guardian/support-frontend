feea8fb90aa068fc2c26c66b5044c56bf8b0a784
Tom Forbes <tom.forbes@theguardian.com>
2019-12-04 15:01:58 +0000

    amazon pay errors

--- support-frontend/assets/helpers/errorReasons.js
+++ support-frontend/assets/helpers/errorReasons.js
@@ -2,8 +2,13 @@
 
 // ----- Types ----- //
 
+export type AmazonPayErrorReason =
+  'amazon_pay_try_other_card' |
+  'amazon_pay_try_again' |
+  'amazon_pay_fatal';
+
 // See https://github.com/guardian/support-models/blob/master/src/main/scala/com/gu/support/workers/model/CheckoutFailureReasons.scala
-export type ErrorReason =
+export type ErrorReason = AmazonPayErrorReason &
   'insufficient_funds' |
   'payment_details_incorrect' |
   'personal_details_incorrect' |
@@ -27,14 +32,17 @@ function appropriateErrorMessage(errorReason: ?ErrorReason): ?string {
     case 'insufficient_funds':
       return 'The transaction was declined due to insufficient funds in your account. Please use a different card or contact your bank.';
     case 'payment_details_incorrect':
+    case 'amazon_pay_try_again':
       return 'An error occurred while trying to process your payment. Please double check your card details and try again. Alternatively, try another card or payment method.';
     case 'personal_details_incorrect':
       return 'Please double check the name and contact details you provided and try again.';
     case 'payment_method_temporarily_declined':
       return 'The transaction was temporarily declined. Please try entering your payment details again. Alternatively, try another payment method.';
     case 'payment_method_unacceptable':
+    case 'amazon_pay_try_other_card':
       return 'The transaction was unsuccessful and you have not been charged. Please use a different card or choose another payment method.';
     case 'payment_provider_unavailable':
+    case 'amazon_pay_fatal':
       return 'The transaction was unsuccessful. This does not mean there’s anything wrong with your card, and you have not been charged. Please try using an alternative payment method.';
     case 'all_payment_methods_unavailable':
       return 'Sorry, our payment methods are unavailable at this time. We are working hard to fix the problem and hope to be back up and running soon. Please come back later to complete your contribution or consider another type of contribution from the tabs above. Thank you.';
