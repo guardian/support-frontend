c5e87a9d5162a0b85039b21f30dd689af7f387eb
Tom Forbes <tom.forbes@theguardian.com>
2019-12-10 16:49:44 +0000

    load appropriate script in setup function

--- support-frontend/assets/helpers/paymentIntegrations/amazonPay.js
+++ support-frontend/assets/helpers/paymentIntegrations/amazonPay.js
@@ -3,6 +3,7 @@
 import { setAmazonPayLibrary } from 'pages/contributions-landing/contributionsLandingActions';
 import { isProd } from 'helpers/url';
 import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
+import { logException } from 'helpers/logger';
 
 const getAmazonRegion = (countryGroupId: CountryGroupId, amazonLoginObject: Object): ?string => {
   switch (countryGroupId) {
@@ -19,13 +20,15 @@ const getAmazonPayClientId = (isTestUser: boolean): string => (isTestUser ?
   window.guardian.amazonPayClientId.default);
 
 const setupAmazonPay = (countryGroupId: CountryGroupId, dispatch: Function, isTestUser: boolean): void => {
+  const isSandbox = !isProd();
+
   window.setOnAmazonReady((amazonLoginObject, amazonPaymentsObject) => {
     const amazonRegion = getAmazonRegion(countryGroupId, amazonLoginObject);
     if (amazonRegion) {
       const clientId = getAmazonPayClientId(isTestUser);
       amazonLoginObject.setClientId(clientId);
 
-      if (isTestUser || !isProd()) {
+      if (isTestUser || isSandbox) {
         amazonLoginObject.setSandboxMode(true);
       }
 
@@ -33,6 +36,24 @@ const setupAmazonPay = (countryGroupId: CountryGroupId, dispatch: Function, isTe
 
       dispatch(setAmazonPayLibrary({ amazonLoginObject, amazonPaymentsObject }));
     }
+  });
+
+  // Amazon pay requires us to use a different script for sandbox mode
+  const widgetsUrl = isTestUser || isSandbox ?
+    'https://static-na.payments-amazon.com/OffAmazonPayments/us/sandbox/js/Widgets.js' :
+    'https://static-na.payments-amazon.com/OffAmazonPayments/us/js/Widgets.js';
+
+  new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+
+    script.onerror = reject;
+    script.src = widgetsUrl;
+
+    if (document.head) {
+      document.head.appendChild(script);
+    }
+  }).catch(error => {
+    logException(`Error loading ${widgetsUrl}`, error)
   });
 };
 
