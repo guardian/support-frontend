3cc7b362d2a117666368f11758657e4624b59294
Tom Forbes <tom.forbes@theguardian.com>
2019-12-11 11:09:45 +0000

    revert webpack change

--- support-frontend/webpack.prod.js
+++ support-frontend/webpack.prod.js
@@ -1,15 +1,15 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common('[name].[chunkhash].css', '[name].[chunkhash].js', true), {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: 'production',
+  devtool: 'source-map',
   plugins: [
-    // new UglifyJSPlugin({
-    //   sourceMap: true,
-    // }),
+    new UglifyJSPlugin({
+      sourceMap: true,
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
