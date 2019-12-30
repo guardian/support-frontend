2b1b84d1cc43cea008c30716ee51f072c14fb0f6
Joshua <jlieb10@users.noreply.github.com>
2019-12-30 11:45:54 +0000

    Revert "temporarily disabling amazon pay for our biggest contribution days of the year (#2314)"
    This reverts commit b639a4e885c07e1dfc4aeff8a173fcaa3b8cf3cb.

--- support-frontend/assets/helpers/abTests/abtestDefinitions.js
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.js
@@ -34,7 +34,7 @@ export const tests: Tests = {
         size: 1,
       },
     },
-    isActive: false,
+    isActive: true,
     independent: true,
     seed: 1,
     targetPage: usContributionsLandingPageMatch,
