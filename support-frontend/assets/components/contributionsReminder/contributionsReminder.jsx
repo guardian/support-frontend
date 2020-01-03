e8ed5a6c8586fc04444670c3ec6768fafcf11578
Joshua <joshuathomaslieberman@gmail.com>
2020-01-03 12:23:25 +0000

    wip

--- support-frontend/assets/components/contributionsReminder/contributionsReminder.jsx
+++ support-frontend/assets/components/contributionsReminder/contributionsReminder.jsx
@@ -3,14 +3,15 @@
 // ----- Imports ----- //
 
 import React, { Component } from 'react';
-import SvgSubscribe from 'components/svgs/subscribe';
-import SvgSubscribed from 'components/svgs/subscribed';
 import TrackableButton from 'components/button/trackableButton';
 import { trackComponentClick, trackComponentLoad } from 'helpers/tracking/behaviour';
 import { connect } from 'react-redux';
 import type { Csrf as CsrfState } from 'helpers/csrf/csrfReducer';
 import { routes } from 'helpers/routes';
 import { logException } from 'helpers/logger';
+import { Fieldset } from 'components/forms/fieldset';
+import { RadioInput } from 'components/forms/customFields/radioInput';
+import { Label as FormLabel } from 'components/forms/label';
 
 // ----- Types ----- //
 type PropTypes = {
@@ -22,6 +23,8 @@ type ButtonState = 'initial' | 'pending' | 'success' | 'fail';
 
 type StateTypes = {
   buttonState: ButtonState;
+  selectedDate: string | null;
+  selectedDateAsWord: string | null,
 }
 
 const mapStateToProps = state => ({
@@ -37,17 +40,30 @@ class ContributionsReminder extends Component<PropTypes, StateTypes> {
 
   state = {
     buttonState: 'initial',
+    selectedDate: null,
+    selectedDateAsWord: null,
   }
 
-  requestIsPending = (): void => {
-    this.setState({
-      buttonState: 'pending',
+  setDateState = (evt: any, date: string, dateAsWord: string) => {
+    if (!evt.target.checked || !date) {
+      return null;
+    }
+
+    return this.setState({
+      selectedDate: date,
+      selectedDateAsWord: dateAsWord,
     });
   }
 
   requestHasSucceeded = (): void => {
     this.setState({
       buttonState: 'success',
+    });
+  }
+
+  requestIsPending = (): void => {
+    this.setState({
+      buttonState: 'pending',
     });
   }
 
@@ -60,7 +76,7 @@ class ContributionsReminder extends Component<PropTypes, StateTypes> {
   renderButtonCopy = (buttonState: ButtonState): string => {
     switch (buttonState) {
       case 'initial':
-        return 'Remind me in March';
+        return 'Set my reminder';
       case 'pending':
         return 'Pending...';
       case 'success':
@@ -68,12 +84,13 @@ class ContributionsReminder extends Component<PropTypes, StateTypes> {
       case 'fail':
         return 'Sorry, something went wrong';
       default:
-        return 'Remind me in March';
+        return 'Set my reminder';
     }
   }
 
   render() {
     const { email, csrf } = this.props;
+
     const requestParams = {
       method: 'POST',
       headers: {
@@ -82,16 +99,15 @@ class ContributionsReminder extends Component<PropTypes, StateTypes> {
       },
       body: {
         email,
-        reminderDate: '2020-03-19 00:00:00',
+        reminderDate: this.state.selectedDate,
       },
     };
 
     if (email && csrf.token) {
-      const isSuccess = this.state.buttonState === 'success';
       const isClicked = this.state.buttonState !== 'initial';
 
       return (
-        <section className="contribution-thank-you-block">
+        <section className="contribution-thank-you-block contribution-thank-you-block--reminders">
           <h3 className="contribution-thank-you-block__title">
             Set up a reminder for the end of the year
           </h3>
@@ -99,11 +115,19 @@ class ContributionsReminder extends Component<PropTypes, StateTypes> {
             {'Lots of readers choose to make single contributions at various points in the year. Opt in to receive a reminder in case you would like to support our journalism again. This will be a single email, with no obligation.'}
           </p>
 
+          <FormLabel label="Remind me in:" htmlFor={null}>
+            <Fieldset legend="Choose one of the following dates to receive your reminder:">
+              <RadioInput id="march2020" text="March 2020" name="reminder" onChange={evt => this.setDateState(evt, '2020-03-19 00:00:00', 'march2020')} />
+              <RadioInput id="june2020" text="June 2020" name="reminder" onChange={evt => this.setDateState(evt, '2020-06-19 00:00:00', 'june2020')} />
+              <RadioInput id="december2020" text="December 2020" name="reminder" onChange={evt => this.setDateState(evt, '2020-12-19 00:00:00', 'december2020')} />
+            </Fieldset>
+          </FormLabel>
+
           <TrackableButton
-            icon={isSuccess ? <SvgSubscribed /> : <SvgSubscribe />}
+            icon={false}
             aria-label={this.renderButtonCopy(this.state.buttonState)}
-            appearance={isClicked ? 'disabled' : 'primary'}
-            disabled={isClicked}
+            appearance={isClicked ? 'disabled' : 'secondary'}
+            disabled={isClicked || !this.state.selectedDate}
             trackingEvent={
               () => {
                 trackComponentLoad('reminder-test-link-loaded');
@@ -114,6 +138,7 @@ class ContributionsReminder extends Component<PropTypes, StateTypes> {
                 this.requestIsPending();
 
                 trackComponentClick('reminder-test-link-clicked');
+                if (this.state.selectedDateAsWord) { trackComponentClick(`reminder-test-date-${this.state.selectedDateAsWord}`); }
 
                 fetch(routes.createReminder, {
                   method: requestParams.method,
