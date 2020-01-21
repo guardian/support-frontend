b6de1179cce97482d3f07477562b8bdfdb720d5c
Joshua <joshuathomaslieberman@gmail.com>
2020-01-21 15:59:40 +0000

    removing eoy campaign landing page and related tests

--- support-frontend/assets/helpers/campaigns.jsx
+++ support-frontend/assets/helpers/campaigns.jsx
@@ -1,6 +1,5 @@
 // @flow
 
-import React from 'react';
 import type { ContributionTypes } from 'helpers/contributions';
 
 export type TickerType = 'unlimited' | 'hardstop';
@@ -28,45 +27,9 @@ export type Campaigns = {
   [string]: CampaignSettings,
 };
 
-const currentCampaignName = 'us/contribute';
+const currentCampaignName = null;
 
-export const campaigns: Campaigns = {
-  [currentCampaignName]: {
-    termsAndConditions: (contributionsTermsLink: string, contactEmail: string) => (
-      <div className="component-terms-privacy component-terms-privacy--campaign-landing">
-        <p>
-          Monthly contributions are billed each month and annual contributions are billed
-          once a year. You can change how much you give or cancel your contributions at any time.
-        </p>
-        <p>
-          By proceeding, you are agreeing to our <a href={contributionsTermsLink}>Terms and Conditions</a>.
-          To find out what personal data we collect and how we use it, please visit
-          our <a href="https://www.theguardian.com/help/privacy-policy">Privacy Policy</a>.
-        </p>
-        <span className="component-terms-privacy__philo-ask">
-          <p className="component-terms-privacy__philo-ask-header">
-            Contribute another way
-          </p>
-          <p className="component-terms-privacy__philo-ask-body">
-            Please <a href={`mailto:${contactEmail || ''}`}>contact us</a> if you would like to: make a
-            larger contribution as an individual, contribute as a company or foundation, or would
-            like to discuss legacy gifting. Thank you for your generous support.
-          </p>
-        </span>
-      </div>
-    ),
-    cssModifiers: ['eoy2019'],
-    extraComponent: (
-      <div className="eoy2019_image" />
-    ),
-    tickerSettings: {
-      goalReachedCopy: null,
-      tickerJsonUrl: '/ticker.json',
-      tickerType: 'hardstop',
-      localCurrencySymbol: '$',
-    },
-  },
-};
+export const campaigns: Campaigns = {};
 
 export type CampaignName = $Keys<typeof campaigns>
 
