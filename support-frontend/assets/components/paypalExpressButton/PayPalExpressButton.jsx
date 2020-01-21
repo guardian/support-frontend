4d9c75798d1ea10c640c90e9223ff84ce7b94b3a
TJ Silver <tjsilver@protonmail.com>
2020-01-21 15:44:17 +0000

    Revert "wip use hooks"
    This reverts commit fb38b372a7b23311c7f9b3a4ee00553c9fc7d1d4.

--- support-frontend/assets/components/paypalExpressButton/PayPalExpressButton.jsx
+++ support-frontend/assets/components/paypalExpressButton/PayPalExpressButton.jsx
@@ -43,80 +43,42 @@ type PropTypes = {|
 // 1. Loading this iframe is an expensive operation which causes an obvious visual re-render
 // 2. We don't want to have to re-bind handlers which interact with the iframe
 //    (e.g. the handler bound in getPayPalOptions)
+export class PayPalExpressButton extends React.Component<PropTypes> {
+  shouldComponentUpdate(nextProps: PropTypes) {
+    return (this.props.hasLoaded !== nextProps.hasLoaded);
+  }
 
-const tokenToAuthorisation = (token: string): PayPalAuthorisation => ({
-  paymentMethod: PayPal,
-  token,
-});
+  props: PropTypes;
 
+  render() {
+    // hasLoaded determines whether window.paypal is available
+    if (!this.props.hasLoaded) {
+      return null;
+    }
+    const tokenToAuthorisation = (token: string): PayPalAuthorisation => ({
+      paymentMethod: PayPal,
+      token,
+    });
 
-const PayPalExpressButton = (props: PropTypes) => {
-  console.log('PaypalExpressButton loaded', props.hasLoaded)
-  const onPaymentAuthorisation = (token: string): void => {
-    props.onPaymentAuthorisation(tokenToAuthorisation(token));
-  };
-  // hasLoaded determines whether window.paypal is available
-  // const [
-  //   loaded,
-  //   // setLoaded
-  // ] = React.useState(props.hasLoaded);
-  // console.log('initial loaded state', loaded);
-  // if (!loaded) {
-  //   console.log('NOT loaded', loaded);
-  //   // return null;
-  // }
+    const onPaymentAuthorisation = (token: string): void => {
+      this.props.onPaymentAuthorisation(tokenToAuthorisation(token));
+    };
 
-  React.useEffect(() => {
-    console.log('Behavior when the component receives new state or props.', props.hasLoaded);
-    // setLoaded(loaded);
-    console.log('loaded is', props.hasLoaded);
-  }, [props.hasLoaded]);
-  // });
-
-  if (props.hasLoaded) {
     // This element contains an iframe which contains the actual button
-    console.log('CREATE element', props.hasLoaded);
     return React.createElement(
       window.paypal.Button.driver('react', { React, ReactDOM }),
       getPayPalOptions(
-        props.currencyId,
-        props.csrf,
+        this.props.currencyId,
+        this.props.csrf,
         onPaymentAuthorisation,
-        props.canOpen,
-        props.onClick,
-        props.formClassName,
-        props.isTestUser,
-        props.amount,
-        props.billingPeriod,
-        props.setupRecurringPayPalPayment,
+        this.props.canOpen,
+        this.props.onClick,
+        this.props.formClassName,
+        this.props.isTestUser,
+        this.props.amount,
+        this.props.billingPeriod,
+        this.props.setupRecurringPayPalPayment,
       ),
     );
   }
-  console.log('returning NULL');
-  return null;
-  // // This element contains an iframe which contains the actual button
-  // console.log('CREATE element', props.hasLoaded);
-  // return React.createElement(
-  //   window.paypal.Button.driver('react', { React, ReactDOM }),
-  //   getPayPalOptions(
-  //     props.currencyId,
-  //     props.csrf,
-  //     onPaymentAuthorisation,
-  //     props.canOpen,
-  //     props.onClick,
-  //     props.formClassName,
-  //     props.isTestUser,
-  //     props.amount,
-  //     props.billingPeriod,
-  //     props.setupRecurringPayPalPayment,
-  //   ),
-  // );
-};
-
-export default React.memo<PropTypes>(PayPalExpressButton, (prevProps, nextProps) => {
-  // Only re-render when hasLoaded  changes
-  console.log('MEMO', prevProps.hasLoaded === nextProps.hasLoaded);
-  return prevProps.hasLoaded === nextProps.hasLoaded;
-});
-
-// export default PayPalExpressButton;
+}
