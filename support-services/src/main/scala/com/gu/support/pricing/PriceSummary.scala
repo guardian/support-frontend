0363ce937817e598195a43c4576b6d6f66c98d80
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-01-24 17:24:47 +0000

    Send saving to js `productPrices` array

--- support-services/src/main/scala/com/gu/support/pricing/PriceSummary.scala
+++ support-services/src/main/scala/com/gu/support/pricing/PriceSummary.scala
@@ -6,6 +6,7 @@ import com.gu.support.promotions._
 
 case class PriceSummary(
   price: BigDecimal,
+  saving: Int,
   currency: Currency,
   fixedTerm: Boolean,
   promotions: List[PromotionSummary]
