7af668a757cbcdf328f3071cc1043620e81cd3f7
damien-shaw-guardian <damien.shaw.freelancer@guardian.co.uk>
2020-02-05 18:45:43 +0000

    lint: disabled no-unused-expressions
    this is a dynamic import, which is special case for this eslint rule

--- support-frontend/assets/helpers/page/page.js
+++ support-frontend/assets/helpers/page/page.js
@@ -46,6 +46,7 @@ import { getSettings } from 'helpers/globals';
 import { doNotTrack } from 'helpers/tracking/doNotTrack';
 
 if (process.env.NODE_ENV === 'DEV') {
+  // eslint-disable-next-line no-unused-expressions
   import('preact/devtools');
 }
 
