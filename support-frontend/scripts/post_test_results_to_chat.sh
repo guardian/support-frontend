05ed0f1f0179fb466963695a34b3c55554824423
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2020-02-21 11:00:24 +0000

    Post message on failed build

---
+++ support-frontend/scripts/post_test_results_to_chat.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -H 'Content-Type: application/json' "${GOOGLE_CHAT_WEB_HOOK}" -d '{"text": " ❌ The post deployment tests for support frontend have failed! \n \n 👉 <https://travis-ci.org/guardian/support-frontend|Travis build> \n 🤖 <https://automate.browserstack.com/dashboard/v2/builds/31f35a1d9bccc9d45360aa7bfd651fcd9e1499d0|Browser stack test results> \n \n 📖 <https://github.com/guardian/support-frontend/wiki/Post-deployment-test-runbook|Check the runbook for a step by step guide>"}'
