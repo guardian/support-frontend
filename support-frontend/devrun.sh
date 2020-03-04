e81307e807dd00747033d696bd8fb869378d565a
Joshua <joshuathomaslieberman@gmail.com>
2020-03-04 16:12:38 +0000

    made sourcing more robust

--- support-frontend/devrun.sh
+++ support-frontend/devrun.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
 # source NVM inside script before invoking it
-. ~/.nvm/nvm.sh
+nvm_available() {
+  type -t nvm > /dev/null
+}
+
+source_nvm() {
+  if ! nvm_available; then
+    [ -e "/usr/local/opt/nvm/nvm.sh" ] && source /usr/local/opt/nvm/nvm.sh
+  fi
+  if ! nvm_available; then
+    [ -e "$HOME/.nvm/nvm.sh" ] && source $HOME/.nvm/nvm.sh
+  fi
+}
+
+source_nvm
 
 nvm use &
 yarn devrun &
