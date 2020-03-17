8ac53afa55f96ae218122464a3cfa6dcd3b2fa35
John Duffell <john.duffell@guardian.co.uk>
2020-03-17 14:53:43 +0000

    test user firstname will end up in username field

--- support-frontend/app/controllers/RegularContributions.scala
+++ support-frontend/app/controllers/RegularContributions.scala
@@ -130,7 +130,7 @@ class RegularContributions(
       // TODO: in a subsequent PR set these values based on the respective user.
       allowThirdPartyMail = false,
       allowGURelatedMail = false,
-      isTestUser = testUsers.isTestUser(user.privateFields.firstName)
+      isTestUser = testUsers.isTestUser(user.publicFields.username)
     )
   }
 
