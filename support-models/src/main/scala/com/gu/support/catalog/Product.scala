2cdc97d4b0fecf7235001b445fd7e994eb89600b
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-03-24 12:01:00 +0000

    Add in product structure for new digital voucher

--- support-models/src/main/scala/com/gu/support/catalog/Product.scala
+++ support-models/src/main/scala/com/gu/support/catalog/Product.scala
@@ -6,6 +6,8 @@ import com.gu.support.config.TouchPointEnvironments.{PROD, SANDBOX, UAT}
 import com.gu.support.workers._
 import io.circe.{Decoder, Encoder}
 
+import scala.collection.immutable
+
 sealed trait Product {
   val ratePlans: Map[TouchPointEnvironment, List[ProductRatePlan[Product]]]
 
@@ -67,76 +69,137 @@ case object Contribution extends Product {
 }
 
 case object Paper extends Product {
-  private lazy val ukOnly = List(CountryGroup.UK)
+
+  private def collection(productRatePlanId: ProductRatePlanId, productOptions: ProductOptions, description: String): ProductRatePlan[Paper.type] =
+    ProductRatePlan(productRatePlanId, Monthly, Collection, productOptions, description, List(CountryGroup.UK))
+
+  private def homeDelivery(productRatePlanId: ProductRatePlanId, productOptions: ProductOptions, description: String): ProductRatePlan[Paper.type] =
+    ProductRatePlan(productRatePlanId, Monthly, HomeDelivery, productOptions, description, List(CountryGroup.UK))
+
+  private lazy val useDigitalVoucher = true
+
+  private val prodCollection: List[ProductRatePlan[Paper.type]] = if(useDigitalVoucher) {
+    List(
+      collection("2c92a00870ec598001710740ce702ff0", SaturdayPlus, "Voucher Saturday+"),
+      collection("2c92a00870ec598001710740cdd02fbd", Saturday, "Voucher Saturday"),
+      collection("2c92a00870ec598001710740cf9e3004", SundayPlus, "Voucher Sunday+"),
+      collection("2c92a00870ec598001710740d0d83017", Sunday, "Voucher Sunday"),
+      collection("2c92a00870ec598001710740c6672ee7", WeekendPlus, "Voucher Weekend+"),
+      collection("2c92a00870ec598001710740d24b3022", Weekend, "Voucher Weekend"),
+      collection("2c92a00870ec598001710740c4582ead", SixdayPlus, "Voucher Sixday+"),
+      collection("2c92a00870ec598001710740ca532f69", Sixday, "Voucher Sixday"),
+      collection("2c92a00870ec598001710740d3d03035", EverydayPlus, "Voucher Everyday+"),
+      collection("2c92a00870ec598001710740c78d2f13", Everyday, "Voucher Everyday")
+    )
+  } else {
+    List(
+      collection("2c92a0fd6205707201621fa1350710e3", SaturdayPlus, "Voucher Saturday+"),
+      collection("2c92a0fd6205707201621f9f6d7e0116", Saturday, "Voucher Saturday"),
+      collection("2c92a0fe56fe33ff0157040d4b824168", SundayPlus, "Voucher Sunday+"),
+      collection("2c92a0fe5af9a6b9015b0fe1ecc0116c", Sunday, "Voucher Sunday"),
+      collection("2c92a0fd56fe26b60157040cdd323f76", WeekendPlus, "Voucher Weekend+"),
+      collection("2c92a0ff56fe33f00157040f9a537f4b", Weekend, "Voucher Weekend"),
+      collection("2c92a0fc56fe26ba0157040c5ea17f6a", SixdayPlus, "Voucher Sixday+"),
+      collection("2c92a0fd56fe270b0157040e42e536ef", Sixday, "Voucher Sixday"),
+      collection("2c92a0ff56fe33f50157040bbdcf3ae4", EverydayPlus, "Voucher Everyday+"),
+      collection("2c92a0fd56fe270b0157040dd79b35da", Everyday, "Voucher Everyday")
+    )
+  }
+
+  private val uatCollection: List[ProductRatePlan[Paper.type]] = if(useDigitalVoucher) {
+    List(
+      collection("2c92c0f961f9cf350161fc0454283f3e", SaturdayPlus, "Voucher Saturday+"),
+      collection("2c92c0f961f9cf300161fc02a7d805c9", Saturday, "Voucher Saturday"),
+      collection("2c92c0f858aa38af0158b9dae19110a3", SundayPlus, "Voucher Sunday+"),
+      collection("2c92c0f95aff3b54015b0ee0eb500b2e", Sunday, "Voucher Sunday"),
+      collection("2c92c0f855c9f4b20155d9f1dd0651ab", WeekendPlus, "Voucher Weekends+"),
+      collection("2c92c0f855c9f4b20155d9f1db9b5199", Weekend, "Voucher Weekend"),
+      collection("2c92c0f855c9f4540155da2607db6402", SixdayPlus, "Voucher Sixday+"),
+      collection("2c92c0f955ca02910155da254a641fb3", Sixday, "Voucher Sixday"),
+      collection("2c92c0f955ca02920155da240cdb4399", EverydayPlus, "Voucher Everyday+"),
+      collection("2c92c0f855c9f4b20155d9f1d3d4512a", Everyday, "Voucher Everyday"),
+    )
+  } else {
+    List(
+      collection("2c92c0f961f9cf350161fc0454283f3e", SaturdayPlus, "Voucher Saturday+"),
+      collection("2c92c0f961f9cf300161fc02a7d805c9", Saturday, "Voucher Saturday"),
+      collection("2c92c0f858aa38af0158b9dae19110a3", SundayPlus, "Voucher Sunday+"),
+      collection("2c92c0f95aff3b54015b0ee0eb500b2e", Sunday, "Voucher Sunday"),
+      collection("2c92c0f855c9f4b20155d9f1dd0651ab", WeekendPlus, "Voucher Weekends+"),
+      collection("2c92c0f855c9f4b20155d9f1db9b5199", Weekend, "Voucher Weekend"),
+      collection("2c92c0f855c9f4540155da2607db6402", SixdayPlus, "Voucher Sixday+"),
+      collection("2c92c0f955ca02910155da254a641fb3", Sixday, "Voucher Sixday"),
+      collection("2c92c0f955ca02920155da240cdb4399", EverydayPlus, "Voucher Everyday+"),
+      collection("2c92c0f855c9f4b20155d9f1d3d4512a", Everyday, "Voucher Everyday"),
+    )
+  }
+
+  private val sandboxCollection: List[ProductRatePlan[Paper.type]] = if(useDigitalVoucher) {
+    List(
+      collection("2c92c0f961f9cf300161fc44f2661258", SaturdayPlus, "Voucher Saturday paper+"),
+      collection("2c92c0f861f9c26d0161fc434bfe004c", Saturday, "Voucher Saturday paper"),
+      collection("2c92c0f955a0b5bf0155b62623846fc8", SundayPlus, "Voucher Sunday paper+"),
+      collection("2c92c0f95aff3b56015b1045fb9332d2", Sunday, "Voucher Sunday paper"),
+      collection("2c92c0f95aff3b54015b1047efaa2ac3", WeekendPlus, "Voucher Weekend+"),
+      collection("2c92c0f8555ce5cf01556e7f01b81b94", Weekend, "Voucher Weekend"),
+      collection("2c92c0f855c3b8190155c585a95e6f5a", SixdayPlus, "Voucher Sixday+"),
+      collection("2c92c0f8555ce5cf01556e7f01771b8a", Sixday, "Voucher Sixday"),
+      collection("2c92c0f95aff3b53015b10469bbf5f5f", EverydayPlus, "Voucher Everyday+"),
+      collection("2c92c0f9555cf10501556e84a70440e2", Everyday, "Voucher Everyday"),
+    )
+  } else {
+    List(
+      collection("2c92c0f961f9cf300161fc44f2661258", SaturdayPlus, "Voucher Saturday paper+"),
+      collection("2c92c0f861f9c26d0161fc434bfe004c", Saturday, "Voucher Saturday paper"),
+      collection("2c92c0f955a0b5bf0155b62623846fc8", SundayPlus, "Voucher Sunday paper+"),
+      collection("2c92c0f95aff3b56015b1045fb9332d2", Sunday, "Voucher Sunday paper"),
+      collection("2c92c0f95aff3b54015b1047efaa2ac3", WeekendPlus, "Voucher Weekend+"),
+      collection("2c92c0f8555ce5cf01556e7f01b81b94", Weekend, "Voucher Weekend"),
+      collection("2c92c0f855c3b8190155c585a95e6f5a", SixdayPlus, "Voucher Sixday+"),
+      collection("2c92c0f8555ce5cf01556e7f01771b8a", Sixday, "Voucher Sixday"),
+      collection("2c92c0f95aff3b53015b10469bbf5f5f", EverydayPlus, "Voucher Everyday+"),
+      collection("2c92c0f9555cf10501556e84a70440e2", Everyday, "Voucher Everyday"),
+    )
+  }
 
   lazy val ratePlans: Map[TouchPointEnvironment, List[ProductRatePlan[Paper.type]]] =
     Map(
-      PROD -> List(
-        ProductRatePlan("2c92a0fd6205707201621fa1350710e3", Monthly, Collection, SaturdayPlus, "Voucher Saturday+", ukOnly),
-        ProductRatePlan("2c92a0fd6205707201621f9f6d7e0116", Monthly, Collection, Saturday, "Voucher Saturday", ukOnly),
-        ProductRatePlan("2c92a0fe56fe33ff0157040d4b824168", Monthly, Collection, SundayPlus, "Voucher Sunday+", ukOnly),
-        ProductRatePlan("2c92a0fe5af9a6b9015b0fe1ecc0116c", Monthly, Collection, Sunday, "Voucher Sunday", ukOnly),
-        ProductRatePlan("2c92a0fd56fe26b60157040cdd323f76", Monthly, Collection, WeekendPlus, "Voucher Weekend+", ukOnly),
-        ProductRatePlan("2c92a0ff56fe33f00157040f9a537f4b", Monthly, Collection, Weekend, "Voucher Weekend", ukOnly),
-        ProductRatePlan("2c92a0fc56fe26ba0157040c5ea17f6a", Monthly, Collection, SixdayPlus, "Voucher Sixday+", ukOnly),
-        ProductRatePlan("2c92a0fd56fe270b0157040e42e536ef", Monthly, Collection, Sixday, "Voucher Sixday", ukOnly),
-        ProductRatePlan("2c92a0ff56fe33f50157040bbdcf3ae4", Monthly, Collection, EverydayPlus, "Voucher Everyday+", ukOnly),
-        ProductRatePlan("2c92a0fd56fe270b0157040dd79b35da", Monthly, Collection, Everyday, "Voucher Everyday", ukOnly),
-        ProductRatePlan("2c92a0ff6205708e01622484bb2c4613", Monthly, HomeDelivery, SaturdayPlus, "Home Delivery Saturday+", ukOnly),
-        ProductRatePlan("2c92a0fd5e1dcf0d015e3cb39d0a7ddb", Monthly, HomeDelivery, Saturday, "Home Delivery Saturday", ukOnly),
-        ProductRatePlan("2c92a0fd560d13880156136b8e490f8b", Monthly, HomeDelivery, SundayPlus, "Home Delivery Sunday+", ukOnly),
-        ProductRatePlan("2c92a0ff5af9b657015b0fea5b653f81", Monthly, HomeDelivery, Sunday, "Home Delivery Sunday", ukOnly),
-        ProductRatePlan("2c92a0ff560d311b0156136b9f5c3968", Monthly, HomeDelivery, WeekendPlus, "Home Delivery Weekend+", ukOnly),
-        ProductRatePlan("2c92a0fd5614305c01561dc88f3275be", Monthly, HomeDelivery, Weekend, "Home Delivery Weekend", ukOnly),
-        ProductRatePlan("2c92a0ff560d311b0156136b697438a9", Monthly, HomeDelivery, SixdayPlus, "Home Delivery Sixday+", ukOnly),
-        ProductRatePlan("2c92a0ff560d311b0156136f2afe5315", Monthly, HomeDelivery, Sixday, "Home Delivery Sixday", ukOnly),
-        ProductRatePlan("2c92a0fd560d132301560e43cf041a3c", Monthly, HomeDelivery, EverydayPlus, "Home Delivery Everyday+", ukOnly),
-        ProductRatePlan("2c92a0fd560d13880156136b72e50f0c", Monthly, HomeDelivery, Everyday, "Home Delivery Everyday", ukOnly),
-      ),
-      UAT -> List(
-        ProductRatePlan("2c92c0f961f9cf350161fc0454283f3e", Monthly, Collection, SaturdayPlus, "Voucher Saturday+", ukOnly),
-        ProductRatePlan("2c92c0f961f9cf300161fc02a7d805c9", Monthly, Collection, Saturday, "Voucher Saturday", ukOnly),
-        ProductRatePlan("2c92c0f858aa38af0158b9dae19110a3", Monthly, Collection, SundayPlus, "Voucher Sunday+", ukOnly),
-        ProductRatePlan("2c92c0f95aff3b54015b0ee0eb500b2e", Monthly, Collection, Sunday, "Voucher Sunday", ukOnly),
-        ProductRatePlan("2c92c0f855c9f4b20155d9f1dd0651ab", Monthly, Collection, WeekendPlus, "Voucher Weekends+", ukOnly),
-        ProductRatePlan("2c92c0f855c9f4b20155d9f1db9b5199", Monthly, Collection, Weekend, "Voucher Weekend", ukOnly),
-        ProductRatePlan("2c92c0f855c9f4540155da2607db6402", Monthly, Collection, SixdayPlus, "Voucher Sixday+", ukOnly),
-        ProductRatePlan("2c92c0f955ca02910155da254a641fb3", Monthly, Collection, Sixday, "Voucher Sixday", ukOnly),
-        ProductRatePlan("2c92c0f955ca02920155da240cdb4399", Monthly, Collection, EverydayPlus, "Voucher Everyday+", ukOnly),
-        ProductRatePlan("2c92c0f855c9f4b20155d9f1d3d4512a", Monthly, Collection, Everyday, "Voucher Everyday", ukOnly),
-        ProductRatePlan("2c92c0f961f9cf300161fbfa943b6f54", Monthly, HomeDelivery, SaturdayPlus, "Home Delivery Saturday+", ukOnly),
-        ProductRatePlan("2c92c0f85b8fa30e015b9108a83253c7", Monthly, HomeDelivery, Saturday, "Home Delivery Saturday", ukOnly),
-        ProductRatePlan("2c92c0f955ca02900155da27f4872d4d", Monthly, HomeDelivery, SundayPlus, "Home Delivery Sunday+", ukOnly),
-        ProductRatePlan("2c92c0f95aff3b54015b0ede33bc04f2", Monthly, HomeDelivery, Sunday, "Home Delivery Sunday", ukOnly),
-        ProductRatePlan("2c92c0f955ca02900155da27f9402dad", Monthly, HomeDelivery, WeekendPlus, "Home Delivery Weekend+", ukOnly),
-        ProductRatePlan("2c92c0f955ca02900155da27f83c2d9b", Monthly, HomeDelivery, Weekend, "Home Delivery Weekend", ukOnly),
-        ProductRatePlan("2c92c0f955ca02900155da27f29e2d13", Monthly, HomeDelivery, SixdayPlus, "Home Delivery Sixday+", ukOnly),
-        ProductRatePlan("2c92c0f955ca02900155da27ff142e01", Monthly, HomeDelivery, Sixday, "Home Delivery Sixday", ukOnly),
-        ProductRatePlan("2c92c0f955ca02900155da2803b02e33", Monthly, HomeDelivery, EverydayPlus, "Home Delivery Everyday+", ukOnly),
-        ProductRatePlan("2c92c0f955ca02900155da27f55b2d5f", Monthly, HomeDelivery, Everyday, "Home Delivery Everyday", ukOnly),
-      ),
-      SANDBOX -> List(
-        ProductRatePlan("2c92c0f961f9cf300161fc44f2661258", Monthly, Collection, SaturdayPlus, "Voucher Saturday paper+", ukOnly),
-        ProductRatePlan("2c92c0f861f9c26d0161fc434bfe004c", Monthly, Collection, Saturday, "Voucher Saturday paper", ukOnly),
-        ProductRatePlan("2c92c0f955a0b5bf0155b62623846fc8", Monthly, Collection, SundayPlus, "Voucher Sunday paper+", ukOnly),
-        ProductRatePlan("2c92c0f95aff3b56015b1045fb9332d2", Monthly, Collection, Sunday, "Voucher Sunday paper", ukOnly),
-        ProductRatePlan("2c92c0f95aff3b54015b1047efaa2ac3", Monthly, Collection, WeekendPlus, "Voucher Weekend+", ukOnly),
-        ProductRatePlan("2c92c0f8555ce5cf01556e7f01b81b94", Monthly, Collection, Weekend, "Voucher Weekend", ukOnly),
-        ProductRatePlan("2c92c0f855c3b8190155c585a95e6f5a", Monthly, Collection, SixdayPlus, "Voucher Sixday+", ukOnly),
-        ProductRatePlan("2c92c0f8555ce5cf01556e7f01771b8a", Monthly, Collection, Sixday, "Voucher Sixday", ukOnly),
-        ProductRatePlan("2c92c0f95aff3b53015b10469bbf5f5f", Monthly, Collection, EverydayPlus, "Voucher Everyday+", ukOnly),
-        ProductRatePlan("2c92c0f9555cf10501556e84a70440e2", Monthly, Collection, Everyday, "Voucher Everyday", ukOnly),
-        ProductRatePlan("2c92c0f961f9cf300161fc4f71473a34", Monthly, HomeDelivery, SaturdayPlus, "Home Delivery Saturday+", ukOnly),
-        ProductRatePlan("2c92c0f961f9cf300161fc4d2e3e3664", Monthly, HomeDelivery, Saturday, "Home Delivery Saturday", ukOnly),
-        ProductRatePlan("2c92c0f955c3cf0f0155c5d9e83a3cb7", Monthly, HomeDelivery, SundayPlus, "Home Delivery Sunday+", ukOnly),
-        ProductRatePlan("2c92c0f85aff3453015b1041dfd2317f", Monthly, HomeDelivery, Sunday, "Home Delivery Sunday", ukOnly),
-        ProductRatePlan("2c92c0f95aff3b56015b104aa9a13ea5", Monthly, HomeDelivery, WeekendPlus, "Home Delivery Weekend+", ukOnly),
-        ProductRatePlan("2c92c0f955c3cf0f0155c5d9df433bf7", Monthly, HomeDelivery, Weekend, "Home Delivery Weekend", ukOnly),
-        ProductRatePlan("2c92c0f85aff33ff015b1042d4ba0a05", Monthly, HomeDelivery, SixdayPlus, "Home Delivery Sixday+", ukOnly),
-        ProductRatePlan("2c92c0f955c3cf0f0155c5d9ddf13bc5", Monthly, HomeDelivery, Sixday, "Home Delivery Sixday", ukOnly),
-        ProductRatePlan("2c92c0f85aff3453015b10496b5e3d17", Monthly, HomeDelivery, EverydayPlus, "Home Delivery Everyday+", ukOnly),
-        ProductRatePlan("2c92c0f955c3cf0f0155c5d9e2493c43", Monthly, HomeDelivery, Everyday, "Home Delivery Everyday", ukOnly),
-      )
+      PROD -> (prodCollection ++ List(
+        homeDelivery("2c92a0ff6205708e01622484bb2c4613", SaturdayPlus, "Home Delivery Saturday+"),
+        homeDelivery("2c92a0fd5e1dcf0d015e3cb39d0a7ddb", Saturday, "Home Delivery Saturday"),
+        homeDelivery("2c92a0fd560d13880156136b8e490f8b", SundayPlus, "Home Delivery Sunday+"),
+        homeDelivery("2c92a0ff5af9b657015b0fea5b653f81", Sunday, "Home Delivery Sunday"),
+        homeDelivery("2c92a0ff560d311b0156136b9f5c3968", WeekendPlus, "Home Delivery Weekend+"),
+        homeDelivery("2c92a0fd5614305c01561dc88f3275be", Weekend, "Home Delivery Weekend"),
+        homeDelivery("2c92a0ff560d311b0156136b697438a9", SixdayPlus, "Home Delivery Sixday+"),
+        homeDelivery("2c92a0ff560d311b0156136f2afe5315", Sixday, "Home Delivery Sixday"),
+        homeDelivery("2c92a0fd560d132301560e43cf041a3c", EverydayPlus, "Home Delivery Everyday+"),
+        homeDelivery("2c92a0fd560d13880156136b72e50f0c", Everyday, "Home Delivery Everyday"),
+      )),
+      UAT -> (uatCollection ++ List(
+        homeDelivery("2c92c0f961f9cf300161fbfa943b6f54", SaturdayPlus, "Home Delivery Saturday+"),
+        homeDelivery("2c92c0f85b8fa30e015b9108a83253c7", Saturday, "Home Delivery Saturday"),
+        homeDelivery("2c92c0f955ca02900155da27f4872d4d", SundayPlus, "Home Delivery Sunday+"),
+        homeDelivery("2c92c0f95aff3b54015b0ede33bc04f2", Sunday, "Home Delivery Sunday"),
+        homeDelivery("2c92c0f955ca02900155da27f9402dad", WeekendPlus, "Home Delivery Weekend+"),
+        homeDelivery("2c92c0f955ca02900155da27f83c2d9b", Weekend, "Home Delivery Weekend"),
+        homeDelivery("2c92c0f955ca02900155da27f29e2d13", SixdayPlus, "Home Delivery Sixday+"),
+        homeDelivery("2c92c0f955ca02900155da27ff142e01", Sixday, "Home Delivery Sixday"),
+        homeDelivery("2c92c0f955ca02900155da2803b02e33", EverydayPlus, "Home Delivery Everyday+"),
+        homeDelivery("2c92c0f955ca02900155da27f55b2d5f", Everyday, "Home Delivery Everyday"),
+      )),
+      SANDBOX -> (sandboxCollection ++ List(
+        homeDelivery("2c92c0f961f9cf300161fc4f71473a34", SaturdayPlus, "Home Delivery Saturday+"),
+        homeDelivery("2c92c0f961f9cf300161fc4d2e3e3664", Saturday, "Home Delivery Saturday"),
+        homeDelivery("2c92c0f955c3cf0f0155c5d9e83a3cb7", SundayPlus, "Home Delivery Sunday+"),
+        homeDelivery("2c92c0f85aff3453015b1041dfd2317f", Sunday, "Home Delivery Sunday"),
+        homeDelivery("2c92c0f95aff3b56015b104aa9a13ea5", WeekendPlus, "Home Delivery Weekend+"),
+        homeDelivery("2c92c0f955c3cf0f0155c5d9df433bf7", Weekend, "Home Delivery Weekend"),
+        homeDelivery("2c92c0f85aff33ff015b1042d4ba0a05", SixdayPlus, "Home Delivery Sixday+"),
+        homeDelivery("2c92c0f955c3cf0f0155c5d9ddf13bc5", Sixday, "Home Delivery Sixday"),
+        homeDelivery("2c92c0f85aff3453015b10496b5e3d17", EverydayPlus, "Home Delivery Everyday+"),
+        homeDelivery("2c92c0f955c3cf0f0155c5d9e2493c43", Everyday, "Home Delivery Everyday"),
+      ))
     )
 }
 
