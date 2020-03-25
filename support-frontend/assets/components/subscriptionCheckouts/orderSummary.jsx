0c53b8ecd57602f3c0b276eb8d7b2d69cdfec000
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2020-03-25 12:29:29 +0000

    Add media queries for change link and tablet view

--- support-frontend/assets/components/subscriptionCheckouts/orderSummary.jsx
+++ support-frontend/assets/components/subscriptionCheckouts/orderSummary.jsx
@@ -4,11 +4,10 @@ import React from 'react';
 import { css } from '@emotion/core';
 import { headline, body, textSans } from '@guardian/src-foundations/typography';
 import { space } from '@guardian/src-foundations';
-import { border, brandAltBackground } from '@guardian/src-foundations/palette';
+import { border, brandAltBackground, text } from '@guardian/src-foundations/palette';
 
 import { type ProductPrice } from 'helpers/productPrice/productPrices';
 import type { BillingPeriod } from 'helpers/billingPeriods';
-import type { SubscriptionProduct } from 'helpers/subscriptions';
 import typeof GridImageType from 'components/gridImage/gridImage';
 import { type GridImg } from 'components/gridImage/gridImage';
 import { getPriceDescription } from 'helpers/productPrice/priceDescriptions';
@@ -20,16 +19,24 @@ export type DataListItem = {
 
 type PropTypes = {
   billingPeriod: BillingPeriod,
+  // eslint-disable-next-line react/no-unused-prop-types
   changeSubscription?: string | null,
   image: $Call<GridImageType, GridImg>,
   productPrice: ProductPrice,
   title: string,
-  // eslint-disable-next-line react/no-unused-prop-types
-  product: SubscriptionProduct,
 };
 
 const wrapper = css`
   padding: ${space[3]}px;
+  color: ${text.primary};
+  a, a:visited {
+    display: none;
+    @media (min-width: 660px) {
+      display: block;
+      ${textSans.medium()};
+      color: ${text.primary};
+    }
+  }
 `;
 
 const topLine = css`
@@ -37,16 +44,20 @@ const topLine = css`
   display: flex;
   justify-content: space-between;
   margin-bottom: ${space[3]}px;
+
+  a, a:visited {
+    display: block;
+    ${textSans.medium()};
+    color: ${text.primary};
+    @media (min-width: 660px) {
+      display: none;
+    }
+  }
 `;
 
 const sansTitle = css`
   ${textSans.medium()};
   font-weight: bold;
-`;
-
-const link = css`
-  ${textSans.medium()};
-  display: block;
 `;
 
 const contentBlock = css`
@@ -74,9 +85,17 @@ const textBlock = css`
   h3 {
     ${headline.xxsmall()};
     font-weight: bold;
+    @media (min-width: 740px) and (max-width: 975px) {
+      ${body.medium()};
+      font-weight: bold;
+    }
   }
-  p {
+  p, span {
     ${body.medium()};
+    max-width: 220px;
+    @media (min-width: 740px) and (max-width: 975px) {
+      ${body.small()};
+    }
   }
   span {
     background-color: ${brandAltBackground.primary};
@@ -84,15 +103,15 @@ const textBlock = css`
   }
 `;
 
-
 function OrderSummary(props: PropTypes) {
   const description = getPriceDescription(props.productPrice, props.billingPeriod, true);
+  const ChangeLink = () => <a href={props.changeSubscription}>Change</a>;
 
   return (
     <div css={wrapper}>
       <div css={topLine}>
         <h2 css={sansTitle}>Order summary</h2>
-        <a css={link} href={props.changeSubscription}>Change</a>
+        <ChangeLink />
       </div>
       <div css={contentBlock}>
         <div css={image}>{props.image}</div>
@@ -102,7 +121,7 @@ function OrderSummary(props: PropTypes) {
           <span>14 day free trial</span>
         </div>
       </div>
-
+      <ChangeLink />
     </div>
   );
 }
