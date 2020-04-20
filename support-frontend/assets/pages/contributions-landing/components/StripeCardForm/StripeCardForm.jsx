144f23423db45bd70f7d450c7710fbec53e3f7db
Joe Griffiths <joe.griffiths.freelancer@guardian.co.uk>
2020-04-20 09:20:58 +0100

    reinstate comment

--- support-frontend/assets/pages/contributions-landing/components/StripeCardForm/StripeCardForm.jsx
+++ support-frontend/assets/pages/contributions-landing/components/StripeCardForm/StripeCardForm.jsx
@@ -230,6 +230,10 @@ class CardForm extends Component<PropTypes, StateTypes> {
   };
 
   setupRecurringHandlers(): void {
+    // Start by requesting the client_secret for a new Payment Method.
+    // Note - because this value is requested asynchronously when the component loads,
+    // it's possible for it to arrive after the user clicks 'Contribute'.
+    // This is handled in the callback below by checking the value of paymentWaiting.
     if (window.grecaptcha && window.grecaptcha.render) {
       this.setupRecurringRecaptchaCallback();
     } else {
