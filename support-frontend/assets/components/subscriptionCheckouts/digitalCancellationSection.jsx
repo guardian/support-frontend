34b1c4955c8d1574c0676b3e0f7f9806a1801699
damien-shaw-guardian <damien.shaw.freelancer@guardian.co.uk>
2020-04-25 00:19:25 +0100

    refactor: remove cancellation copy and the bottom of checkouts
    This was remove because it was duplicated in the checkout sidebar

--- support-frontend/assets/components/subscriptionCheckouts/digitalCancellationSection.jsx
+++ support-frontend/assets/components/subscriptionCheckouts/digitalCancellationSection.jsx
@@ -4,13 +4,18 @@ import { FormSection } from 'components/checkoutForm/checkoutForm';
 import { type Option } from 'helpers/types/option';
 import type { PaymentMethod } from 'helpers/paymentMethods';
 import { DirectDebit } from 'helpers/paymentMethods';
+import styles from './summary.module.scss';
+import EndSummary from 'components/subscriptionCheckouts/endSummary/endSummary';
 import DirectDebitTerms from 'components/subscriptionCheckouts/directDebitTerms';
 
-export default function CancellationSection(props: {
+export default function digitalCancellationSection(props: {
   paymentMethod: Option<PaymentMethod>,
 }) {
   return (
     <FormSection>
+      <div className={styles.endSummay}>
+        <EndSummary />
+      </div>
       {(props.paymentMethod === DirectDebit) && <DirectDebitTerms />}
     </FormSection>
   );
