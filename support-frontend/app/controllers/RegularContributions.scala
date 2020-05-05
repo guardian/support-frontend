8abcb97a566c7b38d925fab6862a3cc730aa4b12
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-05-05 13:56:05 +0100

    PR feedback

--- support-frontend/app/controllers/RegularContributions.scala
+++ support-frontend/app/controllers/RegularContributions.scala
@@ -88,12 +88,15 @@ class RegularContributions(
         InternalServerError
       },
       { statusResponse =>
-        if (!testUsers.isTestUser(request) && body.paymentFields.isDefined) {
-          monitoredRegion(body.billingAddress.country).map { region =>
-            val tipPath = TipPath(region, RecurringContribution, monitoredPaymentMethod(body.paymentFields.get), guestCheckout)
-            verify(tipPath, tipMonitoring.verify)
+        body.paymentFields.map(
+          paymentFields => if (!testUsers.isTestUser(request)) {
+            monitoredRegion(body.billingAddress.country).map { region =>
+              val tipPath = TipPath(region, RecurringContribution, monitoredPaymentMethod(paymentFields), guestCheckout)
+              verify(tipPath, tipMonitoring.verify)
+            }
           }
-        }
+        )
+
         Accepted(statusResponse.asJson).withCookies(RecurringContributionCookie.create(guardianDomain, billingPeriod))
       }
     )
