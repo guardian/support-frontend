93827a74eba5042f59cbe6bc327130565d024f9a
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2020-05-11 22:28:17 +0100

    Fix dot background

--- support-frontend/assets/pages/digital-subscription-checkout/components/digitalPaymentTerms.jsx
+++ support-frontend/assets/pages/digital-subscription-checkout/components/digitalPaymentTerms.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { css } from '@emotion/core';
 import { space } from '@guardian/src-foundations';
 import { border } from '@guardian/src-foundations/palette';
+import { from } from '@guardian/src-foundations/mq';
 
 import { type Option } from 'helpers/types/option';
 import type { PaymentMethod } from 'helpers/paymentMethods';
@@ -12,11 +13,20 @@ import DirectDebitTerms from 'components/subscriptionCheckouts/directDebitTerms'
 const directDebitSection = css`
   padding: 0 ${space[3]}px ${space[3]}px;
   background-color: #F6F6F6;
+  border: none;
+
+  ${from.desktop} {
+    border-top: 1px solid ${border.secondary};
+  }
 `;
 
 const borderTop = css`
   width: 100%;
   border-top: 1px solid ${border.secondary};
+
+  ${from.desktop} {
+    border-top: none;
+  }
 `;
 
 export default function DigitalPaymentTerms(props: {
