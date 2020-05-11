cfa445c37d0f0414613678a4d04687df8df2bb76
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2020-05-11 17:56:43 +0100

    Give end summary a grey background

--- support-frontend/assets/pages/digital-subscription-checkout/components/endSummary/endSummaryMobile.jsx
+++ support-frontend/assets/pages/digital-subscription-checkout/components/endSummary/endSummaryMobile.jsx
@@ -4,14 +4,14 @@ import React from 'react';
 import { css } from '@emotion/core';
 import { from } from '@guardian/src-foundations/mq';
 import { space } from '@guardian/src-foundations';
-import { border } from '@guardian/src-foundations/palette';
+import { border, background } from '@guardian/src-foundations/palette';
 import EndSummary from 'pages/digital-subscription-checkout/components/endSummary/endSummary';
 
 const endSummaryMobile = css`
   display: block;
   padding:${space[3]}px;
   border-top: 1px solid ${border.secondary};
-  border-bottom: 1px solid ${border.secondary};
+  background-color: #f6f6f6; /* Using the hex code as ${background.secondary} isn't exposed in the API yet */
 
   ${from.desktop} {
     display: none;
