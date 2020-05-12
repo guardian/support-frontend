7495f7654ed92e977582dc7f75606251424d8e0d
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-05-12 16:17:22 +0100

    Fix the rounding issue properly this time + test

--- support-models/src/main/scala/com/gu/support/workers/PaymentSchedule.scala
+++ support-models/src/main/scala/com/gu/support/workers/PaymentSchedule.scala
@@ -3,12 +3,21 @@ package com.gu.support.workers
 import com.gu.support.encoding.Codec
 import com.gu.support.encoding.Codec._
 import com.gu.support.encoding.CustomCodecs.{decodeLocalTime, encodeLocalTime}
+import com.gu.support.zuora.api.response.Charge
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import org.joda.time.LocalDate
 
 case class Payment(date: LocalDate, amount: Double)
 case class PaymentSchedule(payments: List[Payment])
 
 object PaymentSchedule {
-  implicit val paymentCodec: Codec[Payment] = deriveCodec
+  def round(d: Double) = BigDecimal(d).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  implicit val decoder: Decoder[Payment] = deriveDecoder[Payment].map(
+    payment =>
+      payment.copy(
+        amount = round(payment.amount)
+      ))
+  implicit val encoder: Encoder[Payment] = deriveEncoder
   implicit val paymentScheduleCodec: Codec[PaymentSchedule] = deriveCodec
 }
--- support-workers/src/test/scala/com/gu/support/workers/PreviewPaymentScheduleSpec.scala
+++ support-workers/src/test/scala/com/gu/support/workers/PreviewPaymentScheduleSpec.scala
@@ -1,9 +1,11 @@
 package com.gu.support.workers
 
-import com.gu.support.zuora.api.response.Charge
+import com.gu.support.zuora.api.response.{Charge, PreviewSubscribeResponse}
 import org.joda.time.LocalDate
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import io.circe.parser._
+import JsonFixtures.previewSubscribeResponseJson
 
 class PreviewPaymentScheduleSpec extends AnyFlatSpec with Matchers {
 
@@ -20,7 +22,7 @@ class PreviewPaymentScheduleSpec extends AnyFlatSpec with Matchers {
     assert(schedule == expected)
   }
 
-  "paymentSchedule" should "calculate a payment schedule correctly for products with tax" in {
+  it should "calculate a payment schedule correctly for products with tax" in {
     val charges = List(
       Charge(firstPaymentDate, firstPaymentDate.plusMonths(1), 1.25, 4.00),
       Charge(firstPaymentDate.plusMonths(1), firstPaymentDate.plusMonths(2), 1.50, 5.00)
@@ -30,15 +32,15 @@ class PreviewPaymentScheduleSpec extends AnyFlatSpec with Matchers {
     assert(schedule == expected)
   }
 
-  "paymentSchedule" should "round charge amounts to two decimal places" in {
-    val taxExclusiveCharge = Charge(firstPaymentDate, firstPaymentDate.plusMonths(1), 0, 5.9901)
-    val charges = List(
-      taxExclusiveCharge,
-      taxExclusiveCharge.copy(serviceStartDate = firstPaymentDate.plusMonths(1), serviceEndDate = firstPaymentDate.plusMonths(2), chargeAmount = 5.99)
-    )
-    val expected = PaymentSchedule(List(Payment(firstPaymentDate, 5.99), Payment(firstPaymentDate.plusMonths(1), 5.99)))
+  it should "round Payment amounts to two decimal places" in {
+    val previewSubscribeResponse = decode[PreviewSubscribeResponse](previewSubscribeResponseJson).right.get
+
+    val charges = previewSubscribeResponse.invoiceData.flatMap(_.invoiceItem)
+
     val schedule = PreviewPaymentSchedule.paymentSchedule(charges)
-    assert(schedule == expected)
+
+    schedule.payments.length shouldBe 13
+    schedule.payments.forall(p => p.amount == 52.99) shouldBe true
   }
 
 }
