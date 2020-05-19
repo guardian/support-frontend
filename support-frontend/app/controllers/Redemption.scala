fc5529ff00658ea47677b76025ebfb07785bf449
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-05-19 17:54:22 +0100

    Return a CorporateCustomer to the js

--- support-frontend/app/controllers/Redemption.scala
+++ support-frontend/app/controllers/Redemption.scala
@@ -4,10 +4,13 @@ import actions.CustomActionBuilders
 import admin.settings.{AllSettings, AllSettingsProvider}
 import assets.{AssetsResolver, RefPath, StyleContent}
 import com.gu.identity.model.User
+import com.gu.support.redemptions.CorporateCustomer
+import com.gu.support.redemptions.redemptions.RedemptionCode
 import play.api.mvc.{AbstractController, ControllerComponents}
 import play.twirl.api.Html
 import services.{IdentityService, MembersDataService}
 import views.EmptyDiv
+import io.circe.syntax._
 
 import scala.concurrent.ExecutionContext
 
@@ -27,7 +30,11 @@ class Redemption(
 
   implicit val a: AssetsResolver = assets
 
-  //def getRedemptionData(redemptionCode: RedemptionCode): Option[CorporateRedemption] =
+  def getCorporateCustomer(redemptionCode: RedemptionCode): Option[CorporateCustomer] =
+    if (redemptionCode == "test-code")
+      Some(CorporateCustomer("1", "Test Company", "test-code"))
+    else
+      None
 
   def displayForm(redemptionCode: String) = NoCacheAction() {
     implicit request =>
@@ -37,43 +44,50 @@ class Redemption(
       val js = "subscriptionsRedemptionPage.js"
       val css = "digitalSubscriptionCheckoutPage.css" //TODO: Don't need this?
 
-      Ok(views.html.main(
-        title,
-        id,
-        Left(RefPath(js)),
-        Left(RefPath(css)),
-        fontLoaderBundle
-      ) {
-        Html(s"""<script type="text/javascript">window.guardian.productPrices = ""</script>""")
-      })
+      getCorporateCustomer(redemptionCode).map(
+        customer =>
+          Ok(views.html.main(
+            title,
+            id,
+            Left(RefPath(js)),
+            Left(RefPath(css)),
+            fontLoaderBundle
+          ) {
+            Html(s"""
+        <script type="text/javascript">
+            window.guardian.corporateCustomer = ${customer.asJson}
+        </script>
+        """)
+          })
+      ).getOrElse(NotFound)
   }
 
-//  def create: Action[CreateSupportWorkersRequest] =
-//    authenticatedAction(recurringIdentityClientId).async(circe.json[CreateSupportWorkersRequest]) {
-//      implicit request: AuthRequest[CreateSupportWorkersRequest] =>
-//        handleCreateSupportWorkersRequest(request, CheckoutValidationRules.validatorFor(request.body.product))
-//    }
+  //  def create: Action[CreateSupportWorkersRequest] =
+  //    authenticatedAction(recurringIdentityClientId).async(circe.json[CreateSupportWorkersRequest]) {
+  //      implicit request: AuthRequest[CreateSupportWorkersRequest] =>
+  //        handleCreateSupportWorkersRequest(request, CheckoutValidationRules.validatorFor(request.body.product))
+  //    }
 
-//  def redeemCode() = NoCacheAction() {
-//    implicit request =>
-//
-//  }
+  //  def redeemCode() = NoCacheAction() {
+  //    implicit request =>
+  //
+  //  }
 
-//  def processRedemption(): Action[AnyContent] =
-//    authenticatedAction(subscriptionsClientId).async { implicit request =>
-//      identityService.getUser(request.user.minimalUser).fold(
-//        error => {
-//          SafeLogger.error(scrub"Failed to display digital subscriptions form for ${request.user.minimalUser.id} due to error from identityService: $error")
-//          Future.successful(InternalServerError)
-//        },
-//        user => {
-//          userHasDigipack(membersDataService, request.user) map {
-//            case true => Redirect(routes.DigitalSubscription.displayThankYouExisting().url, request.queryString, status = FOUND)
-//            case _ => Ok(redemptionForm(user))
-//          }
-//        }
-//      ).flatten
-//    }
+  //  def processRedemption(): Action[AnyContent] =
+  //    authenticatedAction(subscriptionsClientId).async { implicit request =>
+  //      identityService.getUser(request.user.minimalUser).fold(
+  //        error => {
+  //          SafeLogger.error(scrub"Failed to display digital subscriptions form for ${request.user.minimalUser.id} due to error from identityService: $error")
+  //          Future.successful(InternalServerError)
+  //        },
+  //        user => {
+  //          userHasDigipack(membersDataService, request.user) map {
+  //            case true => Redirect(routes.DigitalSubscription.displayThankYouExisting().url, request.queryString, status = FOUND)
+  //            case _ => Ok(redemptionForm(user))
+  //          }
+  //        }
+  //      ).flatten
+  //    }
 
   def redemptionForm(user: User) = {
     "hi"
