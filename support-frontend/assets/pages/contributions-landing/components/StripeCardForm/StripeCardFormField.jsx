86658d4c84100db350304188b72494fe68f0ce29
TJ Silver <tjsilver@protonmail.com>
2020-05-28 17:13:55 +0100

    style stripe fields to have focus highlight

--- support-frontend/assets/pages/contributions-landing/components/StripeCardForm/StripeCardFormField.jsx
+++ support-frontend/assets/pages/contributions-landing/components/StripeCardForm/StripeCardFormField.jsx
@@ -8,11 +8,20 @@ type PropTypes = {|
   hint?: Node,
   input: Node,
   error: boolean,
+  focus: boolean,
 |}
 
+
+const getClass = (props: PropTypes) => {
+  if (props.focus) {
+    return 'ds-stripe-card-input__focus';
+  } else if (props.error) {
+    return 'ds-stripe-card-input__error';
+  }
+  return '';
+};
+
 export function StripeCardFormField(props: PropTypes) {
-
-
   return (
     <>
       <div
@@ -22,7 +31,7 @@ export function StripeCardFormField(props: PropTypes) {
       </div>
       {props.hint || null}
       <div
-        className={`ds-stripe-card-input ${props.error ? 'ds-stripe-card-input-error' : ''}`}
+        className={`ds-stripe-card-input ${getClass(props)}`}
       >
         {props.input}
       </div>
