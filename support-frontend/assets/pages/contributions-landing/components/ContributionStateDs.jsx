f4f1b02918e6b7beb7706753b18e7d9256c66fad
TJ Silver <tjsilver@protonmail.com>
2020-05-28 17:14:18 +0100

    wip style state field

--- support-frontend/assets/pages/contributions-landing/components/ContributionStateDs.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionStateDs.jsx
@@ -11,10 +11,9 @@ import { usStates, caStates, type StateProvince, auStates } from 'helpers/intern
 import { type CountryGroupId, type CountryGroup, countryGroups, AUDCountries } from 'helpers/internationalisation/countryGroup';
 import { Canada, UnitedStates } from 'helpers/internationalisation/countryGroup';
 import { InlineError } from '@guardian/src-inline-error';
-import './contributionStateDs.scss';
 import DownChevronDs from 'components/svgs/downChevronDs';
 import { focusHalo } from '@guardian/src-foundations/accessibility';
-import { size, space } from '@guardian/src-foundations';
+import { space } from '@guardian/src-foundations';
 
 import { type State } from '../contributionsLandingReducer';
 
@@ -38,6 +37,40 @@ const renderState = (selectedState: StateProvince | null) => (state: {abbreviati
   <option value={state.abbreviation} selected={selectedState === state.abbreviation}>{state.name}</option>
 );
 
+const selectCss = {
+  appearance: 'none !important',
+  background: 'transparent',
+  transition: 'box-shadow .2s ease-in-out',
+  borderRadius: '0px',
+  fontSize: '17px',
+  width: '100%',
+  height: '48px',
+  border: '2px solid #999',
+  zIndex: 1,
+  margin: 0,
+  padding: 0,
+  outline: 0,
+  boxSizing: 'border-box',
+
+  '&:active': {
+    border: '2px solid #007ABC',
+    outline: 0,
+  },
+
+  '&:focus': {
+    ...focusHalo,
+    outline: 0,
+  },
+};
+
+const errorBorderCss = {
+  ...selectCss,
+  outline: '4px solid #c70000',
+  height: '44px',
+  border: 0,
+};
+
+
 const renderStatesField = (
   states: {[string]: string},
   selectedState: StateProvince | null,
@@ -46,70 +79,46 @@ const renderStatesField = (
   label: string,
 ) => (
   <div
-    className="state-container"
+    css={css`
+      margin-top: 12px;
+      margin-bottom: 4px;
+      position: relative;
+    `}
   >
-    <label className="state-label" htmlFor="contributionState">
+    <label
+      htmlFor="contributionState"
+      css={css`
+        font-family: $gu-text-sans-web;
+        font-weight: 700;
+        line-height: 1.5;
+        font-size: 17px;
+      `}
+    >
       {label}
     </label>
+    <div
+      css={css`
+        font-size: 15px;
+        color: #767676;
+        margin-bottom: ${space[1]}px;
+      `}
+    >Select your {label.toLowerCase()}
+    </div>
     {showError ? (
       <InlineError>
-        Please provide a {label.toLowerCase()}
+        Please select your {label.toLowerCase()}
       </InlineError>
       ) : null}
-    <div>Please select your {label.toLowerCase()}</div>
     <div
-      // className={`state-field ${showError ? 'state-field__error' : ''}`}
       css={css`
-        margin-top: 4px;
-        width: 100%;
-        border: 2px solid #999;
-        box-sizing: border-box;
-        /* height: ${size.medium}px; */
-        height: 48px;
-        /* padding: ${space[10]}px ${space[2]}px; */
-        box-sizing: border-box;
-        padding: 0 ${space[2]}px;
+        /* width: 100%; */
+        /* padding: 0; */
         position: relative;
-
-        &:active {
-          ${focusHalo}
-		      border: 2px solid #007ABC;
-        }
-        &:focus {
-          /* ${focusHalo}; */
-          border: 2px solid #007ABC;
-        }
-        &:hover {
-          /* ${focusHalo}; */
-          border: 2px solid #007ABC;
-        }
-        &:invalid {
-          border: 4px solid ##c70000;
-          color: #c70000;
-        }
+        /* margin-top: 0; */
       `}
     >
       <select
-        css={css`
-            appearance: none;
-            /* background: #ffffff; */
-            background: transparent;
-            transition: box-shadow .2s ease-in-out;
-            border-radius: 0px;
-            font-size: 17px;
-            width: 110%;
-            border: 0px;
-            z-index: 1;
-            margin: 0;
-            height: 100%;
-
-            &:focus {
-              border: 0px;
-              outline: 0px;
-            }
-          `}
-
-          // className="state-field__select"
+        css={showError ? errorBorderCss : selectCss}
         id="contributionState"
         onChange={onChange}
         required
@@ -119,23 +128,24 @@ const renderStatesField = (
             .map(key => ({ abbreviation: key, name: states[key] }))
             .map(renderState(selectedState))}
       </select>
-
       <div
         css={css`
         float: right;
         z-index: 0;
         position: absolute;
-        top: 4px;
+        top: 12px;
         right: 16px;
+        pointer-events: none;
+        // TODO: change chevron colour on error only
+        path {
+          fill: #970000;
+        };
       `}
       >
-        <DownChevronDs />
+        <DownChevronDs
+        />
       </div>
-
-
-
     </div>
-
   </div>
 );
 
