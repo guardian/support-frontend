c1ce18e6a53c087cd0fc91163620fa316c5e3395
Tom Forbes <tom.forbes@theguardian.com>
2020-06-04 10:34:06 +0100

    latest copy

--- support-frontend/stories/contributionTicker.jsx
+++ support-frontend/stories/contributionTicker.jsx
@@ -17,14 +17,14 @@ const stories = storiesOf('Tickers', module)
 stories.add('Contribution ticker', () => (
   <div style={{ width: '100%', maxWidth: '500px' }}>
     <ContributionTicker
-      tickerCountType="money"
+      tickerCountType="people"
       onGoalReached={() => {}}
       tickerEndType="unlimited"
       currencySymbol="£"
       copy={{
-        countLabel: 'contributors',
-        goalReachedPrimary: 'You can still support us today',
-        goalReachedSecondary: 'thank you - we\'re over our goal',
+        countLabel: 'supporters in Australia',
+        goalReachedPrimary: 'We\'ve hit our goal',
+        goalReachedSecondary: 'but you can still support us',
       }}
     />
   </div>
