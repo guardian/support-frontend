2b3300517c50197f63dbb6f1764cad011bb0a0ef
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-06-05 13:51:58 +0100

    Type tweak

--- support-frontend/assets/pages/subscriptions-redemption/thankYouContainer.js
+++ support-frontend/assets/pages/subscriptions-redemption/thankYouContainer.js
@@ -3,12 +3,12 @@
 import { connect } from 'react-redux';
 import ThankYouContent from 'pages/digital-subscription-checkout/thankYouContent';
 import type { RedemptionPageState } from 'pages/subscriptions-redemption/subscriptionsRedemptionReducer';
-import { Stripe } from 'helpers/paymentMethods';
 
 function mapStateToProps(state: RedemptionPageState) {
   return {
     countryGroupId: state.common.internationalisation.countryGroupId,
-    paymentMethod: Stripe,
+    // $FlowIgnore -- paymentMethod can be null (see thankYouContent.jsx) but Flow won't allow it
+    paymentMethod: null,
     includePaymentCopy: false,
   };
 }
