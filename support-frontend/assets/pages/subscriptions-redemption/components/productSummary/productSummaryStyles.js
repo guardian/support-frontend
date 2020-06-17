70c8483c661733680305386511f90bf965172bb8
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-06-17 12:00:22 +0100

    Tidy up CSS

--- support-frontend/assets/pages/subscriptions-redemption/components/productSummary/productSummaryStyles.js
+++ support-frontend/assets/pages/subscriptions-redemption/components/productSummary/productSummaryStyles.js
@@ -1,7 +1,7 @@
 import { css } from '@emotion/core';
-import { body, headline, textSans } from '@guardian/src-foundations/typography/obj';
+import { headline, textSans } from '@guardian/src-foundations/typography/obj';
 import { space } from '@guardian/src-foundations';
-import { background, border, brandAltBackground, neutral, text } from '@guardian/src-foundations/palette';
+import { background, border, neutral, text } from '@guardian/src-foundations/palette';
 import { from, until } from '@guardian/src-foundations/mq';
 
 export const wrapper = css`
@@ -12,17 +12,13 @@ export const wrapper = css`
 export const contentBlock = css`
   display: block;
   width: 100%;
-
-  ${from.tablet} {
-    display: block;
-  }
 `;
 
 export const imageContainer = css`
   display: inline-flex;
   align-items: flex-start;
   width: calc(100% - 30px);
-  padding: 15px 10px 0 15px;
+  padding: ${space[4]}px ${space[4]}px 0 ${space[4]}px;
   background-color: ${neutral['60']};
 
   img {
@@ -30,15 +26,13 @@ export const imageContainer = css`
     height: auto;
     margin-left: auto;
     margin-right: auto;
-
   }
 
   ${until.tablet} {
     box-sizing: border-box;
     width: 100%;
     margin-top: 20px;
-    padding: 8px 8px 0;
-
+    padding: ${space[2]}px ${space[2]}px 0;
   }
 `;
 
@@ -55,42 +49,9 @@ export const fromTablet = css`
 `;
 
 export const textBlock = css`
-  margin-left: ${space[3]}px;
-
-  ${from.desktop} {
-    display: flex;
-    justify-content: space-between;
-    width: calc(100%-${space[3]}px * 2);
-    margin: ${space[3]}px;
-    align-items: baseline;
-  }
-
   h3 {
     ${headline.xxsmall({ fontWeight: 'bold' })};
-    margin: 8px 0 2px;
-    ${from.desktop} {
-      ${headline.xxsmall({ fontWeight: 'bold' })};
-      margin-top: 0;
-    }
-  }
-  p, span {
-    max-width: 240px;
-  }
-  span {
-    background-color: ${brandAltBackground.primary};
-    padding: 2px;
-    ${textSans.small({ fontWeight: 'bold' })};
-    ${from.desktop} {
-      padding: ${space[1]}px;
-      ${textSans.medium()};
-    }
-  }
-  p {
-    ${body.small({ fontWeight: 'normal' })};
-    line-height: 135%;
-    ${from.desktop} {
-      display: none;
-    }
+    margin: ${space[2]}px ${space[2]}px 2px;
   }
 `;
 
