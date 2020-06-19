59bcab6decaf4513c7711b1affbba95034d9121a
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-06-19 15:53:08 +0100

    Only run ds address test in UK

--- support-frontend/assets/helpers/abTests/abtestDefinitions.js
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.js
@@ -2,6 +2,7 @@
 import type { Tests } from './abtest';
 import { USV1 } from './data/testAmountsData';
 import ausMomentEnabled from 'helpers/ausMoment';
+import { detect as detectCountryGroupId, GBPCountries } from 'helpers/internationalisation/countryGroup';
 
 // ----- Tests ----- //
 export type StripePaymentRequestButtonTestVariants = 'control' | 'button';
@@ -143,6 +144,7 @@ export const tests: Tests = {
     isActive: true,
     referrerControlled: false,
     seed: 7,
+    canRun: () => detectCountryGroupId() === GBPCountries,
     targetPage: digitalCheckout,
     optimizeId: 'tdBE5yqdR0aQ19E06j1zRA',
   },
