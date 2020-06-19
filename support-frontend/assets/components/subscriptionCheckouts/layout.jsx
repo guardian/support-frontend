092a0b8274c07ce92d9dec02f3305d9f71d24541
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-06-19 11:39:31 +0100

    Start to replace scss with emotion

--- support-frontend/assets/components/subscriptionCheckouts/layout.jsx
+++ support-frontend/assets/components/subscriptionCheckouts/layout.jsx
@@ -4,6 +4,9 @@ import React, { type Node } from 'react';
 
 import styles from './layout.module.scss';
 import LeftMarginSection from 'components/leftMarginSection/leftMarginSection';
+import { css } from '@emotion/core';
+import { from } from '@guardian/src-foundations/mq';
+import { space } from '@guardian/src-foundations';
 
 type AsideWrapPosition = 'top' | 'bottom';
 
@@ -12,6 +15,28 @@ type PropTypes = {
   aside: Node,
   wrapPosition: ?AsideWrapPosition,
 };
+
+const asideCss = css`
+  z-index: 99;
+  width: 100%;
+  border-bottom: 1px solid gu-colour(neutral-86);
+  ${from.leftCol} {
+    border-right: 1px solid gu-colour(neutral-86);
+  }
+`;
+
+const mainCss = css`
+  max-width: ${space[280]}px;
+  ${from.tablet} {
+    display: flex;
+    align-items: flex-start;
+  }
+  .form {
+    flex: 0 0 auto;
+    width: 100%;
+  }
+`;
+
 const CheckoutLayout = ({ children, aside, wrapPosition }: PropTypes) => {
   const mainClass = [
     styles.root,
@@ -29,7 +54,7 @@ const CheckoutLayout = ({ children, aside, wrapPosition }: PropTypes) => {
         {children}
       </div>
       {wrapPosition === 'bottom' &&
-        <div className={styles.aside}>
+        <div css={asideCss}>
           {aside}
         </div>
       }
