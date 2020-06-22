de81be4395696138c726e3eb9814c4dc3da61d55
John Duffell <john.duffell@guardian.co.uk>
2020-06-22 16:13:19 +0100

    ensure closing quote is present

--- support-frontend/app/views/JsStringLiteral.scala
+++ support-frontend/app/views/JsStringLiteral.scala
@@ -5,7 +5,7 @@ import play.twirl.api.Html
 object JsStringLiteral {
 
   def apply(rawString: String): Html =
-    Html(s""""${rawString.replaceAll(""""""", """\\"""")}""")
+    Html(s""""${rawString.replaceAll(""""""", """\\"""")}"""")
 
   def fromOption(maybeRawString: Option[String]): Html =
     maybeRawString match {
