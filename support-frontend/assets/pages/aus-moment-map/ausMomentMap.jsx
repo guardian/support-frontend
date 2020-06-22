b9f6668c7f0a300f29d36b7c71eb33f409072923
Michael Jacobson <contact@michaeljacobson.co.uk>
2020-06-22 23:01:33 +0100

    improve responsiveness

--- support-frontend/assets/pages/aus-moment-map/ausMomentMap.jsx
+++ support-frontend/assets/pages/aus-moment-map/ausMomentMap.jsx
@@ -13,19 +13,21 @@ import { SocialLinks } from 'pages/aus-moment-map/components/footer';
 const content = (
   <div>
     <Header />
-    <div className="left">
-      <Map />
-      <p className="map-caption">Tap the map to read messages from supporters</p>
-    </div>
-    <div className="right">
-      <h2 className="blurb">Hear from Guardian supporters across Australia</h2>
-      <p className="blurb">
-        Our supporters are doing something powerful. As our readership grows, more people are supporting Guardian
-        journalism than ever before. But what drives this support? We asked readers across every state to share their
-        reasons with us – and here is a selection. You can become a supporter this winter and add to the conversation.
-      </p>
-      <p className="supporters-total">123,895</p>
-      <p className="supporters-total-caption">Total supporters in Australia</p>
+    <div className="main">
+      <div className="left">
+        <Map />
+        <p className="map-caption">Tap the map to read messages from supporters</p>
+      </div>
+      <div className="right">
+        <h2 className="blurb">Hear from Guardian supporters across Australia</h2>
+        <p className="blurb">
+          Our supporters are doing something powerful. As our readership grows, more people are supporting Guardian
+          journalism than ever before. But what drives this support? We asked readers across every state to share their
+          reasons with us – and here is a selection. You can become a supporter this winter and add to the conversation.
+        </p>
+        <p className="supporters-total">123,895</p>
+        <p className="supporters-total-caption">Total supporters in Australia</p>
+      </div>
     </div>
     <SocialLinks />
   </div>
