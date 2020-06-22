c7dc66d3c71b6d61ffd24dcbaa2a581dd224f013
John Duffell <john.duffell@guardian.co.uk>
2020-06-22 16:32:24 +0100

    escape string literals properly!

--- support-frontend/app/views/JsStringLiteral.scala
+++ support-frontend/app/views/JsStringLiteral.scala
@@ -4,8 +4,18 @@ import play.twirl.api.Html
 
 object JsStringLiteral {
 
-  def apply(rawString: String): Html =
-    Html(s""""${rawString.replaceAll(""""""", """\\"""")}"""")
+  def apply(rawString: String): Html = {
+    val cleanString = rawString.flatMap {
+      case '<' => "\\u003c"
+      case '>' => "\\u003e"
+      case '"' => "\\u0022"
+      case '\'' => "\\u0027"
+      case '\\' => "\\u005c"
+      case '&' => "\\u0026"
+      case other => other.toString
+    }
+    Html(s""""$cleanString"""")
+  }
 
   def fromOption(maybeRawString: Option[String]): Html =
     maybeRawString match {
