f35c0e0adefa7875ba2719599fa6e9b3bbff51ca
Michael Jacobson <contact@michaeljacobson.co.uk>
2020-06-22 11:44:19 +0100

    use custom MapPath react component to DRY out code

---
+++ support-frontend/assets/pages/aus-moment-map/components/mapPath.jsx
@@ -0,0 +1,33 @@
+// @flow
+import React from 'react';
+
+export const MapPath = (props) => {
+  const territory = (DOMEvent) => {
+    const dataTerritory = DOMEvent.target.getAttribute('data-territory')
+    return window.document.querySelectorAll(`[data-territory="${dataTerritory}"`);
+  }
+
+  const mouseEnter = (e) => {
+    territory(e).forEach(t => t.classList.add('hover'))
+  }
+
+  const mouseLeave = (e) => {
+    territory(e).forEach(t => t.classList.remove('hover'))
+  }
+
+  const click = (e) => {
+    document.querySelectorAll('.selected').forEach(t => t.classList.remove('selected'))
+    territory(e).forEach(t => t.classList.add('selected'))
+  }
+
+  return (
+    <path
+      onMouseEnter={mouseEnter}
+      onMouseLeave={mouseLeave}
+      onClick={click}
+      className={props.type}
+      d={props.d}
+      data-territory={props.territory}
+    />
+  )
+}
