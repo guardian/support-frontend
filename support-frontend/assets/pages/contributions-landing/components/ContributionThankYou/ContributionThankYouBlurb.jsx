3f39992c8eed5f8922f8598188170feb38117299
Joe Griffiths <joe.griffiths.freelancer@guardian.co.uk>
2020-06-22 10:46:13 +0100

    updating copy

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYouBlurb.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYouBlurb.jsx
@@ -28,7 +28,7 @@ const mapStateToProps = state => ({
 const ContributionThankYouBlurb = (props: PropTypes) => {
   const { firstName, thankYouPageStage } = props;
   const headerText = (firstName && firstName.length < 10 && firstName.trim() !== '') ?
-    `Thank\xa0you\xa0${firstName}\nfor\xa0your\xa0valuable\ncontribution` : 'Thank\xa0you\xa0for\na\xa0valuable\ncontribution';
+    `Thank\xa0you\xa0${firstName}\nfor\xa0your\xa0valuable\ncontribution` : 'Thank\xa0you\xa0for\nyour\xa0valuable\ncontribution';
 
   return (
     <div className="gu-content__blurb gu-content__blurb--thank-you">
