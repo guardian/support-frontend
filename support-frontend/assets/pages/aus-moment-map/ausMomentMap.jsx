f45cae80a374760f183fabcf05cf222a07f05013
Tom Pretty <tompretty2@gmail.com>
2020-07-01 16:21:07 +0100

    Add custom useTestimonials hook

--- support-frontend/assets/pages/aus-moment-map/ausMomentMap.jsx
+++ support-frontend/assets/pages/aus-moment-map/ausMomentMap.jsx
@@ -1,7 +1,7 @@
 // @flow
 
 // ----- Imports ----- //
-import React from 'react'
+import React from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import { renderPage } from 'helpers/render';
 import './ausMomentMap.scss';
@@ -11,46 +11,38 @@ import { SocialLinks } from 'pages/aus-moment-map/components/social-links';
 import { Blurb } from 'pages/aus-moment-map/components/blurb';
 import { CloseButton } from 'pages/aus-moment-map/components/closeButton';
 import { TestimonialsCollection } from 'pages/aus-moment-map/types/testimonials';
+import useTestimonials from 'pages/aus-moment-map/hooks/useTestimonials';
 
 // ----- Render ----- //
 const AusMomentMap = () => {
-  const [selectedTerritory, setSelectedTerritory] = React.useState(null)
-  const [testimonials, setTestimonials] = React.useState<TestimonialsCollection>(null)
+  const [selectedTerritory, setSelectedTerritory] = React.useState(null);
+  const testimonials = useTestimonials();
 
-  const testimonialsEndpoint = 'https://interactive.guim.co.uk/docsdata/18tKS4fsHcEo__gdAwp3UySA3-FVje72_adHBZBhWjXE.json'
+  console.log(testimonials);
 
-  React.useEffect(() => {
-    fetch(testimonialsEndpoint)
-      .then(response => response.json())
-      .then(data => data['sheets'])
-      .then(testimonialsData => setTestimonials(testimonialsData))
-  }, [])
-
-  console.log(testimonials)
-
-  const mapControls = useAnimation()
-  const testimonialsControls = useAnimation()
+  const mapControls = useAnimation();
+  const testimonialsControls = useAnimation();
 
   const mapVariants = {
-    initial: {width: '55%'},
-    active: {width: '40%'}
-  }
+    initial: { width: '55%' },
+    active: { width: '40%' },
+  };
 
   const testimonialsVariants = {
-    initial: {x: "57.5vw"},
-    active: {x: "-57.5vw"}
-  }
+    initial: { x: '57.5vw' },
+    active: { x: '-57.5vw' },
+  };
 
   const runAnimation = (variant) => {
-    testimonialsControls.start(variant)
-    mapControls.start(variant)
-  }
+    testimonialsControls.start(variant);
+    mapControls.start(variant);
+  };
 
   const resetToInitial = () => {
     document.querySelectorAll('.selected').forEach(t => t.classList.remove('selected'));
     runAnimation('initial');
-    setSelectedTerritory(null)
-  }
+    setSelectedTerritory(null);
+  };
 
   const handleClick = (e) => {
     const elementClassList = e.target.classList;
@@ -59,16 +51,16 @@ const AusMomentMap = () => {
 
     if (isPartOfMap) {
       const selectedTerritory = e.target.getAttribute('data-territory');
-      setSelectedTerritory(selectedTerritory)
-      runAnimation('active')
+      setSelectedTerritory(selectedTerritory);
+      runAnimation('active');
     } else if (isWhitespace) {
-      resetToInitial()
+      resetToInitial();
     }
-  }
+  };
 
   const handleCloseButtonClick = () => {
-    resetToInitial()
-  }
+    resetToInitial();
+  };
 
   return (
     <div onClick={handleClick}>
@@ -78,22 +70,22 @@ const AusMomentMap = () => {
           className="left"
           animate={mapControls}
           variants={mapVariants}
-          transition={{type: "tween", duration: .2}}
+          transition={{ type: 'tween', duration: 0.2 }}
           positionTransition
         >
           <Map />
           <p className="map-caption">Tap the map to read messages from supporters</p>
         </motion.div>
         <div className="right">
-          <Blurb slim={false}/>
+          <Blurb slim={false} />
           <motion.div
             className="testimonials-overlay"
             animate={testimonialsControls}
             variants={testimonialsVariants}
-            transition={{type: "tween", duration: .2}}
+            transition={{ type: 'tween', duration: 0.2 }}
             positionTransition
           >
-            <CloseButton onClick={handleCloseButtonClick}/>
+            <CloseButton onClick={handleCloseButtonClick} />
             <h2 className="blurb">
               <span className="selected-territory">{selectedTerritory}</span>
               <br />
@@ -103,7 +95,7 @@ const AusMomentMap = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 renderPage(<AusMomentMap />, 'aus-moment-map');
