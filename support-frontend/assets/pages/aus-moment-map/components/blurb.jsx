3285cfe54c853742579fe36c6a27d5b2b8b74f94
Tom Pretty <tompretty2@gmail.com>
2020-07-01 16:16:25 +0100

    Use useSupportersCount hook in blurb component

--- support-frontend/assets/pages/aus-moment-map/components/blurb.jsx
+++ support-frontend/assets/pages/aus-moment-map/components/blurb.jsx
@@ -3,37 +3,28 @@
 // ----- Imports ----- //
 import React from 'react';
 import { SocialLinks } from 'pages/aus-moment-map/components/social-links';
+import useSupportersCount from '../hooks/useSupportersCount';
 
 type PropTypes = {| slim: boolean |}
 
 // ----- Render ----- //
 export const Blurb = (props: PropTypes) => {
-  const [supporters, setSupporters] = React.useState(0)
-  const supportersCountEndpoint = '/supporters-ticker.json'
-
-  React.useEffect(() => {
-    fetch(supportersCountEndpoint)
-      .then(response => response.json())
-      .then(data => setSupporters(data.total));
-  }, [])
+  const supportersCount = useSupportersCount();
 
   return (
-    <div>
+    (<div>
       <div>
         <h2 className="blurb">Hear from Guardian supporters across Australia</h2>
         {!props.slim &&
-          <div>
-            <p className="blurb">
-              Our supporters are doing something powerful. As our readership grows, more people are supporting Guardian
-              journalism than ever before. But what drives this support? We asked readers across every state to share their
-              reasons with us – and here is a selection. You can become a supporter this winter and add to the conversation.
-            </p>
-            <p className="supporters-total">{supporters.toLocaleString()}</p>
-            <p className="supporters-total-caption">Total supporters in Australia</p>
-          </div>
-        }
+        <div>
+          <p className="blurb">
+          Our supporters are doing something powerful. As our readership grows, more people are supporting Guardian journalism than ever before. But what drives this support? We asked readers across every state to share their reasons with us – and here is a selection. You can become a supporter this winter and add to the conversation.
+          </p>
+          <p className="supporters-total">{supportersCount.toLocaleString()}</p>
+          <p className="supporters-total-caption">Total supporters in Australia</p>
+        </div>}
       </div>
       <SocialLinks />
-    </div>
-  )
-}
+    </div>)
+  );
+};
