deb9f3670c173b7bcc07d6f0471a1b2497942941
Michael Jacobson <contact@michaeljacobson.co.uk>
2020-07-01 15:30:29 +0100

    raise testimonials data fetching up to main component

--- support-frontend/assets/pages/aus-moment-map/ausMomentMap.jsx
+++ support-frontend/assets/pages/aus-moment-map/ausMomentMap.jsx
@@ -10,10 +10,23 @@ import { Map } from 'pages/aus-moment-map/components/map';
 import { SocialLinks } from 'pages/aus-moment-map/components/social-links';
 import { Testimonials } from 'pages/aus-moment-map/components/testimonials';
 import { CloseButton } from 'pages/aus-moment-map/components/closeButton';
+import { TestimonialsCollection } from 'pages/aus-moment-map/types/testimonials';
 
 // ----- Render ----- //
 const AusMomentMap = () => {
   const [selectedTerritory, setSelectedTerritory] = React.useState(null)
+  const [testimonials, setTestimonials] = React.useState<TestimonialsCollection>(null)
+
+  const testimonialsEndpoint = 'https://interactive.guim.co.uk/docsdata/18tKS4fsHcEo__gdAwp3UySA3-FVje72_adHBZBhWjXE.json'
+
+  React.useEffect(() => {
+    fetch(testimonialsEndpoint)
+      .then(response => response.json())
+      .then(data => data['sheets'])
+      .then(testimonialsData => setTestimonials(testimonialsData))
+  }, [])
+
+  console.log(testimonials)
 
   const mapControls = useAnimation()
   const testimonialsControls = useAnimation()
---
+++ support-frontend/assets/pages/aus-moment-map/types/testimonials.js
@@ -0,0 +1,11 @@
+import type { AuState } from 'helpers/internationalisation/country';
+
+export type Testimonial = {
+  name: string,
+  city: string,
+  body: string,
+}
+
+export type TestimonialsCollection = {
+  [AuState]: Array<Testimonial>
+}
