00394a87bbba25c474d8f0ab1bd79e6db70c6c5c
Tom Pretty <tompretty2@gmail.com>
2020-07-07 09:39:58 +0100

    Map refactor (#2590)
    * refactor each map segment into it's own svg
    
    * Add hover styles and set selected state on click
    
    * Remove unused code

--- support-frontend/assets/pages/aus-moment-map/components/map.jsx
+++ support-frontend/assets/pages/aus-moment-map/components/map.jsx
@@ -1,14 +1,25 @@
 // @flow
-import React from 'react';
-import { WesternAustralia } from 'pages/aus-moment-map/components/territories/westernAustralia';
-import { NorthernTerritory } from 'pages/aus-moment-map/components/territories/northernTerritory';
-import { Queensland } from 'pages/aus-moment-map/components/territories/queensland';
-import { NewSouthWales } from 'pages/aus-moment-map/components/territories/newSouthWales';
-import { AustralianCapitalTerritory } from 'pages/aus-moment-map/components/territories/australianCapitalTerritory';
-import { Victoria } from 'pages/aus-moment-map/components/territories/victoria';
-import { Tasmania } from 'pages/aus-moment-map/components/territories/tasmania';
-import { SouthAustralia } from 'pages/aus-moment-map/components/territories/southAustralia';
+import * as React from 'react';
+import VictoriaSvg from './territories/victoriaSvg';
+import NewSouthWalesSvg from './territories/newSouthWalesSvg';
+import ActSvg from './territories/actSvg';
+import QueenslandSvg from './territories/queenslandSvg';
+import SouthAustraliaSvg from './territories/southAustraliaSvg';
+import WesternAustraliaSvg from './territories/westernAustraliaSvg';
+import TasmaniaSvg from './territories/tasmaniaSvg';
+import NorthernTerritorySvg from './territories/northernTerritorySvg';
 
+type TerritorySvgContainerProps = {
+  onClick: () => void,
+  isSelected: boolean,
+  children: React.Node,
+}
+
+const TerritorySvgContainer = (props: TerritorySvgContainerProps) => (
+  <g className={`territory ${props.isSelected ? 'territory-selected' : ''}`} onClick={props.onClick}>
+    { props.children }
+  </g>
+);
 
 type MapProps = {
   selectedTerritory: string,
@@ -17,15 +28,38 @@ type MapProps = {
 
 export const Map = (props: MapProps) => (
   <div className="svg-wrapper">
-    <svg className="svg-content" viewBox="0 0 700 645" fill="none" preserveAspectRatio="xMinYMin meet">
-      <WesternAustralia selectedTerritory={props.selectedTerritory} onClick={props.setSelectedTerritory} />
-      <NorthernTerritory selectedTerritory={props.selectedTerritory} onClick={props.setSelectedTerritory} />
-      <Queensland selectedTerritory={props.selectedTerritory} onClick={props.setSelectedTerritory} />
-      <NewSouthWales selectedTerritory={props.selectedTerritory} onClick={props.setSelectedTerritory} />
-      <AustralianCapitalTerritory selectedTerritory={props.selectedTerritory} onClick={props.setSelectedTerritory} />
-      <Victoria selectedTerritory={props.selectedTerritory} onClick={props.setSelectedTerritory} />
-      <Tasmania selectedTerritory={props.selectedTerritory} onClick={props.setSelectedTerritory} />
-      <SouthAustralia selectedTerritory={props.selectedTerritory} onClick={props.setSelectedTerritory} />
+    <svg className="svg-content" viewBox="0 0 694 645" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <TerritorySvgContainer onClick={() => props.setSelectedTerritory('VIC')} isSelected={props.selectedTerritory === 'VIC'}>
+        <VictoriaSvg />
+      </TerritorySvgContainer>
+
+      <TerritorySvgContainer onClick={() => props.setSelectedTerritory('NSW')} isSelected={props.selectedTerritory === 'NSW'}>
+        <NewSouthWalesSvg />
+      </TerritorySvgContainer>
+
+      <TerritorySvgContainer onClick={() => props.setSelectedTerritory('ACT')} isSelected={props.selectedTerritory === 'ACT'}>
+        <ActSvg />
+      </TerritorySvgContainer>
+
+      <TerritorySvgContainer onClick={() => props.setSelectedTerritory('QLD')} isSelected={props.selectedTerritory === 'QLD'}>
+        <QueenslandSvg />
+      </TerritorySvgContainer>
+
+      <TerritorySvgContainer onClick={() => props.setSelectedTerritory('SA')} isSelected={props.selectedTerritory === 'SA'}>
+        <SouthAustraliaSvg />
+      </TerritorySvgContainer>
+
+      <TerritorySvgContainer onClick={() => props.setSelectedTerritory('WA')} isSelected={props.selectedTerritory === 'WA'}>
+        <WesternAustraliaSvg />
+      </TerritorySvgContainer>
+
+      <TerritorySvgContainer onClick={() => props.setSelectedTerritory('TAS')} isSelected={props.selectedTerritory === 'TAS'}>
+        <TasmaniaSvg />
+      </TerritorySvgContainer>
+
+      <TerritorySvgContainer onClick={() => props.setSelectedTerritory('NT')} isSelected={props.selectedTerritory === 'NT'}>
+        <NorthernTerritorySvg />
+      </TerritorySvgContainer>
     </svg>
   </div>
 );
