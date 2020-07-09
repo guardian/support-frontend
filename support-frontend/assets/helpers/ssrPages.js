8f7228e7979ed5c8450fdceb908c126f5b7b82f4
Tom Pretty <tompretty2@gmail.com>
2020-07-09 09:30:12 +0100

    Remove aus map from ssr pages

--- support-frontend/assets/helpers/ssrPages.js
+++ support-frontend/assets/helpers/ssrPages.js
@@ -1,7 +1,6 @@
 // @flow
 import { renderToString } from 'react-dom/server';
 import { content as showcase } from 'pages/showcase/showcase';
-import { AusMomentMap } from 'pages/aus-moment-map/ausMomentMap';
 import { EmptyContributionsLandingPage } from 'pages/contributions-landing/EmptyContributionsLandingPage';
 
 const render = content => renderToString(content);
@@ -14,9 +13,5 @@ export const pages = [
   {
     filename: 'contributions-landing.html',
     html: render(EmptyContributionsLandingPage()),
-  },
-  {
-    filename: 'aus-moment-map.html',
-    html: render(AusMomentMap()),
   },
 ];
