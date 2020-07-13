dbf2a1f3e29a32b6b0ea2814cb1b4cb3cfeac520
George Haberis <george.haberis@guardian.co.uk>
2020-07-13 17:48:34 +0100

    copy refresh

--- support-frontend/assets/pages/showcase/components/regionalContent.jsx
+++ support-frontend/assets/pages/showcase/components/regionalContent.jsx
@@ -5,11 +5,11 @@ export const regionalContent = {
     title: 'Windrush',
     caption: (
       <span>
-        The Guardian is committed to holding power to account &mdash; from our reporting on&#32;
-        the Windrush revelations to the Cambridge Analytica scandal &mdash; while keeping the climate emergency&#32;
-        front and centre. We will continue to report on the fallout from the EU referendum, the immigration&#32;
-        crisis, and data privacy issues. We remain dedicated to truth, to giving a voice to those&#32;
-        who have gone unheard, and to helping bring about a more hopeful future.
+        The Guardian has an illustrious record of holding power to account and championing the voices of&#32;
+        those less heard. Our Covid-19 investigations exposed governmental and social failings, as did our&#32;
+        earlier work on the Snowden disclosures, the Windrush scandal, Cambridge Analytica and the Panama Papers.&#32;
+        We are passionate about the climate emergency, social justice, fairness and progress. We remain dedicated&#32;
+        to truth and to bringing about a more hopeful future.
       </span>),
     images: {
       first: 'showcaseUKProtest',
@@ -22,10 +22,11 @@ export const regionalContent = {
     title: 'Climate crisis',
     caption: (
       <span>
-        <i>Guardian Australia</i> has made the climate emergency a top editorial priority, covering issues from the&#32;
-        management of the Murray-Darling basin, to farming in a changing climate, and disappearing species across&#32;
-        the nation. Our political team in Canberra continues to report on the issues of the day and, across&#32;
-        Australia, our award-winning coverage of Indigenous affairs and immigration issues remains core to our work.
+        <i>Guardian Australia</i> publishes the key stories in Australia and around the world. Our political team&#32;
+        in Canberra reports on the issues of the day, and across the country our award-winning journalism continues&#32;
+        to focus on the climate emergency, Indigenous affairs, corporate transparency, immigration, and all forms&#32;
+        of inequality. We are committed to holding power to account, giving a voice to those who have previously&#32;
+        gone unheard while helping to bring about a more hopeful future.
       </span>),
     images: {
       first: 'showcaseAusRuby',
@@ -35,7 +36,14 @@ export const regionalContent = {
     },
   },
   UnitedStates: {
-    caption: 'From reproductive rights to the climate crisis; from the unchecked power of Big Tech to the fast-approaching 2020 election, and the changes it could bring for the nation and the world, Guardian US is committed to reporting the big issues of the day with honesty and integrity, to giving a voice to those who have previously gone unheard, and helping bring about a more hopeful future.',
+    caption: (
+      <span>
+        From racial inequality to the climate emergency; from the economic crisis to the extraordinary stakes of&#32;
+        the 2020 election, the Guardian reports with honesty and integrity on the big issues of the day in the US&#32;
+        and around the world. We are committed to holding power to account, giving a voice to those who have&#32;
+        previously gone unheard and helping bring about a more hopeful future.
+      </span>
+    ),
     images: {
       first: 'showcaseUSTrump',
       second: 'showcaseUSLiberty',
