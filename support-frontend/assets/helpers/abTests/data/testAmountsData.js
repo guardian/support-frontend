27e9a3aca8a843b0cddcdd5404a850cd35555874
Tom Forbes <tom.forbes@theguardian.com>
2020-07-17 14:23:05 +0100

    UK amounts test

--- support-frontend/assets/helpers/abTests/data/testAmountsData.js
+++ support-frontend/assets/helpers/abTests/data/testAmountsData.js
@@ -13,3 +13,19 @@ export const AusAmounts = {
     ANNUAL: [{ value: '100', isDefault: true }, { value: '250' }, { value: '500' }, { value: '1000' }],
   },
 };
+
+export const UkAmountsV1 = {
+  GBPCountries: {
+    ONE_OFF: [{ value: '35' }, { value: '70', isDefault: true }, { value: '140' }, { value: '280' }],
+    MONTHLY: [{ value: '4' }, { value: '8', isDefault: true }, { value: '16' }],
+    ANNUAL: [{ value: '75', isDefault: true }, { value: '150' }, { value: '300' }, { value: '500' }],
+  },
+};
+
+export const UkAmountsV2 = {
+  GBPCountries: {
+    ONE_OFF: [{ value: '52' }, { value: '104', isDefault: true }, { value: '150' }, { value: '300' }],
+    MONTHLY: [{ value: '4.34' }, { value: '13', isDefault: true }, { value: '21.68' }],
+    ANNUAL: [{ value: '104', isDefault: true }, { value: '156' }, { value: '208' }, { value: '260' }],
+  },
+};
