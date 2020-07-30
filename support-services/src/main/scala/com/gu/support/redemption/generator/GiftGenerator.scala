36a3a71e28af78f1cfb8f68207fcab8d09db4d4a
John Duffell <john.duffell@guardian.co.uk>
2020-07-30 19:06:20 +0100

    separate out to improve testability

--- support-services/src/main/scala/com/gu/support/redemption/generator/GiftGenerator.scala
+++ support-services/src/main/scala/com/gu/support/redemption/generator/GiftGenerator.scala
@@ -2,18 +2,39 @@ package com.gu.support.redemption.generator
 
 import java.security.SecureRandom
 
+import com.gu.support.redemption.generator.ConstructCode.GenerateGiftCode
+
 object GiftGenerator {
 
-  sealed abstract class GiftDuration(val code: String)
-  object GiftDuration {
-    case object Gift3Month extends GiftDuration("03")
-    case object Gift6Month extends GiftDuration("06")
-    case object Gift12Month extends GiftDuration("12")
+  lazy val randomGiftCodes = {
+    val gen = new SecureRandom()
+    val ints = Iterator.continually(gen.nextInt())
+    apply(ints)
   }
+
+  def apply(random: Iterator[Int]): Iterator[GenerateGiftCode] =
+    IntsToTypableString(random).map(ConstructCode.apply)
+
+}
+
+sealed abstract class GiftDuration(val code: String)
+
+object GiftDuration {
+
+  case object Gift3Month extends GiftDuration("03")
+
+  case object Gift6Month extends GiftDuration("06")
+
+  case object Gift12Month extends GiftDuration("12")
+
+}
+
+object ConstructCode {
 
   private val prefix = "gd"
 
   case class GiftCode private(value: String) extends AnyVal
+
   object GiftCode {
     def apply(value: String): Option[GiftCode] =
       Some(value)
@@ -25,18 +46,11 @@ object GiftGenerator {
     def apply(duration: GiftDuration): GiftCode
   }
 
-  val defaultRandom = {
-    val gen = new SecureRandom()
-    Iterator.continually(gen.nextInt())
-  }
-
-  def apply(random: Iterator[Int] = defaultRandom): GenerateGiftCode = new GenerateGiftCode {
-    override def apply(duration: GiftDuration): GiftCode = {
+  def apply(code: IntsToTypableString.Code): GenerateGiftCode =
+    (duration: GiftDuration) => {
       val init = prefix + duration.code + "-"
-      GiftCode(init + IntsToTypableString(random).value).get
+      GiftCode(init + code.value).get
     }
-
-  }
 
 }
 
@@ -50,16 +64,20 @@ object IntsToTypableString {
         .map(new Code(_))
   }
 
-  def apply(random: Iterator[Int]): Code = {
-    val chars = random
+  def apply(random: Iterator[Int]): Iterator[Code] = {
+    random
       .map(int => java.lang.Integer.toString(int, 34).last)
       .map {
         case '1' => 'y'
         case 'l' => 'z'
         case other => other
       }
+      .grouped(6)
+      .map(_.toArray)
+      .map(new String(_))
+      .map(Code.apply)
+      .map(_.get)
 
-    Code(new String(chars.take(6).toArray)).get
 
   }
 
--- support-services/src/test/scala/com/gu/support/redemption/generator/GiftGeneratorSpec.scala
+++ support-services/src/test/scala/com/gu/support/redemption/generator/GiftGeneratorSpec.scala
@@ -1,19 +1,18 @@
 package com.gu.support.redemption.generator
 
-import com.gu.support.redemption.generator.GiftGenerator.GiftDuration
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class GiftGeneratorSpec extends AnyFlatSpec with Matchers {
 
   it should "work in the basic case" in {
-    val giftCode = GiftGenerator(Iterator.continually(0))(GiftDuration.Gift3Month)
+    val giftCode = GiftGenerator(Iterator.continually(0)).take(1).toList.head(GiftDuration.Gift3Month)
     giftCode.value should be("gd03-000000")
   }
 
   it should "always produce different codes with the built in random" in {
-    val generateGiftCode = GiftGenerator(GiftGenerator.defaultRandom)
-    val giftCodes = Iterator.continually(generateGiftCode(GiftDuration.Gift3Month))
+    val generateGiftCode = GiftGenerator.randomGiftCodes
+    val giftCodes = generateGiftCode.map(_(GiftDuration.Gift3Month))
     val numberToCheck = 10000
     val duplicateCodes = giftCodes.take(numberToCheck).toList.groupBy(identity).collect {
       case (code, list) if list.length > 1 => (code, list.length)
@@ -23,25 +22,31 @@ class GiftGeneratorSpec extends AnyFlatSpec with Matchers {
 
 }
 
+class ConstructCodeSpec  extends AnyFlatSpec with Matchers {
+
+  it should "get the durations right for the codes" in {
+    ConstructCode(IntsToTypableString.Code("000000").get)(GiftDuration.Gift3Month).value should be("gd03-000000")
+    ConstructCode(IntsToTypableString.Code("000000").get)(GiftDuration.Gift6Month).value should be("gd06-000000")
+    ConstructCode(IntsToTypableString.Code("000000").get)(GiftDuration.Gift12Month).value should be("gd12-000000")
+  }
+
+}
+
 class IntsToTypableStringSpec extends AnyFlatSpec with Matchers {
 
   it should "extreme values" in {
-    IntsToTypableString(Iterator.continually(0)).value should be("000000")
-    IntsToTypableString(Iterator.continually(-1)).value should be("yyyyyy")
-    IntsToTypableString(Iterator.continually(34)).value should be("000000")
-    IntsToTypableString(Iterator.continually(Int.MaxValue)).value should be("pppppp")
-    IntsToTypableString(Iterator.continually(Int.MinValue)).value should be("qqqqqq")
+    IntsToTypableString(Iterator.continually(0)).take(1).toList.head.value should be("000000")
+    IntsToTypableString(Iterator.continually(-1)).take(1).toList.head.value should be("yyyyyy")
+    IntsToTypableString(Iterator.continually(34)).take(1).toList.head.value should be("000000")
+    IntsToTypableString(Iterator.continually(Int.MaxValue)).take(1).toList.head.value should be("pppppp")
+    IntsToTypableString(Iterator.continually(Int.MinValue)).take(1).toList.head.value should be("qqqqqq")
   }
 
   it should "cycle through all the possiblities" in {
     val seq = Stream.from(0).iterator
-    IntsToTypableString(seq).value should be("0y2345")
-    IntsToTypableString(seq).value should be("6789ab")
-    IntsToTypableString(seq).value should be("cdefgh")
-    IntsToTypableString(seq).value should be("ijkzmn")
-    IntsToTypableString(seq).value should be("opqrst")
-    IntsToTypableString(seq).value should be("uvwx0y")
-    IntsToTypableString(seq).value should be("234567")
+    IntsToTypableString(seq).take(7).toList.map(_.value) should be(
+      List("0y2345", "6789ab", "cdefgh", "ijkzmn", "opqrst", "uvwx0y", "234567")
+    )
   }
 
 }
