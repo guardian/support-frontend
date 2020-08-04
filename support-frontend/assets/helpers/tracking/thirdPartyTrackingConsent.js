6994c28ddd22ce027cd8d60090fec9787a88b594
George Haberis <george.haberis@guardian.co.uk>
2020-08-04 17:36:48 +0100

    dynamically load cmp dependencies to fix ssr

--- support-frontend/assets/helpers/tracking/thirdPartyTrackingConsent.js
+++ support-frontend/assets/helpers/tracking/thirdPartyTrackingConsent.js
@@ -1,7 +1,8 @@
 // @flow
 
-import { onConsentChange } from '@guardian/consent-management-platform';
+// import { onConsentChange } from '@guardian/consent-management-platform';
 import { logException } from 'helpers/logger';
+import { getGlobal } from 'helpers/globals';
 
 const OptedIn: 'OptedIn' = 'OptedIn';
 const OptedOut: 'OptedOut' = 'OptedOut';
@@ -23,17 +24,34 @@ type ConsentState = {
     };
 }
 
-const getTrackingConsent = (): Promise<ThirdPartyTrackingConsent> => new Promise((resolve) => {
-  onConsentChange((state: ConsentState) => {
-    const consentGranted = state.ccpa ?
-      !state.ccpa.doNotSell : state.tcfv2 && Object.values(state.tcfv2.consents).every(Boolean);
+const getTrackingConsent = (): Promise<ThirdPartyTrackingConsent> => new Promise((resolve, reject) => {
+  /**
+   * Dynamically load @guardian/consent-management-platform
+   * on condition we're not server side rendering (ssr) the page.
+   * @guardian/consent-management-platform breaks ssr otherwise.
+   */
+  if (!getGlobal('ssr')) {
+    import('@guardian/consent-management-platform').then(({
+      onConsentChange,
+    }) => {
+      try {
+        onConsentChange((state: ConsentState) => {
+          const consentGranted = state.ccpa ?
+            !state.ccpa.doNotSell : state.tcfv2 && Object.values(state.tcfv2.consents).every(Boolean);
 
-    if (consentGranted) {
-      resolve(OptedIn);
-    } else {
-      resolve(OptedOut);
-    }
-  });
+          if (consentGranted) {
+            resolve(OptedIn);
+          } else {
+            resolve(OptedOut);
+          }
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  } else {
+    resolve(OptedOut);
+  }
 }).catch((err) => {
   logException(`CCPA: ${err}`);
   // fallback to OptedOut if there's an issue getting consentState
