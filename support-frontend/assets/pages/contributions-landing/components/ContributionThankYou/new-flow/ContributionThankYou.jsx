22c65ba4028e76545db314b17360fe1c376ca46b
Tom Pretty <tompretty2@gmail.com>
2020-09-01 14:22:06 +0100

    Add basic 'hear from our newsroom' section

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/new-flow/ContributionThankYou.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/new-flow/ContributionThankYou.jsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/core';
 import { GridRow, GridItem } from '@guardian/src-grid';
 import ContributionThankYouHeader from './ContributionThankYouHeader';
 import ContributionThankYouContinueToAccount from './ContributionThankYouContinueToAccount';
+import ContributionThankYouHearFromOurNewsroom from './ContributionThankYouHearFromOurNewsroom';
 
 const container = css`
   background: white;
@@ -17,6 +18,11 @@ const ContributionThankYou = () => (
     <GridRow breakpoints={['mobile', 'tablet']} >
       <GridItem spans={[4, 12]}>
         <ContributionThankYouContinueToAccount />
+      </GridItem>
+    </GridRow>
+    <GridRow breakpoints={['mobile', 'tablet']} >
+      <GridItem spans={[4, 12]}>
+        <ContributionThankYouHearFromOurNewsroom />
       </GridItem>
     </GridRow>
   </div>
