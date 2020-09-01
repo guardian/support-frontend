3380be6bfdd5287e8c0a284f8b6b191c7155c26f
Tom Pretty <tompretty2@gmail.com>
2020-09-01 08:44:49 +0100

    Use abtestParticipation to show old/new flow

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYou.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYou.jsx
@@ -1,11 +1,20 @@
+// @flow
+import React from 'react';
+import { connect } from 'react-redux';
+import { type State } from '../../contributionsLandingReducer';
 import OldContributionThankYou from './old-flow/ContributionThankYouContainer';
 import NewContribtuionThankYou from './new-flow/ContributionThankYou';
 
-const ContributionThankYou = () => {
-  const showNew = false;
-  return (
-    showNew ? <NewContribtuionThankYou /> : <OldContributionThankYou />
-  );
-};
+type ContributionThankYouProps = {|
+  shouldShowNewFlow: boolean,
+|};
 
-export default ContributionThankYou;
+const mapStateToProps = (state: State) => ({
+  shouldShowNewFlow: state.common.abParticipations.newThankYouFlowR1 === 'newFlow',
+});
+
+const ContributionThankYou = ({ shouldShowNewFlow }: ContributionThankYouProps) => (
+  shouldShowNewFlow ? <NewContribtuionThankYou /> : <OldContributionThankYou />
+);
+
+export default connect(mapStateToProps)(ContributionThankYou);
