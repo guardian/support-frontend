af1c97eb6818b0444f46a6168bd66023e78c73cf
Tom Pretty <tompretty2@gmail.com>
2020-09-02 09:10:33 +0100

    Add desktop styles to header

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/new-flow/ContributionThankYouHeader.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/new-flow/ContributionThankYouHeader.jsx
@@ -1,6 +1,7 @@
 import { css } from '@emotion/core';
 import { titlepiece, body } from '@guardian/src-foundations/typography';
 import { space } from '@guardian/src-foundations';
+import { from } from '@guardian/src-foundations/mq';
 
 const header = css`
   padding-top: ${space[4]}px;
@@ -12,24 +13,91 @@ const headerTitleText = css`
   flex-direction: column;
   ${titlepiece.small()};
   font-size: 24px;
+
+  ${from.desktop} {
+    font-size: 40px;
+  }
+`;
+
+const headerTitleTextThreeLines = css`
+  ${headerTitleText}
+  display: flex;
+
+  ${from.desktop} {
+    display: none;
+  }
+`;
+
+const headerTitleTextTwoLines = css`
+  ${headerTitleText}
+  display: none;
+
+  ${from.desktop} {
+    display: flex;
+  }
 `;
 
 const headerSupportingText = css`
   ${body.small()};
+
+  ${from.desktop} {
+    font-size: 17px;
+    padding-top: ${space[2]}px;
+  }
+`;
+
+const headerSupportingTextOneLine = css`
+  ${headerSupportingText}
+
+  ${from.desktop} {
+    display: none;
+  }
+`;
+
+const headerSupportingTextTwoLines = css`
+  ${headerSupportingText}
+  display: none;
+
+  ${from.desktop} {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 const ContributionThankYouHeader = () => (
   <header css={header}>
-    <h1 css={headerTitleText}>
-      <span>Thank you Anastasia</span>
-      <span>for your valuable</span>
-      <span>contribution <span role="img" aria-label="heart" >❤️</span></span>
+    <h1>
+      <span css={headerTitleTextThreeLines}>
+        <span>Thank you Anastasia</span>
+        <span>for your valuable</span>
+        <span>
+          contribution{' '}
+          <span role="img" aria-label="heart">
+            ❤️
+          </span>
+        </span>
+      </span>
+      <span css={headerTitleTextTwoLines}>
+        <span>Thank you Anastasia for your</span>
+        <span>
+          valuable contribution{' '}
+          <span role="img" aria-label="heart">
+            ❤️
+          </span>
+        </span>
+      </span>
     </h1>
-    <p css={headerSupportingText}>
-      To support us further, and enhance your experience with the Guardian, select the add-ons that suit you best.
+    <p>
+      <span css={headerSupportingTextOneLine}>
+        To support us further, and enhance your experience with the Guardian,
+        select the add-ons that suit you best.
+      </span>
+      <span css={headerSupportingTextTwoLines}>
+        <span>To support us further, and enhance your experience with the</span>
+        <span>Guardian, select the add-ons that suit you best.</span>
+      </span>
     </p>
   </header>
 );
-
 
 export default ContributionThankYouHeader;
