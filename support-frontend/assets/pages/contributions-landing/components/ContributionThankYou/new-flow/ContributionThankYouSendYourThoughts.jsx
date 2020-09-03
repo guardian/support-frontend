54977082c2eed7c0ef945fb5b9983e253543cf1b
Tom Pretty <tompretty2@gmail.com>
2020-09-03 10:46:49 +0100

    Add thank you version of 'share your thoughts' section

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/new-flow/ContributionThankYouSendYourThoughts.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/new-flow/ContributionThankYouSendYourThoughts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/core';
 import { space } from '@guardian/src-foundations';
 import { Button } from '@guardian/src-button';
@@ -14,32 +14,53 @@ const buttonContainer = css`
 `;
 
 const ContributionThankYouSendYourThoughts = () => {
+  const [hasBeenInteractedWith, setHasBeenInteractedWith] = useState(false);
   const actionIcon = <SvgSpeechBubbleWithPlus />;
-  const actionHeader = <ActionHeader title="Send us your thoughts" />;
+  const actionHeader = (
+    <ActionHeader
+      title={
+        hasBeenInteractedWith
+          ? 'Thank you for sharing your thoughts'
+          : 'Send us your thoughts'
+      }
+    />
+  );
   const actionBody = (
     <ActionBody>
-      <p>
-        <span css={styles.hideAfterDesktop}>
-          Fill out this short form to tell us more about your experience of
-          supporting us today.
-        </span>
-        <span css={styles.hideBeforeDesktop}>
+      {hasBeenInteractedWith ? (
+        // TODO: Replace this with final copy
+        <p>
           We would love to hear more about your experience of supporting the
           Guardian today. Please fill out this short form – it only takes a
           minute.
-        </span>
-      </p>
-      <div css={buttonContainer}>
-        <Button
-          priority="primary"
-          size="default"
-          icon={<SvgArrowRightStraight />}
-          iconSide="right"
-          nudgeIcon
-        >
-          Provide feedback
-        </Button>
-      </div>
+        </p>
+      ) : (
+        <>
+          <p>
+            <span css={styles.hideAfterDesktop}>
+              Fill out this short form to tell us more about your experience of
+              supporting us today.
+            </span>
+            <span css={styles.hideBeforeDesktop}>
+              We would love to hear more about your experience of supporting the
+              Guardian today. Please fill out this short form – it only takes a
+              minute.
+            </span>
+          </p>
+          <div css={buttonContainer}>
+            <Button
+              onClick={() => setHasBeenInteractedWith(true)}
+              priority="primary"
+              size="default"
+              icon={<SvgArrowRightStraight />}
+              iconSide="right"
+              nudgeIcon
+            >
+              Provide feedback
+            </Button>
+          </div>
+        </>
+      )}
     </ActionBody>
   );
 
