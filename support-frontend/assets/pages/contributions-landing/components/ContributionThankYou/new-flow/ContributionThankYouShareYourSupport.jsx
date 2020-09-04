09d26411947c6505baa3d481eaa78c8f301d854f
Tom Pretty <tompretty2@gmail.com>
2020-09-04 13:48:59 +0100

    Add basic social link destinations

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/new-flow/ContributionThankYouShareYourSupport.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/new-flow/ContributionThankYouShareYourSupport.jsx
@@ -8,6 +8,12 @@ import ActionHeader from './components/ActionHeader';
 import ActionBody from './components/ActionBody';
 import SvgShare from './components/SvgShare';
 import SvgLinkedIn from './components/SvgLinkedIn';
+import {
+  getFacebookShareLink,
+  getTwitterShareLink,
+  getLinkedInShareLink,
+  getEmailShareLink,
+} from './utils/social';
 
 const buttonsContainer = css`
   margin-top: ${space[6]}px;
@@ -16,6 +22,13 @@ const buttonsContainer = css`
     margin-left: ${space[3]}px;
   }
 `;
+
+const LANDING_PAGE_URL = 'https://support.theguardian.com/contribute';
+const TWITTER_TEXT_COPY =
+  'Join me and over one million others in supporting a different model for open, independent journalism. Together we can help safeguard The Guardian’s future – so more people, across the world, can keep accessing factual information for free';
+const EMAIL_SUBJECT_COPY = 'Join me in supporting open, independent journalism';
+const EMAIL_BODY_COPY =
+  'Join me and over one million others in supporting a different model for open, independent journalism. Together we can help safeguard The Guardian’s future – so more people, across the world, can keep accessing factual information for free: https://support.theguardian.com/contribute';
 
 const ContributionThankYouSendYourThoughts = () => {
   const actionIcon = <SvgShare />;
@@ -28,24 +41,36 @@ const ContributionThankYouSendYourThoughts = () => {
       </p>
       <div css={buttonsContainer}>
         <LinkButton
+          href={getFacebookShareLink(LANDING_PAGE_URL)}
+          target="_blank"
+          rel="noopener noreferrer"
           priority="tertiary"
           size="default"
           icon={<SvgFacebook />}
           hideLabel
         />
         <LinkButton
+          href={getTwitterShareLink(LANDING_PAGE_URL, TWITTER_TEXT_COPY)}
+          target="_blank"
+          rel="noopener noreferrer"
           priority="tertiary"
           size="default"
           icon={<SvgTwitter />}
           hideLabel
         />
         <LinkButton
+          href={getLinkedInShareLink(LANDING_PAGE_URL)}
+          target="_blank"
+          rel="noopener noreferrer"
           priority="tertiary"
           size="default"
           icon={<SvgLinkedIn />}
           hideLabel
         />
         <LinkButton
+          href={getEmailShareLink(EMAIL_SUBJECT_COPY, EMAIL_BODY_COPY)}
+          target="_blank"
+          rel="noopener noreferrer"
           priority="tertiary"
           size="default"
           icon={<SvgEnvelope />}
---
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/new-flow/utils/social.js
@@ -0,0 +1,23 @@
+// @flow
+
+export const getFacebookShareLink = (url: string): string => {
+  const encodedUrl = encodeURI(url);
+  return `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`;
+};
+
+export const getTwitterShareLink = (url: string, text: string): string => {
+  const encodedUrl = encodeURI(url);
+  const encodedText = encodeURI(text);
+  return `https://twitter.com/intent/tweet?url=${encodedUrl}&text=${encodedText}`;
+};
+
+export const getLinkedInShareLink = (url: string): string => {
+  const encodedUrl = encodeURI(url);
+  return `https://www.linkedin.com/shareArticle?mini=true&url=${encodedUrl}`;
+};
+
+export const getEmailShareLink = (subject: string, body: string): string => {
+  const encodedSubject = encodeURI(subject);
+  const encodedBody = encodeURI(body);
+  return `mailto:?subject=${encodedSubject}&body=${encodedBody}`;
+};
