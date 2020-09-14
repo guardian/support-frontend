7555d3e300940b7ad522bcf543fc8ab692e24a6e
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2020-09-14 12:15:46 +0100

    Fix overly long line in digital controller again

--- support-frontend/app/controllers/DigitalSubscriptionController.scala
+++ support-frontend/app/controllers/DigitalSubscriptionController.scala
@@ -109,8 +109,11 @@ class DigitalSubscriptionController(
     }
 
   private def digitalSubscriptionFormHtml(idUser: IdUser, orderIsAGift: Boolean)(implicit request: RequestHeader, settings: AllSettings): Html = {
-    val title = if (orderIsAGift)
-      "Support the Guardian | The Guardian Digital Gift Subscription" else "Support the Guardian | The Guardian Digital Subscription"
+    val title = if (orderIsAGift) {
+      "Support the Guardian | The Guardian Digital Gift Subscription"
+     } else {
+       "Support the Guardian | The Guardian Digital Subscription"
+     }
     val id = EmptyDiv("digital-subscription-checkout-page")
     val js = "digitalSubscriptionCheckoutPage.js"
     val css = "digitalSubscriptionCheckoutPage.css"
