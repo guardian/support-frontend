b4423c277485a54803335946b99d14c188197722
John Duffell <john.duffell@guardian.co.uk>
2020-09-24 17:57:24 +0100

    add tests for email SQS message content for DS

--- support-workers/src/test/scala/com/gu/emailservices/EmailFieldsSpec.scala
+++ support-workers/src/test/scala/com/gu/emailservices/EmailFieldsSpec.scala
@@ -1,7 +1,13 @@
 package com.gu.emailservices
 
+import com.gu.support.workers.integration.TestData.{countryOnlyAddress, directDebitPaymentMethod, subsFields}
+import com.gu.support.workers.states.PaymentMethodWithSchedule
+import com.gu.support.workers.{Annual, Payment, PaymentSchedule, User}
+import com.gu.support.zuora.api.ReaderType
 import io.circe.parser._
 import io.circe.syntax._
+import org.joda.time.LocalDate
+import org.scalatest.Inside
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -37,5 +43,87 @@ class EmailFieldsSpec extends AnyFlatSpec with Matchers {
     ).asJson
 
     serializedJson shouldBe expectedJson
+  }
+}
+
+class DigitalPackEmailFieldsSpec extends AnyFlatSpec with Matchers with Inside {
+
+  it should "generate the right json for direct subs" in {
+    val expectedJson = parse(
+      """{
+        |"To" : {
+        |  "Address" : "test@gu.com",
+        |  "ContactAttributes" : {
+        |    "SubscriberAttributes" : {
+        |      "first_name" : "Mickey",
+        |      "emailaddress" : "test@gu.com",
+        |      "mandateid" : "65HK26E",
+        |      "subscription_details" : "£119.90 for the first year",
+        |      "date_of_first_payment" : "Monday, 14 January 2019",
+        |      "country" : "United Kingdom",
+        |      "trial_period" : "14",
+        |      "account_number" : "******11",
+        |      "zuorasubscriberid" : "A-S00045678",
+        |      "sort_code" : "20-20-20",
+        |      "last_name" : "Mouse",
+        |      "subscription_term" : "year",
+        |      "account_name" : "Mickey Mouse",
+        |      "currency" : "£",
+        |      "payment_amount" : "119.90",
+        |      "default_payment_method" : "Direct Debit"
+        |    }
+        |  }
+        |},
+        |"DataExtensionName" : "digipack",
+        |"SfContactId" : "0033E00001DTBHJQA5",
+        |"IdentityUserId" : null
+        |}
+        |""".stripMargin)
+    val actual = new DigitalPackEmailFields(
+      subsFields(Annual, User("1234", "test@gu.com", None, "Mickey", "Mouse", billingAddress = countryOnlyAddress))
+    ).build(
+      paidSubPaymentData = Some(PaymentMethodWithSchedule(
+        directDebitPaymentMethod,
+        PaymentSchedule(List(Payment(new LocalDate(2019, 1, 14), 119.90)))
+      )),
+      ReaderType.Direct
+    ).map(_.map(ef => parse(ef.payload)))
+    inside(actual) {
+      case Right(actualJson :: Nil) =>
+        actualJson should be(expectedJson)
+    }
+  }
+
+
+  it should "generate the right json for corporate subs" in {
+    val expectedJson = parse(
+      """{
+        |"To" : {
+        |  "Address" : "test@gu.com",
+        |  "ContactAttributes" : {
+        |    "SubscriberAttributes" : {
+        |      "first_name" : "Mickey",
+        |      "emailaddress" : "test@gu.com",
+        |      "subscription_details" : "Group subscription",
+        |      "zuorasubscriberid" : "A-S00045678",
+        |      "last_name" : "Mouse"
+        |    }
+        |  }
+        |},
+        |"DataExtensionName" : "digipack-corp",
+        |"SfContactId" : "0033E00001DTBHJQA5",
+        |"IdentityUserId" : null
+        |}
+        |""".stripMargin)
+    val actual = new DigitalPackEmailFields(
+      subsFields(Annual, User("1234", "test@gu.com", None, "Mickey", "Mouse", billingAddress = countryOnlyAddress))
+    ).build(
+      paidSubPaymentData = None,
+      ReaderType.Corporate
+    ).map(_.map(ef => parse(ef.payload)))
+    inside(actual) {
+      case Right(actualJson :: Nil) =>
+        actualJson should be(expectedJson)
+    }
   }
 }
