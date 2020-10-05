d73db41b9812797519718fbb9caabe85a01e2580
Rupert Bates <Rupert.Bates@guardian.co.uk>
2020-10-05 12:15:12 +0100

    Extract prefix to val

--- support-models/src/test/scala/com/gu/support/redemptions/RedemptionCodeSpec.scala
+++ support-models/src/test/scala/com/gu/support/redemptions/RedemptionCodeSpec.scala
@@ -5,17 +5,19 @@ import org.scalatest.matchers.should.Matchers
 
 class RedemptionCodeSpec extends AnyFlatSpec with Matchers {
 
+  val codePrefix = "code-123456-"
+
   "RedemptionCode" should "disallow invalid chars" in {
     val badCodes = """ !"#$%&'()*+,./:;<=>?@[\]^_`{|}~"""
     badCodes.foreach { char =>
-      RedemptionCode(s"code-123456-$char").isLeft should be(true)
+      RedemptionCode(s"$codePrefix$char").isLeft should be(true)
     }
   }
 
   it should "allow valid chars" in {
     val codes = """-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"""
     codes.foreach { char =>
-      RedemptionCode(s"code-123456-$char").isRight should be(true)
+      RedemptionCode(s"$codePrefix$char").isRight should be(true)
     }
   }
 
