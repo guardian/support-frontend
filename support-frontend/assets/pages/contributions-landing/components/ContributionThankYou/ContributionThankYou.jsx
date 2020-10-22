81a0d3dd2042a15ce395694c7f62d299e2883105
Tom Pretty <tompretty2@gmail.com>
2020-10-22 10:13:50 +0100

    Update survey link and make it hide 2 weeks from now

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYou.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYou.jsx
@@ -174,9 +174,11 @@ const ContributionThankYou = ({
     />,
     shouldShow: contributionType === 'ONE_OFF',
   };
+  const SURVEY_END_DATE = new Date(2020, 10, 5);
+  const now = new Date();
   const surveyAction = {
     component: <ContributionThankYouSurvey />,
-    shouldShow: true,
+    shouldShow: now < SURVEY_END_DATE,
   };
   const socialShareAction = {
     component: <ContributionThankYouSocialShare
