a693c9453e5febb9f7ce21eb1e7ffa4a71f11453
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2020-11-02 12:43:50 +0000

    Remove gift version toggle from main page for testing

--- support-frontend/assets/pages/digital-subscription-landing/components/giftNonGiftLink.jsx
+++ support-frontend/assets/pages/digital-subscription-landing/components/giftNonGiftLink.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { css } from '@emotion/core';
 import { ThemeProvider } from 'emotion-theming';
-import { headline, body } from '@guardian/src-foundations/typography';
+import { headline /* body (commented out as temporarily not in use) */ } from '@guardian/src-foundations/typography';
 import { space } from '@guardian/src-foundations';
 import { from } from '@guardian/src-foundations/mq';
 import { LinkButton, buttonReaderRevenue } from '@guardian/src-button';
@@ -20,33 +20,59 @@ const title = css`
   }
 `;
 
-const paragraph = css`
-  ${body.medium()};
-  display: block;
-  margin: ${space[1]}px 0 ${space[5]}px;
-`;
+// commented out as temporarily not in use
+// const paragraph = css`
+//   ${body.medium()};
+//   display: block;
+//   margin: ${space[1]}px 0 ${space[5]}px;
+// `;
 
 const button = css`
   margin-top: ${space[5]}px;
 `;
 
+const giftPageVersion = (
+  <span>
+    <h4 css={title}>Looking for a subscription for yourself?</h4>
+    <div css={button}>
+      <ThemeProvider theme={buttonReaderRevenue}>
+        <LinkButton
+          href={routes.digitalSubscriptionLanding}
+          priority="tertiary"
+        >
+            See personal subscriptions
+        </LinkButton>
+      </ThemeProvider>
+    </div>
+  </span>
+);
+
+// commented out as temporarily not in use
+// const nonGiftPageVersion = (
+//   <span>
+//     <h4 css={title}>Gift subscriptions</h4>
+//     <span css={paragraph}>A digital subscription makes a great gift.</span>
+//     <div css={button}>
+//       <ThemeProvider theme={buttonReaderRevenue}>
+//         <LinkButton
+//           href={routes.digitalSubscriptionLandingGift}
+//           priority="tertiary"
+//         >
+//             See gift subscriptions
+//         </LinkButton>
+//       </ThemeProvider>
+//     </div>
+//   </span>
+// );
+
 const GiftNonGiftLink = (props: {orderIsAGift: boolean}) => (
   <div className="hope-is-power__terms">
     <div className="hope-is-power--centered">
       {props.orderIsAGift ?
-        <h4 css={title}>Looking for a subscription for yourself?</h4> :
-        <h4 css={title}>Gift subscriptions</h4>}
-      {!props.orderIsAGift && <span css={paragraph}>A digital subscription makes a great gift.</span>}
-      <div css={button}>
-        <ThemeProvider theme={buttonReaderRevenue}>
-          <LinkButton
-            href={props.orderIsAGift ? routes.digitalSubscriptionLanding : routes.digitalSubscriptionLandingGift}
-            priority="tertiary"
-          >
-            {props.orderIsAGift ? 'See personal subscriptions' : 'See gift subscriptions'}
-          </LinkButton>
-        </ThemeProvider>
-      </div>
+        giftPageVersion :
+        null // to be replaced with nonGiftPageVersion when we're ready to go live
+      }
+
     </div>
   </div>);
 
