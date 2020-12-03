2118a22f8ca31fca8de8161fe47ff5b82177dd49
Tom Forbes <tom.forbes@theguardian.com>
2020-12-03 15:38:21 +0000

    update jackson dependency

--- project/LibraryVersions.scala
+++ project/LibraryVersions.scala
@@ -3,7 +3,7 @@ object LibraryVersions {
   val circeVersion = "0.12.1"
   val awsClientVersion = "1.11.827"
   val catsVersion = "1.4.0"
-  val jacksonVersion = "2.10.5"
+  val jacksonVersion = "2.12.0"
   val okhttpVersion = "3.10.0"
   val scalaUriVersion = "2.2.2"
   val playCirceVersion = "2712.0"
