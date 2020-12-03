a88809c53ddb61894d9c826f4f637a40f0adc68d
Imogen Hardy <imogen.hardy@guardian.co.uk>
2020-12-03 15:00:49 +0000

    fix product sort and card positioning on digital subs landing page

--- support-frontend/assets/pages/digital-subscription-landing/components/paymentSelection/paymentSelection.jsx
+++ support-frontend/assets/pages/digital-subscription-landing/components/paymentSelection/paymentSelection.jsx
@@ -12,36 +12,43 @@ import {
 // styles
 import {
   paymentSelection,
-  paymentSelectionCard,
+  productOverride,
+  productOverrideWithLabel,
 } from './paymentSelectionStyles';
 
 export type PropTypes = {
   paymentOptions: Product[],
 }
 
-const PaymentSelection = ({ paymentOptions }: PropTypes) =>
-// The following line makes sure the Annual payment selection card is on the right hand side
+function isAnnualOption(option: Product) {
+  return option.title === 'Annual' || option.title === '12 months';
+}
 
+const PaymentSelection = ({ paymentOptions }: PropTypes) =>
+// The following makes sure the Annual payment selection card is on the right hand side
   (
     <div css={paymentSelection}>
-      {([...paymentOptions].sort((opt1) => { // Spread operator because .sort is mutating
-          if (opt1.title === 'Annual' || opt1.title === '12 months') {
+      {/* Spread operator because .sort is mutating */}
+      {([...paymentOptions].sort((opt1, opt2) => {
+          if (isAnnualOption(opt1)) {
             return 1;
+          }
+          if (isAnnualOption(opt2)) {
+            return -1;
           }
           return 0;
         }).map(product => (
-          <div css={paymentSelectionCard}>
-            <ProductOption
-              title={product.title}
-              price={product.price}
-              offerCopy={product.offerCopy}
-              priceCopy={product.priceCopy}
-              buttonCopy={product.buttonCopy}
-              href={product.href}
-              onClick={product.onClick}
-              label={product.label}
-            />
-          </div>
+          <ProductOption
+            cssOverrides={product.label ? productOverrideWithLabel : productOverride}
+            title={product.title}
+            price={product.price}
+            offerCopy={product.offerCopy}
+            priceCopy={product.priceCopy}
+            buttonCopy={product.buttonCopy}
+            href={product.href}
+            onClick={product.onClick}
+            label={product.label}
+          />
         )))
       }
     </div>);
--- support-frontend/assets/pages/digital-subscription-landing/components/paymentSelection/paymentSelectionStyles.js
+++ support-frontend/assets/pages/digital-subscription-landing/components/paymentSelection/paymentSelectionStyles.js
@@ -1,4 +1,5 @@
 import { css } from '@emotion/core';
+import { space } from '@guardian/src-foundations';
 import { from } from '@guardian/src-foundations/mq';
 
 export const paymentSelection = css`
@@ -45,3 +46,32 @@ export const paymentSelectionCard = css`
     }
   }
 `;
+
+export const productOverride = css`
+  &:not(:first-of-type) {
+    margin-top: ${space[4]}px;
+  }
+  ${from.tablet} {
+    &:not(:first-of-type) {
+      margin-top: 0;
+    }
+    &:not(:last-of-type) {
+      margin-right: ${space[5]}px;
+    }
+  }
+`;
+
+export const productOverrideWithLabel = css`
+  &:not(:first-of-type) {
+    margin-top: ${space[12]}px;
+  }
+  ${from.tablet} {
+    &:not(:first-of-type) {
+      margin-top: 0;
+    }
+    &:not(:last-of-type) {
+      margin-right: ${space[5]}px;
+    }
+  }
+`;
+
