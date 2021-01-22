c080b313342c94390974d2dd7e9f39f9fe94fa6a
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-01-22 14:52:49 +0000

    Fix test

--- supporter-product-data/src/test/scala/com/gu/model/zuora/request/ZuoraQueriesSpec.scala
+++ supporter-product-data/src/test/scala/com/gu/model/zuora/request/ZuoraQueriesSpec.scala
@@ -11,12 +11,16 @@ class ZuoraQueriesSpec extends AnyFlatSpec with Matchers with LazyLogging {
     val date = LocalDate.of(2011, 11, 1)
     val expected = """SELECT
           Account.IdentityId__c,
+          RatePlan.Id,
+          ProductRatePlan.Id,
           RatePlan.Name,
           Subscription.TermEndDate
             FROM
             rateplan
             WHERE
+            Account.IdentityId__c != null AND
             Subscription.Status = 'Active' AND
+            ProductRatePlan.Id != '2c92c0f852f2ebb20152f9269f067819'
             Subscription.TermEndDate >= '2011-11-01'
     """
     val actual = ExportZoqlQueries.selectActiveRatePlans(date)
