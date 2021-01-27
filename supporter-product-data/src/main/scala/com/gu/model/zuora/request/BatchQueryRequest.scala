52ce2ffdc18977d01e357b7e273aa5a75d7abbac
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-01-27 16:01:08 +0000

    Make Aqua query stateful

--- supporter-product-data/src/main/scala/com/gu/model/zuora/request/BatchQueryRequest.scala
+++ supporter-product-data/src/main/scala/com/gu/model/zuora/request/BatchQueryRequest.scala
@@ -22,7 +22,9 @@ object ZoqlExportQuery{
 object BatchQueryRequest{
   implicit val encoder: Encoder[BatchQueryRequest] = deriveEncoder[BatchQueryRequest].mapJsonObject(_
     .add("format", Json.fromString("csv"))
-    .add("version", Json.fromString("1.0"))
+    .add("version", Json.fromString("1.1"))
+    .add("partner", Json.fromString("guardiannews4398"))  // These parameters make the request stateful
+    .add("project", Json.fromString("supporter-product-data")) // see https://knowledgecenter.zuora.com/Central_Platform/API/AB_Aggregate_Query_API/BA_Stateless_and_Stateful_Modes
     .add("encrypted", Json.fromString("none"))
     .add("useQueryLabels", Json.fromString("true"))
     .add("dateTimeUtc", Json.fromString("true"))
