4fec90ac3424323668908e82d5e75c5714d2076d
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-02-26 10:02:33 +0000

    Utilities to extract data & query it

---
+++ supporter-product-data/data-extracts/query.sh
@@ -0,0 +1,3 @@
+ # To run this you will need to install Q http://harelba.github.io/q/
+ # eg. `brew install q`
+ q -d , -H "Select count(0) from ./select-active-rate-plans-2021-02-25T18:25:35.969.csv"
--- supporter-product-data/src/test/scala/com/gu/lambdas/RunFullExportSpec.scala
+++ supporter-product-data/src/test/scala/com/gu/lambdas/RunFullExportSpec.scala
@@ -1,23 +1,60 @@
 package com.gu.lambdas
 
+import com.gu.lambdas.FetchResultsLambda.getValueOrThrow
 import com.gu.lambdas.RunFullExportSpec.sleep
-import com.gu.model.Stage.{DEV, PROD, UAT}
-import com.gu.model.states.QueryType.{Full, Incremental}
+import com.gu.model.Stage
+import com.gu.model.Stage.PROD
+import com.gu.model.states.QueryType.Full
+import com.gu.model.states.UpdateDynamoState
+import com.gu.model.zuora.response.JobStatus.Completed
+import com.gu.okhttp.RequestRunners.configurableFutureRunner
+import com.gu.services.{ConfigService, ZuoraQuerierService}
 import com.gu.test.tags.annotations.IntegrationTest
+import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import scala.concurrent.Future
+import java.nio.file.{FileSystems, Files}
+import java.time.ZonedDateTime
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.Try
 
 @IntegrationTest
-class RunFullExportSpec extends AsyncFlatSpec with Matchers {
-  QueryZuoraLambda.getClass.getSimpleName should "be able to run a query" in {
-    val stage = PROD
+class RunFullExportSpec extends AsyncFlatSpec with Matchers with LazyLogging {
+  val stage = PROD
+
+  "This test is just an easy way to run an aqua query. It" should "save the results to a csv in supporter-product-data/data-extracts" in {
     for {
       fetchResultsState <- QueryZuoraLambda.queryZuora(stage, Full)
-      _ <- sleep(30000)
-      updateDynamoState <- FetchResultsLambda.fetchResults(stage, fetchResultsState.jobId, fetchResultsState.attemptedQueryTime)
+      _ <- sleep(30 * 1000)
+      updateDynamoState <- fetchResults(stage, fetchResultsState.jobId, fetchResultsState.attemptedQueryTime)
     } yield updateDynamoState.filename should startWith ("select-active-rate-plans")
+  }
+
+  def fetchResults(stage: Stage, jobId: String, attemptedQueryTime: ZonedDateTime) = {
+    logger.info(s"Attempting to fetch results for jobId $jobId")
+    for {
+      config <- ConfigService(stage).load
+      service = new ZuoraQuerierService(config, configurableFutureRunner(60.seconds))
+      result <- service.getResults(jobId)
+      _ = assert(result.status == Completed, s"Job with id $jobId is still in status ${result.status}")
+      batch = getValueOrThrow(result.batches.headOption, s"No batches were returned in the batch query response for jobId $jobId")
+      fileId = getValueOrThrow(batch.fileId, s"Batch.fileId was missing in jobId $jobId")
+      filename = s"${batch.name}.csv"
+      fileResponse <- service.getResultFileResponse(fileId)
+      _ = assert(fileResponse.isSuccessful, s"File download for job with id $jobId failed with http code ${fileResponse.code}")
+      filePath = FileSystems.getDefault.getPath(System.getProperty("user.dir"), "supporter-product-data", "data-extracts", filename)
+      _ = Files.copy(fileResponse.body.byteStream, filePath)
+    } yield {
+      logger.info(s"Successfully wrote file $filePath to S3 with ${batch.recordCount} records for jobId $jobId")
+      UpdateDynamoState(
+        filename,
+        batch.recordCount,
+        processedCount = 0,
+        attemptedQueryTime
+      )
+    }
   }
 }
 
