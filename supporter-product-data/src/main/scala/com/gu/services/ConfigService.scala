cb2f0b8aee5750908bc8f0cd00ad8dda0968486d
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-02-26 11:44:04 +0000

    Exclude all discounts from PROD query

--- supporter-product-data/src/main/scala/com/gu/services/ConfigService.scala
+++ supporter-product-data/src/main/scala/com/gu/services/ConfigService.scala
@@ -34,13 +34,23 @@ class ConfigService(stage: Stage) extends StrictLogging {
       case DEV => List("2c92c0f852f2ebb20152f9269f067819")
       case UAT => List("2c92c0f953078a5601531299dae54a4d")
       case PROD => List(
+        // These are all various types of discount so are there
+        // in addition to the actual product subscription
         "2c92a00d6f9de7f6016f9f6f52765aa4",
         "2c92a0076ae9189c016b080c930a6186",
         "2c92a0ff5345f9220153559d915d5c26",
         "2c92a0fe7375d60901737c64808e4be1",
         "2c92a0fe750b35d001750d4522f43817",
         "2c92a00f7468817d01748bd88f0d1d6c",
-        "2c92a0117468816901748bdb3a8c1ac4"
+        "2c92a0117468816901748bdb3a8c1ac4",
+        "2c92a0086f1426d1016f18a9c71058a5",
+        "2c92a0fd6f1426ef016f18a86c515ed7",
+        "2c92a0ff64176cd40164232c8ec97661",
+        "2c92a00864176ce90164232ac0d90fc1",
+        "2c92a0086b25c750016b32548239308d",
+        "2c92a0ff74296d7201742b7daf20123d",
+        "2c92a00772c5c2e90172c7ebd62a68c4",
+        "2c92a01072c5c2e20172c7efe01125c6"
       )
     }
 
