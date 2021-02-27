6598d6f44283cb641712105ef42a60c48c83e988
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-02-27 15:45:42 +0000

    Fix test

--- supporter-product-data/src/test/scala/com/gu/model/zuora/request/SelectActiveRatePlansQuerySpec.scala
+++ supporter-product-data/src/test/scala/com/gu/model/zuora/request/SelectActiveRatePlansQuerySpec.scala
@@ -12,6 +12,8 @@ class SelectActiveRatePlansQuerySpec extends AnyFlatSpec with Matchers with Lazy
   "SelectActiveRatePlansQuery" should "be correct" in {
     val date = LocalDate.of(2011, 11, 1)
     val expected = """SELECT
+          RatePlan.AmendmentType,
+          Subscription.Id,
           Account.IdentityId__c,
           Subscription.GifteeIdentityId__c,
           RatePlan.Id,
@@ -23,6 +25,7 @@ class SelectActiveRatePlansQuerySpec extends AnyFlatSpec with Matchers with Lazy
             WHERE
             Subscription.TermEndDate >= '2011-11-01' AND
             (Subscription.Status = 'Active' OR Subscription.Status = 'Cancelled') AND
+            (RatePlan.AmendmentType is null OR RatePlan.AmendmentType = 'NewProduct' OR RatePlan.AmendmentType = 'UpdateProduct') AND
             ProductRatePlan.Id != '2c92c0f852f2ebb20152f9269f067819' AND
 ProductRatePlan.Id != '2c92c0f852f2ebb20152f9269f067818' AND
             Account.IdentityId__c != null AND Account.IdentityId__c != '' AND
