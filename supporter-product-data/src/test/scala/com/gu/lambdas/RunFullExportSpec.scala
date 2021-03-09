458f67f194416b3ef334a053adff4bfaa9626e19
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-03-09 18:49:02 +0000

    Fix test

--- supporter-product-data/src/test/scala/com/gu/lambdas/RunFullExportSpec.scala
+++ supporter-product-data/src/test/scala/com/gu/lambdas/RunFullExportSpec.scala
@@ -6,6 +6,7 @@ import com.gu.model.Stage
 import com.gu.model.Stage.PROD
 import com.gu.model.states.QueryType.Full
 import com.gu.model.states.UpdateDynamoState
+import com.gu.model.zuora.response.BatchQueryResponse
 import com.gu.model.zuora.response.JobStatus.Completed
 import com.gu.okhttp.RequestRunners.configurableFutureRunner
 import com.gu.services.{ConfigService, ZuoraQuerierService}
@@ -17,6 +18,7 @@ import org.scalatest.matchers.should.Matchers
 import java.io.{File, PrintWriter}
 import java.nio.file.{FileSystems, Files, StandardCopyOption}
 import java.time.ZonedDateTime
+import scala.annotation.tailrec
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.io.Source
@@ -28,32 +30,43 @@ class RunFullExportSpec extends AsyncFlatSpec with Matchers with LazyLogging {
   "This test is just an easy way to run an aqua query. It" should "save the results to a csv in supporter-product-data/data-extracts" in {
     for {
       fetchResultsState <- QueryZuoraLambda.queryZuora(stage, Full)
-      _ <- sleep(45 * 1000)
       updateDynamoState <- fetchResults(stage, fetchResultsState.jobId, fetchResultsState.attemptedQueryTime)
-    } yield updateDynamoState.filename should endWith ("last-extract.csv")
+    } yield updateDynamoState.filename should endWith("last-extract.csv")
   }
 
-  def fetchResults(stage: Stage, jobId: String, attemptedQueryTime: ZonedDateTime) = {
+  def fetchResults(stage: Stage, jobId: String, attemptedQueryTime: ZonedDateTime): Future[UpdateDynamoState] = {
     logger.info(s"Attempting to fetch results for jobId $jobId")
+    sleep(20 * 1000)
     for {
       config <- ConfigService(stage).load
       service = new ZuoraQuerierService(config, configurableFutureRunner(60.seconds))
       result <- service.getResults(jobId)
-      _ = assert(result.status == Completed, s"Job with id $jobId is still in status ${result.status}")
-      batch = getValueOrThrow(result.batches.headOption, s"No batches were returned in the batch query response for jobId $jobId")
-      fileId = getValueOrThrow(batch.fileId, s"Batch.fileId was missing in jobId $jobId")
-      fileResponse <- service.getResultFileResponse(fileId)
-      _ = assert(fileResponse.isSuccessful, s"File download for job with id $jobId failed with http code ${fileResponse.code}")
-      filePath = FileSystems.getDefault.getPath(System.getProperty("user.dir"), "supporter-product-data", "data-extracts", "last-extract.csv")
-      _ = Files.copy(fileResponse.body.byteStream, filePath, StandardCopyOption.REPLACE_EXISTING)
-      _ = sanitizeFieldNames(filePath.toString)
+      finalState <- if (result.status == Completed)
+        downloadResults(result, service)
+      else
+        fetchResults(stage, jobId, attemptedQueryTime)
     } yield {
-      logger.info(s"Successfully wrote file $filePath to S3 with ${batch.recordCount} records for jobId $jobId")
+      finalState
+    }
+  }
+
+  def downloadResults(result: BatchQueryResponse, service: ZuoraQuerierService) = {
+    val batch = getValueOrThrow(result.batches.headOption, s"No batches were returned in the batch query response")
+    val fileId = getValueOrThrow(batch.fileId, s"Batch.fileId was missing in the job")
+    for {
+
+         fileResponse <- service.getResultFileResponse(fileId)
+       _ = assert(fileResponse.isSuccessful, s"File download for job failed with http code ${fileResponse.code}")
+       filePath = FileSystems.getDefault.getPath(System.getProperty("user.dir"), "supporter-product-data", "data-extracts", "last-extract.csv")
+       _ = Files.copy(fileResponse.body.byteStream, filePath, StandardCopyOption.REPLACE_EXISTING)
+       _ = sanitizeFieldNames(filePath.toString)
+    } yield {
+      logger.info(s"Successfully wrote file $filePath with ${batch.recordCount} records")
       UpdateDynamoState(
         filePath.toString,
         batch.recordCount,
         processedCount = 0,
-        attemptedQueryTime
+        ZonedDateTime.now
       )
     }
   }
