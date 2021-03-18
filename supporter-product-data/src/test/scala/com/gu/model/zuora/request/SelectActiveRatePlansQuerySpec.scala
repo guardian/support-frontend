fa8d92705f23e15a9d17524055e84f748d38a616
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-03-18 15:11:01 +0000

    Fix test

--- supporter-product-data/src/test/scala/com/gu/model/zuora/request/SelectActiveRatePlansQuerySpec.scala
+++ supporter-product-data/src/test/scala/com/gu/model/zuora/request/SelectActiveRatePlansQuerySpec.scala
@@ -24,8 +24,7 @@ class SelectActiveRatePlansQuerySpec extends AnyFlatSpec with Matchers with Lazy
             rateplan
             WHERE
             Subscription.TermEndDate >= '2011-11-01' AND
-            (Subscription.Status = 'Active' OR (Subscription.Status = 'Cancelled' AND ProductRatePlan.Id != '2c92c0f852f2ebb20152f9269f067819' AND
-ProductRatePlan.Id != '2c92c0f852f2ebb20152f9269f067818')) AND
+            (Subscription.Status = 'Active' OR Subscription.Status = 'Cancelled') AND
             (RatePlan.AmendmentType is null OR RatePlan.AmendmentType = 'NewProduct' OR RatePlan.AmendmentType = 'UpdateProduct') AND
             ProductRatePlan.Id != '2c92c0f852f2ebb20152f9269f067819' AND
 ProductRatePlan.Id != '2c92c0f852f2ebb20152f9269f067818' AND
