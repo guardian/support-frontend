adf5a84573ef78ba9ec958ecf75bc46f8d7abc3b
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-03-24 12:08:09 +0000

    Better batching

--- supporter-product-data/src/main/scala/com/gu/lambdas/UpdateDynamoLambda.scala
+++ supporter-product-data/src/main/scala/com/gu/lambdas/UpdateDynamoLambda.scala
@@ -5,11 +5,14 @@ import com.gu.lambdas.UpdateDynamoLambda.writeToDynamo
 import com.gu.model.Stage
 import com.gu.model.dynamo.SupporterRatePlanItem
 import com.gu.model.states.UpdateDynamoState
-import com.gu.services.{AlarmService, DynamoDBService, ConfigService, S3Service}
+import com.gu.model.zuora.SubscriptionStatus.Cancelled
+import com.gu.services.{AlarmService, ConfigService, DynamoDBService, S3Service}
 import com.typesafe.scalalogging.StrictLogging
+import io.circe.syntax.EncoderOps
 import kantan.csv._
 import kantan.csv.ops._
 
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
@@ -29,8 +32,8 @@ class UpdateDynamoLambda extends Handler[UpdateDynamoState, UpdateDynamoState] {
 }
 
 object UpdateDynamoLambda extends StrictLogging {
-  val batchSize = 5
-  val timeoutBufferInMillis = batchSize * 5 * 1000
+  val maxBatchSize = 5
+  val timeoutBufferInMillis = maxBatchSize * 5 * 1000
 
   def writeToDynamo(stage: Stage, state: UpdateDynamoState, timeOutCheck: TimeOutCheck): Future[UpdateDynamoState] = {
     logger.info(s"Starting write to dynamo task for ${state.recordCount} records from ${state.filename}")
@@ -52,7 +55,7 @@ object UpdateDynamoLambda extends StrictLogging {
       alarmService.triggerCsvReadAlarm
     }
 
-    val batches = validUnprocessed.grouped(batchSize)
+    val batches = batchItemsWhichCanUpdateConcurrently(validUnprocessed)
 
     val processedCount = writeBatchesUntilTimeout(
       state.processedCount,
@@ -62,10 +65,9 @@ object UpdateDynamoLambda extends StrictLogging {
       alarmService
     )
 
-
     val maybeSaveSuccessTime = if (processedCount == state.recordCount)
-        ConfigService(stage).putLastSuccessfulQueryTime(state.attemptedQueryTime)
-      else Future.successful(())
+      ConfigService(stage).putLastSuccessfulQueryTime(state.attemptedQueryTime)
+    else Future.successful(())
 
     maybeSaveSuccessTime.map(_ => state.copy(processedCount = processedCount))
   }
@@ -75,13 +77,13 @@ object UpdateDynamoLambda extends StrictLogging {
 
   def writeBatchesUntilTimeout(
     processedCount: Int,
-    groups: Iterator[List[(SupporterRatePlanItem, Int)]],
+    batches: List[List[(SupporterRatePlanItem, Int)]],
     timeOutCheck: TimeOutCheck,
     dynamoDBService: DynamoDBService,
     alarmService: AlarmService
   ): Int =
-    groups.foldLeft(processedCount) {
-      (processedCount, group) =>
+    batches.foldLeft(processedCount) {
+      (processedCount, batch) =>
         if (timeOutCheck.timeRemainingMillis < timeoutBufferInMillis) {
           logger.info(
             s"Aborting processing - time remaining: ${timeOutCheck.timeRemainingMillis / 1000} seconds, buffer: ${timeoutBufferInMillis / 1000} seconds"
@@ -89,12 +91,12 @@ object UpdateDynamoLambda extends StrictLogging {
           return processedCount
         }
         logger.info(
-          s"Continuing processing - time remaining: ${timeOutCheck.timeRemainingMillis / 1000} seconds, buffer: ${timeoutBufferInMillis / 1000} seconds"
+          s"Continuing processing with batch of ${batch.length} - time remaining: ${timeOutCheck.timeRemainingMillis / 1000} seconds, buffer: ${timeoutBufferInMillis / 1000} seconds"
         )
 
-        Await.result(writeBatch(group, dynamoDBService, alarmService), 120.seconds)
+        Await.result(writeBatch(batch, dynamoDBService, alarmService), 120.seconds)
 
-        val (_, highestProcessedIndex) = group.last
+        val (_, highestProcessedIndex) = batch.last
         val newProcessedCount = highestProcessedIndex + 1
         logger.info(s"$newProcessedCount items processed")
         newProcessedCount
@@ -104,18 +106,45 @@ object UpdateDynamoLambda extends StrictLogging {
     val futures = list.map {
       case (supporterRatePlanItem, index) =>
         logger.info(
-          s"Attempting to write item index $index - ${supporterRatePlanItem.productRatePlanName} " +
-            s"rate plan with term end date ${supporterRatePlanItem.termEndDate} to Dynamo")
+          s"Attempting to write item index $index - ${supporterRatePlanItem.productRatePlanName} to Dynamo - ${supporterRatePlanItem.asJson.noSpaces}")
+
         dynamoDBService
           .writeItem(supporterRatePlanItem)
-//          .recover {
-//            // let's alarm and keep going if one insert fails
-//            case error: Throwable =>
-//              logger.error(s"An error occurred trying to write item $supporterRatePlanItem, at index $index", error)
-//              alarmService.triggerDynamoWriteAlarm
-//          }
+      //          .recover {
+      //            // let's alarm and keep going if one insert fails
+      //            case error: Throwable =>
+      //              logger.error(s"An error occurred trying to write item $supporterRatePlanItem, at index $index", error)
+      //              alarmService.triggerDynamoWriteAlarm
+      //          }
 
     }
     Future.sequence(futures)
   }
+
+  def batchItemsWhichCanUpdateConcurrently(items: List[(SupporterRatePlanItem, Int)]): List[List[(SupporterRatePlanItem, Int)]] = {
+    // 'Batch' supporterRatePlanItems up into groups which we can update in Dynamo concurrently.
+    // For this to be safe we need to make sure that no group has more than one item for the same user in it because if it does
+    // then order of execution is important and we can't guarantee this with parallel executions.
+    // It actually would be ok to have multiple records for the same user as long as none of them were cancellations but this code
+    // is complex enough without adding that logic in and we will still get most of the benefit
+
+    def batchAlreadyHasAnItemForThisUser(batch: ListBuffer[(SupporterRatePlanItem, Int)], identityId: String) =
+      batch.exists {
+        case (item, _) => item.identityId == identityId
+      }
+
+    // Using mutable state for performance reasons, it is many times faster than the immutable version
+    val result = ListBuffer(ListBuffer.empty[(SupporterRatePlanItem, Int)])
+
+    for ((item, index) <- items) {
+      val currentBatch = result.last
+      if (batchAlreadyHasAnItemForThisUser(currentBatch, item.identityId) || currentBatch.length == maxBatchSize)
+        result += ListBuffer((item, index))
+      else
+        currentBatch += ((item, index))
+    }
+    result.map(_.toList).toList
+  }
+
+
 }
--- supporter-product-data/src/test/scala/com/gu/lambdas/UpdateDynamoSpec.scala
+++ supporter-product-data/src/test/scala/com/gu/lambdas/UpdateDynamoSpec.scala
@@ -1,11 +1,18 @@
 package com.gu.lambdas
 
 import com.gu.Fixtures
+import com.gu.lambdas.UpdateDynamoLambda.maxBatchSize
+import com.gu.model.Stage.PROD
 import com.gu.model.dynamo.SupporterRatePlanItem
+import com.gu.model.states.UpdateDynamoState
 import kantan.csv.ops.toCsvInputOps
 import kantan.csv.rfc
+import org.joda.time.DateTime
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
+
+import java.time.ZonedDateTime
+import scala.collection.mutable.ListBuffer
 
 class UpdateDynamoSpec extends AsyncFlatSpec with Matchers {
 
@@ -16,4 +23,35 @@ class UpdateDynamoSpec extends AsyncFlatSpec with Matchers {
 
     UpdateDynamoLambda.getUnprocessedItems(csvReader, 2).length shouldBe 8
   }
+
+  "batchItemsWhichCanUpdateConcurrently" should "return the correct items" in {
+    val results = Fixtures.loadQueryResults
+    val csvReader = results.asCsvReader[SupporterRatePlanItem](rfc.withHeader)
+    val items = csvReader.zipWithIndex.toList.collect { case (Right(item), index) => (item, index) }
+
+    val batchedItems = UpdateDynamoLambda.batchItemsWhichCanUpdateConcurrently(items)
+
+    // The test rows should batch up into 3 separate lists - there are two rows which are not
+    // suitable to run concurrently because they belong to the same user at index 6 & 7
+    // so the pattern is
+    // - items 0 - 4 can all run concurrently and so are allocated to the same batch (up to max batch size of 5)
+    // - items 5 & 6 are unrelated and so go in a batch
+    // - then we get to index 7 which cannot go into the same batch as 6 so a new batch is started with this and the final 2 items
+    batchedItems.map(_.map(_._2)) shouldBe List(List(0,1,2,3,4), List(5,6), List(7,8,9))
+  }
+
+
+  //  "Run the job" should "work" in {
+  //    UpdateDynamoLambda.writeToDynamo(
+  //      PROD, UpdateDynamoState(
+  //        "select-active-rate-plans-2021-03-22T17:29:31.039.csv",
+  //        1285440,
+  //        0,
+  //        ZonedDateTime.parse("2021-03-22T10:29:31.039-07:00[America/Los_Angeles]")
+  //      ), new TimeOutCheck {
+  //        override def timeRemainingMillis = Int.MaxValue
+  //      }).map( state =>
+  //      succeed
+  //    )
+  //  }
 }
