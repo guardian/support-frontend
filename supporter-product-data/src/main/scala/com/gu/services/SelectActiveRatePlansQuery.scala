3987f098ec58f0f53727c120e2ff7b1be0094ead
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-03-31 16:35:27 +0100

    Update comment

--- supporter-product-data/src/main/scala/com/gu/services/SelectActiveRatePlansQuery.scala
+++ supporter-product-data/src/main/scala/com/gu/services/SelectActiveRatePlansQuery.scala
@@ -22,33 +22,34 @@ object SelectActiveRatePlansQuery {
 
   /* -------- Notes on this query -----------
     * Select *
-    A number of fields in the select clause such as contract effective date and Subscription status are not actually
-    written to Dynamo, they are only included for debugging purposes. They could be removed at a later
-    stage if we felt they were no longer useful.
+    Subscription status is not actually written to Dynamo, this are only included for debugging purposes. It could be removed at a later
+    stage if we felt it was no longer useful.
 
     * From *
-    We select from rateplan because this gives us the best level of granularity to get all the information
-    we need with the minimum amount of records returned. We could have used RatePlanCharge instead but this
-    would have returned multiple records for multi-day print subs or print + digital subs
+    We select from rateplan because this gives us the best level of granularity to get all the information we need with the minimum amount
+    of records returned. We could have used RatePlanCharge instead but this would have returned multiple records for multi-day print subs
+    or print + digital subs
 
     * Where *
-    - Subscription.Status -    We are only interested in subs which are active or cancelled, cancelled subs will update the previous active version with the
-                               new term end date.
+    - Subscription.Status -    We are only interested in subs which are active or cancelled, cancelled subs will update the previous active
+                               version with the new term end date.
 
-    - RatePlan.AmendmentType - When removing a product from a subscription with an amendment a new rate plan is created with amendment type 'RemoveProduct'
-                               we are not interested in these rate plans in this system.
+    - RatePlan.AmendmentType - When removing a product from a subscription with an amendment a new rate plan is created with amendment type
+                              'RemoveProduct' we are not interested in these rate plans in this system.
 
-    - excludeDiscountProductRatePlans - There are a lot of rate plans in PROD which are only there to provide various discounts - these are irrelevant to this job
+    - excludeDiscountProductRatePlans - There are a lot of rate plans in PROD which are only there to provide various discounts - these are
+                                        irrelevant to this job
 
-    - identityId like '_%' - This checks that the account this rate plan is attached to has an associated identityId - since the purpose of this data is to
-                             provide product information in members-data-api which is keyed off identityId, we're not interested in any accounts which don't have one
+    - identityId like '_%' - This checks that the account this rate plan is attached to has an associated identityId - since the purpose of
+                             this data is to provide product information in members-data-api which is keyed off identityId, we're not
+                             interested in any accounts which don't have one
 
-    - is not DS gift or is redeemed DS gift - Until digital subscription gifts are redeemed they should not show up in members-data-api and when they do they
-                                              should show up against the giftee's identity id not the purchasers
+    - is not DS gift or is redeemed DS gift - Until digital subscription gifts are redeemed they should not show up in members-data-api and
+                                              when they do they should show up against the giftee's identity id not the purchasers
 
    * Order By *
-   It's important that rate plans for the same user id are ordered in ASC order by contract effective date so that if a customer cancels a product and then
-   resubscribes at a later date we record that correctly
+   Rate plans for the same user id are ordered in ASC order by subscription name and then subscription version
+   this makes sure that cancellations are written after the active version of the sub in case we have both (we shouldn't but you never know!)
    */
 
   def query(discountProductRatePlanIds: List[String]): String =
