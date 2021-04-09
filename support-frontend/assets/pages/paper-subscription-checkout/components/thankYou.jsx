a5051371c0eeec0062695e82f3b51ae50ae4f51b
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2021-04-09 18:45:53 +0100

    Fix thank you text for subs card

--- support-frontend/assets/pages/paper-subscription-checkout/components/thankYou.jsx
+++ support-frontend/assets/pages/paper-subscription-checkout/components/thankYou.jsx
@@ -99,10 +99,12 @@ const whatNextText: { [FulfilmentOptions]: Array<string> } = {
     'Your newspaper will be delivered to your door.',
   ],
   [Collection]: [
-    `Look out for an email from us confirming your subscription.
-          It has everything you need to know about how to manage it in the future.`,
-    'You will receive your personalised book of vouchers.',
-    'Exchange your voucher for a newspaper at your newsagent or wherever you buy your paper',
+    `Keep an eye on your inbox. You should receive an email confirming the details of your subscription,
+      and another email shortly afterwards that contains details of how you can pick up your newspapers from tomorrow.`,
+    `You will receive your Subscription Card in your subscriber pack in the post, along with your home
+      delivery letter.`,
+    `Visit your chosen participating newsagent to pick up your newspaper using your Subscription Card, or
+      arrange a home delivery using your delivery letter.`,
   ],
 };
 
