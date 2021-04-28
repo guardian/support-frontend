31c930dec3da7617034dd38846e27e272fc3ec0d
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-04-28 10:43:13 +0100

    Tidy up

--- support-frontend/assets/helpers/subscriptionsForms/countryPaymentMethods.js
+++ support-frontend/assets/helpers/subscriptionsForms/countryPaymentMethods.js
@@ -4,7 +4,7 @@ import type { PaymentMethod } from 'helpers/paymentMethods';
 import { DirectDebit, PayPal, Stripe } from 'helpers/paymentMethods';
 import type { IsoCurrency } from 'helpers/internationalisation/currency';
 
-function supportedPaymentMethods(currencyId: IsoCurrency) {
+function supportedPaymentMethods(currencyId: IsoCurrency): PaymentMethod[] {
   const countrySpecific: PaymentMethod[] = currencyId === 'GBP' ? [DirectDebit, Stripe, PayPal] : [Stripe, PayPal];
 
   return countrySpecific;
