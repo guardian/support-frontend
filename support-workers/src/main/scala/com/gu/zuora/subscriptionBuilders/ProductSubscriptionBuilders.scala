c86cd0e9f4644a0b80651d2870ec86e095d047fb
John Duffell <john.duffell@guardian.co.uk>
2021-05-13 17:47:17 +0100

    clean up unused imports

--- support-workers/src/main/scala/com/gu/zuora/subscriptionBuilders/ProductSubscriptionBuilders.scala
+++ support-workers/src/main/scala/com/gu/zuora/subscriptionBuilders/ProductSubscriptionBuilders.scala
@@ -1,17 +1,11 @@
 package com.gu.zuora.subscriptionBuilders
 
-import java.util.UUID
 import com.gu.i18n.Country
 import com.gu.support.catalog
 import com.gu.support.catalog.{ProductRatePlan, ProductRatePlanId}
-import com.gu.support.config.ZuoraContributionConfig
 import com.gu.support.promotions.{PromoCode, PromoError, PromotionService}
-import com.gu.support.workers._
 import com.gu.support.workers.exceptions.CatalogDataNotFoundException
-import com.gu.support.workers.states.CreateZuoraSubscriptionProductState.ContributionState
-import com.gu.support.zuora.api.ReaderType.Direct
 import com.gu.support.zuora.api._
-import org.joda.time.{DateTimeZone, LocalDate}
 
 object ProductSubscriptionBuilders {
 
