e26406c4b313205e5d0e77fcc0b26c1d1fa2cc71
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2021-06-16 10:56:06 +0100

    Remove commented-out line from stories

--- support-frontend/stories/events.jsx
+++ support-frontend/stories/events.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import EventsModule from 'pages/digital-subscription-landing/components/events/eventsModule';
-// import FeedbackWidget from 'pages/digital-subscription-landing/components/feedbackWidget/feedbackWidget';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withCenterAlignment } from '../.storybook/decorators/withCenterAlignment';
 import CentredContainer from 'components/containers/centredContainer';
