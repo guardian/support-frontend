0d7a370d15cca8e722bd71e357ec9fe4045896a9
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2021-06-16 10:54:31 +0100

    Update events module to use existing block label component

--- support-frontend/assets/pages/digital-subscription-landing/components/events/eventsModule.jsx
+++ support-frontend/assets/pages/digital-subscription-landing/components/events/eventsModule.jsx
@@ -4,12 +4,13 @@ import React from 'react';
 import { css } from '@emotion/core';
 import { from } from '@guardian/src-foundations/mq';
 import { space } from '@guardian/src-foundations';
-import { background, text, border } from '@guardian/src-foundations/palette';
+import { background, border } from '@guardian/src-foundations/palette';
 import { headline, body } from '@guardian/src-foundations/typography';
 import { SvgTicket } from './icons/ticket';
 import EventCard from './eventCard';
 import { ennyImage, emmaJohnImage } from './eventsImages';
 import { eventsOfferPink, eventsOfferRed } from 'stylesheets/emotion/colours';
+import BlockLabel from 'components/blockLabel/blockLabel';
 
 const container = css`
   box-sizing: border-box;
@@ -18,23 +19,16 @@ const container = css`
 `;
 
 const label = css`
-  display: inline;
-  background-color: ${background.inverse};
-  color: ${text.ctaPrimary};
-  padding: ${space[1]}px ${space[2]}px;
-  ${headline.xxxsmall({ fontWeight: 'bold' })};
-  top: -27px;
   position: absolute;
   left: 0;
+  top: -27px;
 
   ${from.tablet} {
-    ${headline.xxsmall({ fontWeight: 'bold' })};
-    top: -30px;
+    top: -31px;
   }
 
   ${from.desktop} {
-    ${headline.xsmall({ fontWeight: 'bold' })};
-    top: -34px;
+    top: -35px;
   }
 `;
 
@@ -146,41 +140,43 @@ const eventCardContainer = css`
 `;
 
 const EventsModule = () => (
-  <div css={container}>
-    <h2 css={label}>Special offer</h2>
-    <div css={contentContainer}>
-      <div css={textContentContainer}>
-        <div css={icon}><SvgTicket /></div>
-        <h3 css={cardTitle} aria-label="Offer for new Guardian subscribers: ">Enjoy 6 free tickets to digital Guardian events</h3>
-        <p css={para}>In the <strong>first 3 months</strong> of your subscription</p>
-        <p css={paraSecond}>
+  <>
+    <BlockLabel tag="h2" cssOverrides={label}>Special offer</BlockLabel>
+    <div css={container}>
+      <div css={contentContainer}>
+        <div css={textContentContainer}>
+          <div css={icon}><SvgTicket /></div>
+          <h3 css={cardTitle} aria-label="Offer for new Guardian subscribers: ">Enjoy 6 free tickets to digital Guardian events</h3>
+          <p css={para}>In the <strong>first 3 months</strong> of your subscription</p>
+          <p css={paraSecond}>
         Join interactive Live conversations with journalists, political leaders and cultural
         icons. Or get inspired to learn a new skill in selected Masterclasses. With events
         launching weekly and available on-demand.
-        </p>
-      </div>
-      <div css={eventCardContainer}>
-        <EventCard
-          eventType="Featured event"
-          eventImage={ennyImage}
-          eventDate="27 July 2021"
-          eventTime="8-9pm BST"
-          eventColour={eventsOfferPink}
-          eventSectionText="Culture / "
-          eventDescription="In conversation with UK rapper Enny"
-        />
-        <EventCard
-          eventType="Featured masterclass"
-          eventImage={emmaJohnImage}
-          eventDate="22 July 2021"
-          eventTime="6-8pm BST"
-          eventColour={eventsOfferRed}
-          eventSectionText="Sport / "
-          eventDescription="How to use sports psychology to improve your life"
-        />
+          </p>
+        </div>
+        <div css={eventCardContainer}>
+          <EventCard
+            eventType="Featured event"
+            eventImage={ennyImage}
+            eventDate="27 July 2021"
+            eventTime="8-9pm BST"
+            eventColour={eventsOfferPink}
+            eventSectionText="Culture / "
+            eventDescription="In conversation with UK rapper Enny"
+          />
+          <EventCard
+            eventType="Featured masterclass"
+            eventImage={emmaJohnImage}
+            eventDate="22 July 2021"
+            eventTime="6-8pm BST"
+            eventColour={eventsOfferRed}
+            eventSectionText="Sport / "
+            eventDescription="How to use sports psychology to improve your life"
+          />
+        </div>
       </div>
     </div>
-  </div>
+  </>
 );
 
 
