894ab29768d7ffe2e094fc58874abab9de303d71
John Duffell <john.duffell@guardian.co.uk>
2021-06-17 12:36:20 +0100

    update support-frontend to play 2.8

--- project/LibraryVersions.scala
+++ project/LibraryVersions.scala
@@ -1,13 +1,13 @@
 
 object LibraryVersions {
-  val circeVersion = "0.12.1"
+  val circeVersion = "0.14.1"
   val awsClientVersion = "1.11.827"
   val catsVersion = "2.1.1"
   val jacksonVersion = "2.10.5"
   val jacksonDatabindVersion = "2.10.5.1"
   val okhttpVersion = "3.10.0"
   val scalaUriVersion = "2.2.2"
-  val playCirceVersion = "2712.0"
+  val playCirceVersion = "2814.2"
   val AWSJavaSDKVersion = "1.11.568"
   val stripeVersion = "10.12.0" // Supports API version 2019-05-16
 }
