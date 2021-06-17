c893ed5cb750f5fd58201467df0a3109f0a78485
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-06-17 11:00:18 +0100

    Update Weekly promotions T&Cs

--- support-frontend/assets/pages/promotion-terms/weeklyTerms.jsx
+++ support-frontend/assets/pages/promotion-terms/weeklyTerms.jsx
@@ -10,7 +10,7 @@ import {
 } from 'helpers/internationalisation/countryGroup';
 import { Domestic, RestOfWorld } from 'helpers/productPrice/fulfilmentOptions';
 import { NoProductOptions } from 'helpers/productPrice/productOptions';
-import { Annual, Quarterly } from 'helpers/productPrice/billingPeriods';
+import { Annual, postIntroductorySixForSixBillingPeriod, Quarterly } from 'helpers/productPrice/billingPeriods';
 import { extendedGlyph } from 'helpers/internationalisation/currency';
 import type { CountryGroupPrices } from 'helpers/productPrice/productPrices';
 import { showPrice } from 'helpers/productPrice/productPrices';
@@ -18,14 +18,18 @@ import { showPrice } from 'helpers/productPrice/productPrices';
 type NameAndSaving = {name: CountryGroupName, saving: ?string};
 
 const orderedCountryGroupNames: NameAndSaving[] = [
-  { name: 'United Kingdom', saving: '35%' },
-  { name: 'United States', saving: '31%' },
-  { name: 'Australia', saving: '31%' },
-  { name: 'Europe', saving: '26%' },
-  { name: 'New Zealand', saving: '20%' },
-  { name: 'Canada', saving: '16%' },
+  { name: 'United Kingdom', saving: '34%' },
+  { name: 'United States', saving: '34%' },
+  { name: 'Australia', saving: '30%' },
+  { name: 'Europe', saving: '30%' },
+  { name: 'New Zealand', saving: '19%' },
+  { name: 'Canada', saving: '30%' },
   { name: 'International', saving: null },
 ];
+
+const shortTermDescription = postIntroductorySixForSixBillingPeriod  === Quarterly ?
+  'Quarterly (13 weeks)' :
+  'Monthly (4 weeks)';
 
 export default function WeeklyTerms(props: PromotionTermsPropTypes) {
 
@@ -42,11 +46,11 @@ function getCountryPrice(countryGroupName: CountryGroupName, prices: CountryGrou
   const { currency } = fromCountryGroupName(countryGroupName);
   const currencyGlyph = extendedGlyph(currency);
   const fulfilmentOption = countryGroupName === International ? RestOfWorld : Domestic;
-  const quarterlyPrice = prices[fulfilmentOption][NoProductOptions][Quarterly][currency];
+  const shortTermPrice = prices[fulfilmentOption][NoProductOptions][postIntroductorySixForSixBillingPeriod][currency];
   const annualPrice = prices[fulfilmentOption][NoProductOptions][Annual][currency];
   return {
     currencyGlyph,
-    quarterlyPrice,
+    shortTermPrice,
     annualPrice,
   };
 }
@@ -59,23 +63,25 @@ function getSaving(saving: ?string) {
 }
 
 function StandardCountryPrice(nameAndSaving: NameAndSaving, prices: CountryGroupPrices) {
-  const { quarterlyPrice, annualPrice } = getCountryPrice(nameAndSaving.name, prices);
+  const { shortTermPrice, annualPrice } = getCountryPrice(nameAndSaving.name, prices);
+
   return (
     <SansParagraph>
       <strong>{nameAndSaving.name}:</strong>{' '}
-      Quarterly (13 weeks) subscription rate {showPrice(quarterlyPrice)},
+      {shortTermDescription} subscription rate {showPrice(shortTermPrice)},
       or annual rate {showPrice(annualPrice)}{getSaving(nameAndSaving.saving)}
     </SansParagraph>
   );
 }
 
 function SixForSixCountryPrice(nameAndSaving: NameAndSaving, prices: CountryGroupPrices) {
-  const { currencyGlyph, quarterlyPrice } = getCountryPrice(nameAndSaving.name, prices);
+  const { currencyGlyph, shortTermPrice } = getCountryPrice(nameAndSaving.name, prices);
+
   return (
     <SansParagraph>
       <strong>{nameAndSaving.name}:</strong>{' '}
-      Offer is {currencyGlyph}6 for the first 6 issues followed by quarterly (13 weeks)
-      subscription payments of {showPrice(quarterlyPrice)} thereafter{getSaving(nameAndSaving.saving)}
+      Offer is {currencyGlyph}6 for the first 6 issues followed by {shortTermDescription.toLowerCase()}
+      subscription payments of {showPrice(shortTermPrice)} thereafter{getSaving(nameAndSaving.saving)}
     </SansParagraph>
   );
 }
