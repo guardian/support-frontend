56f5a3fa3a62cd737cd5791ae10da77825015d8b
Imogen Hardy <imogen.hardy@guardian.co.uk>
2021-06-18 15:04:58 +0100

    update to @axe-core/react to fix react-axe license issue

--- support-frontend/assets/helpers/rendering/render.js
+++ support-frontend/assets/helpers/rendering/render.js
@@ -52,7 +52,7 @@ const renderPage = (content: Object, id: string, callBack?: () => void) => {
     delete element.dataset.notHydrated;
     try {
       if (process.env.NODE_ENV === 'DEV' && !isSafari) {
-        import('react-axe').then((axe) => {
+        import('@axe-core/react').then((axe) => {
           console.log('Loading react-axe for accessibility analysis');
           axe.default(React, ReactDOM, 1000);
           ReactDOM.render(content, element, callBack);
