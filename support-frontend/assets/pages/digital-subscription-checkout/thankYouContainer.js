27ac6455ca05294b0ac7d15d5fc9dd7ae56aa2d9
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2021-06-18 13:08:25 +0100

    Remove participations from thankyoucontainer as it's not used yet

--- support-frontend/assets/pages/digital-subscription-checkout/thankYouContainer.js
+++ support-frontend/assets/pages/digital-subscription-checkout/thankYouContainer.js
@@ -7,5 +7,4 @@ import ThankYouContent from 'pages/digital-subscription-checkout/thankYouContent
 export default connect(state => ({
   ...getFormFields(state),
   includePaymentCopy: true,
-  participations: state.common.abParticipations,
 }))(ThankYouContent);
