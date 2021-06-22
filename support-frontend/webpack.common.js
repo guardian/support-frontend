f94742bf7e000e92b09208ceea6072c5446ec22f
Imogen Hardy <imogen.hardy@guardian.co.uk>
2021-06-22 16:31:26 +0100

    add strictModuleErrorHandling check to prod builds to prevent this in future

--- support-frontend/webpack.common.js
+++ support-frontend/webpack.common.js
@@ -95,6 +95,7 @@ module.exports = (cssFilename, jsFilename, minimizeCss) => ({
     chunkFilename: `webpack/${jsFilename}`,
     filename: `javascripts/${jsFilename}`,
     publicPath: '/assets/',
+    strictModuleErrorHandling: process.env.NODE_ENV === 'production',
   },
 
   resolve: {
