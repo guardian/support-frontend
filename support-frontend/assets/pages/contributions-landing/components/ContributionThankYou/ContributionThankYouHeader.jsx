727d492bfb60b6f401799fbe0bda0e308ca62f79
Tom Forbes <tom.forbes@theguardian.com>
2021-06-28 15:42:55 +0100

    update copy

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYouHeader.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYouHeader.jsx
@@ -124,18 +124,31 @@ const ContributionThankYouHeader = ({
     }
   };
 
-  const AdditionalCopy = () => (
-    <>
-      { shouldShowLargeDonationMessage ? 'It’s not every day that we receive such a generous contribution – thank you. We would love to stay in touch. So that we can, please pick the add-ons that suit you best. '
-        : 'To support us further, and enhance your experience with the Guardian, select the add-ons that suit you best. '}
-      {contributionType !== 'ONE_OFF' &&
-        <span>
-            As you’re now a valued supporter, we’ll be in touch to bring you closer to our journalism. You can amend your email preferences at any time via <a
-          href="https://manage.theguardian.com">your account</a>.
-        </span>
-      }
-    </>
-  );
+  const AdditionalCopy = () => {
+    const mainText = shouldShowLargeDonationMessage ? 'It’s not every day that we receive such a generous contribution – thank you. We would love to stay in touch. So that we can, please pick the add-ons that suit you best. '
+      : 'To support us further, and enhance your experience with the Guardian, select the add-ons that suit you best. ';
+
+    const MarketingCopy = () => (
+      <span>
+        { shouldShowLargeDonationMessage ?
+          'We’ll be in touch to bring you closer to our journalism. Please select the extra add-ons that suit you best. ' :
+          'As you’re now a valued supporter, we’ll be in touch to bring you closer to our journalism. '
+        }
+        You can amend your email preferences at any time via <a
+          href="https://manage.theguardian.com"
+        >your account
+        </a>.
+      </span>
+    );
+    return (
+      <>
+        {mainText}
+        { contributionType !== 'ONE_OFF' &&
+          <MarketingCopy />
+        }
+      </>
+    );
+  };
 
   return (
     <header css={header}>
