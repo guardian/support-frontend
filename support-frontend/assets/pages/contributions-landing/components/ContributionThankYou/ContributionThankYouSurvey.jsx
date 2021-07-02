b72b14ec5152b174db150e2c0bd2d4f0e4e9bd26
Tom Forbes <tom.forbes@theguardian.com>
2021-07-02 14:16:58 +0100

    Aus survey (#3155)
    * Aus survey
    
    * better
    
    * fix build

--- support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYouSurvey.jsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionThankYou/ContributionThankYouSurvey.jsx
@@ -1,3 +1,5 @@
+// @flow
+// $FlowIgnore - required for hooks
 import React, { useState, useEffect } from 'react';
 import { css } from '@emotion/core';
 import { space } from '@guardian/src-foundations';
@@ -10,19 +12,25 @@ import SvgSpeechBubbleWithPlus from './components/SvgSpeechBubbleWithPlus';
 import styles from './styles';
 import { OPHAN_COMPONENT_ID_SURVEY } from './utils/ophan';
 import { trackComponentClick, trackComponentLoad } from 'helpers/tracking/behaviour';
+import type { IsoCountry } from 'helpers/internationalisation/country';
 
 const buttonContainer = css`
   margin-top: ${space[6]}px;
 `;
 
 const SURVEY_LINK = 'https://www.surveymonkey.co.uk/r/KY7XRR2';
+const AUS_SURVEY_LINK = 'https://guardiannewsampampmedia.formstack.com/forms/australia_2021';
 
-const ContributionThankYouSurvey = () => {
+const ContributionThankYouSurvey = ({ countryId }: { countryId: IsoCountry }) => {
   const [hasBeenCompleted, setHasBeenCompleted] = useState(false);
 
   useEffect(() => {
     trackComponentLoad(OPHAN_COMPONENT_ID_SURVEY);
   }, []);
+
+  const isAus = countryId === 'AU';
+
+  const heading = isAus ? 'Tell us why you value Guardian Australia' : 'Send us your thoughts';
 
   const actionIcon = <SvgSpeechBubbleWithPlus />;
   const actionHeader = (
@@ -30,7 +38,7 @@ const ContributionThankYouSurvey = () => {
       title={
         hasBeenCompleted
           ? 'Thank you for sharing your thoughts'
-          : 'Send us your thoughts'
+          : heading
       }
     />
   );
@@ -39,6 +47,8 @@ const ContributionThankYouSurvey = () => {
     trackComponentClick(OPHAN_COMPONENT_ID_SURVEY);
     setHasBeenCompleted(true);
   };
+
+  const url = isAus ? AUS_SURVEY_LINK : SURVEY_LINK;
 
   const actionBody = (
     <ActionBody>
@@ -49,20 +59,30 @@ const ContributionThankYouSurvey = () => {
       ) : (
         <>
           <p>
-            <span css={styles.hideAfterTablet}>
-              Fill out this short form to tell us more about your experience of
-              supporting us today – it only takes a minute.
-            </span>
-            <span css={styles.hideBeforeTablet}>
-              We would love to hear more about your experience of supporting the
-              Guardian today. Please fill out this short form – it only takes a
-              minute.
-            </span>
+            { isAus &&
+              <span>
+                We would love to know more about your decision to support our journalism today.
+                We’ll publish a selection of our favourite messages, so other readers can enjoy them too.
+              </span>
+            }
+            { !isAus &&
+              <>
+                <span css={styles.hideAfterTablet}>
+                  Fill out this short form to tell us more about your experience of
+                  supporting us today – it only takes a minute.
+                </span>
+                <span css={styles.hideBeforeTablet}>
+                We would love to hear more about your experience of supporting the
+                Guardian today. Please fill out this short form – it only takes a
+                minute.
+                </span>
+              </>
+            }
           </p>
           <div css={buttonContainer}>
             <LinkButton
               onClick={onClick}
-              href={SURVEY_LINK}
+              href={url}
               target="_blank"
               rel="noopener noreferrer"
               priority="primary"
