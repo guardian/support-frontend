0b41eb28705e9cdaf4093d9da1b891a21d07f203
Maria <m.olanipekun@hotmail.com>
2021-08-03 15:03:15 +0100

    turn off comparison table A/B test

--- support-frontend/assets/helpers/abTests/abtestDefinitions.js
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.js
@@ -102,7 +102,7 @@ export const tests: Tests = {
         size: 1,
       },
     },
-    isActive: true,
+    isActive: false,
     referrerControlled: false,
     targetPage: pageUrlRegexes.subscriptions.digiSub.nonGiftLandingAndCheckout,
     seed: 10,
