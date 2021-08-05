29f909c2aa9bf88e34388fb9ce0ce361355d5b9a
Ara_cho <47318984+Aracho1@users.noreply.github.com>
2021-08-05 15:49:11 +0100

    update test

---
+++ support-lambdas/acquisition-events-api/src/test/scala/com/gu/acquisitionEventsApi/LambdaSpec.scala
@@ -0,0 +1,7 @@
+package com.gu.acquisitionEventsApi
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.flatspec.AnyFlatSpec
+
+class LambdaSpec extends AnyFlatSpec with Matchers {
+}
