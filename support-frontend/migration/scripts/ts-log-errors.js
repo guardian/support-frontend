569f4a026f02fad6be584f10fd86e6cf23ec3072
Imogen Hardy <imogen.hardy@guardian.co.uk>
2021-08-18 16:35:26 +0100

    fix formatting issue

--- support-frontend/migration/scripts/ts-log-errors.js
+++ support-frontend/migration/scripts/ts-log-errors.js
@@ -7,41 +7,39 @@ const errorFileName = 'typescript-errors.json';
 console.log(`Executing ${tscCmd}.`);
 
 exec(tscCmd, (error, stdout) => {
-	if (error && error.code && error.code > 2) {
-		console.error(`exec error: ${error}`);
-		return;
-	}
+  if (error && error.code && error.code > 2) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
 
-	const lines = stdout.split(/(\r?\n)/g);
+  const lines = stdout.split(/(\r?\n)/g);
 
-	const typescriptErrors = lines.reduce((accumulator, line) => {
-		const capturedGroups =
-			/^(?<errorPath>[^(]*(\.tsx|\.ts)).*?(?<errorCode>TS\d+):\s(?<errorMessage>.*)$/gm.exec(
-				line,
-			);
+  const typescriptErrors = lines.reduce((accumulator, line) => {
+    const capturedGroups = /^(?<errorPath>[^(]*(\.tsx|\.ts)).*?(?<errorCode>TS\d+):\s(?<errorMessage>.*)$/gm.exec(line);
 
-		if (capturedGroups && capturedGroups.groups) {
-			const { errorPath, errorCode, errorMessage } = capturedGroups.groups;
+    if (capturedGroups && capturedGroups.groups) {
+      const { errorPath, errorCode, errorMessage } = capturedGroups.groups;
 
-			const errorInstance = {
-				path: errorPath,
-				message: errorMessage,
-			};
+      const errorInstance = {
+        path: errorPath,
+        message: errorMessage,
+      };
 
-			const errorInstances = accumulator[errorCode]
-				? [...accumulator[errorCode], errorInstance]
-				: [errorInstance];
+      const errorInstances = accumulator[errorCode]
+        ? [...accumulator[errorCode], errorInstance]
+        : [errorInstance];
 
-			return {
-				...accumulator,
-				[errorCode]: errorInstances,
-			};
-		}
+      return {
+        ...accumulator,
+        [errorCode]: errorInstances,
+      };
+    }
 
-		return accumulator;
-	}, {});
+    return accumulator;
+  }, {});
 
   const errorCounts = Object.entries(typescriptErrors).reduce((counts, [errorCode, errorList]) => {
+    // eslint-disable-next-line no-param-reassign
     counts[errorCode] = errorList.length;
     return counts;
   }, {});
@@ -49,58 +47,50 @@ exec(tscCmd, (error, stdout) => {
   const fileContents = {
     errorCounts,
     ...typescriptErrors,
-  }
+  };
 
-	fs.writeFileSync(errorFileName, JSON.stringify(fileContents, null, 4));
+  fs.writeFileSync(errorFileName, JSON.stringify(fileContents, null, 4));
 
-	let totalErrorCount = Object.keys(typescriptErrors).reduce(
-		(accumulator, errorCode) => {
-			return accumulator + typescriptErrors[errorCode].length;
-		},
-		0,
-	);
+  const totalErrorCount = Object.keys(typescriptErrors).reduce(
+    (accumulator, errorCode) => accumulator + typescriptErrors[errorCode].length,
+    0,
+  );
 
-	console.log(
-		`${totalErrorCount} Typescript errors have been logged and saved in ${errorFileName}.`,
-	);
+  console.log(`${totalErrorCount} Typescript errors have been logged and saved in ${errorFileName}.`);
 
-	const topErrors = Object.keys(typescriptErrors)
-		.filter((errorCode) => typescriptErrors[errorCode].length >= 50)
-		.map((errorCode) => ({
-			errorCode,
-			count: typescriptErrors[errorCode].length,
-		}))
-		.sort((err1, err2) => {
-			if (err1.count < err2.count) {
-				return 1;
-			}
+  const topErrors = Object.keys(typescriptErrors)
+    .filter(errorCode => typescriptErrors[errorCode].length >= 50)
+    .map(errorCode => ({
+      errorCode,
+      count: typescriptErrors[errorCode].length,
+    }))
+    .sort((err1, err2) => {
+      if (err1.count < err2.count) {
+        return 1;
+      }
 
-			if (err1.count > err2.count) {
-				return -1;
-			}
+      if (err1.count > err2.count) {
+        return -1;
+      }
 
-			// count is equal
-			return 0;
-		});
+      // count is equal
+      return 0;
+    });
 
-	console.log('Errors with over 50 instances:');
+  console.log('Errors with over 50 instances:');
 
-	topErrors.forEach((error) => {
-		console.log(`${error.errorCode} (count: ${error.count})`);
-	});
+  topErrors.forEach((err) => {
+    console.log(`${err.errorCode} (count: ${err.count})`);
+  });
 
-	const filesWithErrors = Object.keys(typescriptErrors)
-		.map((errorCode) => {
-			return typescriptErrors[errorCode].map(
-				(errorInstance) => errorInstance.path,
-			);
-		})
-		.flat()
-		.map((filePath) => `./${filePath}`);
+  const filesWithErrors = Object.keys(typescriptErrors)
+    .map(errorCode => typescriptErrors[errorCode].map(errorInstance => errorInstance.path))
+    .flat()
+    .map(filePath => `./${filePath}`);
 
-	const filesWithErrorsMinusDuplicates = [...new Set(filesWithErrors)];
+  const filesWithErrorsMinusDuplicates = [...new Set(filesWithErrors)];
 
-	filesWithErrorsMinusDuplicates.forEach((filePath) => {
-		console.log(`"${filePath}",`);
-	});
+  filesWithErrorsMinusDuplicates.forEach((filePath) => {
+    console.log(`"${filePath}",`);
+  });
 });
