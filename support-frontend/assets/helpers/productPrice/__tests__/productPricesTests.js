956146a8bc65f0dbb8ec3fbd2a38063885e6c083
Maria Olanipekun <m.olanipekun@hotmail.com>
2021-08-24 17:58:02 +0100

    add getProductPrice tests for productPricesTests

--- support-frontend/assets/helpers/productPrice/__tests__/productPricesTests.js
+++ support-frontend/assets/helpers/productPrice/__tests__/productPricesTests.js
@@ -1,7 +1,18 @@
 // @flow
 
 // ----- Imports ----- //
-import { isNumeric } from 'helpers/productPrice/productPrices';
+import
+{
+  getProductPrice,
+  // getFirstValidPrice,
+  // finalPrice,
+  // getCurrency,
+  // getCountryGroup,
+  // showPrice,
+  // displayPrice,
+  isNumeric,
+} from 'helpers/productPrice/productPrices';
+
 
 // ----- Tests ----- //
 
@@ -16,5 +27,66 @@ describe('isNumeric', () => {
     expect(isNumeric(0)).toEqual(true);
     expect(isNumeric(50)).toEqual(true);
 
+  });
+});
+
+describe('getProductPrice', () => {
+  const productPrices = {
+    'United Kingdom': {
+      Collection: {
+        Weekend: {
+          Monthly: {
+            GBP: {
+              price: 20.76, savingVsRetail: 20, currency: 'GBP', fixedTerm: false, promotions: [],
+            },
+          },
+        },
+        NoProductOptions: {
+          Monthly: {
+            GBP: {
+              price: 11.95, savingVsRetail: 5, currency: 'GBP', fixedTerm: false, promotions: [],
+            },
+          },
+        },
+      },
+      NoFulfilmentOptions: {
+        NoProductOptions: {
+          Monthly: {
+            GBP: {
+              price: 15.95, savingVsRetail: 10, currency: 'GBP', fixedTerm: false, promotions: [],
+            },
+          },
+        },
+      },
+    },
+  };
+
+  it('should return product price information for a given currency', () => {
+    expect(getProductPrice(productPrices, 'United Kingdom', 'Monthly', 'Collection', 'Weekend'))
+      .toEqual({
+        currency: 'GBP',
+        fixedTerm: false,
+        price: 20.76,
+        promotions: [],
+        savingVsRetail: 20,
+      });
+
+    expect(getProductPrice(productPrices, 'United Kingdom', 'Monthly', 'Collection'))
+      .toEqual({
+        currency: 'GBP',
+        fixedTerm: false,
+        price: 11.95,
+        promotions: [],
+        savingVsRetail: 5,
+      });
+
+    expect(getProductPrice(productPrices, 'United Kingdom', 'Monthly'))
+      .toEqual({
+        currency: 'GBP',
+        fixedTerm: false,
+        price: 15.95,
+        promotions: [],
+        savingVsRetail: 10,
+      });
   });
 });
