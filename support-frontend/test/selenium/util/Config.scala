51e1ce30156074b8f76f745b2dec68d54c1f9489
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-08-27 14:57:57 +0100

    Debugging TC

--- support-frontend/test/selenium/util/Config.scala
+++ support-frontend/test/selenium/util/Config.scala
@@ -35,6 +35,7 @@ object Config {
     logger.info("=============================")
     logger.info(s"Stage: ${conf.getString("stage")}")
     logger.info(s"Support Frontend: ${supportFrontendUrl}")
+    logger.info(s"Client secret: *****${idapiClientAccessTokenSecret.substring(5)}")
   }
 
 }
