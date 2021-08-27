bf7f32bd4d9d55fdf2d1d22f5fcaf37ec0ffd360
Rupert Bates <Rupert.Bates@guardian.co.uk>
2021-08-27 14:41:57 +0100

    Debug TC

--- support-frontend/test/selenium/subscriptions/CheckoutsSpec.scala
+++ support-frontend/test/selenium/subscriptions/CheckoutsSpec.scala
@@ -65,6 +65,7 @@ class CheckoutsSpec extends AnyFeatureSpec
 
   def testCheckout(checkoutName: String, checkoutPage: CheckoutPage, productPage: ProductPage, paymentFunction: CheckoutPage => Unit): Unit = {
     val testUserRequest = new IdapiTestUserRequest()
+    logger.info(s"Identity access token = ${Config.idapiClientAccessTokenName}")
     val testUser = testUserRequest.getCookies match {
       case Left(error) => fail(error)
       case Right(cookies) => new PostDeployTestUser(driverConfig, Some(cookies))
