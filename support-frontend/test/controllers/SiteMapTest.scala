a03881d21c08fbdfe6d4446b0ca9a82e92cf6ddd
John Duffell <john.duffell@guardian.co.uk>
2021-09-21 17:51:02 +0100

    add stage param to tests

--- support-frontend/test/controllers/SiteMapTest.scala
+++ support-frontend/test/controllers/SiteMapTest.scala
@@ -5,6 +5,7 @@ import actions.CustomActionBuilders
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{status, stubControllerComponents}
 import akka.util.Timeout
+import com.gu.support.config.Stages
 import org.scalatestplus.mockito.MockitoSugar.mock
 import config.Configuration.IdentityUrl
 import services.AsyncAuthenticationService
@@ -12,13 +13,13 @@ import services.AsyncAuthenticationService
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.must.Matchers
 
 class SiteMapTest extends AnyWordSpec with Matchers with TestCSRFComponents {
 
   implicit val timeout = Timeout(2.seconds)
+  val stage = Stages.DEV
 
   val actionRefiner = new CustomActionBuilders(
     asyncAuthenticationService = mock[AsyncAuthenticationService],
@@ -27,7 +28,8 @@ class SiteMapTest extends AnyWordSpec with Matchers with TestCSRFComponents {
     cc = stubControllerComponents(),
     addToken = csrfAddToken,
     checkToken = csrfCheck,
-    csrfConfig = csrfConfig
+    csrfConfig = csrfConfig,
+    stage = stage
   )
 
   "GET /sitemap.xml" should {
