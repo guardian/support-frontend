e261bb9d2f6d7d1fdb379fa835c7d0c1492a9400
Tom Forbes <tom.forbes@theguardian.com>
2021-09-29 14:25:19 +0100

    WIP - remove acquisitions service from payment-api

---
+++ support-payment-api/src/main/scala/services/GoogleAnalyticsServices.scala
@@ -0,0 +1,16 @@
+package services
+
+import com.gu.support.acquisitions.ga.{GoogleAnalyticsService, GoogleAnalyticsServiceImpl, MockGoogleAnalyticsService}
+import model.Environment
+import okhttp3.OkHttpClient
+
+object GoogleAnalyticsServices {
+  val liveService = new GoogleAnalyticsServiceImpl()()(new OkHttpClient())
+
+  val testService = MockGoogleAnalyticsService
+
+  def apply(env: Environment): GoogleAnalyticsService = env match {
+    case Environment.Live => liveService
+    case Environment.Test => testService
+  }
+}
