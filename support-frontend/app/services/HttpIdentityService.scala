b8710157f8611ed6ee478597c5b8c8cd12c4e5fa
John Duffell <john.duffell@guardian.co.uk>
2021-10-04 12:04:09 +0100

    make the guest checkout identity timeout 5 seconds

--- support-frontend/app/services/HttpIdentityService.scala
+++ support-frontend/app/services/HttpIdentityService.scala
@@ -153,7 +153,7 @@ class IdentityService(apiUrl: String, apiClientToken: String)(implicit wsClient:
       wsClient.url(s"$apiUrl/guest")
         .withHttpHeaders(postHeaders(req): _*)
         .withBody(body)
-        .withRequestTimeout(1.second)
+        .withRequestTimeout(5.seconds)
         .withMethod("POST")
         .withQueryStringParameters(("accountVerificationEmail", "true"))
     ) { resp =>
