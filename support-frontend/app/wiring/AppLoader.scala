935a98300157b14975a9babedc5fbf5cd491b96d
John Duffell <john.duffell@guardian.co.uk>
2021-10-14 16:40:18 +0100

    use faster start version of simple-configuration

--- support-frontend/app/wiring/AppLoader.scala
+++ support-frontend/app/wiring/AppLoader.scala
@@ -18,11 +18,11 @@ class AppLoader extends ApplicationLoader with StrictLogging {
     EnvironmentVariableCredentialsProvider.create()
   ).build
 
-  private def getParameterStoreConfig(initialConfiguration: Configuration): Configuration = {
-    val identity = AppIdentity.whoAmI(defaultAppName = "support-frontend", CredentialsProvider)
+  private def getParameterStoreConfig (initialConfiguration: Configuration, isDev: Boolean): Configuration = {
+    val identity = AppIdentity.whoAmI(defaultAppName = "support-frontend", CredentialsProvider, isDev)
     val loadedConfig = ConfigurationLoader.load(identity, CredentialsProvider) {
-      case AwsIdentity(app, stack, stage, _) =>
-        val privateConfig = SSMConfigurationLocation(s"/$stack/$app/$stage")
+      case AwsIdentity(app, stack, stage, region) =>
+        val privateConfig = SSMConfigurationLocation(s"/$stack/$app/$stage", region)
         val publicConfig = ResourceConfigurationLocation(s"$stage.public.conf")
 
         ComposedConfigurationLocation(List(privateConfig, publicConfig))
@@ -30,7 +30,7 @@ class AppLoader extends ApplicationLoader with StrictLogging {
       case DevIdentity(_) =>
         //If a local private config file exists then override any DEV Parameter Store config
         val privateConfigLocal = FileConfigurationLocation(new File(s"/etc/gu/support-frontend.private.conf"))
-        val privateConfigSSM = SSMConfigurationLocation(s"/support/frontend/DEV")
+        val privateConfigSSM = SSMConfigurationLocation(s"/support/frontend/DEV", "eu-west-1")
         val publicConfig = ResourceConfigurationLocation(s"DEV.public.conf")
 
         ComposedConfigurationLocation(List(privateConfigLocal, privateConfigSSM, publicConfig))
@@ -45,7 +45,9 @@ class AppLoader extends ApplicationLoader with StrictLogging {
       _.configure(context.environment)
     }
 
-    val contextWithConfig = context.copy(initialConfiguration = getParameterStoreConfig(context.initialConfiguration))
+    val isDev = context.environment.mode == Mode.Dev
+
+    val contextWithConfig = context.copy(initialConfiguration = getParameterStoreConfig(context.initialConfiguration, isDev))
 
     try {
       (new BuiltInComponentsFromContext(contextWithConfig) with AppComponents).application
