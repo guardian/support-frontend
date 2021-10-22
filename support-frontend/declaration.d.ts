e22370f1579cba783f217193ef3ccd59ecfcf295
Imogen Hardy <imogen.hardy@guardian.co.uk>
2021-10-22 17:12:39 +0100

    add defs for ophan without breaking everything else

--- support-frontend/declaration.d.ts
+++ support-frontend/declaration.d.ts
@@ -9,3 +9,10 @@ declare module '*.svg' {
 	const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
 	export default content;
 }
+
+declare module 'ophan' {
+	export const init: () => void;
+	export const record: (...args: unknown[]) => void;
+	export const sendInitialEvent: (...args: unknown[]) => void;
+	export const viewId: string;
+}
