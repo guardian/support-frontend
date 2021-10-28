765d69536d18c993608b21b017a9950d06426b26
George Haberis <george.haberis@guardian.co.uk>
2021-10-28 18:50:33 +0100

    small refactor

--- support-frontend/assets/helpers/tracking/quantumMetric.ts
+++ support-frontend/assets/helpers/tracking/quantumMetric.ts
@@ -1,21 +1,16 @@
 import { getGlobal, isSwitchOn } from 'helpers/globalsAndSwitches/globals';
 import { logException } from 'helpers/utilities/logger';
 
-// Contribution landing page and thank you page (all regions)
-const contributionsRegex = /\/contribute(\/thankyou)?(\?.*)?$/;
-// Non-Gifting Digi Subs landing page, checkout and thank you page (all regions)
-const digiSubRegex = /\/subscribe\/digital(\/checkout|\/thankyou)?(\?.*)?$/;
+const pathIsValid = (): boolean => {
+	const locationPath = window.location.pathname;
+	// Contribution landing page and thank you page (all regions)
+	const contributionsRegex = /\/contribute(\/thankyou)?(\?.*)?$/;
+	// Non-Gifting Digi Subs landing page, checkout and thank you page (all regions)
+	const digiSubRegex = /\/subscribe\/digital(\/checkout|\/thankyou)?(\?.*)?$/;
 
-// For this POC targetPageMatches has been copied from abtest.ts
-const targetPageMatches = (
-	locationPath: string,
-	targetPage: (string | null | undefined) | RegExp,
-): boolean => {
-	if (!targetPage) {
-		return true;
-	}
-
-	return locationPath.match(targetPage) != null;
+	return [contributionsRegex, digiSubRegex].some(
+		(pathRegEx) => locationPath.match(pathRegEx) != null,
+	);
 };
 
 const addQM = (): void => {
@@ -32,17 +27,17 @@ const addQM = (): void => {
 };
 
 const init = (): void => {
-	// Check feature switch is enabled
-	if (!isSwitchOn('enableQuantumMetric')) {
-		return;
-	}
-
-	const validPage = [contributionsRegex, digiSubRegex].some((regEx) =>
-		targetPageMatches(window.location.pathname, regEx),
-	);
-
-	// Only run QM on valid page paths
-	if (!validPage) {
+	/**
+	 * Return immediately if we're Server Side Rendering OR
+	 * The feature switch is off OR
+	 * the window.location.pathname is NOT valid.
+	 *
+	 */
+	if (
+		getGlobal('ssr') ||
+		!isSwitchOn('enableQuantumMetric') ||
+		!pathIsValid()
+	) {
 		return;
 	}
 
@@ -51,25 +46,24 @@ const init = (): void => {
 	 * on condition we're not server side rendering (ssr) the page.
 	 * @guardian/consent-management-platform breaks ssr otherwise.
 	 */
-	if (!getGlobal('ssr')) {
-		import('@guardian/consent-management-platform')
-			.then(({ onConsentChange }) => {
-				onConsentChange((state) => {
-					if (
-						state.ccpa?.doNotSell === false ||
-						(state.tcfv2?.consents &&
-							state.tcfv2.consents['1'] && // Store and/or access information on a device
-							state.tcfv2.consents['8'] && // Measure content performance
-							state.tcfv2.consents['10']) // Develop and improve products
-					) {
-						addQM();
-					}
-				});
-			})
-			.catch((error) => {
-				logException('Failed to load Quantum Metric', error);
+	import('@guardian/consent-management-platform')
+		.then(({ onConsentChange }) => {
+			onConsentChange((state) => {
+				// Check users consent state
+				if (
+					state.ccpa?.doNotSell === false ||
+					(state.tcfv2?.consents &&
+						state.tcfv2.consents['1'] && // Store and/or access information on a device
+						state.tcfv2.consents['8'] && // Measure content performance
+						state.tcfv2.consents['10']) // Develop and improve products
+				) {
+					addQM();
+				}
 			});
-	}
+		})
+		.catch((error) => {
+			logException('Failed to load Quantum Metric', error);
+		});
 };
 
 export { init };
