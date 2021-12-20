51dfd2f5991cbf7d0066737964c8367ae29afb27
Tom Pretty <tompretty2@gmail.com>
2021-12-20 12:26:27 +0000

    refactor animated dots to use emotion

--- support-frontend/assets/components/spinners/animatedDots.tsx
+++ support-frontend/assets/components/spinners/animatedDots.tsx
@@ -1,26 +1,71 @@
 // ----- Imports ----- //
+import { css } from '@emotion/core';
+import { neutral } from '@guardian/src-foundations';
 import React from 'react';
-import { classNameWithModifiers } from 'helpers/utilities/utilities';
-import './animatedDots.scss';
+
 // ----- Component ----- //
+
 type PropTypes = {
 	appearance: 'light' | 'medium' | 'dark';
-	modifierClasses: Array<string | null | undefined>;
 };
-export default function AnimatedDots(props: PropTypes) {
+
+export default function AnimatedDots({ appearance }: PropTypes): JSX.Element {
 	return (
-		<div
-			className={classNameWithModifiers('component-animated-dots', [
-				props.appearance,
-				...props.modifierClasses,
-			])}
-		>
-			<div className="component-animated-dots__bounce1" />
-			<div className="component-animated-dots__bounce2" />
-			<div className="component-animated-dots__bounce3" />
+		<div css={styles.container}>
+			<div css={styles.bounce} data-appearance={appearance} data-bounce="1" />
+			<div css={styles.bounce} data-appearance={appearance} data-bounce="2" />
+			<div css={styles.bounce} data-appearance={appearance} data-bounce="3" />
 		</div>
 	);
 }
-AnimatedDots.defaultProps = {
-	modifierClasses: [],
+
+// ---- Styles ---- //
+
+const styles = {
+	container: css`
+		margin: 10px auto 0;
+		width: 70px;
+		text-align: center;
+	`,
+	bounce: css`
+		width: 18px;
+		height: 18px;
+		border-radius: 100%;
+		display: inline-block;
+		animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+
+		&[data-appearance='light'] {
+			background-color: ${neutral[100]};
+		}
+
+		&[data-appearance='medium'] {
+			background-color: ${neutral[60]};
+		}
+
+		&[data-appearance='dark'] {
+			background-color: ${neutral[46]};
+		}
+
+		&[data-bounce='1'] {
+			animation-delay: -0.32s;
+		}
+		&[data-bounce='2'] {
+			animation-delay: -0.16s;
+		}
+		&[data-bounce='3'] {
+			animation-delay: -0.00s;
+		}
+
+	@keyframes sk-bouncedelay {
+		0%,
+		80%,
+		100% {
+			transform: scale(0);
+		}
+		40% {
+			transform: scale(1);
+		}
+	}
+}
+	`,
 };
