8a3cb7b61aa5df632bad7208be2eeb95edfe31f5
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2022-01-26 10:56:10 +0000

    Fix TS error in `options.tsx`

--- support-frontend/assets/components/forms/customFields/options.tsx
+++ support-frontend/assets/components/forms/customFields/options.tsx
@@ -1,10 +1,10 @@
 // ----- Imports ----- //
 import { Option } from '@guardian/src-select';
-import type { Node } from 'react';
+import type { ReactNode } from 'react';
 import React from 'react';
 
 // ----- Functions ----- //
-const options = (optionsForMapping: Record<string, string>): Node =>
+const options = (optionsForMapping: Record<string, string>): ReactNode =>
 	Object.keys(optionsForMapping).map((key) => (
 		<Option value={key}>{optionsForMapping[key]}</Option>
 	));
