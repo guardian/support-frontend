a348d516558e0d8617b4bb510e96b5695bf6bdc3
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-02-08 12:45:27 +0000

    fix options.tsx errors

--- support-frontend/assets/components/forms/customFields/options.tsx
+++ support-frontend/assets/components/forms/customFields/options.tsx
@@ -1,13 +1,17 @@
 // ----- Imports ----- //
 import { Option } from '@guardian/src-select';
-import type { Node } from 'react';
 import React from 'react';
 
 // ----- Functions ----- //
-const options = (optionsForMapping: Record<string, string>): Node =>
-	Object.keys(optionsForMapping).map((key) => (
-		<Option value={key}>{optionsForMapping[key]}</Option>
-	));
+const options = (optionsForMapping: Record<string, string>): JSX.Element => {
+	return (
+		<>
+			{Object.keys(optionsForMapping).map((key) => (
+				<Option value={key}>{optionsForMapping[key]}</Option>
+			))}
+		</>
+	);
+};
 
 // ----- Exports ----- //
 export { options };
