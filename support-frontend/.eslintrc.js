7faa114e4bed42a422043ebadc5eb3cd36416c10
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2022-02-09 16:29:29 +0000

    Add text role exemption to ESLint rules

--- support-frontend/.eslintrc.js
+++ support-frontend/.eslintrc.js
@@ -1,21 +1,28 @@
 module.exports = {
-  extends: '@guardian/eslint-config-typescript',
-  rules: {
-    // TODO: update this to 'warn' or delete once the post-migration fixing process is done
-    "import/no-default-export": "off",
-    "react/function-component-definition": [1, {
-      "namedComponents": "function-declaration",
-      "unnamedComponents": "function-expression",
-    }],
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project: 'tsconfig.json',
-      }
-    }
-  },
-  plugins: [
-    "react"
-  ]
-}
+	extends: '@guardian/eslint-config-typescript',
+	rules: {
+		// TODO: update this to 'warn' or delete once the post-migration fixing process is done
+		'import/no-default-export': 'off',
+		'react/function-component-definition': [
+			1,
+			{
+				namedComponents: 'function-declaration',
+				unnamedComponents: 'function-expression',
+			},
+		],
+		'jsx-a11y/aria-role': [
+			1,
+			{
+				allowedInvalidRoles: ['text'],
+			},
+		],
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {
+				project: 'tsconfig.json',
+			},
+		},
+	},
+	plugins: ['react'],
+};
