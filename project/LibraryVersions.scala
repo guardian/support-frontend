7b775c72b491f6747e9970e8ad1bd195fdde2695
Scala Steward <me@scala-steward.org>
2022-03-01 22:31:22 +0100

    Update scala-uri to 2.2.6

--- project/LibraryVersions.scala
+++ project/LibraryVersions.scala
@@ -1,4 +1,3 @@
-
 object LibraryVersions {
   val circeVersion = "0.14.1"
 
@@ -9,7 +8,7 @@ object LibraryVersions {
   val jacksonVersion = "2.10.5"
   val jacksonDatabindVersion = "2.11.4"
   val okhttpVersion = "3.10.0"
-  val scalaUriVersion = "2.2.2"
+  val scalaUriVersion = "2.2.6"
   val playCirceVersion = "2814.2"
   val stripeVersion = "10.12.0" // Supports API version 2019-05-16
 }
