4ac5d9288cd8ecfbc1fc208146e1f67848b867c8
Michael Jacobson <contact@michaeljacobson.co.uk>
2022-03-02 15:04:03 +0000

    update unit test for PaymentMethodEncoder

--- support-models/src/test/scala/com/gu/support/encoding/PaymentMethodEncoderSpec.scala
+++ support-models/src/test/scala/com/gu/support/encoding/PaymentMethodEncoderSpec.scala
@@ -94,9 +94,7 @@ class PaymentMethodEncoderSpec extends AsyncFlatSpec with Matchers with LazyLogg
          |},
          |"BankTransferType": "SEPA",
          |"Type": "BankTransfer",
-         |"PaymentGateway": "Stripe Bank Transfer - GNM Membership",
-         |"Country": null,
-         |"StreetName": null
+         |"PaymentGateway": "Stripe Bank Transfer - GNM Membership"
          |}""".stripMargin
 
     testEncoding[PaymentMethod](pm, expected)
