f86f738f09410d351edc0e2cd532b03170ea60ae
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-03-08 18:13:19 +0000

    styling updates

--- support-frontend/assets/pages/contributions-landing/components/BenefitsMessage.tsx
+++ support-frontend/assets/pages/contributions-landing/components/BenefitsMessage.tsx
@@ -1,6 +1,8 @@
 import { css } from '@emotion/react';
 import {
 	brandAlt,
+	from,
+	headline,
 	neutral,
 	space,
 	textSans,
@@ -10,13 +12,21 @@ import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
 const container = css`
 	border-top: 1px solid ${neutral[86]};
 	border-bottom: 1px solid ${neutral[86]};
-	margin: ${space[6]}px 0 ${space[3]}px 0;
-	padding: ${space[3]}px 0 ${space[4]}px 0;
+	margin: ${space[5]}px 0 ${space[3]}px 0;
+	padding: 6px 0 ${space[3]}px 0;
+
+	${from.desktop} {
+		margin: ${space[6]}px 0 ${space[3]}px 0;
+	}
 `;
 
 const title = css`
-	${textSans.medium({ fontWeight: 'bold', lineHeight: 'regular' })};
-	margin-bottom: 6px;
+	${headline.xxxsmall({ fontWeight: 'bold', lineHeight: 'regular' })};
+	margin-bottom: 10px;
+
+	${from.desktop} {
+		font-size: 20px;
+	}
 `;
 
 const body = css`
@@ -38,20 +48,13 @@ function BenefitsMessage({ countryGroupId }: PropTypes): JSX.Element {
 			? 'regularly'
 			: 'each week';
 
-	const shouldBeHighlighted =
-		countryGroupId === 'AUDCountries' ||
-		countryGroupId === 'UnitedStates' ||
-		countryGroupId === 'International' ||
-		countryGroupId === 'NZDCountries' ||
-		countryGroupId === 'Canada';
-
 	return (
 		<div css={container}>
 			<h3 css={title}>Supporter extras</h3>
 			<p css={body}>
-				To say thank you for your recurring contribution, our executive editor
-				will email you <span>{frequency}</span>,{' '}
-				<span css={shouldBeHighlighted && highlighted}>
+				To say thank you for your recurring contribution, a senior editor will
+				email you <span>{frequency}</span>,{' '}
+				<span css={highlighted}>
 					giving you an exclusive insight into our newsroom.
 				</span>{' '}
 				And we will stop asking you to fund our work as often.
