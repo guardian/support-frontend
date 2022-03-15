3eb68245365c2d041832cf47ace5fd4217132bc1
Rupert Bates <Rupert.Bates@guardian.co.uk>
2022-03-15 13:54:28 +0000

    The rules config needs to be moved to the overrides

--- support-frontend/.eslintrc.js
+++ support-frontend/.eslintrc.js
@@ -9,20 +9,20 @@ module.exports = {
 
 			// Only use Typescript linting on Typescript files
 			extends: ['@guardian/eslint-config-typescript'],
+			rules: {
+				// TODO: update this to 'warn' or delete once the post-migration fixing process is done
+				'import/no-default-export': 'off',
+				'react/function-component-definition': [
+					1,
+					{
+						namedComponents: 'function-declaration',
+						unnamedComponents: 'function-expression',
+					},
+				],
+				'@emotion/pkg-renaming': 'error',
+			},
 		},
 	],
-	rules: {
-		// TODO: update this to 'warn' or delete once the post-migration fixing process is done
-		'import/no-default-export': 'off',
-		'react/function-component-definition': [
-			1,
-			{
-				namedComponents: 'function-declaration',
-				unnamedComponents: 'function-expression',
-			},
-		],
-		'@emotion/pkg-renaming': 'error',
-	},
 	settings: {
 		'import/resolver': {
 			typescript: {
