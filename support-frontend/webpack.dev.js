d4bdfb872d62c7497b47be94d22579838e54a01e
Rupert Bates <Rupert.Bates@guardian.co.uk>
2022-04-01 15:47:38 +0100

    Fix more stuff

--- support-frontend/webpack.dev.js
+++ support-frontend/webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common('[name].css', '[name].js', false), {
 	mode: 'development',
 	devtool: 'inline-source-map',
 	devServer: {
-		disableHostCheck: true,
+		allowedHosts: 'all',
 		proxy: {
 			'**': {
 				target: 'http://support.thegulocal.com:9210',
