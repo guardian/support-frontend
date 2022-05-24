a08c9f7e0ed8103fa3d989d64c6756e2d040b12d
George Haberis <george.haberis@guardian.co.uk>
2022-05-24 17:05:39 +0100

    add SEPA form fields to the QM blocklist

---
+++ support-frontend/assets/pages/contributions-landing/components/SepaForm.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { SepaForm } from './SepaForm';
+
+describe('Direct debit form', () => {
+	const updateAddressStreetName = jest.fn();
+	const updateAddressCountry = jest.fn();
+	const updateIban = jest.fn();
+	const updateAccountHolderName = jest.fn();
+
+	beforeEach(() => {
+		render(
+			<SepaForm
+				iban={null}
+				accountHolderName={null}
+				updateAddressStreetName={updateAddressStreetName}
+				updateAddressCountry={updateAddressCountry}
+				updateIban={updateIban}
+				updateAccountHolderName={updateAccountHolderName}
+				checkoutFormHasBeenSubmitted={false}
+			/>,
+		);
+	});
+
+	describe('SEPA Form PII fields are in Quantum Metrics Blocklist', () => {
+		it("successfully moves to the review stage, showing the user's details", () => {
+			const elementTestIds = [
+				'sepa-account-holder-name-input',
+				'sepa-account-number',
+				'sepa-address-line-one',
+				'sepa-country',
+			];
+
+			elementTestIds.forEach((elementTestId) => {
+				const element = screen.getByTestId(elementTestId);
+				expect(element).toBeInTheDocument();
+				expect(element.dataset.qmMasking).toBe('blocklist');
+			});
+		});
+	});
+});
