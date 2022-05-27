88121c725d91260cfc18dd1f2fbc5d7b86fffb15
Rupert Bates <Rupert.Bates@guardian.co.uk>
2022-05-27 13:49:46 +0100

    Add monitoring

--- cdk/lib/stripe-patrons-data.ts
+++ cdk/lib/stripe-patrons-data.ts
@@ -4,6 +4,10 @@ import { Runtime } from "@aws-cdk/aws-lambda";
 import type { App } from "@aws-cdk/core";
 import { Duration } from "@aws-cdk/core";
 import { GuScheduledLambda } from "@guardian/cdk";
+import type {
+  GuLambdaErrorPercentageMonitoringProps,
+  NoMonitoring,
+} from "@guardian/cdk/lib/constructs/cloudwatch";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 
@@ -16,17 +20,31 @@ class StripePatronsDataLambda extends GuScheduledLambda {
       functionName: `${appName}-${scope.stage}`,
       handler:
         "com.gu.patrons.lambdas.ProcessStripeSubscriptionsLambda::handleRequest",
-      monitoringConfiguration: { noMonitoring: true },
+      monitoringConfiguration: monitoringForEnvironment(scope.stage),
       rules: [{ schedule: scheduleRateForEnvironment(scope.stage) }],
       runtime: Runtime.JAVA_11,
     });
+
+    function monitoringForEnvironment(
+      stage: string
+    ): NoMonitoring | GuLambdaErrorPercentageMonitoringProps {
+      if (stage == "PROD")
+        return {
+          alarmName: `${appName}-${scope.stage}-ErrorAlarm`,
+          alarmDescription: `Triggers if there are errors from ${appName} on ${scope.stage}`,
+          snsTopicName: "reader-revenue-dev",
+          toleratedErrorPercentage: 1,
+          numberOfMinutesAboveThresholdBeforeAlarm: 1,
+        };
+      return { noMonitoring: true };
+    }
 
     function environmentFromStage(stage: string) {
       return stage == "CODE" ? "DEV" : stage;
     }
 
     function scheduleRateForEnvironment(stage: string) {
-      return Schedule.rate(Duration.minutes(stage == "PROD" ? 15 : 3600));
+      return Schedule.rate(Duration.minutes(stage == "PROD" ? 15 : 60));
     }
 
     this.addToRolePolicy(
