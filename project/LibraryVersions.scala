efc80ac0753e18f19438519ceca2e182d491df07
Scala Steward <scala-steward@users.noreply.github.com>
2022-06-14 10:11:52 +0000

    Update aws-java-sdk-cloudwatch, ... to 1.12.239

--- project/LibraryVersions.scala
+++ project/LibraryVersions.scala
@@ -1,7 +1,7 @@
 object LibraryVersions {
   val circeVersion = "0.14.1"
 
-  val awsClientVersion = "1.12.227"
+  val awsClientVersion = "1.12.239"
   val awsClientVersion2 = "2.17.52"
 
   val catsVersion = "2.1.1"
