8317f250cab686a1939aff9f61fd752419580e10
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-07-20 14:17:53 +0100

    ensure supported payment methods are also switched on in rrcp

--- support-frontend/assets/helpers/subscriptionsForms/countryPaymentMethods.ts
+++ support-frontend/assets/helpers/subscriptionsForms/countryPaymentMethods.ts
@@ -1,4 +1,5 @@
 import { DirectDebit, PayPal, Stripe } from 'helpers/forms/paymentMethods';
+import { isSwitchOn } from 'helpers/globalsAndSwitches/globals';
 import type { IsoCountry } from 'helpers/internationalisation/country';
 import type { IsoCurrency } from 'helpers/internationalisation/currency';
 
@@ -7,17 +8,52 @@ export type SubscriptionsPaymentMethod =
 	| typeof PayPal
 	| typeof Stripe;
 
+type SubscriptionsSwitches = 'paypal' | 'directDebit' | 'creditCard';
+
+function getPaymentMethodSwitchName(
+	paymentMethod: SubscriptionsPaymentMethod,
+): SubscriptionsSwitches | null {
+	switch (paymentMethod) {
+		case PayPal:
+			return 'paypal';
+
+		case Stripe:
+			return 'directDebit';
+
+		case DirectDebit:
+			return 'creditCard';
+
+		default:
+			return null;
+	}
+}
+
 function supportedPaymentMethods(
 	currencyId: IsoCurrency,
 	countryId: IsoCountry,
 ): SubscriptionsPaymentMethod[] {
+	const subsPaymentMethods: SubscriptionsPaymentMethod[] = [
+		DirectDebit,
+		Stripe,
+		PayPal,
+	];
+
+	const switchedOnPaymentMethods: SubscriptionsPaymentMethod[] =
+		subsPaymentMethods.filter((paymentMethod) =>
+			isSwitchOn(
+				`subscriptionsPaymentMethods.${
+					getPaymentMethodSwitchName(paymentMethod) ?? '-'
+				}`,
+			),
+		);
+
 	// We only want to offer DD payment where the currency is GBP AND the country is GB
 	// This excludes eg. the Isle of Man
 	const allowsDirectDebit = currencyId === 'GBP' && countryId === 'GB';
-	const countrySpecific: SubscriptionsPaymentMethod[] = allowsDirectDebit
-		? [DirectDebit, Stripe, PayPal]
-		: [Stripe, PayPal];
-	return countrySpecific;
+
+	return allowsDirectDebit
+		? switchedOnPaymentMethods
+		: switchedOnPaymentMethods.filter((pm) => pm !== DirectDebit);
 }
 
 export { supportedPaymentMethods };
