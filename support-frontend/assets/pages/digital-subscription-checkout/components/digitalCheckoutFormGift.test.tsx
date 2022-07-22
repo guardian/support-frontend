06692203807078a3331e96bf9f685ca0949980e7
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-07-22 09:39:01 +0100

    update digitalCheckoutFormGift test setup

--- support-frontend/assets/pages/digital-subscription-checkout/components/digitalCheckoutFormGift.test.tsx
+++ support-frontend/assets/pages/digital-subscription-checkout/components/digitalCheckoutFormGift.test.tsx
@@ -6,6 +6,7 @@ import {
 import { fireEvent, screen } from '@testing-library/react';
 import { digitalProducts } from '__mocks__/productInfoMocks';
 import { renderWithStore } from '__test-utils__/render';
+import { isSwitchOn } from 'helpers/globalsAndSwitches/globals';
 import { addAddressSideEffects } from 'helpers/redux/checkout/address/sideEffects';
 import { setInitialCommonState } from 'helpers/redux/commonState/actions';
 import { commonReducer } from 'helpers/redux/commonState/reducer';
@@ -36,10 +37,20 @@ function setUpStore(initialState: WithDeliveryCheckoutState) {
 	return store;
 }
 
+jest.mock('helpers/globalsAndSwitches/globals', () => ({
+	__esModule: true,
+	isSwitchOn: jest.fn(),
+	getSettings: jest.fn(),
+	getGlobal: jest.fn(),
+}));
+
+const mock = (mockFn: unknown) => mockFn as jest.Mock;
+
 describe('Digital gift checkout form', () => {
 	// Suppress warnings related to our version of Redux and improper JSX
 	console.warn = jest.fn();
 	console.error = jest.fn();
+
 	let initialState;
 	beforeEach(() => {
 		initialState = {
@@ -71,12 +82,16 @@ describe('Digital gift checkout form', () => {
 				},
 			},
 		};
+
+		mock(isSwitchOn).mockImplementation(() => true);
+
 		renderWithStore(<DigitalCheckoutFormGift />, {
 			initialState,
 			// @ts-expect-error -- Type mismatch is unimportant for tests
 			store: setUpStore(initialState),
 		});
 	});
+
 	describe('Payment methods', () => {
 		it('shows the direct debit option when the currency is GBP and the billing address is in the UK', () => {
 			expect(screen.queryByText('Direct debit')).toBeInTheDocument();
