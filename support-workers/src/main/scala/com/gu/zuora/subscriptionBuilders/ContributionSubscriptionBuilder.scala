72567092800fe6bfad2863bf0143054b3518f9eb
Paul Brown <paulbrown1982@yahoo.co.uk>
2022-07-26 12:00:00 +0100

    Fix typo

--- support-workers/src/main/scala/com/gu/zuora/subscriptionBuilders/ContributionSubscriptionBuilder.scala
+++ support-workers/src/main/scala/com/gu/zuora/subscriptionBuilders/ContributionSubscriptionBuilder.scala
@@ -21,7 +21,7 @@ class ContributionSubscriptionBuilder(
     // Before 26 July 2022 the Customer Acceptance Date for Recurring Contributions was always  the same as the
     // Contract Effective Date (i.e. today). A change was made to push the CAS one day forward so as not to trigger
     // an invoice and payment immediately when making a Recurring Contribution. This will affect the customer experience
-    // for users who have no money in their account when purchasing, as tbey will no longer be notified on the checkout,
+    // for users who have no money in their account when purchasing, as they will no longer be notified on the checkout,
     // instead they will be notified by email the day after purchasing.
     //
     // The reason for making this change relates to a project being undertaken by the Finance team on August 1st 2022
