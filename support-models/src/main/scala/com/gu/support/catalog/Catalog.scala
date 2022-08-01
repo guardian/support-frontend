d76e7f98d7fa6c614699250a0a3b5fa39dff4305
Rupert Bates <Rupert.Bates@guardian.co.uk>
2022-08-01 09:52:20 +0100

    Fix compilation error

--- support-models/src/main/scala/com/gu/support/catalog/Catalog.scala
+++ support-models/src/main/scala/com/gu/support/catalog/Catalog.scala
@@ -13,11 +13,11 @@ case class Catalog(
 )
 
 object Catalog {
-  lazy val productRatePlansWithPrices: List[ProductRatePlanId] = (for {
+  lazy val productRatePlansWithPrices: List[ProductRatePlanId] = for {
     product <- List(SupporterPlus, DigitalPack, Paper, GuardianWeekly)
     env <- List(PROD, UAT, SANDBOX)
     plan <- product.ratePlans(env)
-  } yield plan.id).flatten
+  } yield plan.id
 
   implicit val encoder: Encoder[Catalog] = deriveEncoder
   implicit val decoder: Decoder[Catalog] = deriveDecoder[Catalog].prepare(mapFields)
