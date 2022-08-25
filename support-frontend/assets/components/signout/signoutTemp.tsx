e07f778c8e6c01d3b2d77877a13deb586ae0ca14
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2022-08-25 14:22:43 +0100

    fixed abtest variant check

--- support-frontend/assets/components/signout/signoutTemp.tsx
+++ support-frontend/assets/components/signout/signoutTemp.tsx
@@ -11,28 +11,12 @@ interface SignoutProps {
 // ----- Component ----- //
 
 function SignoutTemp(abTests: SignoutProps) {
-	console.log(
-		'SignOut2 here!!',
-		abTests.abVariant,
-		//abVariant.abVariant.{'newProduct'},
-	);
+	const testmsg2 = !abTests.abVariant.newProduct
+		? 'NOT Found ABtest newProduct'
+		: `FOUND ABtest newProduct='${abTests.abVariant.newProduct}'`;
+	console.log(testmsg2);
 
-	// if (abVariant.newProduct !== undefined) {
-	// 	console.log('SignOut2 newProduct Field Found!', abVariant.newProduct);
-	// }
-	// abVariant.abVariant.newProduct;
-	// if (abVariant.abVariant.newProduct !== undefined) {
-	// 	console.log('SignOut2 NotFound!');
-	// } else {
-	// 	console.log('SignOut2 Found!', abVariant.newProduct);
-	// }
-
-	const testmsg2 = abTests.abVariant.newProduct;
-	return (
-		<a className="component-signout">
-			2Not you? Sign out2 {abTests.abVariant.newProduct} {testmsg2}
-		</a>
-	);
+	return <a className="component-signout">- {testmsg2} -</a>;
 }
 
 // ----- Map State/Props ----- //
