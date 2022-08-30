a4c31f89f32e4134ac920cceaf9a3909cfec2e5c
George Haberis <george.haberis@guardian.co.uk>
2022-08-30 10:46:17 +0100

    Remove email max-length validation message (#4157)
    * remove email max-length validation message
    
    * update max email length

--- support-frontend/assets/helpers/subscriptionsForms/rules.ts
+++ support-frontend/assets/helpers/subscriptionsForms/rules.ts
@@ -67,7 +67,7 @@ function applyPersonalDetailsRules(
 			error: formError('email', 'Please enter a valid email address.'),
 		},
 		{
-			rule: notLongerThan(fields.email, 40),
+			rule: notLongerThan(fields.email, 80),
 			error: formError('email', 'Email address is too long.'),
 		},
 		{
