3857e64567492a4e580b460847ea1bf4660b55f7
George Haberis <george.haberis@guardian.co.uk>
2022-08-30 10:26:09 +0100

    update max email length

--- support-frontend/assets/helpers/subscriptionsForms/rules.ts
+++ support-frontend/assets/helpers/subscriptionsForms/rules.ts
@@ -67,6 +67,10 @@ function applyPersonalDetailsRules(
 			error: formError('email', 'Please enter a valid email address.'),
 		},
 		{
+			rule: notLongerThan(fields.email, 80),
+			error: formError('email', 'Email address is too long.'),
+		},
+		{
 			rule: emailAddressesMatch(
 				fields.isSignedIn,
 				fields.email,
