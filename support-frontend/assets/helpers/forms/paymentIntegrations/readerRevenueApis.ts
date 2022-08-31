cb9f1a2eb25ded9ad496287f653f94e1906dabc5
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2022-08-31 17:16:12 +0100

    Revert changes to readerRevenueApis and Dev.public.conf files

--- support-frontend/assets/helpers/forms/paymentIntegrations/readerRevenueApis.ts
+++ support-frontend/assets/helpers/forms/paymentIntegrations/readerRevenueApis.ts
@@ -244,11 +244,6 @@ const MAX_POLLS = 10;
 function regularPaymentFieldsFromAuthorisation(
 	authorisation: PaymentAuthorisation,
 ): RegularPaymentFields {
-  return {
-      accountHolderName: "testuser",
-      sortCode: "200000",
-      accountNumber: "55779911",
-  }
 	switch (authorisation.paymentMethod) {
 		case Stripe:
 			if (authorisation.paymentMethodId) {
