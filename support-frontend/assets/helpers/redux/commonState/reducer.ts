967a13b5b22ed59bc2d4731ef9986d5ed16871bf
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-09-01 17:51:19 +0100

    add tracking and additional effects to tab switching

--- support-frontend/assets/helpers/redux/commonState/reducer.ts
+++ support-frontend/assets/helpers/redux/commonState/reducer.ts
@@ -1,10 +1,14 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
-import type { ContributionTypes } from 'helpers/contributions';
+import type {
+	ContributionAmounts,
+	ContributionTypes,
+} from 'helpers/contributions';
 import type { ExistingPaymentMethod } from 'helpers/forms/existingPaymentMethods/existingPaymentMethods';
 import type { IsoCountry } from 'helpers/internationalisation/country';
 import { fromCountry } from 'helpers/internationalisation/countryGroup';
 import { fromCountryGroupId } from 'helpers/internationalisation/currency';
+import { setProductType } from 'helpers/redux/checkout/product/actions';
 import type { CommonStateSetupData, Internationalisation } from './state';
 import { initialCommonState } from './state';
 
@@ -22,6 +26,30 @@ function getInternationalisationFromCountry(
 	};
 }
 
+function getLocalisedCurrencyId(
+	internationalisation: Internationalisation,
+	shouldUseLocalCurrency: boolean,
+) {
+	if (shouldUseLocalCurrency && internationalisation.localCurrencyCountry) {
+		return internationalisation.localCurrencyCountry.currency;
+	}
+	return internationalisation.defaultCurrency;
+}
+
+function getLocalisedAmounts(
+	internationalisation: Internationalisation,
+	defaultAmounts: ContributionAmounts,
+	shouldUseLocalCurrency: boolean,
+) {
+	if (shouldUseLocalCurrency && internationalisation.localCurrencyCountry) {
+		return {
+			...defaultAmounts,
+			...internationalisation.localCurrencyCountry.amounts,
+		};
+	}
+	return defaultAmounts;
+}
+
 export const commonSlice = createSlice({
 	name: 'common',
 	initialState: initialCommonState,
@@ -37,14 +65,17 @@ export const commonSlice = createSlice({
 				amounts,
 				defaultAmounts,
 			} = action.payload;
-			state.campaign = campaign;
-			state.referrerAcquisitionData = referrerAcquisitionData;
-			state.otherQueryParams = otherQueryParams;
-			state.internationalisation = internationalisation;
-			state.abParticipations = abParticipations;
-			state.settings = settings;
-			state.amounts = amounts;
-			state.defaultAmounts = defaultAmounts;
+			return {
+				...state,
+				campaign,
+				referrerAcquisitionData,
+				otherQueryParams,
+				internationalisation,
+				abParticipations,
+				settings,
+				amounts,
+				defaultAmounts,
+			};
 		},
 		setCountryInternationalisation(state, action: PayloadAction<IsoCountry>) {
 			state.internationalisation = {
@@ -65,23 +96,47 @@ export const commonSlice = createSlice({
 			state.settings.contributionTypes = action.payload;
 		},
 		setCurrencyId(state, action: PayloadAction<boolean>) {
-			state.internationalisation.currencyId =
-				action.payload && state.internationalisation.localCurrencyCountry
-					? state.internationalisation.localCurrencyCountry.currency
-					: state.internationalisation.defaultCurrency;
+			state.internationalisation.currencyId = getLocalisedCurrencyId(
+				state.internationalisation,
+				action.payload,
+			);
 		},
 		setUseLocalCurrencyFlag(state, action: PayloadAction<boolean>) {
 			state.internationalisation.useLocalCurrency = action.payload;
 		},
 		setUseLocalAmounts(state, action: PayloadAction<boolean>) {
-			state.amounts =
-				action.payload && state.internationalisation.localCurrencyCountry
-					? {
-							...state.defaultAmounts,
-							...state.internationalisation.localCurrencyCountry.amounts,
-					  }
-					: state.defaultAmounts;
+			state.amounts = getLocalisedAmounts(
+				state.internationalisation,
+				state.defaultAmounts,
+				action.payload,
+			);
 		},
+	},
+	extraReducers: (builder) => {
+		builder.addCase(setProductType, (state, action) => {
+			// Reset amount localisation when the contributions product changes
+			if (action.payload === 'MONTHLY' || action.payload === 'ANNUAL') {
+				state.amounts = getLocalisedAmounts(
+					state.internationalisation,
+					state.defaultAmounts,
+					false,
+				);
+				state.internationalisation.currencyId = getLocalisedCurrencyId(
+					state.internationalisation,
+					false,
+				);
+			} else if (action.payload === 'ONE_OFF') {
+				state.amounts = getLocalisedAmounts(
+					state.internationalisation,
+					state.defaultAmounts,
+					state.internationalisation.useLocalCurrency,
+				);
+				state.internationalisation.currencyId = getLocalisedCurrencyId(
+					state.internationalisation,
+					false,
+				);
+			}
+		});
 	},
 });
 
