700b07efa3f6de592a4ee11381c0d39b6f812654
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-09-22 15:21:50 +0100

    expand selectors for payment errors

--- support-frontend/assets/helpers/redux/checkout/checkoutSelectors.ts
+++ support-frontend/assets/helpers/redux/checkout/checkoutSelectors.ts
@@ -1,6 +1,36 @@
 import type { ContributionsState } from '../contributionsStore';
 import { getContributionType } from './product/selectors/productType';
 
+function getPaymentMethodErrors(
+	state: ContributionsState,
+): Record<string, string[]> {
+	const { payment } = state.page.checkoutForm;
+
+	switch (payment.paymentMethod) {
+		case 'Stripe':
+			return payment.stripe.errors;
+
+		case 'DirectDebit':
+			return payment.directDebit.errors ?? {};
+
+		case 'Sepa':
+			return payment.sepa.errors;
+
+		default:
+			return {};
+	}
+}
+
+function formHasPaymentMethodErrors(state: ContributionsState): boolean {
+	const errors = getPaymentMethodErrors(state);
+	const errorObjectHasKeys = !!Object.keys(errors).length;
+	const atLeastOneKeyHasErrors = Object.keys(errors).some(
+		(key) => errors[key].length,
+	);
+
+	return errorObjectHasKeys && atLeastOneKeyHasErrors;
+}
+
 export function getAllErrorsForContributions(
 	state: ContributionsState,
 ): Partial<Record<string, string[]>> {
@@ -12,26 +42,32 @@ export function getAllErrorsForContributions(
 	if (contributionType === 'ONE_OFF') {
 		return {
 			email,
+			...getPaymentMethodErrors(state),
 		};
 	}
 	return {
 		firstName,
 		lastName,
 		email,
+		...getPaymentMethodErrors(state),
 	};
 }
 
 export function contributionsFormHasErrors(state: ContributionsState): boolean {
 	const contributionType = getContributionType(state);
+	const hasPaymentErrors = formHasPaymentMethodErrors(state);
 	const { firstName, lastName, email } =
 		state.page.checkoutForm.personalDetails.errors ?? {};
 
 	if (contributionType === 'ONE_OFF') {
-		return !!email?.length;
+		return hasPaymentErrors || !!email?.length;
 	}
 
 	const allErrorsLength =
-		!!firstName?.length || !!lastName?.length || !!email?.length;
+		hasPaymentErrors ||
+		!!firstName?.length ||
+		!!lastName?.length ||
+		!!email?.length;
 
 	return allErrorsLength;
 }
