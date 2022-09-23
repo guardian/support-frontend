cebe4d51a1ee4f1959c246893f0117e9c3447233
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-09-23 14:24:16 +0100

    add css tweaks and breakpoint-specific padding

--- support-frontend/assets/components/priceCards/priceCards.tsx
+++ support-frontend/assets/components/priceCards/priceCards.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
-import { ChoiceCardGroup } from '@guardian/source-react-components';
+import { from, space, until } from '@guardian/source-foundations';
+import { ChoiceCard, ChoiceCardGroup } from '@guardian/source-react-components';
 import { simpleFormatAmount } from 'helpers/forms/checkouts';
 import type { IsoCurrency } from 'helpers/internationalisation/currency';
 import { currencies } from 'helpers/internationalisation/currency';
@@ -8,6 +9,42 @@ import { PriceCard } from './priceCard';
 const cardStyles = css`
 	label {
 		border-radius: 10px;
+	}
+`;
+
+const otherAmountFullWidthStyles = css`
+	> div > label:last-of-type {
+		grid-column-start: 1;
+		grid-column-end: 3;
+	}
+`;
+
+const mobileGrid = css`
+	> div {
+		${until.mobileLandscape} {
+			display: grid;
+			column-gap: ${space[2]}px;
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+`;
+
+const cardsContainer = css`
+	padding-top: ${space[2]}px;
+	padding-bottom: ${space[6]}px;
+
+	${from.mobileLandscape} {
+		padding-top: ${space[3]}px;
+		padding-bottom: 28px;
+	}
+
+	${from.tablet} {
+		padding-top: ${space[2]}px;
+		padding-bottom: 32px;
+	}
+
+	${from.desktop} {
+		padding-bottom: ${space[9]}px;
 	}
 `;
 
@@ -27,16 +64,38 @@ export function PriceCards({
 	paymentInterval,
 }: PriceCardsProps): JSX.Element {
 	return (
-		<ChoiceCardGroup cssOverrides={cardStyles} name="amounts" columns={2}>
-			{amounts.map((amount) => (
-				<PriceCard
-					amount={amount}
-					amountWithCurrency={simpleFormatAmount(currencies[currency], amount)}
-					isSelected={amount === selectedAmount}
-					onClick={onAmountChange}
-					paymentInterval={paymentInterval}
+		<ChoiceCardGroup
+			cssOverrides={[
+				cardStyles,
+				otherAmountFullWidthStyles,
+				mobileGrid,
+				cardsContainer,
+			]}
+			name="amounts"
+			columns={2}
+		>
+			<>
+				{amounts.map((amount) => (
+					<PriceCard
+						amount={amount}
+						amountWithCurrency={simpleFormatAmount(
+							currencies[currency],
+							amount,
+						)}
+						isSelected={amount === selectedAmount}
+						onClick={onAmountChange}
+						paymentInterval={paymentInterval}
+					/>
+				))}
+				<ChoiceCard
+					id="amount-other"
+					key="amount-other"
+					name="amount"
+					onChange={() => onAmountChange('other')}
+					value="other"
+					label="Choose your amount"
 				/>
-			))}
+			</>
 		</ChoiceCardGroup>
 	);
 }
