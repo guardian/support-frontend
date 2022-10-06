7bf3a40a20480778971103155b1a84301eb0c91f
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-10-06 11:51:53 +0100

    remove scss styles from credit card icons component

--- support-frontend/assets/pages/contributions-landing/components/StripeCardForm/CreditCardIcons.tsx
+++ support-frontend/assets/pages/contributions-landing/components/StripeCardForm/CreditCardIcons.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import type { IsoCountry } from 'helpers/internationalisation/country';
 import CreditCardsROW from './creditCardsROW.svg';
 import CreditCardsUS from './creditCardsUS.svg';
@@ -6,12 +7,20 @@ interface CreditCardIconsProps {
 	country: IsoCountry;
 }
 
+const icons = css`
+	margin-bottom: 5px;
+	float: right;
+	position: absolute;
+	right: 0;
+	top: -7px;
+`;
+
 export function CreditCardIcons({
 	country,
 }: CreditCardIconsProps): JSX.Element {
 	if (country === 'US') {
-		return <CreditCardsUS className="form__credit-card-icons" />;
+		return <CreditCardsUS css={icons} />;
 	}
 
-	return <CreditCardsROW className="form__credit-card-icons" />;
+	return <CreditCardsROW css={icons} />;
 }
