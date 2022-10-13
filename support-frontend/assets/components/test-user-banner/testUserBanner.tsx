92bba1f4d079b0b19e6d7bb75c60bc34aeb06b4b
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-10-13 15:39:05 +0100

    don't rely on state for test user status

--- support-frontend/assets/components/test-user-banner/testUserBanner.tsx
+++ support-frontend/assets/components/test-user-banner/testUserBanner.tsx
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
 import { error, neutral, space, textSans } from '@guardian/source-foundations';
-import { useContributionsSelector } from 'helpers/redux/storeHooks';
+import { isTestUser } from 'helpers/user/user';
 
 const testUserBannerStyles = css`
 	${textSans.large()};
@@ -11,11 +11,9 @@ const testUserBannerStyles = css`
 `;
 
 export function TestUserBanner(): JSX.Element | null {
-	const isTestUser = useContributionsSelector(
-		(state) => state.page.user.isTestUser,
-	);
+	const testUser = isTestUser();
 
-	if (isTestUser) {
+	if (testUser) {
 		return (
 			<div css={testUserBannerStyles}>
 				<p>You are a test user</p>
