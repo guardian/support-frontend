25c77842944ae1b382e93d22c32b594d993011a2
Michael Jacobson <contact@michaeljacobson.co.uk>
2022-10-18 12:31:23 +0100

    revert to mid-migration state, to being prod migration process

--- cdk/lib/payment-api.ts
+++ cdk/lib/payment-api.ts
@@ -1,11 +1,13 @@
+import { join } from "path";
 import {
   ComparisonOperator,
   Metric,
   TreatMissingData,
 } from "@aws-cdk/aws-cloudwatch";
 import { InstanceClass, InstanceSize, InstanceType } from "@aws-cdk/aws-ec2";
+import { CfnInclude } from "@aws-cdk/cloudformation-include";
 import type { App } from "@aws-cdk/core";
-import { Duration } from "@aws-cdk/core";
+import { Duration, Tags } from "@aws-cdk/core";
 import { GuPlayApp } from "@guardian/cdk";
 import { AccessScope } from "@guardian/cdk/lib/constants";
 import { GuAlarm } from "@guardian/cdk/lib/constructs/cloudwatch";
@@ -73,6 +75,26 @@ export class PaymentApi extends GuStack {
 
     const sqsKmsArn = new GuStringParameter(this, "SqsKmsArn", {
       description: "ARN of the KMS key for encrypting SQS data",
+    });
+
+    const yamlTemplateFilePath = join(
+      __dirname,
+      "../..",
+      "support-payment-api/src/main/resources/cloud-formation.yaml"
+    );
+
+    new CfnInclude(this, "YamlTemplate", {
+      templateFile: yamlTemplateFilePath,
+      parameters: {
+        Stage: props.stage,
+        EmailSqsQueueCodeArn: emailSqsCodeArn,
+        EmailSqsQueueProdArn: emailSqsProdArn,
+        OphanRole: ophanRole,
+        KinesisStreamArn: kinesisStreamArn,
+        ContributionsStoreSqsQueueCodeArn: contributionsStoreSqsQueueCodeArn,
+        ContributionsStoreSqsQueueProdArn: contributionsStoreSqsQueueProdArn,
+        SqsKmsArn: sqsKmsArn,
+      },
     });
 
     // TODO: Should these remain as cloudformation parameters?
@@ -155,6 +177,10 @@ export class PaymentApi extends GuStack {
         ],
       },
     });
+
+    // TODO: remove this tag after the migration
+    const playAppAsg = playApp.autoScalingGroup;
+    Tags.of(playAppAsg).add("gu:riffraff:new-asg", "true");
 
     // ---- Alarms ---- //
 
