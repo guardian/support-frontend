ddb58201cc237fa6f975afc8bbd56e7254571012
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-10-18 16:04:24 +0100

    abstract more in the element decorator, handle focus states

--- support-frontend/assets/components/stripeCardForm/elementDecorator.tsx
+++ support-frontend/assets/components/stripeCardForm/elementDecorator.tsx
@@ -2,26 +2,21 @@ import { css } from '@emotion/react';
 import {
 	descriptionId,
 	error,
+	focusHalo,
 	height,
 	neutral,
 	space,
 } from '@guardian/source-foundations';
 import type { LabelProps } from '@guardian/source-react-components';
 import { InlineError, Label } from '@guardian/source-react-components';
+import type {
+	StripeCardElementOptions,
+	StripeElementStyleVariant,
+} from '@stripe/stripe-js';
+import { useState } from 'react';
 
 const inlineMessageMargin = css`
 	margin-top: 2px;
-`;
-
-const stripeElementStyles = css`
-	display: block;
-
-	& .StripeElement {
-		margin-top: ${space[1]}px;
-		height: ${height.inputMedium}px;
-		border: 2px solid ${neutral[60]};
-		padding: 10px ${space[2]}px;
-	}
 `;
 
 const stripeElementErrorStyles = css`
@@ -30,33 +25,83 @@ const stripeElementErrorStyles = css`
 	}
 `;
 
+const stripeElementFocusStyles = css`
+	& .StripeElement {
+		${focusHalo}
+	}
+`;
+
+const stripeElementStyles = (isFocused: boolean, error?: string) => css`
+	display: block;
+
+	& .StripeElement {
+		margin-top: ${space[1]}px;
+		height: ${height.inputMedium}px;
+		border: 2px solid ${neutral[60]};
+		padding: 10px ${space[2]}px;
+	}
+
+	${isFocused && stripeElementFocusStyles};
+
+	${error && stripeElementErrorStyles}
+`;
+
+type ElementRenderProps = {
+	id: string;
+	options: StripeCardElementOptions;
+	onFocus: () => void;
+	onBlur: () => void;
+};
+
 type ElementDecoratorProps = LabelProps & {
-	children: React.ReactNode;
+	element: (renderProps: ElementRenderProps) => React.ReactNode;
 	id: string;
 	error?: string;
 };
 
-function getElementStyles(error?: string) {
-	if (error) {
-		return [stripeElementStyles, stripeElementErrorStyles];
-	}
-	return stripeElementStyles;
-}
+// Styles for stripe elements
+const baseStyles: StripeElementStyleVariant = {
+	fontSize: `${space[4]}px`,
+	color: neutral[7],
+	'::placeholder': {
+		color: neutral[100],
+	},
+};
+
+const invalidStyles: StripeElementStyleVariant = {
+	color: error[400],
+};
 
 export function ElementDecorator({
-	children,
 	id,
+	element,
 	error,
 	...labelProps
 }: ElementDecoratorProps): JSX.Element {
+	const [isFocused, setIsFocused] = useState<boolean>(false);
+
 	return (
-		<Label {...labelProps} htmlFor={id} cssOverrides={getElementStyles(error)}>
+		<Label
+			{...labelProps}
+			htmlFor={id}
+			cssOverrides={stripeElementStyles(isFocused, error)}
+		>
 			{error && (
 				<div css={inlineMessageMargin}>
 					<InlineError id={descriptionId(id)}>{error}</InlineError>
 				</div>
 			)}
-			{children}
+			{element({
+				id,
+				options: {
+					style: {
+						base: { ...baseStyles },
+						invalid: { ...invalidStyles },
+					},
+				},
+				onFocus: () => setIsFocused(true),
+				onBlur: () => setIsFocused(false),
+			})}
 		</Label>
 	);
 }
