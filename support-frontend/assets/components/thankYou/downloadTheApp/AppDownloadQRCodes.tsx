e9f0ce720011bf7a2f358a57339509e305f99a68
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-10-19 10:30:35 +0100

    styling updates

--- support-frontend/assets/components/thankYou/downloadTheApp/AppDownloadQRCodes.tsx
+++ support-frontend/assets/components/thankYou/downloadTheApp/AppDownloadQRCodes.tsx
@@ -1,16 +1,18 @@
 import { css } from '@emotion/react';
-import { space, sport, textSans } from '@guardian/source-foundations';
+import { neutral, space, sport, textSans } from '@guardian/source-foundations';
 
 const container = css`
 	display: flex;
+	margin-bottom: ${space[3]}px;
 
-	& div:first-child {
+	& div:first-of-type {
 		margin-right: ${space[5]}px;
 	}
 `;
 
 const qrContainer = css`
 	background-color: ${sport[800]};
+	border: 1px solid ${neutral[93]};
 `;
 
 const title = css`
@@ -24,20 +26,27 @@ const img = css`
 	margin: ${space[5]}px 40px 35px;
 `;
 
-function QRCodes(): JSX.Element {
+function AppDownloadQRCodes(): JSX.Element {
 	return (
 		<div css={container}>
 			<div css={qrContainer}>
 				<p css={title}>Download on the App Store</p>
 				<img
 					css={img}
+					/////////////////////
+					// PLACEHOLDER LINK //
+					//////////////////////
 					src="https://tools-qr-production.s3.amazonaws.com/output/apple-toolbox/731eb34571091e0b5081c965ebb0d652/f04d238959ff2fb888b8cb92efae42d8.png"
 				/>
 			</div>
+
 			<div css={qrContainer}>
 				<p css={title}>Get it on Google Play</p>
 				<img
 					css={img}
+					//////////////////////
+					// PLACEHOLDER LINK //
+					//////////////////////
 					src="https://tools-qr-production.s3.amazonaws.com/output/apple-toolbox/731eb34571091e0b5081c965ebb0d652/f04d238959ff2fb888b8cb92efae42d8.png"
 				/>
 			</div>
@@ -45,4 +54,4 @@ function QRCodes(): JSX.Element {
 	);
 }
 
-export default QRCodes;
+export default AppDownloadQRCodes;
