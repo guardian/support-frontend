b0275ed1550301b0e395d0cfa198a3b7d986dd92
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2022-10-20 08:23:12 +0100

    changed queryPromos to promos in LandingCopyProvider

--- support-frontend/app/controllers/WeeklySubscriptionController.scala
+++ support-frontend/app/controllers/WeeklySubscriptionController.scala
@@ -12,7 +12,6 @@ import play.api.mvc._
 import play.twirl.api.Html
 import views.EmptyDiv
 import views.ViewHelpers.outputJson
-import com.typesafe.scalalogging.StrictLogging
 
 import scala.concurrent.ExecutionContext
 
