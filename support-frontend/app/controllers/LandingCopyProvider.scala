832b8b363b62dfd42039e5fe73967ec3fd8dfa41
Rupert Bates <Rupert.Bates@guardian.co.uk>
2022-10-24 17:18:38 +0100

    Fix for Guardian Weekly promo copy issue

--- support-frontend/app/controllers/LandingCopyProvider.scala
+++ support-frontend/app/controllers/LandingCopyProvider.scala
@@ -1,10 +1,9 @@
 package controllers
 
-import com.gu.i18n.Country.UK
 import com.gu.i18n.{Country, CountryGroup}
-import com.gu.support.catalog.Product
-import com.gu.support.config.Stage
-import com.gu.support.promotions.{ProductPromotionCopy, PromoCode, PromotionCopy, PromotionServiceProvider}
+import com.gu.support.catalog._
+import com.gu.support.config.{Stage, TouchPointEnvironments}
+import com.gu.support.promotions.{ProductPromotionCopy, PromotionCopy, PromotionServiceProvider}
 
 class LandingCopyProvider(
     promotionServiceProvider: PromotionServiceProvider,
@@ -15,22 +14,39 @@ class LandingCopyProvider(
   // To see if there is any promotional copy in place for this page we need to get a country in the current region (country group)
   // this is because promotions apply to countries not regions. We can use any country however because the promo tool UI only deals
   // with regions and then adds all the countries for that region to the promotion
-  def promotionCopy(queryPromos: List[String], product1: Product, countryCode: String): Option[PromotionCopy] = {
-    val country = (for {
+  def promotionCopy(queryPromos: List[String], product1: Product, countryCode: String): Option[PromotionCopy] =
+    for {
       countryGroup <- CountryGroup.byId(countryCode)
+      productRatePlanIds = getProductRatePlanIdsForCountryGroup(product1, countryGroup)
       country <- countryGroup.countries.headOption
-    } yield country).getOrElse(UK)
-    promotionCopyForPrimaryCountry(queryPromos, product1, country)
+      promoCopy <- promotionCopyForPrimaryCountry(queryPromos, productRatePlanIds, country)
+    } yield promoCopy
+
+  def getProductRatePlanIdsForCountryGroup(product: Product, countryGroup: CountryGroup) = {
+    val environment = TouchPointEnvironments.fromStage(stage)
+    (product, countryGroup) match {
+      case (GuardianWeekly, CountryGroup.RestOfTheWorld) =>
+        GuardianWeekly
+          .ratePlans(environment)
+          .filter(productRatePlan => productRatePlan.fulfilmentOptions == RestOfWorld)
+          .map(_.id)
+      case (GuardianWeekly, _) =>
+        GuardianWeekly
+          .ratePlans(environment)
+          .filter(productRatePlan => productRatePlan.fulfilmentOptions == Domestic)
+          .map(_.id)
+      case _ => product.getProductRatePlanIds(environment)
+    }
   }
 
   def promotionCopyForPrimaryCountry(
       queryPromos: List[String],
-      product1: Product,
+      productRatePlanIds: List[ProductRatePlanId],
       country: Country,
   ): Option[PromotionCopy] = {
     queryPromos
       .to(LazyList) // For efficiency - only call getCopyForPromoCode until we find a valid promo
-      .map(promoCode => promoCopyService.getCopyForPromoCode(promoCode, product1, country))
+      .map(promoCode => promoCopyService.getCopyForPromoCode(promoCode, productRatePlanIds, country))
       .collectFirst { case Some(promoCopy) => promoCopy }
   }
 }
