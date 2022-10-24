24d7ce6e1de8dc141c57c9552146513cdab1e43f
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2022-10-24 14:23:25 +0100

    Revert "updateBillState moved into container, firstName,lastName cleanup"
    This reverts commit ee760402ca72300635278dc5777980df78bef604.

--- support-frontend/assets/components/personalDetails/personalDetailsContainer.tsx
+++ support-frontend/assets/components/personalDetails/personalDetailsContainer.tsx
@@ -1,4 +1,3 @@
-import type { StateProvince } from 'helpers/internationalisation/country';
 import {
 	setEmail,
 	setFirstName,
@@ -9,7 +8,6 @@ import {
 	useContributionsDispatch,
 	useContributionsSelector,
 } from 'helpers/redux/storeHooks';
-import { updateBillingState } from 'pages/contributions-landing/contributionsLandingActions';
 import type { PersonalDetailsProps } from './personalDetails';
 
 // We only want to use the user state value if the form state value has not been changed since it was initialised,
@@ -28,7 +26,6 @@ export function PersonalDetailsContainer({
 	renderPersonalDetails,
 }: PersonalDetailsContainerProps): JSX.Element {
 	const dispatch = useContributionsDispatch();
-
 	const { email, firstName, lastName } = useContributionsSelector(
 		(state) => state.page.checkoutForm.personalDetails,
 	);
@@ -56,14 +53,12 @@ export function PersonalDetailsContainer({
 		(state) => state.page.checkoutForm.personalDetails.isSignedIn,
 	);
 
-	function onFirstNameChange(firstName: string) {
-		dispatch(setFirstName(firstName));
+	function onFirstNameChange(email: string) {
+		dispatch(setFirstName(email));
 	}
-	function onLastNameChange(lastName: string) {
-		dispatch(setLastName(lastName));
-	}
-	function updateBillState(billingState: StateProvince | null) {
-		dispatch(updateBillingState(billingState));
+
+	function onLastNameChange(email: string) {
+		dispatch(setLastName(email));
 	}
 
 	return renderPersonalDetails({
@@ -78,6 +73,5 @@ export function PersonalDetailsContainer({
 		onEmailChange,
 		onFirstNameChange,
 		onLastNameChange,
-		updateBillState,
 	});
 }
