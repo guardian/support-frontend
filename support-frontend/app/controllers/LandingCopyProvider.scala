1b35d29358a85f1e26050793ba6142280f65f9d3
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2022-10-25 12:03:19 +0100

    Changes for Offer Strap Line Promotion codes

--- support-frontend/app/controllers/LandingCopyProvider.scala
+++ support-frontend/app/controllers/LandingCopyProvider.scala
@@ -4,6 +4,7 @@ import com.gu.i18n.{Country, CountryGroup}
 import com.gu.support.catalog._
 import com.gu.support.config.{Stage, TouchPointEnvironments}
 import com.gu.support.promotions.{ProductPromotionCopy, PromotionCopy, PromotionServiceProvider}
+import com.gu.support.zuora.api.ReaderType.{Direct, Gift}
 
 class LandingCopyProvider(
     promotionServiceProvider: PromotionServiceProvider,
@@ -14,7 +15,12 @@ class LandingCopyProvider(
   // To see if there is any promotional copy in place for this page we need to get a country in the current region (country group)
   // this is because promotions apply to countries not regions. We can use any country however because the promo tool UI only deals
   // with regions and then adds all the countries for that region to the promotion
-  def promotionCopy(queryPromos: List[String], product1: Product, countryCode: String): Option[PromotionCopy] =
+  def promotionCopy(
+      queryPromos: List[String],
+      product1: Product,
+      countryCode: String,
+      isGift: Boolean = false,
+  ): Option[PromotionCopy] =
     for {
       countryGroup <- CountryGroup.byId(countryCode)
       productRatePlanIds = getProductRatePlanIdsForCountryGroup(product1, countryGroup)
@@ -22,18 +28,23 @@ class LandingCopyProvider(
       promoCopy <- promotionCopyForPrimaryCountry(queryPromos, productRatePlanIds, country)
     } yield promoCopy
 
-  def getProductRatePlanIdsForCountryGroup(product: Product, countryGroup: CountryGroup) = {
+  def getProductRatePlanIdsForCountryGroup(product: Product, countryGroup: CountryGroup, isGift: Boolean = false) = {
     val environment = TouchPointEnvironments.fromStage(stage)
+    val readerType = if (isGift) Gift else Direct
     (product, countryGroup) match {
       case (GuardianWeekly, CountryGroup.RestOfTheWorld) =>
         GuardianWeekly
           .ratePlans(environment)
-          .filter(productRatePlan => productRatePlan.fulfilmentOptions == RestOfWorld)
+          .filter(productRatePlan =>
+            productRatePlan.fulfilmentOptions == RestOfWorld && productRatePlan.readerType == readerType,
+          )
           .map(_.id)
       case (GuardianWeekly, _) =>
         GuardianWeekly
           .ratePlans(environment)
-          .filter(productRatePlan => productRatePlan.fulfilmentOptions == Domestic)
+          .filter(productRatePlan =>
+            productRatePlan.fulfilmentOptions == Domestic && productRatePlan.readerType == readerType,
+          )
           .map(_.id)
       case _ => product.getProductRatePlanIds(environment)
     }
