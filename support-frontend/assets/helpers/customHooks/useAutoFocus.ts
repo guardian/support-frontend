cba747649b17a66b68963275893429799c7998a9
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-10-27 11:24:58 +0100

    improve and add doc comments to hooks

--- support-frontend/assets/helpers/customHooks/useAutoFocus.ts
+++ support-frontend/assets/helpers/customHooks/useAutoFocus.ts
@@ -1,10 +1,13 @@
 import { useCallback } from 'react';
 
-// This provides a 'callback ref' to consistently auto-focus on any node once it's mounted
-// cf https://tkdodo.eu/blog/avoiding-use-effect-with-callback-refs
+/**
+ * A hook that returns a ref which will move focus to its node on first render
+ */
 export function useAutoFocus<T extends HTMLElement | null>(): (
 	node: T | null,
 ) => void {
+	// This provides a 'callback ref'
+	// cf https://tkdodo.eu/blog/avoiding-use-effect-with-callback-refs
 	const refCallback = useCallback((node: T | null) => {
 		node?.focus();
 	}, []);
