45575dbb04039fdeb41fce0bb289642ed34179ef
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-11-03 14:00:57 +0000

    update default cta copy

--- support-frontend/assets/components/paymentButton/defaultPaymentButtonContainer.tsx
+++ support-frontend/assets/components/paymentButton/defaultPaymentButtonContainer.tsx
@@ -4,6 +4,7 @@ import { currencies } from 'helpers/internationalisation/currency';
 import { getContributionType } from 'helpers/redux/checkout/product/selectors/productType';
 import { getUserSelectedAmount } from 'helpers/redux/checkout/product/selectors/selectedAmount';
 import { useContributionsSelector } from 'helpers/redux/storeHooks';
+import { shouldShowBenefitsMessaging } from 'pages/contributions-landing/components/DigiSubBenefits/helpers';
 import { DefaultPaymentButton } from './defaultPaymentButton';
 
 const contributionTypeToPaymentInterval: Partial<
@@ -19,10 +20,13 @@ export type DefaultPaymentContainerProps = {
 
 function getButtonText(
 	amountWithCurrency: string,
+	amountIsAboveThreshold: boolean,
 	paymentInterval?: 'month' | 'year',
 ) {
 	if (paymentInterval) {
-		return `Support us with ${amountWithCurrency} per ${paymentInterval}`;
+		return `${
+			amountIsAboveThreshold ? 'Pay' : 'Support us with'
+		} ${amountWithCurrency} per ${paymentInterval}`;
 	}
 
 	return `Support us with ${amountWithCurrency}`;
@@ -37,16 +41,31 @@ export function DefaultPaymentButtonContainer({
 	const paymentWaiting = useContributionsSelector(
 		(state) => state.page.form.isWaiting,
 	);
+	const { selectedAmounts, otherAmounts } = useContributionsSelector(
+		(state) => state.page.checkoutForm.product,
+	);
 	const selectedAmount = useContributionsSelector(getUserSelectedAmount);
 	const contributionType = useContributionsSelector(getContributionType);
 
 	const currency = currencies[currencyId];
 	const amountWithCurrency = simpleFormatAmount(currency, selectedAmount);
 
+	const { countryGroupId } = useContributionsSelector(
+		(state) => state.common.internationalisation,
+	);
+
+	const amountIsAboveThreshold = shouldShowBenefitsMessaging(
+		contributionType,
+		selectedAmounts,
+		otherAmounts,
+		countryGroupId,
+	);
+
 	const buttonText = Number.isNaN(selectedAmount)
 		? 'Pay now'
 		: getButtonText(
 				amountWithCurrency,
+				amountIsAboveThreshold,
 				contributionTypeToPaymentInterval[contributionType],
 		  );
 
