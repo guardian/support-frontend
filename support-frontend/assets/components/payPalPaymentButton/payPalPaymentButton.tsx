8028029827a3f56f5b50b221b49d48f20e88b0fb
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-11-03 11:53:49 +0000

    run validation on button render

--- support-frontend/assets/components/payPalPaymentButton/payPalPaymentButton.tsx
+++ support-frontend/assets/components/payPalPaymentButton/payPalPaymentButton.tsx
@@ -1,18 +1,28 @@
+import { useEffect } from 'preact/hooks';
 import AnimatedDots from 'components/spinners/animatedDots';
 import { usePayPal } from 'helpers/customHooks/usePayPal';
+import { validateForm } from 'helpers/redux/checkout/checkoutActions';
 import { getContributionType } from 'helpers/redux/checkout/product/selectors/productType';
 import { contributionsFormHasErrors } from 'helpers/redux/selectors/formValidation';
-import { useContributionsSelector } from 'helpers/redux/storeHooks';
+import {
+	useContributionsDispatch,
+	useContributionsSelector,
+} from 'helpers/redux/storeHooks';
 import { PayPalButtonOneOffContainer } from './payPalOneOffContainer';
 import { PayPalButtonRecurringContainer } from './payPalRecurringContainer';
 
 export function PayPalPaymentButton(): JSX.Element {
 	const payPalHasLoaded = usePayPal();
 
+	const dispatch = useContributionsDispatch();
 	const contributionType = useContributionsSelector(getContributionType);
 	const errorsPreventSubmission = useContributionsSelector(
 		contributionsFormHasErrors,
 	);
+
+	useEffect(() => {
+		dispatch(validateForm('PayPal'));
+	}, []);
 
 	if (!payPalHasLoaded) {
 		return <AnimatedDots appearance="dark" />;
