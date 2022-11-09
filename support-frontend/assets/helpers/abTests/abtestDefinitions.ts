97a39ef09ab9bd5291ab72c50bd792c05b8ef42b
George Haberis <george.haberis@guardian.co.uk>
2022-11-09 11:20:58 +0000

    Scale supporterPlus test audience up

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -164,9 +164,9 @@ export const tests: Tests = {
 		audiences: {
 			ALL: {
 				offset: 0,
-				// This will allocate 2% of the whole audience into the test *at all*,
-				// with half of that ie. 1% seeing the variant
-				size: 0.02,
+				// This will allocate 20% of the whole audience into the test *at all*,
+				// with half of that ie. 10% seeing the variant
+				size: 0.2,
 			},
 		},
 		isActive: true,
