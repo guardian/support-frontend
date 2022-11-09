0b96aa3dea6c132280cf7774ae77b9efb39f41ad
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-11-09 17:53:06 +0000

    revert supporter plus landing story

--- support-frontend/stories/screens/SupporterPlusLanding.stories.tsx
+++ support-frontend/stories/screens/SupporterPlusLanding.stories.tsx
@@ -2,8 +2,6 @@ import '__mocks__/settingsMock';
 import { Provider } from 'react-redux';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { createTestStoreForContributions } from '__test-utils__/testStore';
-import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
-import { countryGroups } from 'helpers/internationalisation/countryGroup';
 import { setCountryInternationalisation } from 'helpers/redux/commonState/actions';
 import { init as formInit } from 'pages/contributions-landing/contributionsLandingInit';
 import { SupporterPlusLandingPage } from 'pages/supporter-plus-landing/supporterPlusLanding';
@@ -24,44 +22,8 @@ formInit(store);
 export default {
 	component: SupporterPlusLandingPage,
 	title: 'Screens/Supporter Plus Landing Page',
-	argTypes: {
-		countryGroup: {
-			options: (Object.keys(countryGroups) as CountryGroupId[]).filter(
-				(countryGroup) => countryGroup !== 'AUDCountries',
-			),
-			control: { type: 'radio' },
-			if: { arg: 'countryGroup', exists: true },
-		},
-	},
-};
-
-function Template() {
-	return <SupporterPlusLandingPage thankYouRoute={'/uk/thankyou'} />;
-}
-
-Template.args = {} as Record<string, unknown>;
-Template.decorators = [] as unknown[];
-
-export const Default = Template.bind({});
-
-Default.args = {
-	countryGroup: 'GBPCountries',
-};
-
-Default.decorators = [
-	(
-		Story: React.FC,
-		{ args }: Record<string, { countryGroup: CountryGroupId }>,
-	): JSX.Element => {
-		const { countryGroup } = args;
-
-		const store = createTestStoreForContributions();
-
-		store.dispatch(
-			setCountryInternationalisation(countryGroups[countryGroup].countries[0]),
-		);
-
-		return (
+	decorators: [
+		(Story: React.FC): JSX.Element => (
 			<Provider store={store}>
 				<MemoryRouter>
 					<Routes>
@@ -69,6 +31,12 @@ Default.decorators = [
 					</Routes>
 				</MemoryRouter>
 			</Provider>
-		);
-	},
-];
+		),
+	],
+};
+
+function Template() {
+	return <SupporterPlusLandingPage thankYouRoute={'/uk/thankyou'} />;
+}
+
+export const Default = Template.bind({});
