74d907fd8f5312858ec66a7bb93144ccc3079eaf
Paul Heron <55701358+worldofpablo@users.noreply.github.com>
2022-11-10 14:33:45 +0000

    Teenyweeny copy tweak

--- support-frontend/assets/pages/contributions-landing/components/ContributionFormContainer.tsx
+++ support-frontend/assets/pages/contributions-landing/components/ContributionFormContainer.tsx
@@ -164,7 +164,7 @@ const defaultHeaderCopy = 'Support\xa0fearless,\nindependent\njournalism';
 const defaultContributeCopy = (
 	<span>
 		As a reader-funded news organisation, we rely on your generosity. Please
-		give what you can, so millions more can benefit from quality reporting on
+		give what you can, so millions can benefit from quality reporting on
 		the events shaping our world.
 	</span>
 );
