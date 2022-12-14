a624c785d114562141b538a29b84d6f8664e3820
Imogen Hardy <imogen.hardy@guardian.co.uk>
2022-12-14 13:30:00 +0000

    remove ab test and all references to it

--- support-frontend/assets/pages/contributions-landing/newProductTestHelper.ts
+++ support-frontend/assets/pages/contributions-landing/newProductTestHelper.ts
@@ -4,10 +4,6 @@ import type { ContributionsState } from '../../helpers/redux/contributionsStore'
 import { getThresholdPrice } from './components/DigiSubBenefits/helpers';
 
 export function isSupporterPlusPurchase(state: ContributionsState): boolean {
-	if (state.common.abParticipations.supporterPlus != 'variant') {
-		return false;
-	}
-
 	const contributionType = getContributionType(state);
 
 	const thresholdPrice = getThresholdPrice(
