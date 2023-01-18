435c377753333d1d5ad4445faa4dfb4888611dc5
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-01-18 15:54:24 +0000

    Reset

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -133,7 +133,7 @@ export const tests: Tests = {
 				size: 1,
 			},
 		},
-		isActive: true,
+		isActive: false,
 		referrerControlled: false,
 		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
 		seed: 15,
