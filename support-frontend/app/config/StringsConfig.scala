ad43468998855d3983acd87b607f34ec9abbe525
Emily Bourke <emily.bourke@guardian.co.uk>
2023-01-19 15:00:39 +0000

    Remove showcase page (server side)

--- support-frontend/app/config/StringsConfig.scala
+++ support-frontend/app/config/StringsConfig.scala
@@ -6,7 +6,6 @@ import config.ConfigImplicits._
 class StringsConfig {
   val config = ConfigFactory.load("strings.conf")
 
-  val showcaseLandingDescription = config.getOptionalString("showcaseLanding.description")
   val contributionsLandingDescription = config.getOptionalString("contributionsLanding.description")
   val subscriptionsLandingDescription = config.getOptionalString("subscriptionsLanding.description")
   val digitalPackLandingDescription = config.getOptionalString("digitalPackLanding.description")
