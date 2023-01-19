a517008b21825ff0b6f0502a012213b68fdee4b6
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2023-01-19 10:26:24 +0000

    Clear State property in Redux on Country Change

--- support-frontend/assets/helpers/redux/checkout/address/reducer.ts
+++ support-frontend/assets/helpers/redux/checkout/address/reducer.ts
@@ -78,6 +78,7 @@ function getAddressFieldsSlice(type: AddressType) {
 
 				if (country) {
 					state.country = country;
+					state.state = getInitialAddressFieldsState().state;
 				}
 			},
 			setFormErrors(state, action: PayloadAction<AddressFormFieldError[]>) {
