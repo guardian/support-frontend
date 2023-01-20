7a29e2a6e9fa192d748ff25e8340c3d0dbcc2db1
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2023-01-20 14:31:12 +0000

    Removed the validate postcode changes

--- support-frontend/app/utils/CheckoutValidationRules.scala
+++ support-frontend/app/utils/CheckoutValidationRules.scala
@@ -32,9 +32,9 @@ object CheckoutValidationRules {
   case class Invalid(message: String) extends Result
   case object Valid extends Result
   def checkSubscriptionPaymentMethodEnabled(
-                                             switches: SubscriptionsPaymentMethodSwitches,
-                                             paymentFields: Either[PaymentFields, RedemptionData],
-                                           ) = paymentFields match {
+      switches: SubscriptionsPaymentMethodSwitches,
+      paymentFields: Either[PaymentFields, RedemptionData],
+  ) = paymentFields match {
     case Left(_: PayPalPaymentFields) =>
       if (switches.paypal.isOn) Valid else Invalid("Invalid Payment Method")
     case Left(_: DirectDebitPaymentFields) =>
@@ -46,9 +46,9 @@ object CheckoutValidationRules {
   }
 
   def checkContributionPaymentMethodEnabled(
-                                             switches: RecurringPaymentMethodSwitches,
-                                             paymentFields: Either[PaymentFields, RedemptionData],
-                                           ) = paymentFields match {
+      switches: RecurringPaymentMethodSwitches,
+      paymentFields: Either[PaymentFields, RedemptionData],
+  ) = paymentFields match {
     case Left(_: PayPalPaymentFields) =>
       if (switches.payPal.isOn) Valid else Invalid("Invalid Payment Method")
     case Left(_: DirectDebitPaymentFields) =>
@@ -77,10 +77,10 @@ object CheckoutValidationRules {
 
   }
   def checkPaymentMethodEnabled(
-                                 product: ProductType,
-                                 paymentFields: Either[PaymentFields, RedemptionData],
-                                 switches: Switches,
-                               ) =
+      product: ProductType,
+      paymentFields: Either[PaymentFields, RedemptionData],
+      switches: Switches,
+  ) =
     product match {
       case _: Contribution | _: SupporterPlus =>
         checkContributionPaymentMethodEnabled(
@@ -181,22 +181,12 @@ object AddressAndCurrencyValidationRules {
       stateFromRequest.isDefined.otherwise(s"state is required for $countryFromRequest")
     } else Valid
 
-  //    hasValidPostcodeLength checks if the length of postCodeIsShortEnoughForSalesforce(must be less than or equal to 20 characters)
-  def hasValidPostcodeLength(postcodeFromRequest: Option[String], addressType: String): Result = {
-    val validPostCode = postcodeFromRequest match {
-      case Some(postCode) if (postCode.length > 20) =>
-        Invalid(s"$addressType PostCode length must not be greater than 20 characters")
-      case _ => Valid
-    }
-    validPostCode
-  }
-
   def hasPostcodeIfRequired(countryFromRequest: Country, postcodeFromRequest: Option[String]): Result =
     if (
       countryFromRequest == Country.UK ||
-        countryFromRequest == Country.US ||
-        countryFromRequest == Country.Canada ||
-        countryFromRequest == Country.Australia
+      countryFromRequest == Country.US ||
+      countryFromRequest == Country.Canada ||
+      countryFromRequest == Country.Australia
     ) {
       postcodeFromRequest.isDefined.otherwise(s"postcode is required for $countryFromRequest")
     } else Valid
@@ -234,7 +224,6 @@ object DigitalPackValidation {
         hasStateIfRequired(country, state) and
         hasAddressLine1AndCity(billingAddress) and
         hasPostcodeIfRequired(country, postCode) and
-        hasValidPostcodeLength(postCode, "Billing") and
         currencyIsSupportedForCountry(country, currency) and
         PaidProductValidation.noEmptyPaymentFields(paymentFields)
 
@@ -278,11 +267,7 @@ object GuardianWeeklyValidation {
         PaidProductValidation.passes(createSupportWorkersRequest) and
           createSupportWorkersRequest.firstDeliveryDate.nonEmpty.otherwise("first delivery date is required") and
           hasAddressLine1AndCity(createSupportWorkersRequest.billingAddress) and
-          hasAddressLine1AndCity(address) and
-          hasValidPostcodeLength(address.postCode, "Delivery") and hasValidPostcodeLength(
-          createSupportWorkersRequest.billingAddress.postCode,
-          "Billing",
-        )
+          hasAddressLine1AndCity(address)
       case None => Invalid("missing delivery address")
     }
 
@@ -315,10 +300,7 @@ object PaperValidation {
         createSupportWorkersRequest.firstDeliveryDate.nonEmpty.otherwise("first delivery date is missing") and
         hasAddressLine1AndCity(createSupportWorkersRequest.billingAddress) and
         deliveryAddressHasAddressLine1AndCity and
-        validPostcode and hasValidPostcodeLength(address.postCode, "Delivery") and hasValidPostcodeLength(
-        createSupportWorkersRequest.billingAddress.postCode,
-        "Billing",
-      )
+        validPostcode
 
     }
 
