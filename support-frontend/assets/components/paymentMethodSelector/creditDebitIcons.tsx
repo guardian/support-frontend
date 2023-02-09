9dd28c35eb499a3285319e3b0f3523e87db0821d
Maria Olanipekun <m.olanipekun@hotmail.com>
2023-02-09 14:33:55 +0000

    add themed credit card icon component

--- support-frontend/assets/components/paymentMethodSelector/creditDebitIcons.tsx
+++ support-frontend/assets/components/paymentMethodSelector/creditDebitIcons.tsx
@@ -1,11 +1,9 @@
 import { css } from '@emotion/react';
-import { from } from '@guardian/source-foundations';
+import { brand, from } from '@guardian/source-foundations';
+import type { IconSize } from '@guardian/source-react-components';
 import { SvgCreditCard } from '@guardian/source-react-components';
 
 const creditCardIcon = css`
-	display: flex;
-	width: 100%;
-
 	${from.mobileMedium} {
 		display: none;
 	}
@@ -20,11 +18,34 @@ const brandedIcons = css`
 	}
 `;
 
+export function SvgCreditCardWithTheme({
+	color = brand[400],
+	size = 'medium',
+}: {
+	color?: string;
+	size?: IconSize;
+}): JSX.Element {
+	const creditCard = css`
+		display: flex;
+		width: 100%;
+
+		& svg {
+			fill: ${color};
+		}
+	`;
+
+	return (
+		<div css={creditCard}>
+			<SvgCreditCard size={size} />
+		</div>
+	);
+}
+
 export function CreditDebitIcons(): JSX.Element {
 	return (
 		<>
 			<div css={creditCardIcon}>
-				<SvgCreditCard size="medium" />
+				<SvgCreditCardWithTheme />
 			</div>
 
 			<div css={brandedIcons}>
