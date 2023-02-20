e50fe4af82c6d48cd2534e71e7b1153fea9a701f
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-02-20 17:14:20 +0000

    function containing each (anuual,monthly) copy changes

--- support-frontend/assets/components/checkoutNudge/checkoutNudgeContainer.tsx
+++ support-frontend/assets/components/checkoutNudge/checkoutNudgeContainer.tsx
@@ -41,12 +41,34 @@ export function CheckoutNudgeContainer({
 			: currencyGlyph +
 			  (Math.ceil((minAmount * 100) / 52) / 100).toFixed(2).toString();
 
-	const title2CopyMonthly = `Support us every month`;
-	const paragraphCopyMonthly = `Regular, reliable support powers Guardian journalism in perpetuity. If you can, please consider setting up a ${recurringType.toLowerCase()} payment today from just  ${currencyGlyph}${minAmount} – it takes less than a minute.`;
+	const [titleNudge, title2Nudge, paragraphNudge] = copyNudge(
+		recurringType,
+		currencyGlyph + minAmount.toString(),
+		minWeeklyAmount,
+	);
 
-	const title2CopyAnnual = `from just ${currencyGlyph}${minAmount} (${minWeeklyAmount} a week)`;
-	const paragraphCopyAnnual = `Funding Guardian journalism every year doesn’t need to be expensive. Make a bigger impact today, and protect our independence long term. Please consider annual support.`;
-
+	function copyNudge(
+		recurringType: string,
+		minAmount: string,
+		minWeeklyAmount: string,
+	): string[] {
+		switch (recurringType) {
+			case 'ANNUAL': {
+				return [
+					`Support us every year`,
+					`from just ${minAmount} (${minWeeklyAmount} a week)`,
+					`Funding Guardian journalism every year doesn’t need to be expensive. Make a bigger impact today, and protect our independence long term. Please consider annual support.`,
+				];
+			}
+			default: {
+				return [
+					`Make a bigger impact`,
+					`Support us every month`,
+					`Regular, reliable support powers Guardian journalism in perpetuity. If you can, please consider setting up a ${recurringType.toLowerCase()} payment today from just  ${minAmount} – it takes less than a minute.`,
+				];
+			}
+		}
+	}
 	function onNudgeClose() {
 		setDisplayNudge(false);
 	}
@@ -57,14 +79,9 @@ export function CheckoutNudgeContainer({
 	return renderNudge({
 		contributionType,
 		nudgeDisplay: displayNudge,
-		nudgeTitleCopySection1:
-			recurringType === 'MONTHLY'
-				? 'Make a bigger impact'
-				: 'Support us every year',
-		nudgeTitleCopySection2:
-			recurringType === 'MONTHLY' ? title2CopyMonthly : title2CopyAnnual,
-		nudgeParagraphCopy:
-			recurringType === 'MONTHLY' ? paragraphCopyMonthly : paragraphCopyAnnual,
+		nudgeTitleCopySection1: titleNudge,
+		nudgeTitleCopySection2: title2Nudge,
+		nudgeParagraphCopy: paragraphNudge,
 		nudgeLinkCopy: `See ${recurringType.toLowerCase()}`,
 		recurringType: recurringType,
 		countryGroupId: countryGroupId,
