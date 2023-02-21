20f3c89b71470ec28c9520aa45f8895899a4e2d6
Maria Olanipekun <m.olanipekun@hotmail.com>
2023-02-21 23:51:06 +0000

    remove unused variables and components

--- support-frontend/assets/components/subscriptionCheckouts/personalDetailsGift.tsx
+++ support-frontend/assets/components/subscriptionCheckouts/personalDetailsGift.tsx
@@ -57,41 +57,4 @@ function PersonalDetailsGift(props: PropTypes): JSX.Element {
 	);
 }
 
-function PersonalDetailsDigitalGift(props: PropTypes): JSX.Element {
-	return (
-		<div>
-			<TextInput
-				css={marginBotom}
-				id="firstNameGiftRecipient"
-				data-qm-masking="blocklist"
-				label="First name"
-				type="text"
-				value={props.firstNameGiftRecipient}
-				onChange={(e) => props.setFirstNameGift(e.target.value)}
-				error={firstError('firstNameGiftRecipient', props.formErrors)}
-			/>
-			<TextInput
-				css={marginBotom}
-				id="lastNameGiftRecipient"
-				data-qm-masking="blocklist"
-				label="Last name"
-				type="text"
-				value={props.lastNameGiftRecipient}
-				onChange={(e) => props.setLastNameGift(e.target.value)}
-				error={firstError('lastNameGiftRecipient', props.formErrors)}
-			/>
-			<TextInput
-				css={marginBotom}
-				id="emailGiftRecipient"
-				data-qm-masking="blocklist"
-				label="Email"
-				type="email"
-				onChange={(e) => props.setEmailGift(e.target.value)}
-				value={props.emailGiftRecipient}
-				error={firstError('emailGiftRecipient', props.formErrors)}
-			/>
-		</div>
-	);
-}
-
-export { PersonalDetailsGift, PersonalDetailsDigitalGift };
+export { PersonalDetailsGift };
