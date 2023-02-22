96456a54c67f5873c9ed0ed8628a2a19399fd54b
Maria Olanipekun <m.olanipekun@hotmail.com>
2023-02-22 08:40:53 +0000

    remove DigitalPackTerms component

--- support-frontend/assets/pages/promotion-terms/legalTerms.tsx
+++ support-frontend/assets/pages/promotion-terms/legalTerms.tsx
@@ -1,10 +1,6 @@
 import Content, { Divider } from 'components/content/content';
 import { Title } from 'components/text/text';
-import {
-	DigitalPack,
-	GuardianWeekly,
-} from 'helpers/productPrice/subscriptions';
-import DigitalPackTerms from 'pages/promotion-terms/DigitalPackTerms';
+import { GuardianWeekly } from 'helpers/productPrice/subscriptions';
 import PaperTerms from 'pages/promotion-terms/PaperTerms';
 import type { PromotionTermsPropTypes } from 'pages/promotion-terms/promotionTermsReducer';
 import WeeklyTerms from 'pages/promotion-terms/weeklyTerms';
@@ -14,20 +10,14 @@ const getTermsForProduct = (props: PromotionTermsPropTypes) => {
 		case GuardianWeekly:
 			return <WeeklyTerms {...props} />;
 
-		case DigitalPack:
-			return (
-				<DigitalPackTerms
-					{...props.promotionTerms}
-					countryGroupId={props.countryGroupId}
-				/>
-			);
-
 		default:
 			return <PaperTerms {...props.promotionTerms} />;
 	}
 };
 
-export default function LegalTerms(props: PromotionTermsPropTypes) {
+export default function LegalTerms(
+	props: PromotionTermsPropTypes,
+): JSX.Element {
 	return (
 		<Content>
 			<Divider />
