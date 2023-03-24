1420d23cb641ca8f5c65d2d3d217a4e6b35ef7dc
George Haberis <george.haberis@guardian.co.uk>
2023-03-24 10:56:01 +0000

    add qm blocklist data atrribute to existing payment

--- support-frontend/assets/components/paymentMethodSelector/paymentMethodSelector.tsx
+++ support-frontend/assets/components/paymentMethodSelector/paymentMethodSelector.tsx
@@ -127,7 +127,7 @@ export function PaymentMethodSelector({
 
 										return (
 											<AvailablePaymentMethodAccordionRow
-												data-qm-masking="blocklist"
+												addQuantumMetricBlockListAttribute={true}
 												id={`paymentMethod-existing${preExistingPaymentMethod.billingAccountId}`}
 												name="paymentMethod"
 												label={getExistingPaymentMethodLabel(
