ec8c85888c0ad6d8238f93dab7e12aef54196887
Tom Forbes <tom.forbes@theguardian.com>
2023-04-26 15:30:47 +0100

    lint

--- cdk/lib/bigquery-acquisitions-publisher.ts
+++ cdk/lib/bigquery-acquisitions-publisher.ts
@@ -1,9 +1,10 @@
-import {GuStack, GuStackProps} from '@guardian/cdk/lib/constructs/core';
-import {App} from 'aws-cdk-lib';
+import type { GuStackProps} from '@guardian/cdk/lib/constructs/core';
+import {GuStack} from '@guardian/cdk/lib/constructs/core';
 import {GuLambdaFunction} from '@guardian/cdk/lib/constructs/lambda';
+import type {App} from 'aws-cdk-lib';
 import {Runtime} from 'aws-cdk-lib/aws-lambda';
-import {Queue} from "aws-cdk-lib/aws-sqs";
 import {SqsEventSource} from "aws-cdk-lib/aws-lambda-event-sources";
+import {Queue} from "aws-cdk-lib/aws-sqs";
 
 const appName = 'BigqueryAcquisitionsPublisher';
 
