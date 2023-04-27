4e4abf6d56853fc1f8a3fc0ef2cfe2dbb2c2a502
Tom Forbes <tom.forbes@theguardian.com>
2023-04-27 10:52:38 +0100

    ssm perm

--- cdk/lib/bigquery-acquisitions-publisher.ts
+++ cdk/lib/bigquery-acquisitions-publisher.ts
@@ -2,6 +2,7 @@ import type { GuStackProps} from '@guardian/cdk/lib/constructs/core';
 import {GuStack} from '@guardian/cdk/lib/constructs/core';
 import {GuLambdaFunction} from '@guardian/cdk/lib/constructs/lambda';
 import type {App} from 'aws-cdk-lib';
+import {PolicyStatement} from "aws-cdk-lib/aws-iam";
 import {Runtime} from 'aws-cdk-lib/aws-lambda';
 import {SqsEventSource} from "aws-cdk-lib/aws-lambda-event-sources";
 import {Queue} from "aws-cdk-lib/aws-sqs";
@@ -24,6 +25,14 @@ export class BigqueryAcquisitionsPublisher extends GuStack {
       functionName: `${appName}-${props.stage}`,
       handler: 'com.gu.bigqueryAcquisitionsPublisher.Lambda::handler',
       events: [eventSource],
+      initialPolicy: [
+        new PolicyStatement({
+          actions: ["ssm:GetParameter"],
+          resources: [
+            `arn:aws:ssm:${this.region}:${this.account}:parameter/${appName}/${props.stage}/gcp-wif-credentials-config`,
+          ],
+        })
+      ]
     });
   }
 }
