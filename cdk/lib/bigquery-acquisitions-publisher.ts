2cf7e549d2d5f71135538c2c9c8d707f69ba6137
Tom Forbes <tom.forbes@theguardian.com>
2023-04-27 11:02:32 +0100

    2mins

--- cdk/lib/bigquery-acquisitions-publisher.ts
+++ cdk/lib/bigquery-acquisitions-publisher.ts
@@ -2,6 +2,7 @@ import type { GuStackProps} from '@guardian/cdk/lib/constructs/core';
 import {GuStack} from '@guardian/cdk/lib/constructs/core';
 import {GuLambdaFunction} from '@guardian/cdk/lib/constructs/lambda';
 import type {App} from 'aws-cdk-lib';
+import {Duration} from "aws-cdk-lib";
 import {PolicyStatement} from "aws-cdk-lib/aws-iam";
 import {Runtime} from 'aws-cdk-lib/aws-lambda';
 import {SqsEventSource} from "aws-cdk-lib/aws-lambda-event-sources";
@@ -25,6 +26,7 @@ export class BigqueryAcquisitionsPublisher extends GuStack {
       functionName: `${appName}-${props.stage}`,
       handler: 'com.gu.bigqueryAcquisitionsPublisher.Lambda::handler',
       events: [eventSource],
+      timeout: Duration.minutes(2),
       initialPolicy: [
         new PolicyStatement({
           actions: ["ssm:GetParameter"],
