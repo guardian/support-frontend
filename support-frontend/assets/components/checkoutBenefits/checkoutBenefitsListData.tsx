5bf754381a23b41348f1e4b52e44a0b6591048b3
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2023-05-05 11:14:42 +0100

    Small refactoring

--- support-frontend/assets/components/checkoutBenefits/checkoutBenefitsListData.tsx
+++ support-frontend/assets/components/checkoutBenefits/checkoutBenefitsListData.tsx
@@ -51,110 +51,114 @@ export const checkListData = ({
 }: CheckListDataProps): CheckListData[] => {
 	const maybeGreyedOutHigherTier = higherTier ? undefined : greyedOut;
 
+	const emotionalBenefits = [
+		{
+			icon: getSvgIcon(true),
+			text: (
+				<p>
+					<span css={boldText}>Fund fearless, independent reporting </span>
+					that's open and free for everyone
+				</p>
+			),
+		},
+		{
+			icon: getSvgIcon(true),
+			text: (
+				<p>
+					<span css={boldText}>Get a regular supporter newsletter </span> with
+					exclusive insights from our newsroom
+				</p>
+			),
+		},
+		{
+			icon: getSvgIcon(true),
+			text: (
+				<p>
+					<span css={boldText}>See fewer asks for support </span>so you can read
+					uninterrupted
+				</p>
+			),
+		},
+		{
+			icon: getSvgIcon(higherTier),
+			text: (
+				<p>
+					<span css={boldText}>Gain full access to our news app </span>to read
+					our reporting on the go
+				</p>
+			),
+			maybeGreyedOut: maybeGreyedOutHigherTier,
+		},
+		{
+			icon: getSvgIcon(higherTier),
+			text: (
+				<p>
+					<span css={boldText}>Read ad-free </span>on all devices
+				</p>
+			),
+			maybeGreyedOut: maybeGreyedOutHigherTier,
+		},
+	];
+
+	const existingBenefits = [
+		{
+			icon: getSvgIcon(true),
+			text: (
+				<p>
+					<span css={boldText}>A regular supporter newsletter. </span>Get
+					exclusive insight from our newsroom
+				</p>
+			),
+		},
+		{
+			icon: getSvgIcon(true),
+			text: (
+				<p>
+					<span css={boldText}>Uninterrupted reading. </span> See far fewer asks
+					for support
+				</p>
+			),
+		},
+		...(isAustralia
+			? [
+					{
+						icon: getSvgIcon(higherTier),
+						text: (
+							<p>
+								<span css={boldText}>
+									Limited-edition Guardian Australia tote bag.{' '}
+								</span>
+								<span css={highlightTextSpacing} />
+								<span css={highlightText}>Offer ends 31 May</span>
+							</p>
+						),
+						maybeGreyedOut: maybeGreyedOutHigherTier,
+					},
+			  ]
+			: []),
+		{
+			icon: getSvgIcon(higherTier),
+			text: (
+				<p>
+					<span css={boldText}>Full access to our news app. </span>Read our
+					reporting on the go
+				</p>
+			),
+			maybeGreyedOut: maybeGreyedOutHigherTier,
+		},
+		{
+			icon: getSvgIcon(higherTier),
+			text: (
+				<p>
+					<span css={boldText}>Ad-free reading. </span>Avoid ads on all your
+					devices
+				</p>
+			),
+			maybeGreyedOut: maybeGreyedOutHigherTier,
+		},
+	];
+
 	return isEmotionalBenefitTestVariant && !isAustralia
-		? [
-				{
-					icon: getSvgIcon(true),
-					text: (
-						<p>
-							<span css={boldText}>Fund fearless, independent reporting </span>
-							that's open and free for everyone
-						</p>
-					),
-				},
-				{
-					icon: getSvgIcon(true),
-					text: (
-						<p>
-							<span css={boldText}>Get a regular supporter newsletter </span>{' '}
-							with exclusive insights from our newsroom
-						</p>
-					),
-				},
-				{
-					icon: getSvgIcon(true),
-					text: (
-						<p>
-							<span css={boldText}>See fewer asks for support </span>so you can
-							read uninterrupted
-						</p>
-					),
-				},
-				{
-					icon: getSvgIcon(higherTier),
-					text: (
-						<p>
-							<span css={boldText}>Gain full access to our news app </span>to
-							read our reporting on the go
-						</p>
-					),
-					maybeGreyedOut: maybeGreyedOutHigherTier,
-				},
-				{
-					icon: getSvgIcon(higherTier),
-					text: (
-						<p>
-							<span css={boldText}>Read ad-free </span>on all devices
-						</p>
-					),
-					maybeGreyedOut: maybeGreyedOutHigherTier,
-				},
-		  ]
-		: [
-				{
-					icon: getSvgIcon(true),
-					text: (
-						<p>
-							<span css={boldText}>A regular supporter newsletter. </span>Get
-							exclusive insight from our newsroom
-						</p>
-					),
-				},
-				{
-					icon: getSvgIcon(true),
-					text: (
-						<p>
-							<span css={boldText}>Uninterrupted reading. </span> See far fewer
-							asks for support
-						</p>
-					),
-				},
-				...(isAustralia
-					? [
-							{
-								icon: getSvgIcon(higherTier),
-								text: (
-									<p>
-										<span css={boldText}>
-											Limited-edition Guardian Australia tote bag.{' '}
-										</span>
-										<span css={highlightTextSpacing} />
-										<span css={highlightText}>Offer ends 31 May</span>
-									</p>
-								),
-								maybeGreyedOut: maybeGreyedOutHigherTier,
-							},
-					  ]
-					: []),
-				{
-					icon: getSvgIcon(higherTier),
-					text: (
-						<p>
-							<span css={boldText}>Full access to our news app. </span>Read our
-							reporting on the go
-						</p>
-					),
-					maybeGreyedOut: maybeGreyedOutHigherTier,
-				},
-				{
-					icon: getSvgIcon(higherTier),
-					text: (
-						<p>
-							<span css={boldText}>Ad-free reading. </span>Avoid ads on all your
-							devices
-						</p>
-					),
-					maybeGreyedOut: maybeGreyedOutHigherTier,
-				},
-		  ];
+		? emotionalBenefits
+		: existingBenefits;
 };
