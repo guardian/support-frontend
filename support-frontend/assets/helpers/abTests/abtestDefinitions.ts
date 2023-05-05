417a7de7f90e73a8a61c2956218cca4d40c3c101
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-05-05 16:05:39 +0100

    abtest supporterPlusMobileTest1 shown by default

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -120,28 +120,4 @@ export const tests: Tests = {
 		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
 		seed: 11,
 	},
-	supporterPlusMobileTest1: {
-		variants: [
-			{
-				id: 'control',
-			},
-			{
-				id: 'variant',
-			},
-		],
-		audiences: {
-			ALL: {
-				offset: 0,
-				size: 1,
-				breakpoint: {
-					minWidth: 'mobile',
-					maxWidth: 'tablet',
-				},
-			},
-		},
-		isActive: true,
-		referrerControlled: false,
-		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
-		seed: 12,
-	},
 };
--- support-frontend/assets/pages/supporter-plus-landing/supporterPlusLanding.tsx
+++ support-frontend/assets/pages/supporter-plus-landing/supporterPlusLanding.tsx
@@ -50,10 +50,7 @@ import { setPaymentMethod } from 'helpers/redux/checkout/payment/paymentMethod/a
 import { setProductType } from 'helpers/redux/checkout/product/actions';
 import { getContributionType } from 'helpers/redux/checkout/product/selectors/productType';
 import { getUserSelectedAmount } from 'helpers/redux/checkout/product/selectors/selectedAmount';
-import {
-	isUserInAbVariant,
-	isUserInAnyAbVariant,
-} from 'helpers/redux/commonState/selectors';
+import { isUserInAbVariant } from 'helpers/redux/commonState/selectors';
 import {
 	useContributionsDispatch,
 	useContributionsSelector,
@@ -83,12 +80,8 @@ const checkoutContainer = css`
 	}
 `;
 
-const backgroundContainer = css`
-	background-color: ${neutral[97]};
-`;
-
 const darkBackgroundContainerMobile = css`
-	${backgroundContainer}
+	background-color: ${neutral[97]};
 	${until.tablet} {
 		background-color: ${brand[400]};
 		border-bottom: 1px solid ${brand[600]};
@@ -130,13 +123,6 @@ export function SupporterPlusLandingPage({
 		selectedAmounts,
 		otherAmounts,
 		countryGroupId,
-	);
-
-	const optimisedMobileLayout = useContributionsSelector(
-		isUserInAnyAbVariant(
-			['supporterPlusMobileTest1', 'singleLessProminent'],
-			'variant',
-		),
 	);
 
 	const { paymentComplete, isWaiting } = useContributionsSelector(
@@ -228,33 +214,20 @@ export function SupporterPlusLandingPage({
 					reporting on the events shaping our world.
 				</p>
 			</CheckoutHeading>
-			<Container
-				sideBorders
-				cssOverrides={
-					optimisedMobileLayout
-						? darkBackgroundContainerMobile
-						: backgroundContainer
-				}
-			>
+			<Container sideBorders cssOverrides={darkBackgroundContainerMobile}>
 				<Columns cssOverrides={checkoutContainer} collapseUntil="tablet">
 					<Column span={[0, 2, 5]}></Column>
 					<Column span={[1, 8, 7]}>
 						<Hide from="desktop">
-							{optimisedMobileLayout ? (
-								<SecureTransactionIndicator
-									align="left"
-									theme="light"
-									cssOverrides={css`
-										margin-bottom: 10px;
-									`}
-								/>
-							) : (
-								heading
-							)}
+							<SecureTransactionIndicator
+								align="left"
+								theme="light"
+								cssOverrides={css`
+									margin-bottom: 10px;
+								`}
+							/>
 						</Hide>
-						<Box
-							cssOverrides={optimisedMobileLayout ? shorterBoxMargin : css``}
-						>
+						<Box cssOverrides={shorterBoxMargin}>
 							<AmountAndBenefits hideOneOff={supportOnceDisplay} />
 						</Box>
 						{supportOnceDisplay && (
@@ -265,9 +238,7 @@ export function SupporterPlusLandingPage({
 								onCardClick={onCardClick}
 							/>
 						)}
-						<Box
-							cssOverrides={optimisedMobileLayout ? shorterBoxMargin : css``}
-						>
+						<Box cssOverrides={shorterBoxMargin}>
 							<BoxContents>
 								{/* The same Stripe provider *must* enclose the Stripe card form and payment button(s). Also enclosing the PRB reduces re-renders. */}
 								<ContributionsStripe>
@@ -287,13 +258,9 @@ export function SupporterPlusLandingPage({
 										)}
 									/>
 									<PaymentButtonController
-										cssOverrides={
-											optimisedMobileLayout
-												? css`
-														margin-top: 30px;
-												  `
-												: css``
-										}
+										cssOverrides={css`
+											margin-top: 30px;
+										`}
 										paymentButtons={getPaymentMethodButtons(
 											contributionType,
 											switches,
@@ -313,21 +280,13 @@ export function SupporterPlusLandingPage({
 								/>
 							</BoxContents>
 						</Box>
-						<CheckoutDivider
-							spacing="loose"
-							mobileTheme={optimisedMobileLayout ? 'light' : 'dark'}
-						/>
+						<CheckoutDivider spacing="loose" mobileTheme={'light'} />
 						<PatronsMessage
 							countryGroupId={countryGroupId}
-							mobileTheme={optimisedMobileLayout ? 'light' : 'dark'}
+							mobileTheme={'light'}
 						/>
-						<CheckoutDivider
-							spacing="tight"
-							mobileTheme={optimisedMobileLayout ? 'light' : 'dark'}
-						/>
-						<GuardianTsAndCs
-							mobileTheme={optimisedMobileLayout ? 'light' : 'dark'}
-						/>
+						<CheckoutDivider spacing="tight" mobileTheme={'light'} />
+						<GuardianTsAndCs mobileTheme={'light'} />
 					</Column>
 				</Columns>
 			</Container>
