39dfc79061ced6ad288d071d2615b03daeb9f100
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2023-05-24 10:46:40 +0100

    Minor refactoring

--- support-frontend/stories/checkoutLayout/CheckoutBenefitsList.stories.tsx
+++ support-frontend/stories/checkoutLayout/CheckoutBenefitsList.stories.tsx
@@ -45,10 +45,7 @@ export const AllBenefitsUnlocked = Template.bind({});
 
 AllBenefitsUnlocked.args = {
 	title: "For £12 per month, you'll unlock",
-	checkListData: checkListData({
-		higherTier: true,
-		isAustralia: false,
-	}),
+	checkListData: checkListData({ higherTier: true, isAustralia: false }),
 	buttonCopy: null,
 	countryGroupId: 'GBPCountries',
 };
@@ -57,10 +54,7 @@ export const LowerTierUnlocked = Template.bind({});
 
 LowerTierUnlocked.args = {
 	title: "For £5 per month, you'll unlock",
-	checkListData: checkListData({
-		higherTier: false,
-		isAustralia: false,
-	}),
+	checkListData: checkListData({ higherTier: false, isAustralia: false }),
 	buttonCopy: 'Switch to £12 per month to unlock all extras',
 	countryGroupId: 'GBPCountries',
 };
