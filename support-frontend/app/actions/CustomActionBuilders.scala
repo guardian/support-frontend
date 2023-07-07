guardian/support-frontend

commit 2020275389c3812d25992d8bca7c3655b017fa16

Include identity endpoint in error messages
We’ve had some timeouts recently, and it’d be helpful to know which
endpoint we’re getting the timeout from: this should mean we see it in
the logs.

support-frontend/app/controllers/CreateSubscriptionController.scala
@@ -140,7 +140,7 @@ class CreateSubscriptionController(
     else if (IdentityError.isBadEmailError(identityError))
       RequestValidationError(BadEmailAddress.errorReasonCode)
     else
-      ServerError(identityError.description)
+      ServerError(identityError.message.concat(identityError.description))
 
   private def logIncomingRequest(
       request: OptionalAuthRequest[CreateSupportWorkersRequest],

support-frontend/app/services/HttpIdentityService.scala
@@ -164,6 +164,10 @@ class IdentityService(apiUrl: String, apiClientToken: String)(implicit wsClient:
           ),
         )
         .map(userResponse => userResponse.user.id)
+    }.leftMap { e =>
+      e match {
+        case IdentityError(message, description) => IdentityError("Error calling /user:".concat(message), description)
+      }
     }
 
   def createUserIdFromEmailUser(
@@ -210,6 +214,10 @@ class IdentityService(apiUrl: String, apiClientToken: String)(implicit wsClient:
           ),
         )
         .map(response => response.guestRegistrationRequest.userId)
+    }.leftMap { e =>
+      e match {
+        case IdentityError(message, description) => IdentityError("Error calling /guest:".concat(message), description)
+      }
     }
   }
 

commit a8362e77fa2b4b8402b46d932c1da64675cd4c74

Fix detection of specific identity errors
I broke this in commit 2020275389c3812d25992d8bca7c3655b017fa16, when I
modified the error messages to add information about which endpoint was
called. I forgot or didn’t realise that we were later checking those
messages to check the specific kind of error.

To reduce the chance of this happening again, I’ve moved the specific
error recognition to the JSON parsing, and used specific case classes
for the specific errors (avoiding redundant info and checking error
messages multiple times).

support-frontend/app/actions/CustomActionBuilders.scala
@@ -8,7 +8,7 @@ import com.gu.aws.{AwsCloudWatchMetricPut, AwsCloudWatchMetricSetup}
 import com.gu.monitoring.SafeLogger
 import com.gu.monitoring.SafeLogger.Sanitizer
 import com.gu.support.config.Stage
-import models.identity.responses.IdentityErrorResponse.IdentityError.{BadEmailAddress, InvalidEmailAddress}
+import models.identity.responses.IdentityErrorResponse._
 import play.api.libs.streams.Accumulator
 import play.api.mvc._
 import play.filters.csrf._
@@ -50,9 +50,9 @@ class CustomActionBuilders(
     private def maybePushAlarmMetric(result: Result) =
       if (
         result.header.status.toString.head != '2' && !result.header.reasonPhrase.contains(
-          InvalidEmailAddress.errorReasonCode,
+          emailProviderRejectedCode,
         ) && !result.header.reasonPhrase.contains(
-          BadEmailAddress.errorReasonCode,
+          invalidEmailAddressCode,
         )
       ) {
         SafeLogger.error(scrub"pushing alarm metric - non 2xx response ${result.toString()}")

support-frontend/app/controllers/CreateSubscriptionController.scala
@@ -17,7 +17,7 @@ import io.circe.generic.semiauto._
 import io.circe.syntax._
 import lib.PlayImplicits._
 import models.identity.responses.IdentityErrorResponse.IdentityError
-import models.identity.responses.IdentityErrorResponse.IdentityError.{InvalidEmailAddress, BadEmailAddress}
+import models.identity.responses.IdentityErrorResponse._
 import org.joda.time.DateTime
 import play.api.http.Writeable
 import play.api.libs.circe.Circe
@@ -135,12 +135,16 @@ class CreateSubscriptionController(
   }
 
   private def mapIdentityErrorToCreateSubscriptionError(identityError: IdentityError) =
-    if (IdentityError.isDisallowedEmailError(identityError))
-      RequestValidationError(InvalidEmailAddress.errorReasonCode)
-    else if (IdentityError.isBadEmailError(identityError))
-      RequestValidationError(BadEmailAddress.errorReasonCode)
-    else
-      ServerError(identityError.message.concat(identityError.description))
+    identityError match {
+      case RejectedEmailProvider(_endpoint) => RequestValidationError(emailProviderRejectedCode)
+      case InvalidEmailAddress(_endpoint) => RequestValidationError(invalidEmailAddressCode)
+      case OtherIdentityError(message, description, endpoint) =>
+        endpoint match {
+          case Some(GuestEndpoint) => ServerError(s"Error calling /guest: ${message}; ${description}")
+          case Some(UserEndpoint) => ServerError(s"Error calling /user: ${message}; ${description}")
+          case None => ServerError(s"${message}: ${description}")
+        }
+    }
 
   private def logIncomingRequest(
       request: OptionalAuthRequest[CreateSupportWorkersRequest],
@@ -338,5 +342,4 @@ class CreateSubscriptionController(
       deliveryInstructions = request.deliveryInstructions,
     )
   }
-
 }

support-frontend/app/models/identity/responses/IdentityErrorResponse.scala
@@ -1,7 +1,8 @@
 package models.identity.responses
 
 import models.identity.responses.IdentityErrorResponse.IdentityError
-import play.api.libs.json.{Json, Reads}
+import play.api.libs.json.{Json, Reads, JsPath}
+import play.api.libs.functional.syntax._
 
 // Models and error returned by Identity
 //
@@ -25,32 +26,48 @@ case class IdentityErrorResponse(
 
 object IdentityErrorResponse {
   implicit val reads: Reads[IdentityErrorResponse] = Json.reads[IdentityErrorResponse]
+  val emailProviderRejectedCode = "invalid_email_address"
+  val invalidEmailAddressCode = "bad_email_address"
 
-  case class IdentityError(
-      message: String,
-      description: String,
-  )
+  sealed trait IdentityError {
+    def endpoint: Option[IdentityEndpoint]
+    def setEndpoint(e: IdentityEndpoint): IdentityError = {
+      this match {
+        case RejectedEmailProvider(_) => RejectedEmailProvider(Some(e))
+        case InvalidEmailAddress(_) => InvalidEmailAddress(Some(e))
+        case OtherIdentityError(m, d, _endpoint) => OtherIdentityError(m, d, endpoint = Some(e))
+      }
+    }
+  }
+
+  /** We don’t accept email addresses from this provider. */
+  case class RejectedEmailProvider(endpoint: Option[IdentityEndpoint]) extends IdentityError
+
+  /** The email address is invalid. */
+  case class InvalidEmailAddress(endpoint: Option[IdentityEndpoint]) extends IdentityError
+
+  /** Some other error occurred. */
+  case class OtherIdentityError(message: String, description: String, endpoint: Option[IdentityEndpoint])
+      extends IdentityError
+
+  sealed trait IdentityEndpoint
+  case object UserEndpoint extends IdentityEndpoint
+  case object GuestEndpoint extends IdentityEndpoint
 
   object IdentityError {
-    object InvalidEmailAddress {
-      val message = "Registration Error"
-      val description = "Please sign up using an email address from a different provider"
-      val errorReasonCode = "invalid_email_address"
+    implicit val reads: Reads[IdentityError] = (
+      (JsPath \ "message").read[String] and
+        (JsPath \ "description").read[String]
+    ) { (message, description) =>
+      if (
+        message == "Registration Error" && description == "Please sign up using an email address from a different provider"
+      ) {
+        RejectedEmailProvider(endpoint = None)
+      } else if (message == "Invalid emailAddress:" && description == "Please enter a valid email address") {
+        InvalidEmailAddress(endpoint = None)
+      } else {
+        OtherIdentityError(message, description, endpoint = None)
+      }
     }
-
-    object BadEmailAddress {
-      val message = "Invalid emailAddress:"
-      val description = "Please enter a valid email address"
-      val errorReasonCode = "bad_email_address"
-    }
-    def isDisallowedEmailError(identityError: IdentityError): Boolean =
-      identityError.message == InvalidEmailAddress.message &&
-        identityError.description == InvalidEmailAddress.description
-
-    def isBadEmailError(identityError: IdentityError): Boolean =
-      identityError.message == BadEmailAddress.message &&
-        identityError.description == BadEmailAddress.description
-
-    implicit val reads: Reads[IdentityError] = Json.reads[IdentityError]
   }
 }

support-frontend/app/services/HttpIdentityService.scala
@@ -11,7 +11,7 @@ import config.Identity
 import io.circe.Encoder
 import io.circe.generic.semiauto.deriveEncoder
 import models.identity.requests.CreateGuestAccountRequestBody
-import models.identity.responses.IdentityErrorResponse.IdentityError
+import models.identity.responses.IdentityErrorResponse.{IdentityError, OtherIdentityError, GuestEndpoint, UserEndpoint}
 import models.identity.responses.{GuestRegistrationResponse, IdentityErrorResponse, UserResponse}
 import play.api.libs.json.{Json, JsonValidationError, JsPath, Reads}
 import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
@@ -158,17 +158,14 @@ class IdentityService(apiUrl: String, apiClientToken: String)(implicit wsClient:
         .validate[UserResponse]
         .asEither
         .leftMap(err =>
-          IdentityError(
+          OtherIdentityError(
             message = "Error deserialising json to UserResponse",
             description = err.mkString(","),
+            endpoint = Some(UserEndpoint),
           ),
         )
         .map(userResponse => userResponse.user.id)
-    }.leftMap { e =>
-      e match {
-        case IdentityError(message, description) => IdentityError("Error calling /user:".concat(message), description)
-      }
-    }
+    }.leftMap(e => e.setEndpoint(UserEndpoint))
 
   def createUserIdFromEmailUser(
       email: String,
@@ -208,16 +205,13 @@ class IdentityService(apiUrl: String, apiClientToken: String)(implicit wsClient:
         .validate[GuestRegistrationResponse]
         .asEither
         .leftMap(err =>
-          IdentityError(
+          OtherIdentityError(
             message = "Error deserialising json to GuestRegistrationResponse",
             description = err.mkString(","),
+            endpoint = Some(GuestEndpoint),
           ),
         )
         .map(response => response.guestRegistrationRequest.userId)
-    }.leftMap { e =>
-      e match {
-        case IdentityError(message, description) => IdentityError("Error calling /guest:".concat(message), description)
-      }
     }
   }
 
@@ -228,7 +222,7 @@ class IdentityService(apiUrl: String, apiClientToken: String)(implicit wsClient:
   )(func: (WSResponse) => Either[IdentityError, A])(implicit ec: ExecutionContext, scheduler: Scheduler) = {
 
     EitherT(requestHolder.execute().map(response => Right(response)).recover { case t =>
-      Left(IdentityError("An exception was thrown in HttpIdentityService.execute", t.getMessage))
+      Left(OtherIdentityError("An exception was thrown in HttpIdentityService.execute", t.getMessage, endpoint = None))
     }).subflatMap {
       case r if r.success => func(r)
       case r =>
@@ -236,10 +230,11 @@ class IdentityService(apiUrl: String, apiClientToken: String)(implicit wsClient:
           .validate[IdentityErrorResponse]
           .asEither
           .leftMap(_ =>
-            IdentityError(
+            OtherIdentityError(
               message = s"${r.body}",
               description =
                 s"Identity API error: ${requestHolder.method} ${uriWithoutQuery(requestHolder.uri)} STATUS ${r.status}, BODY: ${r.body}",
+              endpoint = None,
             ),
           )
           .flatMap(error => Left(error.errors.head))