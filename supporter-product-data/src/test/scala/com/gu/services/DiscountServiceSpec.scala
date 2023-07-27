d90a7a6a8497c067f24cf82f3a2892188c0a8f5a
Rupert Bates <Rupert.Bates@guardian.co.uk>
2023-07-27 10:12:45 +0100

    DiscountServiceSpec isn't an integration test

--- supporter-product-data/src/test/scala/com/gu/services/DiscountServiceSpec.scala
+++ supporter-product-data/src/test/scala/com/gu/services/DiscountServiceSpec.scala
@@ -1,13 +1,11 @@
 package com.gu.services
 
-import com.gu.test.tags.annotations.IntegrationTest
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import scala.io.Source
 import scala.util.{Success, Try, Using}
 
-@IntegrationTest
 class DiscountServiceSpec extends AnyFlatSpec with Matchers {
   class FixtureCatalogLoader extends CatalogLoader {
     override def loadCatalog: Try[String] =
