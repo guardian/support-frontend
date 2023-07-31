1b7215e5a5d6ba6479ce7304501b83a01a5040a0
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2023-07-31 15:23:28 +0100

    Exp

--- support-frontend/test/selenium/util/Config.scala
+++ support-frontend/test/selenium/util/Config.scala
@@ -13,7 +13,7 @@ object Config {
 
   val supportFrontendUrl = conf.getString("support.url")
 
-  val waitTimeout = 45
+  val waitTimeout = 180
 
   val testUsersSecret = conf.getString("identity.test.users.secret")
 
