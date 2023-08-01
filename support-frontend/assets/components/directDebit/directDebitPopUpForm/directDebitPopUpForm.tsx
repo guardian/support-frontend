4de3eb1847bab80c1edad59e3dbf046962b53f9b
Maria Olanipekun <m.olanipekun@hotmail.com>
2023-08-01 12:27:56 +0100

    remove DD pageTitle

--- support-frontend/assets/components/directDebit/directDebitPopUpForm/directDebitPopUpForm.tsx
+++ support-frontend/assets/components/directDebit/directDebitPopUpForm/directDebitPopUpForm.tsx
@@ -9,14 +9,12 @@ import {
 	resetFormError,
 	setPopupClose,
 } from 'helpers/redux/checkout/payment/directDebit/actions';
-import type { Phase } from 'helpers/redux/checkout/payment/directDebit/state';
 import type { ContributionsState } from 'helpers/redux/contributionsStore';
 
 // ----- Map State/Props ----- //
 function mapStateToProps(state: ContributionsState) {
 	return {
 		isPopUpOpen: state.page.checkoutForm.payment.directDebit.isPopUpOpen,
-		phase: state.page.checkoutForm.payment.directDebit.phase,
 	};
 }
 
@@ -32,7 +30,6 @@ type PropTypes = ConnectedProps<typeof connector> & {
 	onPaymentAuthorisation: (authorisation: PaymentAuthorisation) => void;
 };
 
-// ----- Component ----- //
 function DirectDebitPopUpForm(props: PropTypes): JSX.Element {
 	function closePopup() {
 		props.setPopupClose();
@@ -43,9 +40,6 @@ function DirectDebitPopUpForm(props: PropTypes): JSX.Element {
 		return (
 			<div className="component-direct-debit-pop-up-form">
 				<div className="component-direct-debit-pop-up-form__content">
-					<h1 className="component-direct-debit-pop-up-form__heading">
-						<PageTitle phase={props.phase} />
-					</h1>
 					<button
 						id="qa-pay-with-direct-debit-close-pop-up"
 						className="component-direct-debit-pop-up-form__close-button focus-target"
@@ -66,32 +60,5 @@ function DirectDebitPopUpForm(props: PropTypes): JSX.Element {
 
 	return <></>;
 }
-
-// ----- Auxiliary Components ----- //
-function PageTitle(props: { phase: Phase }) {
-	if (props.phase === 'confirmation') {
-		return (
-			<span>
-				<span className="component-direct-debit-pop-up-form__heading--title">
-					Please confirm
-				</span>
-				<span className="component-direct-debit-pop-up-form__heading--title">
-					your details
-				</span>
-			</span>
-		);
-	}
-
-	return (
-		<span>
-			<span className="component-direct-debit-pop-up-form__heading--title">
-				Please enter
-			</span>
-			<span className="component-direct-debit-pop-up-form__heading--title">
-				your details below
-			</span>
-		</span>
-	);
-} // ----- Exports ----- //
 
 export default connector(DirectDebitPopUpForm);
