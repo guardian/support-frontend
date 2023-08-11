29471d247ff084ea853719e2cbf72375dfce7650
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2023-08-11 15:53:28 +0100

    countryGroupId specific percentage discounts reintroduced

--- support-frontend/assets/pages/weekly-subscription-landing/components/content/benefits.tsx
+++ support-frontend/assets/pages/weekly-subscription-landing/components/content/benefits.tsx
@@ -1,4 +1,5 @@
 import { List } from 'components/list/list';
+import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
 import BenefitsContainer from './benefitsContainer';
 import BenefitsHeading from './benefitsHeading';
 
@@ -17,11 +18,31 @@ const coreBenefits = [
 	},
 ];
 
-function getBenefits() {
-	return coreBenefits;
+function getBenefits(countryGroupId: CountryGroupId) {
+	const discount = getDiscountGW(countryGroupId);
+	return [
+		{
+			content: `Every issue delivered with up to ${discount}% off the cover price`,
+		},
+		...coreBenefits,
+	];
 }
 
-function Benefits(): JSX.Element {
+function getDiscountGW(countryGroupId: CountryGroupId): number {
+	switch (countryGroupId) {
+		case 'Canada':
+		case 'UnitedStates':
+			return 50;
+		default:
+			return 20;
+	}
+}
+
+function Benefits({
+	countryGroupId,
+}: {
+	countryGroupId: CountryGroupId;
+}): JSX.Element {
 	return (
 		<BenefitsContainer
 			sections={[
@@ -30,7 +51,7 @@ function Benefits(): JSX.Element {
 					content: (
 						<>
 							<BenefitsHeading text="As a subscriber you’ll enjoy" />
-							<List items={getBenefits()} />
+							<List items={getBenefits(countryGroupId)} />
 						</>
 					),
 				},
