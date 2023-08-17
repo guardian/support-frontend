9b5c5a379b6999c16b2349f8495f45535f8c5a0d
Richard Bangay <richard.bangay@guardian.co.uk>
2023-08-17 16:08:18 +0100

    [RB LP] - update test to use credit card and select element inside an iframe

--- support-frontend/playwright/recurring_contribution.spec.ts
+++ support-frontend/playwright/recurring_contribution.spec.ts
@@ -4,22 +4,23 @@ const userName = 'asdfasdfdsaf';
 const userLastName = 'dfgslksdfgkjbsdf';
 const userEmail = 'asdfa@example.com';
 
-test.beforeEach(async ({ page, browser, context }) => {
-  const pageUrl = 'https://support.thegulocal.com/uk/contribute';
-  context.addCookies([
-    { name: 'pre-signin-test-user', value: userName, url: pageUrl },
-    { name: '_test_username', value: userName, url: pageUrl },
-    { name: '_post_deploy_user', value: 'true', url: pageUrl },
-    { name: 'GU_TK', value: '1.1', url: pageUrl },
-  ]);
-  await page.goto(pageUrl);
+test.beforeEach(async ({ page, context }) => {
+	const pageUrl = 'https://support.thegulocal.com/uk/contribute';
+	context.addCookies([
+		{ name: 'pre-signin-test-user', value: userName, url: pageUrl },
+		{ name: '_test_username', value: userName, url: pageUrl },
+		{ name: '_post_deploy_user', value: 'true', url: pageUrl },
+		{ name: 'GU_TK', value: '1.1', url: pageUrl },
+	]);
+	await page.goto(pageUrl);
 });
 
 test.describe(
 	'Sign up for a Recurring Contribution (New Contributions Flow)',
 	() => {
-
-		test('Monthly contribution sign-up with Stripe - GBP - step 1', async ({ page, browser }) => {
+		test('Monthly contribution sign-up with Stripe - GBP - step 1', async ({
+			page,
+		}) => {
 			const contributeButton =
 				'#qa-contributions-landing-submit-contribution-button';
 
@@ -28,16 +29,22 @@ test.describe(
 			await expect(page.locator(contributeButton)).toBeVisible();
 		});
 
-    test('Monthly contribution sign-up with Stripe - GBP - step 2', async ({page}) => {
-      const monthlyTab = '#MONTHLY';
-      await page.locator(monthlyTab).click();
-      
-      await page.locator('#email').type(userEmail);
-      await page.locator('#firstName').type(userName);
-      await page.locator('#lastName').type(userLastName);
+		test('Monthly contribution sign-up with Stripe - GBP - step 2', async ({
+			page,
+		}) => {
+			const monthlyTab = '#MONTHLY';
+			await page.locator(monthlyTab).click();
 
-      await page.locator('#qa-direct-debit').click();
-    });
+			await page.locator('#email').type(userEmail);
+			await page.locator('#firstName').type(userName);
+			await page.locator('#lastName').type(userLastName);
 
+			await page.locator('#qa-credit-card').click();
+
+			const cardnumber = await page
+				.frameLocator('#cardNumber iframe')
+				.locator('input[name="cardnumber"]');
+			await cardnumber.fill('4242424242424242');
+		});
 	},
 );
