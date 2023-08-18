f8b211c66d6ca05ad553fff087d1dceaa0370a4e
Richard Bangay <richard.bangay@guardian.co.uk>
2023-08-18 09:21:13 +0100

    [RB LP] - (WIP) - add the requisite cookies to make the CMP banner go away

--- support-frontend/webdrivertest/test.e2e.js
+++ support-frontend/webdrivertest/test.e2e.js
@@ -1,23 +1,50 @@
-const { expect, browser, $ } = require('@wdio/globals')
+const { expect, browser, $ } = require('@wdio/globals');
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('Sign up for a Recurring Contribution (New Contributions Flow)', () => {
+	it('Monthly contribution sign-up with Stripe', async () => {
+		const emailCheckMock = await browser.mock('**/identity/get-user-type?**', {
+			method: 'get',
+		});
+		emailCheckMock.respond({ userType: 'new', hi: 'Lakshmi' });
 
+    const url = 'https://support.thegulocal.com/uk/contribute';
+    const userName = 'dobedobeodo';
 
-        const emailCheckMock = await browser.mock('**/identity/get-user-type?**', {
-            method: 'get'
-        })
-        emailCheckMock.respond({"userType":"new", "hi": "Lakshmi"})
+		await browser.url(url);
 
-        await browser.url('https://support.thegulocal.com/uk/contribute');
+		await browser.setCookies([
+      { name: 'pre-signin-test-user', value: userName },
+			{ name: '_test_username', value: userName },
+			{ name: '_post_deploy_user', value: 'true' },
+			{ name: 'GU_TK', value: '1.1' },
+		]);
 
-        await $('#email').setValue('test@email.com')
-        await $('#firstName').setValue('dobedobeodo')
-        await $('#lastName').setValue('dobedobeodo')
-        await $('#qa-credit-card').click()
+    const cookies = await browser.getCookies()
+    console.log(cookies);
 
-        await expect($('#flash')).toBeExisting()
-        // await expect($('#flash')).toHaveTextContaining(
-        //     'You logged into a secure area!')
-    })
-})
+		await $('#email').setValue('test@email.com');
+		await $('#firstName').setValue(userName);
+		await $('#lastName').setValue('dobedobeodo');
+		await $('#qa-credit-card').click();
+
+		const cardNumberIframe = await browser.findElement(
+			'css selector',
+			'#cardNumber iframe',
+		);
+		await browser.switchToFrame(cardNumberIframe);
+
+		await $('input[name="cardnumber"]').setValue('4242424242424242');
+		await expect($('input[name="cardnumber"]')).toBeExisting();
+
+		browser.pause(10000);
+		//
+		const expiryDateIframe = await browser.findElement(
+			'css selector',
+			'#expiry iframe',
+		);
+		await browser.switchToFrame(expiryDateIframe);
+
+		await $('input[name="exp-date"]').setValue('01/23');
+		//
+	});
+});
