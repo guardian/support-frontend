79662301d897477103bf7333b479b2acbb57d7e9
Maria Olanipekun <m.olanipekun@hotmail.com>
2023-09-05 09:56:45 +0100

    set billing country and state based on payment method details

--- support-frontend/assets/helpers/redux/checkout/payment/contributionsSideEffects.ts
+++ support-frontend/assets/helpers/redux/checkout/payment/contributionsSideEffects.ts
@@ -1,6 +1,13 @@
+import {
+	setBillingCountry,
+	setBillingState,
+} from 'helpers/redux/checkout/address/actions';
+import {
+	setPaymentMethod,
+	setPaymentMethodCountryAndState,
+} from 'helpers/redux/checkout/payment/paymentMethod/actions';
 import type { ContributionsStartListening } from 'helpers/redux/contributionsStore';
 import * as storage from 'helpers/storage/storage';
-import { setPaymentMethod } from './paymentMethod/actions';
 
 export function addPaymentsSideEffects(
 	startListening: ContributionsStartListening,
@@ -9,6 +16,15 @@ export function addPaymentsSideEffects(
 		actionCreator: setPaymentMethod,
 		effect(action) {
 			storage.setSession('selectedPaymentMethod', action.payload.paymentMethod);
+		},
+	});
+
+	startListening({
+		actionCreator: setPaymentMethodCountryAndState,
+		effect(action, listenerApi) {
+			listenerApi.dispatch(setBillingCountry(action.payload[0]));
+			action.payload[1] &&
+				listenerApi.dispatch(setBillingState(action.payload[1]));
 		},
 	});
 }
