4c3ff3d3b4c0bb28a17da8aa3c3d5b44d7a00de2
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2023-09-25 14:36:25 +0100

    First working post deployment browserstack setup

---
+++ support-lambdas/support-post-deployment-tests/packages/lambda/src/playwright/tests/local_test.ts
@@ -0,0 +1,50 @@
+import { test, expect } from '@playwright/test';
+
+const userName = 'asdfasdfdsaf';
+const userLastName = 'dfgslksdfgkjbsdf';
+const userEmail = 'asdfa@example.com';
+
+test.beforeEach(async ({ page, context }) => {
+  const pageUrl = 'https://support.theguardian.com/uk/contribute';
+  context.addCookies([
+    { name: 'pre-signin-test-user', value: userName, url: pageUrl },
+    { name: '_test_username', value: userName, url: pageUrl },
+    { name: '_post_deploy_user', value: 'true', url: pageUrl },
+    { name: 'GU_TK', value: '1.1', url: pageUrl },
+  ]);
+  await page.goto(pageUrl);
+});
+
+test.describe(
+  'Sign up for a Recurring Contribution (New Contributions Flow)',
+  () => {
+    test('Monthly contribution sign-up with Stripe - GBP - step 1', async ({
+                                                                             page,
+                                                                           }) => {
+      const contributeButton =
+        '#qa-contributions-landing-submit-contribution-button';
+
+      await expect(page.url()).toContain('/contribute');
+
+      await expect(page.locator(contributeButton)).toBeVisible();
+    });
+
+    test('Monthly contribution sign-up with Stripe - GBP - step 2', async ({
+                                                                             page,
+                                                                           }) => {
+      const monthlyTab = '#MONTHLY';
+      await page.locator(monthlyTab).click();
+
+      await page.locator('#email').type(userEmail);
+      await page.locator('#firstName').type(userName);
+      await page.locator('#lastName').type(userLastName);
+
+      await page.locator('#qa-credit-card').click();
+
+      const cardnumber = await page
+        .frameLocator('#cardNumber iframe')
+        .locator('input[name="cardnumber"]');
+      await cardnumber.fill('4242424242424242');
+    });
+  },
+);
