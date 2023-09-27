2e9a969435a7bbb99b9bb30b7489f35abe53ef1f
Lakshmi Pillai <lakshmi.pillai@guardian.co.uk>
2023-09-27 15:41:46 +0100

    Changes for the scheduled lambda

--- support-lambdas/support-post-deployment-tests/packages/cdk/bin/cdk.ts
+++ support-lambdas/support-post-deployment-tests/packages/cdk/bin/cdk.ts
@@ -1,6 +1,6 @@
 import 'source-map-support/register';
 import { GuRootExperimental } from '@guardian/cdk/lib/experimental/constructs';
-import { ExampleTypescriptLambda } from '../lib/example-typescript-lambda';
+import { LoggingLevel,SupportFrontendPostDeploymentTestsLambda } from '../lib/support-frontend-post-deployment-tests-lambda';
 
 /**
  * GuRootExperimental will generate a `riff-raff.yaml` configuration file to deploy this project with Riff-Raff.
@@ -9,7 +9,7 @@ import { ExampleTypescriptLambda } from '../lib/example-typescript-lambda';
  */
 const app = new GuRootExperimental();
 
-new ExampleTypescriptLambda(app, 'ExampleTypescriptLambda-PROD', {
+new SupportFrontendPostDeploymentTestsLambda(app, 'SupportFrontendPostDeploymentTestsLambda-CODE', {
 	/**
 	 * This becomes the value of the STACK tag on provisioned resources.
 	 *
@@ -17,12 +17,12 @@ new ExampleTypescriptLambda(app, 'ExampleTypescriptLambda-PROD', {
 	 *
 	 * @see https://riffraff.gutools.co.uk/deployinfo/data?key=credentials%3Aaws-cfn-role
 	 */
-	stack: 'playground',
+	stack: 'membership',
 
 	/**
 	 * This becomes the value of the STAGE tag on provisioned resources.
 	 */
-	stage: 'PROD',
+	stage: 'CODE',
 
 	env: {
 		/**
@@ -30,4 +30,29 @@ new ExampleTypescriptLambda(app, 'ExampleTypescriptLambda-PROD', {
 		 */
 		region: 'eu-west-1',
 	},
-});
+
+},LoggingLevel.DEBUG);
+
+new SupportFrontendPostDeploymentTestsLambda(app, 'SupportFrontendPostDeploymentTestsLambda-PROD', {
+  /**
+   * This becomes the value of the STACK tag on provisioned resources.
+   *
+   * It is also used by Riff-Raff to determine the AWS account to deploy into.
+   *
+   * @see https://riffraff.gutools.co.uk/deployinfo/data?key=credentials%3Aaws-cfn-role
+   */
+  stack: 'membership',
+
+  /**
+   * This becomes the value of the STAGE tag on provisioned resources.
+   */
+  stage: 'PROD',
+
+  env: {
+    /**
+     * Which AWS region should this service be deployed into?
+     */
+    region: 'eu-west-1',
+  },
+},LoggingLevel.INFO);
+
---
+++ support-lambdas/support-post-deployment-tests/packages/cdk/lib/support-frontend-post-deployment-tests-lambda.ts
@@ -0,0 +1,52 @@
+// cdk/lib/starter-typescript-lambda.ts
+
+import { GuScheduledLambda } from '@guardian/cdk';
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { Duration } from 'aws-cdk-lib';
+import type { App } from 'aws-cdk-lib';
+import { Schedule } from 'aws-cdk-lib/aws-events';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+
+const APP_NAME = 'support-frontend-post-deployment-tests-lambda';
+
+export enum LoggingLevel {
+  SILLY,
+  TRACE,
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+  FATAL,
+}
+
+export class SupportFrontendPostDeploymentTestsLambda extends GuStack {
+  constructor(
+    scope: App,
+    id: string,
+    props: GuStackProps,
+    loggingLevel: number = LoggingLevel.WARN,
+  ) {
+    super(scope, id, props);
+
+    const otherConfig = {
+      app: APP_NAME,
+      runtime: Runtime.NODEJS_18_X,
+      fileName: `${APP_NAME}.zip`,
+      timeout: Duration.millis(45000),
+    };
+
+    new GuScheduledLambda(this, APP_NAME, {
+      handler: 'dist/lambda/index.handler',
+      rules: [
+        {
+          schedule: Schedule.cron({ hour: '10', minute: '00', weekDay: '2' }),
+        },
+      ],
+      monitoringConfiguration: {
+        noMonitoring: true,
+      },
+      ...otherConfig,
+    });
+  }
+}
