3ee34f9a0263cb0e52ff9865a69a9fe9e39b5179
James Gorrie <jamesgorrie@users.noreply.github.com>
2023-10-26 14:17:14 +0100

    fix: remove showUnchecked from ContributionsOrderSummary story

--- support-frontend/stories/checkouts/ContributionsOrderSummary.stories.tsx
+++ support-frontend/stories/checkouts/ContributionsOrderSummary.stories.tsx
@@ -50,7 +50,7 @@ Default.args = {
 		isSuffixGlyph: false,
 		isPaddedGlyph: false,
 	},
-	checkListData: checkListData({ higherTier: true, showUnchecked: false }),
+	checkListData: checkListData({ higherTier: true }),
 	headerButton: (
 		<Button priority="tertiary" size="xsmall">
 			Change
@@ -65,7 +65,7 @@ Default.args = {
 			</p>
 		</>
 	),
-};
+} as ContributionsOrderSummaryProps;
 
 export const SingleContribution = Template.bind({});
 
