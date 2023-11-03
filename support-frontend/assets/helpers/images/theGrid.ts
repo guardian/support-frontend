c72b3721f1f589277c056a7832e1c509f60d19e2
James Gorrie <jamesgorrie@users.noreply.github.com>
2023-11-03 18:19:40 +0000

    feat: use fastly image service for grid images

--- support-frontend/assets/helpers/images/theGrid.ts
+++ support-frontend/assets/helpers/images/theGrid.ts
@@ -3,19 +3,20 @@ import catalogue from './imageCatalogue.json';
 // ----- Types ----- //
 export type ImageType = 'jpg' | 'png';
 // ----- Setup ----- //
-export const GRID_DOMAIN = 'https://media.guim.co.uk';
+export const GRID_DOMAIN = 'https://i.guim.co.uk/img/media/';
 export const imageCatalogue: Record<string, string> = catalogue;
 // Utility type: https://flow.org/en/docs/types/utilities/#toc-keys
 export type ImageId = $Keys<typeof imageCatalogue>;
 // ----- Functions ----- //
-// Builds a grid url from and id and an image size.
-// Example: https://media.guim.co.uk/g65756g5/300.jpg
+// Builds a fastly-image-service url from and id and an image size.
+// see: https://github.com/guardian/fastly-image-service
+// Example: https://i.guim.co.uk/img/media/017a2f5c27394635b53c414962bbb775ce9b131d/5_39_1572_861/500.jpg?dpr=1&s=none&width=500`
 export function gridUrl(
 	gridId: ImageId,
 	size: number,
 	imgType: ImageType = 'jpg',
 ): string {
-	const path = `${imageCatalogue[gridId]}/${size}.${imgType}`;
+	const path = `${imageCatalogue[gridId]}/master/${size}.${imgType}?dpr=1&s=none&width=${size}`;
 	const url = new URL(path, GRID_DOMAIN);
 	return url.toString();
 }
