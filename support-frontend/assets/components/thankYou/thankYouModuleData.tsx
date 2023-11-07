9c88d643a0ec358281cb8d54ae7f08e1b2f52cc1
“LAKSHMIRPILLAI” <luxmi.r.pillai@gmail.com>
2023-11-07 17:18:10 +0000

    Prettier issue fix

--- support-frontend/assets/components/thankYou/thankYouModuleData.tsx
+++ support-frontend/assets/components/thankYou/thankYouModuleData.tsx
@@ -131,7 +131,7 @@ export const getThankYouModuleData = (
 			bodyCopy: (
 				<SupportReminderBodyCopy supportReminderState={supportReminder} />
 			),
-			ctas:supportReminder.hasBeenCompleted? null:(
+			ctas: supportReminder.hasBeenCompleted ? null : (
 				<SupportReminderCTAandPrivacy
 					email={email}
 					supportReminderState={supportReminder}
