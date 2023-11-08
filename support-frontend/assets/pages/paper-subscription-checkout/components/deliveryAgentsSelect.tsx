44eb13b2b54cd5d18b44157fe8e4cb445f214f0a
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2023-11-08 12:12:25 +0000

    fix: make delivery provider Green label case insensitive

--- support-frontend/assets/pages/paper-subscription-checkout/components/deliveryAgentsSelect.tsx
+++ support-frontend/assets/pages/paper-subscription-checkout/components/deliveryAgentsSelect.tsx
@@ -16,6 +16,9 @@ const marginBottom = css`
 
 const singleDeliveryProviderCss = css(marginBottom, `border: 0;`);
 
+const greenDeliveryMethods = ['green delivery', 'green options'] as const;
+type GreenDeliveryMethod = typeof greenDeliveryMethods[number];
+
 interface DeliveryAgentsSelectProps {
 	chosenDeliveryAgent?: number;
 	deliveryAgentsResponse?: DeliveryAgentsResponse;
@@ -160,19 +163,17 @@ function GreenDeliverySummary({ deliveryMethod }: { deliveryMethod: string }) {
 
 function isGreenOption(
 	deliveryMethod: string,
-): deliveryMethod is 'Green delivery' | 'Green options' {
-	const greenDeliveryMethods = ['Green delivery', 'Green options'] as const;
+): deliveryMethod is GreenDeliveryMethod {
+	const isGreenDeliveryMethod = isOneOf(greenDeliveryMethods);
 
-	const isGreenDeliveryMethods = isOneOf(greenDeliveryMethods);
-
-	return isGreenDeliveryMethods(deliveryMethod);
+	return isGreenDeliveryMethod(deliveryMethod.toLowerCase());
 }
 
-function getGreenSummary(deliveryMethod: 'Green delivery' | 'Green options') {
+function getGreenSummary(deliveryMethod: GreenDeliveryMethod) {
 	switch (deliveryMethod) {
-		case 'Green delivery':
+		case 'green delivery':
 			return 'This provider will deliver your newspaper via foot, bicycle, or electric vehicle.';
-		case 'Green options':
+		case 'green options':
 			return 'This provider may deliver your newspaper via foot, bicycle, electric vehicle or petrol/diesel vehicle.';
 	}
 }
