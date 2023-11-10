adb6e20418a5b9fef8118963e07b772e90748483
George Haberis <george_haberis@31404.gnm.int>
2023-11-10 11:24:50 +0000

    add targetPage to ab test definitions

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -33,7 +33,7 @@ export const pageUrlRegexes = {
 		paper: {
 			// Requires /subscribe/paper, allows /checkout or /checkout/guest, allows any query string
 			paperLandingWithGuestCheckout:
-				/\/subscribe\/paper(\/checkout|\/checkout\/guest)?(\?.*)?$/,
+				/\/subscribe\/paper(\/delivery|\/checkout|\/checkout\/guest)?(\?.*)?$/,
 		},
 		subsWeeklyPages:
 			'(/??/subscribe(\\?.*)?$|/??/subscribe/weekly(\\/checkout)?(\\?.*)?$)',
@@ -58,6 +58,7 @@ export const tests: Tests = {
 		isActive: true,
 		referrerControlled: true,
 		seed: 2,
+		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages
 	},
 	nationalDelivery: {
 		variants: [
@@ -74,6 +75,7 @@ export const tests: Tests = {
 		},
 		referrerControlled: false,
 		seed: 0,
+		targetPage: pageUrlRegexes.subscriptions.paper.paperLandingWithGuestCheckout 
 	},
 	makeItAnnualNudge: {
 		variants: [
@@ -93,5 +95,6 @@ export const tests: Tests = {
 		},
 		referrerControlled: false,
 		seed: 0,
+		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages
 	},
 };
