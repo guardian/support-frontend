16376d9a3be2eecca83512c3071b3ee563a06a20
James Gorrie <jamesgorrie@users.noreply.github.com>
2023-11-15 17:22:21 +0000

    feat: make deploying fastly snippets iterative

--- .github/workflows/deploy-fastly-snippets.js
+++ .github/workflows/deploy-fastly-snippets.js
@@ -1,9 +1,74 @@
 const FASTLY_SERVICE_ID = process.env.FASTLY_SERVICE_ID;
 const FASTLY_API_TOKEN = process.env.FASTLY_API_TOKEN;
-const FASTLY_SERVICE_SNIPPET_FILE = process.env.FASTLY_SERVICE_SNIPPET_FILE;
-const FASTLY_SERVICE_SNIPPET_NAME = process.env.FASTLY_SERVICE_SNIPPET_NAME;
-const FASTLY_SERVICE_SNIPPET_TYPE = process.env.FASTLY_SERVICE_SNIPPET_TYPE;
 
+const snippets = [
+  // redirects table
+  {
+    file: "./support-frontend/conf/fastly-snippets/redirects-table.vcl",
+    name: "support-frontend - Redirects table",
+    type: "init",
+  },
+  {
+    file: "./support-frontend/conf/fastly-snippets/redirects-table-recv.vcl",
+    name: "support-frontend - Redirects table recv",
+    type: "recv",
+  },
+  {
+    file: "./support-frontend/conf/fastly-snippets/redirects-table-deliver.vcl",
+    name: "support-frontend - Redirects table deliver",
+    type: "deliver",
+  },
+  // redirects error
+  {
+    file: "./support-frontend/conf/fastly-snippets/redirects-error.vcl",
+    name: "support-frontend - Redirects error",
+    type: "error",
+  },
+  // Compliance redirect
+  {
+    file: "./support-frontend/conf/fastly-snippets/compliance-redirect-recv.vcl",
+    name: "support-frontend - Compliance redirect recv",
+    type: "recv",
+  },
+  {
+    file: "./support-frontend/conf/fastly-snippets/compliance-redirect-deliver.vcl",
+    name: "support-frontend - Compliance redirect deliver",
+    type: "deliver",
+  },
+  // Frontline redirect
+  {
+    file: "./support-frontend/conf/fastly-snippets/frontline-redirect-recv.vcl",
+    name: "support-frontend - Frontline redirect recv",
+    type: "recv",
+  },
+  {
+    file: "./support-frontend/conf/fastly-snippets/frontline-redirect-deliver.vcl",
+    name: "support-frontend - Frontline redirect deliver",
+    type: "deliver",
+  },
+  // Books redirect
+  {
+    file: "./support-frontend/conf/fastly-snippets/books-redirect-rec.vcl",
+    name: "support-frontend - Books redirect recv",
+    type: "recv",
+  },
+  // gu_geo_country-cookie
+  {
+    file: "./support-frontend/conf/fastly-snippets/gu_geo_country-cookie.vcl",
+    name: "support-frontend - gu_geo_country cookie deliver",
+    type: "deliver",
+  },
+]
+
+/**
+ * This method
+ * - Finds the active version
+ * - Clones the active version into the new version
+ * - Deletes all the snippets (conditional)
+ * - Creates | updates the snippets
+ * - Activates the new version
+ * - Locks the new version
+ */
 async function run() {
   const activeVersionData = await fetch(
     `https://api.fastly.com/service/${FASTLY_SERVICE_ID}/version`,
@@ -32,61 +97,65 @@ async function run() {
   const clonedVersion = clonedVersionData.number;
   console.info(`Cloned version ${activeVersion} => ${clonedVersion}`);
 
-  // Check the snippet exists
-  const snippetStatus = await fetch(
-    `https://api.fastly.com/service/${FASTLY_SERVICE_ID}/version/${clonedVersion}/snippet/${encodeURIComponent(
-      FASTLY_SERVICE_SNIPPET_NAME
-    )}`,
-    {
-      headers: {
-        "Fastly-Key": FASTLY_API_TOKEN,
-        Accept: "application/json",
-      },
-    }
-  ).then((resp) => resp.status);
-  const snippetExists = snippetStatus === 200;
-  console.info(`Snippet exists: ${snippetExists} (${snippetStatus})`);
 
-  const fs = require("fs");
-  const snippetContent = fs.readFileSync(FASTLY_SERVICE_SNIPPET_FILE, "utf-8");
-  const formaData = new FormData();
-  formaData.set("name", FASTLY_SERVICE_SNIPPET_NAME);
-  formaData.set("type", FASTLY_SERVICE_SNIPPET_TYPE);
-  formaData.set("content", snippetContent);
-  formaData.set("dynamic", 0);
-  const body = new URLSearchParams(formaData).toString();
-
-  if (snippetExists) {
-    const updateData = await fetch(
+  // Loop through the snippets
+  for (const { file, name, type } of snippets) {
+    // Check the snippet exists
+    const snippetStatus = await fetch(
       `https://api.fastly.com/service/${FASTLY_SERVICE_ID}/version/${clonedVersion}/snippet/${encodeURIComponent(
-        FASTLY_SERVICE_SNIPPET_NAME
+        name
       )}`,
       {
-        method: "PUT",
-        body,
         headers: {
           "Fastly-Key": FASTLY_API_TOKEN,
-          "Content-type": "application/x-www-form-urlencoded",
           Accept: "application/json",
         },
       }
-    ).then((resp) => resp.json());
-    console.info("Updated", updateData.updated_at);
-  } else {
-    // Create the snippet
-    const createData = await fetch(
-      `https://api.fastly.com/service/${FASTLY_SERVICE_ID}/version/${clonedVersion}/snippet`,
-      {
-        method: "POST",
-        body,
-        headers: {
-          "Fastly-Key": FASTLY_API_TOKEN,
-          "Content-type": "application/x-www-form-urlencoded",
-          Accept: "application/json",
-        },
-      }
-    ).then((resp) => resp.json());
-    console.info("Created", createData.updated_at);
+    ).then((resp) => resp.status);
+    const snippetExists = snippetStatus === 200;
+    console.info(`Snippet exists: ${snippetExists} (${snippetStatus})`);
+
+    const fs = require("fs");
+    const snippetContent = fs.readFileSync(file, "utf-8");
+    const formaData = new FormData();
+    formaData.set("name", name);
+    formaData.set("type", type);
+    formaData.set("content", snippetContent);
+    formaData.set("dynamic", 0);
+    const body = new URLSearchParams(formaData).toString();
+
+    if (snippetExists) {
+      const updateData = await fetch(
+        `https://api.fastly.com/service/${FASTLY_SERVICE_ID}/version/${clonedVersion}/snippet/${encodeURIComponent(
+          name
+        )}`,
+        {
+          method: "PUT",
+          body,
+          headers: {
+            "Fastly-Key": FASTLY_API_TOKEN,
+            "Content-type": "application/x-www-form-urlencoded",
+            Accept: "application/json",
+          },
+        }
+      ).then((resp) => resp.json());
+      console.info("Updated", updateData.updated_at);
+    } else {
+      // Create the snippet
+      const createData = await fetch(
+        `https://api.fastly.com/service/${FASTLY_SERVICE_ID}/version/${clonedVersion}/snippet`,
+        {
+          method: "POST",
+          body,
+          headers: {
+            "Fastly-Key": FASTLY_API_TOKEN,
+            "Content-type": "application/x-www-form-urlencoded",
+            Accept: "application/json",
+          },
+        }
+      ).then((resp) => resp.json());
+      console.info("Created", createData.updated_at);
+    }
   }
 
   const activatedVersionData = await fetch(
