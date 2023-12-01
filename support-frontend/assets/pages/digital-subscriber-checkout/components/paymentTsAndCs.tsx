bc7836b48d1dc269630a18b67bbc02920092a6e9
James Gorrie <jamesgorrie@users.noreply.github.com>
2023-12-01 16:50:42 +0000

    refactor: unabstract things in the paymentTsAndCs

--- support-frontend/assets/pages/digital-subscriber-checkout/components/paymentTsAndCs.tsx
+++ support-frontend/assets/pages/digital-subscriber-checkout/components/paymentTsAndCs.tsx
@@ -15,30 +15,6 @@ const container = css`
 	}
 `;
 
-const terms = (linkText: string) => (
-	<a href="https://www.theguardian.com/info/2014/aug/06/guardian-observer-digital-subscriptions-terms-conditions">
-		{linkText}
-	</a>
-);
-
-function TsAndCsFooterLinks() {
-	const privacy = <a href={privacyLink}>Privacy Policy</a>;
-
-	return (
-		<div css={marginTop}>
-			By proceeding, you are agreeing to our{' '}
-			<a href="https://www.theguardian.com/info/2014/aug/06/guardian-observer-digital-subscriptions-terms-conditions">
-				Terms and Conditions
-			</a>
-			.{' '}
-			<p css={marginTop}>
-				To find out what personal data we collect and how we use it, please
-				visit our {privacy}.
-			</p>
-		</div>
-	);
-}
-
 export function PaymentTsAndCs(): JSX.Element {
 	return (
 		<div css={container}>
@@ -47,8 +23,22 @@ export function PaymentTsAndCs(): JSX.Element {
 			each month at the full price of £14.99 per month or £149 per year unless
 			you cancel. You can cancel at any time before your next renewal date.
 			Cancellation will take effect at the end of your current subscription
-			month. To cancel, go to Manage My Account or see our {terms('Terms')}.
-			<TsAndCsFooterLinks />
+			month. To cancel, go to Manage My Account or see our
+			<a href="https://www.theguardian.com/info/2014/aug/06/guardian-observer-digital-subscriptions-terms-conditions">
+				Terms
+			</a>
+			.
+			<div css={marginTop}>
+				By proceeding, you are agreeing to our{' '}
+				<a href="https://www.theguardian.com/info/2014/aug/06/guardian-observer-digital-subscriptions-terms-conditions">
+					Terms and Conditions
+				</a>
+				.{' '}
+				<p css={marginTop}>
+					To find out what personal data we collect and how we use it, please
+					visit our <a href={privacyLink}>Privacy Policy</a>.
+				</p>
+			</div>
 		</div>
 	);
 }
