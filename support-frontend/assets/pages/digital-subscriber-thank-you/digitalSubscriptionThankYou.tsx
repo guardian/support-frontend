f62597a979a39b4cdb14eebad0329c542a1b572e
“LAKSHMIRPILLAI” <luxmi.r.pillai@gmail.com>
2023-12-13 13:25:29 +0000

    Merge

--- support-frontend/assets/pages/digital-subscriber-thank-you/digitalSubscriptionThankYou.tsx
+++ support-frontend/assets/pages/digital-subscriber-thank-you/digitalSubscriptionThankYou.tsx
@@ -15,119 +15,117 @@ import { OPHAN_COMPONENT_ID_RETURN_TO_GUARDIAN } from 'helpers/thankYouPages/uti
 import { trackComponentClick } from 'helpers/tracking/behaviour';
 import ThankYouFooter from 'pages/supporter-plus-thank-you/components/thankYouFooter';
 import {
-	buttonContainer,
-	checkoutContainer,
-	columnContainer,
-	firstColumnContainer,
-	headerContainer,
+  buttonContainer,
+  checkoutContainer,
+  columnContainer,
+  firstColumnContainer,
+  headerContainer,
 } from 'pages/supporter-plus-thank-you/supporterPlusThankYou';
 import ThankYouHeader from './components/thankYouHeader';
 
 export function DigitalSubscriptionThankYou(): JSX.Element {
-	const campaignSettings = useMemo<CampaignSettings | null>(
-		() => getCampaignSettings(campaignCode),
-		[],
-	);
-	const { countryId, countryGroupId } = useContributionsSelector(
-		(state) => state.common.internationalisation,
-	);
-	const { csrf } = useContributionsSelector((state) => state.page.checkoutForm);
-	const { campaignCode } = useContributionsSelector(
-		(state) => state.common.referrerAcquisitionData,
-	);
-	const { firstName, email, userTypeFromIdentityResponse } =
-		useContributionsSelector(
-			(state) => state.page.checkoutForm.personalDetails,
-		);
-	const paymentMethod = useContributionsSelector(
-		(state) => state.page.checkoutForm.payment.paymentMethod.name,
-	);
-	const { isSignedIn } = useContributionsSelector((state) => state.page.user);
-	const isNewAccount = userTypeFromIdentityResponse === 'new';
+  const campaignSettings = useMemo<CampaignSettings | null>(
+    () => getCampaignSettings(campaignCode),
+    [],
+  );
+  const { countryId, countryGroupId } = useContributionsSelector(
+    (state) => state.common.internationalisation,
+  );
+  const { csrf } = useContributionsSelector((state) => state.page.checkoutForm);
+  const { campaignCode } = useContributionsSelector(
+    (state) => state.common.referrerAcquisitionData,
+  );
+  const { firstName, email, userTypeFromIdentityResponse } =
+    useContributionsSelector(
+      (state) => state.page.checkoutForm.personalDetails,
+    );
+  const paymentMethod = useContributionsSelector(
+    (state) => state.page.checkoutForm.payment.paymentMethod.name,
+  );
+  const { isSignedIn } = useContributionsSelector((state) => state.page.user);
+  const isNewAccount = userTypeFromIdentityResponse === 'new';
+  const thankYouModuleData = getThankYouModuleData(
+    countryId,
+    countryGroupId,
+    csrf,
+    email,
+    false,
+    campaignSettings?.campaignCode,
+  );
 
-	const thankYouModuleData = getThankYouModuleData(
-		countryId,
-		countryGroupId,
-		csrf,
-		email,
-		false,
-		true,
-		campaignSettings?.campaignCode,
-	);
+  const maybeThankYouModule = (
+    condtion: boolean,
+    moduleType: ThankYouModuleType,
+  ): ThankYouModuleType[] => (condtion ? [moduleType] : []);
 
-	const maybeThankYouModule = (
-		condtion: boolean,
-		moduleType: ThankYouModuleType,
-	): ThankYouModuleType[] => (condtion ? [moduleType] : []);
+  const thankYouModules: ThankYouModuleType[] = [
+    ...maybeThankYouModule(isNewAccount, 'signUp'),
+    ...maybeThankYouModule(
+      !isNewAccount && !isSignedIn && email.length > 0,
+      'signIn',
+    ),
+    'appDownloadEditions',
+    ...maybeThankYouModule(countryId === 'AU', 'ausMap'),
+    'socialShare',
+  ];
 
-	const thankYouModules: ThankYouModuleType[] = [
-		...maybeThankYouModule(isNewAccount, 'signUp'),
-		...maybeThankYouModule(
-			!isNewAccount && !isSignedIn && email.length > 0,
-			'signIn',
-		),
-		'appDownloadEditions',
-		...maybeThankYouModule(countryId === 'AU', 'ausMap'),
-		'socialShare',
-	];
+  const numberOfModulesInFirstColumn = thankYouModules.length === 3 ? 1 : 2;
+  const firstColumn = thankYouModules.slice(0, numberOfModulesInFirstColumn);
+  const secondColumn = thankYouModules.slice(numberOfModulesInFirstColumn);
 
-	const numberOfModulesInFirstColumn = thankYouModules.length === 3 ? 1 : 2;
-	const firstColumn = thankYouModules.slice(0, numberOfModulesInFirstColumn);
-	const secondColumn = thankYouModules.slice(numberOfModulesInFirstColumn);
+  return (
+    <PageScaffold
+      id="supporter-plus-thank-you"
+      header={<Header />}
+      footer={
+        <FooterWithContents>
+          <ThankYouFooter />
+        </FooterWithContents>
+      }
+    >
+      <div css={checkoutContainer}>
+        <Container>
+          <div css={headerContainer}>
+            <ThankYouHeader
+              name={firstName}
+              showDirectDebitMessage={paymentMethod === DirectDebit}
+            />
+          </div>
 
-	return (
-		<PageScaffold
-			id="supporter-plus-thank-you"
-			header={<Header />}
-			footer={
-				<FooterWithContents>
-					<ThankYouFooter />
-				</FooterWithContents>
-			}
-		>
-			<div css={checkoutContainer}>
-				<Container>
-					<div css={headerContainer}>
-						<ThankYouHeader
-							name={firstName}
-							showDirectDebitMessage={paymentMethod === DirectDebit}
-						/>
-					</div>
+          <Columns collapseUntil="desktop">
+            <Column cssOverrides={[columnContainer, firstColumnContainer]}>
+              {firstColumn.map((moduleType) => (
+                <ThankYouModule
+                  moduleType={moduleType}
+                  isSignedIn={isSignedIn}
+                  {...thankYouModuleData[moduleType]}
+                />
+              ))}
+            </Column>
+            <Column cssOverrides={columnContainer}>
+              {secondColumn.map((moduleType) => (
+                <ThankYouModule
+                  moduleType={moduleType}
+                  isSignedIn={isSignedIn}
+                  {...thankYouModuleData[moduleType]}
+                />
+              ))}
+            </Column>
+          </Columns>
 
-					<Columns collapseUntil="desktop">
-						<Column cssOverrides={[columnContainer, firstColumnContainer]}>
-							{firstColumn.map((moduleType) => (
-								<ThankYouModule
-									moduleType={moduleType}
-									isSignedIn={isSignedIn}
-									{...thankYouModuleData[moduleType]}
-								/>
-							))}
-						</Column>
-						<Column cssOverrides={columnContainer}>
-							{secondColumn.map((moduleType) => (
-								<ThankYouModule
-									moduleType={moduleType}
-									isSignedIn={isSignedIn}
-									{...thankYouModuleData[moduleType]}
-								/>
-							))}
-						</Column>
-					</Columns>
-
-					<div css={buttonContainer}>
-						<LinkButton
-							href="https://www.theguardian.com"
-							priority="tertiary"
-							onClick={() =>
-								trackComponentClick(OPHAN_COMPONENT_ID_RETURN_TO_GUARDIAN)
-							}
-						>
-							Return to the Guardian
-						</LinkButton>
-					</div>
-				</Container>
-			</div>
-		</PageScaffold>
-	);
+          <div css={buttonContainer}>
+            <LinkButton
+              href="https://www.theguardian.com"
+              priority="tertiary"
+              onClick={() =>
+                trackComponentClick(OPHAN_COMPONENT_ID_RETURN_TO_GUARDIAN)
+              }
+            >
+              Return to the Guardian
+            </LinkButton>
+          </div>
+        </Container>
+      </div>
+    </PageScaffold>
+  );
 }
