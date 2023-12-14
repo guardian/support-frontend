85eb995f17bbfa9c4da743d249e43642faaf4b3d
Richard Bangay <richard.bangay@guardian.co.uk>
2023-12-14 11:20:53 +0000

    seperate browserstack builds into singular entities in browserstack automate by tagging them with the latest git commit id.
    update playwright browserstack test config
    
    update paypal form filling in functino to use id selectors instead of labels (noticed in browserstack sometimes the paypal form was showing up in dutch)
    
    update the paypal test user credentials used in the tests

--- support-e2e/browserstack.config.ts
+++ support-e2e/browserstack.config.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
-import { exec } from 'child_process';
+import { execSync } from 'child_process';
 import BrowserStackLocal from 'browserstack-local';
+import { Page } from '@playwright/test';
 
 interface BrowserDetails {
 	browser: string;
@@ -10,7 +11,7 @@ interface BrowserDetails {
   name: string;
 }
 
-const clientPlaywrightVersion = exec('npx playwright --version')
+const clientPlaywrightVersion = execSync('npx playwright --version')
 	.toString()
 	.trim()
 	.split(' ')[1] as string;
@@ -19,8 +20,8 @@ const browserStackProperties = {
 	'browserstack.username': process.env.BROWSERSTACK_USERNAME ?? '',
 	'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY ?? '',
 	'browserstack.local': false,
+	'browserstack.geoLocation': 'GB',
 	'client.playwrightVersion': clientPlaywrightVersion,
-	build: 'playwright-build',
 }
 
 export const bsLocal = new BrowserStackLocal.Local();
@@ -32,10 +33,29 @@ export const BS_LOCAL_ARGS = {
 export const getCdpEndpoint = (
 	browserDetails: BrowserDetails,
 ) => {
-  const caps = {...browserStackProperties, ...browserDetails}
+  const latestCommitID = execSync("git rev-parse --short HEAD").toString().trim();
+  const niceDateStringNow = new Intl.DateTimeFormat('en-GB', {
+    dateStyle: 'full',
+    timeStyle: 'long',
+  }).format(new Date());
+  const caps = {
+    ...browserStackProperties,
+    ...browserDetails,
+    build: `playwright-build-${latestCommitID}`,
+    name: `playwright E2E test - ${niceDateStringNow}`
+  };
 	const cdpUrl = `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(
 		JSON.stringify(caps),
 	)}`;
 	console.log(`--> ${cdpUrl}`);
 	return cdpUrl;
 };
+
+export const markTest = (page:Page, status:string, reason:string) => {
+    return page.evaluate(
+        _ => {},
+        `browserstack_executor: ${JSON.stringify({
+            action: 'setSessionStatus',
+            arguments: { status, reason }
+        })}`);
+}
