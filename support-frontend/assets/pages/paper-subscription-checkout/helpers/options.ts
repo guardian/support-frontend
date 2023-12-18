a8e5a8d0da68bdf5818b6d04ff81357a9c274b58
“LAKSHMIRPILLAI” <luxmi.r.pillai@gmail.com>
2023-12-18 10:12:34 +0000

    Removing unnecessary parts

--- support-frontend/assets/pages/paper-subscription-checkout/helpers/options.ts
+++ support-frontend/assets/pages/paper-subscription-checkout/helpers/options.ts
@@ -14,7 +14,6 @@ import {
 	ActivePaperProductTypes,
 	Everyday,
 } from 'helpers/productPrice/productOptions';
-import * as storage from 'helpers/storage/storage';
 import type { DateYMDString } from 'helpers/types/DateString';
 import { getQueryParameter } from 'helpers/urls/url';
 import { formatMachineDate } from 'helpers/utilities/dateConversions';
@@ -22,35 +21,17 @@ import { getHomeDeliveryDays } from 'pages/paper-subscription-checkout/helpers/h
 import { getVoucherDays } from 'pages/paper-subscription-checkout/helpers/voucherDeliveryDays';
 
 function getProductOption(): PaperProductOptions {
-	const PRODUCT_STORAGE_KEY = 'selectedProduct'; // create the key for product in session storage
-	const productInUrl = getQueryParameter('product'); // get product from Query |Params
-	const productInSessionStorage = storage.getSession(PRODUCT_STORAGE_KEY); // get product from Session Storage
-	const productInUrlOrSessionStorage = productInUrl
-		? productInUrl
-		: productInSessionStorage; // get the value from url or session storage(whichever is available)
-	if (productInUrl) {
-		storage.setSession(PRODUCT_STORAGE_KEY, productInUrl); // always set product in session storage if  there is a value in the url
-	}
+	const productInUrl = getQueryParameter('product');
+
 	return (
-		ActivePaperProductTypes.find(
-			(product) => product === productInUrlOrSessionStorage,
-		) ?? Everyday
+		ActivePaperProductTypes.find((product) => product === productInUrl) ??
+		Everyday
 	);
 }
 
 function getFulfilmentOption(): PaperFulfilmentOptions {
-	const FULFILMENT_STORAGE_KEY = 'selectedFulfilment';
 	const fulfilmentInUrl = getQueryParameter('fulfilment');
-	const fulfilmentInSessionStorage = storage.getSession(FULFILMENT_STORAGE_KEY);
-	const fulfilmentInUrlOrSessionStorage = fulfilmentInUrl
-		? fulfilmentInUrl
-		: fulfilmentInSessionStorage;
-	if (fulfilmentInUrl) {
-		storage.setSession(FULFILMENT_STORAGE_KEY, fulfilmentInUrl);
-	}
-	return fulfilmentInUrlOrSessionStorage === 'HomeDelivery'
-		? HomeDelivery
-		: Collection;
+	return fulfilmentInUrl === 'HomeDelivery' ? HomeDelivery : Collection;
 }
 
 function getDays(
