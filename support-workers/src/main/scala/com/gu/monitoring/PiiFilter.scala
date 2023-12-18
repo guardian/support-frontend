85fc1680da401babae72ecaafa675193d03cb8f9
James Gorrie <jamesgorrie@users.noreply.github.com>
2023-12-18 14:19:20 +0000

    fix: use getMarkerList over deprecated getMarker in PiiFilter

--- support-workers/src/main/scala/com/gu/monitoring/PiiFilter.scala
+++ support-workers/src/main/scala/com/gu/monitoring/PiiFilter.scala
@@ -3,10 +3,17 @@ package com.gu.monitoring
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.filter.Filter
 import ch.qos.logback.core.spi.FilterReply
+import org.slf4j.Marker
+
+import scala.jdk.CollectionConverters._
 
 class PiiFilter extends Filter[ILoggingEvent] {
-  override def decide(event: ILoggingEvent): FilterReply =
-    if (event.getMarker.contains(SafeLogger.sanitizedLogMessage))
+  override def decide(event: ILoggingEvent): FilterReply = {
+    val sanitizedMarkerExists = event.getMarkerList.asScala.toList
+      .collectFirst(marker => marker.contains(SafeLogger.sanitizedLogMessage))
+      .nonEmpty
+    if (sanitizedMarkerExists)
       FilterReply.ACCEPT
     else FilterReply.DENY
+  }
 }
