f4ff6643ed1ba804808018748a6823258233e33a
Tom Forbes <tom.forbes@theguardian.com>
2024-02-06 12:50:52 +0000

    Add excludeIfInReferrerControlledTest to AB test model (#5661)
    * Add excludeIfInReferrerControlledTest to AB test model
    
    * prettier
    
    * tidy

--- support-frontend/assets/helpers/redux/commonState/reducer.ts
+++ support-frontend/assets/helpers/redux/commonState/reducer.ts
@@ -4,7 +4,6 @@ import type { ContributionTypes } from 'helpers/contributions';
 import { CountryGroup } from 'helpers/internationalisation';
 import type { IsoCountry } from 'helpers/internationalisation/country';
 import { fromCountryGroupId } from 'helpers/internationalisation/currency';
-import type { Participations } from '../../abTests/abtest';
 import type { CommonStateSetupData, Internationalisation } from './state';
 import { initialCommonState } from './state';
 
@@ -22,24 +21,11 @@ function getInternationalisationFromCountry(
 	};
 }
 
-function getABTestNames(abParticipations: Participations) {
-	const isUserInSupporterPlusABTest = 'supporterPlusOnly' in abParticipations;
-
-	const abTestType = isUserInSupporterPlusABTest
-		? { supporterPlusOnly: abParticipations.supporterPlusOnly }
-		: abParticipations;
-
-	return abTestType;
-}
-
 export const commonSlice = createSlice({
 	name: 'common',
 	initialState: initialCommonState,
 	reducers: {
 		setInitialCommonState(state, action: PayloadAction<CommonStateSetupData>) {
-			action.payload.abParticipations = getABTestNames(
-				action.payload.abParticipations,
-			);
 			const {
 				campaign,
 				referrerAcquisitionData,
