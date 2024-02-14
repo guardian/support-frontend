e22eefddbce6f1cccb1362e338daed3ff3a2ffb9
George Haberis <george.haberis@guardian.co.uk>
2024-02-14 14:51:51 +0000

    fix threeTierCheckout canRun function page targetting (#5696)

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -135,14 +135,11 @@ export const tests: Tests = {
 		 * - /subscribe/weekly/checkout
 		 */
 		canRun: () => {
-			// Contribution pages
-			const isContribution =
+			// Contribute pages
+			const isContributionLandingPageOrThankyou =
 				window.location.pathname.match(
-					/\/uk|us|au|eu|int|nz|ca\/contribute(\/.*)?$/,
+					pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
 				) !== null;
-			const isThankYou =
-				window.location.pathname.match(/\/uk|us|au|eu|int|nz|ca\/thankyou/) !==
-				null;
 
 			// Weekly pages
 			const urlParams = new URLSearchParams(window.location.search);
@@ -151,7 +148,9 @@ export const tests: Tests = {
 			const isWeeklyCheckout =
 				window.location.pathname === '/subscribe/weekly/checkout';
 
-			return isContribution || isThankYou || (isWeeklyCheckout && isThirdTier);
+			return (
+				isContributionLandingPageOrThankyou || (isWeeklyCheckout && isThirdTier)
+			);
 		},
 	},
 };
