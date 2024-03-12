eca44f9267616ece4334f6aae935109a95ec1344
James Gorrie <jamesgorrie@users.noreply.github.com>
2024-03-12 14:21:34 +0000

    fix: prefix names with test info in e2e

--- support-e2e/tests/utils/users.ts
+++ support-e2e/tests/utils/users.ts
@@ -1,16 +1,19 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-function generateString(length:number) {
-    let result = '';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+function generateString(length: number) {
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
-export const firstName = () => `${generateString(5)}TestF`;
-export const lastName = () => `${generateString(5)}TestL`;
+export const firstName = () =>
+  `test.support.frontend.e2e.firstName+${generateString(5)}`;
+export const lastName = () =>
+  `test.support.frontend.e2e.lastName+${generateString(5)}`;
 export const email = () => `test${uuidv4()}@theguardian.com`;
