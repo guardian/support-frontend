6a70d114a3a82e8e4fcb7283f1dd3aae2b8e2b2d
GHaberis <george.haberis@guardian.co.uk>
2024-03-14 11:05:53 +0000

    include Guardian specific prettier rules

--- support-e2e/playwright.base.config.ts
+++ support-e2e/playwright.base.config.ts
@@ -1,37 +1,37 @@
-import { devices } from "@playwright/test";
-import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from '@playwright/test';
+import type { PlaywrightTestConfig } from '@playwright/test';
 
 export const baseObject: PlaywrightTestConfig = {
-  testDir: "tests",
-  testMatch: "**/*.test.ts",
+	testDir: 'tests',
+	testMatch: '**/*.test.ts',
 
-  /* Maximum time one test can run for. */
-  timeout: 120 * 1000,
-  expect: {
-    timeout: 90000,
-  },
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  retries: 1,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
-  use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-    baseURL: "https://support.theguardian.com",
-  },
+	/* Maximum time one test can run for. */
+	timeout: 120 * 1000,
+	expect: {
+		timeout: 90000,
+	},
+	fullyParallel: true,
+	/* Fail the build on CI if you accidentally left test.only in the source code. */
+	forbidOnly: !!process.env.CI,
+	retries: 1,
+	/* Opt out of parallel tests on CI. */
+	workers: process.env.CI ? 1 : undefined,
+	reporter: 'html',
+	use: {
+		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+		actionTimeout: 0,
+		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		trace: 'on-first-retry',
+		baseURL: 'https://support.theguardian.com',
+	},
 
-  projects: [
-    {
-      name: "chromium",
-      use: {
-        ...devices["Desktop Chrome"],
-        viewport: { width: 1280, height: 720 },
-      },
-    },
-  ],
+	projects: [
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+				viewport: { width: 1280, height: 720 },
+			},
+		},
+	],
 };
