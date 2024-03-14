02595657922008fc805bb7a0620d4b3d18678a3a
GHaberis <george.haberis@guardian.co.uk>
2024-03-14 17:04:11 +0000

    prettify support-e2e

--- support-e2e/tests/utils/directDebitDetails.ts
+++ support-e2e/tests/utils/directDebitDetails.ts
@@ -1,22 +1,22 @@
-import { Page } from "@playwright/test";
+import { Page } from '@playwright/test';
 
 export const fillInDirectDebitDetails = async (
-  page: Page,
-  pageType: "subscription" | "contribution"
+	page: Page,
+	pageType: 'subscription' | 'contribution',
 ) => {
-  await page
-    .getByLabel(
-      pageType === "subscription" ? "Bank account holder name" : "Account name"
-    )
-    .fill("CP Scott");
-  await page.getByLabel("Sort code").fill("200000");
-  await page.getByLabel("Account number").fill("55779911");
+	await page
+		.getByLabel(
+			pageType === 'subscription' ? 'Bank account holder name' : 'Account name',
+		)
+		.fill('CP Scott');
+	await page.getByLabel('Sort code').fill('200000');
+	await page.getByLabel('Account number').fill('55779911');
 
-  await page
-    .getByRole("checkbox", {
-      name:
-        "I confirm that I am the account holder and I am solely able to\n" +
-        "\t\t\t\t\t\t\tauthorise debit from the account",
-    })
-    .check();
+	await page
+		.getByRole('checkbox', {
+			name:
+				'I confirm that I am the account holder and I am solely able to\n' +
+				'\t\t\t\t\t\t\tauthorise debit from the account',
+		})
+		.check();
 };
