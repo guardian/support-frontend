e90d8a947580a7e6ba9997c55f1bea4211e80763
George Haberis <george.haberis@guardian.co.uk>
2024-03-15 10:14:49 +0000

    Prettify support-e2e project (#5807)
    * e2e tests for 3-tier checkout
    
    * prettify support-e2e
    
    * remove tiered tests

--- support-e2e/tests/utils/testUserDetails.ts
+++ support-e2e/tests/utils/testUserDetails.ts
@@ -1,12 +1,12 @@
-import { Page } from "@playwright/test";
+import { Page } from '@playwright/test';
 
 export const setTestUserDetails = async (
-  page:Page,
-  firstName: string,
-  lastName: string,
-  email: string,
+	page: Page,
+	firstName: string,
+	lastName: string,
+	email: string,
 ) => {
-  await page.getByLabel("Email address").fill(email);
-  await page.getByLabel("First name").fill(firstName);
-  await page.getByLabel("Last name").fill(lastName);
+	await page.getByLabel('Email address').fill(email);
+	await page.getByLabel('First name').fill(firstName);
+	await page.getByLabel('Last name').fill(lastName);
 };
