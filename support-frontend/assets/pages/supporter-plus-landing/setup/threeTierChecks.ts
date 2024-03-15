235230782d1367232bc6bb9b89552e6340196500
GHaberis <george.haberis@guardian.co.uk>
2024-03-15 14:53:38 +0000

    update path logic

--- support-frontend/assets/pages/supporter-plus-landing/setup/threeTierChecks.ts
+++ support-frontend/assets/pages/supporter-plus-landing/setup/threeTierChecks.ts
@@ -9,8 +9,14 @@ export const threeTierCheckoutEnabled = (
 	const isPaperCheckout = window.location.pathname.startsWith(
 		'/subscribe/paper/checkout',
 	);
-	const isDigitalEditionCheckout = window.location.pathname.startsWith(
-		'/subscribe/digitaledition/checkout',
+	/**
+	 * Unlike Paper and Guardian Weekly the
+	 * Digital Edition path includes region eg: uk/subscribe/digitaledition
+	 * so we just check for presence of /subscribe/digitaledition substring in
+	 * window.location.path here instead of using startsWith.
+	 */
+	const isDigitalEditionCheckout = window.location.pathname.includes(
+		'/subscribe/digitaledition',
 	);
 
 	if (isPaperCheckout || isDigitalEditionCheckout) {
