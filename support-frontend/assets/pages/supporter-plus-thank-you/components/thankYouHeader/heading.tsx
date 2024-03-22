15550fdd71e629d0834fdf0935d572881ccf0732
James Gorrie <jamesgorrie@users.noreply.github.com>
2024-03-22 15:51:45 +0000

    feat: promotion text on thank you page

--- support-frontend/assets/pages/supporter-plus-thank-you/components/thankYouHeader/heading.tsx
+++ support-frontend/assets/pages/supporter-plus-thank-you/components/thankYouHeader/heading.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import { from, space, titlepiece } from '@guardian/source-foundations';
 import type { ContributionType } from 'helpers/contributions';
 import { formatAmount } from 'helpers/forms/checkouts';
 import type { IsoCurrency } from 'helpers/internationalisation/currency';
@@ -6,43 +7,74 @@ import {
 	currencies,
 	spokenCurrencies,
 } from 'helpers/internationalisation/currency';
+import type { Promotion } from 'helpers/productPrice/promotions';
 
-const MAX_DISPLAY_NAME_LENGTH = 10;
-
-export const amountText = css`
-	background-color: #ffe500;
-	padding: 0 5px;
-`;
-
-interface HeadingProps {
-	name: string | null;
-	isOneOffPayPal: boolean;
-	amount: number | undefined;
-	currency: IsoCurrency;
-	contributionType: ContributionType;
-}
-
-function Heading({
-	name,
-	isOneOffPayPal,
-	amount,
-	currency,
-	contributionType,
-}: HeadingProps): JSX.Element {
-	const maybeNameAndTrailingSpace: string =
-		name && name.length < MAX_DISPLAY_NAME_LENGTH ? `${name} ` : '';
-
-	// Do not show special header to paypal/one-off as we don't have the relevant info after the redirect
-	if (isOneOffPayPal || !amount) {
+type MonthlyProps = {
+	amount: number;
+	isoCurrency: IsoCurrency;
+	promotion?: Promotion;
+	name?: string;
+};
+function Monthly({ isoCurrency, amount, promotion, name }: MonthlyProps) {
+	if (
+		/**
+		 * This is a check to see if the promo exists,
+		 * it's a little odd due to the type having
+		 * every prop optional.
+		 *
+		 * We could/should do some parsing pre-render to avoid this.
+		 * */
+		promotion?.discountedPrice &&
+		promotion.discount?.durationMonths
+	) {
 		return (
-			<div>
-				Thank you {maybeNameAndTrailingSpace}for your valuable contribution
-			</div>
+			<>
+				<h1 css={headerTitleText}>
+					Thank you {name}for supporting us with{' '}
+					<YellowAmount
+						amount={promotion.discountedPrice}
+						currency={isoCurrency}
+					/>
+					{`/month`}
+					<sup>*</sup>
+				</h1>
+				<p
+					css={css`
+						margin-top: ${space[2]}px;
+					`}
+				>
+					<sup>*</sup> for {promotion.discount.durationMonths} months, then{' '}
+					{formatAmount(
+						currencies[isoCurrency],
+						spokenCurrencies[isoCurrency],
+						amount,
+						false,
+					)}
+					{`/month`} afterwards unless you cancel.{' '}
+				</p>
+			</>
 		);
 	}
 
-	const currencyAndAmount = (
-		<span css={amountText}>
+	return (
+		<div>
+			Thank you {name}for supporting us with{' '}
+			<YellowAmount currency={isoCurrency} amount={amount} /> each month ❤️
+		</div>
+	);
+}
+
+const yellowAmountText = css`
+	background-color: #ffe500;
+	padding: 0 5px;
+`;
+type YellowAmountProps = {
+	amount: number;
+	currency: IsoCurrency;
+};
+function YellowAmount({ amount, currency }: YellowAmountProps) {
+	return (
+		<span css={yellowAmountText}>
 			{formatAmount(
 				currencies[currency],
 				spokenCurrencies[currency],
@@ -51,34 +83,75 @@ function Heading({
 			)}
 		</span>
 	);
+}
+
+const headerTitleText = css`
+	${titlepiece.small()};
+	font-size: 24px;
+	${from.tablet} {
+		font-size: 40px;
+	}
+`;
+type HeadingProps = {
+	name: string | null;
+	isOneOffPayPal: boolean;
+	amount: number | undefined;
+	currency: IsoCurrency;
+	contributionType: ContributionType;
+	promotion?: Promotion;
+};
+function Heading({
+	name,
+	isOneOffPayPal,
+	amount,
+	currency,
+	contributionType,
+	promotion,
+}: HeadingProps): JSX.Element {
+	const maybeNameAndTrailingSpace: string =
+		name && name.length < 10 ? `${name} ` : '';
+
+	// Do not show special header to paypal/one-off as we don't have the relevant info after the redirect
+	if (isOneOffPayPal || !amount) {
+		return (
+			<h1 css={headerTitleText}>
+				Thank you {maybeNameAndTrailingSpace}for your valuable contribution
+			</h1>
+		);
+	}
 
 	switch (contributionType) {
 		case 'ONE_OFF':
 			return (
-				<div>Thank you for supporting us today with {currencyAndAmount} ❤️</div>
+				<h1 css={headerTitleText}>
+					Thank you for supporting us today with{' '}
+					<YellowAmount currency={currency} amount={amount} /> ❤️
+				</h1>
 			);
 
 		case 'MONTHLY':
 			return (
-				<div>
-					Thank you {maybeNameAndTrailingSpace}for supporting us with{' '}
-					{currencyAndAmount} each month ❤️
-				</div>
+				<Monthly
+					amount={amount}
+					isoCurrency={currency}
+					promotion={promotion}
+					name={maybeNameAndTrailingSpace}
+				/>
 			);
 
 		case 'ANNUAL':
 			return (
-				<div>
+				<h1 css={headerTitleText}>
 					Thank you {maybeNameAndTrailingSpace}for supporting us with{' '}
-					{currencyAndAmount} each year ❤️
-				</div>
+					<YellowAmount currency={currency} amount={amount} /> each year ❤️
+				</h1>
 			);
 
 		default:
 			return (
-				<div>
+				<h1 css={headerTitleText}>
 					Thank you {maybeNameAndTrailingSpace}for your valuable contribution
-				</div>
+				</h1>
 			);
 	}
 }
