e8017a0678dec4da51af769b5a043cacdbcad538
John Duffell <john.duffell@guardian.co.uk>
2024-03-27 12:42:32 +0000

    tweak credential chain to improve lambda cold start

--- support-modules/aws/src/main/scala/com/gu/aws/aws.scala
+++ support-modules/aws/src/main/scala/com/gu/aws/aws.scala
@@ -11,9 +11,12 @@ package object aws {
       .builder()
       .credentialsProviders(
         credentials.ProfileCredentialsProvider.create(ProfileName),
-        credentials.InstanceProfileCredentialsProvider.builder().asyncCredentialUpdateEnabled(false).build(),
         credentials.EnvironmentVariableCredentialsProvider.create(),
         credentials.ContainerCredentialsProvider.builder().build(), // for use with lambda snapstart
+        credentials.InstanceProfileCredentialsProvider
+          .builder()
+          .asyncCredentialUpdateEnabled(false)
+          .build(), // last because it takes 2s to fail in Lambda/local
       )
       .build()
 
