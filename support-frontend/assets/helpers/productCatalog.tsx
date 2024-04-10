826477f79a512789bfeba2359607fa9ab2fa352f
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-04-10 11:47:05 +0100

    feat: productCatolog offer tooltip added within the jsx element

--- support-frontend/assets/helpers/productCatalog.tsx
+++ support-frontend/assets/helpers/productCatalog.tsx
@@ -1,4 +1,19 @@
+import { css } from '@emotion/react';
+import { from } from '@guardian/source-foundations';
+import Tooltip from 'components/tooltip/Tooltip';
+
 export const productCatalog = window.guardian.productCatalog;
+
+const tooltipOfferStyle = css`
+	> div {
+		display: none;
+		${from.desktop} {
+			display: inline;
+			margin-left: 1px;
+			vertical-align: middle;
+		}
+	}
+`;
 
 export const productCatalogDescription = {
 	SupporterPlusWithGuardianWeekly: {
@@ -53,10 +68,23 @@ export const productCatalogDescription = {
 						<span style={{ fontWeight: 'bold' }}>
 							A free book as our gift to you.**{' '}
 						</span>
-						Choose from a selection curated by Guardian staff
+						Choose from a selection curated by Guardian staff{' '}
+						<span css={tooltipOfferStyle}>
+							<Tooltip
+								children={
+									<p>
+										{
+											'A free book as our gift to you. Choose from a selection curated by Guardian staff.'
+										}
+									</p>
+								}
+								xAxisOffset={108}
+								yAxisOffset={12}
+								placement="bottom"
+							></Tooltip>
+						</span>
 					</p>
 				),
-				tooltip: 'tba',
 			},
 		],
 	},
