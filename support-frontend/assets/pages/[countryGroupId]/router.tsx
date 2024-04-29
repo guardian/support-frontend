dda3677023d90c19b5609de4587f5f277512d29d
James Gorrie <jamesgorrie@users.noreply.github.com>
2024-04-29 15:18:49 +0100

    refactor: flat the map

--- support-frontend/assets/pages/[countryGroupId]/router.tsx
+++ support-frontend/assets/pages/[countryGroupId]/router.tsx
@@ -4,18 +4,16 @@ import { geoIds } from 'pages/geoIdConfig';
 import { Checkout } from './checkout';
 
 const router = createBrowserRouter(
-	geoIds
-		.map((geoId) => [
-			{
-				path: `/${geoId}/checkout`,
-				element: <Checkout geoId={geoId} />,
-			},
-			{
-				path: `/${geoId}/thank-you`,
-				element: <div>Thanks!</div>,
-			},
-		])
-		.flat(),
+	geoIds.flatMap((geoId) => [
+		{
+			path: `/${geoId}/checkout`,
+			element: <Checkout geoId={geoId} />,
+		},
+		{
+			path: `/${geoId}/thank-you`,
+			element: <div>Thanks!</div>,
+		},
+	]),
 );
 
 function Router() {
