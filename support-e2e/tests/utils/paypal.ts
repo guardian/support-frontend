e22993061f999f43ed6a094be40fa68f8435da1d
GHaberis <george.haberis@guardian.co.uk>
2024-05-01 16:09:59 +0100

    randomise the username used in our Paypal e2e tests

--- support-e2e/tests/utils/paypal.ts
+++ support-e2e/tests/utils/paypal.ts
@@ -1,13 +1,27 @@
 import 'dotenv/config';
 import { Page } from '@playwright/test';
 
+const paypalUsernamePrefixes = [
+	'sb-pxv43g30517058',
+	'sb-8xque30655602',
+	'sb-hk3ov30650585',
+];
+
 export const fillInPayPalDetails = async (page: Page) => {
-	await page.locator('#email').fill('sb-k6ax328721376@personal.example.com');
+	const paypalUsernamePrefix =
+		paypalUsernamePrefixes[
+			Math.floor(Math.random() * paypalUsernamePrefixes.length)
+		];
+	await page
+		.locator('#email')
+		.fill(`${[paypalUsernamePrefix]}@personal.example.com`);
 	const nextButton = page.locator('#btnNext');
 	if (await nextButton.isVisible()) {
 		await nextButton.click();
 	}
-	await page.locator('#password').fill(`${process.env.PAYPAL_TEST_PASSWORD}`);
+	await page
+		.locator('#password')
+		.fill(`${paypalUsernamePrefix}-${process.env.PAYPAL_TEST_PASSWORD}`);
 	const loginButton = page.locator('#btnLogin');
 	if (await loginButton.isVisible()) {
 		await loginButton.click();
