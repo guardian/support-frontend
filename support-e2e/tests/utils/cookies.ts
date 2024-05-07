5e17f7986710c45494344c21085fc2f9f65bf055
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-05-07 17:18:46 +0100

    add check for cookie setting and removal to e2e tests

--- support-e2e/tests/utils/cookies.ts
+++ support-e2e/tests/utils/cookies.ts
@@ -1,4 +1,4 @@
-import { BrowserContext } from '@playwright/test';
+import { BrowserContext, expect } from '@playwright/test';
 
 export const setTestCookies = async (
 	context: BrowserContext,
@@ -11,4 +11,18 @@ export const setTestCookies = async (
 		{ name: '_post_deploy_user', value: 'true', domain, path: '/' },
 		{ name: 'GU_TK', value: '1.1', domain, path: '/' },
 	]);
+};
+
+export const checkAbandonedBasketCookieExists = async (
+	context: BrowserContext,
+) => {
+	const cookies = await context.cookies();
+	expect(cookies.map((c) => c.name)).toContain('GU_CO_INCOMPLETE');
+};
+
+export const checkAbandonedBasketCookieRemoved = async (
+	context: BrowserContext,
+) => {
+	const cookies = await context.cookies();
+	expect(cookies.map((c) => c.name)).not.toContain('GU_CO_INCOMPLETE');
 };
