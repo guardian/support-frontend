97cbd1e5c1508b66cbbfacc887bf74718f7735a0
GHaberis <george.haberis@guardian.co.uk>
2024-05-08 10:17:33 +0100

    enable tierCardCtaCopy AB test

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -144,7 +144,7 @@ export const tests: Tests = {
 				size: 1,
 			},
 		},
-		isActive: false,
+		isActive: true,
 		referrerControlled: false, // ab-test name not needed to be in paramURL
 		seed: 6,
 		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
