954639e5c6e0d396f646ebeb0a35d3f99f449948
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-05-28 18:04:17 +0100

    feat: router tracking and consent simplified, abtest targetpageUrl's have generic checkout added including tests

--- support-frontend/assets/pages/[countryGroupId]/router.tsx
+++ support-frontend/assets/pages/[countryGroupId]/router.tsx
@@ -1,21 +1,11 @@
-import ophan from 'ophan';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import * as abTest from 'helpers/abTests/abtest';
-import { Country, CountryGroup } from 'helpers/internationalisation';
+import { setUpTrackingAndConsents } from 'helpers/page/page';
 import { renderPage } from 'helpers/rendering/render';
-import { trackAbTests } from 'helpers/tracking/ophan';
 import { geoIds } from 'pages/geoIdConfig';
 import { Checkout } from './checkout';
 import { ThankYou } from './thank-you';
 
-const countryId = Country.detect();
-const countryGroupId = CountryGroup.detect();
-const participations = abTest.init({
-	countryId,
-	countryGroupId,
-});
-ophan.init();
-trackAbTests(participations);
+setUpTrackingAndConsents();
 
 const router = createBrowserRouter(
 	geoIds.flatMap((geoId) => [
