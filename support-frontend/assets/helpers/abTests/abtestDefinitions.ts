1af7f74525a6f5af69882faf933effb7f43c28db
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-06-12 10:48:34 +0100

    create AB test definition and check for variant before setting cookie

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -142,4 +142,24 @@ export const tests: Tests = {
 		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
 		excludeCountriesSubjectToContributionsOnlyAmounts: true,
 	},
+	abandonedBasket: {
+		variants: [
+			{
+				id: 'control',
+			},
+			{
+				id: 'variant',
+			},
+		],
+		audiences: {
+			ALL: {
+				offset: 0,
+				size: 1,
+			},
+		},
+		isActive: true,
+		referrerControlled: false, // ab-test name not needed to be in paramURL
+		seed: 1,
+		excludeCountriesSubjectToContributionsOnlyAmounts: true,
+	},
 };
