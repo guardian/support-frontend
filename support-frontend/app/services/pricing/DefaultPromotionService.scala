fe4ed2e61182a54cb443d1f3e6bf6901a23d182b
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-07-05 17:23:41 +0100

    Fetch default promos from code not dev when running locally

--- support-frontend/app/services/pricing/DefaultPromotionService.scala
+++ support-frontend/app/services/pricing/DefaultPromotionService.scala
@@ -1,7 +1,7 @@
 package services.pricing
 
 import com.gu.aws.{AwsCloudWatchMetricPut, AwsS3Client}
-import com.gu.support.config.Stage
+import com.gu.support.config.{Stage, Stages}
 import io.circe._
 import io.circe.parser._
 import io.circe.generic.auto._
@@ -9,7 +9,8 @@ import org.apache.pekko.actor.ActorSystem
 import com.gu.aws.AwsCloudWatchMetricPut.{client => cloudwatchClient}
 import com.gu.aws.AwsCloudWatchMetricSetup.defaultPromotionsLoadingFailure
 import com.gu.aws.AwsS3Client.S3Location
-import com.gu.support.catalog.{DigitalPack, GuardianWeekly, Paper, Product, SupporterPlus}
+import com.gu.support.catalog.{DigitalPack, GuardianWeekly, Paper, Product, SupporterPlus, TierThree}
+import com.gu.support.config.Stages.CODE
 import com.typesafe.scalalogging.LazyLogging
 import services.pricing.DefaultPromotionService.DefaultPromotions
 
@@ -28,6 +29,7 @@ object DefaultPromotionService {
       paper: List[String],
       digital: List[String],
       supporterPlus: List[String],
+      tierThree: List[String],
   )
 
   implicit val decoder = Decoder[DefaultPromotions]
@@ -41,14 +43,20 @@ class DefaultPromotionServiceS3(
     extends DefaultPromotionService
     with LazyLogging {
 
+  // There is a DEV version of the default promos file, but CODE is much more use to us in regular use
+  private val awsStage = stage match {
+    case Stages.DEV => CODE
+    case _ => stage
+  }
+
   private val s3Uri =
-    S3Location("support-admin-console", s"$stage/default-promos.json")
+    S3Location("support-admin-console", s"$awsStage/default-promos.json")
 
   private val defaultPromoCodes = new AtomicReference[DefaultPromotions](
-    DefaultPromotions(guardianWeekly = Nil, paper = Nil, digital = Nil, supporterPlus = Nil),
+    DefaultPromotions(guardianWeekly = Nil, paper = Nil, digital = Nil, supporterPlus = Nil, tierThree = Nil),
   )
 
-  private def fetch(): Try[DefaultPromotions] =
+  def fetch(): Try[DefaultPromotions] =
     for {
       raw <- client.fetchAsString(s3Uri)
       defaultPromos <- decode[DefaultPromotions](raw).toTry
@@ -72,6 +80,7 @@ class DefaultPromotionServiceS3(
       case Paper => defaultPromoCodes.get().paper
       case DigitalPack => defaultPromoCodes.get().digital
       case SupporterPlus => defaultPromoCodes.get().supporterPlus
+      case TierThree => defaultPromoCodes.get().tierThree
       case _ => Nil
     }
 
