2d037efde97a9e8c2652d74d24fb43b68027c417
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-07-05 15:17:57 +0100

    Integration test used to work out what was wrong with the T3 promo codes

--- support-frontend/test/services/pricing/PriceSummaryServiceIntegrationSpec.scala
+++ support-frontend/test/services/pricing/PriceSummaryServiceIntegrationSpec.scala
@@ -2,16 +2,43 @@ package services.pricing
 
 import org.apache.pekko.actor.ActorSystem
 import com.gu.aws.AwsS3Client
-import com.gu.i18n.CountryGroup.{UK, US}
+import com.gu.i18n.{CountryGroup, Currency}
+import com.gu.i18n.CountryGroup.{Australia, Canada, Europe, NewZealand, RestOfTheWorld, UK, US}
+import com.gu.i18n.Currency.{AUD, CAD, EUR, GBP, NZD, USD}
 import com.gu.support.catalog._
 import com.gu.support.config.{Stages, TouchPointEnvironments}
 import com.gu.support.promotions.PromotionServiceSpec
-import com.gu.support.workers.{Annual, Quarterly}
+import com.gu.support.workers.{Annual, BillingPeriod, Monthly, Quarterly}
 import com.gu.support.zuora.api.ReaderType.Gift
 import com.gu.test.tags.annotations.IntegrationTest
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
+
+case class PromoTestData(
+    promoCode: String,
+    countryGroup: CountryGroup,
+    billingPeriod: BillingPeriod,
+    currency: Currency,
+)
+object PromoTestData {
+  val t3Promos = List(
+    PromoTestData("TIER3_UK_MONTHLY", UK, Monthly, GBP),
+    PromoTestData("TIER3_UK_ANNUAL", UK, Annual, GBP),
+    PromoTestData("TIER3_US_MONTHLY", US, Monthly, USD),
+    PromoTestData("TIER3_US_ANNUAL", US, Annual, USD),
+    PromoTestData("TIER3_AU_MONTHLY", Australia, Monthly, AUD),
+    PromoTestData("TIER3_AU_ANNUAL", Australia, Annual, AUD),
+    PromoTestData("TIER3_CA_MONTHLY", Canada, Monthly, CAD),
+    PromoTestData("TIER3_CA_ANNUAL", Canada, Annual, CAD),
+    PromoTestData("TIER3_EU_MONTHLY", Europe, Monthly, EUR),
+    PromoTestData("TIER3_EU_ANNUAL", Europe, Annual, EUR),
+    PromoTestData("TIER3_NZ_MONTHLY", NewZealand, Monthly, NZD),
+    PromoTestData("TIER3_NZ_ANNUAL", NewZealand, Annual, NZD),
+    PromoTestData("TIER3_INT_MONTHLY", RestOfTheWorld, Monthly, USD),
+    PromoTestData("TIER3_INT_ANNUAL", RestOfTheWorld, Annual, USD),
+  )
+}
 
 @IntegrationTest
 class PriceSummaryServiceIntegrationSpec extends AsyncFlatSpec with Matchers with LazyLogging {
@@ -30,10 +57,13 @@ class PriceSummaryServiceIntegrationSpec extends AsyncFlatSpec with Matchers wit
     result(UK)(Domestic)(NoProductOptions).size shouldBe 4
   }
 
-  "PriceSummaryService" should "return prices for Tier Three" in {
-    val result = service.getPrices(TierThree, Nil)
-    result.size shouldBe 7
-    result(UK)(Domestic)(NoProductOptions).size shouldBe 2
+  "PriceSummaryService" should "should not return any Tier Three promotions in the Supporter Plus pricing" in {
+    val failures = PromoTestData.t3Promos.filter(promo => {
+      import promo._
+      val result = service.getPrices(SupporterPlus, List(promo.promoCode))
+      result(countryGroup)(NoFulfilmentOptions)(NoProductOptions)(billingPeriod)(currency).promotions.nonEmpty
+    })
+    failures.size shouldBe 0
   }
 
   it should "return gift prices" in {
