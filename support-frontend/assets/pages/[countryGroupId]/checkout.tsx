7c97137720c84f55972c0b0c4fae25e1994adffa
James Gorrie <jamesgorrie@users.noreply.github.com>
2024-07-24 19:43:59 +0100

    fix: remove CLS from stripe ExpressCheckout

--- support-frontend/assets/pages/[countryGroupId]/checkout.tsx
+++ support-frontend/assets/pages/[countryGroupId]/checkout.tsx
@@ -315,12 +315,13 @@ function ChangeButton({ geoId }: ChangeButtonProps) {
 type Props = {
 	geoId: GeoId;
 	appConfig: AppConfig;
-	useStripeExpressCheckout: boolean;
+	useStripeExpressCheckout?: boolean;
 };
+
 function CheckoutComponent({
 	geoId,
 	appConfig,
-	useStripeExpressCheckout,
+	useStripeExpressCheckout = false,
 }: Props) {
 	/** we unset any previous orders that have been made */
 	unsetThankYouOrder();
@@ -954,65 +955,75 @@ function CheckoutComponent({
 								<BoxContents>
 									{useStripeExpressCheckout && (
 										<>
-											<ExpressCheckoutElement
-												onClick={({ resolve }) => {
-													/** @see https://docs.stripe.com/elements/express-checkout-element/accept-a-payment?locale=en-GB#handle-click-event */
-													const options = {
-														emailRequired: true,
-													};
-													resolve(options);
-												}}
-												onConfirm={async (event) => {
-													if (!(stripe && elements)) {
-														console.error('Stripe not loaded');
-														return;
-													}
+											<div
+												/**
+												 * This is the height of the `ExpressCheckoutElement` to avoid `CLS`
+												 * The element itself animates in, but it's not very smooth
+												 */
+												css={css`
+													height: 44px;
+												`}
+											>
+												<ExpressCheckoutElement
+													onClick={({ resolve }) => {
+														/** @see https://docs.stripe.com/elements/express-checkout-element/accept-a-payment?locale=en-GB#handle-click-event */
+														const options = {
+															emailRequired: true,
+														};
+														resolve(options);
+													}}
+													onConfirm={async (event) => {
+														if (!(stripe && elements)) {
+															console.error('Stripe not loaded');
+															return;
+														}
 
-													const { error: submitError } =
-														await elements.submit();
+														const { error: submitError } =
+															await elements.submit();
 
-													if (submitError) {
-														setErrorMessage(submitError.message);
-														return;
-													}
+														if (submitError) {
+															setErrorMessage(submitError.message);
+															return;
+														}
 
-													setPaymentMethod('StripeExpressCheckoutElement');
-													setStripeExpressCheckoutPaymentType(
-														event.expressPaymentType,
-													);
+														setPaymentMethod('StripeExpressCheckoutElement');
+														setStripeExpressCheckoutPaymentType(
+															event.expressPaymentType,
+														);
 
-													const name = event.billingDetails?.name ?? '';
+														const name = event.billingDetails?.name ?? '';
 
-													/**
-													 * splits by the last space, and uses the head as firstName
-													 * and tail as lastName
-													 */
-													const firstName = name
-														.substring(0, name.lastIndexOf(' ') + 1)
-														.trim();
-													const lastName = name
-														.substring(name.lastIndexOf(' ') + 1, name.length)
-														.trim();
-													setFirstName(firstName);
-													setLastName(lastName);
+														/**
+														 * splits by the last space, and uses the head as firstName
+														 * and tail as lastName
+														 */
+														const firstName = name
+															.substring(0, name.lastIndexOf(' ') + 1)
+															.trim();
+														const lastName = name
+															.substring(name.lastIndexOf(' ') + 1, name.length)
+															.trim();
+														setFirstName(firstName);
+														setLastName(lastName);
 
-													event.billingDetails?.email &&
-														setEmail(event.billingDetails.email);
+														event.billingDetails?.email &&
+															setEmail(event.billingDetails.email);
 
-													/**
-													 * There is a useEffect that listens to this and submits the form
-													 * when true
-													 */
-													setStripeExpressCheckoutSuccessful(true);
-												}}
-												options={{
-													paymentMethods: {
-														applePay: 'always',
-														googlePay: 'always',
-														link: 'never',
-													},
-												}}
-											/>
+														/**
+														 * There is a useEffect that listens to this and submits the form
+														 * when true
+														 */
+														setStripeExpressCheckoutSuccessful(true);
+													}}
+													options={{
+														paymentMethods: {
+															applePay: 'always',
+															googlePay: 'always',
+															link: 'never',
+														},
+													}}
+												/>
+											</div>
 											<Divider
 												displayText="or"
 												size="full"
@@ -1029,6 +1040,16 @@ function CheckoutComponent({
 													margin-top: 14px;
 													margin-bottom: 14px;
 													width: 100%;
+
+													@keyframes fadeIn {
+														0% {
+															opacity: 0;
+														}
+														100% {
+															opacity: 1;
+														}
+													}
+													animation: fadeIn 1s;
 												`}
 											/>
 										</>
