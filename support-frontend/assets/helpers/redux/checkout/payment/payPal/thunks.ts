b363fc81a70412d7638222e4377f56d1a9631c1a
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-08-07 17:29:48 +0100

    feat: displays correct price and promo price within the digital edition paypal screen

--- support-frontend/assets/helpers/redux/checkout/payment/payPal/thunks.ts
+++ support-frontend/assets/helpers/redux/checkout/payment/payPal/thunks.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { fetchJson } from 'helpers/async/fetch';
 import { billingPeriodFromContrib, getAmount } from 'helpers/contributions';
 import { loadPayPalRecurring } from 'helpers/forms/paymentIntegrations/payPalRecurringCheckout';
+import { getProductPrice } from 'helpers/productPrice/productPrices';
 import { getPromotion } from 'helpers/productPrice/promotions';
 import type { ContributionsState } from 'helpers/redux/contributionsStore';
 import { contributionsFormHasErrors } from 'helpers/redux/selectors/formValidation';
@@ -43,6 +44,68 @@ export const setUpPayPalPayment = createAsyncThunk<
 			state.page.checkoutForm.product.otherAmounts,
 			contributionType,
 		);
+		const finalAmount = promotion?.discountedPrice ?? amount;
+
+		const requestBody = {
+			amount: finalAmount,
+			billingPeriod,
+			currency: currencyId,
+			requireShippingAddress: false,
+		};
+
+		const payPalResponse = await fetchJson<{ token?: string }>(
+			routes.payPalSetupPayment,
+			{
+				credentials: 'include',
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Csrf-Token': csrfToken,
+				},
+				body: JSON.stringify(requestBody),
+			},
+		);
+
+		if (payPalResponse.token) {
+			resolve(payPalResponse.token);
+		} else {
+			throw new Error('PayPal token came back blank');
+		}
+	} catch (error) {
+		logException((error as Error).message);
+		reject(error as Error);
+	}
+});
+
+export const setUpPayPalPaymentDigitalEdition = createAsyncThunk<
+	unknown,
+	PayPalLoadFns,
+	{
+		state: ContributionsState;
+	}
+>('paypal/setUpPayment', async function setUp({ resolve, reject }, thunkApi) {
+	try {
+		const state = thunkApi.getState();
+		const errorsPreventOpening = contributionsFormHasErrors(state);
+
+		if (errorsPreventOpening) {
+			reject(new Error('form invalid'));
+		}
+
+		const { currencyId, countryId } = state.common.internationalisation;
+		const csrfToken = state.page.checkoutForm.csrf.token ?? '';
+		const { billingPeriod } = state.page.checkoutForm.product;
+		const promotion = getPromotion(
+			state.page.checkoutForm.product.productPrices,
+			countryId,
+			billingPeriod === 'Annual' ? 'Annual' : 'Monthly',
+		);
+		const amount = getProductPrice(
+			state.page.checkoutForm.product.productPrices,
+			countryId,
+			billingPeriod === 'Annual' ? 'Annual' : 'Monthly',
+		).price;
+
 		const finalAmount = promotion?.discountedPrice ?? amount;
 
 		const requestBody = {
