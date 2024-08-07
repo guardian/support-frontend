df85db3daf83f6b0a00e5f3bd56297ebe6392456
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-08-07 11:40:31 +0100

    feat: checkout container centered

--- support-frontend/assets/pages/[countryGroupId]/events/events.tsx
+++ support-frontend/assets/pages/[countryGroupId]/events/events.tsx
@@ -1,14 +1,33 @@
 import { css } from '@emotion/react';
-import { from, sport } from '@guardian/source/foundations';
-import { Container } from '@guardian/source/react-components';
+import { palette, space, textSans, until } from '@guardian/source/foundations';
+import { Column, Columns, Container } from '@guardian/source/react-components';
 import { FooterWithContents } from '@guardian/source-development-kitchen/react-components';
+import { Box, BoxContents } from 'components/checkoutBox/checkoutBox';
+import { CheckoutHeading } from 'components/checkoutHeading/checkoutHeading';
 import { Header } from 'components/headers/simpleHeader/simpleHeader';
 import { PageScaffold } from 'components/page/pageScaffold';
 import ThankYouFooter from 'pages/supporter-plus-thank-you/components/thankYouFooter';
 
-const checkoutContainer = css`
-	${from.tablet} {
-		background-color: ${sport[800]};
+const darkBackgroundContainerMobile = css`
+	background-color: ${palette.neutral[97]};
+	${until.tablet} {
+		background-color: ${palette.brand[400]};
+		border-bottom: 1px solid ${palette.brand[600]};
+	}
+`;
+
+const columns = css`
+	position: relative;
+	color: ${palette.neutral[7]};
+	${textSans.medium()};
+	padding-top: ${space[2]}px;
+`;
+
+const shorterBoxMargin = css`
+	:not(:last-child) {
+		${until.tablet} {
+			margin-bottom: ${space[2]}px;
+		}
 	}
 `;
 
@@ -25,32 +44,40 @@ export function Events() {
 				</FooterWithContents>
 			}
 		>
-			<div css={checkoutContainer}>
-				<Container>
-					<div className="tt-widget">
-						<div className="tt-widget-fallback">
-							<p>
-								<a
-									href={`https://tickets.theguardian.live/checkout/new-session/id/${eventId}/chk/${chk}/?ref=support-theguardian-com`}
-									target="_blank"
-								>
-									Click here to buy tickets
-								</a>
-							</p>
-						</div>
-						<script
-							src="https://cdn.tickettailor.com/js/widgets/min/widget.js"
-							data-url={`https://tickets.theguardian.live/checkout/new-session/id/${eventId}/chk/${chk}/?ref=support-theguardian-com`}
-							data-type="inline"
-							data-inline-minimal="true"
-							data-inline-show-logo="false"
-							data-inline-bg-fill="false"
-							data-inline-inherit-ref-from-url-param=""
-							data-inline-ref="support-theguardian-com"
-						></script>
-					</div>
-				</Container>
-			</div>
+			<CheckoutHeading withTopBorder={true}></CheckoutHeading>
+			<Container sideBorders cssOverrides={darkBackgroundContainerMobile}>
+				<Columns cssOverrides={columns} collapseUntil="tablet">
+					<Column span={[0, 2, 2, 3, 4]}></Column>
+					<Column span={[1, 8, 8, 8, 8]}>
+						<Box cssOverrides={shorterBoxMargin}>
+							<BoxContents>
+								<div className="tt-widget">
+									<div className="tt-widget-fallback">
+										<p>
+											<a
+												href={`https://tickets.theguardian.live/checkout/new-session/id/${eventId}/chk/${chk}/?ref=support-theguardian-com`}
+												target="_blank"
+											>
+												Click here to buy tickets
+											</a>
+										</p>
+									</div>
+									<script
+										src="https://cdn.tickettailor.com/js/widgets/min/widget.js"
+										data-url={`https://tickets.theguardian.live/checkout/new-session/id/${eventId}/chk/${chk}/?ref=support-theguardian-com`}
+										data-type="inline"
+										data-inline-minimal="true"
+										data-inline-show-logo="false"
+										data-inline-bg-fill="false"
+										data-inline-inherit-ref-from-url-param=""
+										data-inline-ref="support-theguardian-com"
+									></script>
+								</div>
+							</BoxContents>
+						</Box>
+					</Column>
+				</Columns>
+			</Container>
 		</PageScaffold>
 	);
 }
