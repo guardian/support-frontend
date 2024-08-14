8d94ea930c76c849bcfcd2060de70a4768b87039
Andrew Howe-Ely <114918544+andrewHEguardian@users.noreply.github.com>
2024-08-14 10:48:05 +0100

    PayPal env in generic checkout (#6227)
    * fix: paypal env in generic checkout
    
    * rearrange logic
    
    Co-authored-by: Rupert Bates <Rupert.Bates@guardian.co.uk>
    
    * make prettier
    
    ---------
    
    Co-authored-by: Rupert Bates <Rupert.Bates@guardian.co.uk>

--- support-frontend/assets/pages/[countryGroupId]/checkout.tsx
+++ support-frontend/assets/pages/[countryGroupId]/checkout.tsx
@@ -109,6 +109,7 @@ import {
 	getSupportAbTests,
 } from 'helpers/tracking/acquisitions';
 import { trackComponentClick } from 'helpers/tracking/behaviour';
+import { isProd } from 'helpers/urls/url';
 import type { GeoId } from 'pages/geoIdConfig';
 import { getGeoIdConfig } from 'pages/geoIdConfig';
 import { CheckoutDivider } from 'pages/supporter-plus-landing/components/checkoutDivider';
@@ -1809,7 +1810,9 @@ function CheckoutComponent({
 												/>
 
 												<PayPalButton
-													env={isTestUser ? 'sandbox' : 'production'}
+													env={
+														isProd() && !isTestUser ? 'production' : 'sandbox'
+													}
 													style={{
 														color: 'blue',
 														size: 'responsive',
