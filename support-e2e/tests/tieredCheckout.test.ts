b7777ba64ca07d5a836136081b092a61c60fcf15
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-08-14 12:16:11 +0100

    fix: temporary tier3 patch

--- support-e2e/tests/tieredCheckout.test.ts
+++ support-e2e/tests/tieredCheckout.test.ts
@@ -191,13 +191,14 @@ test.describe('Contribute/Subscribe Tiered Checkout', () => {
 			const ctaCopy =
 				testDetails.internationalisationId === 'US' ? 'Subscribe' : 'Support';
 
+			// TODO : Temporary tier3 generic checkout forced within Playwright
 			await setupPage(
 				page,
 				context,
 				baseURL,
 				`/${
 					testDetails.internationalisationId?.toLowerCase() || 'uk'
-				}/contribute`,
+				}/contribute#ab-tierThreeFromApi=variant`,
 			);
 
 			await page.getByRole('tab').getByText(testDetails.ratePlan).click();
