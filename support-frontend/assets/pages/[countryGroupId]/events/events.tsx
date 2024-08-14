54dc311ae70fb7b8fbabebdc7511223472ec9bed
James Gorrie <jamesgorrie@users.noreply.github.com>
2024-08-14 16:45:05 +0100

    feat: first attempt at showing event details

--- support-frontend/assets/pages/[countryGroupId]/events/events.tsx
+++ support-frontend/assets/pages/[countryGroupId]/events/events.tsx
@@ -1,5 +1,11 @@
 import { css } from '@emotion/react';
-import { palette, space, textSans, until } from '@guardian/source/foundations';
+import {
+	from,
+	palette,
+	space,
+	textSans,
+	until,
+} from '@guardian/source/foundations';
 import { Column, Columns, Container } from '@guardian/source/react-components';
 import { FooterWithContents } from '@guardian/source-development-kitchen/react-components';
 import { useParams } from 'react-router-dom';
@@ -14,9 +20,15 @@ import ThankYouFooter from 'pages/supporter-plus-thank-you/components/thankYouFo
 
 const darkBackgroundContainerMobile = css`
 	background-color: ${palette.neutral[97]};
+
 	${until.tablet} {
-		background-color: ${palette.brand[400]};
+		/* background-color: ${palette.brand[400]}; */
 		border-bottom: 1px solid ${palette.brand[600]};
+	}
+
+	div {
+		display: flex;
+		flex-direction: column;
 	}
 `;
 
@@ -54,12 +66,27 @@ const tscs = css`
 	}
 `;
 
+const iframe = css`
+	flex: 1 1 calc(100vh - 216px);
+
+	${from.tablet} {
+		flex: 1 1 calc(100vh - 171px);
+	}
+
+	${from.desktop} {
+		flex: 1 1 calc(100vh - 210px);
+	}
+`;
+
 export function Events() {
 	const isTestUser = !!cookie.get('_test_username');
 	const shouldUseCode = isTestUser || !isProd();
 	const ticketTailorUrl = shouldUseCode
 		? 'https://www.tickettailor.com/events/guardianlivecode'
 		: 'https://tickets.theguardian.live/events/guardianlive';
+
+	const urlSearchParams = new URLSearchParams(window.location.search);
+	const showDetails = urlSearchParams.get('showDetails') === 'true';
 
 	const params = useParams();
 	const eventId = params.eventId;
@@ -75,49 +102,68 @@ export function Events() {
 				</FooterWithContents>
 			}
 		>
-			<CheckoutHeading withTopBorder={true}></CheckoutHeading>
 			<Container sideBorders cssOverrides={darkBackgroundContainerMobile}>
-				<Columns cssOverrides={columns} collapseUntil="tablet">
-					<Column span={[0, 2, 2, 3, 4]}></Column>
-					<Column span={[1, 8, 8, 8, 8]}>
-						<Box cssOverrides={shorterBoxMargin}>
-							<BoxContents>
-								<div className="tt-widget">
-									<div className="tt-widget-fallback">
-										<p>
-											<a
-												href={`${ticketTailorUrl}/${eventId}/book`}
-												target="_blank"
-											>
-												Click here to buy tickets
-											</a>
-										</p>
-									</div>
-									<script
-										src="https://cdn.tickettailor.com/js/widgets/min/widget.js"
-										data-url={`${ticketTailorUrl}/${eventId}/book`}
-										data-type="inline"
-										data-inline-minimal="true"
-										data-inline-show-logo="false"
-										data-inline-bg-fill="false"
-										data-inline-inherit-ref-from-url-param=""
-										data-inline-ref="support-theguardian-com"
-									></script>
+				{showDetails && (
+					<iframe
+						css={iframe}
+						// src={`${ticketTailorUrl}/${eventId}`}
+						src="https://tickets.theguardian.live/events/guardianlive/1344365"
+						title="Event page"
+						sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+					></iframe>
+				)}
+				{!showDetails && (
+					<>
+						<CheckoutHeading withTopBorder={true}></CheckoutHeading>
+						<Columns cssOverrides={columns} collapseUntil="tablet">
+							<Column span={[0, 2, 2, 2, 2]}></Column>
+							<Column span={[1, 12, 12, 12, 12]}>
+								<Box cssOverrides={shorterBoxMargin}>
+									<BoxContents
+										cssOverrides={css`
+											display: flex;
+											flex-direction: column;
+											flex: 1 1 0%;
+										`}
+									>
+										<div className="tt-widget">
+											<div className="tt-widget-fallback">
+												<p>
+													<a
+														href={`${ticketTailorUrl}/${eventId}/book`}
+														target="_blank"
+													>
+														Click here to buy tickets
+													</a>
+												</p>
+											</div>
+											<script
+												src="https://cdn.tickettailor.com/js/widgets/min/widget.js"
+												data-url={`${ticketTailorUrl}/${eventId}/book`}
+												data-type="inline"
+												data-inline-minimal="true"
+												data-inline-show-logo="false"
+												data-inline-bg-fill="false"
+												data-inline-inherit-ref-from-url-param=""
+												data-inline-ref="support-theguardian-com"
+											></script>
+										</div>
+									</BoxContents>
+								</Box>
+								<div css={tscs}>
+									<p>
+										By proceeding, you agree to the Guardian Live events{' '}
+										{termsEvents}.
+									</p>
+									<p>
+										To find out what personal data we collect and how we use it,
+										please visit our {privacyPolicy}.
+									</p>
 								</div>
-							</BoxContents>
-						</Box>
-						<div css={tscs}>
-							<p>
-								By proceeding, you agree to the Guardian Live events{' '}
-								{termsEvents}.
-							</p>
-							<p>
-								To find out what personal data we collect and how we use it,
-								please visit our {privacyPolicy}.
-							</p>
-						</div>
-					</Column>
-				</Columns>
+							</Column>
+						</Columns>
+					</>
+				)}
 			</Container>
 		</PageScaffold>
 	);
