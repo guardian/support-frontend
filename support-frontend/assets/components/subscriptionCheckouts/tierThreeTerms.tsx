6a953f1aab149f98c7ae23f0dc050d7a12233c0c
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-08-14 09:33:07 +0100

    review: further tierThree terms cleanup

--- support-frontend/assets/components/subscriptionCheckouts/tierThreeTerms.tsx
+++ support-frontend/assets/components/subscriptionCheckouts/tierThreeTerms.tsx
@@ -34,40 +34,32 @@ export default function TierThreeTerms(props: {
 		props.paymentFrequency === 'year' ? 'annual' : 'monthly';
 	const productName = 'Digital + print';
 
-	const tierThreeCopy = () => {
-		return (
-			<>
-				<p>
-					By signing up, you are taking out a {productName} subscription. Your{' '}
-					{productName} subscription will auto-renew each{' '}
-					{props.paymentFrequency} unless cancelled. Your first payment will be
-					taken on the publication date of your first Guardian Weekly magazine
-					(as shown in the checkout) but you will start to receive your digital
-					benefits when you sign up. Unless you cancel, subsequent{' '}
-					{paymentFrequencyName} payments will be taken on this date using your
-					chosen payment method. You can cancel your Digital + print
-					subscription at any time before your next renewal date. If you cancel
-					your {productName} subscription within 14 days of signing up, your
-					subscription will stop immediately and we will not take the first
-					payment from you. Cancellation of your subscription after 14 days will
-					take effect at the end of your current {paymentFrequencyName} payment
-					period. To cancel go to&nbsp;
-					{ManageMyAccountLink} or see our {productName}{' '}
-					{termsLink('Terms', tierThreeTermsLink)}.
-				</p>
-				<p>
-					By proceeding, you are agreeing to the {productName}{' '}
-					{termsLink('Terms', tierThreeTermsLink)}.
-				</p>
-			</>
-		);
-	};
-
 	return (
 		<>
 			<FormSection>
 				<div css={tierThreeTerms}>
-					{tierThreeCopy()}
+					<p>
+						By signing up, you are taking out a {productName} subscription. Your{' '}
+						{productName} subscription will auto-renew each{' '}
+						{props.paymentFrequency} unless cancelled. Your first payment will
+						be taken on the publication date of your first Guardian Weekly
+						magazine (as shown in the checkout) but you will start to receive
+						your digital benefits when you sign up. Unless you cancel,
+						subsequent {paymentFrequencyName} payments will be taken on this
+						date using your chosen payment method. You can cancel your Digital +
+						print subscription at any time before your next renewal date. If you
+						cancel your {productName} subscription within 14 days of signing up,
+						your subscription will stop immediately and we will not take the
+						first payment from you. Cancellation of your subscription after 14
+						days will take effect at the end of your current{' '}
+						{paymentFrequencyName} payment period. To cancel go to&nbsp;
+						{ManageMyAccountLink} or see our {productName}{' '}
+						{termsLink('Terms', tierThreeTermsLink)}.
+					</p>
+					<p>
+						By proceeding, you are agreeing to the {productName}{' '}
+						{termsLink('Terms', tierThreeTermsLink)}.
+					</p>
 					<p>
 						To find out what personal data we collect and how we use it, please
 						visit our {termsLink('Privacy Policy', privacyLink)}.
