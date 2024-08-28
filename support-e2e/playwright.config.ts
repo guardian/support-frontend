6c8f53a8aa5f16c539a07cee39d964b34e7944f6
James Gorrie <jamesgorrie@users.noreply.github.com>
2024-08-28 09:25:57 +0100

    feat: categorise Playwright tests

---
+++ support-e2e/playwright.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+	testDir: 'tests',
+	testMatch: '**/*.test.ts',
+	/* Maximum time one test can run for. */
+	timeout: 120 * 1000,
+	fullyParallel: true,
+	/** Fail the build on CI if you accidentally left test.only in the source code. */
+	forbidOnly: !!process.env.CI,
+	// maxFailures: process.env.CI ? 1 : undefined,
+	retries: 1,
+	use: {
+		/** Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		trace: 'on-first-retry',
+		screenshot: 'only-on-failure',
+		video: 'on-first-retry',
+		baseURL: 'https://support.theguardian.com',
+	},
+	reporter: 'html',
+
+	/** We are using projects to split tests into priorities, and thus when they are run */
+	projects: [
+		/** This is an example of another project we might have, running pre-merge of a PR */
+		// {
+		// 	name: 'Pre-deploy'
+		// },
+		{
+			name: 'smoke',
+			testMatch: 'tests/smoke/*.test.ts',
+			use: {
+				...devices['Desktop Chrome'],
+				viewport: { width: 1280, height: 720 },
+			},
+		},
+		{
+			/** We run some tests on a Cron as they are sensitive to timeouts and limits from external services */
+			name: 'cron',
+			testMatch: 'tests/cron/*.test.ts',
+			use: {
+				...devices['Desktop Chrome'],
+				viewport: { width: 1280, height: 720 },
+			},
+		},
+	],
+});
