661f2e6781478ee370f0b7771c6e72aac2e6a950
James Gorrie <jamesgorrie@users.noreply.github.com>
2024-09-24 17:48:27 +0100

    feat: add info box with country change message in

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -43,25 +43,6 @@ export const pageUrlRegexes = {
 };
 
 export const tests: Tests = {
-	redirectCountryToCountryGroup: {
-		variants: [
-			// not really an AB test
-			{
-				id: 'variant',
-			},
-		],
-		audiences: {
-			ALL: {
-				offset: 0,
-				size: 0,
-			},
-		},
-		isActive: false,
-		referrerControlled: true,
-		seed: 1,
-		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
-		excludeCountriesSubjectToContributionsOnlyAmounts: true,
-	},
 	patronsOneOffOnly: {
 		variants: [
 			// not really an AB test
