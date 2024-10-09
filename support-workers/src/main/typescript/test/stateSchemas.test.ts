e2d570a1d882c28dd3a259b6b7b20bcab2c3a3c1
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-10-09 17:28:15 +0100

    Add jest test for CreateSalesforceContact schema

--- support-workers/src/main/typescript/test/stateSchemas.test.ts
+++ support-workers/src/main/typescript/test/stateSchemas.test.ts
@@ -1,13 +1,19 @@
 import {
   CreatePaymentMethodState,
   createPaymentMethodStateSchema,
+  createSalesforceContactStateSchema,
+  DirectDebitPaymentFields,
   StripePaymentFields,
 } from "../stateSchemas";
-import supporterPlusAnnualEurJson from "./fixtures/supporterPlusAnnualEUR.json";
-import contributionMonthlyUsdJson from "./fixtures/contributionMonthlyUSD.json";
-test("ChargedThroughDate is null in the model when it is null in Zuora", async () => {
+import createPaymentSupporterPlus from "./fixtures/createPaymentMethod/supporterPlusAnnualEUR.json";
+import createPaymentContribution from "./fixtures/createPaymentMethod/contributionMonthlyUSD.json";
+import createPaymentPaper from "./fixtures/createPaymentMethod/paperDirectDebit.json";
+import createSalesforceContactContribution from "./fixtures/createSalesforceContact/contributionMonthlyUSD.json";
+import createSalesforceContactPaper from "./fixtures/createSalesforceContact/paperDirectDebit.json";
+
+test("createPaymentMethodSchema works with real inputs", async () => {
   const supporterPlus = createPaymentMethodStateSchema.parse(
-    supporterPlusAnnualEurJson
+    createPaymentSupporterPlus
   );
   expect(supporterPlus.product.currency).toBe("EUR");
   expect(supporterPlus.acquisitionData?.ophanIds.pageviewId).toBe(
@@ -15,12 +21,46 @@ test("ChargedThroughDate is null in the model when it is null in Zuora", async (
   );
 
   const contribution: CreatePaymentMethodState =
-    createPaymentMethodStateSchema.parse(contributionMonthlyUsdJson);
+    createPaymentMethodStateSchema.parse(createPaymentContribution);
   expect(contribution.product.currency).toBe("USD");
   expect(contribution.product.productType).toBe("Contribution");
   if (contribution.product.productType === "Contribution") {
     expect(contribution.product.amount).toBe(5);
   }
-  const paymentFields = contribution.paymentFields as StripePaymentFields; //TODO should be a discriminated union
-  expect(paymentFields.stripePaymentType).toBe("StripePaymentRequestButton");
+  const stripePaymentFields = contribution.paymentFields as StripePaymentFields; //TODO should be a discriminated union
+  expect(stripePaymentFields.stripePaymentType).toBe(
+    "StripePaymentRequestButton"
+  );
+
+  const paper = createPaymentMethodStateSchema.parse(createPaymentPaper);
+  expect(paper.product.productType).toBe("Paper");
+  expect(paper.firstDeliveryDate?.getFullYear()).toBe(2024);
+  expect(paper.user.deliveryAddress?.lineOne).toBe("123 Test Street");
+  const ddPaymentFields = paper.paymentFields as DirectDebitPaymentFields;
+  expect(ddPaymentFields.accountNumber).toBe("00000000");
+});
+
+test("createSalesforceContactSchema works with real inputs", async () => {
+  const contribution = createSalesforceContactStateSchema.parse(
+    createSalesforceContactContribution
+  );
+  expect(contribution.product.currency).toBe("USD");
+  expect(contribution.product.productType).toBe("Contribution");
+  if (contribution.product.productType === "Contribution") {
+    expect(contribution.product.amount).toBe(5);
+  }
+  expect(contribution.paymentMethod.Type).toBe(
+    "CreditCardReferenceTransaction"
+  );
+  if (contribution.paymentMethod.Type === "CreditCardReferenceTransaction") {
+    expect(contribution.paymentMethod.CreditCardType).toBe("MasterCard");
+  }
+
+  const paper = createSalesforceContactStateSchema.parse(
+    createSalesforceContactPaper
+  );
+  expect(paper.paymentMethod.Type).toBe("BankTransfer");
+  if (paper.paymentMethod.Type === "BankTransfer") {
+    expect(paper.paymentMethod.PaymentGateway).toBe("GoCardless");
+  }
 });
