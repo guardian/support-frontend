8c1461d3a7b1fdbd5e5110ed09d9f727ee8cd685
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-10-15 12:15:34 +0100

    Use a twirl template to render the page

--- support-frontend/app/controllers/NewspaperArchiveController.scala
+++ support-frontend/app/controllers/NewspaperArchiveController.scala
@@ -1,14 +1,17 @@
 package controllers
 
 import actions.CustomActionBuilders
-import play.api.mvc.{Action, AnyContent}
 import play.api.mvc.Results.Ok
+import play.api.mvc.{Action, AnyContent}
+import play.twirl.api.Html
+import views.html.newspaperArchive
 
 class NewspaperArchiveController(
     actionRefiners: CustomActionBuilders,
 ) {
   import actionRefiners._
-  def getHeader(): Action[AnyContent] = CachedAction() { request =>
-    Ok("Newspaper Archive")
+
+  def getHeader: Action[AnyContent] = CachedAction() { implicit request =>
+    Ok(newspaperArchive("Newspaper Archive")(Html("<h1>Hello World</h1>")))
   }
 }
