ad3a21c9f9152e14cce68b03ba98f712bc69cda8
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-10-16 15:53:37 +0100

    Rename test

--- support-frontend/test/admin/settings/ContributionTypesSpec.scala
+++ support-frontend/test/admin/settings/ContributionTypesSpec.scala
@@ -9,8 +9,8 @@ import org.scalatest.wordspec.AnyWordSpec
 import scala.io.Source
 
 class ContributionTypesSpec extends AnyWordSpec with Matchers {
-  "Amounts" should {
-    "decode json" in {
+  "ContributionTypes" should {
+    "decode correctly" in {
       val json: String = Source.fromURL(getClass.getResource("/contributionTypes.json")).mkString
       val contributionTypes: Either[circe.Error, ContributionTypes] = decode[ContributionTypes](json)
       contributionTypes.isRight mustBe true
