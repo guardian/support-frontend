406c0e9fee91ef8a4b49e2a1aea9ddcd86192a48
GHaberis <george.haberis@guardian.co.uk>
2024-10-18 15:55:29 +0100

    omit users on the Contributions Only checkout from the coverTransactionCost test

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -148,7 +148,7 @@ export const tests: Tests = {
 		referrerControlled: false, // ab-test name not needed to be in paramURL
 		seed: 3,
 		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
-		excludeCountriesSubjectToContributionsOnlyAmounts: false,
+		excludeCountriesSubjectToContributionsOnlyAmounts: true,
 	},
 	newOneTimeCheckout: {
 		variants: [
