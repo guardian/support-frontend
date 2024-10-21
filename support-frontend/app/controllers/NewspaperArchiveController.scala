ea6e364ea6ad6a1987c7289955d33eb14aa66282
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-10-21 15:45:43 +0100

    Tidy up NewspaperArchiveController

--- support-frontend/app/controllers/NewspaperArchiveController.scala
+++ support-frontend/app/controllers/NewspaperArchiveController.scala
@@ -2,9 +2,10 @@ package controllers
 
 import actions.CustomActionBuilders
 import assets.AssetsResolver
-import play.api.mvc.Results.Ok
+import play.api.mvc.Results.{InternalServerError, Ok}
 import play.api.mvc.{Action, AnyContent}
 import play.twirl.api.Html
+import views.{EmptyDiv, SSRContent}
 import views.html.newspaperArchive
 
 class NewspaperArchiveController(
@@ -14,7 +15,10 @@ class NewspaperArchiveController(
   import actionRefiners._
 
   def getHeader: Action[AnyContent] = CachedAction() { implicit request =>
-    val htmlElement = assetsResolver.getSsrCacheContentsAsHtml(divId = "content", file = "ssr-holding-content.html")
-    Ok(newspaperArchive("Newspaper Archive", htmlElement))
+    assetsResolver.getSsrCacheContentsAsHtml(divId = "content", file = "ssr-holding-content.html") match {
+      case EmptyDiv(_) => InternalServerError("Failed to load newspaper archive content")
+      case SSRContent(_, htmlElement, _) => Ok(newspaperArchive("Newspaper Archive", htmlElement))
+    }
+
   }
 }
