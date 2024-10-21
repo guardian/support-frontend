9f403991af35745fc4d7f090826d84ebeb277985
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-10-21 12:26:26 +0100

    feat: tierThree newspaper benefit productCatalog & banner update

--- support-frontend/assets/helpers/productCatalog.tsx
+++ support-frontend/assets/helpers/productCatalog.tsx
@@ -354,8 +354,9 @@ export const productCatalogDescriptionNewBenefits: Record<
 		benefits: [
 			...productCatalogDescription.TierThree.benefits,
 			{
-				copy: 'Newspaper archive',
+				copy: `Unlimited access to the Guardian's 200-year newspaper archive`,
 				isNew: true,
+				tooltip: `Look back on more than 200 years of world history with the Guardian newspaper archive. Get digital access to every front page, article and advertisement, as it was printed, since 1821.`,
 			},
 		],
 	},
