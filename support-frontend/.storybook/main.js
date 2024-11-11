c749be706d475c252e9ef8191bc0f4ca8800527b
Tom Wey <tom.wey@theguardian.com>
2024-11-11 11:48:05 +0000

    Upgrade Storybook to v8 (#6484)
    I ran:
    
    $ npx storybook@latest upgrade
    
    And went with the defaults.
    
    It was necessary to fix the interaction tests, see:
    https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#implicit-actions-can-not-be-used-during-rendering-for-example-in-the-play-function

--- support-frontend/.storybook/main.js
+++ support-frontend/.storybook/main.js
@@ -3,7 +3,15 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const devConfig = require('../webpack.dev.js');
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-viewport', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport',
+    '@storybook/addon-interactions',
+    '@storybook/addon-webpack5-compiler-babel'
+  ],
   framework: {
     name: '@storybook/preact-webpack5',
     options: {}
@@ -29,7 +37,5 @@ module.exports = {
       }
     };
   },
-  docs: {
-    autodocs: true
-  }
+  docs: {}
 };
