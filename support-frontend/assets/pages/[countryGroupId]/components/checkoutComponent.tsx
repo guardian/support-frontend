991a919b0476b336c8aaab5d44ab72f950a3f90e
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-11-13 16:46:33 +0000

    fix product catalog merge conflict

--- support-frontend/assets/pages/[countryGroupId]/components/checkoutComponent.tsx
+++ support-frontend/assets/pages/[countryGroupId]/components/checkoutComponent.tsx
@@ -215,7 +215,7 @@ export function CheckoutComponent({
 	);
 
 	const productDescription = showNewspaperArchiveBenefit
-		? productCatalogDescriptionNewBenefits[productKey]
+		? productCatalogDescriptionNewBenefits(countryGroupId)[productKey]
 		: productCatalogDescription[productKey];
 	const ratePlanDescription = productDescription.ratePlans[ratePlanKey];
 
