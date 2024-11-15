63a2cb739e457d83acf65a7aeb087dc4542158b7
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-11-15 11:48:32 +0000

    testing: e2e testing turned off

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -99,7 +99,7 @@ export const tests: Tests = {
 				size: 1,
 			},
 		},
-		isActive: true,
+		isActive: false,
 		referrerControlled: false, // ab-test name not needed to be in paramURL
 		seed: 2,
 		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
