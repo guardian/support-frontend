c86bb853f83bdf966efeb34497ed76588ecd8f85
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-11-21 11:08:35 +0000

    wip: scaffold

--- support-frontend/assets/pages/[countryGroupId]/guardianLightLanding.tsx
+++ support-frontend/assets/pages/[countryGroupId]/guardianLightLanding.tsx
@@ -1,3 +1,10 @@
+import { css } from '@emotion/react';
+import {
+	from,
+	headlineBold24,
+	palette,
+	space,
+} from '@guardian/source/foundations';
 import { Accordion, AccordionRow } from '@guardian/source/react-components';
 import type { CountryGroupSwitcherProps } from 'components/countryGroupSwitcher/countryGroupSwitcher';
 import { simpleFormatAmount } from 'helpers/forms/checkouts';
@@ -15,6 +22,41 @@ import { LandingPageLayout } from './components/landingPageLayout';
 type GuardianLightLandingProps = {
 	geoId: GeoId;
 };
+
+const innerContentContainer = css`
+	max-width: 940px;
+	margin: ${space[2]}px auto;
+	text-align: center;
+	${from.desktop} {
+		margin: 0px auto;
+	}
+`;
+
+const heading = css`
+	text-align: left;
+	color: ${palette.neutral[100]};
+	${headlineBold24}
+	margin-bottom: ${space[4]}px;
+	${from.tablet} {
+		text-align: center;
+	}
+	${from.desktop} {
+		font-size: 2.625rem;
+		margin-bottom: ${space[10]}px;
+	}
+`;
+const headingAboveMobile = css`
+	display: none;
+	${from.tablet} {
+		display: block;
+	}
+`;
+const headingBelowMobile = css`
+	display: block;
+	${from.tablet} {
+		display: none;
+	}
+`;
 
 export function GuardianLightLanding({
 	geoId,
@@ -52,21 +94,26 @@ export function GuardianLightLanding({
 		productDescription: productCatalogGuardianLight().GuardianLightGoBack,
 		ctaCopy: 'Return to privacy options',
 	};
-
 	return (
 		<LandingPageLayout countrySwitcherProps={countrySwitcherProps}>
-			<GuardianLightCards cardsContent={[card1, card2]} />
-			<Accordion>
-				<AccordionRow label="Collecting from multiple newsagents">
-					Present your card to a newsagent each time you collect the paper. The
-					newsagent will scan your card and will be reimbursed for each
-					transaction automatically.
-				</AccordionRow>
-				<AccordionRow label="Delivery from your retailer">
-					Simply give your preferred store / retailer the barcode printed on
-					your subscription letter.
-				</AccordionRow>
-			</Accordion>
+			<div css={innerContentContainer}>
+				<h1 css={[heading, headingAboveMobile]}>
+					Choose how to read the Guardian
+				</h1>
+				<h1 css={[heading, headingBelowMobile]}>Choose one option</h1>
+				<GuardianLightCards cardsContent={[card1, card2]} />
+				<Accordion>
+					<AccordionRow label="Collecting from multiple newsagents">
+						Present your card to a newsagent each time you collect the paper.
+						The newsagent will scan your card and will be reimbursed for each
+						transaction automatically.
+					</AccordionRow>
+					<AccordionRow label="Delivery from your retailer">
+						Simply give your preferred store / retailer the barcode printed on
+						your subscription letter.
+					</AccordionRow>
+				</Accordion>
+			</div>
 		</LandingPageLayout>
 	);
 }
