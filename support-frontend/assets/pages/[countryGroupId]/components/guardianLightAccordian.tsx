ded5b5c2fee24b346c4f8cc137b866cd1d22e0c2
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-11-22 10:09:16 +0000

    feat: accordian initial tidyup

--- support-frontend/assets/pages/[countryGroupId]/components/guardianLightAccordian.tsx
+++ support-frontend/assets/pages/[countryGroupId]/components/guardianLightAccordian.tsx
@@ -1,5 +1,11 @@
 import { css } from '@emotion/react';
-import { palette, space } from '@guardian/source/foundations';
+import {
+	from,
+	headlineBold24,
+	headlineBold34,
+	palette,
+	space,
+} from '@guardian/source/foundations';
 import {
 	Accordion,
 	AccordionRow,
@@ -7,15 +13,30 @@ import {
 } from '@guardian/source/react-components';
 
 const container = css`
-	display: flex;
 	background-color: ${palette.neutral[100]};
 	> div {
-		padding: ${space[5]}px 72px;
+		display: flex;
+		justify-content: center;
+		padding: ${space[5]}px 12px;
 	}
 `;
 const innerContainer = css`
 	max-width: 940px;
 	text-align: center;
+`;
+const heading = css`
+	text-align: center;
+	color: ${palette.neutral[7]};
+	${headlineBold24}
+	margin-bottom: ${space[6]}px;
+	${from.desktop} {
+		${headlineBold34}
+	}
+`;
+const accordian = css`
+	${from.desktop} {
+		width: 940px;
+	}
 `;
 
 export function GuardianLightAccordian(): JSX.Element {
@@ -26,17 +47,23 @@ export function GuardianLightAccordian(): JSX.Element {
 			cssOverrides={container}
 		>
 			<div css={innerContainer}>
-				<Accordion>
-					<AccordionRow label="Collecting from multiple newsagents">
-						Present your card to a newsagent each time you collect the paper.
-						The newsagent will scan your card and will be reimbursed for each
-						transaction automatically.
-					</AccordionRow>
-					<AccordionRow label="Delivery from your retailer">
-						Simply give your preferred store / retailer the barcode printed on
-						your subscription letter.
-					</AccordionRow>
-				</Accordion>
+				<h2 css={heading}>Any questions?</h2>
+				<div css={accordian}>
+					<Accordion>
+						<AccordionRow label="What is included in my Guardian Light subscription?">
+							test
+						</AccordionRow>
+						<AccordionRow label="Will my Guardian Light subscription work across all devices?">
+							test
+						</AccordionRow>
+						<AccordionRow label="How do I cancel my Guardian Light subscription?">
+							test
+						</AccordionRow>
+						<AccordionRow label="How do I contact customer services?">
+							test
+						</AccordionRow>
+					</Accordion>
+				</div>
 			</div>
 		</Container>
 	);
