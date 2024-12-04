85b88619ac660ac203a440ba860bf56ae86746a8
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-12-04 11:42:04 +0000

    correction2

--- support-frontend/assets/helpers/__tests__/validationTest.ts
+++ support-frontend/assets/helpers/__tests__/validationTest.ts
@@ -1,17 +1,17 @@
 import { doesNotContainExtendedEmojiOrLeadingSpace } from 'pages/[countryGroupId]/validation';
 
-const regexValidation = new RegExp(doesNotContainExtendedEmojiOrLeadingSpace);
+const regexToValidate = new RegExp(doesNotContainExtendedEmojiOrLeadingSpace);
 
 describe('validateRegex', () => {
 	it('leading space', () => {
-		expect(' z').not.toMatch(regexValidation);
-		expect('z z').toMatch(regexValidation);
+		expect(' z').not.toMatch(regexToValidate);
+		expect('z z').toMatch(regexToValidate);
 	});
 	it('pictographic', () => {
-		expect('\\p✈️z').not.toMatch(regexValidation);
+		expect('\\p✈️z').not.toMatch(regexToValidate);
 	});
 	it('combined', () => {
-		expect(' z\\p✈️z').not.toMatch(regexValidation);
-		expect('\\p✈️ z').not.toMatch(regexValidation);
+		expect(' z\\p✈️z').not.toMatch(regexToValidate);
+		expect('\\p✈️ z').not.toMatch(regexToValidate);
 	});
 });
