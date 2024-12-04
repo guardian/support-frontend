ac0d98a16b5d1024ef025ec75d947f78336c2b66
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-12-04 10:32:46 +0000

    correction

--- support-frontend/assets/helpers/__tests__/validationTest.ts
+++ support-frontend/assets/helpers/__tests__/validationTest.ts
@@ -1,25 +1,17 @@
 import { doesNotContainExtendedEmojiOrLeadingSpace } from 'pages/[countryGroupId]/validation';
 
+const regexValidation = new RegExp(doesNotContainExtendedEmojiOrLeadingSpace);
+
 describe('validateRegex', () => {
 	it('leading space', () => {
-		expect(' z').not.toMatch(
-			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
-		);
-		expect('z z').toMatch(
-			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
-		);
+		expect(' z').not.toMatch(regexValidation);
+		expect('z z').toMatch(regexValidation);
 	});
 	it('pictographic', () => {
-		expect('\\p✈️z').not.toMatch(
-			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
-		);
+		expect('\\p✈️z').not.toMatch(regexValidation);
 	});
 	it('combined', () => {
-		expect(' z\\p✈️z').not.toMatch(
-			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
-		);
-		expect('\\p✈️ z').not.toMatch(
-			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
-		);
+		expect(' z\\p✈️z').not.toMatch(regexValidation);
+		expect('\\p✈️ z').not.toMatch(regexValidation);
 	});
 });
