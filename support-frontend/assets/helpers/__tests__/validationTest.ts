02801d6d049ea639c611379f4b6ac17a6872d4a4
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-12-04 10:03:50 +0000

    review: regex tests now expect toMatch

--- support-frontend/assets/helpers/__tests__/validationTest.ts
+++ support-frontend/assets/helpers/__tests__/validationTest.ts
@@ -2,30 +2,24 @@ import { doesNotContainExtendedEmojiOrLeadingSpace } from 'pages/[countryGroupId
 
 describe('validateRegex', () => {
 	it('leading space', () => {
-		expect(matchRegex(doesNotContainExtendedEmojiOrLeadingSpace, ' z')).toEqual(
-			false,
+		expect(' z').not.toMatch(
+			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
 		);
-		expect(
-			matchRegex(doesNotContainExtendedEmojiOrLeadingSpace, 'z z'),
-		).toEqual(true);
+		expect('z z').toMatch(
+			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
+		);
 	});
 	it('pictographic', () => {
-		expect(
-			matchRegex(doesNotContainExtendedEmojiOrLeadingSpace, '\\p✈️z'),
-		).toEqual(false);
+		expect('\\p✈️z').not.toMatch(
+			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
+		);
 	});
 	it('combined', () => {
-		expect(
-			matchRegex(doesNotContainExtendedEmojiOrLeadingSpace, ' z\\p✈️z'),
-		).toEqual(false);
-		expect(
-			matchRegex(doesNotContainExtendedEmojiOrLeadingSpace, '\\p✈️ z'),
-		).toEqual(false);
+		expect(' z\\p✈️z').not.toMatch(
+			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
+		);
+		expect('\\p✈️ z').not.toMatch(
+			new RegExp(doesNotContainExtendedEmojiOrLeadingSpace),
+		);
 	});
 });
-
-// ----- Helpers ----- //
-
-function matchRegex(regex: string, value: string): boolean {
-	return new RegExp(regex).test(value);
-}
