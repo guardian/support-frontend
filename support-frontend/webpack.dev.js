cccee383669dceb11c9262702bb59bd94f0f3e8d
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2024-12-13 16:13:48 +0000

    review: webpack chunk donot split

--- support-frontend/webpack.dev.js
+++ support-frontend/webpack.dev.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
@@ -24,25 +23,32 @@ module.exports = merge(common('[name].css', '[name].js', false), {
 	},
 	optimization: {
 		splitChunks: {
-			chunks: 'initial',
-			minSize: 20000,
-			minRemainingSize: 0,
-			minChunks: 1,
-			maxAsyncRequests: 30,
-			maxInitialRequests: 30,
-			enforceSizeThreshold: 50000,
-			cacheGroups: {
-				defaultVendors: {
-					test: /[\\/]node_modules[\\/]/,
-					priority: -10,
-					reuseExistingChunk: true,
-				},
-				default: {
-					minChunks: 2,
-					priority: -20,
-					reuseExistingChunk: true,
-				},
+			chunks() {
+				return false;
 			},
 		},
 	},
+	// optimization: {
+	// 	splitChunks: {
+	// 		chunks: 'initial',
+	// 		minSize: 20000,
+	// 		minRemainingSize: 0,
+	// 		minChunks: 1,
+	// 		maxAsyncRequests: 30,
+	// 		maxInitialRequests: 30,
+	// 		enforceSizeThreshold: 50000,
+	// 		cacheGroups: {
+	// 			defaultVendors: {
+	// 				test: /[\\/]node_modules[\\/]/,
+	// 				priority: -10,
+	// 				reuseExistingChunk: true,
+	// 			},
+	// 			default: {
+	// 				minChunks: 2,
+	// 				priority: -20,
+	// 				reuseExistingChunk: true,
+	// 			},
+	// 		},
+	// 	},
+	// },
 });
--- support-frontend/webpack.prod.js
+++ support-frontend/webpack.prod.js
@@ -32,26 +32,33 @@ module.exports = (
 		],
 		optimization: {
 			splitChunks: {
-				chunks: 'initial',
-				minSize: 20000,
-				minRemainingSize: 0,
-				minChunks: 1,
-				maxAsyncRequests: 30,
-				maxInitialRequests: 30,
-				enforceSizeThreshold: 50000,
-				cacheGroups: {
-					defaultVendors: {
-						test: /[\\/]node_modules[\\/]/,
-						priority: -10,
-						reuseExistingChunk: true,
-					},
-					default: {
-						minChunks: 2,
-						priority: -20,
-						reuseExistingChunk: true,
-					},
+				chunks() {
+					return false;
 				},
 			},
 		},
+		// optimization: {
+		// 	splitChunks: {
+		// 		chunks: 'initial',
+		// 		minSize: 20000,
+		// 		minRemainingSize: 0,
+		// 		minChunks: 1,
+		// 		maxAsyncRequests: 30,
+		// 		maxInitialRequests: 30,
+		// 		enforceSizeThreshold: 50000,
+		// 		cacheGroups: {
+		// 			defaultVendors: {
+		// 				test: /[\\/]node_modules[\\/]/,
+		// 				priority: -10,
+		// 				reuseExistingChunk: true,
+		// 			},
+		// 			default: {
+		// 				minChunks: 2,
+		// 				priority: -20,
+		// 				reuseExistingChunk: true,
+		// 			},
+		// 		},
+		// 	},
+		// },
 	});
 };
