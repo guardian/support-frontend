3864c4062b1d02f462afbba55b42858396cc0bed
Tom Wey <tom.wey@theguardian.com>
2024-12-19 13:13:37 +0000

    Add fields required for Guardian Ad Lite

--- support-workers/src/main/scala/com/gu/emailservices/GuardianLightEmailFields.scala
+++ support-workers/src/main/scala/com/gu/emailservices/GuardianLightEmailFields.scala
@@ -1,16 +1,46 @@
 package com.gu.emailservices
 
+import com.gu.emailservices.SubscriptionEmailFieldHelpers.formatDate
+import com.gu.support.workers.{
+  ClonedDirectDebitPaymentMethod,
+  CreditCardReferenceTransaction,
+  DirectDebitPaymentMethod,
+  PayPalReferenceTransaction,
+  PaymentMethod,
+  SepaPaymentMethod,
+}
 import com.gu.support.workers.states.SendThankYouEmailState.SendThankYouEmailGuardianLightState
+import org.joda.time.DateTime
+import com.gu.zuora.subscriptionBuilders.GuardianLightSubscriptionBuilder
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class GuardianLightEmailFields {
+class GuardianLightEmailFields(created: DateTime) {
   def build(
       state: SendThankYouEmailGuardianLightState,
   )(implicit ec: ExecutionContext): Future[EmailFields] = {
-    // TODO: populate this when we know what's needed
-    val fields = List()
+    val subscription_details = SubscriptionEmailFieldHelpers
+      .describe(state.paymentSchedule, state.product.billingPeriod, state.product.currency)
+
+    val fields = List(
+      "zuorasubscriberid" -> state.subscriptionNumber,
+      "email_address" -> state.user.primaryEmailAddress,
+      "billing_period" -> state.product.billingPeriod.toString.toLowerCase,
+      "first_payment_date" -> formatDate(
+        created.plusDays(GuardianLightSubscriptionBuilder.gracePeriodInDays).toLocalDate,
+      ),
+      "payment_method" -> paymentMethodName(state.paymentMethod),
+      "subscription_details" -> subscription_details,
+    )
 
     Future.successful(EmailFields(fields, state.user, "guardian-light"))
+  }
+
+  private def paymentMethodName(method: PaymentMethod): String = method match {
+    case _: DirectDebitPaymentMethod => "Direct Debit"
+    case _: ClonedDirectDebitPaymentMethod => "Direct Debit"
+    case _: SepaPaymentMethod => "SEPA"
+    case _: PayPalReferenceTransaction => "PayPal"
+    case _: CreditCardReferenceTransaction => "credit / debit card"
   }
 }
