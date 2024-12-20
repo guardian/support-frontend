1833e05f7fc3dbb6b0352df2dbe7f50266a1aff9
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-12-20 14:44:30 +0000

    Stripe and serviceHandler

--- support-workers/src/typescript/services/stripe.ts
+++ support-workers/src/typescript/services/stripe.ts
@@ -1,49 +1,82 @@
-import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm';
 import Stripe from 'stripe';
 import { z } from 'zod';
+import type { StripePaymentGateway } from '../model/paymentMethod';
+import { stripePaymentGatewaySchema } from '../model/paymentMethod';
 import type { Stage } from '../model/stage';
-import { awsConfig } from '../util/awsConfig';
+import { getIfDefined } from '../util/nullAndUndefined';
+import { getConfig } from './config';
 
 const stripeConfigSchema = z.object({
-	apiVersion: z.string(),
 	accounts: z.object({
 		australia: z.object({
 			secretKey: z.string(),
 			publicKey: z.string(),
+			paymentGateway: stripePaymentGatewaySchema,
 		}),
 		unitedStates: z.object({
 			secretKey: z.string(),
 			publicKey: z.string(),
+			paymentGateway: stripePaymentGatewaySchema,
 		}),
 		defaultAccount: z.object({
 			secretKey: z.string(),
 			publicKey: z.string(),
+			paymentGateway: stripePaymentGatewaySchema,
 		}),
 	}),
 });
+type StripeConfig = z.infer<typeof stripeConfigSchema>;
 
-export const getStripeConfig = async (stage: Stage) => {
-	const ssmClient = new SSMClient(awsConfig);
-	const params = {
-		Name: `/${stage}/support/support-workers/stripe-config`,
-		WithDecryption: true,
-	};
-	const command = new GetParameterCommand(params);
-	const response = await ssmClient.send(command);
-	console.log(response.Parameter?.Value);
-
-	return stripeConfigSchema.parse(JSON.parse(response.Parameter?.Value ?? ''));
+export const getStripeConfig = async (stage: Stage): Promise<StripeConfig> => {
+	return getConfig(stage, 'stripe-config', stripeConfigSchema);
 };
 
-const stripe = new Stripe('sk_test_...', {});
+export class StripeService {
+	stripeAccountForPublicKey: Record<
+		string,
+		{ paymentGateway: StripePaymentGateway; stripe: Stripe }
+	>;
+	constructor(config: StripeConfig) {
+		this.stripeAccountForPublicKey = Object.fromEntries(
+			Object.entries(config.accounts).map(([, account]) => [
+				account.publicKey,
+				{
+					paymentGateway: account.paymentGateway,
+					stripe: new Stripe(account.secretKey),
+				},
+			]),
+		);
+	}
+	private stripeForPublicKey(publicKey: string) {
+		const stripe = getIfDefined(
+			this.stripeAccountForPublicKey[publicKey],
+			`No Stripe client found for public key ${publicKey}`,
+		);
+		return stripe.stripe;
+	}
 
-export const createCustomer = async () => {
-	const params: Stripe.CustomerCreateParams = {
-		description: 'test customer',
-		payment_method: 'pm_card_visa',
+	getPaymentGateway(publicKey: string) {
+		const stripe = getIfDefined(
+			this.stripeAccountForPublicKey[publicKey],
+			`No Stripe client found for public key ${publicKey}`,
+		);
+		return stripe.paymentGateway;
+	}
+
+	createCustomer = async (publicKey: string, paymentMethodId: string) => {
+		const stripe = this.stripeForPublicKey(publicKey);
+		const params: Stripe.CustomerCreateParams = {
+			payment_method: paymentMethodId,
+		};
+
+		const customer: Stripe.Customer = await stripe.customers.create(params);
+		console.log(`Created Stripe customer with id ${customer.id}`);
+		return customer;
 	};
 
-	const customer: Stripe.Customer = await stripe.customers.create(params);
-
-	console.log(customer.id);
-};
+	getPaymentMethod = async (publicKey: string, paymentMethodId: string) => {
+		const stripe = this.stripeForPublicKey(publicKey);
+		const paymentMethod = await stripe.paymentMethods.retrieve(paymentMethodId);
+		return paymentMethod;
+	};
+}
