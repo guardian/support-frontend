5a6a740c1e7189d58e89167cf1b8038b3caee698
Tom Wey <tom.wey@theguardian.com>
2024-12-30 15:44:17 +0000

    Remove overlay if one time card payment fails server side
    Previously we noticed that if there was a server side failure on the one
    time checkout, you'd see an error message but the "processing payment"
    overlay would remain up, which meant you couldn't interact with the form
    to fix whatever the problem was.
    
    We've also added some playwright coverage for this scenario.

--- support-e2e/tests/test/oneTimeCheckout.ts
+++ support-e2e/tests/test/oneTimeCheckout.ts
@@ -3,13 +3,23 @@ import { email, firstName, lastName } from '../utils/users';
 import { setupPage } from '../utils/page';
 import { setTestUserRequiredDetails } from '../utils/testUserDetails';
 import { fillInPayPalDetails } from '../utils/paypal';
-import { fillInCardDetails } from '../utils/cardDetails';
+import {
+	fillInCardDetails,
+	fillInDeclinedCardDetails,
+} from '../utils/cardDetails';
 import { checkRecaptcha } from '../utils/recaptcha';
 
 type TestDetails = {
 	paymentType: string;
 	internationalisationId: string;
 };
+
+const submitForm = (page) =>
+	page
+		.getByRole('button', {
+			name: ` with `,
+		})
+		.click();
 
 export const testOneTimeCheckout = (testDetails: TestDetails) => {
 	const { internationalisationId, paymentType } = testDetails;
@@ -25,15 +35,22 @@ export const testOneTimeCheckout = (testDetails: TestDetails) => {
 		await page.getByRole('radio', { name: paymentType }).check();
 
 		if (paymentType === 'Credit/Debit card') {
+			// First check that a failed card payment behaves as expected
+			await fillInDeclinedCardDetails(page);
+			await checkRecaptcha(page);
+
+			await submitForm(page);
+
+			await expect(page.getByText('Sorry, something went wrong')).toBeVisible({
+				timeout: 30000,
+			});
+
+			// Now fill in with good card details
 			await fillInCardDetails(page);
 			await checkRecaptcha(page);
 		}
 
-		await page
-			.getByRole('button', {
-				name: ` with `,
-			})
-			.click();
+		await submitForm(page);
 
 		if (paymentType === 'PayPal') {
 			await expect(page).toHaveURL(/.*paypal.com/, { timeout: 600000 });
--- support-e2e/tests/utils/cardDetails.ts
+++ support-e2e/tests/utils/cardDetails.ts
@@ -1,12 +1,18 @@
 import { Page } from '@playwright/test';
 
-export const fillInCardDetails = async (page: Page) => {
+export const fillInCardDetails = async (page: Page) =>
+	fillInCardDetailsWithNumber(page, '4242424242424242');
+
+export const fillInDeclinedCardDetails = async (page: Page) =>
+	fillInCardDetailsWithNumber(page, '4000000000000002');
+
+const fillInCardDetailsWithNumber = async (page: Page, cardNumber: string) => {
 	// it would be nice to use aria style selectors here, but given Stripes
 	// very secure implementation, it is quite hard
 	await page
 		.frameLocator("iframe[title='Secure card number input frame']")
 		.locator('input[name="cardnumber"]')
-		.fill('4242424242424242');
+		.fill(cardNumber);
 
 	await page
 		.frameLocator("iframe[title='Secure expiration date input frame']")
