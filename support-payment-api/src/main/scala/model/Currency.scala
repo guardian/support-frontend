cfc83252e407e4da41c18de53799af3e71af89c8
Tom Wey <tom.wey@theguardian.com>
2024-12-30 17:14:13 +0000

    Validate min as well as max

--- support-payment-api/src/main/scala/model/Currency.scala
+++ support-payment-api/src/main/scala/model/Currency.scala
@@ -4,6 +4,10 @@ import enumeratum.{CirceEnum, Enum, EnumEntry}
 
 import scala.collection.immutable.IndexedSeq
 
+case class Range(min: BigDecimal, max: BigDecimal) {
+  def contains(amount: BigDecimal): Boolean = (amount >= min) && (amount <= max)
+}
+
 // Models currencies supported by the API
 sealed trait Currency extends EnumEntry
 
@@ -24,12 +28,15 @@ object Currency extends Enum[Currency] with CirceEnum[Currency] {
   case object NZD extends Currency
 
   def exceedsMaxAmount(amount: BigDecimal, currency: Currency): Boolean = {
-    val maxAmount = currency match {
-      case AUD => 16000
-      case USD => 10000
-      case _ => 2000
-    }
     // Users can opt in to add 4% to cover the transaction cost
-    amount > maxAmount * 1.04
+    val transactionCostAsPercentage = 1.04
+
+    val currencyRange = currency match {
+      case AUD => Range(min = 1, max = 16000 * transactionCostAsPercentage)
+      case USD => Range(min = 1, max = 10000 * transactionCostAsPercentage)
+      case _ => Range(min = 1, max = 2000 * transactionCostAsPercentage)
+    }
+
+    !currencyRange.contains(amount)
   }
 }
