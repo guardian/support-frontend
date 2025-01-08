527ded85344486f60bd0a1368347886683158445
Tom Wey <tom.wey@theguardian.com>
2025-01-08 10:16:15 +0000

    Prevent Ad-Lite purchase if the user already has ad free OR reject tracking benefits

--- support-frontend/app/controllers/CreateSubscriptionController.scala
+++ support-frontend/app/controllers/CreateSubscriptionController.scala
@@ -152,7 +152,7 @@ class CreateSubscriptionController(
   }
 
   private def validateBenefits(response: UserBenefitsResponse): EitherT[Future, CreateSubscriptionError, Unit] = {
-    if (response.benefits.contains("adFree")) {
+    if (response.benefits.contains("adFree") || response.benefits.contains("rejectTracking")) {
       // Not eligible
       EitherT.leftT(RequestValidationError("guardian_light_purchase_not_allowed"))
     } else {
