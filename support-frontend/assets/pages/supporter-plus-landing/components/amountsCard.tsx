67ea545f4e3933f239ab4d3ac309d8404e7b52e7
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-01-09 17:35:26 +0000

    feat: display all contribution prices in amountsCard

---
+++ support-frontend/assets/pages/supporter-plus-landing/components/amountsCard.tsx
@@ -0,0 +1,150 @@
+import { css, ThemeProvider } from '@emotion/react';
+import {
+	from,
+	headlineBold24,
+	palette,
+	space,
+	textSans15,
+	until,
+} from '@guardian/source/foundations';
+import {
+	buttonThemeReaderRevenueBrand,
+	LinkButton,
+} from '@guardian/source/react-components';
+import { useState } from 'react';
+import { OtherAmount } from '../../../components/otherAmount/otherAmount';
+import { PriceCards } from '../../../components/priceCards/priceCards';
+import type {
+	AmountValuesObject,
+	ContributionType,
+} from '../../../helpers/contributions';
+import type { CountryGroupId } from '../../../helpers/internationalisation/countryGroup';
+import type { IsoCurrency } from '../../../helpers/internationalisation/currency';
+import { trackComponentClick } from '../../../helpers/tracking/behaviour';
+import { PaymentCards } from './PaymentIcons';
+
+const sectionStyle = css`
+	max-width: 600px;
+	margin: auto;
+	background-color: ${palette.neutral[100]};
+	border-radius: ${space[3]}px;
+	padding: 32px;
+	${until.desktop} {
+		padding-top: ${space[6]}px;
+	}
+`;
+const titleStyle = css`
+	margin: 0 0 ${space[2]}px;
+	text-align: center;
+	${headlineBold24}
+	${from.tablet} {
+		font-size: 28px;
+	}
+`;
+const standFirstStyle = css`
+	color: #606060;
+	margin-bottom: ${space[2]}px;
+	${from.desktop} {
+		margin-bottom: ${space[3]}px;
+	}
+`;
+
+const btnStyleOverrides = css`
+	width: 100%;
+	justify-content: center;
+	margin-bottom: ${space[3]}px;
+	margin-top: ${space[5]}px;
+`;
+
+const buttonContainer = css`
+	display: flex;
+	flex-direction: column;
+`;
+
+interface AmountsCardProps {
+	amountsData: AmountValuesObject;
+	countryGroupId: CountryGroupId;
+	currencyId: IsoCurrency;
+	heading?: JSX.Element;
+	standFirst?: JSX.Element;
+	contributionType: ContributionType;
+}
+
+export function AmountsCard({
+	countryGroupId,
+	amountsData,
+	currencyId,
+	heading,
+	standFirst,
+	contributionType,
+}: AmountsCardProps) {
+	const [selectedAmount, setSelectedAmount] = useState<number | 'other'>(
+		amountsData.defaultAmount,
+	);
+	const [otherAmount, setOtherAmount] = useState('');
+
+	const billingPeriod = contributionType === 'ANNUAL' ? 'Annual' : 'Monthly';
+	const amount = selectedAmount === 'other' ? otherAmount : selectedAmount;
+	const checkoutLink =
+		contributionType === 'ONE_OFF'
+			? `one-time-checkout?contribution=${amount}`
+			: `checkout?product=Contribution&ratePlan=${billingPeriod}&contribution=${amount}`;
+
+	return (
+		<section css={sectionStyle}>
+			<div
+				css={css`
+					${textSans15}
+				`}
+			>
+				{heading && <h2 css={titleStyle}>{heading}</h2>}
+				{standFirst && <p css={standFirstStyle}>{standFirst}</p>}
+				<PriceCards
+					amounts={amountsData.amounts}
+					selectedAmount={selectedAmount}
+					currency={currencyId}
+					// This is always undefined as we're ONE_OFF
+					paymentInterval={undefined}
+					onAmountChange={(amount: string) => {
+						if (amount === 'other') {
+							setSelectedAmount(amount);
+						} else {
+							const amountNumber = parseInt(amount, 10);
+							if (!isNaN(amountNumber)) {
+								setSelectedAmount(amountNumber);
+							}
+						}
+					}}
+					hideChooseYourAmount={amountsData.hideChooseYourAmount}
+					otherAmountField={
+						<OtherAmount
+							currency={currencyId}
+							selectedAmount={selectedAmount}
+							otherAmount={otherAmount}
+							onOtherAmountChange={(otherAmount) => {
+								setOtherAmount(otherAmount);
+							}}
+							errors={[]}
+						/>
+					}
+				/>
+			</div>
+			<div css={buttonContainer}>
+				<ThemeProvider theme={buttonThemeReaderRevenueBrand}>
+					<LinkButton
+						href={checkoutLink}
+						cssOverrides={btnStyleOverrides}
+						onClick={() => {
+							trackComponentClick(
+								`npf-contribution-amount-toggle-${countryGroupId}-${contributionType}`,
+							);
+						}}
+					>
+						Continue to checkout
+					</LinkButton>
+				</ThemeProvider>
+				<PaymentCards />
+			</div>
+		</section>
+	);
+}
--- support-frontend/assets/pages/supporter-plus-landing/twoStepPages/contributionsOnlyLanding.tsx
+++ support-frontend/assets/pages/supporter-plus-landing/twoStepPages/contributionsOnlyLanding.tsx
@@ -6,65 +6,22 @@ import {
 	palette,
 	space,
 	textSans17,
-	textSansBold20,
 } from '@guardian/source/foundations';
 import { Container } from '@guardian/source/react-components';
 import {
 	FooterLinks,
 	FooterWithContents,
 } from '@guardian/source-development-kitchen/react-components';
-import { useEffect, useMemo, useState } from 'preact/hooks';
-import CountryGroupSwitcher from 'components/countryGroupSwitcher/countryGroupSwitcher';
-import type { CountryGroupSwitcherProps } from 'components/countryGroupSwitcher/countryGroupSwitcher';
-import { CountrySwitcherContainer } from 'components/headers/simpleHeader/countrySwitcherContainer';
+import { useState } from 'preact/hooks';
 import { Header } from 'components/headers/simpleHeader/simpleHeader';
 import { PageScaffold } from 'components/page/pageScaffold';
 import { PaymentFrequencyButtons } from 'components/paymentFrequencyButtons/paymentFrequencyButtons';
-import {
-	init as abTestInit,
-	getAmountsTestVariant,
-} from 'helpers/abTests/abtest';
-import {
-	countdownSwitchOn,
-	getCampaignSettings,
-} from 'helpers/campaigns/campaigns';
-import type {
-	CampaignSettings,
-	CountdownSetting,
-} from 'helpers/campaigns/campaigns';
-import type {
-	ContributionType,
-	RegularContributionType,
-} from 'helpers/contributions';
+import { getAmountsTestVariant } from 'helpers/abTests/abtest';
+import type { ContributionType } from 'helpers/contributions';
 import { Country } from 'helpers/internationalisation/classes/country';
-import {
-	AUDCountries,
-	Canada,
-	EURCountries,
-	GBPCountries,
-	International,
-	NZDCountries,
-	UnitedStates,
-} from 'helpers/internationalisation/countryGroup';
-import { currencies } from 'helpers/internationalisation/currency';
-import {
-	productCatalogDescription as canonicalProductCatalogDescription,
-	productCatalog,
-	productCatalogDescriptionNewBenefits,
-} from 'helpers/productCatalog';
-import type { BillingPeriod } from 'helpers/productPrice/billingPeriods';
-import type { Promotion } from 'helpers/productPrice/promotions';
-import { getPromotion } from 'helpers/productPrice/promotions';
-import * as storage from 'helpers/storage/storage';
 import type { GeoId } from 'pages/geoIdConfig';
 import { getGeoIdConfig } from 'pages/geoIdConfig';
-import Countdown from '../components/countdown';
-import { LandingPageBanners } from '../components/landingPageBanners';
-import { OneOffCard } from '../components/oneOffCard';
-import { SupportOnce } from '../components/supportOnce';
-import { ThreeTierCards } from '../components/threeTierCards';
-import { ThreeTierTsAndCs } from '../components/threeTierTsAndCs';
-import { TickerContainer } from './tickerContainer';
+import { AmountsCard } from '../components/amountsCard';
 
 const recurringContainer = css`
 	background-color: ${palette.brand[400]};
@@ -87,14 +44,6 @@ const recurringContainer = css`
 		> div {
 			padding: 40px 10px 72px;
 		}
-	}
-`;
-
-const oneTimeContainer = css`
-	display: flex;
-	background-color: ${palette.neutral[97]};
-	> div {
-		padding: ${space[5]}px 72px;
 	}
 `;
 
@@ -142,33 +91,6 @@ const paymentFrequencyButtonsCss = css`
 	}
 `;
 
-const tabletLineBreak = css`
-	${from.desktop} {
-		display: none;
-	}
-`;
-
-const supportAnotherWayContainer = css`
-	display: flex;
-	background-color: #1e3e72;
-`;
-
-const supportAnotherWay = css`
-	margin: 20px 0;
-	max-width: 940px;
-	text-align: left;
-	color: ${palette.neutral[100]};
-	h4 {
-		${textSansBold20};
-	}
-	p {
-		${textSans17};
-	}
-	a {
-		color: ${palette.neutral[100]};
-	}
-`;
-
 const disclaimerContainer = css`
 	background-color: ${palette.brand[400]};
 	> div {
@@ -211,57 +133,6 @@ const paymentFrequencyMap = {
 	MONTHLY: 'Monthly',
 	ANNUAL: 'Annual',
 };
-const isCardUserSelected = (
-	cardPrice: number,
-	cardPriceDiscount?: number,
-): boolean => {
-	const urlParams = new URLSearchParams(window.location.search);
-	const urlSelectedAmount = urlParams.get('selected-amount');
-	const hasUrlSelectedAmount = !isNaN(Number(urlSelectedAmount));
-	if (!hasUrlSelectedAmount) {
-		return false;
-	}
-	return (
-		Number(urlSelectedAmount) === cardPrice ||
-		Number(urlSelectedAmount) === cardPriceDiscount
-	);
-};
-
-/**
- * @deprecated - we should be useing ProductCatalog data types.
- * TODO - remove this once TsAndCs work of ☝️ types
- */
-function getPlanCost(
-	pricing: number,
-	contributionType: ContributionType,
-	promotion?: Promotion,
-) {
-	const promotionDurationPeriod: RegularContributionType =
-		contributionType === 'ANNUAL' && promotion?.discount?.durationMonths === 12
-			? 'ANNUAL'
-			: 'MONTHLY';
-
-	const promotionDurationValue =
-		promotionDurationPeriod === 'ANNUAL'
-			? 1
-			: promotion?.discount?.durationMonths;
-
-	return {
-		price: pricing,
-		promoCode: promotion?.name,
-		discount:
-			promotion?.discount?.amount && promotion.discountedPrice
-				? {
-						percentage: promotion.discount.amount,
-						price: promotion.discountedPrice,
-						duration: {
-							value: promotionDurationValue ?? 0,
-							period: promotionDurationPeriod,
-						},
-				  }
-				: undefined,
-	};
-}
 
 type ContributionsOnlyLandingProps = {
 	geoId: GeoId;
@@ -269,103 +140,11 @@ type ContributionsOnlyLandingProps = {
 export function ContributionsOnlyLanding({
 	geoId,
 }: ContributionsOnlyLandingProps): JSX.Element {
-	// const urlSearchParams = new URLSearchParams(window.location.search);
-	// const urlSearchParamsProduct = urlSearchParams.get('product');
-	// const urlSearchParamsRatePlan = urlSearchParams.get('ratePlan');
-	// const urlSearchParamsOneTime = urlSearchParams.has('oneTime');
-	// const urlSearchParamsPromoCode = urlSearchParams.get('promoCode');
-
 	const { currencyKey: currencyId, countryGroupId } = getGeoIdConfig(geoId);
 	const countryId = Country.detect();
 
-	// const countrySwitcherProps: CountryGroupSwitcherProps = {
-	// 	countryGroupIds: [
-	// 		GBPCountries,
-	// 		UnitedStates,
-	// 		AUDCountries,
-	// 		EURCountries,
-	// 		NZDCountries,
-	// 		Canada,
-	// 		International,
-	// 	],
-	// 	selectedCountryGroup: countryGroupId,
-	// 	subPath: '/contribute',
-	// };
-
-	// const abParticipations = abTestInit({ countryId, countryGroupId });
-	// // Persist any tests for tracking in the checkout page
-	// storage.setSession('abParticipations', JSON.stringify(abParticipations));
-
-	// const campaignSettings = getCampaignSettings(
-	// 	countryGroupId,
-	// 	urlSearchParamsPromoCode,
-	// );
-
-	// const enableSingleContributionsTab =
-	// 	campaignSettings?.enableSingleContributions ??
-	// 	urlSearchParams.has('enableOneTime');
-
-	// const getInitialContributionType = () => {
-	// 	if (enableSingleContributionsTab && urlSearchParamsOneTime) {
-	// 		return 'ONE_OFF';
-	// 	}
-	// 	return urlSearchParamsRatePlan === 'Annual' ? 'ANNUAL' : 'MONTHLY';
-	// };
-
 	const [contributionType, setContributionType] =
 		useState<ContributionType>('MONTHLY');
-
-	// const tierPlanPeriod = contributionType.toLowerCase();
-	// const billingPeriod = (tierPlanPeriod[0].toUpperCase() +
-	// 	tierPlanPeriod.slice(1)) as BillingPeriod;
-
-	// // Handle which countdown to show (if any).
-	// const [currentCountdownSettings, setCurrentCountdownSettings] =
-	// 	useState<CountdownSetting>();
-	// const [showCountdown, setShowCountdown] = useState<boolean>(false);
-
-	// const memoizedCurrentCountdownCampaign = useMemo(() => {
-	// 	if (!campaignSettings?.countdownSettings) {
-	// 		return undefined;
-	// 	}
-
-	// 	const now = Date.now();
-	// 	return campaignSettings.countdownSettings.find(
-	// 		(c) =>
-	// 			c.countdownStartInMillis < now && c.countdownDeadlineInMillis > now,
-	// 	);
-	// }, [campaignSettings?.countdownSettings]);
-
-	// useEffect(() => {
-	// 	if (memoizedCurrentCountdownCampaign) {
-	// 		setCurrentCountdownSettings(memoizedCurrentCountdownCampaign);
-	// 		setShowCountdown(true);
-	// 	}
-	// }, [memoizedCurrentCountdownCampaign]);
-
-	// const getHeadline = (
-	// 	showCountdown: boolean,
-	// 	currentCountdownSettings?: CountdownSetting,
-	// 	campaignSettings?: CampaignSettings | null,
-	// ) => {
-	// 	if (showCountdown && currentCountdownSettings?.label.trim()) {
-	// 		return currentCountdownSettings.label;
-	// 	} else {
-	// 		return (
-	// 			<>
-	// 				{campaignSettings?.copy.headingFragment ?? <>Support </>}
-	// 				fearless, <br css={tabletLineBreak} />
-	// 				independent journalism
-	// 				{campaignSettings?.copy.punctuation ??
-	// 					campaignSettings?.copy.punctuation}
-	// 			</>
-	// 		);
-	// 	}
-	// };
-
-	// /*
-	//  * /////////////// END US EOY 2024 Campaign
-	//  */
 
 	const paymentFrequencies: ContributionType[] = [
 		'ONE_OFF',
@@ -379,150 +158,11 @@ export function ContributionsOnlyLanding({
 		}
 	};
 
-	// const selectedContributionRatePlan =
-	// 	contributionType === 'ANNUAL' ? 'Annual' : 'Monthly';
-
-	// const productCatalogDescription = ['v1', 'v2'].includes(
-	// 	abParticipations.newspaperArchiveBenefit ?? '',
-	// )
-	// 	? productCatalogDescriptionNewBenefits(countryGroupId)
-	// 	: canonicalProductCatalogDescription;
-
-	// /**
-	//  * Tier 1: Contributions
-	//  * We use the amounts from RRCP to populate the Contribution tier
-	//  */
 	const { selectedAmountsVariant: amounts } = getAmountsTestVariant(
 		countryId,
 		countryGroupId,
 		window.guardian.settings,
 	);
-	// const monthlyRecurringAmount = amounts.amountsCardData.MONTHLY.amounts[0];
-	// const annualRecurringAmount = amounts.amountsCardData.ANNUAL.amounts[0];
-	// const recurringAmount =
-	// 	contributionType === 'MONTHLY'
-	// 		? monthlyRecurringAmount
-	// 		: annualRecurringAmount;
-
-	// const tier1UrlParams = new URLSearchParams({
-	// 	product: 'Contribution',
-	// 	ratePlan: selectedContributionRatePlan,
-	// 	contribution: recurringAmount.toString(),
-	// });
-	// const tier1Link = `checkout?${tier1UrlParams.toString()}`;
-
-	// const tier1Card = {
-	// 	productDescription: productCatalogDescription.Contribution,
-	// 	price: recurringAmount,
-	// 	link: tier1Link,
-	// 	isUserSelected:
-	// 		urlSearchParamsProduct === 'Contribution' ||
-	// 		isCardUserSelected(recurringAmount),
-	// 	isRecommended: false,
-	// 	ctaCopy: 'Support',
-	// };
-
-	// /** Tier 2: SupporterPlus */
-	// const supporterPlusRatePlan =
-	// 	contributionType === 'ANNUAL' ? 'Annual' : 'Monthly';
-	// const tier2Pricing =
-	// 	productCatalog.SupporterPlus.ratePlans[supporterPlusRatePlan].pricing[
-	// 		currencyId
-	// 	];
-
-	// const tier2UrlParams = new URLSearchParams({
-	// 	product: 'SupporterPlus',
-	// 	ratePlan: supporterPlusRatePlan,
-	// });
-
-	// const promotionTier2 = getPromotion(
-	// 	window.guardian.allProductPrices.SupporterPlus,
-	// 	countryId,
-	// 	billingPeriod,
-	// );
-	// if (promotionTier2) {
-	// 	tier2UrlParams.set('promoCode', promotionTier2.promoCode);
-	// }
-	// const tier2Url = `checkout?${tier2UrlParams.toString()}`;
-	// const tier2Card = {
-	// 	productDescription: productCatalogDescription.SupporterPlus,
-	// 	price: tier2Pricing,
-	// 	link: tier2Url,
-	// 	/** The promotion from the querystring is for the SupporterPlus product only */
-	// 	promotion: promotionTier2,
-	// 	isRecommended: true,
-	// 	isUserSelected:
-	// 		urlSearchParamsProduct === 'SupporterPlus' ||
-	// 		isCardUserSelected(tier2Pricing, promotionTier2?.discount?.amount),
-	// 	ctaCopy: 'Support',
-	// };
-
-	// /**
-	//  * Tier 3: SupporterPlus with Guardian Weekly
-	//  * This products promotions are hard-coded for now
-	//  */
-
-	// /**
-	//  * We do this as sending the old amount (£10) down the pipes will cause
-	//  * `support-workers` to fail as it calculates the contribution amount from the amount sent minus the catalog price
-	//  * e.g. state.amount - catalogPrice i.e. 10-12 and failes if the price is less than 0
-	//  *
-	//  * @see: https://github.com/guardian/support-frontend/blob/main/support-workers/src/main/scala/com/gu/zuora/subscriptionBuilders/SupporterPlusSubcriptionBuilder.scala#L38-L42
-	//  *
-	//  * This should avoid a race condition of us deploying the price rise before frontend is deployed.
-	//  *
-	//  * This should only exist as long as the Tier three hack is in place.
-	//  */
-	// const getTier3RatePlan = () => {
-	// 	const ratePlan =
-	// 		countryGroupId === 'International'
-	// 			? contributionType === 'ANNUAL'
-	// 				? 'RestOfWorldAnnual'
-	// 				: 'RestOfWorldMonthly'
-	// 			: contributionType === 'ANNUAL'
-	// 			? 'DomesticAnnual'
-	// 			: 'DomesticMonthly';
-
-	// 	return abParticipations.newspaperArchiveBenefit === undefined
-	// 		? ratePlan
-	// 		: `${ratePlan}V2`;
-	// };
-
-	// const tier3RatePlan = getTier3RatePlan();
-	// const tier3Pricing =
-	// 	productCatalog.TierThree.ratePlans[tier3RatePlan].pricing[currencyId];
-
-	// const tier3UrlParams = new URLSearchParams({
-	// 	product: 'TierThree',
-	// 	ratePlan: tier3RatePlan,
-	// });
-	// const promotionTier3 = getPromotion(
-	// 	window.guardian.allProductPrices.TierThree,
-	// 	countryId,
-	// 	billingPeriod,
-	// 	countryGroupId === 'International' ? 'RestOfWorld' : 'Domestic',
-
-	// 	abParticipations.newspaperArchiveBenefit === undefined
-	// 		? 'NoProductOptions'
-	// 		: 'NewspaperArchive',
-	// );
-	// if (promotionTier3) {
-	// 	tier3UrlParams.set('promoCode', promotionTier3.promoCode);
-	// }
-	// const tier3Card = {
-	// 	productDescription: productCatalogDescription.TierThree,
-	// 	price: tier3Pricing,
-	// 	link: `checkout?${tier3UrlParams.toString()}`,
-	// 	promotion: promotionTier3,
-	// 	isRecommended: false,
-	// 	isUserSelected:
-	// 		urlSearchParamsProduct === 'TierThree' ||
-	// 		isCardUserSelected(tier3Pricing, promotionTier3?.discount?.amount),
-	// 	ctaCopy: 'Support',
-	// };
-
-	// const showNewspaperArchiveBanner =
-	// 	abParticipations.newspaperArchiveBenefit === 'v2';
 
 	return (
 		<PageScaffold
@@ -548,46 +188,12 @@ export function ContributionsOnlyLanding({
 				cssOverrides={recurringContainer}
 			>
 				<div css={innerContentContainer}>
-					{/* {countdownSwitchOn() && showCountdown && currentCountdownSettings && (
-						<Countdown
-							countdownCampaign={currentCountdownSettings}
-							showCountdown={showCountdown}
-							setShowCountdown={setShowCountdown}
-						/>
-					)} */}
-					{/* <h1 css={heading}>
-						{getHeadline(
-							showCountdown,
-							currentCountdownSettings,
-							campaignSettings,
-						)}
-					</h1> */}
-					{/* {countryGroupId !== 'UnitedStates' && (
-						<p css={standFirst}>
-							{campaignSettings?.copy.subheading ?? (
-								<>
-									We're not owned by a billionaire or shareholders - our readers
-									support us. Choose to join with one of the options below.{' '}
-									<strong>Cancel anytime.</strong>
-								</>
-							)}
-						</p>
-					)} */}
-					{/* {countryGroupId === 'UnitedStates' && (
-						<p css={standFirst}>
-							{campaignSettings?.copy.subheading ?? (
-								<>
-									We're not owned by a billionaire or profit-driven corporation:
-									our fiercely independent journalism is funded by our readers.
-									Monthly giving makes the most impact (and you can cancel
-									anytime). Thank you.
-								</>
-							)}
-						</p>
-					)} */}
-					{/* {campaignSettings?.tickerSettings && (
-						<TickerContainer tickerSettings={campaignSettings.tickerSettings} />
-					)} */}
+					<h1 css={heading}>Support fearless, independent journalism</h1>
+					<p css={standFirst}>
+						We're not owned by a billionaire or shareholders - our readers
+						support us. Choose to join with one of the options below.{' '}
+						<strong>Cancel anytime.</strong>
+					</p>
 					<PaymentFrequencyButtons
 						paymentFrequencies={paymentFrequencies.map(
 							(paymentFrequency, index) => ({
@@ -599,103 +205,19 @@ export function ContributionsOnlyLanding({
 						buttonClickHandler={handlePaymentFrequencyBtnClick}
 						additionalStyles={paymentFrequencyButtonsCss}
 					/>
-					{contributionType === 'ONE_OFF' && (
-						<OneOffCard
-							amounts={amounts}
-							currencyGlyph={currencies[currencyId].glyph}
-							countryGroupId={countryGroupId}
-							currencyId={currencyId}
-							heading={undefined}
-						/>
-					)}
-					{/* {contributionType !== 'ONE_OFF' && (
-						<ThreeTierCards
-							cardsContent={[tier1Card, tier2Card, tier3Card]}
-							currencyId={currencyId}
-							countryGroupId={countryGroupId}
-							paymentFrequency={contributionType}
-							abParticipations={abParticipations}
-						/>
-					)} */}
-					{/* {showNewspaperArchiveBanner && <LandingPageBanners />} */}
+					<AmountsCard
+						amountsData={amounts.amountsCardData[contributionType]}
+						countryGroupId={countryGroupId}
+						currencyId={currencyId}
+						contributionType={contributionType}
+					/>
 				</div>
 			</Container>
-			{/* {!enableSingleContributionsTab && (
-				<Container
-					sideBorders
-					borderColor="rgba(170, 170, 180, 0.5)"
-					cssOverrides={oneTimeContainer}
-				>
-					<SupportOnce
-						currency={currencies[currencyId].glyph}
-						countryGroupId={countryGroupId}
-					/>
-				</Container>
-			)} */}
-			{/* {countryGroupId === UnitedStates && (
-				<Container
-					sideBorders
-					borderColor="rgba(170, 170, 180, 0.5)"
-					cssOverrides={supportAnotherWayContainer}
-				>
-					<div css={supportAnotherWay}>
-						<h4>Support another way</h4>
-						<p>
-							If you are interested in contributing through a donor-advised
-							fund, foundation or retirement account, or by mailing a check,{' '}
-							<br />
-							please visit our{' '}
-							<a href="https://manage.theguardian.com/help-centre/article/contribute-another-way?INTCMP=gdnwb_copts_support_contributions_referral">
-								help page
-							</a>{' '}
-							to learn how.
-						</p>
-					</div>
-				</Container>
-			)} */}
-			{/* <Container
+			<Container
 				sideBorders
 				borderColor="rgba(170, 170, 180, 0.5)"
 				cssOverrides={disclaimerContainer}
-			>
-				<ThreeTierTsAndCs
-					tsAndCsContent={[
-						{
-							title: tier1Card.productDescription.label,
-							planCost: getPlanCost(tier1Card.price, contributionType),
-						},
-						{
-							title: tier2Card.productDescription.label,
-							planCost: getPlanCost(
-								tier2Card.price,
-								contributionType,
-								promotionTier2,
-							),
-							starts: promotionTier2?.starts
-								? new Date(promotionTier2.starts)
-								: undefined,
-							expires: promotionTier2?.expires
-								? new Date(promotionTier2.expires)
-								: undefined,
-						},
-						{
-							title: tier3Card.productDescription.label,
-							planCost: getPlanCost(
-								tier3Card.price,
-								contributionType,
-								promotionTier3,
-							),
-							starts: promotionTier3?.starts
-								? new Date(promotionTier3.starts)
-								: undefined,
-							expires: promotionTier3?.expires
-								? new Date(promotionTier3.expires)
-								: undefined,
-						},
-					]}
-					currency={currencies[currencyId].glyph}
-				></ThreeTierTsAndCs>
-			</Container> */}
+			></Container>
 		</PageScaffold>
 	);
 }
