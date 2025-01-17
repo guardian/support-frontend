170bed25384fd80e6fde298ebca6aaa3003aaa8c
Tom Wey <tom.wey@theguardian.com>
2025-01-17 17:16:08 +0000

    Update cookie set by Ad-Lite purchase to be persistent
    This cookie has also been renamed. It should persist for 7 days.
    Previously it was a session cookie.
    
    This includes a refactoring to support persistent cookies on the
    checkout in a product specific way.

--- support-frontend/app/controllers/CreateSubscriptionController.scala
+++ support-frontend/app/controllers/CreateSubscriptionController.scala
@@ -342,55 +342,74 @@ class CreateSubscriptionController(
       )
   }
 
+  sealed trait SupportCookie {
+    def toPlayCookie: Cookie
+  }
+  case class SessionCookie(name: String, value: String) extends SupportCookie {
+    def toPlayCookie: Cookie = Cookie(
+      name = name,
+      value = value,
+      secure = true,
+      httpOnly = false,
+      domain = Some(guardianDomain.value),
+    )
+  }
+  case class PersistentCookie(name: String, value: String, maxAge: Int) extends SupportCookie {
+    def toPlayCookie: Cookie = Cookie(
+      name = name,
+      value = value,
+      maxAge = Some(maxAge),
+      secure = true,
+      httpOnly = false,
+      domain = Some(guardianDomain.value),
+    )
+  }
+
   private def cookies(product: ProductType, userEmail: String): Future[List[Cookie]] = {
     // Setting the user attributes cookies used by frontend. See:
     // https://github.com/guardian/dotcom-rendering/blob/3c4700cae532993ace6f40c3b59c337f3efe2247/dotcom-rendering/src/client/userFeatures/user-features.ts
-    val standardCookies = List(
-      "gu_user_features_expiry" -> DateTime.now.plusDays(1).getMillis.toString,
-      "gu_hide_support_messaging" -> true.toString,
+    val standardCookies: List[SupportCookie] = List(
+      SessionCookie("gu_user_features_expiry", DateTime.now.plusDays(1).getMillis.toString),
+      SessionCookie("gu_hide_support_messaging", true.toString),
     )
-    val productCookies = product match {
+    val oneWeekInSeconds = 604800
+    val productCookies: List[SupportCookie] = product match {
       case Contribution(_, _, billingPeriod) =>
         List(
-          s"gu.contributions.recurring.contrib-timestamp.$billingPeriod" -> DateTime.now.getMillis.toString,
-          "gu_recurring_contributor" -> true.toString,
+          SessionCookie(
+            s"gu.contributions.recurring.contrib-timestamp.$billingPeriod",
+            DateTime.now.getMillis.toString,
+          ),
+          SessionCookie("gu_recurring_contributor", true.toString),
         )
       case _: SupporterPlus =>
         List(
-          "gu_digital_subscriber" -> true.toString,
+          SessionCookie("gu_digital_subscriber", true.toString),
           // "gu_supporter_plus" -> true.toString, // TODO: add this and remove the digisub one now that the CMP cookie list has been updated
-          "GU_AF1" -> DateTime.now().plusDays(1).getMillis.toString,
+          SessionCookie("GU_AF1", DateTime.now().plusDays(1).getMillis.toString),
         )
       case _: TierThree =>
         List(
-          "gu_digital_subscriber" -> true.toString,
-          // "gu_supporter_plus" -> true.toString, // TODO: add this and remove the digisub one now that the CMP cookie list has been updated
-          "GU_AF1" -> DateTime.now().plusDays(1).getMillis.toString,
+          SessionCookie("gu_digital_subscriber", true.toString),
+          // SessionCookie("gu_supporter_plus", true.toString), // TODO: add this and remove the digisub one now that the CMP cookie list has been updated
+          SessionCookie("GU_AF1", DateTime.now().plusDays(1).getMillis.toString),
         )
       case _: DigitalPack =>
         List(
-          "gu_digital_subscriber" -> true.toString,
-          "GU_AF1" -> DateTime.now().plusDays(1).getMillis.toString,
+          SessionCookie("gu_digital_subscriber", true.toString),
+          SessionCookie("GU_AF1", DateTime.now().plusDays(1).getMillis.toString),
         )
       case p: Paper if p.productOptions.hasDigitalSubscription =>
         List(
-          "gu_digital_subscriber" -> true.toString,
-          "GU_AF1" -> DateTime.now().plusDays(1).getMillis.toString,
+          SessionCookie("gu_digital_subscriber", true.toString),
+          SessionCookie("GU_AF1", DateTime.now().plusDays(1).getMillis.toString),
         )
       case _: Paper => List.empty
       case _: GuardianWeekly => List.empty
-      case _: GuardianAdLite => List("gu_allow_reject_all" -> true.toString)
+      case _: GuardianAdLite => List(PersistentCookie("gu_allow_reject_all", true.toString, oneWeekInSeconds))
     }
 
-    val standardAndProductCookies = (standardCookies ++ productCookies).map { case (name, value) =>
-      Cookie(
-        name = name,
-        value = value,
-        secure = true,
-        httpOnly = false,
-        domain = Some(guardianDomain.value),
-      )
-    }
+    val standardAndProductCookies = (standardCookies ++ productCookies).map(_.toPlayCookie)
 
     checkoutCompleteCookies(product, userEmail).map(_ ++ standardAndProductCookies)
   }
