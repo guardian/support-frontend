52c10d8d386f1c0fa185e16a80696d7f946a1d24
Tom Wey <tom.wey@theguardian.com>
2025-01-22 12:26:27 +0000

    Remove some cookies set by the checkout
    I've removed the following as they're no longer used:
    * gu_recurring_contributor
    * gu_digital_subscriber
    * gu.contributions.recurring.contrib-timestamp.<billing period>
    
    I've refactored the cookie setting code a bit to consolidate things and
    make it clear where products set the same cookie(s).

--- support-frontend/app/controllers/SubscriptionProductCookiesCreator.scala
+++ support-frontend/app/controllers/SubscriptionProductCookiesCreator.scala
@@ -32,47 +32,25 @@ case class SubscriptionProductCookiesCreator(domain: GuardianDomain) {
     )
   }
 
-  def createCookiesForProduct(product: ProductType): List[Cookie] = {
+  def createCookiesForProduct(product: ProductType, now: DateTime): List[Cookie] = {
     // Setting the user attributes cookies used by frontend. See:
     // https://github.com/guardian/dotcom-rendering/blob/3c4700cae532993ace6f40c3b59c337f3efe2247/dotcom-rendering/src/client/userFeatures/user-features.ts
     val standardCookies: List[SupportCookie] = List(
-      SessionCookie("gu_user_features_expiry", DateTime.now.plusDays(1).getMillis.toString),
+      SessionCookie("gu_user_features_expiry", now.plusDays(1).getMillis.toString),
       SessionCookie("gu_hide_support_messaging", true.toString),
     )
+
     val oneWeekInSeconds = 604800
+
     val productCookies: List[SupportCookie] = product match {
-      case Contribution(_, _, billingPeriod) =>
-        List(
-          SessionCookie(
-            s"gu.contributions.recurring.contrib-timestamp.$billingPeriod",
-            DateTime.now.getMillis.toString,
-          ),
-          SessionCookie("gu_recurring_contributor", true.toString),
-        )
-      case _: SupporterPlus =>
-        List(
-          SessionCookie("gu_digital_subscriber", true.toString),
-          // "gu_supporter_plus" -> true.toString, // TODO: add this and remove the digisub one now that the CMP cookie list has been updated
-          SessionCookie("GU_AF1", DateTime.now().plusDays(1).getMillis.toString),
-        )
-      case _: TierThree =>
-        List(
-          SessionCookie("gu_digital_subscriber", true.toString),
-          // SessionCookie("gu_supporter_plus", true.toString), // TODO: add this and remove the digisub one now that the CMP cookie list has been updated
-          SessionCookie("GU_AF1", DateTime.now().plusDays(1).getMillis.toString),
-        )
-      case _: DigitalPack =>
-        List(
-          SessionCookie("gu_digital_subscriber", true.toString),
-          SessionCookie("GU_AF1", DateTime.now().plusDays(1).getMillis.toString),
-        )
+      case _: SupporterPlus | _: TierThree | _: DigitalPack =>
+        List(SessionCookie("GU_AF1", now.plusDays(1).getMillis.toString))
+
       case p: Paper if p.productOptions.hasDigitalSubscription =>
-        List(
-          SessionCookie("gu_digital_subscriber", true.toString),
-          SessionCookie("GU_AF1", DateTime.now().plusDays(1).getMillis.toString),
-        )
-      case _: Paper => List.empty
-      case _: GuardianWeekly => List.empty
+        List(SessionCookie("GU_AF1", now.plusDays(1).getMillis.toString))
+
+      case _: Contribution | _: Paper | _: GuardianWeekly => List.empty
+
       case _: GuardianAdLite => List(PersistentCookie("gu_allow_reject_all", true.toString, oneWeekInSeconds))
     }
 
