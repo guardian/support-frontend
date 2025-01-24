8cf532226b3f1b1fb0120b8e275106c799249c3f
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2025-01-24 16:41:07 +0000

    end contributions only test and use new page

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -109,27 +109,6 @@ export const tests: Tests = {
 		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
 		excludeContributionsOnlyCountries: true,
 	},
-	contributionsOnly: {
-		variants: [
-			{
-				id: 'control',
-			},
-			{
-				id: 'variant',
-			},
-		],
-		audiences: {
-			CONTRIBUTIONS_ONLY: {
-				offset: 0,
-				size: 1,
-			},
-		},
-		isActive: true,
-		referrerControlled: false, // ab-test name not needed to be in paramURL
-		seed: 9,
-		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
-		excludeContributionsOnlyCountries: false,
-	},
 	benefitsReset: {
 		variants: [
 			{
