dacc40e00d9f73b5340f44c3171bf96f67cffd52
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-01-30 15:52:57 +0000

    Tidy up build script

--- support-workers/deploy-to-code.sh
+++ support-workers/deploy-to-code.sh
@@ -1,11 +1,10 @@
 S3_BUCKET="membership-dist"
 S3_KEY="support/CODE/support-workers-typescript/support-workers.zip"
-aws s3 cp target/support-workers.zip s3://$S3_BUCKET/$S3_KEY --profile membership
 LAMBDA_FUNCTIONS=(
   "-CreatePaymentMethodLambda-"
 )
-
-aws s3 cp target/support-workers.zip s3://$S3_BUCKET/$S3_KEY --profile membership
+echo "Uploading target/typescript/support-workers.zip to S3 (to update the zipfile before upload use the yarn deploy-to-code command)"
+aws s3 cp target/typescript/support-workers.zip s3://$S3_BUCKET/$S3_KEY --profile membership
 
 for function in "${LAMBDA_FUNCTIONS[@]}"; do
   echo "Updating $function..."
