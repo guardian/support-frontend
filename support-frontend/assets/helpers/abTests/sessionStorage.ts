001537c71d6a0638daaad6084ac7a9ddcefb66e3
Tom Forbes <tom.forbes@theguardian.com>
2025-02-03 10:31:24 +0000

    storage comments

--- support-frontend/assets/helpers/abTests/sessionStorage.ts
+++ support-frontend/assets/helpers/abTests/sessionStorage.ts
@@ -1,7 +1,9 @@
 import * as storage from '../storage/storage';
 import type { Participations } from './models';
 
+// For participation in tests defined in abtestDefinitions.ts
 const PARTICIPATIONS_KEY = 'abParticipations';
+// For participation in landing page tests, which are passed through from the server
 const LANDING_PAGE_PARTICIPATIONS_KEY = 'landingPageParticipations';
 type Key = typeof PARTICIPATIONS_KEY | typeof LANDING_PAGE_PARTICIPATIONS_KEY;
 
