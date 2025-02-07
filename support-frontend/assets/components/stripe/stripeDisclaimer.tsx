2a45dae5510c55ac5e150a4507f06de197eb5255
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-02-07 17:10:00 +0000

    feat:  checkout adjustments reset, in separate PR

--- support-frontend/assets/components/stripe/stripeDisclaimer.tsx
+++ support-frontend/assets/components/stripe/stripeDisclaimer.tsx
@@ -14,11 +14,11 @@ export function StripeDisclaimer() {
 		<>
 			All card payments are powered by Stripe. Read the Stripe{' '}
 			<a href="https://stripe.com/privacy" target="_blank">
-				Privacy Policy
+				privacy policy
 			</a>{' '}
 			and{' '}
 			<a href="https://stripe.com/legal/end-users" target="_blank">
-				Terms and conditions
+				terms and conditions
 			</a>
 			.
 		</>
