cc5410cf4e2df0e54adb55f7739804134d4216dd
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-02-12 15:44:11 +0000

    feat: abtest targeting refined to just  checkouts

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -156,11 +156,11 @@ export const tests: Tests = {
 				size: 1,
 			},
 		},
-		isActive: false,
+		isActive: true,
 		referrerControlled: false, // ab-test name not needed to be in paramURL
 		seed: 5,
 		targetPage:
-			pageUrlRegexes.subscriptions.paper.paperLandingWithGuestCheckout,
+			'(/subscribe/paper/checkout$|/subscribe/weekly/checkout$|/subscribe/weekly/checkout/gift$)',
 		excludeContributionsOnlyCountries: true,
 	},
 };
