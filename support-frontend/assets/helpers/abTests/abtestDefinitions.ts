7936557901878cd8630c935cc6f76d87883b182a
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-02-17 14:20:54 +0000

    feat: test off

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -155,7 +155,7 @@ export const tests: Tests = {
 				size: 1,
 			},
 		},
-		isActive: true,
+		isActive: false,
 		referrerControlled: false, // ab-test name not needed to be in paramURL
 		seed: 5,
 		targetPage:
