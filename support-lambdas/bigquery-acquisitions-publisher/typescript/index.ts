9ebedbad3d88ca1468b0ed5253dfe3ef621864ea
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-02-24 18:25:39 +0000

    feat: load credentials from param store, authorise client, access the bigQuery client

--- support-lambdas/bigquery-acquisitions-publisher/typescript/index.ts
+++ support-lambdas/bigquery-acquisitions-publisher/typescript/index.ts
@@ -1,3 +1,27 @@
-export const handler = (event: unknown) => {
-	console.log(event);
+import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm';
+import { awsConfig } from './config';
+import { Stage } from './stage';
+import { buildAuthClient, createBigQueryClient } from './bigQuery';
+
+// Retrieve Google Cloud Parameters from AWS Systems Manager -> Parameter Store
+export const getGCPCredentials = async (stage: Stage) => {
+	const ssmClient = new SSMClient(awsConfig);
+	const params = {
+		Name: `/bigquery-acquisitions-publisher/${stage}/gcp-wif-credentials-config`,
+		WithDecryption: true,
+	};
+	const command = new GetParameterCommand(params);
+	const response = await ssmClient.send(command);
+
+	if (!response.Parameter?.Value) {
+		throw new Error("Couldn't retrieve GCP credentials from parameter store");
+	}
+	return response.Parameter.Value;
+};
+
+export const handler = async (event: unknown) => {
+	const stage = process.env.STAGE as Stage;
+	const credentials = await getGCPCredentials(stage);
+	const authClient = await buildAuthClient(credentials);
+	const bigQueryClient = createBigQueryClient(authClient, stage);
 };
