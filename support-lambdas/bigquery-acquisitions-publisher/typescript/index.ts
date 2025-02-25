93fa6012186563f5487f4ca69184dec76567d523
Tom Wey <tom.wey@theguardian.com>
2025-02-25 12:22:43 +0000

    Comment as not used

--- support-lambdas/bigquery-acquisitions-publisher/typescript/index.ts
+++ support-lambdas/bigquery-acquisitions-publisher/typescript/index.ts
@@ -1,20 +1,20 @@
 import { Stage } from './stage';
 import { buildAuthClient, createBigQueryClient } from './bigQuery';
-import { BigQuery, Query } from '@google-cloud/bigquery';
+// import { BigQuery, Query } from '@google-cloud/bigquery';
 import { getGCPCredentialsFromSSM } from './ssm';
 
-const exampleReadFromBigQuery = async (bigQueryClient: BigQuery) => {
-	const query: Query = {
-		query: 'SELECT COUNT(*) FROM datalake.fact_acquisition_event',
-		location: 'europe-west2',
-	};
-	const [job] = await bigQueryClient.createQueryJob(query);
-	console.log(`Job ${job.id} started.`);
+// const exampleReadFromBigQuery = async (bigQueryClient: BigQuery) => {
+// 	const query: Query = {
+// 		query: 'SELECT COUNT(*) FROM datalake.fact_acquisition_event',
+// 		location: 'europe-west2',
+// 	};
+// 	const [job] = await bigQueryClient.createQueryJob(query);
+// 	console.log(`Job ${job.id} started.`);
 
-	const [rows] = await job.getQueryResults();
-	console.log('Rows:');
-	rows.forEach((row) => console.log(row));
-};
+// 	const [rows] = await job.getQueryResults();
+// 	console.log('Rows:');
+// 	rows.forEach((row) => console.log(row));
+// };
 
 export const handler = async (event: unknown) => {
 	const stage = process.env.STAGE as Stage;
