43e32432ecce76bba6ef1baa8c8d5565d843fe19
Tom Wey <tom.wey@theguardian.com>
2025-02-25 12:21:42 +0000

    Move the BigQuery read to a function so we can refer back to it

--- support-lambdas/bigquery-acquisitions-publisher/typescript/index.ts
+++ support-lambdas/bigquery-acquisitions-publisher/typescript/index.ts
@@ -1,13 +1,9 @@
 import { Stage } from './stage';
 import { buildAuthClient, createBigQueryClient } from './bigQuery';
-import { Query } from '@google-cloud/bigquery';
+import { BigQuery, Query } from '@google-cloud/bigquery';
 import { getGCPCredentialsFromSSM } from './ssm';
 
-export const handler = async (event: unknown) => {
-	const stage = process.env.STAGE as Stage;
-	const credentials = await getGCPCredentialsFromSSM(stage);
-	const authClient = await buildAuthClient(credentials);
-	const bigQueryClient = createBigQueryClient(authClient, stage);
+const exampleReadFromBigQuery = async (bigQueryClient: BigQuery) => {
 	const query: Query = {
 		query: 'SELECT COUNT(*) FROM datalake.fact_acquisition_event',
 		location: 'europe-west2',
@@ -18,4 +14,11 @@ export const handler = async (event: unknown) => {
 	const [rows] = await job.getQueryResults();
 	console.log('Rows:');
 	rows.forEach((row) => console.log(row));
+};
+
+export const handler = async (event: unknown) => {
+	const stage = process.env.STAGE as Stage;
+	const credentials = await getGCPCredentialsFromSSM(stage);
+	const authClient = await buildAuthClient(credentials);
+	createBigQueryClient(authClient, stage);
 };
