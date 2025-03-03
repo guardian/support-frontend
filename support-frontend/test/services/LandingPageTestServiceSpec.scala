545ff07b8d0b0a382aff3574514d23fc7d6eb68f
Tom Forbes <tom.forbes@theguardian.com>
2025-03-03 14:10:52 +0000

    refactor tests

--- support-frontend/test/services/LandingPageTestServiceSpec.scala
+++ support-frontend/test/services/LandingPageTestServiceSpec.scala
@@ -5,236 +5,121 @@ import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 
-import scala.jdk.CollectionConverters.MapHasAsJava
+import scala.jdk.CollectionConverters.{SeqHasAsJava, MapHasAsJava}
 
 class LandingPageTestServiceSpec extends AsyncFlatSpec with Matchers {
+
+  private def stringAttr(value: String): AttributeValue = AttributeValue.builder().s(value).build()
+  private def numberAttr(value: Int): AttributeValue = AttributeValue.builder().n(value.toString).build()
+  private def mapAttr(value: Map[String, AttributeValue]): AttributeValue =
+    AttributeValue.builder().m(value.asJava).build()
+  private def listAttr(value: List[AttributeValue]): AttributeValue = AttributeValue.builder().l(value.asJava).build()
+
   "LandingPageTestService" should "parseLandingPageTest" in {
     val dynamoTest = Map(
-      "channel" -> AttributeValue.builder().s("SupportLandingPage").build(),
-      "name" -> AttributeValue.builder().s("test1").build(),
-      "priority" -> AttributeValue.builder().n(3.toString).build(),
-      "status" -> AttributeValue.builder().s("Live").build(),
-      "regionTargeting" -> AttributeValue
-        .builder()
-        .m(
-          Map(
-            "targetedCountryGroups" -> AttributeValue
-              .builder()
-              .l(
-                AttributeValue.builder().s("GBPCountries").build(),
-              )
-              .build(),
-          ).asJava,
-        )
-        .build(),
-      "variants" -> AttributeValue
-        .builder()
-        .l(
-          AttributeValue
-            .builder()
-            .m(
-              Map(
-                "name" -> AttributeValue.builder().s("control").build(),
-                "copy" -> AttributeValue
-                  .builder()
-                  .m(
+      "channel" -> stringAttr("SupportLandingPage"),
+      "name" -> stringAttr("test1"),
+      "priority" -> numberAttr(3),
+      "status" -> stringAttr("Live"),
+      "regionTargeting" -> mapAttr(
+        Map(
+          "targetedCountryGroups" -> listAttr(List(stringAttr("GBPCountries"))),
+        ),
+      ),
+      "variants" -> listAttr(
+        List(
+          mapAttr(
+            Map(
+              "name" -> stringAttr("control"),
+              "copy" -> mapAttr(
+                Map(
+                  "heading" -> stringAttr("test heading"),
+                  "subheading" -> stringAttr("test subheading"),
+                ),
+              ),
+              "products" -> mapAttr(
+                Map(
+                  "Contribution" -> mapAttr(
                     Map(
-                      "heading" -> AttributeValue.builder().s("test heading").build(),
-                      "subheading" -> AttributeValue.builder().s("test subheading").build(),
-                    ).asJava,
-                  )
-                  .build(),
-                "products" -> AttributeValue
-                  .builder()
-                  .m(
+                      "title" -> stringAttr("Support"),
+                      "benefits" -> listAttr(
+                        List(
+                          mapAttr(
+                            Map("copy" -> stringAttr("Give to the Guardian every month with Support")),
+                          ),
+                        ),
+                      ),
+                      "cta" -> mapAttr(Map("copy" -> stringAttr("Support"))),
+                    ),
+                  ),
+                  "SupporterPlus" -> mapAttr(
                     Map(
-                      "Contribution" -> AttributeValue
-                        .builder()
-                        .m(
-                          Map(
-                            "title" -> AttributeValue.builder().s("Support").build(),
-                            "benefits" -> AttributeValue
-                              .builder()
-                              .l(
-                                AttributeValue
-                                  .builder()
-                                  .m(
-                                    Map(
-                                      "copy" -> AttributeValue
-                                        .builder()
-                                        .s("Give to the Guardian every month with Support")
-                                        .build(),
-                                    ).asJava,
-                                  )
-                                  .build(),
-                              )
-                              .build(),
-                            "cta" -> AttributeValue
-                              .builder()
-                              .m(
-                                Map(
-                                  "copy" -> AttributeValue.builder().s("Support").build(),
-                                ).asJava,
-                              )
-                              .build(),
-                          ).asJava,
-                        )
-                        .build(),
-                      "SupporterPlus" -> AttributeValue
-                        .builder()
-                        .m(
-                          Map(
-                            "title" -> AttributeValue.builder().s("All-access digital!!").build(),
-                            "benefits" -> AttributeValue
-                              .builder()
-                              .l(
-                                AttributeValue
-                                  .builder()
-                                  .m(
-                                    Map(
-                                      "copy" -> AttributeValue
-                                        .builder()
-                                        .s("Unlimited access to the Guardian app")
-                                        .build(),
-                                      "tooltip" -> AttributeValue
-                                        .builder()
-                                        .s(
-                                          "Read beyond our 20 article-per-month limit, enjoy offline access and personalised recommendations, and access our full archive of journalism. Never miss a story with the Guardian News app – a beautiful, intuitive reading experience.",
-                                        )
-                                        .build(),
-                                    ).asJava,
-                                  )
-                                  .build(),
-                                AttributeValue
-                                  .builder()
-                                  .m(
-                                    Map(
-                                      "copy" -> AttributeValue
-                                        .builder()
-                                        .s("Ad-free reading on all your devices")
-                                        .build(),
-                                    ).asJava,
-                                  )
-                                  .build(),
-                                AttributeValue
-                                  .builder()
-                                  .m(
-                                    Map(
-                                      "copy" -> AttributeValue
-                                        .builder()
-                                        .s(
-                                          "Exclusive newsletter for supporters, sent every week from the Guardian newsroom",
-                                        )
-                                        .build(),
-                                    ).asJava,
-                                  )
-                                  .build(),
-                                AttributeValue
-                                  .builder()
-                                  .m(
-                                    Map(
-                                      "copy" -> AttributeValue.builder().s("Far fewer asks for support").build(),
-                                      "tooltip" -> AttributeValue
-                                        .builder()
-                                        .s(
-                                          "You'll see far fewer financial support asks at the bottom of articles or in pop-up banners.",
-                                        )
-                                        .build(),
-                                    ).asJava,
-                                  )
-                                  .build(),
-                                AttributeValue
-                                  .builder()
-                                  .m(
-                                    Map(
-                                      "copy" -> AttributeValue
-                                        .builder()
-                                        .s("Unlimited access to the Guardian Feast app")
-                                        .build(),
-                                      "tooltip" -> AttributeValue
-                                        .builder()
-                                        .s(
-                                          "Make a feast out of anything with the Guardian’s new recipe app. Feast has thousands of recipes including quick and budget-friendly weeknight dinners, and showstopping weekend dishes – plus smart app features to make mealtimes inspiring.",
-                                        )
-                                        .build(),
-                                      "label" -> AttributeValue
-                                        .builder()
-                                        .m(
-                                          Map(
-                                            "copy" -> AttributeValue.builder().s("New").build(),
-                                          ).asJava,
-                                        )
-                                        .build(),
-                                    ).asJava,
-                                  )
-                                  .build(),
-                              )
-                              .build(),
-                            "cta" -> AttributeValue
-                              .builder()
-                              .m(
-                                Map(
-                                  "copy" -> AttributeValue.builder().s("Support").build(),
-                                ).asJava,
-                              )
-                              .build(),
-                            "label" -> AttributeValue
-                              .builder()
-                              .m(
-                                Map(
-                                  "copy" -> AttributeValue.builder().s("Recommended").build(),
-                                ).asJava,
-                              )
-                              .build(),
-                          ).asJava,
-                        )
-                        .build(),
-                      "TierThree" -> AttributeValue
-                        .builder()
-                        .m(
-                          Map(
-                            "title" -> AttributeValue.builder().s("Digital + print").build(),
-                            "benefits" -> AttributeValue
-                              .builder()
-                              .l(
-                                AttributeValue
-                                  .builder()
-                                  .m(
-                                    Map(
-                                      "copy" -> AttributeValue
-                                        .builder()
-                                        .s("Guardian Weekly print magazine delivered to your door every week")
-                                        .build(),
-                                      "tooltip" -> AttributeValue
-                                        .builder()
-                                        .s(
-                                          "Guardian Weekly is a beautifully concise magazine featuring a handpicked selection of in-depth articles, global news, long reads, opinion and more. Delivered to you every week, wherever you are in the world.",
-                                        )
-                                        .build(),
-                                    ).asJava,
-                                  )
-                                  .build(),
-                              )
-                              .build(),
-                            "cta" -> AttributeValue
-                              .builder()
-                              .m(
-                                Map(
-                                  "copy" -> AttributeValue.builder().s("Support").build(),
-                                ).asJava,
-                              )
-                              .build(),
-                          ).asJava,
-                        )
-                        .build(),
-                    ).asJava,
-                  )
-                  .build(),
-              ).asJava,
-            )
-            .build(),
-        )
-        .build(),
+                      "title" -> stringAttr("All-access digital!!"),
+                      "benefits" -> listAttr(
+                        List(
+                          mapAttr(
+                            Map(
+                              "copy" -> stringAttr("Unlimited access to the Guardian app"),
+                              "tooltip" -> stringAttr(
+                                "Read beyond our 20 article-per-month limit, enjoy offline access and personalised recommendations, and access our full archive of journalism. Never miss a story with the Guardian News app – a beautiful, intuitive reading experience.",
+                              ),
+                            ),
+                          ),
+                          mapAttr(Map("copy" -> stringAttr("Ad-free reading on all your devices"))),
+                          mapAttr(
+                            Map(
+                              "copy" -> stringAttr(
+                                "Exclusive newsletter for supporters, sent every week from the Guardian newsroom",
+                              ),
+                            ),
+                          ),
+                          mapAttr(
+                            Map(
+                              "copy" -> stringAttr("Far fewer asks for support"),
+                              "tooltip" -> stringAttr(
+                                "You'll see far fewer financial support asks at the bottom of articles or in pop-up banners.",
+                              ),
+                            ),
+                          ),
+                          mapAttr(
+                            Map(
+                              "copy" -> stringAttr("Unlimited access to the Guardian Feast app"),
+                              "tooltip" -> stringAttr(
+                                "Make a feast out of anything with the Guardian’s new recipe app. Feast has thousands of recipes including quick and budget-friendly weeknight dinners, and showstopping weekend dishes – plus smart app features to make mealtimes inspiring.",
+                              ),
+                              "label" -> mapAttr(Map("copy" -> stringAttr("New"))),
+                            ),
+                          ),
+                        ),
+                      ),
+                      "cta" -> mapAttr(Map("copy" -> stringAttr("Support"))),
+                      "label" -> mapAttr(Map("copy" -> stringAttr("Recommended"))),
+                    ),
+                  ),
+                  "TierThree" -> mapAttr(
+                    Map(
+                      "title" -> stringAttr("Digital + print"),
+                      "benefits" -> listAttr(
+                        List(
+                          mapAttr(
+                            Map(
+                              "copy" -> stringAttr("Guardian Weekly print magazine delivered to your door every week"),
+                              "tooltip" -> stringAttr(
+                                "Guardian Weekly is a beautifully concise magazine featuring a handpicked selection of in-depth articles, global news, long reads, opinion and more. Delivered to you every week, wherever you are in the world.",
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      "cta" -> mapAttr(Map("copy" -> stringAttr("Support"))),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     )
 
     val result = LandingPageTestService.parseLandingPageTest(dynamoTest.asJava)
