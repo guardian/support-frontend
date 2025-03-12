1f7d4ec94723576cc33ee95146b7ae06e8c26a73
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-03-12 17:50:50 +0000

    feat: add paymentTsAndCs container snapshot test

--- support-frontend/assets/pages/supporter-plus-landing/components/paymentTsAndCs.test.tsx
+++ support-frontend/assets/pages/supporter-plus-landing/components/paymentTsAndCs.test.tsx
@@ -1,27 +1,18 @@
 import { render } from '@testing-library/react';
-
-// import { create } from 'react-test-renderer';
-// import { PaymentTsAndCs } from './paymentTsAndCs';
-
-function Link() {
-	return <div>Hello World</div>;
-}
+import { PaymentTsAndCs } from './paymentTsAndCs';
 
 describe('Payment Ts&Cs Snapshot comparison', () => {
-	it('renders correctly', () => {
-		const { asFragment } = render(<Link />);
-		expect(asFragment()).toMatchSnapshot();
+	it('render SupportPlus', () => {
+		const { container } = render(
+			<PaymentTsAndCs
+				contributionType={'MONTHLY'}
+				countryGroupId={'GBPCountries'}
+				productKey={'SupporterPlus'}
+				currency={'GBP'}
+				amount={0}
+				amountIsAboveThreshold={false}
+			/>,
+		);
+		expect(container).toMatchSnapshot();
 	});
-	// it('render SupportPlus', () => {
-	// 	const tree = create(
-	// 		<PaymentTsAndCs
-	// 			contributionType={'MONTHLY'}
-	// 			countryGroupId={'GBPCountries'}
-	// 			productKey={'SupporterPlus'}
-	// 			currency={'GBP'}
-	// 			amount={0}
-	// 		/>,
-	// 	).toJSON();
-	// 	expect(tree).toMatchSnapshot();
-	// });
 });
