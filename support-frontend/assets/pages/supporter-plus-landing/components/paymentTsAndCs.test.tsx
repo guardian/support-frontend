857a030ef9ba432398a2cb07c9056ea6a6baf052
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-03-12 15:38:49 +0000

    feat: match snapshot test setup

---
+++ support-frontend/assets/pages/supporter-plus-landing/components/paymentTsAndCs.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+
+// import { create } from 'react-test-renderer';
+// import { PaymentTsAndCs } from './paymentTsAndCs';
+
+function Link() {
+	return <div>Hello World</div>;
+}
+
+describe('Payment Ts&Cs Snapshot comparison', () => {
+	it('renders correctly', () => {
+		const { asFragment } = render(<Link />);
+		expect(asFragment()).toMatchSnapshot();
+	});
+	// it('render SupportPlus', () => {
+	// 	const tree = create(
+	// 		<PaymentTsAndCs
+	// 			contributionType={'MONTHLY'}
+	// 			countryGroupId={'GBPCountries'}
+	// 			productKey={'SupporterPlus'}
+	// 			currency={'GBP'}
+	// 			amount={0}
+	// 		/>,
+	// 	).toJSON();
+	// 	expect(tree).toMatchSnapshot();
+	// });
+});
