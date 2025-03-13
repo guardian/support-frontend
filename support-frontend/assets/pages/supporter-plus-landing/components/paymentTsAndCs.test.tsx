a6220ec7ec677e739a3532683957ec20145257d0
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-03-13 12:31:00 +0000

    feat: Snapshot tests applied for all products

--- support-frontend/assets/pages/supporter-plus-landing/components/paymentTsAndCs.test.tsx
+++ support-frontend/assets/pages/supporter-plus-landing/components/paymentTsAndCs.test.tsx
@@ -1,4 +1,6 @@
 import { render } from '@testing-library/react';
+import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
+import type { ActiveProductKey } from 'helpers/productCatalog';
 import { PaymentTsAndCs } from './paymentTsAndCs';
 
 // Mocking price retrieval from productCatalog (not available in window at runtime)
@@ -7,17 +9,29 @@ jest.mock('helpers/supporterPlus/benefitsThreshold', () => ({
 }));
 
 describe('Payment Ts&Cs Snapshot comparison', () => {
-	it('render SupportPlus', () => {
-		const { container } = render(
-			<PaymentTsAndCs
-				contributionType={'MONTHLY'}
-				countryGroupId={'GBPCountries'}
-				productKey={'SupporterPlus'}
-				currency={'GBP'}
-				amount={0}
-				amountIsAboveThreshold={true}
-			/>,
-		);
-		expect(container).toMatchSnapshot();
-	});
+	const productKeys = [
+		['Contribution', 'UnitedStates', false],
+		['SupporterPlus', 'GBPCountries', true],
+		['TierThree', 'GBPCountries', true],
+		['OneTimeContribution', 'AUDCountries', false],
+		['GuardianAdLite', 'GBPCountries', false],
+		['DigitalSubscription', 'GBPCountries', false],
+	];
+	it.each(productKeys)(
+		`render product %s for region %s (above threshold %s) correctly`,
+		(productKey, countryGroupId, amountIsAboveThreshold) => {
+			console.log('productKey:', productKey);
+			const { container } = render(
+				<PaymentTsAndCs
+					contributionType={'MONTHLY'}
+					countryGroupId={countryGroupId as CountryGroupId}
+					productKey={productKey as ActiveProductKey}
+					currency={'GBP'}
+					amount={0}
+					amountIsAboveThreshold={amountIsAboveThreshold as boolean}
+				/>,
+			);
+			expect(container).toMatchSnapshot();
+		},
+	);
 });
