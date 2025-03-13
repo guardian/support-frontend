e3176083f158c2251e1e3a736223b709833a740f
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-03-13 11:46:52 +0000

    feat: test setup for S+

--- support-frontend/assets/pages/supporter-plus-landing/components/paymentTsAndCs.test.tsx
+++ support-frontend/assets/pages/supporter-plus-landing/components/paymentTsAndCs.test.tsx
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react';
 import { PaymentTsAndCs } from './paymentTsAndCs';
 
+// Mocking price retrieval from productCatalog (not available in window at runtime)
+jest.mock('helpers/supporterPlus/benefitsThreshold', () => ({
+	getLowerProductBenefitThreshold: () => 12,
+}));
+
 describe('Payment Ts&Cs Snapshot comparison', () => {
 	it('render SupportPlus', () => {
 		const { container } = render(
@@ -10,7 +15,7 @@ describe('Payment Ts&Cs Snapshot comparison', () => {
 				productKey={'SupporterPlus'}
 				currency={'GBP'}
 				amount={0}
-				amountIsAboveThreshold={false}
+				amountIsAboveThreshold={true}
 			/>,
 		);
 		expect(container).toMatchSnapshot();
