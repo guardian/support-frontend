b51ee9b7f4b8fe8daff71a8c72684be4b0d86652
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-03-14 12:17:16 +0000

    feat: further payment Ts&Cs products added

--- support-frontend/stories/checkouts/paymentTsAndCs.stories.tsx
+++ support-frontend/stories/checkouts/paymentTsAndCs.stories.tsx
@@ -16,12 +16,52 @@ function Template(args: PaymentTsAndCsProps) {
 
 Template.args = {} as Omit<PaymentTsAndCsProps, ''>;
 
+export const DigitalSubscription = Template.bind({});
+DigitalSubscription.args = {
+	productKey: 'DigitalSubscription',
+	contributionType: 'MONTHLY',
+	currency: 'GBP',
+	amount: 0,
+	countryGroupId: 'GBPCountries',
+	amountIsAboveThreshold: true,
+};
+
+export const GuardianAdLite = Template.bind({});
+GuardianAdLite.args = {
+	productKey: 'GuardianAdLite',
+	contributionType: 'MONTHLY',
+	currency: 'GBP',
+	amount: 0,
+	countryGroupId: 'GBPCountries',
+	amountIsAboveThreshold: false,
+};
+
+export const OneTimeContribution = Template.bind({});
+OneTimeContribution.args = {
+	productKey: 'OneTimeContribution',
+	contributionType: 'ONE_OFF',
+	currency: 'AUD',
+	amount: 5,
+	countryGroupId: 'AUDCountries',
+	amountIsAboveThreshold: false,
+};
+
+export const Contribution = Template.bind({});
+Contribution.args = {
+	productKey: 'Contribution',
+	contributionType: 'MONTHLY',
+	currency: 'USD',
+	amount: 5,
+	countryGroupId: 'UnitedStates',
+	amountIsAboveThreshold: false,
+};
+
 export const SupporterPlus = Template.bind({});
 SupporterPlus.args = {
 	productKey: 'SupporterPlus',
 	contributionType: 'MONTHLY',
 	currency: 'GBP',
-	amount: 0,
+	amount: 12,
 	countryGroupId: 'GBPCountries',
 	amountIsAboveThreshold: true,
 };
@@ -31,7 +71,7 @@ TierThree.args = {
 	productKey: 'TierThree',
 	contributionType: 'MONTHLY',
 	currency: 'GBP',
-	amount: 0,
+	amount: 27,
 	countryGroupId: 'GBPCountries',
 	amountIsAboveThreshold: true,
 };
