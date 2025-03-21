7f640a87f68386b5ed4c006a752d57cb39188eaf
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-03-21 18:18:48 +0000

    review: paymentTsAndCs tests and stories include ContributionType and PromoCode fields

--- support-frontend/assets/pages/supporter-plus-landing/components/paymentTsAndCs.test.tsx
+++ support-frontend/assets/pages/supporter-plus-landing/components/paymentTsAndCs.test.tsx
@@ -1,26 +1,42 @@
 import { render } from '@testing-library/react';
+import type { ContributionType } from 'helpers/contributions';
 import type { CountryGroupId } from 'helpers/internationalisation/countryGroup';
 import type { ActiveProductKey } from 'helpers/productCatalog';
+import type { Promotion } from 'helpers/productPrice/promotions';
 import { PaymentTsAndCs } from './paymentTsAndCs';
 
 describe('Payment Ts&Cs Snapshot comparison', () => {
+	const promotionTierThreUnitedStatesMonthly: Promotion = {
+		name: '$8 off for 12 months',
+		description: 'Tier Three United States Monthly',
+		promoCode: 'TIER_THREE_USA_MONTHLY',
+		numberOfDiscountedPeriods: 12,
+		discountedPrice: 37,
+	};
 	const paymentProductKeys = [
-		['Contribution', 'UnitedStates', 0],
-		['SupporterPlus', 'GBPCountries', 12],
-		['TierThree', 'GBPCountries', 27],
-		['OneTimeContribution', 'AUDCountries', 0],
-		['GuardianAdLite', 'GBPCountries', 0],
-		['DigitalSubscription', 'GBPCountries', 0],
+		['GuardianAdLite', 'ANNUAL', 'GBPCountries', 0],
+		['DigitalSubscription', 'MONTHLY', 'GBPCountries', 0],
+		['OneTimeContribution', 'MONTHLY', 'UnitedStates', 0],
+		['Contribution', 'ANNUAL', 'AUDCountries', 0],
+		['SupporterPlus', 'MONTHLY', 'GBPCountries', 12],
+		['TierThree', 'MONTHLY', 'UnitedStates', 45],
 	];
 	it.each(paymentProductKeys)(
 		`paymentTs&Cs render product %s for region %s correctly`,
-		(paymentProductKey, countryGroupId, amount) => {
+		(paymentProductKey, contributionType, countryGroupId, amount) => {
+			const promo: Promotion | undefined =
+				paymentProductKey === 'TierThree' &&
+				contributionType === 'MONTHLY' &&
+				countryGroupId === 'UnitedStates'
+					? promotionTierThreUnitedStatesMonthly
+					: undefined;
 			const { container } = render(
 				<PaymentTsAndCs
-					contributionType={'MONTHLY'}
+					contributionType={contributionType as ContributionType}
 					countryGroupId={countryGroupId as CountryGroupId}
 					productKey={paymentProductKey as ActiveProductKey}
 					thresholdAmount={amount as number}
+					promotion={promo}
 				/>,
 			);
 			expect(container).toMatchSnapshot();
