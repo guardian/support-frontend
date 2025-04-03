48a4ba190fca05b5f1ade647ff322c0c6fb99aa4
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-04-03 18:07:50 +0100

    feat: nullish checks amended

--- support-frontend/assets/components/thankYou/signIn/signInItems.tsx
+++ support-frontend/assets/components/thankYou/signIn/signInItems.tsx
@@ -86,7 +86,7 @@ export function SignInBodyCopy({
 		setIsExpanded(true);
 	};
 
-	const isTierThreeOrObserver = isTierThree ?? observerPrint ?? false;
+	const isTierThreeOrObserver = Boolean(observerPrint ?? isTierThree);
 	const upperCopy = `By signing in, you help us to recognise you as a valued supporter when you visit our website or app. This means we can:`;
 	const upperCopyTier3 = `Make sure you sign in on all your devices when browsing our website and app. This helps us recognise you as a valued subscriber so you can enjoy all the benefits included in your subscription.`;
 	const lowerCopy = `Make sure you sign in on each of the devices you use to read our journalism – either today or next time you use them.`;
@@ -153,7 +153,7 @@ export function SignInCTA({
 	observerPrint,
 }: SignInBodyCopyProps): JSX.Element {
 	const [signInUrl, setSignInUrl] = useState('https://theguardian.com');
-	const isTierThreeOrObserver = isTierThree ?? observerPrint ?? false;
+	const isTierThreeOrObserver = Boolean(observerPrint ?? isTierThree);
 	function fetchSignInLink(payload: { email: string }) {
 		if (!isCodeOrProd()) {
 			return;
