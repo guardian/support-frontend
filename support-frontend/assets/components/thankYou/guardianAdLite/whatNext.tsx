f1bb9b1122d9d25681164e2c56ac0b3ede95ba72
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-04-14 16:58:10 +0100

    review: simplify whatNext

--- support-frontend/assets/components/thankYou/guardianAdLite/whatNext.tsx
+++ support-frontend/assets/components/thankYou/guardianAdLite/whatNext.tsx
@@ -17,17 +17,23 @@ export function WhatNext({
 	observerPrint,
 }: WhatNextProps): JSX.Element {
 	if (observerPrint) {
-		const observerListItems = [
-			observerPrint === ObserverPrint.SubscriptionCard
-				? `You should receive an email confirming the details of your subscription, and another email shortly afterwards that contains details of how you can pick up your newspapers from tomorrow.`
-				: 'Look out for an email from us confirming your subscription. It has everything you need to know about how to manage it in the future.',
-			observerPrint === ObserverPrint.SubscriptionCard
-				? 'You will receive your Subscription Card in your subscriber pack in the post, along with your home delivery letter.'
-				: 'Your newspaper will be delivered to your door.',
-			observerPrint === ObserverPrint.SubscriptionCard &&
-				'Visit your chosen participating newsagent to pick up your newspaper using your Subscription Card, or arrange a home delivery using your delivery letter.',
-			'The Observer team will be in touch shortly via email to welcome you. Check your spam folder or add the observer.co.uk domain to your preferred sender list.',
+		const observerPrintItem =
+			'The Observer team will be in touch shortly via email to welcome you. Check your spam folder or add the observer.co.uk domain to your preferred sender list.';
+		const observerSubscriptionCardItems = [
+			'You should receive an email confirming the details of your subscription, and another email shortly afterwards that contains details of how you can pick up your newspapers from tomorrow.',
+			'You will receive your Subscription Card in your subscriber pack in the post, along with your home delivery letter.',
+			'Visit your chosen participating newsagent to pick up your newspaper using your Subscription Card, or arrange a home delivery using your delivery letter.',
+			observerPrintItem,
 		];
+		const observerPaperItems = [
+			'Look out for an email from us confirming your subscription. It has everything you need to know about how to manage it in the future.',
+			'Your newspaper will be delivered to your door.',
+			observerPrintItem,
+		];
+		const observerListItems =
+			observerPrint === ObserverPrint.Paper
+				? observerPaperItems
+				: observerSubscriptionCardItems;
 		return <OrderedList items={observerListItems.filter(Boolean)} />;
 	}
 
