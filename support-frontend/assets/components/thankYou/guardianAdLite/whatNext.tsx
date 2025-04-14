93b5ce4e329e951f957c503a63b6c26d2c83a54d
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-04-14 14:11:56 +0100

    feat: subscriptionCard whatnext copy points 1 and 2 updated

--- support-frontend/assets/components/thankYou/guardianAdLite/whatNext.tsx
+++ support-frontend/assets/components/thankYou/guardianAdLite/whatNext.tsx
@@ -18,8 +18,12 @@ export function WhatNext({
 }: WhatNextProps): JSX.Element {
 	if (observerPrint) {
 		const observerListItems = [
-			'Look out for an email from us confirming your subscription. It has everything you need to know about how to manage it in the future.',
-			'Your newspaper will be delivered to your door.',
+			observerPrint === ObserverPrint.SubscriptionCard
+				? `You should receive an email confirming the details of your subscription, and another email shortly afterwards that contains details of how you can pick up your newspapers from tomorrow.`
+				: 'Look out for an email from us confirming your subscription. It has everything you need to know about how to manage it in the future.',
+			observerPrint === ObserverPrint.SubscriptionCard
+				? 'You will receive your Subscription Card in your subscriber pack in the post, along with your home delivery letter.'
+				: 'Your newspaper will be delivered to your door.',
 			observerPrint === ObserverPrint.SubscriptionCard &&
 				'Visit your chosen participating newsagent to pick up your newspaper using your Subscription Card, or arrange a home delivery using your delivery letter.',
 			'The Observer team will be in touch shortly via email to welcome you. Check your spam folder or add the observer.co.uk domain to your preferred sender list.',
