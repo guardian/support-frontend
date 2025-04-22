a29b9d5c05ff74904444b1a5050f82c07d4f8a2b
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-04-22 13:17:15 +0100

    step11: exports supporter-plus removed

--- support-frontend/assets/helpers/supporterPlus/isContributionRecurring.ts
+++ support-frontend/assets/helpers/supporterPlus/isContributionRecurring.ts
@@ -3,12 +3,6 @@ import type {
 	RegularContributionType,
 } from 'helpers/contributions';
 
-export function isOneOff(
-	contributionType: ContributionType,
-): contributionType is 'ONE_OFF' {
-	return contributionType === 'ONE_OFF';
-}
-
 export function isRecurring(
 	contributionType: ContributionType,
 ): contributionType is RegularContributionType {
