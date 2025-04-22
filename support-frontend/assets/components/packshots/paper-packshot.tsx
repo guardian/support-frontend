4bd809c559c113612b677a57adc10f3b89c389c8
Jorge Costa <jorge.costa@thetrainline.com>
2025-04-22 22:57:23 +0100

    cleanup paperPackshot image

--- support-frontend/assets/components/packshots/paper-packshot.tsx
+++ support-frontend/assets/components/packshots/paper-packshot.tsx
@@ -1,15 +1,10 @@
 import GridImage from 'components/gridImage/gridImage';
-import shouldShowObserverCard from 'pages/paper-subscription-landing/helpers/shouldShowObserver';
 
 function PaperPackshot() {
-	const gridId = shouldShowObserverCard()
-		? 'subscriptionPrintObserver'
-		: 'subscriptionPrint';
-
 	return (
 		<div className="subscriptions__paper-packshot">
 			<GridImage
-				gridId={gridId}
+				gridId="subscriptionPrintObserver"
 				srcSizes={[500, 140]}
 				sizes="(max-width: 739px) 140px,
              (max-width: 979px) 500px,
