5d9a87cf2e88cb6f3bdee0bb1ba9beec88cfc2e4
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-04-25 17:54:32 +0100

    feat: DirectDebit account number and sort code numeric length check

--- support-frontend/assets/helpers/forms/formValidation.ts
+++ support-frontend/assets/helpers/forms/formValidation.ts
@@ -48,3 +48,12 @@ export function isValidPostcode(postcode: string): boolean {
 		/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) {0,1}[0-9][A-Za-z]{2})$/g;
 	return postcodeRegex.test(postcode);
 }
+
+export function isValidDirectDebit(
+	sortCode: string,
+	accountNumber: string,
+): boolean {
+	const sortCodeRegex = /^[0-9]{6}$/;
+	const accountNumberRegex = /^[0-9]{8}$/;
+	return sortCodeRegex.test(sortCode) && accountNumberRegex.test(accountNumber);
+}
