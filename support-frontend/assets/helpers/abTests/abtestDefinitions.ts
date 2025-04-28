17a5e0a44d326ab4187c5a30a8b375feaf1eeafd
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-04-28 14:39:29 +0100

    feat: setup abtest guardianWeeklyGenericCheckout

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -25,6 +25,8 @@ export const pageUrlRegexes = {
 			paperLandingWithGuestCheckout:
 				/\/subscribe\/paper(\/delivery|\/checkout|\/checkout\/guest)?(\?.*)?$/,
 			paperLandingPage: /^\/uk\/subscribe\/paper?(\?.*)?$/,
+			weeklyLandingPage:
+				/\/subscribe\/weekly(\/checkout|\/checkout\/gift)?(\?.*)?$/,
 		},
 		subsWeeklyPages:
 			'(/??/subscribe(\\?.*)?$|/??/subscribe/weekly(\\/checkout)?(\\?.*)?$)',
@@ -113,6 +115,30 @@ export const tests: Tests = {
 		seed: 9,
 		targetPage:
 			pageUrlRegexes.subscriptions.paper.paperLandingWithGuestCheckout,
+		persistPage:
+			// match generic checkout & thank you page
+			'^/uk/(checkout|thank-you)',
+		excludeContributionsOnlyCountries: true,
+	},
+	guardianWeeklyGenericCheckout: {
+		variants: [
+			{
+				id: 'control',
+			},
+			{
+				id: 'variant',
+			},
+		],
+		audiences: {
+			ALL: {
+				offset: 0,
+				size: 1,
+			},
+		},
+		isActive: false,
+		referrerControlled: false, // ab-test name not needed to be in paramURL
+		seed: 9,
+		targetPage: pageUrlRegexes.subscriptions.paper.weeklyLandingPage,
 		persistPage:
 			// match generic checkout & thank you page
 			'^/uk/(checkout|thank-you)',
