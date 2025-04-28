a858e8907320cdd34f2a2e7331d9472db6357d4a
Tom Wey <tom.wey@theguardian.com>
2025-04-28 09:50:33 +0100

    Add 2 more Stripe errors to the list we don't want to alarm on

--- support-workers/src/main/scala/com/gu/support/workers/exceptions/CardDeclinedMessages.scala
+++ support-workers/src/main/scala/com/gu/support/workers/exceptions/CardDeclinedMessages.scala
@@ -12,6 +12,8 @@ object CardDeclinedMessages {
     "Transaction declined.402 - [card_error/card_declined/card_velocity_exceeded] Your card was declined for making repeated attempts too frequently or exceeding its amount limit.",
     "Transaction declined.402 - [card_error/card_declined/revocation_of_authorization] Your card was declined.",
     "Transaction declined.402 - [card_error/authentication_required/authentication_required] Your card was declined. This transaction requires authentication.",
+    "Transaction declined.402 - [card_error/card_declined/fraudulent] Your card was declined.",
+    "Transaction declined.402 - [card_error/expired_card/expired_card] Your card has expired.",
     "Transaction declined.10417 - Instruct the customer to retry the transaction using an alternative payment method from the customers PayPal wallet.",
     "Transaction declined.validation_failed - account_number did not pass modulus check",
     "Transaction declined.validation_failed - account_number is the wrong length (should be 8 characters)",
