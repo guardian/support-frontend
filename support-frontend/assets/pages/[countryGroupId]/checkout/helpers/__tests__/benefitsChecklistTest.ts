b98982696484a38abf352816f8855fcadd340de0
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-04-30 14:07:11 +0100

    Refactor benefits checklist logic into a separate file

---
+++ support-frontend/assets/pages/[countryGroupId]/checkout/helpers/__tests__/benefitsChecklistTest.ts
@@ -0,0 +1,135 @@
+import { getBenefitsChecklistFromLandingPageTool } from '../benefitsChecklist';
+
+describe('getBenefitsChecklistFromLandingPageTool', () => {
+	const landingPageSettings = {
+		name: 'TierThree',
+		copy: {
+			heading: 'TierThree heading',
+			subheading: 'TierThree subheading',
+		},
+		products: {
+			TierThree: {
+				title: 'TierThree title',
+				benefits: [
+					{
+						copy: 'TierThree benefit 1',
+					},
+					{
+						copy: 'TierThree benefit 2',
+					},
+				],
+				cta: {
+					copy: 'TierThree cta',
+				},
+			},
+			SupporterPlus: {
+				title: 'SupporterPlus title',
+				benefits: [
+					{
+						copy: 'SupporterPlus benefit 1',
+					},
+					{
+						copy: 'SupporterPlus benefit 2',
+					},
+				],
+				cta: {
+					copy: 'SupporterPlus cta',
+				},
+			},
+			Contribution: {
+				title: 'Contribution title',
+				benefits: [
+					{
+						copy: 'Contribution benefit 1',
+					},
+					{
+						copy: 'Contribution benefit 2',
+					},
+				],
+				cta: {
+					copy: 'Contribution cta',
+				},
+			},
+		},
+	};
+	it('should return the correct benefits checklist for TierThree', () => {
+		expect(
+			getBenefitsChecklistFromLandingPageTool('TierThree', landingPageSettings),
+		).toEqual([
+			{
+				isChecked: true,
+				text: 'TierThree benefit 1',
+			},
+			{
+				isChecked: true,
+				text: 'TierThree benefit 2',
+			},
+			{
+				isChecked: true,
+				text: 'SupporterPlus benefit 1',
+			},
+			{
+				isChecked: true,
+				text: 'SupporterPlus benefit 2',
+			},
+		]);
+	});
+	it('should return the correct benefits checklist for Contribution', () => {
+		expect(
+			getBenefitsChecklistFromLandingPageTool(
+				'Contribution',
+				landingPageSettings,
+			),
+		).toMatchObject([
+			{
+				isChecked: true,
+				text: 'Contribution benefit 1',
+			},
+			{
+				isChecked: true,
+				text: 'Contribution benefit 2',
+			},
+			{
+				isChecked: false,
+				maybeGreyedOut: {
+					name: expect.any(String) as string,
+					styles: expect.any(String) as string,
+				},
+				text: 'SupporterPlus benefit 1',
+			},
+			{
+				isChecked: false,
+				maybeGreyedOut: {
+					name: expect.any(String) as string,
+					styles: expect.any(String) as string,
+				},
+				text: 'SupporterPlus benefit 2',
+			},
+		]);
+	});
+	it('should return the correct benefits checklist for SupporterPlus', () => {
+		expect(
+			getBenefitsChecklistFromLandingPageTool(
+				'SupporterPlus',
+				landingPageSettings,
+			),
+		).toEqual([
+			{
+				isChecked: true,
+				text: 'SupporterPlus benefit 1',
+			},
+			{
+				isChecked: true,
+				text: 'SupporterPlus benefit 2',
+			},
+		]);
+	});
+	it('should return undefined for products other than T3, S+ & RC', () => {
+		expect(
+			getBenefitsChecklistFromLandingPageTool(
+				'GuardianAdLite',
+				landingPageSettings,
+			),
+		).toBeUndefined();
+	});
+});
---
+++ support-frontend/assets/pages/[countryGroupId]/checkout/helpers/benefitsChecklist.ts
@@ -0,0 +1,83 @@
+import { css } from '@emotion/react';
+import { palette } from '@guardian/source/foundations';
+import type { ProductKey } from '@modules/product-catalog/productCatalog';
+import type { BenefitsCheckListData } from '../../../../components/checkoutBenefits/benefitsCheckList';
+import type { Participations } from '../../../../helpers/abTests/models';
+import type { LandingPageVariant } from '../../../../helpers/globalsAndSwitches/landingPageSettings';
+import type { CountryGroupId } from '../../../../helpers/internationalisation/countryGroup';
+import {
+	filterBenefitByABTest,
+	filterBenefitByRegion,
+} from '../../../../helpers/productCatalog';
+import type {
+	ProductBenefit,
+	ProductDescription,
+} from '../../../../helpers/productCatalog';
+
+const benefitsAsChecklist = ({
+	checked,
+	unchecked,
+}: {
+	checked: ProductBenefit[];
+	unchecked: ProductBenefit[];
+}): BenefitsCheckListData[] => {
+	return [
+		...checked.map((benefit) => ({
+			isChecked: true,
+			text: benefit.copy,
+		})),
+		...unchecked.map((benefit) => ({
+			isChecked: false,
+			text: benefit.copy,
+			maybeGreyedOut: css`
+				color: ${palette.neutral[60]};
+				svg {
+					fill: ${palette.neutral[60]};
+				}
+			`,
+		})),
+	];
+};
+
+export const getBenefitsChecklistFromLandingPageTool = (
+	productKey: ProductKey,
+	landingPageSettings: LandingPageVariant,
+): BenefitsCheckListData[] | undefined => {
+	// Three Tier products get their config from the Landing Page tool
+	if (productKey === 'Contribution') {
+		// Also show SupporterPlus benefits greyed out
+		return benefitsAsChecklist({
+			checked: landingPageSettings.products.Contribution.benefits,
+			unchecked: landingPageSettings.products.SupporterPlus.benefits,
+		});
+	} else if (productKey === 'SupporterPlus') {
+		return benefitsAsChecklist({
+			checked: landingPageSettings.products.SupporterPlus.benefits,
+			unchecked: [],
+		});
+	} else if (productKey === 'TierThree') {
+		// Also show SupporterPlus benefits
+		return benefitsAsChecklist({
+			checked: [
+				...landingPageSettings.products.TierThree.benefits,
+				...landingPageSettings.products.SupporterPlus.benefits,
+			],
+			unchecked: [],
+		});
+	}
+	return;
+};
+
+export const getBenefitsChecklistFromProductDescription = (
+	productDescription: ProductDescription,
+	countryGroupId: CountryGroupId,
+	abParticipations: Participations,
+): BenefitsCheckListData[] => {
+	return productDescription.benefits
+		.filter((benefit) => filterBenefitByRegion(benefit, countryGroupId))
+		.filter((benefit) => filterBenefitByABTest(benefit, abParticipations))
+		.map((benefit) => ({
+			isChecked: true,
+			text: `${benefit.copyBoldStart ?? ''}${benefit.copy}`,
+		}));
+};
