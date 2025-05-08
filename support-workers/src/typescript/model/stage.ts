02e45e041cdf820d485f22cca8bbb711835a4935
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-05-08 14:52:16 +0100

    Remove createPaymentMethodLambda to simplify the PR

--- support-workers/src/typescript/model/stage.ts
+++
@@ -1,9 +0,0 @@
-export type Stage = 'CODE' | 'PROD';
-
-export const stageFromEnvironment = (): Stage => {
-	const stage = process.env.STAGE;
-	if (stage === undefined) {
-		throw new Error('STAGE is not defined as an environment variable');
-	}
-	return stage as Stage;
-};
