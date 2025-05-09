67507e525c28567f4dd23ce08ecfaa6604f63ab8
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-05-09 13:32:44 +0100

    Revert "Remove createPaymentMethodLambda to simplify the PR"
    This reverts commit 4c9d623b412859603824956748539a0651553fc2.

---
+++ support-workers/src/typescript/model/paymentMethod.ts
@@ -0,0 +1,73 @@
+import { z } from 'zod';
+import { stripePaymentTypeSchema } from './paymentFields';
+// Payment methods are the activated payment details which are passed into Zuora as opposed to
+// payment fields which are the details entered by the user into the checkout
+
+export const stripePaymentGatewaySchema = z.union([
+	z.literal('Stripe PaymentIntents GNM Membership'),
+	z.literal('Stripe PaymentIntents GNM Membership AUS'),
+	z.literal('Stripe - Observer - Tortoise Media'),
+]);
+export type StripePaymentGateway = z.infer<typeof stripePaymentGatewaySchema>;
+
+const directDebitPaymentGatewaySchema = z.literal('GoCardless');
+
+export const paymentGatewaySchema = z
+	.union([
+		z.literal('PayPal Express'),
+		directDebitPaymentGatewaySchema,
+		z.literal('Amazon Pay - Contributions USA'),
+	])
+	.or(stripePaymentGatewaySchema);
+
+const payPalPaymentPaymentMethodSchema = z.object({
+	PaypalBaid: z.string(),
+	PaypalEmail: z.string(),
+	PaypalType: z.literal('ExpressCheckout'),
+	Type: z.literal('PayPal'),
+	PaymentGateway: z.literal('PayPal Express'),
+});
+export type PayPalPaymentMethod = z.infer<
+	typeof payPalPaymentPaymentMethodSchema
+>;
+
+const stripePaymentMethodSchema = z.object({
+	TokenId: z.string(), // Stripe Card id
+	SecondTokenId: z.string(), // Stripe Customer Id
+	CreditCardNumber: z.string(),
+	CreditCardCountry: z.string().nullable(),
+	CreditCardExpirationMonth: z.number(),
+	CreditCardExpirationYear: z.number(),
+	CreditCardType: z.string().optional(),
+	PaymentGateway: stripePaymentGatewaySchema,
+	Type: z.literal('CreditCardReferenceTransaction'),
+	StripePaymentType: stripePaymentTypeSchema.optional(),
+});
+export type StripePaymentMethod = z.infer<typeof stripePaymentMethodSchema>;
+
+const directDebitPaymentMethodSchema = z.object({
+	FirstName: z.string(),
+	LastName: z.string(),
+	BankTransferAccountName: z.string(),
+	BankCode: z.string(),
+	BankTransferAccountNumber: z.string(),
+	Country: z.string(), //TODO: build a schema for this
+	City: z.string().nullable(),
+	PostalCode: z.string().nullable(),
+	State: z.string().nullable(),
+	StreetName: z.string().nullable(),
+	StreetNumber: z.string().nullable(),
+	BankTransferType: z.literal('DirectDebitUK'),
+	Type: z.literal('BankTransfer'),
+	PaymentGateway: directDebitPaymentGatewaySchema,
+});
+export type DirectDebitPaymentMethod = z.infer<
+	typeof directDebitPaymentMethodSchema
+>;
+
+export const paymentMethodSchema = z.discriminatedUnion('Type', [
+	payPalPaymentPaymentMethodSchema,
+	stripePaymentMethodSchema,
+	directDebitPaymentMethodSchema,
+]);
+export type PaymentMethod = z.infer<typeof paymentMethodSchema>;
