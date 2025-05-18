b8f476ef6960f04cf491f8db1f60e96f40575fa5
Jorge Costa <jorge.costa@thetrainline.com>
2025-05-18 15:35:38 +0100

    refactor heading for print products

---
+++ support-frontend/assets/pages/supporter-plus-thank-you/components/thankYouHeader/PrintProductsHeading.tsx
@@ -0,0 +1,92 @@
+import type { ActiveRatePlanKey } from 'helpers/productCatalog';
+import { css } from '@emotion/react';
+
+const yellowHighlightText = css`
+	background-color: #ffe500;
+	padding: 0 5px;
+`;
+
+export function YellowHighlightText({ text }: { text: string }) {
+	return <span css={yellowHighlightText}>{text}</span>;
+}
+
+export function PrintProductsHeading({
+	isObserverPrint,
+	ratePlanKey,
+	isPending,
+}: {
+	isObserverPrint: boolean;
+	ratePlanKey?: ActiveRatePlanKey;
+	isPending: boolean;
+}) {
+	if (isObserverPrint) {
+		const statusText = isPending ? (
+			<>
+				Your <YellowHighlightText text="Observer subscription" /> is being
+				processed
+			</>
+		) : (
+			<>
+				You are now an <YellowHighlightText text="Observer subscription" />.
+			</>
+		);
+		return (
+			<>
+				{statusText}
+				Welcome and thank you for supporting Observer journalism!
+			</>
+		);
+	}
+
+	if (!ratePlanKey) return;
+
+	const thankYouText = 'Thank you for supporting our journalism!';
+	const guardianWeekly = ['Monthly', 'Annual', 'Quarterly'].includes(
+		ratePlanKey,
+	);
+	if (guardianWeekly) {
+		const statusText = isPending ? (
+			<>
+				Your subscription to <YellowHighlightText text="the Guardian Weekly" />{' '}
+				is being processed
+			</>
+		) : (
+			<>
+				You have now subscribed to{' '}
+				<YellowHighlightText text="the Guardian Weekly" />
+			</>
+		);
+		return (
+			<>
+				{thankYouText}
+				{statusText}
+			</>
+		);
+	}
+
+	const guardianWeeklyGifting = ['ThreeMonthGift', 'OneYearGift'].includes(
+		ratePlanKey,
+	);
+	if (guardianWeeklyGifting) {
+		const statusText = isPending
+			? 'Your Guardian Weekly gift subscription is being processed'
+			: 'Your purchase of a Guardian Weekly gift subscription is now complete';
+		return (
+			<>
+				{thankYouText}
+				{statusText}
+			</>
+		);
+	}
+
+	const paperRatePlanName =
+		ratePlanKey === 'Everyday' ? 'Every day' : ratePlanKey;
+	return (
+		<>
+			{thankYouText}
+			{isPending
+				? `Your subscription to the ${paperRatePlanName} package is being processed`
+				: `You have now subscribed to the ${paperRatePlanName} package`}
+		</>
+	);
+}
