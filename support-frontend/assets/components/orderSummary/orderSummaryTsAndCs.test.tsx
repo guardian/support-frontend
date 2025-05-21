71a98ee84b37ad963fff69c1d4c5199c06656781
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-05-21 09:17:37 +0100

    review: orderSummaryTs&Cs snapshot reset

--- support-frontend/assets/components/orderSummary/orderSummaryTsAndCs.test.tsx
+++ support-frontend/assets/components/orderSummary/orderSummaryTsAndCs.test.tsx
@@ -8,34 +8,42 @@ import type { Promotion } from 'helpers/productPrice/promotions';
 import { OrderSummaryTsAndCs } from './orderSummaryTsAndCs';
 
 describe('orderSummaryTs&Cs Snapshot comparison', () => {
-	const promotionTierThreUnitedStatesMonthly: Promotion = {
+	const promotionTierThreeUnitedStatesMonthly: Promotion = {
 		name: '$8 off for 12 months',
 		description: 'Tier Three United States Monthly',
 		promoCode: 'TIER_THREE_USA_MONTHLY',
 		numberOfDiscountedPeriods: 12,
 		discountedPrice: 37,
 	};
-	const paymentProductKeys = [
+
+	type OrderSummaryTestParams = [
+		ActiveProductKey,
+		CountryGroupId,
+		ActiveRatePlanKey,
+		number,
+	];
+
+	const orderSummaryProductKeys: OrderSummaryTestParams[] = [
 		['GuardianAdLite', 'GBPCountries', 'Monthly', 0],
 		['Contribution', 'AUDCountries', 'Annual', 0],
-		['SupporterPlus', 'Monthly', 'GBPCountries', 'Monthly', 12],
-		['TierThree', 'Monthly', 'UnitedStates', 'Monthly', 45],
+		['SupporterPlus', 'GBPCountries', 'Monthly', 12],
+		['TierThree', 'UnitedStates', 'Monthly', 45],
 	];
-	it.each(paymentProductKeys)(
+	it.each(orderSummaryProductKeys)(
 		`orderSummaryTs&Cs render product %s for region %s correctly`,
 		(paymentProductKey, countryGroupId, activeRatePlanKey, amount) => {
 			const promo: Promotion | undefined =
 				paymentProductKey === 'TierThree' &&
 				activeRatePlanKey === 'Monthly' &&
 				countryGroupId === 'UnitedStates'
-					? promotionTierThreUnitedStatesMonthly
+					? promotionTierThreeUnitedStatesMonthly
 					: undefined;
 			const { container } = render(
 				<OrderSummaryTsAndCs
-					productKey={paymentProductKey as ActiveProductKey}
-					ratePlanKey={activeRatePlanKey as ActiveRatePlanKey}
-					countryGroupId={countryGroupId as CountryGroupId}
-					thresholdAmount={amount as number}
+					productKey={paymentProductKey}
+					ratePlanKey={activeRatePlanKey}
+					countryGroupId={countryGroupId}
+					thresholdAmount={amount}
 					promotion={promo}
 				/>,
 			);
