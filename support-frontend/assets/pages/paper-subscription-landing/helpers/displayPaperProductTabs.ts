bead35a66138fad9acb73ff456e28a32a203bfcb
Jorge Costa <jorge.costa@thetrainline.com>
2025-06-09 09:37:47 +0100

    do not display paper tabs on code by default

--- support-frontend/assets/pages/paper-subscription-landing/helpers/displayPaperProductTabs.ts
+++ support-frontend/assets/pages/paper-subscription-landing/helpers/displayPaperProductTabs.ts
@@ -1,7 +1,4 @@
-import { isCode } from 'helpers/urls/url';
-
 export const displayPaperProductTabs = () => {
 	const searchParams = new URLSearchParams(window.location.search);
-	const enableObserver = searchParams.get('paperProductTabs') === 'true';
-	return enableObserver || isCode();
+	return searchParams.get('paperProductTabs') === 'true';
 };
