cc9f54b211ab4e34b23240cb3b8dc88e0c3609a9
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-06-10 15:05:47 +0100

    Make support-frontend linting work

--- support-frontend/.eslintrc.js
+++ support-frontend/.eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
-	extends: ['@guardian/eslint-config', 'plugin:storybook/recommended'],
+	extends: ['plugin:storybook/recommended'],
 	overrides: [
 		{
 			files: ['*.ts', '*.tsx'],
 			// Only use Typescript linting on Typescript files
-			extends: ['@guardian/eslint-config-typescript'],
 			rules: {
 				// TODO: update this to 'warn' or delete once the post-migration fixing process is done
 				'import/no-default-export': 'off',
@@ -16,12 +15,6 @@ module.exports = {
 					},
 				],
 				'@emotion/pkg-renaming': 'error',
-				'@typescript-eslint/no-unused-vars': [
-					'error',
-					{
-						ignoreRestSiblings: true,
-					},
-				],
 			},
 		},
 	],
