ada010a9f0ca643cd201a5094d8afce4bcfffe08
Tom Wey <tom.wey@theguardian.com>
2025-06-25 12:01:23 +0100

    Revert "Revert "Merge pull request #7044 from guardian/dependabot/npm_and_yarn/cdk/guardian/cdk-61.5.1" (#7101)"
    This reverts commit ae01964e1cf68301207303b772a897291b72fc62.

--- cdk/lib/frontend.ts
+++ cdk/lib/frontend.ts
@@ -141,6 +141,7 @@ export class Frontend extends GuStack {
         domainName,
         hostedZoneId: "Z1E4V12LQGXFEC",
       },
+      instanceMetricGranularity: "5Minute",
       monitoringConfiguration: {
         snsTopicName: `alarms-handler-topic-${this.stage}`,
         http5xxAlarm: http5xxAlarm,
