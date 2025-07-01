51e1a1e9f9563e5e0c051f4f6a26dc8e176c7377
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-07-01 15:10:12 +0100

    review: student folder aligned, accordianFAQ and landingPageLayout moved into shared components , studentHeaderCard unused and removed

--- support-frontend/assets/pages/[countryGroupId]/student/components/studentTsAndCs.tsx
+++ support-frontend/assets/pages/[countryGroupId]/student/components/studentTsAndCs.tsx
@@ -1,46 +1,7 @@
-import { css } from '@emotion/react';
-import { from, palette, space, textSans12 } from '@guardian/source/foundations';
 import type { CountryGroupId } from '@modules/internationalisation/countryGroup';
 import { Container } from 'components/layout/container';
 import { privacyLink } from 'helpers/legal';
-
-const container = css`
-	${textSans12}
-	color: ${palette.neutral[97]};
-	background-color: ${palette.brand[400]};
-	position: relative;
-	align-content: end;
-	> div {
-		padding: ${space[3]}px ${space[4]}px ${space[4]}px;
-		display: flex;
-		justify-content: center;
-		border-bottom: 1px solid ${palette.brand[600]};
-		${from.tablet} {
-			width: 742px;
-			border-left: 1px solid ${palette.brand[600]};
-			border-right: 1px solid ${palette.brand[600]};
-		}
-		${from.desktop} {
-			width: 982px;
-		}
-		${from.leftCol} {
-			width: 1142px;
-		}
-		${from.wide} {
-			width: 1302px;
-		}
-	}
-	> div > div {
-		max-width: 940px;
-		& a {
-			font-weight: bold;
-			color: ${palette.neutral[97]};
-			:visited {
-				color: ${palette.neutral[97]};
-			}
-		}
-	}
-`;
+import { container } from './studentTsAndCsStyles';
 
 const studentTsAndCs: Partial<Record<CountryGroupId, JSX.Element>> = {
 	AUDCountries: (
--- support-frontend/assets/pages/[countryGroupId]/student/studentLandingPage.tsx
+++ support-frontend/assets/pages/[countryGroupId]/student/studentLandingPage.tsx
@@ -1,9 +1,8 @@
 import { AUDCountries } from '@modules/internationalisation/countryGroup';
 import type { CountryGroupSwitcherProps } from 'components/countryGroupSwitcher/countryGroupSwitcher';
 import { type GeoId, getGeoIdConfig } from 'pages/geoIdConfig';
-import { StudentHeaderCard } from './components/studentHeaderCard';
+import { LandingPageLayout } from '../guardianAdLiteLanding/components/landingPageLayout';
 import { StudentTsAndCs } from './components/studentTsAndCs';
-import { LandingPageLayout } from './guardianAdLiteLanding/components/landingPageLayout';
 
 type Props = {
 	geoId: GeoId;
@@ -18,7 +17,6 @@ export function StudentLandingPage({ geoId }: Props) {
 	}; // AU initially, further updates will display with more regions
 	return (
 		<LandingPageLayout countrySwitcherProps={countrySwitcherProps}>
-			<StudentHeaderCard geoId={geoId} />
 			<StudentTsAndCs countryGroupId={countryGroupId} />
 		</LandingPageLayout>
 	);
