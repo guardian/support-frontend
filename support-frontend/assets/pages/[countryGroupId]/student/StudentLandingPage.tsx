a539ea5d088460add7d69d06fdea7b58a70c1175
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-07-02 14:32:32 +0100

    feat: rebase linting error

--- support-frontend/assets/pages/[countryGroupId]/student/StudentLandingPage.tsx
+++ support-frontend/assets/pages/[countryGroupId]/student/StudentLandingPage.tsx
@@ -8,8 +8,8 @@ import { type GeoId } from 'pages/geoIdConfig';
 import { AccordionFAQ } from '../components/accordionFAQ';
 import StudentHeader from './components/StudentHeader';
 import { StudentTsAndCs } from './components/studentTsAndCs';
-import { getStudentTsAndCs } from './helpers/studentTsAndCsCopy';
 import { getStudentFAQs } from './helpers/studentFAQs';
+import { getStudentTsAndCs } from './helpers/studentTsAndCsCopy';
 
 export function StudentLandingPage({ geoId }: { geoId: GeoId }) {
 	const faqItems = getStudentFAQs(geoId);
