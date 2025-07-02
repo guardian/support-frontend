3428ab426addd7750172f65b0ee95c2837455d92
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-07-02 12:16:43 +0100

    rebase: filename alignment

--- support-frontend/assets/pages/[countryGroupId]/router.tsx
+++ support-frontend/assets/pages/[countryGroupId]/router.tsx
@@ -53,7 +53,7 @@ const LandingPage = lazy(() => {
 });
 const StudentLandingPage = lazy(() => {
 	return import(
-		/* webpackChunkName: "StudentLandingPage" */ './student/studentLandingPage'
+		/* webpackChunkName: "StudentLandingPage" */ './student/StudentLandingPage'
 	).then((mod) => {
 		return { default: mod.StudentLandingPage };
 	});
