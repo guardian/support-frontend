0753224164ef7c2273d820449007cae5f7ff0c2e
Jorge Costa <jorge.costa@thetrainline.com>
2025-07-08 10:14:26 +0100

    fetch benefits from the pageSettings variant

--- support-frontend/assets/pages/[countryGroupId]/student/components/StudentHeader.tsx
+++ support-frontend/assets/pages/[countryGroupId]/student/components/StudentHeader.tsx
@@ -1,5 +1,6 @@
 import { BillingPeriod } from '@modules/product/billingPeriod';
 import { Container } from 'components/layout/container';
+import type { LandingPageVariant } from 'helpers/globalsAndSwitches/landingPageSettings';
 import type { GeoId } from 'pages/geoIdConfig';
 import getProductContents from '../helpers/getProductContents';
 import LogoUTS from '../logos/uts';
@@ -13,8 +14,14 @@ import {
 } from './StudentHeaderStyles';
 import StudentProductCard from './StudentProductCard';
 
-export default function StudentHeader({ geoId }: { geoId: GeoId }) {
-	const productContent = getProductContents(geoId);
+export default function StudentHeader({
+	geoId,
+	landingPageVariant,
+}: {
+	geoId: GeoId;
+	landingPageVariant: LandingPageVariant;
+}) {
+	const productContent = getProductContents(geoId, landingPageVariant);
 	return (
 		<Container
 			sideBorders
