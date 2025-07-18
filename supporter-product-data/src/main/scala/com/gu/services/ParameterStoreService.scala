904ff1e3c11ae3179232a3e3fd8006cdb74a19de
Tom Wey <tom.wey@theguardian.com>
2025-07-18 14:30:34 +0100

    Migrate supporter-product-data to use AWS SDK v2 for SSM

--- supporter-product-data/src/main/scala/com/gu/services/ParameterStoreService.scala
+++ supporter-product-data/src/main/scala/com/gu/services/ParameterStoreService.scala
@@ -1,76 +1,70 @@
 package com.gu.services
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.auth.{
-  AWSCredentialsProviderChain,
-  EC2ContainerCredentialsProviderWrapper,
-  EnvironmentVariableCredentialsProvider,
-  InstanceProfileCredentialsProvider,
-}
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.simplesystemsmanagement.model.{
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.ssm.{SsmAsyncClient, SsmClient}
+import software.amazon.awssdk.services.ssm.model.{
   GetParameterRequest,
   GetParametersByPathRequest,
   ParameterType,
   PutParameterRequest,
 }
-import com.amazonaws.services.simplesystemsmanagement.{
-  AWSSimpleSystemsManagementAsync,
-  AWSSimpleSystemsManagementAsyncClientBuilder,
-}
+import com.gu.aws.CredentialsProvider
 import com.gu.AwsAsync
-import com.gu.aws.ProfileName
 import com.gu.supporterdata.model.Stage
 
 import scala.concurrent.ExecutionContext
 import scala.jdk.CollectionConverters._
 
-class ParameterStoreService(client: AWSSimpleSystemsManagementAsync, stage: Stage) {
+class ParameterStoreService(client: SsmClient, asyncClient: SsmAsyncClient, stage: Stage) {
   val configRoot = s"/supporter-product-data/${stage.value}"
 
   def getParametersByPath(path: String)(implicit executionContext: ExecutionContext) = {
-    val request: GetParametersByPathRequest = new GetParametersByPathRequest()
-      .withPath(s"$configRoot/$path/")
-      .withRecursive(false)
-      .withWithDecryption(true)
-    client.getParametersByPath(request).getParameters.asScala.toList
+    val request: GetParametersByPathRequest = GetParametersByPathRequest
+      .builder()
+      .path(s"$configRoot/$path/")
+      .recursive(false)
+      .withDecryption(true)
+      .build()
+
+    client.getParametersByPath(request).parameters().asScala.toList
   }
 
   def getParameter(name: String)(implicit executionContext: ExecutionContext) = {
-    val request = new GetParameterRequest()
-      .withName(s"$configRoot/$name")
-      .withWithDecryption(true)
+    val request = GetParameterRequest
+      .builder()
+      .name(s"$configRoot/$name")
+      .withDecryption(true)
+      .build()
 
-    AwsAsync(client.getParameterAsync, request).map(_.getParameter.getValue)
+    AwsAsync((request: GetParameterRequest) => asyncClient.getParameter(request), request).map(_.parameter().value())
   }
 
-  def putParameter(name: String, value: String, parameterType: ParameterType = ParameterType.String) = {
+  def putParameter(name: String, value: String) = {
+    val putParameterRequest = PutParameterRequest
+      .builder()
+      .name(s"$configRoot/$name")
+      .`type`(ParameterType.STRING)
+      .value(value)
+      .overwrite(true)
+      .build()
 
-    val putParameterRequest = new PutParameterRequest()
-      .withName(s"$configRoot/$name")
-      .withType(parameterType)
-      .withValue(value)
-      .withOverwrite(true)
-
-    AwsAsync(client.putParameterAsync, putParameterRequest)
+    AwsAsync((request: PutParameterRequest) => asyncClient.putParameter(request), putParameterRequest)
   }
 
 }
 
 object ParameterStoreService {
-  // please update to AWS SDK 2 and use com.gu.aws.CredentialsProvider
-  lazy val CredentialsProviderDEPRECATEDV1 = new AWSCredentialsProviderChain(
-    new ProfileCredentialsProvider(ProfileName),
-    new InstanceProfileCredentialsProvider(false),
-    new EnvironmentVariableCredentialsProvider(),
-    new EC2ContainerCredentialsProviderWrapper(), // for use with lambda snapstart
-  )
-
-  lazy val client = AWSSimpleSystemsManagementAsyncClientBuilder
-    .standard()
-    .withRegion(Regions.EU_WEST_1)
-    .withCredentials(CredentialsProviderDEPRECATEDV1)
+  val client = SsmClient
+    .builder()
+    .credentialsProvider(CredentialsProvider)
+    .region(Region.EU_WEST_1)
     .build()
 
-  def apply(stage: Stage) = new ParameterStoreService(client, stage)
+  val asyncClient = SsmAsyncClient
+    .builder()
+    .credentialsProvider(CredentialsProvider)
+    .region(Region.EU_WEST_1)
+    .build()
+
+  def apply(stage: Stage) = new ParameterStoreService(client, asyncClient, stage)
 }
