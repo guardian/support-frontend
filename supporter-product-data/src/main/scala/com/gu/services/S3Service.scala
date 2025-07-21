ff762c969d41b532202a1abf45e6c8f51f716c83
Tom Wey <tom.wey@theguardian.com>
2025-07-21 16:23:37 +0100

    Finish migrating the S3 SDK code
    Still need to test this, but the compiler is happy at least.
    
    Just SQS to go.

--- supporter-product-data/src/main/scala/com/gu/services/S3Service.scala
+++ supporter-product-data/src/main/scala/com/gu/services/S3Service.scala
@@ -4,18 +4,18 @@ import com.gu.aws.CredentialsProvider
 import com.gu.supporterdata.model.Stage
 import com.typesafe.scalalogging.StrictLogging
 import software.amazon.awssdk.core.async.{AsyncRequestBody, AsyncResponseTransformer}
+import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.{S3AsyncClient, S3Client}
 import software.amazon.awssdk.services.s3.model.{GetObjectRequest, PutObjectRequest}
 
 import scala.compat.java8.FutureConverters._
 import java.io.InputStream
+import java.nio.charset.StandardCharsets
 import java.util.concurrent.{ExecutorService, Executors}
+import scala.io.Source
 
 object S3Service extends StrictLogging {
-  // TODO: what should the size of this thread pool be?
-  val executor: ExecutorService = Executors.newFixedThreadPool(2)
-
   val s3Client: S3Client = S3Client
     .builder()
     .region(Region.EU_WEST_1)
@@ -33,14 +33,21 @@ object S3Service extends StrictLogging {
       .key(filename)
       .build()
 
-    // Upload using the stream and convert to Scala Future
-    val body = AsyncRequestBody.fromInputStream(inputStream, length.getOrElse(0), executor)
-    s3Client.putObject(putObjectRequest, body).toScala
+    val body = RequestBody.fromInputStream(inputStream, length.getOrElse(0))
+    s3Client.putObject(putObjectRequest, body)
   }
 
   def streamFromS3(stage: Stage, filename: String) = {
     logger.info(s"Trying to stream from S3 - bucketName: ${bucketName(stage)}, filename: $filename")
     val request = GetObjectRequest.builder().bucket(bucketName(stage)).key(filename).build()
     s3Client.getObject(request)
+  }
+
+  def getAsString(bucket: String, key: String): String = {
+    val request = GetObjectRequest.builder().bucket(bucket).key(key).build()
+    val responseStream = s3Client.getObject(request)
+    val contentAsString = Source.fromInputStream(responseStream)(StandardCharsets.UTF_8).mkString
+    responseStream.close()
+    contentAsString
   }
 }
