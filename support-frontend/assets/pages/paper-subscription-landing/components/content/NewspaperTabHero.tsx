b9caa4c13f92630f2c644f9df6feb23e5f870229
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-07-21 16:26:28 +0100

    feat: styling and text changes to bring copy and design closer

--- support-frontend/assets/pages/paper-subscription-landing/components/content/NewspaperTabHero.tsx
+++ support-frontend/assets/pages/paper-subscription-landing/components/content/NewspaperTabHero.tsx
@@ -41,10 +41,11 @@ const copyWidthStyle = css`
 	}
 `;
 const paragraphStyle = css`
+	text-align: justify;
 	line-height: 115%;
 	padding-bottom: ${space[2]}px;
 	${from.tablet} {
-		padding-bottom: ${space[3]}px;
+		padding-bottom: ${space[4]}px;
 		max-width: 669px;
 	}
 `;
@@ -68,12 +69,10 @@ const accordionRowOverride = css`
 		fill: ${palette.neutral[100]};
 	}
 	> div > div > p {
-		padding-bottom: ${space[3]}px;
-		font-weight: 700;
+		padding-bottom: ${space[2]}px;
 	}
 `;
 const linkStyle = css`
-	font-weight: bolder;
 	& a {
 		color: ${neutral[100]};
 		:visited {
@@ -82,10 +81,14 @@ const linkStyle = css`
 	}
 `;
 
+type HeroDelivery = {
+	title: string;
+	content: JSX.Element;
+};
 type HeroContent = {
-	productInfo: Array<string | JSX.Element>;
+	productInfo: string[];
 	imageUrl: string;
-	deliveryDetails?: JSX.Element;
+	delivery: HeroDelivery[];
 };
 
 const heroContent: Record<PaperFulfilmentOptions, HeroContent> = {
@@ -95,45 +98,65 @@ const heroContent: Record<PaperFulfilmentOptions, HeroContent> = {
 		],
 		imageUrl:
 			'https://i.guim.co.uk/img/media/6c9e57a633a20d9c863071dc38dfa24680676cbb/0_0_1011_607/1011.png?width=252&quality=75&s=da12a5fe67381ed9c4243223569d7992',
-		deliveryDetails: (
-			<>
-				<p>Your newspaper will arrive before 9am.</p>
-				<p>
-					We can’t deliver to individual flats, or apartments within blocks
-					because we need access to your post box to deliver your newspaper.
-				</p>
-				<p>
-					You can pause your subscription for up to 5 weeks a year. So if you’re
-					going away anywhere, you won’t have to pay for the newspapers that you
-					miss.
-				</p>
-			</>
-		),
+		delivery: [
+			{
+				title: 'Delivery details',
+				content: (
+					<>
+						<p>Your newspaper will arrive before 9am.</p>
+						<p>
+							We can’t deliver to individual flats, or apartments within blocks
+							because we need access to your post box to deliver your newspaper.
+						</p>
+						<p>
+							You can pause your subscription for up to 5 weeks a year. So if
+							you’re going away anywhere, you won’t have to pay for the
+							newspapers that you miss.
+						</p>
+					</>
+				),
+			},
+		],
 	},
 	Collection: {
 		productInfo: [
 			`Use your Guardian subscription card at 40,000 UK shops with news kiosks, including McColl's, Co-op, One Stop, and select SPAR stores. Collect your paper in-store or arrange delivery through your newsagent.`,
-			<strong>
-				To collect from multiple newsagents, present your card each time –
-				they’ll scan it and be reimbursed automatically.
-			</strong>,
-			<strong>
-				To arrange your own delivery with your local retailer, share the barcode
-				from your Home Delivery Letter with your chosen store.
-			</strong>,
-			<div css={linkStyle}>
-				<a
-					target="_blank"
-					href={
-						'https://digitalvouchers-production-storefinder.azurewebsites.net/map/go'
-					}
-				>
-					Find your nearest participating store or delivery retailer
-				</a>
-			</div>,
 		],
 		imageUrl:
 			'https://i.guim.co.uk/img/media/e68254bdbeab6806c83d1fb29ec61aef2c376cc1/0_0_892_714/892.jpg?width=222&quality=75&s=00bd5126359c43bfd98829507f846747',
+		delivery: [
+			{
+				title: 'Collecting from multiple newsagents',
+				content: (
+					<p>
+						To collect from multiple newsagents, present your card each time –
+						they’ll scan it and be reimbursed automatically.
+					</p>
+				),
+			},
+			{
+				title: 'Delivery from your retailer',
+				content: (
+					<>
+						<p>
+							To arrange your own delivery with your local retailer, share the
+							barcode from your Home Delivery Letter with your chosen store.{' '}
+						</p>
+						<div css={linkStyle}>
+							<a
+								target="_blank"
+								href={
+									'https://digitalvouchers-production-storefinder.azurewebsites.net/map/go'
+								}
+							>
+								Find your nearest participating store or delivery retailer
+							</a>
+						</div>
+						,
+					</>
+				),
+			},
+		],
 	},
 };
 
@@ -143,26 +166,26 @@ export default function NewspaperTabHero({
 	tab: PaperFulfilmentOptions;
 }): JSX.Element {
 	const homeDeliveryTrackingId = 'Paper_HomeDelivery-tab_Delivery-accordion';
-	const { productInfo, imageUrl, deliveryDetails } = heroContent[tab];
+	const { productInfo, imageUrl, delivery: delivery } = heroContent[tab];
 	return (
 		<FlexContainer cssOverrides={flexContainerOverride}>
 			<div css={copyWidthStyle}>
 				{productInfo.map((paragraph) => (
 					<p css={paragraphStyle}>{paragraph}</p>
 				))}
-				{deliveryDetails && (
+				{delivery.map((deliveryItem) => (
 					<Accordion cssOverrides={accordionOverride}>
 						{[
 							<TabAccordionRow
 								trackingId={homeDeliveryTrackingId}
-								label={'Delivery details'}
+								label={deliveryItem.title}
 								cssOverrides={accordionRowOverride}
 							>
-								{deliveryDetails}
+								{deliveryItem.content}
 							</TabAccordionRow>,
 						]}
 					</Accordion>
-				)}
+				))}
 			</div>
 			<img src={imageUrl} />
 		</FlexContainer>
