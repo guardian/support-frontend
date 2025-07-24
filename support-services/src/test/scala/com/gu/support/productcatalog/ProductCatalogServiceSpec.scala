04c8805d1df3d3658db1ce3947fbb659feaae8c7
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-07-24 11:54:20 +0100

    Provide fixture for product catalog in ProductCatalogServiceSpec

--- support-services/src/test/scala/com/gu/support/productcatalog/ProductCatalogServiceSpec.scala
+++ support-services/src/test/scala/com/gu/support/productcatalog/ProductCatalogServiceSpec.scala
@@ -1,21 +1,32 @@
 package com.gu.support.productcatalog
 
 import com.gu.support.config.TouchPointEnvironments.PROD
+import com.gu.support.productcatalog.ProductCatalogServiceSpec.serviceWithFixtures
+import io.circe.parser
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import scala.concurrent.Future
+import scala.io.Source
+
+object ProductCatalogServiceSpec {
+  def serviceWithFixtures: ProductCatalogService = {
+    val json = parser.parse(Source.fromURL(getClass.getResource("/product-catalog-prod.json")).mkString)
+    val jsonProvider = new ProductCatalogJsonProvider {
+      override def get: scala.util.Try[io.circe.Json] = json.toTry
+    }
+    new ProductCatalogService(jsonProvider)
+  }
+}
 
 class ProductCatalogServiceSpec extends AsyncFlatSpec with Matchers {
   "ProductCatalogService" should "retrieve product rate plan ID correctly" in {
-    val service = ProductCatalogService(PROD)
-    service.getProductRatePlanId("SupporterPlus", "Monthly").map { productRatePlanId =>
+    serviceWithFixtures.getProductRatePlanId("SupporterPlus", "Monthly").map { productRatePlanId =>
       productRatePlanId shouldBe "8a128ed885fc6ded018602296ace3eb8"
     }
   }
   it should "retrieve charge ID correctly" in {
     val service = ProductCatalogService(PROD)
-    service.getChargeId("Contribution", "Monthly", "Contribution").map { chargeId =>
+    serviceWithFixtures.getChargeId("Contribution", "Monthly", "Contribution").map { chargeId =>
       chargeId shouldBe "2c92a0fc5aacfadd015ad250bf2c6d38"
     }
   }
