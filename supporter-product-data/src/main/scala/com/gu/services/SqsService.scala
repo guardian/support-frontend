6963154edc4a09584eccedb68b66b4bc064cfbc5
Tom Wey <tom.wey@theguardian.com>
2025-07-28 13:25:51 +0100

    Fix jackson-core vulnerability in supporter-product-data (#7160)
    This PR is part 2 of fixing a vulnerablility reported by dependabot for
    jackson-core. The vulnerability affects projects which use jackson-core <
    2.15.0.
    
    Most of our projects use a later version but not stripe-patrons-data or
    supporter-product-data. The vulnerable version of jackson-core is pulled in via
    the AWS SDK v1 libraries, so I think the strategy should be to migrate to v2
    (v1 is old).
    
    The fix for stripe-patrons data was done in #7113.
    
    This PR migrates the v1 SDK usage in supporter-product-data to v2. The
    following v1 SDKs are used by supporter-product-data:
    
    * SSM
    * S3
    * SQS

--- supporter-product-data/src/main/scala/com/gu/services/SqsService.scala
+++ supporter-product-data/src/main/scala/com/gu/services/SqsService.scala
@@ -1,14 +1,18 @@
 package com.gu.services
 
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
-import com.amazonaws.services.sqs.model.{SendMessageBatchRequest, SendMessageBatchRequestEntry}
 import com.gu.monitoring.SafeLogging
-import com.gu.services.ParameterStoreService.CredentialsProviderDEPRECATEDV1
 import com.gu.supporterdata.model.{ContributionAmount, Stage, SupporterRatePlanItem}
+import com.gu.aws.CredentialsProvider
 import io.circe.Encoder
 import io.circe.generic.semiauto.deriveEncoder
 import io.circe.syntax.EncoderOps
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.sqs.model.{
+  GetQueueUrlRequest,
+  SendMessageBatchRequest,
+  SendMessageBatchRequestEntry,
+}
+import software.amazon.awssdk.services.sqs.SqsClient
 
 import scala.concurrent.ExecutionContext
 import scala.jdk.CollectionConverters._
@@ -18,31 +22,44 @@ class SqsService(queueName: String, alarmService: AlarmService)(implicit val exe
     extends SafeLogging {
   implicit val encoder: Encoder[SupporterRatePlanItem] = deriveEncoder
   implicit val contributionAmountEncoder: Encoder[ContributionAmount] = deriveEncoder
-  private val sqsClient = AmazonSQSAsyncClientBuilder.standard
-    .withCredentials(CredentialsProviderDEPRECATEDV1)
-    .withRegion(Regions.EU_WEST_1)
+  private val sqsClient = SqsClient
+    .builder()
+    .region(Region.EU_WEST_1)
+    .credentialsProvider(CredentialsProvider)
     .build()
 
-  private val queueUrl = sqsClient.getQueueUrl(queueName).getQueueUrl
+  private val getQueueUrlRequest = GetQueueUrlRequest
+    .builder()
+    .queueName(queueName)
+    .build()
+  private val queueUrl = sqsClient.getQueueUrl(getQueueUrlRequest).queueUrl
 
   def sendBatch(supporterRatePlanItems: List[(SupporterRatePlanItem, Int)]) = {
     logger.info(s"Sending message batch with ${supporterRatePlanItems.length} items to SQS queue $queueUrl")
 
     val batchRequestEntries = supporterRatePlanItems.map { case (item, index) =>
-      new SendMessageBatchRequestEntry(index.toString, item.asJson.noSpaces)
+      SendMessageBatchRequestEntry
+        .builder()
+        .id(index.toString)
+        .messageBody(item.asJson.noSpaces)
+        .build()
     }
 
-    val batchRequest = new SendMessageBatchRequest(queueUrl, batchRequestEntries.asJava)
+    val batchRequest = SendMessageBatchRequest
+      .builder()
+      .queueUrl(queueUrl)
+      .entries(batchRequestEntries.asJava)
+      .build()
     sqsClient.sendMessageBatch(batchRequest)
 
     Try(sqsClient.sendMessageBatch(batchRequest)) match {
       case Success(batchResult) =>
-        val failures = batchResult.getFailed.asScala.toList
+        val failures = batchResult.failed.asScala.toList
         if (failures.nonEmpty) {
           failures.foreach { error =>
-            val failedItem = supporterRatePlanItems(error.getId.toInt)
+            val failedItem = supporterRatePlanItems(error.id.toInt)
             logger.error(
-              scrub"Error writing to the queue\nFor item with body: ${failedItem.asJson}\nError message: ${error.getMessage}",
+              scrub"Error writing to the queue\nFor item with body: ${failedItem.asJson}\nError message: ${error.message}",
             )
           }
           alarmService.triggerSQSWriteAlarm
