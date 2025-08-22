9d79a41f1837e28949c652c63bcc45d9f0086375
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-08-22 12:58:21 +0100

    feat: gw gift testing without separate billing period added

--- support-e2e/tests/smoke/guardian-weekly-gifting.test.ts
+++ support-e2e/tests/smoke/guardian-weekly-gifting.test.ts
@@ -1,63 +1,58 @@
 import test from '@playwright/test';
 import { visitLandingPageAndCompleteCheckout } from '../utils/visitLandingPageAndCompleteCheckout';
 
-const giftingTests = [
+const giftTests = [
+	// {
+	// 	product: 'GuardianWeeklyDomestic',
+	// 	ratePlan: '3MonthGift',
+	// 	paymentType: 'Credit/Debit card',
+	// 	internationalisationId: 'UK',
+	// },
 	{
 		product: 'GuardianWeeklyDomestic',
-		ratePlan: 'Monthly',
-		paymentType: 'Credit/Debit card',
-		internationalisationId: 'UK',
-	},
-	{
-		product: 'GuardianWeeklyDomestic',
-		ratePlan: 'Annual',
-		paymentType: 'Direct debit',
-		internationalisationId: 'UK',
-	},
-	{
-		product: 'GuardianWeeklyDomestic',
-		ratePlan: 'Quarterly',
-		paymentType: 'Credit/Debit card',
-		internationalisationId: 'NZ',
-	},
-	{
-		product: 'GuardianWeeklyDomestic',
-		ratePlan: 'Monthly',
+		ratePlan: '3MonthGift',
 		paymentType: 'Credit/Debit card',
 		internationalisationId: 'US',
 	},
 	{
 		product: 'GuardianWeeklyRestOfWorld',
-		ratePlan: 'Annual',
+		ratePlan: 'OneYearGift',
 		paymentType: 'Credit/Debit card',
 		internationalisationId: 'INT',
 	},
-	{
-		product: 'GuardianWeeklyDomestic',
-		ratePlan: 'Quarterly',
-		paymentType: 'Credit/Debit card',
-		internationalisationId: 'AU',
-	},
+	// {
+	// 	product: 'GuardianWeeklyDomestic',
+	// 	ratePlan: 'OneYearGift',
+	// 	paymentType: 'Direct debit',
+	// 	internationalisationId: 'UK',
+	// },
 ];
 
-test.describe('Guardian Weekly Gifting Checkout', () =>
-	giftingTests.map((testDetails) => {
+test.describe('Guardian Weekly Gift Checkout', () =>
+	giftTests.map((testDetails) => {
 		const { ratePlan, product, paymentType, internationalisationId } =
 			testDetails;
-
+		const giftPeriod = ratePlan === '3MonthGift' ? '3 months' : '12 months';
 		test(`${product} - ${ratePlan} - ${paymentType} - ${internationalisationId}`, async ({
 			context,
 			baseURL,
 		}) => {
 			await visitLandingPageAndCompleteCheckout(
-				`/${internationalisationId.toLowerCase()}/subscribe/weekly`,
-				{ context, baseURL, product, paymentType, internationalisationId },
+				`/${internationalisationId.toLowerCase()}/subscribe/weekly/gift#ab-guardianWeeklyGiftGenericCheckout=variant`,
+				{
+					context,
+					baseURL,
+					product,
+					ratePlan,
+					paymentType,
+					internationalisationId,
+				},
 				async (page) => {
 					// Transition from landing page to checkout:
 
 					// Click through to the checkout (we use the aria-label to target the link)
 					await page
-						.getByLabel(`${ratePlan}- Subscribe now`, { exact: true })
+						.getByLabel(`${giftPeriod}- Subscribe now`, { exact: true })
 						.click();
 				},
 			);
