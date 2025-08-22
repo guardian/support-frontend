51c0521f232268eb2f6c1c17e83c83b0965039ec
Tom Wey <tom.wey@theguardian.com>
2025-08-22 17:15:51 +0100

    Container/component split at UTS/Global level

--- support-frontend/assets/pages/[countryGroupId]/student/StudentLandingPageUTSContainer.tsx
+++ support-frontend/assets/pages/[countryGroupId]/student/StudentLandingPageUTSContainer.tsx
@@ -9,11 +9,8 @@ import { ratePlanToBillingPeriod } from 'helpers/productPrice/billingPeriods';
 import { allProductPrices } from 'helpers/productPrice/productPrices';
 import { getPromotion } from 'helpers/productPrice/promotions';
 import { type GeoId } from 'pages/geoIdConfig';
-import StudentHeader from './components/StudentHeader';
-import { universityBadge } from './components/StudentHeaderStyles';
 import { getStudentDiscount } from './helpers/discountDetails';
-import LogoUTS from './logos/uts';
-import { StudentLandingPage } from './StudentLandingPage';
+import { StudentLandingPageUTS } from './StudentLandingPageUTS';
 
 export function StudentLandingPageUTSContainer({
 	landingPageVariant,
@@ -24,11 +21,6 @@ export function StudentLandingPageUTSContainer({
 	const productKey: ActiveProductKey = 'SupporterPlus';
 	const ratePlanKey: ActiveRatePlanKey = 'Monthly';
 
-	/**
-	 * Non-AU Students have ratePlanKey as OneYearStudent
-	 * AU Students have ratePlanKey as Monthly, productKey as SupporterPlus
-	 * and optional promoCode
-	 */
 	const countryId: IsoCountry = Country.detect();
 	const promotionSupporterPlus = getPromotion(
 		allProductPrices.SupporterPlus,
@@ -45,37 +37,12 @@ export function StudentLandingPageUTSContainer({
 	return (
 		<>
 			{studentDiscount && (
-				<StudentLandingPage
+				<StudentLandingPageUTS
 					geoId={geoId}
-					header={
-						<StudentHeader
-							geoId={geoId}
-							productKey={productKey}
-							ratePlanKey={ratePlanKey}
-							landingPageVariant={landingPageVariant}
-							studentDiscount={studentDiscount}
-							headingCopy="Subscribe to fearless, independent and inspiring journalism"
-							subheadingCopy={
-								<>
-									For a limited time, students with a valid UTS email address
-									can unlock the premium experience of Guardian journalism,
-									including unmetered app access
-									{studentDiscount.promoDuration && (
-										<>
-											,{' '}
-											<strong>free for {studentDiscount.promoDuration}</strong>
-										</>
-									)}
-									.
-								</>
-							}
-							universityBadge={
-								<p css={universityBadge}>
-									<LogoUTS /> <span>Special offer for UTS students</span>
-								</p>
-							}
-						/>
-					}
+					landingPageVariant={landingPageVariant}
+					productKey={productKey}
+					ratePlanKey={ratePlanKey}
+					studentDiscount={studentDiscount}
 				/>
 			)}
 		</>
