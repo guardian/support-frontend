3372d356d72604939e432a0314a831a990b8fe60
Tom Wey <tom.wey@theguardian.com>
2025-08-25 10:56:58 +0100

    Fix TypeScript errors in StudentLandingPage tests
    This interface is now much simpler.

--- support-frontend/assets/pages/[countryGroupId]/student/StudentLandingPage.test.tsx
+++ support-frontend/assets/pages/[countryGroupId]/student/StudentLandingPage.test.tsx
@@ -1,38 +1,15 @@
 import { render, screen } from '@testing-library/react';
-import { getLandingPageParticipations } from 'helpers/abTests/landingPageAbTests';
-import {
-	auStudentDiscount,
-	oneYearStudentDiscount,
-} from './components/StudentHeader.test';
 import { StudentLandingPage } from './StudentLandingPage';
 
 describe('StudentLandingPage', () => {
-	const landingPageParticipations = getLandingPageParticipations();
-
 	it('should display country switcher for global student page', () => {
-		render(
-			<StudentLandingPage
-				geoId="uk"
-				productKey="SupporterPlus"
-				ratePlanKey="OneYearStudent"
-				landingPageVariant={landingPageParticipations.variant}
-				studentDiscount={oneYearStudentDiscount}
-			/>,
-		);
+		render(<StudentLandingPage geoId="uk" header={<div>My header</div>} />);
 		const countrySwitcherhButton = screen.getByLabelText('Select a country');
 		expect(countrySwitcherhButton).toBeInTheDocument();
 	});
 
 	it('should not display country switcher for australian student page', () => {
-		render(
-			<StudentLandingPage
-				geoId="au"
-				productKey="SupporterPlus"
-				ratePlanKey="Monthly"
-				landingPageVariant={landingPageParticipations.variant}
-				studentDiscount={auStudentDiscount}
-			/>,
-		);
+		render(<StudentLandingPage geoId="au" header={<div>My header</div>} />);
 		const countrySwitcherhButton = screen.queryByLabelText('Select a country');
 		expect(countrySwitcherhButton).not.toBeInTheDocument();
 	});
