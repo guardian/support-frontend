3acb5af38a7b26cbc407ae1182d48fe3f22fa766
Tom Wey <tom.wey@theguardian.com>
2025-08-25 16:19:10 +0100

    WIP StudentBrandAwareness component

---
+++ support-frontend/stories/landingPage/StudentBrandAwareness.stories.tsx
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+import { StudentBrandAwareness } from 'pages/[countryGroupId]/student/components/StudentBrandAwareness';
+
+export default {
+	title: 'LandingPage/StudentBrandAwareness',
+	component: StudentBrandAwareness,
+	parameters: {
+		docs: {
+			description: {
+				component: `Brand awareness component used by the global student offer landing page.`,
+			},
+		},
+	},
+};
+
+export const Template = () => {
+	const contentContainer = css``;
+
+	return (
+		<div css={contentContainer}>
+			<StudentBrandAwareness />
+		</div>
+	);
+};
