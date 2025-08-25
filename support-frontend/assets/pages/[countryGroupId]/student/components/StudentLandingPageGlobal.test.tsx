c5a6b284e9e81315d4b929571575ccc531601674
Tom Wey <tom.wey@theguardian.com>
2025-08-25 11:26:06 +0100

    Split the tests which were previously in StudentHeader.test.tsx out
    Into tests for the component(s) which are now responsible for the
    functionality under test.
    
    Also remove a bunch of unused mocking code. Now that studentDiscount is
    passed as a prop we don't need this.

---
+++ support-frontend/assets/pages/[countryGroupId]/student/components/StudentLandingPageGlobal.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import { type LandingPageVariant } from 'helpers/globalsAndSwitches/landingPageSettings';
+import type {
+	ActiveProductKey,
+	ActiveRatePlanKey,
+} from 'helpers/productCatalog';
+import type { GeoId } from 'pages/geoIdConfig';
+import { StudentLandingPageGlobal } from '../StudentLandingPageGlobal';
+
+const oneYearStudentDiscount = {
+	amount: 9,
+	periodNoun: 'year',
+	discountPriceWithCurrency: '£9',
+	fullPriceWithCurrency: '£120',
+};
+
+describe('<StudentLandingPageGlobal />', () => {
+	const geoId: GeoId = 'us';
+	const productKey: ActiveProductKey = 'SupporterPlus';
+	const ratePlanKey: ActiveRatePlanKey = 'Monthly';
+	const landingPageVariant = {
+		products: {
+			SupporterPlus: {
+				benefits: ['Benefit 1', 'Benefit 2'],
+			},
+		},
+	} as unknown as LandingPageVariant;
+
+	it("uses 'Subscribe' as CTA label when amount is greater than 0", () => {
+		render(
+			<StudentLandingPageGlobal
+				geoId={geoId}
+				productKey={productKey}
+				ratePlanKey={ratePlanKey}
+				landingPageVariant={landingPageVariant}
+				studentDiscount={oneYearStudentDiscount}
+			/>,
+		);
+		expect(screen.getByTestId('cta-button')).toHaveTextContent('Subscribe');
+	});
+});
