843e6ff06122a61caef4a05728b1998b749162f6
Jorge Costa <jorge.costa@thetrainline.com>
2025-09-10 10:54:40 +0100

    enable paper product flag in CODE

--- support-frontend/assets/pages/paper-subscription-landing/helpers/inPaperProductTest.ts
+++ support-frontend/assets/pages/paper-subscription-landing/helpers/inPaperProductTest.ts
@@ -1,4 +1,7 @@
+import { isCode } from 'helpers/urls/url';
+
 export const inPaperProductTest = () => {
 	const searchParams = new URLSearchParams(window.location.search);
-	return searchParams.get('paperProductTabs') === 'true';
+	const inPaperProductTabs = searchParams.get('paperProductTabs') === 'true';
+	return inPaperProductTabs || isCode();
 };
