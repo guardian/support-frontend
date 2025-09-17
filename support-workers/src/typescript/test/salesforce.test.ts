97e6083087b11057210a947155db1462f8d9c49e
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-09-17 15:28:48 +0100

    add / update tests to verify expected behaviour

--- support-workers/src/typescript/test/salesforce.test.ts
+++ support-workers/src/typescript/test/salesforce.test.ts
@@ -18,7 +18,37 @@ describe('SalesforceService', () => {
 			lastName: 'Doe',
 		});
 		expect('MailingStreet' in newContactWithGift).toBe(false);
+		expect('MailingCity' in newContactWithGift).toBe(false);
+		expect('MailingState' in newContactWithGift).toBe(false);
+		expect('MailingPostalCode' in newContactWithGift).toBe(false);
+		expect('MailingCountry' in newContactWithGift).toBe(false);
 	});
+
+	test('createContactRecordRequest should not include delivery fields for purchases when user delivery address is undefined', () => {
+		const userWithoutDeliveryAddress = {
+			...user,
+			deliveryAddress: undefined,
+		};
+		const newContact = createContactRecordRequest(
+			userWithoutDeliveryAddress,
+			null,
+		);
+		expect('MailingStreet' in newContact).toBe(false);
+		expect('MailingCity' in newContact).toBe(false);
+		expect('MailingState' in newContact).toBe(false);
+		expect('MailingPostalCode' in newContact).toBe(false);
+		expect('MailingCountry' in newContact).toBe(false);
+	});
+
+	test('createContactRecordRequest should include delivery fields for purchases when user delivery address is populated and there is no giftRecipient', () => {
+		const newContact = createContactRecordRequest(user, null);
+		expect(newContact.MailingStreet).toBe(street);
+		expect(newContact.MailingCity).toBe(user.deliveryAddress.city);
+		expect(newContact.MailingState).toBe(user.deliveryAddress.state);
+		expect(newContact.MailingPostalCode).toBe(user.deliveryAddress.postCode);
+		expect(newContact.MailingCountry).toBe(user.deliveryAddress.country);
+	});
+
 	test('it should throw an INSERT_UPDATE_DELETE_NOT_ALLOWED_DURING_MAINTENANCE error when appropriate', () => {
 		const errorString =
 			'Failed Upsert of new Contact: Upsert failed. First exception on row 0; first error: INSERT_UPDATE_DELETE_NOT_ALLOWED_DURING_MAINTENANCE, Updates can’t be made during maintenance. Try again when maintenance is complete: []';
