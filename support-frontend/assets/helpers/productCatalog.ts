14355164f0640674a278269bf825949f5933bad5
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-09-30 10:07:14 +0100

    review: removed unused GW gift tooltip

--- support-frontend/assets/helpers/productCatalog.ts
+++ support-frontend/assets/helpers/productCatalog.ts
@@ -203,14 +203,12 @@ const partnerOffersBenefit = {
 		'Access to special offers (such as free and discounted tickets) from our values-aligned partners, including museums, festivals and cultural institutions.',
 	specificToRegions: ['AUDCountries'],
 };
-const guardianWeeklyTooltip = `Guardian Weekly is a beautifully concise magazine featuring a handpicked selection of in-depth articles, global news, long reads, opinion and more. Delivered to you every week, wherever you are in the world.`;
 const guardianWeeklyBenefit = {
 	copy: 'Guardian Weekly print magazine delivered to your door every week',
-	tooltip: guardianWeeklyTooltip,
+	tooltip: `Guardian Weekly is a beautifully concise magazine featuring a handpicked selection of in-depth articles, global news, long reads, opinion and more. Delivered to you every week, wherever you are in the world.`,
 };
 const guardianWeeklyGiftBenefit = {
 	copy: `Guardian Weekly print magazine delivered to your recipient's door`,
-	tooltip: guardianWeeklyTooltip,
 };
 
 const feastBenefit = {
