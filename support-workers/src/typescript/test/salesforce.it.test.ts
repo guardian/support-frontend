c9c755de9f3c4c66e0f80df46b02d94e77a976b6
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-09-30 16:55:25 +0100

    more commenting out

--- support-workers/src/typescript/test/salesforce.it.test.ts
+++ support-workers/src/typescript/test/salesforce.it.test.ts
@@ -48,72 +48,72 @@ describe('AuthService', () => {
 	});
 });
 
-describe('SalesforceService', () => {
-	let config: SalesforceConfig;
-	let salesforceService: SalesforceService;
+// describe('SalesforceService', () => {
+// 	let config: SalesforceConfig;
+// 	let salesforceService: SalesforceService;
 
-	beforeAll(async () => {
-		config = await getSalesforceConfig('CODE');
-		salesforceService = new SalesforceService(config);
-	});
+// 	beforeAll(async () => {
+// 		config = await getSalesforceConfig('CODE');
+// 		salesforceService = new SalesforceService(config);
+// 	});
 
-	// test('should be able to upsert a customer', async () => {
-	// 	const result = await salesforceService.upsert(customer);
-	// 	expect(result).toEqual({
-	// 		Success: true,
-	// 		ContactRecord: {
-	// 			Id: salesforceId,
-	// 			AccountId: salesforceAccountId,
-	// 		},
-	// 	});
-	// });
+// test('should be able to upsert a customer', async () => {
+// 	const result = await salesforceService.upsert(customer);
+// 	expect(result).toEqual({
+// 		Success: true,
+// 		ContactRecord: {
+// 			Id: salesforceId,
+// 			AccountId: salesforceAccountId,
+// 		},
+// 	});
+// });
 
-	// test('should be able to upsert a customer that has optional fields', async () => {
-	// 	const result = await salesforceService.upsert({
-	// 		...customer,
-	// 		OtherStreet: street,
-	// 		OtherCity: city,
-	// 		OtherPostalCode: postCode,
-	// 		OtherCountry: uk,
-	// 		MailingStreet: street,
-	// 		MailingCity: city,
-	// 		MailingPostalCode: postCode,
-	// 		Phone: telephoneNumber,
-	// 	});
-	// 	expect(result).toEqual({
-	// 		Success: true,
-	// 		ContactRecord: {
-	// 			Id: salesforceId,
-	// 			AccountId: salesforceAccountId,
-	// 		},
-	// 	});
-	// });
+// test('should be able to upsert a customer that has optional fields', async () => {
+// 	const result = await salesforceService.upsert({
+// 		...customer,
+// 		OtherStreet: street,
+// 		OtherCity: city,
+// 		OtherPostalCode: postCode,
+// 		OtherCountry: uk,
+// 		MailingStreet: street,
+// 		MailingCity: city,
+// 		MailingPostalCode: postCode,
+// 		Phone: telephoneNumber,
+// 	});
+// 	expect(result).toEqual({
+// 		Success: true,
+// 		ContactRecord: {
+// 			Id: salesforceId,
+// 			AccountId: salesforceAccountId,
+// 		},
+// 	});
+// });
 
-	// test('it should be able to add a related contact record', async () => {
-	// 	const name = 'integration-test-recipient';
-	// 	const upsertData: DeliveryContactRecordRequest = {
-	// 		AccountId: salesforceAccountId,
-	// 		Email: emailAddress,
-	// 		Salutation: title,
-	// 		FirstName: name,
-	// 		LastName: name,
-	// 		MailingStreet: street,
-	// 		MailingCity: city,
-	// 		MailingState: state,
-	// 		MailingPostalCode: postCode,
-	// 		MailingCountry: uk,
-	// 		RecordTypeId: '01220000000VB50AAG',
-	// 	};
-	// 	const result = await salesforceService.upsert(upsertData);
-	// 	expect(result).toEqual({
-	// 		Success: true,
-	// 		ContactRecord: {
-	// 			Id: expect.stringMatching('[0-9A-Z]+') as string,
-	// 			AccountId: salesforceAccountId,
-	// 		},
-	// 	});
-	// });
-});
+// test('it should be able to add a related contact record', async () => {
+// 	const name = 'integration-test-recipient';
+// 	const upsertData: DeliveryContactRecordRequest = {
+// 		AccountId: salesforceAccountId,
+// 		Email: emailAddress,
+// 		Salutation: title,
+// 		FirstName: name,
+// 		LastName: name,
+// 		MailingStreet: street,
+// 		MailingCity: city,
+// 		MailingState: state,
+// 		MailingPostalCode: postCode,
+// 		MailingCountry: uk,
+// 		RecordTypeId: '01220000000VB50AAG',
+// 	};
+// 	const result = await salesforceService.upsert(upsertData);
+// 	expect(result).toEqual({
+// 		Success: true,
+// 		ContactRecord: {
+// 			Id: expect.stringMatching('[0-9A-Z]+') as string,
+// 			AccountId: salesforceAccountId,
+// 		},
+// 	});
+// });
+// });
 
 describe('CreateSalesforceContatctLambda', () => {
 	test('CreateSalesforceContact lambda should upsert a SalesforceContactRecord', async () => {
