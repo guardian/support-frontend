ad86945bc4ca755e001846d669fb0679d701cf7b
Jorge Costa <jorge.costa@theguardian.com>
2025-10-02 23:40:21 +0100

    clean unecessary css classes

--- support-frontend/assets/components/thankYou/appDownload/AppDownload.tsx
+++ support-frontend/assets/components/thankYou/appDownload/AppDownload.tsx
@@ -11,13 +11,11 @@ import type { CountryGroupId } from '@modules/internationalisation/countryGroup'
 import { AppStoreBadges } from './AppDownloadBadges';
 import { type AppDownload } from './appDownloadItems';
 
-
 const bodyApps = css`
 	display: flex;
 	justify-content: space-between;
 	margin-top: ${space[6]}px;
-`;
-const bodyAppsTop = css`
+
 	${from.tablet} {
 		&:not(:last-child) {
 			border-bottom: 1px solid ${neutral[86]};
@@ -32,15 +30,22 @@ const bodyStyle = css`
 	}
 `;
 
-const bodyCopyStyle = css`
+const descriptionStyle = css`
 	${textEgyptian15};
 	margin-bottom: ${space[1]}px;
 	${from.tablet} {
 		${textEgyptian17};
 	}
+
+	${between.desktop.and.leftCol} {
+		max-width: 260px;
+		display: block;
+	}
 `;
+
 const ctaContainerApps = css`
 	margin-top: ${space[4]}px;
+	padding-bottom: 32px;
 
 	// appDownload ctas require margin-top but appsDownload does not
 	> div {
@@ -55,19 +60,8 @@ const appContainer = css`
 	}
 `;
 
-const ctaTop = css`
-	padding-bottom: 32px;
-`;
-
 const headingCss = css`
 	font-weight: 700;
-`;
-
-const downloadCopy = css`
-	${between.desktop.and.leftCol} {
-		max-width: 260px;
-		display: block;
-	}
 `;
 
 export default function AppDownload({
@@ -84,13 +78,11 @@ export default function AppDownload({
 					{ header, description, appIcon, playStoreUrl, getAppStoreUrl },
 					index,
 				) => (
-					<div css={[bodyApps, bodyAppsTop]} key={index}>
+					<div css={bodyApps} key={index}>
 						<div css={bodyStyle}>
 							<h2 css={headingCss}>{header}</h2>
-							<p css={bodyCopyStyle}>
-								<span css={downloadCopy}>{description}</span>
-							</p>
-							<div css={[ctaContainerApps, ctaTop]}>
+							<p css={descriptionStyle}>{description}</p>
+							<div css={[ctaContainerApps]}>
 								<AppStoreBadges
 									playStoreUrl={playStoreUrl}
 									appStoreUrl={getAppStoreUrl(countryGroupId)}
