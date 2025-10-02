0c692c000d522f84d4bf25d65755a490aa3e58ec
David Pepper <36296660+david-pepper@users.noreply.github.com>
2025-10-02 10:50:34 +0100

    fix import

--- support-workers/src/typescript/test/fixtures/salesforce/integrationTests.ts
+++ support-workers/src/typescript/test/fixtures/salesforce/integrationTests.ts
@@ -1,6 +1,6 @@
 import type { IsoCountry } from '@modules/internationalisation/country';
-import type { PrintContactRecordRequest } from 'src/typescript/services/contactTypes/print';
 import type { Title } from '../../../model/stateSchemas';
+import { PrintContactRecordRequest } from 'src/typescript/services/salesforce';
 
 export const idId = '9999999';
 export const salesforceId = '003UD00000VZnteYAD';
