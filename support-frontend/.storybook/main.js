dc3f807e3d0063e60d5147bc3a70fefedce6133c
Tom Wey <tom.wey@theguardian.com>
2025-10-03 14:16:52 +0100

    Put the node_modules back in the sass-mq/mq import
    The actual build seems to need this, but storybook errors with it in.
    This "fixes" the storybook side with an alias. At some point in future
    we'd like to move entirely over to css-in-js/emotion at which point we'd
    no longer need sass-mq at all.

--- support-frontend/.storybook/main.js
+++ support-frontend/.storybook/main.js
@@ -81,6 +81,13 @@ module.exports = {
 						find: '@modules',
 						replacement: path.resolve(__dirname, '../../modules'),
 					},
+					// This is needed because the build needs the node_modules in the path
+					// when sass-mq/mq is imported but storybook errors with it there, so
+					// fix the storybook side with an alias.
+					{
+						find: 'node_modules/sass-mq/mq',
+						replacement: path.resolve(__dirname, '../node_modules/sass-mq/mq'),
+					},
 				],
 			},
 		});
