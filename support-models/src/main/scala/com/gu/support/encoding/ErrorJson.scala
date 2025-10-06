1279e1931dc6f4af7f960fc5c8c73c80ef05f734
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-10-06 15:01:30 +0100

    Fix handling of transaction failed errors

--- support-models/src/main/scala/com/gu/support/encoding/ErrorJson.scala
+++ support-models/src/main/scala/com/gu/support/encoding/ErrorJson.scala
@@ -1,9 +1,19 @@
 package com.gu.support.encoding
 
 import com.gu.support.encoding.Codec.deriveCodec
+import com.gu.support.encoding.JsonHelpers.JsonObjectExtensions
+import io.circe.generic.semiauto.deriveDecoder
+import io.circe.{ACursor, Decoder, Json}
 
 case class ErrorJson(errorMessage: String, errorType: String, stackTrace: List[String], cause: Option[ErrorJson])
 
 object ErrorJson {
-  implicit val codec: Codec[ErrorJson] = deriveCodec
+  implicit val decoder: Decoder[ErrorJson] = deriveDecoder[ErrorJson].prepare(mapFields)
+
+  private def mapFields(c: ACursor) = c.withFocus {
+    _.mapObject { jsonObject =>
+      jsonObject
+        .renameField("trace", "stackTrace")
+    }
+  }
 }
