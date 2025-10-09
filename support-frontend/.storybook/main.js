a4b991fe3eb6b0da1a14c17a269ba8edce70cb4d
Tom Wey <tom.wey@theguardian.com>
2025-10-09 11:59:08 +0100

    Formatting

--- support-frontend/.storybook/main.js
+++ support-frontend/.storybook/main.js
@@ -3,7 +3,11 @@ const vite = require('vite');
 const preact = require('@preact/preset-vite');
 module.exports = {
 	stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-docs'],
+	addons: [
+		'@storybook/addon-a11y',
+		'@storybook/addon-links',
+		'@storybook/addon-docs',
+	],
 	core: {
 		builder: '@storybook/builder-vite',
 	},
