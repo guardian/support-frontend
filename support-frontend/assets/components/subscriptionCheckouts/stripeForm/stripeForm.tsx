02f3b7f4d8e0f9a6d5305c516e9e915a63a37338
Tom Wey <tom.wey@theguardian.com>
2025-10-10 17:50:03 +0100

    Revert accidental label change

--- support-frontend/assets/components/subscriptionCheckouts/stripeForm/stripeForm.tsx
+++ support-frontend/assets/components/subscriptionCheckouts/stripeForm/stripeForm.tsx
@@ -435,7 +435,7 @@ function StripeForm(props: StripeFormPropTypes): JSX.Element {
 								void fetchPaymentIntent(token);
 							}}
 							onRecaptchaExpired={() => dispatch(expireRecaptchaToken())}
-							label="Security check foo"
+							label="Security check"
 							// TODO: Remove type assertion when we can fix field error types
 							error={recaptchaError?.message as string}
 						/>
