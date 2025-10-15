040eb425d7dca4b7a146c18e1f194c514cf849f1
Jorge Costa <jorge.costa@theguardian.com>
2025-10-15 17:21:57 +0100

    POC for the observer layout

---
+++ support-frontend/assets/pages/[countryGroupId]/components/observer-layout/ObserverFooter.tsx
@@ -0,0 +1,64 @@
+import { css } from '@emotion/react';
+import {
+	from,
+	neutral,
+	space,
+	textSans12,
+	textSans17,
+} from '@guardian/source/foundations';
+import { BackToTop, Container } from '@guardian/source/react-components';
+
+export const footer = css`
+	background-color: #000;
+	color: ${neutral[100]};
+	padding-bottom: ${space[1]}px;
+	${textSans17};
+`;
+
+export const contentWrapperStyles = css`
+	display: flex;
+	position: relative;
+	height: 60px;
+`;
+
+export const copyrightStyles = css`
+	display: block;
+	${textSans12};
+	padding-top: ${space[6]}px;
+	padding-bottom: 18px;
+	${from.tablet} {
+		padding-top: ${space[3]}px;
+	}
+`;
+
+export const backToTopStyles = css`
+	background-color: #000;
+	padding: 0 ${space[2]}px;
+	position: absolute;
+	bottom: -21px;
+	right: ${space[3]}px;
+	${from.tablet} {
+		padding: 0 5px;
+		right: 0;
+	}
+	a {
+		background-color: #000;
+	}
+`;
+
+export default function ObserverFooter() {
+	return (
+		<footer css={footer}>
+			<Container sideBorders>
+				<div css={contentWrapperStyles}>
+					<div css={backToTopStyles}>{BackToTop}</div>
+				</div>
+			</Container>
+			<Container topBorder>
+				<small css={copyrightStyles}>
+					© {new Date().getFullYear()} Tortoise Media
+				</small>
+			</Container>
+		</footer>
+	);
+}
