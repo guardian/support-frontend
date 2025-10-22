965e663804c12c9c199aea2442ecef0c6c44d785
Tom Forbes <tom.forbes@theguardian.com>
2025-10-22 11:35:34 +0100

    Use ticker data from backend

--- support-frontend/assets/pages/supporter-plus-landing/twoStepPages/tickerContainer.tsx
+++ support-frontend/assets/pages/supporter-plus-landing/twoStepPages/tickerContainer.tsx
@@ -1,33 +1,12 @@
 import { css } from '@emotion/react';
 import { space } from '@guardian/source/foundations';
-import type { TickerData } from '@guardian/source-development-kitchen/dist/react-components/ticker/Ticker';
 import { Ticker } from '@guardian/source-development-kitchen/react-components';
-import { useEffect, useState } from 'react';
-import { fetchJson } from '../../../helpers/async/fetch';
 import type { TickerSettings } from '../../../helpers/globalsAndSwitches/landingPageSettings';
-import { isCodeOrProd } from '../../../helpers/urls/url';
 
 const containerStyle = css`
 	max-width: 600px;
 	margin: ${space[6]}px auto;
 `;
-
-function getTickerUrl(tickerId: string) {
-	return isCodeOrProd() ? `/ticker/${tickerId}.json` : '/ticker.json';
-}
-
-async function getInitialTickerValues(tickerId: string): Promise<TickerData> {
-	const data = await fetchJson<{ total: number; goal: number }>(
-		getTickerUrl(tickerId),
-		{},
-	);
-	const total = Math.floor(data.total);
-	const goal = Math.floor(data.goal);
-	return {
-		total,
-		goal,
-	};
-}
 
 interface TickerContainerProps {
 	tickerSettings: TickerSettings;
@@ -36,7 +15,6 @@ interface TickerContainerProps {
 export function TickerContainer({
 	tickerSettings,
 }: TickerContainerProps): JSX.Element {
-	const [tickerData, setTickerData] = useState<TickerData | undefined>();
 	const tickerStylingSettings = {
 		headlineColour: '#FFFFFF',
 		totalColour: '#64B7C4',
@@ -45,11 +23,7 @@ export function TickerContainer({
 		progressBarBackgroundColour: 'rgba(100, 183, 196, 0.3)',
 	};
 
-	useEffect(() => {
-		void getInitialTickerValues(tickerSettings.name).then(setTickerData);
-	}, []);
-
-	if (tickerData) {
+	if (window.guardian.tickerData) {
 		return (
 			<div css={containerStyle}>
 				<Ticker
@@ -58,7 +32,7 @@ export function TickerContainer({
 						headline: tickerSettings.copy.countLabel,
 						goalCopy: tickerSettings.copy.goalCopy,
 					}}
-					tickerData={tickerData}
+					tickerData={window.guardian.tickerData[tickerSettings.name]}
 					tickerStylingSettings={{
 						headlineColour: tickerStylingSettings.headlineColour,
 						totalColour: tickerStylingSettings.totalColour,
