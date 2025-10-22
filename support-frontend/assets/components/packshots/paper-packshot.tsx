c368cf905a78257936d4d8b8dd5b1db4dc353ba5
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-10-22 20:20:59 +0100

    feat: subscriptionProductDescription packshots scss to emotion css

--- support-frontend/assets/components/packshots/paper-packshot.tsx
+++ support-frontend/assets/components/packshots/paper-packshot.tsx
@@ -1,8 +1,39 @@
+import { css } from '@emotion/react';
+import { until } from '@guardian/source/foundations';
 import GridImage from 'components/gridImage/gridImage';
+
+const subscriptions__paper_packshot = css`
+	width: 382px;
+
+	${until.tablet} {
+		width: 100%;
+	}
+
+	img:nth-child(1) {
+		z-index: 2;
+		left: 80px;
+		position: absolute;
+		bottom: 0;
+
+		${until.leftCol} {
+			left: 50px;
+			width: 90%;
+		}
+
+		${until.desktop} {
+			left: 20px;
+		}
+
+		${until.tablet} {
+			width: 100%;
+			position: inherit;
+		}
+	}
+`;
 
 function PaperPackshot() {
 	return (
-		<div className="subscriptions__paper-packshot">
+		<div css={subscriptions__paper_packshot}>
 			<GridImage
 				gridId="newspaperLandingHeroDesktop"
 				srcSizes={[2000, 1000, 500]}
