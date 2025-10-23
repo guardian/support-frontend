66847ea9f275e96bfcd4bb4aa65df65808cd032d
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-10-23 18:15:02 +0100

    review: weeklyPackShot container styling correct

--- support-frontend/assets/components/packshots/weeklyPackshotStyle.tsx
+++ support-frontend/assets/components/packshots/weeklyPackshotStyle.tsx
@@ -2,21 +2,15 @@ import { css } from '@emotion/react';
 import { from } from '@guardian/source/foundations';
 
 export const weeklyPackShotContainer = css`
-	.subscriptions-feature-packshot {
-		width: 140%;
+	width: 100%;
 
-		${from.tablet} {
-			width: 600px;
-			margin-top: 45px;
-		}
+	${from.tablet} {
+		position: absolute;
+		bottom: 0;
+	}
 
-		img {
-			width: 100%;
-
-			${from.desktop} {
-				width: 700px;
-				right: -50px;
-			}
-		}
+	img {
+		width: 100%;
+		vertical-align: bottom;
 	}
 `;
