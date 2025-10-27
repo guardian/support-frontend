ee076b9f1dabc22f1b421778fd61739e30c10b23
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-10-27 16:56:23 +0000

    Enable ProcessSupporterRatePlanItemLambda to deserialise date time strings

--- supporter-product-data/src/main/scala/com/gu/model/dynamo/SupporterRatePlanItemCodecs.scala
+++ supporter-product-data/src/main/scala/com/gu/model/dynamo/SupporterRatePlanItemCodecs.scala
@@ -2,10 +2,13 @@ package com.gu.model.dynamo
 
 import com.gu.model.ZuoraFieldNames._
 import com.gu.supporterdata.model.{ContributionAmount, SupporterRatePlanItem}
-import io.circe.Codec
+import io.circe.{Codec, Decoder}
 import io.circe.generic.semiauto.deriveCodec
 import kantan.csv.HeaderDecoder
 import kantan.csv.java8.defaultLocalDateCellDecoder
+
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDate, OffsetDateTime}
 
 object SupporterRatePlanItemCodecs {
   implicit val decoder: HeaderDecoder[SupporterRatePlanItem] =
@@ -35,6 +38,19 @@ object SupporterRatePlanItemCodecs {
           None,
         ),
     )
+
+  implicit val localDateDecoder: Decoder[LocalDate] = Decoder.decodeString.emap { str =>
+    try {
+      Right(LocalDate.parse(str))
+    } catch {
+      case _: Exception =>
+        try {
+          Right(OffsetDateTime.parse(str).toLocalDate)
+        } catch {
+          case _: Exception => Left(s"Could not parse date: $str")
+        }
+    }
+  }
 
   implicit val codec: Codec[SupporterRatePlanItem] = deriveCodec
   implicit val contributionCodec: Codec[ContributionAmount] = deriveCodec
