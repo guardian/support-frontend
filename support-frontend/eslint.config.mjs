e4eafc97120987a9995f6bfa2ebc973d2db45921
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-10-31 11:19:25 +0000

    Emotion and Knip

--- support-frontend/eslint.config.mjs
+++ support-frontend/eslint.config.mjs
@@ -1,3 +1,4 @@
+import * as emotion from '@emotion/eslint-plugin';
 import guardian from '@guardian/eslint-config';
 
 export default [
@@ -6,6 +7,8 @@ export default [
 	...guardian.configs.react,
 	...guardian.configs.storybook,
 	{
+		files: ['**/*.tsx', '**/*.ts'],
+		plugins: { '@emotion': emotion },
 		rules: {
 			'react-hooks/exhaustive-deps': 'off',
 			'react-hooks/rules-of-hooks': 'off',
@@ -13,6 +16,21 @@ export default [
 			'@typescript-eslint/no-unused-expressions': 'off',
 			'@typescript-eslint/prefer-promise-reject-errors': 'off',
 			'@typescript-eslint/only-throw-error': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					ignoreRestSiblings: true,
+					caughtErrors: 'none',
+				},
+			],
+			'@emotion/pkg-renaming': 'error',
+			'react/function-component-definition': [
+				1,
+				{
+					namedComponents: 'function-declaration',
+					unnamedComponents: 'function-expression',
+				},
+			],
 		},
 		ignores: [
 			'public/**/*',
