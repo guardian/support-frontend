bd7433fd9367194d1c3ad4537378c0cdb42792d9
Jorge Costa <jorge.costa@theguardian.com>
2025-11-05 18:25:32 +0000

    remove unneeded functions

--- support-frontend/assets/components/thankYou/appDownload/appDownloadItems.tsx
+++ support-frontend/assets/components/thankYou/appDownload/appDownloadItems.tsx
@@ -18,6 +18,8 @@ const downloadCopy = css`
 	}
 `;
 
+export const appsDownloadHeader = 'Explore Guardian Apps';
+
 export const appDownloadHeader = 'Download the Guardian app';
 
 export function AppDownloadBodyCopy(): JSX.Element {
@@ -34,28 +36,6 @@ export function AppDownloadEditionsBodyCopy(): JSX.Element {
 	return (
 		<span css={downloadCopy}>
 			Unlock full access to our quality news app today
-		</span>
-	);
-}
-
-export const appsDownloadHeader = 'Explore Guardian Apps';
-
-export const appNewsDownloadHeader = 'The Guardian News app';
-
-export function AppNewsDownloadBodyCopy(): JSX.Element {
-	return (
-		<span css={downloadCopy}>
-			Unlock limitless Guardian journalism in our quality news app today.
-		</span>
-	);
-}
-
-export const appFeastDownloadHeader = 'The Guardian Feast app';
-
-export function AppFeastDownloadBodyCopy(): JSX.Element {
-	return (
-		<span css={downloadCopy}>
-			Make a feast out of anything with the Guardian’s new recipe app : Feast.
 		</span>
 	);
 }
