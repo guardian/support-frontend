73ccc583036829d335410f99fdf358195f4f6f77
Jorge Costa <jorge.costa@theguardian.com>
2025-11-07 12:51:56 +0000

    add 3A's comments to test

--- support-frontend/assets/pages/[countryGroupId]/components/ThankYouNavLinks.test.tsx
+++ support-frontend/assets/pages/[countryGroupId]/components/ThankYouNavLinks.test.tsx
@@ -33,10 +33,12 @@ describe('ThankYouNavLinks', () => {
 	});
 
 	it('renders the Observer button when observerPrint and observerThemeButton are present', () => {
+		// Arrange
 		(useTheme as jest.Mock).mockReturnValue({
 			observerThemeButton,
 		});
 
+		// Act
 		render(
 			<ThankYouNavLinks isObserverPrint={true} isGuardianAdLite={false} />,
 		);
@@ -46,6 +48,8 @@ describe('ThankYouNavLinks', () => {
 			'href',
 			'https://www.observer.co.uk/welcome',
 		);
+
+		// Assert
 		expect(button).toHaveStyle({
 			color: observerThemeButton.textPrimary,
 			backgroundColor: observerThemeButton.backgroundPrimary,
@@ -54,20 +58,25 @@ describe('ThankYouNavLinks', () => {
 	});
 
 	it('renders "Return to the Observer" when observerPrint exists but no observerThemeButton', () => {
+		// Arrange
 		(useTheme as jest.Mock).mockReturnValue({ observerThemeButton: undefined });
 
+		// Act
 		render(
 			<ThankYouNavLinks isObserverPrint={true} isGuardianAdLite={false} />,
 		);
 
+		// Assert
 		expect(
 			screen.getByRole('link', { name: /return to the observer/i }),
 		).toBeInTheDocument();
 	});
 
 	it('renders Guardian link when not ad-lite and no observer theme with the correct colours', () => {
+		// Arrange
 		(useTheme as jest.Mock).mockReturnValue({ observerThemeButton: undefined });
 
+		// Act
 		render(
 			<ThankYouNavLinks isObserverPrint={false} isGuardianAdLite={false} />,
 		);
@@ -75,6 +84,8 @@ describe('ThankYouNavLinks', () => {
 		const button = screen.getByRole('link', {
 			name: /return to the guardian/i,
 		});
+
+		// Assert
 		expect(button).toHaveAttribute('href', 'https://www.theguardian.com');
 
 		expect(button).toHaveStyle({
@@ -84,34 +95,42 @@ describe('ThankYouNavLinks', () => {
 	});
 
 	it('does not render Guardian link in ad-lite mode', () => {
+		// Arrange
 		(useTheme as jest.Mock).mockReturnValue({ observerThemeButton: undefined });
 
+		// Act
 		render(
 			<ThankYouNavLinks isObserverPrint={false} isGuardianAdLite={true} />,
 		);
 
+		// Assert
 		expect(
 			screen.queryByText(/return to the guardian/i),
 		).not.toBeInTheDocument();
 	});
 
 	it('calls correct tracking function when Observer button is clicked', () => {
+		// Arrange
 		(useTheme as jest.Mock).mockReturnValue({ observerThemeButton: {} });
 
+		// Act
 		render(
 			<ThankYouNavLinks isObserverPrint={true} isGuardianAdLite={false} />,
 		);
 
 		fireEvent.click(screen.getByRole('link', { name: /get started/i }));
 
+		// Assert
 		expect(trackComponentClick).toHaveBeenCalledWith(
 			OPHAN_COMPONENT_ID_RETURN_TO_OBSERVER,
 		);
 	});
 
 	it('calls correct tracking function when Guardian button is clicked', () => {
+		// Arrange
 		(useTheme as jest.Mock).mockReturnValue({ observerThemeButton: undefined });
 
+		// Act
 		render(
 			<ThankYouNavLinks isObserverPrint={true} isGuardianAdLite={false} />,
 		);
@@ -120,6 +139,7 @@ describe('ThankYouNavLinks', () => {
 			screen.getByRole('link', { name: /return to the guardian/i }),
 		);
 
+		// Assert
 		expect(trackComponentClick).toHaveBeenCalledWith(
 			OPHAN_COMPONENT_ID_RETURN_TO_GUARDIAN,
 		);
