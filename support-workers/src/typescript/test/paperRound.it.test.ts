4fd08dc34d9b494ef5121640f3fa6b22534d1ee6
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-11-12 18:23:01 +0000

    PaperRoundService

---
+++ support-workers/src/typescript/test/paperRound.it.test.ts
@@ -0,0 +1,14 @@
+/**
+ * @group integration
+ */
+
+import { getPaperRoundConfig, PaperRoundService } from '../services/paperRound';
+
+describe('PaperRoundService Integration Tests', () => {
+	test('Fetch agents from PaperRound API', async () => {
+		const paperRoundConfig = await getPaperRoundConfig('CODE');
+		const paperRoundService = new PaperRoundService(paperRoundConfig);
+		const agents = await paperRoundService.agents();
+		expect(agents.length).toBeGreaterThan(0);
+	}, 30000);
+});
