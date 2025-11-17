474f00fdc6d43899172d878c05dcae782e4a33cd
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-11-17 15:35:42 +0000

    feat: rebased correction, what next copy updated

--- support-frontend/assets/components/thankYou/whatNext/whatNext.tsx
+++ support-frontend/assets/components/thankYou/whatNext/whatNext.tsx
@@ -1,6 +1,5 @@
 import { palette } from '@guardian/source/foundations';
 import type { IsoCurrency } from '@modules/internationalisation/currency';
-import { getCurrencyGlyph } from '@modules/internationalisation/currency';
 import OrderedList from 'components/list/orderedList';
 import type {
 	ActiveProductKey,
@@ -12,6 +11,7 @@ import {
 	isGuardianWeeklyProduct,
 	isPrintProduct,
 } from 'pages/supporter-plus-thank-you/components/thankYouHeader/utils/productMatchers';
+import { glyph } from '../../../helpers/internationalisation/currency';
 import BulletPointedList from '../utilityComponents/BulletPointedList';
 import { helpCenterCta, myAccountCta } from './whatNextCta';
 
@@ -72,32 +72,11 @@ export function WhatNext({
 		);
 	}
 
-	if (observerPrint ?? isGuardianPrint) {
-		const observerItem =
-			'The Observer team will be in touch shortly via email to welcome you. Check your spam folder or add the observer.co.uk domain to your preferred sender list.';
-
-		const subscriptionCardItems = [
-			'You should receive an email confirming the details of your subscription, and another email shortly afterwards that contains details of how you can pick up your newspapers from tomorrow.',
-			'You will receive your Subscription Card in your subscriber pack in the post, along with your home delivery letter.',
-			'Visit your chosen participating newsagent to pick up your newspaper using your Subscription Card, or arrange a home delivery using your delivery letter.',
-		];
-
-		const paperItems = [
-			'Look out for an email from us confirming your subscription. It has everything you need to know about how to manage it in the future.',
-			'Your newspaper will be delivered to your door.',
-		];
-
-		const baseItems = isSubscriptionCard ? subscriptionCardItems : paperItems;
-		const allItems = [...baseItems, observerPrint && observerItem];
-
-		return <OrderedList items={allItems.filter(Boolean)} />;
-	}
-
 	if (isGuardianAdLite) {
 		const guardianAdLiteItems = [
 			'You will receive an email confirming the details of your subscription',
 			startDate
-				? `Your payment of ${getCurrencyGlyph(
+				? `Your payment of ${glyph(
 						currency,
 				  )}${amount}/month will be taken on ${startDate}`
 				: '',
@@ -112,6 +91,48 @@ export function WhatNext({
 				color={palette.neutral[7]}
 			/>
 		);
+	}
+
+	const guardianSubscriptionCardItems = [
+		'You should receive an email confirming the details of your subscription, and another email shortly afterwards that contains details of how you can pick up your newspapers from tomorrow.',
+		'You will receive your Subscription Card in your subscriber pack in the post, along with your home delivery letter.',
+		'Visit your chosen participating newsagent to pick up your newspaper using your Subscription Card, or arrange a home delivery using your delivery letter.',
+	];
+	if (isGuardianPrint) {
+		const guardianHomeDeliveryItems = [
+			'Look out for an email from us confirming your subscription. It has everything you need to know about how to manage it in the future.',
+			'Your newspaper will be delivered to your door.',
+		];
+		const guardianItems = isSubscriptionCard
+			? guardianSubscriptionCardItems
+			: guardianHomeDeliveryItems;
+		return <OrderedList items={guardianItems.filter(Boolean)} />;
+	}
+
+	if (observerPrint) {
+		const observerLink = (
+			<a href="https://www.observer.co.uk">Observer.co.uk</a>
+		);
+		const observerWelcome = (
+			<p>
+				The Observer will be in touch shortly via email to welcome you. Check
+				your spam folder or add the {observerLink} domain to your preferred
+				sender list. Log in to {observerLink} or The Observer app now for full
+				digital access.
+			</p>
+		);
+		const observerSubscriptionCardItems = [
+			...guardianSubscriptionCardItems,
+			observerWelcome,
+		];
+		const observerHomeDeliveryItems = [
+			'Look out for an email from The Guardian confirming your subscription. It has everything you need to know about how to manage your Observer digital & print subscription in the future.',
+			observerWelcome,
+		];
+		const observerItems = isSubscriptionCard
+			? observerSubscriptionCardItems
+			: observerHomeDeliveryItems;
+		return <OrderedList items={observerItems.filter(Boolean)} />;
 	}
 
 	return null;
