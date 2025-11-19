184eeef50d473a34bb4c809da5aa2a5ba2c19425
Jorge Costa <jorge.costa@theguardian.com>
2025-11-19 14:42:36 +0000

    override styles for observer layout change button

--- support-frontend/assets/components/observer-layout/styles.ts
+++ support-frontend/assets/components/observer-layout/styles.ts
@@ -4,6 +4,10 @@ const observerThemeButton: Partial<ThemeButton> = {
 	textPrimary: '#FFFFFF',
 	backgroundPrimary: '#C85000',
 	backgroundPrimaryHover: '#AF4600',
+	textTertiary: '#000',
+	backgroundTertiary: '#FFF',
+	backgroundTertiaryHover: '#E6E6E6',
+	borderTertiary: '#000',
 } as const;
 
 const observerColours = {
