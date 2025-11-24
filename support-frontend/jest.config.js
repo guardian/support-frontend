0b31a0691ca7bc9882d6eac6ac5e7664f0fedbab
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-11-24 15:40:23 +0000

    Upgrade support-service-lambdas & fix paths

---
+++ support-frontend/jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+	testMatch: [
+		'**/__tests__/**/*.[jt]s?(x)',
+		'**/?(*.)+(spec|test).[jt]s?(x)',
+		'!**/__fixtures__/**',
+	],
+	transform: {
+		'.+\\.(js|jsx|ts|tsx)$': './node_modules/babel-jest',
+	},
+	transformIgnorePatterns: [
+		'/node_modules/\\.pnpm/(?!@guardian)',
+		'^.+\\.module\\.(css|sass|scss)$',
+	],
+	moduleFileExtensions: ['js', 'ts', 'json', 'es6', 'jsx', 'tsx'],
+	modulePaths: ['assets'],
+	modulePathIgnorePatterns: ['target', 'assets copy'],
+	moduleNameMapper: {
+		'\\.(css|less|sass|scss)$': '<rootDir>/assets/__mocks__/styleMock.ts',
+		'\\.(svg|png|jpg)$': '<rootDir>/assets/__mocks__/imageMock.ts',
+		'@modules/product-catalog/(.*)$':
+			'@guardian/support-service-lambdas/modules/product-catalog/src/$1',
+		'@modules/internationalisation/(.*)$':
+			'@guardian/support-service-lambdas/modules/internationalisation/src/$1',
+		'@modules/arrayFunctions':
+			'@guardian/support-service-lambdas/modules/arrayFunctions',
+		'@modules/objectFunctions':
+			'@guardian/support-service-lambdas/modules/objectFunctions',
+		'@modules/(.*)$': '<rootDir>/../modules/$1',
+	},
+	setupFilesAfterEnv: ['./jestSetup'],
+	verbose: true,
+	testEnvironment: 'jest-environment-jsdom-global',
+};
