22ca9ef7226fa6a9c35838fc1d444439217d8dd2
Jorge Costa <jorge.costa@theguardian.com>
2025-11-26 16:41:51 +0000

    decomission Highlight error page component in favor of the HighlightText compoment. Simplify ErrorPage props.

--- support-frontend/assets/components/introduction/squaresIntroduction.tsx
+++ support-frontend/assets/components/introduction/squaresIntroduction.tsx
@@ -1,15 +1,16 @@
 // ----- Imports ----- //
 import type { HeadingSize } from 'components/heading/heading';
-import Highlights from 'components/highlights/highlights';
 import SvgSquaresHeroDesktop from 'components/svgs/squaresHeroDesktop';
 import SvgSquaresHeroMobile from 'components/svgs/squaresHeroMobile';
 import SvgSquaresHeroTablet from 'components/svgs/squaresHeroTablet';
 import 'components/heading/heading';
+import HighlightText from 'pages/supporter-plus-thank-you/components/thankYouHeader/HighlightText';
+
 // ----- Types ----- //
 type PropTypes = {
-	highlights?: string[] | null | undefined;
 	headings: string[];
 	highlightsHeadingSize: HeadingSize;
+	errorCode?: string;
 };
 
 // ----- Component ----- //
@@ -20,11 +21,10 @@ function SquaresIntroduction(props: PropTypes) {
 			<SvgSquaresHeroTablet />
 			<SvgSquaresHeroMobile />
 			<div className="component-squares-introduction__content">
-				<Highlights
-					highlights={props.highlights}
-					headingSize={props.highlightsHeadingSize}
-				/>
 				<h1 className="component-squares-introduction__heading">
+					{props.errorCode && (
+						<HighlightText>{`Error ${props.errorCode}`}</HighlightText>
+					)}
 					{props.headings.map((heading) => (
 						<span className="component-squares-introduction__heading-line">
 							{heading}
