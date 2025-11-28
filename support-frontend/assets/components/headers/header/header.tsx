6ef2a4f1660630f860009e59d54a5b69ebac9ac5
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-11-28 20:25:06 +0000

    feat: remove header desktop and logo display and styling

--- support-frontend/assets/components/headers/header/header.tsx
+++ support-frontend/assets/components/headers/header/header.tsx
@@ -2,7 +2,6 @@
 import type { CountryGroupId } from '@modules/internationalisation/countryGroup';
 import { Component } from 'react';
 import SvgGuardianLogo from 'components/svgs/guardianLogo';
-import { Padlock } from 'components/svgs/padlock';
 import { getGlobal } from 'helpers/globalsAndSwitches/globals';
 import { classNameWithModifiers } from 'helpers/utilities/utilities';
 import Links from '../links/links';
@@ -12,7 +11,6 @@ import './header.scss';
 type PropTypes = {
 	utility?: JSX.Element;
 	countryGroupId: CountryGroupId;
-	display?: 'navigation' | 'checkout' | 'guardianLogo' | void;
 };
 type State = {
 	isTestUser: boolean | null | undefined;
@@ -22,24 +20,12 @@ type State = {
 
 type TopNavPropTypes = {
 	utility?: JSX.Element;
-	display: 'navigation' | 'checkout' | 'guardianLogo' | void;
 };
 
-function TopNav({ display, utility }: TopNavPropTypes) {
+function TopNav({ utility }: TopNavPropTypes) {
 	return (
 		<div className="component-header-topnav">
 			<div className="component-header-topnav__utility">{utility}</div>
-			{display === 'checkout' && (
-				<div className="component-header-topnav__checkout">
-					<div />
-					<div className="component-header-topnav--checkout-text">
-						<div className="component-header--padlock">
-							<Padlock />
-						</div>
-						<h1>Checkout</h1>
-					</div>
-				</div>
-			)}
 			<div className="component-header-topnav-logo">
 				<a
 					className="component-header-topnav-logo__graun"
@@ -56,22 +42,19 @@ function TopNav({ display, utility }: TopNavPropTypes) {
 export default class Header extends Component<PropTypes, State> {
 	static defaultProps = {
 		utility: null,
-		display: 'navigation',
 	};
 	state = {
 		isTestUser: getGlobal<boolean>('isTestUser'),
 	};
 
 	componentDidMount(): void {
-		if (this.props.display === 'navigation') {
-			this.setState({
-				isTestUser: getGlobal<boolean>('isTestUser'),
-			});
-		}
+		this.setState({
+			isTestUser: getGlobal<boolean>('isTestUser'),
+		});
 	}
 
 	render(): JSX.Element {
-		const { utility, display, countryGroupId } = this.props;
+		const { utility, countryGroupId } = this.props;
 		const { isTestUser } = this.state;
 
 		return (
@@ -80,8 +63,6 @@ export default class Header extends Component<PropTypes, State> {
 					countryGroupId !== 'GBPCountries'
 						? 'one-row-from-tablet'
 						: 'one-row-from-leftCol',
-					display === 'navigation' ? 'display-navigation' : null,
-					display === 'checkout' ? 'display-checkout' : null,
 				])}
 			>
 				{!!isTestUser && (
@@ -91,32 +72,17 @@ export default class Header extends Component<PropTypes, State> {
 				)}
 				<div className="component-header__wrapper">
 					<div className="component-header__row">
-						<TopNav
-							display={display}
-							utility={display === 'navigation' ? utility : undefined}
+						<TopNav utility={utility} />
+						<MobileMenuToggler
+							links={
+								<Links countryGroupId={countryGroupId} location="mobile" />
+							}
+							utility={utility}
 						/>
-						{display === 'navigation' && (
-							<MobileMenuToggler
-								links={
-									<Links countryGroupId={countryGroupId} location="mobile" />
-								}
-								utility={utility}
-							/>
-						)}
 					</div>
-					{display === 'navigation' && (
-						<div className="component-header__row">
-							<Links countryGroupId={countryGroupId} location="desktop" />
-						</div>
-					)}
-					{display === 'checkout' && (
-						<div className="component-header__row component-header-checkout--row">
-							<div className="component-header--padlock">
-								<Padlock />
-							</div>
-							<div>Checkout</div>
-						</div>
-					)}
+					<div className="component-header__row">
+						<Links countryGroupId={countryGroupId} location="desktop" />
+					</div>
 				</div>
 			</header>
 		);
