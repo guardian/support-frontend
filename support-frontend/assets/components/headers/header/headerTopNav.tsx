e3e2f6da6fb73300dc000804dd87509bee8b3951
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2025-12-01 13:59:52 +0000

    Scss Removal Header: remove unused scss and code (pt1) (#7545)
    * feat: sccs min width calculation correction
    
    * feat : unused display-checkout styling removed
    
    * feat: remove header desktop and logo display and styling
    
    * feat: unused dialog and padlock scss removed
    
    * feat: unused keyframes scss removed
    
    * feat: react function component
    
    * feat: headerTopNav component split out
    
    * feat: pageScaffold containing TestUser banner used, testUser not required and removed
    
    * review: tidy up
    
    * review: header lint bracketing correction

---
+++ support-frontend/assets/components/headers/header/headerTopNav.tsx
@@ -0,0 +1,20 @@
+import SvgGuardianLogo from 'components/svgs/guardianLogo';
+import type { HeaderProps } from './header';
+import './header.scss';
+
+export default function TopNav({ utility }: HeaderProps) {
+	return (
+		<div className="component-header-topnav">
+			<div className="component-header-topnav__utility">{utility}</div>
+			<div className="component-header-topnav-logo">
+				<a
+					className="component-header-topnav-logo__graun"
+					href="https://www.theguardian.com"
+				>
+					<div className="visually-hidden">Return to the Guardian</div>
+					<SvgGuardianLogo />
+				</a>
+			</div>
+		</div>
+	);
+}
