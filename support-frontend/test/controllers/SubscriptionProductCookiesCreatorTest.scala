d7b1259d5c1f01f26e1cc9196edd4a0f71b9fc45
Tom Wey <tom.wey@theguardian.com>
2025-12-02 11:59:53 +0000

    Updates tests to support testing cookies with different expirations

--- support-frontend/test/controllers/SubscriptionProductCookiesCreatorTest.scala
+++ support-frontend/test/controllers/SubscriptionProductCookiesCreatorTest.scala
@@ -12,44 +12,46 @@ class SubscriptionProductCookiesCreatorTest extends AnyFlatSpec with Matchers {
   private val now = DateTime.parse("2025-01-01T00:00:00")
   private val creator = SubscriptionProductCookiesCreator(GuardianDomain("thegulocal.com"))
 
-  private def expectedCookie(name: String) = Cookie(
-    name = name,
-    value = "1736294400000", // 2025-01-08T00:00:00 as epoch timestamp,
-    maxAge = Some(60 * 60 * 24 * 7),
-    secure = true,
-    httpOnly = false,
-    domain = Some("thegulocal.com"),
-  )
+  private def expectedCookie(name: String, ageInDays: Int) = {
+    val ageInSeconds = ageInDays * 24 * 60 * 60
+
+    Cookie(
+      name = name,
+      value = s"${now.getMillis + (ageInSeconds * 1000)}",
+      maxAge = Some(ageInSeconds),
+      secure = true,
+      httpOnly = false,
+      domain = Some("thegulocal.com"),
+    )
+  }
 
   "createCookiesForProduct" should "set the gu_allow_reject_all cookie for GuardianAdLite" in {
     val guardianAdLite = GuardianAdLite(GBP)
     val cookies = creator.createCookiesForProduct(guardianAdLite, now)
 
-    cookies should contain(expectedCookie("gu_allow_reject_all"))
-    cookies should contain(
-      expectedCookie("gu_user_benefits_expiry"),
-    ) // It should also contain the user features expiry cookie
+    cookies should contain(expectedCookie(name = "gu_allow_reject_all", ageInDays = 7))
+    // It should also contain the user features expiry cookie
+    cookies should contain(expectedCookie(name = "gu_user_benefits_expiry", ageInDays = 7))
   }
+
   it should "not set the hide support messaging cookie for Guardian Ad Lite because it is not a supporter product" in {
     val guardianAdLite = GuardianAdLite(GBP)
     val cookies = creator.createCookiesForProduct(guardianAdLite, now)
 
-    cookies should not contain expectedCookie("gu_hide_support_messaging")
-    cookies should contain(
-      expectedCookie("gu_user_benefits_expiry"),
-    ) // It should also contain the user features expiry cookie
+    cookies.map(_.name) should not contain "gu_hide_support_messaging"
+    // It should also contain the user features expiry cookie
+    cookies should contain(expectedCookie(name = "gu_user_benefits_expiry", ageInDays = 7))
   }
 
   it should "set the ad-free cookie, the hide supporter revenue cookie and the allow reject all cookie for SupporterPlus" in {
     val supporterPlus = SupporterPlus(BigDecimal(12), GBP, Monthly)
     val cookies = creator.createCookiesForProduct(supporterPlus, now)
 
-    cookies should contain(expectedCookie("GU_AF1"))
-    cookies should contain(expectedCookie("gu_hide_support_messaging"))
-    cookies should contain(expectedCookie("gu_allow_reject_all"))
-    cookies should contain(
-      expectedCookie("gu_user_benefits_expiry"),
-    ) // It should also contain the user features expiry cookie
+    cookies should contain(expectedCookie(name = "GU_AF1", ageInDays = 7))
+    cookies should contain(expectedCookie(name = "gu_hide_support_messaging", ageInDays = 7))
+    cookies should contain(expectedCookie(name = "gu_allow_reject_all", ageInDays = 7))
+    // It should also contain the user features expiry cookie
+    cookies should contain(expectedCookie(name = "gu_user_benefits_expiry", ageInDays = 7))
   }
 
 }
