c3a9b59ed127bf37a3c57f870a0d43e4acfb79ff
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-12-08 10:26:55 +0000

    Revert "Use Typescript lambda to process Guardian Weekly subscriptions"

--- support-workers/esbuild.config.mjs
+++ support-workers/esbuild.config.mjs
@@ -4,7 +4,6 @@ import tsconfigPaths from 'esbuild-ts-paths';
 build({
 	entryPoints: ['src/typescript/lambdas/*.ts'],
 	bundle: true,
-	sourcemap: true,
 	platform: 'node',
 	target: 'node22',
 	outdir: 'target/typescript',
