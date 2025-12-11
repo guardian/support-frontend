58e1caadfa11043ce89000917d83536fd1a3ec4c
Tom Forbes <tom.forbes@theguardian.com>
2025-12-11 08:32:18 +0000

    Update support-frontend/app/services/mparticle/MParticleError.scala
    Co-authored-by: John Duffell <john.duffell@guardian.co.uk>

--- support-frontend/app/services/mparticle/MParticleError.scala
+++ support-frontend/app/services/mparticle/MParticleError.scala
@@ -2,7 +2,7 @@ package services.mparticle
 
 import io.circe.Decoder
 
-case class MParticleError(message: String) extends Throwable(message)
+case class MParticleError() extends Throwable("MParticleError is just a placeholder class")
 object MParticleError {
   // Decoding fails because we want to fall back on the generic WebServiceClientError
   implicit val mparticleErrorDecoder: Decoder[MParticleError] =
