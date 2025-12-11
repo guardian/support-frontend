e79e48224568c6a3bef11d5c96fb9335aa5dbfe6
Tom Wey <tom.wey@theguardian.com>
2025-12-11 12:54:30 +0000

    Increase audience of paypalCompletePaymentsWithBAID test to 100%
    Increase test participation from 10% to 100%. It's been working well for
    the 10% of users going through the new gateway.

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -84,11 +84,8 @@ export const tests: Tests = {
 		audiences: {
 			ALL: {
 				offset: 0,
-				// X% of audience in this test, all get the variant. Everyone
-				// else gets the control (i.e. PayPal express) This is fine -
-				// we're not comparing the two groups, just rolling out PayPalCP
-				// gradually.
-				size: 0.1, // 10%
+				// Everyone gets PayPalCP
+				size: 1, // 100%
 			},
 		},
 		isActive: true,
