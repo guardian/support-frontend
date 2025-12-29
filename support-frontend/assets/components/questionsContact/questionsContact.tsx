0b998ac8916593ef8ab612c4573017480b2e0a11
Paul Dempsey <paul.dempsey@guardian.co.uk>
2025-12-29 17:29:21 +0000

    feat: refactored component using emotion css

--- support-frontend/assets/components/questionsContact/questionsContact.tsx
+++ support-frontend/assets/components/questionsContact/questionsContact.tsx
@@ -1,6 +1,3 @@
-// ----- Imports ----- //
-import { css } from '@emotion/react';
-import { from, palette, space } from '@guardian/source/foundations';
 import type { CountryGroupId } from '@modules/internationalisation/countryGroup';
 import { GBPCountries } from '@modules/internationalisation/countryGroup';
 import PageSection from 'components/pageSection/pageSection';
@@ -9,48 +6,38 @@ import {
 	ContactPageLink,
 	useDotcomContactPage,
 } from 'helpers/utilities/dotcomContactPage';
-// ---- Types ----- //
-type PropTypes = {
-	countryGroupId: CountryGroupId;
+import {
+	componentQuestionsContactDescription,
+	componentQuestionsContactLink,
+	pageSectionBorderTop,
+} from './questionsContactStyles';
+
+type QuestionsContactProps = {
+	countryGroupId?: CountryGroupId;
 };
 
-const pageSectionBorderTop = css`
-	border-top: 1px solid ${palette.neutral[86]};
-	padding-bottom: ${space[3]}px;
-
-	${from.desktop} {
-		padding-left: ${space[5]}px;
-	}
-`;
-
-// ----- Component ----- //
-function QuestionsContact(props: PropTypes) {
+export default function QuestionsContact({
+	countryGroupId,
+}: QuestionsContactProps): JSX.Element {
 	const contactUs = useDotcomContactPage() ? (
 		<ContactPageLink linkText="contact us" />
 	) : (
 		<a
-			className="component-questions-contact__link"
-			href={contributionsEmail[props.countryGroupId]}
+			css={componentQuestionsContactLink}
+			href={contributionsEmail[countryGroupId ?? GBPCountries]}
 		>
 			contact us
 		</a>
 	);
 	return (
-		<div className="component-questions-contact">
+		<>
 			<PageSection cssOverrides={pageSectionBorderTop} heading="Questions?">
-				<p className="component-questions-contact__description">
+				<p css={componentQuestionsContactDescription}>
 					If you have any questions about contributing to The&nbsp;Guardian,
 					please&nbsp;
 					{contactUs}
 				</p>
 			</PageSection>
-		</div>
+		</>
 	);
 }
-
-// ----- Default Props ----- //
-QuestionsContact.defaultProps = {
-	countryGroupId: GBPCountries,
-}; // ----- Exports ----- //
-
-export default QuestionsContact;
