64abb60b7ca2b77c2480986087124dbe63bf8a31
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-01-06 12:09:28 +0000

    feat: remove scss file and related comments

--- support-frontend/assets/components/content/content.tsx
+++ support-frontend/assets/components/content/content.tsx
@@ -1,4 +1,3 @@
-// ----- Imports ----- //
 import { css } from '@emotion/react';
 import { from, palette } from '@guardian/source/foundations';
 import type { ReactNode } from 'react';
@@ -6,8 +5,6 @@ import LeftMarginSection from 'components/leftMarginSection/leftMarginSection';
 import 'helpers/types/option';
 import { gu_h_spacing, gu_v_spacing } from 'stylesheets/emotion/layout';
 
-// ----- Styles ----- //
-// Replacement for .component-left-margin-section__content rules in content.scss
 const leftBorderStyle = css`
 	max-width: 100%;
 	position: relative;
@@ -17,12 +14,10 @@ const leftBorderStyle = css`
 	}
 `;
 
-// Replacement for .component-content--white block-colours (simplified)
 const contentWrapper = css`
 	background-color: ${palette.neutral[100]};
 `;
 
-// Replacement for .component-content__content padding/max-width
 const innerContent = css`
 	box-sizing: border-box;
 	max-width: 700px; /* approximates gu-span(9) */
@@ -33,7 +28,6 @@ const innerContent = css`
 	}
 `;
 
-// ----- Render ----- //
 function Content({ children }: { children: ReactNode }): JSX.Element {
 	return (
 		<div css={contentWrapper}>
