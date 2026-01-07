1ff98b29fad33be9c9c792b5c719f6c92e9ff640
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-01-07 18:18:01 +0000

    review: line height adjustment3

--- support-frontend/assets/components/text/textStyles.ts
+++ support-frontend/assets/components/text/textStyles.ts
@@ -41,7 +41,7 @@ export const component_text__large = css`
 		font-size: 22px;
 	}
 	margin-top: ${gu_v_spacing}px;
-	line-height: 115%;
+	line-height: 120%;
 `;
 
 export const component_text__sans = css`
