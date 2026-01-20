9673c62142817ebf79f85ba19e319ff1f4830a85
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-01-20 16:35:39 +0000

    review: simplified promotion product and expiry retreival

--- support-frontend/assets/pages/paper-subscription-landing/components/PaperPromotionExpiries.tsx
+++ support-frontend/assets/pages/paper-subscription-landing/components/PaperPromotionExpiries.tsx
@@ -3,31 +3,14 @@ import type { PaperPromotion } from '../helpers/getPromotions';
 import { getTitle } from '../helpers/products';
 import { promotionContainer } from './PaperPromotionExpiriesStyles';
 
-function getPromoProductsAndExpiry(paperPromotion: PaperPromotion): string {
+function getPromotionProductsAndExpiry(paperPromotion: PaperPromotion): string {
 	const products = paperPromotion.activePaperProducts
 		.map((paperProduct) => getTitle(paperProduct))
 		.join(', ');
-	const expiry = getPromoExpiry(paperPromotion);
-	return `${products} ${expiry.charAt(0).toLowerCase() + expiry.slice(1)}`;
-}
-function getPromoExpiry(paperPromotion: PaperPromotion): string {
-	if (!paperPromotion.expires) {
-		return '';
-	}
-	const expiryDate = new Date(paperPromotion.expires);
-	return `Offer ends ${getDateString(expiryDate)}. `;
-}
-
-function getPromotion(
-	paperPromotion: PaperPromotion,
-	index: number,
-	multiPromo: boolean,
-): string {
-	const star = '*'.repeat(index + 1);
-	const promoText = multiPromo
-		? getPromoProductsAndExpiry(paperPromotion)
-		: getPromoExpiry(paperPromotion);
-	return `${star} ${promoText}`;
+	const offerExpiry = paperPromotion.expires
+		? ` offer ends ${getDateString(new Date(paperPromotion.expires))}. `
+		: '';
+	return `${products}${offerExpiry}`;
 }
 
 type PaperPromoExpiriesProps = {
@@ -36,11 +19,12 @@ type PaperPromoExpiriesProps = {
 export default function PaperPromotionExpiries({
 	paperPromotions,
 }: PaperPromoExpiriesProps): JSX.Element {
-	const multiPromo = paperPromotions.length > 1;
 	return (
 		<div css={promotionContainer}>
 			{paperPromotions.map((paperPromotion, index) => (
-				<p>{getPromotion(paperPromotion, index, multiPromo)}</p>
+				<p>{`${'*'.repeat(index + 1)} ${getPromotionProductsAndExpiry(
+					paperPromotion,
+				)}`}</p>
 			))}
 		</div>
 	);
