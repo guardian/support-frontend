6335bd6deae77bb4b6ef7a48fa9b7a0a3e628b58
Tom Wey <tom.wey@theguardian.com>
2026-01-21 11:14:44 +0000

    Inline Stripe error type and remove from redux dir

--- support-frontend/assets/components/stripeCardForm/selectors.ts
+++
@@ -1,6 +0,0 @@
-import type { StripeFormErrors } from 'helpers/redux/checkout/payment/stripe/state';
-
-export type StripeCardFormDisplayErrors = StripeFormErrors & {
-	recaptcha?: string[];
-	zipCode?: string[];
-};
