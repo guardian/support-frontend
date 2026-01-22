99675fd1b87f3081e490ceba5fe3ba3c2172c477
Rupert Bates <Rupert.Bates@guardian.co.uk>
2026-01-22 13:25:58 +0000

    Try to fix git casing issue

--- support-workers/src/typescript/lambdas/FailureHandlerLambda.ts
+++
@@ -1,8 +0,0 @@
-import type { SendAcquisitionEventState } from '../model/sendAcquisitionEventState';
-import type { WrappedState } from '../model/stateSchemas';
-
-export const handler = async (
-	state: WrappedState<SendAcquisitionEventState>,
-) => {
-	console.info(`Input is ${JSON.stringify(state)}`);
-};
