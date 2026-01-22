4a4fb4936a475437ed2c2256516a4ae87c12ec09
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-01-22 09:36:01 +0000

    feat: skeleton file remove

--- support-frontend/.storybook/preview.js
+++ support-frontend/.storybook/preview.js
@@ -1,7 +1,10 @@
 import { withFocusStyleManager } from './decorators/withFocusStyleManager';
 import { withUKPath } from './decorators/withUKPath';
 import { viewports } from './viewports';
-import '../assets/stylesheets/skeleton/skeleton.scss';
+import '../assets/stylesheets/skeleton/html.scss';
+import '../assets/stylesheets/gu-sass/gu-sass.scss';
+import '../assets/stylesheets/skeleton/fonts.scss';
+import '../assets/stylesheets/skeleton/reset-src.scss';
 import MockDate from 'mockdate';
 
 const parameters = {
