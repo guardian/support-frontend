abae2638de0a7b12646fa6b9d1b9a9c0e87187e6
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2026-01-27 09:52:49 +0000

    Scss remove package.json and project dependencies (#7731)
    * feat: skeleton file remove only
    
    * feat: ContributionOrderSummary story decorator global applied
    
    * feat: storybook global styles applied
    
    * feat: reset default font applied from html
    
    * feat: antialiasing and grayscale applied to html/dialog fonts
    
    * feat: remove html.scss
    
    * feat: font.scss and reset-src.scss files removed
    
    * feat: controller missing style bundles set to None (css not supolied as its in the js bundle)
    
    * feat: sass and scss dependency removals
    
    * fix: pnpm lock file correction
    
    * Update support-frontend/jest.config.js
    
    Co-authored-by: Tom Wey <tom.wey@theguardian.com>
    
    ---------
    
    Co-authored-by: Tom Wey <tom.wey@theguardian.com>

--- support-frontend/knip.config.js
+++ support-frontend/knip.config.js
@@ -7,7 +7,7 @@ const flattenedEntryPoints = [...Object.keys(entryPoints)].map(
 module.exports = {
 	// ! signals that this is production code https://knip.dev/features/production-mode
 	entry: [...flattenedEntryPoints, 'scripts/build-ssr-content.tsx!'],
-	project: ['**/*.{js,jsx,ts,tsx,scss}!'],
+	project: ['**/*.{js,jsx,ts,tsx}!'],
 	ignoreExportsUsedInFile: false,
 	ignore: ['**/knip.*.js'],
 	ignoreDependencies: [
