55a262865d3ac1760f6c4e65cad2b4b61f68c695
Tom Wey <tom.wey@theguardian.com>
2026-01-27 12:24:52 +0000

    Add 0% A/B test for the PayPal API and SDK migration
    We will want to A/B test the new experience vs the old. But currently
    this is set to 0% which will allow us to manually opt in and test
    changes in prod before exposing to any real users.

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -117,4 +117,25 @@ export const tests: Tests = {
 		targetPage: pageUrlRegexes.contributions.allLandingPagesAndThankyouPages,
 		excludeContributionsOnlyCountries: true,
 	},
+	paypalMigrationRecurring: {
+		variants: [
+			{
+				id: 'control',
+			},
+			{
+				id: 'variant',
+			},
+		],
+		audiences: {
+			ALL: {
+				offset: 0,
+				size: 0,
+			},
+		},
+		isActive: true,
+		referrerControlled: false, // ab-test name not needed to be in paramURL
+		seed: 3,
+		targetPage: pageUrlRegexes.contributions.genericCheckoutOnly,
+		excludeContributionsOnlyCountries: true,
+	},
 };
