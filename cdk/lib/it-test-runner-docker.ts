61bc58e0a9d93d18b0a0dfbbd8ae18cab072dee4
Rupert Bates <Rupert.Bates@guardian.co.uk>
2026-01-29 11:08:16 +0000

    Build file

--- cdk/lib/it-test-runner-docker.ts
+++ cdk/lib/it-test-runner-docker.ts
@@ -1,18 +1,28 @@
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import { type App, Duration, Size } from "aws-cdk-lib";
+import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class ItTestRunnerDocker extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
-    new lambda.DockerImageFunction(this, "PnpmTestRunner", {
-      code: lambda.DockerImageCode.fromImageAsset(
-        "../support-lambdas/it-test-runner-docker"
-      ),
-      // Increase memory for faster CPU/Install speeds
+
+    // This tag comes from the environment variable set in GitHub Actions
+    const imageTag = process.env.IT_TEST_RUNNER_IMAGE_TAG ?? "latest";
+
+    // Reference the repository you pushed to
+    const repository = ecr.Repository.fromRepositoryName(
+      this,
+      "Repo",
+      "support-workers-it-test-runner"
+    );
+
+    new lambda.DockerImageFunction(this, "ItTestRunner", {
+      code: lambda.DockerImageCode.fromEcr(repository, {
+        tagOrDigest: imageTag,
+      }),
       memorySize: 4096,
-      // Configure up to 10GB of /tmp space
       ephemeralStorageSize: Size.gibibytes(10),
       timeout: Duration.minutes(15),
       environment: {
