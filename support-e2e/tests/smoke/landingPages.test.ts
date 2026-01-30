ae51e326f1db1d69dd2fed49b65cf3e283259d38
jorgeCosta <jorge.costa@theguardian.com>
2026-01-30 12:39:34 +0000

    make contribution e2e test region agnostic. remove redundant tests (#7756)

--- support-e2e/tests/smoke/landingPages.test.ts
+++ support-e2e/tests/smoke/landingPages.test.ts
@@ -5,63 +5,6 @@ import { firstName } from '../utils/users';
 
 afterEachTasks(test);
 
-test.describe('Paper product page', () => {
-	test('Basic loading-when a user goes to the Newspapar Subscriptions page,it should display the page', async ({
-		baseURL,
-		context,
-	}) => {
-		const page = await context.newPage();
-		const baseUrlWithFallback = baseURL ?? 'https://support.theguardian.com';
-		const pageUrl = `${baseUrlWithFallback}/uk/subscribe/paper`;
-		const domain = new URL(pageUrl).hostname;
-		await setTestCookies(context, firstName(), domain);
-		await page.goto(pageUrl);
-
-		await expect(page.locator('id=qa-paper-subscriptions')).toBeVisible();
-		await expect(
-			page.getByRole('heading', { name: 'Newspaper subscription' }).first(),
-		).toBeVisible();
-	});
-});
-
-test.describe('Weekly product page', () => {
-	test('Basic loading-when a user goes to the Guardian Weekly page,it should display the page', async ({
-		baseURL,
-		context,
-	}) => {
-		const page = await context.newPage();
-		const baseUrlWithFallback = baseURL ?? 'https://support.theguardian.com';
-		const pageUrl = `${baseUrlWithFallback}/uk/subscribe/weekly`;
-		const domain = new URL(pageUrl).hostname;
-		await setTestCookies(context, firstName(), domain);
-		await page.goto(pageUrl);
-
-		await expect(page.locator('id=qa-guardian-weekly')).toBeVisible();
-		await expect(
-			page.getByRole('heading', { name: 'The Guardian Weekly' }).first(),
-		).toBeVisible();
-	});
-});
-
-test.describe('Weekly gift product page', () => {
-	test('Basic loading-when a user goes to the Guardian Weekly gift page,it should display the page', async ({
-		baseURL,
-		context,
-	}) => {
-		const page = await context.newPage();
-		const baseUrlWithFallback = baseURL ?? 'https://support.theguardian.com';
-		const pageUrl = `${baseUrlWithFallback}/uk/subscribe/weekly/gift`;
-		const domain = new URL(pageUrl).hostname;
-		await setTestCookies(context, firstName(), domain);
-		await page.goto(pageUrl);
-
-		await expect(page.locator('id=qa-guardian-weekly-gift')).toBeVisible();
-		await expect(
-			page.getByRole('heading', { name: 'Give the Guardian Weekly' }).first(),
-		).toBeVisible();
-	});
-});
-
 test.describe('Subscriptions landing page', () => {
 	test('Basic loading-when a user goes to the Subscriptions landing page,it should display the page', async ({
 		baseURL,
@@ -100,7 +43,7 @@ test.describe('Contributions landing page', () => {
 		await page.goto(pageUrl);
 
 		await expect(
-			page.locator('id=supporter-plus-landing-page-uk'),
+			page.locator('[id^="supporter-plus-landing-page-"]'),
 		).toBeVisible();
 		// assert that there are three sections under the tabpanel (Support, Supporter Plus, Digital Plus)
 		const tabpanel = page.getByRole('tabpanel');
