7ad7ab3c8f2089def4ba3e4989920b37748622f3
Rupert Bates <Rupert.Bates@guardian.co.uk>
2026-02-03 12:14:28 +0000

    Remove support-workers Scala code

--- support-workers/src/main/scala/com/gu/support/workers/exceptions/SalesforceException.scala
+++
@@ -1,3 +0,0 @@
-package com.gu.support.workers.exceptions
-
-class SalesforceException(message: String = "", cause: Throwable = None.orNull) extends Throwable(message, cause)
--- support-workers/src/test/scala/com/gu/test/tags/objects/IntegrationTest.scala
+++
@@ -1,5 +0,0 @@
-package com.gu.test.tags.objects
-
-import org.scalatest.Tag
-
-case object IntegrationTest extends Tag("com.gu.test.tags.annotations.IntegrationTest")
