9c782a83d55722e3c704bc2f6f93cf5d60d61f5b
jorgeCosta <jorge.costa@theguardian.com>
2026-02-09 10:26:43 +0000

    Redesigned card for guardian Weekly + rate plans (#7767)
    * add new component for guardin weekly rate plan cards. storybook included
    
    * make new fields optional
    
    * move styles to own module
    
    * remove optional product properties
    
    * add recomended label
    
    * align billing period to rest of data

---
+++ support-frontend/assets/pages/weekly-subscription-landing/components/WeeklyRatePlanCard.tsx
@@ -0,0 +1,83 @@
+import {
+	LinkButton,
+	themeButtonReaderRevenue,
+} from '@guardian/source/react-components';
+import { useEffect } from 'react';
+import type { Product } from 'components/product/productOption';
+import { useHasBeenSeen } from 'helpers/customHooks/useHasBeenSeen';
+import {
+	ButtonCTA,
+	card,
+	cardHeading,
+	cardLabel,
+	cardOffer,
+	cardPrice,
+	cardWithLabel,
+	savingsTextStyle,
+	strikethroughPriceStyle,
+} from './WeeklyRatePlanCardStyles';
+
+function WeeklyRatePlanCard(product: Product) {
+	const {
+		title,
+		price,
+		discountedPrice,
+		billingPeriodNoun,
+		priceCopy,
+		offerCopy,
+		showLabel,
+		href,
+		onClick,
+		onView,
+		buttonCopy,
+	} = product;
+	const [hasBeenSeen, setElementToObserve] = useHasBeenSeen({
+		threshold: 0.5,
+		debounce: true,
+	});
+
+	/**
+	 * The first time this runs hasBeenSeen
+	 * is false, it's default value. It will run
+	 * once more if the element under observation
+	 * has scrolled into view, then hasBeenSeen should be
+	 * true.
+	 * */
+	useEffect(() => {
+		hasBeenSeen && onView();
+	}, [hasBeenSeen]);
+
+	return (
+		<div ref={setElementToObserve} css={[card, showLabel && cardWithLabel]}>
+			{showLabel && <div css={cardLabel}>Best deal</div>}
+			<section>
+				<h3 css={cardHeading}>{title}</h3>
+				<p css={cardPrice}>
+					{discountedPrice ? (
+						<>
+							<span css={strikethroughPriceStyle}>{price}</span>
+							{discountedPrice}
+						</>
+					) : (
+						price
+					)}
+					<small>/{billingPeriodNoun}</small>
+				</p>
+				<p css={savingsTextStyle}>{offerCopy}</p>
+			</section>
+			<div css={ButtonCTA}>
+				<LinkButton
+					href={href}
+					onClick={onClick}
+					aria-label={`${title}- ${buttonCopy}`}
+					theme={themeButtonReaderRevenue}
+				>
+					{buttonCopy}
+				</LinkButton>
+			</div>
+			{priceCopy && <p css={cardOffer}>{priceCopy}</p>}
+		</div>
+	);
+}
+
+export default WeeklyRatePlanCard;
