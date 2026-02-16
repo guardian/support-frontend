935c2ffe9c4ea48e84ae4e30fa306acadf4f4ecc
Jorge Costa <jorge.costa@theguardian.com>
2026-02-16 14:38:47 +0000

    remove console log

--- support-frontend/assets/components/product/productOption.tsx
+++ support-frontend/assets/components/product/productOption.tsx
@@ -60,7 +60,6 @@ export type Product = {
 };
 
 function ProductOption(props: Product): JSX.Element {
-	console.log('🚀 ~ ProductOption ~ props:', props);
 	const [hasBeenSeen, setElementToObserve] = useHasBeenSeen({
 		threshold: 0.5,
 		debounce: true,
