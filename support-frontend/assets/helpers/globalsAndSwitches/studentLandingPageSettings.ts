2eb91d91b65a6ab79068440cb6df516eb0914be4
sookburt <sue.burt@guardian.co.uk>
2026-02-16 17:06:42 +0000

    prettier fix

--- support-frontend/assets/helpers/globalsAndSwitches/studentLandingPageSettings.ts
+++ support-frontend/assets/helpers/globalsAndSwitches/studentLandingPageSettings.ts
@@ -1,28 +1,27 @@
-
 interface Image {
-    desktopUrl: string;
-    tabletUrl: string;
-    mobileUrl: string;
-    altText: string;
+	desktopUrl: string;
+	tabletUrl: string;
+	mobileUrl: string;
+	altText: string;
 }
 
 interface Institution {
-    name: string;
-    acronym: string;
-    logoUrl: string;
+	name: string;
+	acronym: string;
+	logoUrl: string;
 }
 
 interface StudentLandingPageVariant {
-    name: string;
-    heading: string;
-    subheading: string;
-    image: Image;
-    institution: Institution;
-    promoCode: string[]; // ?? USE THIS TO ENSURE NO MIXING OF PROMOCODES?
+	name: string;
+	heading: string;
+	subheading: string;
+	image: Image;
+	institution: Institution;
+	promoCode: string[]; // ?? USE THIS TO ENSURE NO MIXING OF PROMOCODES?
 }
 export interface StudentLandingPageTest {
 	name: string;
 	status: 'Live' | 'Draft';
 	regionId: 'AUDCountries' | 'NZDCountries'; // TODO: is this correct - should we hard code?
-    variants: StudentLandingPageVariant[];
+	variants: StudentLandingPageVariant[];
 }
