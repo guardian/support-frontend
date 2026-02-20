dddbf0e11eab00ab10bed70ec4bfa6fda6896b1c
sookburt <sue.burt@guardian.co.uk>
2026-02-20 11:54:18 +0000

    updates regEx to ignore student beans, adds fallback for NZ, filters participation tests

--- support-frontend/assets/helpers/abTests/studentLandingPageAbTests.ts
+++ support-frontend/assets/helpers/abTests/studentLandingPageAbTests.ts
@@ -1,4 +1,7 @@
-import type { StudentLandingPageVariant } from 'helpers/globalsAndSwitches/studentLandingPageSettings';
+import type {
+	StudentLandingPageTest,
+	StudentLandingPageVariant,
+} from 'helpers/globalsAndSwitches/studentLandingPageSettings';
 import { getSettings } from '../globalsAndSwitches/globals';
 import type { PageParticipationsConfig } from './models';
 import { STUDENT_LANDING_PAGE_PARTICIPATIONS_KEY } from './sessionStorage';
@@ -9,20 +12,39 @@ const fallBackStudentLandingPageSelection: Record<
 > = {
 	AUDCountries: {
 		name: 'CONTROL',
-		heading: 'Subscribe to fearless and independent journalism - backup', // TODO:
-		subheading: 'Support us with the amount of your choice.', // TODO:
+		heading: 'Subscribe to fearless and independent journalism ...backup', // TODO:
+		subheading:
+			'For a limited time, students with a valid UTS email address can unlock the premium experience of Guardian journalism, including unmetered app access, ...backup.', // TODO:
 		institution: {
 			name: 'University of Technology Sidney',
 			acronym: 'UTS',
 			logoUrl: '', // TODO:
 		},
 		image: {
-			mobileUrl: '', // TODO:
+			mobileUrl: '', // TODO: this doesn't work as expected so may need to be adapted
 			tabletUrl: '', // TODO:
 			desktopUrl: '', // TODO:
 			altText: '', // TODO:
 		},
 		promoCode: ['UTS_STUDENT'],
+	},
+	NZDCountries: {
+		name: 'CONTROL',
+		heading: 'Subscribe to fearless and independent journalism ...backup', // TODO:
+		subheading:
+			'For a limited time, students with a valid UTS email address can unlock the premium experience of Guardian journalism, including unmetered app access, ...backup.', // TODO:
+		institution: {
+			name: 'University of Technology Sidney',
+			acronym: 'UTS',
+			logoUrl: '', // TODO:
+		},
+		image: {
+			mobileUrl: '', // TODO: this doesn't work as expected so may need to be adapted
+			tabletUrl: '', // TODO:
+			desktopUrl: '', // TODO:
+			altText: '', // TODO:
+		},
+		promoCode: ['NZ_STUDENT'],
 	},
 };
 
@@ -34,23 +56,44 @@ const studentLandingPageTestConfig: Omit<
 	PageParticipationsConfig<StudentLandingPageVariant>,
 	'tests'
 > = {
-	pageRegex: '^/.*/student(/.*)?$', // TODO: fix - will this pick up student beans too?
-	forceParamName: 'force-student-landing-page', // TODO: appropriate?
+	pageRegex: '^/[a-z]{2}/student/[^/]+$',
+	forceParamName: 'force-student-lp',
 	sessionStorageKey: STUDENT_LANDING_PAGE_PARTICIPATIONS_KEY,
 	fallbackVariant: (countryGroupId) =>
 		fallBackStudentLandingPageSelection[
 			countryGroupId
 		] as StudentLandingPageVariant,
-	fallbackParticipationKey: 'FALLBACK_STUDENT_LANDING_PAGE', // TODO: is this correct for picking up benefits to pass through to checkout?
+	fallbackParticipationKey: 'FALLBACK_STUDENT_LANDING_PAGE',
 	getVariantName: (variant) => variant.name,
+};
+
+const filterTestsByURL = (
+	tests: StudentLandingPageTest[],
+): StudentLandingPageTest[] => {
+	if (tests.length === 0) {
+		return tests;
+	}
+	const path: string = window.location.pathname;
+
+	const urlCountryGroupId = path.split('/')[1]?.trim(); // fingers-crossed the URL doesn't change!
+	const urlInstitution = path.split('/')[3]?.trim();
+
+	return tests.filter((element: StudentLandingPageTest) => {
+		const region = element.regionId.substring(0, 2).toLowerCase().trim();
+		const institution = element.variants[0]?.institution.acronym.trim();
+		return region === urlCountryGroupId && institution === urlInstitution;
+	});
 };
 
 /**
  * Helper to get student landing page test config with tests from settings
  */
 export function getStudentLandingPageTestConfig(): PageParticipationsConfig<StudentLandingPageVariant> {
+	const filtered = filterTestsByURL(
+		getSettings().studentLandingPageTests ?? [],
+	);
 	return {
 		...studentLandingPageTestConfig,
-		tests: getSettings().studentLandingPageTests ?? [],
+		tests: filtered,
 	};
 }
