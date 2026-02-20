4760ed413f2574a2ca63853f1dc476bb7b39beb7
Tom Wey <tom.wey@theguardian.com>
2026-02-20 16:53:36 +0000

    Show an error message if the backend ppcp requests error

--- support-frontend/assets/pages/[countryGroupId]/components/checkoutForm.tsx
+++ support-frontend/assets/pages/[countryGroupId]/components/checkoutForm.tsx
@@ -1164,6 +1164,8 @@ export default function CheckoutForm({
 								billingPeriod={billingPeriod}
 								csrf={csrf.token ?? ''}
 								paypalClientId={paypalClientId}
+								setErrorMessage={setErrorMessage}
+								setErrorContext={setErrorContext}
 							/>
 						</div>
 						{errorMessage && (
