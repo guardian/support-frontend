ff7c3996f858f0c21278f7e2aac16dd544e86e49
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-03-03 13:01:14 +0000

    fix: ratio name correction, Subscription LP diffs removed

--- support-frontend/assets/components/packshots/weeklyPackshot.tsx
+++ support-frontend/assets/components/packshots/weeklyPackshot.tsx
@@ -10,45 +10,44 @@ function WeeklyPackShot({
 }: {
 	enableDisplayWeeklyDigital?: boolean;
 }): JSX.Element {
+	const weeklyContainer = enableDisplayWeeklyDigital
+		? weeklyDigitalContainer
+		: weeklyPackShotContainer;
 	return (
-		<>
+		<div css={weeklyContainer}>
 			{enableDisplayWeeklyDigital ? (
-				<div css={weeklyDigitalContainer}>
-					<GridImage
-						gridId={'subscriptionGuardianWeeklyDigitalHero_4x3'}
-						srcSizes={[453]}
-						sizes="453px"
-						imgType="png"
-						altText="Illustration of The Guardian Weekly Subscription"
-					/>
-				</div>
+				<GridImage
+					gridId={'subscriptionGuardianWeeklyDigitalHero_5x3'}
+					srcSizes={[453]}
+					sizes="453px"
+					imgType="png"
+					altText="Illustration of The Guardian Weekly Subscription"
+				/>
 			) : (
-				<div css={weeklyPackShotContainer}>
-					<GridPicture
-						sources={[
-							{
-								gridId: 'subscriptionGuardianWeeklyHeroMobile',
-								srcSizes: [1000, 500],
-								sizes: '100vw',
-								imgType: 'png',
-								media: '(max-width: 739px)',
-							},
-							{
-								gridId: 'subscriptionGuardianWeeklyHeroTablet',
-								srcSizes: [1000, 500],
-								sizes: '100vw',
-								imgType: 'png',
-								media: '(max-width: 979px)',
-							},
-						]}
-						fallback={'subscriptionGuardianWeeklyHeroPackShot'}
-						fallbackSize={550}
-						altText=""
-						fallbackImgType="png"
-					/>
-				</div>
+				<GridPicture
+					sources={[
+						{
+							gridId: 'subscriptionGuardianWeeklyHeroMobile',
+							srcSizes: [1000, 500],
+							sizes: '100vw',
+							imgType: 'png',
+							media: '(max-width: 739px)',
+						},
+						{
+							gridId: 'subscriptionGuardianWeeklyHeroTablet',
+							srcSizes: [1000, 500],
+							sizes: '100vw',
+							imgType: 'png',
+							media: '(max-width: 979px)',
+						},
+					]}
+					fallback={'subscriptionGuardianWeeklyHeroPackShot'}
+					fallbackSize={550}
+					altText=""
+					fallbackImgType="png"
+				/>
 			)}
-		</>
+		</div>
 	);
 }
 
