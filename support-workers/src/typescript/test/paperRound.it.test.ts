4bc6ed280d63e9827e1a02f6f8cae1bcab1256cb
Tom Wey <tom.wey@theguardian.com>
2026-03-09 09:59:08 +0000

    Run PaperRound integration test against PROD env
    Testing against the CODE service is failing quite regularly. It seems
    that the CODE service often doesn't respond quickly enough for the test
    to complete without timing out.

--- support-workers/src/typescript/test/paperRound.it.test.ts
+++ support-workers/src/typescript/test/paperRound.it.test.ts
@@ -6,7 +6,9 @@ import { getPaperRoundConfig, PaperRoundService } from '../services/paperRound';
 
 describe('PaperRoundService Integration Tests', () => {
 	test('Fetch agents from PaperRound API', async () => {
-		const paperRoundConfig = await getPaperRoundConfig('CODE');
+		// We test against the PROD service here as the CODE service often fails to
+		// respond in time for the test to complete successfully.
+		const paperRoundConfig = await getPaperRoundConfig('PROD');
 		const paperRoundService = new PaperRoundService(paperRoundConfig);
 		const agents = await paperRoundService.agents();
 		expect(agents.length).toBeGreaterThan(0);
