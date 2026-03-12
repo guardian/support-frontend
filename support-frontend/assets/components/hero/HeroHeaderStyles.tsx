c1a3c29a02b211a8c20024d37c98cc4234acfb01
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-03-12 09:28:44 +0000

    feat: hero top padding removed

--- support-frontend/assets/components/hero/HeroHeaderStyles.tsx
+++ support-frontend/assets/components/hero/HeroHeaderStyles.tsx
@@ -54,7 +54,7 @@ export const weeklyDigitalContainerCssOverrides = css`
 `;
 
 export const heroCopy = css`
-	padding: ${space[1]}px ${space[5]}px ${space[10]}px;
+	padding: ${space[3]}px ${space[5]}px ${space[10]}px;
 `;
 
 export const heroTitle = css`
