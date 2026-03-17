1b47817d127df1a5a985350019271f7aac90efdb
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-03-17 16:25:56 +0000

    feat: thankyou signup component cleanup

--- support-frontend/assets/components/thankYou/signUp/signUpItems.tsx
+++ support-frontend/assets/components/thankYou/signUp/signUpItems.tsx
@@ -4,51 +4,37 @@ import BulletPointedList from 'components/thankYou/utilityComponents/BulletPoint
 
 export const signUpHeader = 'Complete your Guardian account';
 
+const signUpSpacing = css`
+	margin-top: ${space[4]}px;
+`;
+
 export function SignUpBodyCopy({
-	isTierThree,
 	isGuardianPrint,
 }: {
-	isTierThree?: boolean;
 	isGuardianPrint?: boolean;
 }): JSX.Element {
-	const upperCopy = () => {
+	const topSignUpCopy = () => {
 		if (isGuardianPrint) {
 			return 'To finish creating your account, please check your inbox for an email from us. This step will complete your account setup and will allow you to manage your subscription.';
 		}
-		if (isTierThree) {
-			return 'To finish creating your account, please check your inbox for an email from us. This step will complete your account setup.';
-		}
 		return 'Please validate your email address today so we can recognise you as a valued supporter when you visit our website or app. This means we will:';
 	};
-	const lowerCopy =
-		'Make sure you sign in on each of the devices you use to read our journalism – either today or next time you use them.';
-	const lowerCopyTier3 =
-		'Make sure you sign in on all your devices when browsing our website and app. This helps us recognise you as a valued subscriber so you can enjoy all the benefits included in your subscription.';
 	return (
 		<>
-			<p>{upperCopy()}</p>
+			<p>{topSignUpCopy()}</p>
 			{!isGuardianPrint && (
 				<>
-					{!isTierThree && (
-						<div
-							css={css`
-								margin-top: ${space[4]}px;
-							`}
-						>
-							<BulletPointedList
-								items={[
-									'Show you far fewer requests for financial support',
-									'Offer you a simple way to manage your support payments and newsletter subscriptions',
-								]}
-							/>
-						</div>
-					)}
-					<p
-						css={css`
-							margin-top: ${space[4]}px;
-						`}
-					>
-						{isTierThree ? lowerCopyTier3 : lowerCopy}
+					<div css={signUpSpacing}>
+						<BulletPointedList
+							items={[
+								'Show you far fewer requests for financial support',
+								'Offer you a simple way to manage your support payments and newsletter subscriptions',
+							]}
+						/>
+					</div>
+					<p css={signUpSpacing}>
+						Make sure you sign in on each of the devices you use to read our
+						journalism – either today or next time you use them.
 					</p>
 				</>
 			)}
