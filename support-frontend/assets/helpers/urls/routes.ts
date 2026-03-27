03eca02cd89fdc4418dad822cdbce748c8726276
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-03-27 17:31:43 +0000

    fix: prettier

--- support-frontend/assets/helpers/urls/routes.ts
+++ support-frontend/assets/helpers/urls/routes.ts
@@ -165,10 +165,7 @@ function parameteriseUrl(
 	const params = {
 		promoCode,
 	};
-	const urlWithParams = addQueryParamsToURL(url, params).replace(
-		/\?$/,
-		'',
-	); // removes ? when no params
+	const urlWithParams = addQueryParamsToURL(url, params).replace(/\?$/, ''); // removes ? when no params
 	return fulfilmentOption
 		? `${urlWithParams}#${fulfilmentOption}`
 		: urlWithParams;
