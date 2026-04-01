3136f192288affd7d78cc1cacc90acf405fbb960
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-04-01 15:41:31 +0100

    review: linear gradient override applied to SubscriptionHero Weekly with same gradient change in Paper (but in blue)

--- support-frontend/assets/pages/subscriptions-landing/copy/subscriptionCopyStyles.tsx
+++ support-frontend/assets/pages/subscriptions-landing/copy/subscriptionCopyStyles.tsx
@@ -1,11 +1,16 @@
 import { css } from '@emotion/react';
-import { from, neutral, palette } from '@guardian/source/foundations';
+import { from, neutral, palette, until } from '@guardian/source/foundations';
 
 export const weeklySubscriptionProductCardStyle = css`
 	color: ${neutral[0]};
 	background-color: ${palette.brandAlt[400]};
 	div {
 		background-color: #cadbe8;
+		& div:nth-child(2) {
+			${until.tablet} {
+				background: linear-gradient(#cadbe8, #b0c1ce);
+			}
+		}
 	}
 	${from.tablet} {
 		background-color: #cadbe8;
