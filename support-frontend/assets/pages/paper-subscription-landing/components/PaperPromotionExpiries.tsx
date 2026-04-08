1a36796161b7d9d7abc367124cabc363e0c12807
Jorge Costa <jorge.costa@theguardian.com>
2026-04-08 09:37:42 +0100

    early return if no promos

--- support-frontend/assets/pages/paper-subscription-landing/components/PaperPromotionExpiries.tsx
+++ support-frontend/assets/pages/paper-subscription-landing/components/PaperPromotionExpiries.tsx
@@ -1,8 +1,12 @@
 import { usePromoTerms } from 'contexts/PromoTermsContext';
 import { promotionContainer } from './PaperPromotionExpiriesStyles';
 
-export default function PaperPromotionExpiries(): JSX.Element {
+export default function PaperPromotionExpiries(): JSX.Element | null {
 	const { promoTerms } = usePromoTerms();
+	if (!promoTerms) {
+		return null;
+	}
+
 	return (
 		<div css={promotionContainer}>
 			<p>{promoTerms}</p>
