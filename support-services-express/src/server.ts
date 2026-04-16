bb169dfd3a455a7c54e409ecfcf766df21af2c28
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-04-16 16:07:43 +0100

    feat: setup server on http://localhost:3000/

---
+++ support-services-express/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
