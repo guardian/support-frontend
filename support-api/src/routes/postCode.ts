0f9956342ef47c3636d322620c08836f5cce8db3
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-04-17 19:25:29 +0100

    feat: type PostcodeFinderResult with lineOne, lineTwo, City

--- support-api/src/routes/postCode.ts
+++ support-api/src/routes/postCode.ts
@@ -1,42 +1,61 @@
 import express from "express";
 
+type PostcodeFinderResult = {
+  lineOne: string;
+  lineTwo: string;
+  city: string;
+};
+
 // Extend Express Request interface to include line1 property
 declare global {
   namespace Express {
-    interface Request {
-      line1?: string;
-    }
+    interface Request extends PostcodeFinderResult {}
   }
 }
 
 const routerPostcode = express.Router();
 
-const addressLines = [
-  { "RH2 7DP": `X Blanford Rd` },
-  { "N1 9GU": `X York Rd` },
-];
-
+const addresses = {
+  RH27DP: {
+    lineOne: `X Blanford Rd`,
+    lineTwo: "",
+    city: "",
+  },
+  N19GU: {
+    lineOne: `X York Rd`,
+    lineTwo: "",
+    city: "",
+  },
+  OTHER: {
+    lineOne: `Cant Find`,
+    lineTwo: "",
+    city: "",
+  },
+};
 routerPostcode.route("/:postcode").get((req, res) => {
   const { postcode } = req.params;
-  const { line1 } = req;
-  const address = {
-    line1: line1 || "not found",
-    line2: "",
-    town: "",
-    county: "",
-    postcode,
+  const { lineOne, lineTwo, city } = req;
+  const address: PostcodeFinderResult = {
+    lineOne,
+    lineTwo,
+    city,
   };
   res.send(JSON.stringify(address));
 });
 
 routerPostcode.param("postcode", (req, res, next, postcode) => {
-  const line1 = addressLines.find((obj) => Object.hasOwn(obj, postcode));
-  if (line1) {
-    req.line1 = Object.values(line1)[0];
-  }
+  const lookupPostcode = postcode.replace(" ", "").toUpperCase();
+  const indexPostCode =
+    lookupPostcode === "RH27DP" || lookupPostcode === "N19GU"
+      ? (lookupPostcode as keyof typeof addresses)
+      : "OTHER";
+  const address: PostcodeFinderResult = addresses[indexPostCode];
+  req.lineOne = address.lineOne;
+  req.lineTwo = address.lineTwo;
+  req.city = address.city;
   console.log(
-    `Middleware for postcode: ${postcode} gives line1: ${
-      req.line1 || "not found"
+    `Middleware for postcode: ${postcode} gives lineOne: ${
+      req.lineOne || "not found"
     }`
   );
   next();
