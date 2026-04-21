eb1b94a826f240fa248b40597b7629d84cc9cea2
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-04-21 18:45:11 +0100

    review: jest sample setup

---
+++ support-backend/jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	runner: 'groups',
+	testPathIgnorePatterns: ['/node_modules/', 'target'],
+	transformIgnorePatterns: ['/node_modules/\\.pnpm/(?!@guardian)'],
+	setupFilesAfterEnv: ['<rootDir>/src/test/test-setup.ts'],
+	moduleNameMapper: {
+		// Modules directory in support-frontend
+		'@modules/product/(.*)$': '<rootDir>/../modules/product/$1',
+		// Modules directory in support-service-lambdas
+		'@modules/(.*)/(.*)/(.*)$':
+			'@guardian/support-service-lambdas/modules/$1/src/$2/$3',
+		'@modules/(.*)/(.*)$':
+			'@guardian/support-service-lambdas/modules/$1/src/$2',
+		'@modules/(.*)$': '@guardian/support-service-lambdas/modules/$1',
+	},
+};
---
+++ support-backend/src/test/sample.test.ts
@@ -0,0 +1,10 @@
+describe("sample", () => {
+  it("false", () => {
+    const result = false;
+    expect(result).toEqual(false);
+  });
+  it("true", () => {
+    const result = true;
+    expect(result).toEqual(true);
+  });
+});
