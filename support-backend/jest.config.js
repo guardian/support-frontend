a1f82a26cd3276ba362b0b3740cb5696c18efae4
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-04-22 12:43:02 +0100

    review: jest name mapper removd

--- support-backend/jest.config.js
+++ support-backend/jest.config.js
@@ -6,14 +6,4 @@ module.exports = {
 	testPathIgnorePatterns: ['/node_modules/', 'target'],
 	transformIgnorePatterns: ['/node_modules/\\.pnpm/(?!@guardian)'],
 	setupFilesAfterEnv: ['<rootDir>/src/test/test-setup.ts'],
-	moduleNameMapper: {
-		// Modules directory in support-frontend
-		'@modules/product/(.*)$': '<rootDir>/../modules/product/$1',
-		// Modules directory in support-service-lambdas
-		'@modules/(.*)/(.*)/(.*)$':
-			'@guardian/support-service-lambdas/modules/$1/src/$2/$3',
-		'@modules/(.*)/(.*)$':
-			'@guardian/support-service-lambdas/modules/$1/src/$2',
-		'@modules/(.*)$': '@guardian/support-service-lambdas/modules/$1',
-	},
 };
