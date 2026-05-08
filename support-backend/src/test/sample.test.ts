6e4684cb8a83d5c030132b5e4b0fdce2f7444135
jorgeCosta <jorge.costa@theguardian.com>
2026-05-08 10:35:54 +0100

    Move prettier config to the root of the repo (#7961)
    Replace workspace specific prettier config with a single file at the root of
    the repo.
    
    To ensure consistent formatting across workspaces. I noticed that prettier
    wasn't working in the new support-backend workspace because we forgot to add a
    prettier config for it. This PR should prevent that being an issue in future.
    
    Co-authored-by: Tom Wey <tom.wey@theguardian.com>

--- support-backend/src/test/sample.test.ts
+++ support-backend/src/test/sample.test.ts
@@ -1,10 +1,10 @@
-describe("sample", () => {
-  it("false", () => {
-    const result = false;
-    expect(result).toEqual(false);
-  });
-  it("true", () => {
-    const result = true;
-    expect(result).toEqual(true);
-  });
+describe('sample', () => {
+	it('false', () => {
+		const result = false;
+		expect(result).toEqual(false);
+	});
+	it('true', () => {
+		const result = true;
+		expect(result).toEqual(true);
+	});
 });
