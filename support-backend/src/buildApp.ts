e1e8f824ebc88ae7945d50cd36c6664108762dce
Tom Wey <tom.wey@theguardian.com>
2026-05-08 11:00:48 +0100

    Empty API router

--- support-backend/src/buildApp.ts
+++ support-backend/src/buildApp.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { apiRouter } from './apiRouter';
 
 export function buildApp() {
 	const app = express();
+
+	app.use('/api', apiRouter);
 
 	app.get('/healthcheck-express', (req, res) => {
 		console.log(`HealthCheck processed from port: ${req.socket.localPort}`);
