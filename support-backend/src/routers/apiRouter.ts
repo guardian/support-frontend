4adc7c616c76f2d80acca6f9ebbe8803bf0be222
Tom Wey <tom.wey@theguardian.com>
2026-05-11 15:46:40 +0100

    Extract functionality to a handler to keep routes clean

--- support-backend/src/routers/apiRouter.ts
+++ support-backend/src/routers/apiRouter.ts
@@ -1,22 +1,6 @@
 import { Router } from 'express';
+import { postcodeLookupHandler } from '../handlers/postcodeLookup';
 
 export const apiRouter = Router();
 
-interface Address {
-	lineOne?: string;
-	lineTwo?: string;
-	city?: string;
-	state?: string;
-	postCode?: string;
-	country?: string;
-}
-
-apiRouter.get('/postcode-lookup/:postcode', (req, res) => {
-	const postcode = decodeURIComponent(req.params.postcode);
-	if (postcode.length > 10) {
-		res.status(400).send();
-		return;
-	}
-	const addresses: Address[] = [];
-	res.json(addresses);
-});
+apiRouter.get('/postcode-lookup/:postcode', postcodeLookupHandler);
