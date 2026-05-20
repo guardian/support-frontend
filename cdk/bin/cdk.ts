93c6b432203c42ea474be59d469b4df346b7317b
Tom Wey <tom.wey@theguardian.com>
2026-05-20 11:14:15 +0100

    Format cdk workspace with prettier
    For some reason these weren't formatted according to the standard rules
    before. I ran:
    
    $ pnpm --filter cdk format

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,181 +1,181 @@
-import "source-map-support/register";
-import { App } from "aws-cdk-lib";
-import { AcquisitionEventsApi } from "../lib/acquisition-events-api";
-import { Backend } from "../lib/backend";
-import { BigqueryAcquisitionsPublisher } from "../lib/bigquery-acquisitions-publisher";
-import { Frontend } from "../lib/frontend";
-import { PaymentApi } from "../lib/payment-api";
-import { StripePatronsData } from "../lib/stripe-patrons-data";
-import { SupportWorkers } from "../lib/support-workers";
+import 'source-map-support/register';
+import { App } from 'aws-cdk-lib';
+import { AcquisitionEventsApi } from '../lib/acquisition-events-api';
+import { Backend } from '../lib/backend';
+import { BigqueryAcquisitionsPublisher } from '../lib/bigquery-acquisitions-publisher';
+import { Frontend } from '../lib/frontend';
+import { PaymentApi } from '../lib/payment-api';
+import { StripePatronsData } from '../lib/stripe-patrons-data';
+import { SupportWorkers } from '../lib/support-workers';
 
 const app = new App();
 const cloudFormationStackName = process.env.GU_CFN_STACK_NAME;
 
-new Frontend(app, "Frontend-PROD", {
-  stack: "support",
-  stage: "PROD",
-  cloudFormationStackName,
-  membershipSubPromotionsTables: [
-    "arn:aws:dynamodb:*:*:table/support-admin-console-promos-PROD",
-    "arn:aws:dynamodb:*:*:table/support-admin-console-promos-CODE",
-  ],
-  domainName: "support.theguardian.com.origin.membership.guardianapis.com",
-  scaling: {
-    minimumInstances: 3,
-    maximumInstances: 6,
-  },
-  shouldCreateAlarms: true,
+new Frontend(app, 'Frontend-PROD', {
+	stack: 'support',
+	stage: 'PROD',
+	cloudFormationStackName,
+	membershipSubPromotionsTables: [
+		'arn:aws:dynamodb:*:*:table/support-admin-console-promos-PROD',
+		'arn:aws:dynamodb:*:*:table/support-admin-console-promos-CODE',
+	],
+	domainName: 'support.theguardian.com.origin.membership.guardianapis.com',
+	scaling: {
+		minimumInstances: 3,
+		maximumInstances: 6,
+	},
+	shouldCreateAlarms: true,
 });
 
-new Frontend(app, "Frontend-CODE", {
-  stack: "support",
-  stage: "CODE",
-  cloudFormationStackName,
-  membershipSubPromotionsTables: [
-    "arn:aws:dynamodb:*:*:table/support-admin-console-promos-CODE",
-  ],
-  domainName: "support.code.theguardian.com.origin.membership.guardianapis.com",
-  scaling: {
-    minimumInstances: 1,
-    maximumInstances: 2,
-  },
-  shouldCreateAlarms: false,
+new Frontend(app, 'Frontend-CODE', {
+	stack: 'support',
+	stage: 'CODE',
+	cloudFormationStackName,
+	membershipSubPromotionsTables: [
+		'arn:aws:dynamodb:*:*:table/support-admin-console-promos-CODE',
+	],
+	domainName: 'support.code.theguardian.com.origin.membership.guardianapis.com',
+	scaling: {
+		minimumInstances: 1,
+		maximumInstances: 2,
+	},
+	shouldCreateAlarms: false,
 });
 
-new Backend(app, "Backend-PROD", {
-  stack: "support",
-  stage: "PROD",
-  domainName: "backend.theguardian.com.origin.support.guardianapis.com",
-  scaling: {
-    minimumInstances: 1,
-    maximumInstances: 2,
-  },
+new Backend(app, 'Backend-PROD', {
+	stack: 'support',
+	stage: 'PROD',
+	domainName: 'backend.theguardian.com.origin.support.guardianapis.com',
+	scaling: {
+		minimumInstances: 1,
+		maximumInstances: 2,
+	},
 });
 
-new Backend(app, "Backend-CODE", {
-  stack: "support",
-  stage: "CODE",
-  domainName: "backend.code.theguardian.com.origin.support.guardianapis.com",
-  scaling: {
-    minimumInstances: 1,
-    maximumInstances: 2,
-  },
+new Backend(app, 'Backend-CODE', {
+	stack: 'support',
+	stage: 'CODE',
+	domainName: 'backend.code.theguardian.com.origin.support.guardianapis.com',
+	scaling: {
+		minimumInstances: 1,
+		maximumInstances: 2,
+	},
 });
 
-new StripePatronsData(app, "StripePatronsData-CODE", {
-  stack: "support",
-  stage: "CODE",
-  cloudFormationStackName,
-  buildNumber: process.env.GITHUB_RUN_NUMBER ?? "DEV",
+new StripePatronsData(app, 'StripePatronsData-CODE', {
+	stack: 'support',
+	stage: 'CODE',
+	cloudFormationStackName,
+	buildNumber: process.env.GITHUB_RUN_NUMBER ?? 'DEV',
 });
 
-new StripePatronsData(app, "StripePatronsData-PROD", {
-  stack: "support",
-  stage: "PROD",
-  cloudFormationStackName,
-  buildNumber: process.env.GITHUB_RUN_NUMBER ?? "DEV",
+new StripePatronsData(app, 'StripePatronsData-PROD', {
+	stack: 'support',
+	stage: 'PROD',
+	cloudFormationStackName,
+	buildNumber: process.env.GITHUB_RUN_NUMBER ?? 'DEV',
 });
 
-new PaymentApi(app, "Payment-API-CODE", {
-  stack: "support",
-  stage: "CODE",
-  domainName:
-    "pay.code.dev-guardianapis.com.origin.membership.guardianapis.com",
-  scaling: {
-    minimumInstances: 1,
-    maximumInstances: 2,
-  },
+new PaymentApi(app, 'Payment-API-CODE', {
+	stack: 'support',
+	stage: 'CODE',
+	domainName:
+		'pay.code.dev-guardianapis.com.origin.membership.guardianapis.com',
+	scaling: {
+		minimumInstances: 1,
+		maximumInstances: 2,
+	},
 });
 
-new PaymentApi(app, "Payment-API-PROD", {
-  stack: "support",
-  stage: "PROD",
-  domainName: "payment.guardianapis.com.origin.membership.guardianapis.com",
-  scaling: {
-    minimumInstances: 3,
-    maximumInstances: 6,
-  },
+new PaymentApi(app, 'Payment-API-PROD', {
+	stack: 'support',
+	stage: 'PROD',
+	domainName: 'payment.guardianapis.com.origin.membership.guardianapis.com',
+	scaling: {
+		minimumInstances: 3,
+		maximumInstances: 6,
+	},
 });
 
-new AcquisitionEventsApi(app, "Acquisition-Events-API-CODE", {
-  stack: "support",
-  stage: "CODE",
-  certificateId: "b384a6a0-2f54-4874-b99b-96eeff96c009",
-  domainName: "acquisition-events-code.support.guardianapis.com",
-  hostedZoneName: "support.guardianapis.com.",
-  hostedZoneId: "Z3KO35ELNWZMSX",
+new AcquisitionEventsApi(app, 'Acquisition-Events-API-CODE', {
+	stack: 'support',
+	stage: 'CODE',
+	certificateId: 'b384a6a0-2f54-4874-b99b-96eeff96c009',
+	domainName: 'acquisition-events-code.support.guardianapis.com',
+	hostedZoneName: 'support.guardianapis.com.',
+	hostedZoneId: 'Z3KO35ELNWZMSX',
 });
 
-new AcquisitionEventsApi(app, "Acquisition-Events-API-PROD", {
-  stack: "support",
-  stage: "PROD",
-  certificateId: "b384a6a0-2f54-4874-b99b-96eeff96c009",
-  domainName: "acquisition-events.support.guardianapis.com",
-  hostedZoneName: "support.guardianapis.com.",
-  hostedZoneId: "Z3KO35ELNWZMSX",
+new AcquisitionEventsApi(app, 'Acquisition-Events-API-PROD', {
+	stack: 'support',
+	stage: 'PROD',
+	certificateId: 'b384a6a0-2f54-4874-b99b-96eeff96c009',
+	domainName: 'acquisition-events.support.guardianapis.com',
+	hostedZoneName: 'support.guardianapis.com.',
+	hostedZoneId: 'Z3KO35ELNWZMSX',
 });
 
-new BigqueryAcquisitionsPublisher(app, "BigqueryAcquisitionsPublisher-CODE", {
-  stack: "support",
-  stage: "CODE",
+new BigqueryAcquisitionsPublisher(app, 'BigqueryAcquisitionsPublisher-CODE', {
+	stack: 'support',
+	stage: 'CODE',
 });
 
-new BigqueryAcquisitionsPublisher(app, "BigqueryAcquisitionsPublisher-PROD", {
-  stack: "support",
-  stage: "PROD",
+new BigqueryAcquisitionsPublisher(app, 'BigqueryAcquisitionsPublisher-PROD', {
+	stack: 'support',
+	stage: 'PROD',
 });
 
-new SupportWorkers(app, "SupportWorkers-CODE", {
-  stack: "support",
-  stage: "CODE",
-  promotionsDynamoTables: [
-    "arn:aws:dynamodb:*:*:table/support-admin-console-promos-CODE",
-  ],
-  s3Files: [
-    "arn:aws:s3:::gu-zuora-catalog/PROD/Zuora-CODE/catalog.json",
-    "arn:aws:s3:::support-workers-private/*",
-  ],
-  supporterProductDataQueueArns: [
-    "arn:aws:sqs:eu-west-1:865473395570:supporter-product-data-CODE",
-  ],
-  eventBusArns: [
-    "arn:aws:events:eu-west-1:865473395570:event-bus/acquisitions-bus-CODE",
-  ],
-  parameterStorePaths: [
-    "arn:aws:ssm:eu-west-1:865473395570:parameter/CODE/support/support-workers/*",
-    "arn:aws:ssm:eu-west-1:865473395570:parameter/PROD/support/support-workers/paper-round-config",
-  ],
-  secretsManagerPaths: [
-    "arn:aws:secretsmanager:eu-west-1:865473395570:secret:CODE/Zuora-OAuth/SupportServiceLambdas-*",
-  ],
+new SupportWorkers(app, 'SupportWorkers-CODE', {
+	stack: 'support',
+	stage: 'CODE',
+	promotionsDynamoTables: [
+		'arn:aws:dynamodb:*:*:table/support-admin-console-promos-CODE',
+	],
+	s3Files: [
+		'arn:aws:s3:::gu-zuora-catalog/PROD/Zuora-CODE/catalog.json',
+		'arn:aws:s3:::support-workers-private/*',
+	],
+	supporterProductDataQueueArns: [
+		'arn:aws:sqs:eu-west-1:865473395570:supporter-product-data-CODE',
+	],
+	eventBusArns: [
+		'arn:aws:events:eu-west-1:865473395570:event-bus/acquisitions-bus-CODE',
+	],
+	parameterStorePaths: [
+		'arn:aws:ssm:eu-west-1:865473395570:parameter/CODE/support/support-workers/*',
+		'arn:aws:ssm:eu-west-1:865473395570:parameter/PROD/support/support-workers/paper-round-config',
+	],
+	secretsManagerPaths: [
+		'arn:aws:secretsmanager:eu-west-1:865473395570:secret:CODE/Zuora-OAuth/SupportServiceLambdas-*',
+	],
 });
 
-new SupportWorkers(app, "SupportWorkers-PROD", {
-  stack: "support",
-  stage: "PROD",
-  promotionsDynamoTables: [
-    "arn:aws:dynamodb:*:*:table/support-admin-console-promos-CODE",
-    "arn:aws:dynamodb:*:*:table/support-admin-console-promos-PROD",
-  ],
-  s3Files: [
-    "arn:aws:s3:::gu-zuora-catalog/PROD/Zuora-CODE/catalog.json",
-    "arn:aws:s3:::gu-zuora-catalog/PROD/Zuora-PROD/catalog.json",
-    "arn:aws:s3:::support-workers-private/*",
-  ],
-  supporterProductDataQueueArns: [
-    "arn:aws:sqs:eu-west-1:865473395570:supporter-product-data-CODE",
-    "arn:aws:sqs:eu-west-1:865473395570:supporter-product-data-PROD",
-  ],
-  eventBusArns: [
-    "arn:aws:events:eu-west-1:865473395570:event-bus/acquisitions-bus-CODE",
-    "arn:aws:events:eu-west-1:865473395570:event-bus/acquisitions-bus-PROD",
-  ],
-  parameterStorePaths: [
-    `arn:aws:ssm:eu-west-1:865473395570:parameter/CODE/support/support-workers/*`,
-    `arn:aws:ssm:eu-west-1:865473395570:parameter/PROD/support/support-workers/*`,
-  ],
-  secretsManagerPaths: [
-    "arn:aws:secretsmanager:eu-west-1:865473395570:secret:CODE/Zuora-OAuth/SupportServiceLambdas-*",
-    "arn:aws:secretsmanager:eu-west-1:865473395570:secret:PROD/Zuora-OAuth/SupportServiceLambdas-*",
-  ],
+new SupportWorkers(app, 'SupportWorkers-PROD', {
+	stack: 'support',
+	stage: 'PROD',
+	promotionsDynamoTables: [
+		'arn:aws:dynamodb:*:*:table/support-admin-console-promos-CODE',
+		'arn:aws:dynamodb:*:*:table/support-admin-console-promos-PROD',
+	],
+	s3Files: [
+		'arn:aws:s3:::gu-zuora-catalog/PROD/Zuora-CODE/catalog.json',
+		'arn:aws:s3:::gu-zuora-catalog/PROD/Zuora-PROD/catalog.json',
+		'arn:aws:s3:::support-workers-private/*',
+	],
+	supporterProductDataQueueArns: [
+		'arn:aws:sqs:eu-west-1:865473395570:supporter-product-data-CODE',
+		'arn:aws:sqs:eu-west-1:865473395570:supporter-product-data-PROD',
+	],
+	eventBusArns: [
+		'arn:aws:events:eu-west-1:865473395570:event-bus/acquisitions-bus-CODE',
+		'arn:aws:events:eu-west-1:865473395570:event-bus/acquisitions-bus-PROD',
+	],
+	parameterStorePaths: [
+		`arn:aws:ssm:eu-west-1:865473395570:parameter/CODE/support/support-workers/*`,
+		`arn:aws:ssm:eu-west-1:865473395570:parameter/PROD/support/support-workers/*`,
+	],
+	secretsManagerPaths: [
+		'arn:aws:secretsmanager:eu-west-1:865473395570:secret:CODE/Zuora-OAuth/SupportServiceLambdas-*',
+		'arn:aws:secretsmanager:eu-west-1:865473395570:secret:PROD/Zuora-OAuth/SupportServiceLambdas-*',
+	],
 });
--- cdk/lib/backend.ts
+++ cdk/lib/backend.ts
@@ -1,44 +1,44 @@
-import { GuEc2App } from "@guardian/cdk";
-import { AccessScope } from "@guardian/cdk/lib/constants";
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
+import { GuEc2App } from '@guardian/cdk';
+import { AccessScope } from '@guardian/cdk/lib/constants';
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import {
-  GuDistributionBucketParameter,
-  GuStack,
-} from "@guardian/cdk/lib/constructs/core";
-import { GuAllowPolicy } from "@guardian/cdk/lib/constructs/iam";
-import type { GuAsgCapacity } from "@guardian/cdk/lib/types";
-import type { App } from "aws-cdk-lib";
-import { aws_route53_targets } from "aws-cdk-lib";
+	GuDistributionBucketParameter,
+	GuStack,
+} from '@guardian/cdk/lib/constructs/core';
+import { GuAllowPolicy } from '@guardian/cdk/lib/constructs/iam';
+import type { GuAsgCapacity } from '@guardian/cdk/lib/types';
+import type { App } from 'aws-cdk-lib';
+import { aws_route53_targets } from 'aws-cdk-lib';
 import {
-  InstanceClass,
-  InstanceSize,
-  InstanceType,
-  UserData,
-} from "aws-cdk-lib/aws-ec2";
-import { SslPolicy } from "aws-cdk-lib/aws-elasticloadbalancingv2";
-import type { CfnListener } from "aws-cdk-lib/aws-elasticloadbalancingv2";
-import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
+	InstanceClass,
+	InstanceSize,
+	InstanceType,
+	UserData,
+} from 'aws-cdk-lib/aws-ec2';
+import { SslPolicy } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import type { CfnListener } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
 
 interface BackendProps extends GuStackProps {
-  domainName: string;
-  scaling: GuAsgCapacity;
+	domainName: string;
+	scaling: GuAsgCapacity;
 }
 
 export class Backend extends GuStack {
-  constructor(scope: App, id: string, props: BackendProps) {
-    const { domainName, scaling } = props;
+	constructor(scope: App, id: string, props: BackendProps) {
+		const { domainName, scaling } = props;
 
-    super(scope, id, {
-      ...props,
-      // Required for ALB logging
-      env: { region: "eu-west-1" },
-    });
+		super(scope, id, {
+			...props,
+			// Required for ALB logging
+			env: { region: 'eu-west-1' },
+		});
 
-    const app = "support-backend";
+		const app = 'support-backend';
 
-    const hostedZoneId = "Z3KO35ELNWZMSX";
+		const hostedZoneId = 'Z3KO35ELNWZMSX';
 
-    const userData = UserData.custom(`#!/bin/bash
+		const userData = UserData.custom(`#!/bin/bash
 set -e
 groupadd support
 useradd -r -m -s /usr/bin/nologin -g support ${app}
@@ -49,8 +49,8 @@ cd ${app}
 
 sleep 5
 aws --region eu-west-1 s3 cp s3://${
-      GuDistributionBucketParameter.getInstance(this).valueAsString
-    }/support/${this.stage}/${app}/${app}.tar.gz ./
+			GuDistributionBucketParameter.getInstance(this).valueAsString
+		}/support/${this.stage}/${app}/${app}.tar.gz ./
 tar -xzf ${app}.tar.gz
 
 export NODE_ENV=production
@@ -62,57 +62,57 @@ chown -R ${app}:support /var/log/${app}
 cd target
 /usr/local/node/pm2 start --uid ${app} --gid support server.js`);
 
-    const policies = [
-      new GuAllowPolicy(this, "SSMGet", {
-        actions: ["ssm:GetParametersByPath"],
-        resources: [
-          `arn:aws:ssm:${this.region}:${this.account}:parameter/${this.stack}/${app}/${this.stage}`,
-        ],
-      }),
-      new GuAllowPolicy(this, "CloudwatchMetrics", {
-        actions: [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:DescribeLogStreams",
-        ],
-        resources: ["arn:aws:logs:*:*:*"],
-      }),
-    ];
+		const policies = [
+			new GuAllowPolicy(this, 'SSMGet', {
+				actions: ['ssm:GetParametersByPath'],
+				resources: [
+					`arn:aws:ssm:${this.region}:${this.account}:parameter/${this.stack}/${app}/${this.stage}`,
+				],
+			}),
+			new GuAllowPolicy(this, 'CloudwatchMetrics', {
+				actions: [
+					'logs:CreateLogGroup',
+					'logs:CreateLogStream',
+					'logs:PutLogEvents',
+					'logs:DescribeLogStreams',
+				],
+				resources: ['arn:aws:logs:*:*:*'],
+			}),
+		];
 
-    const ec2App = new GuEc2App(this, {
-      applicationPort: 3000,
-      app: app,
-      access: { scope: AccessScope.PUBLIC },
-      certificateProps: {
-        domainName,
-        hostedZoneId,
-      },
-      healthcheck: {
-        path: "/healthcheck-express",
-      },
-      instanceMetricGranularity: "5Minute",
-      monitoringConfiguration: { noMonitoring: true },
-      userData,
-      roleConfiguration: {
-        additionalPolicies: policies,
-      },
-      scaling,
-      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.SMALL),
-    });
+		const ec2App = new GuEc2App(this, {
+			applicationPort: 3000,
+			app: app,
+			access: { scope: AccessScope.PUBLIC },
+			certificateProps: {
+				domainName,
+				hostedZoneId,
+			},
+			healthcheck: {
+				path: '/healthcheck-express',
+			},
+			instanceMetricGranularity: '5Minute',
+			monitoringConfiguration: { noMonitoring: true },
+			userData,
+			roleConfiguration: {
+				additionalPolicies: policies,
+			},
+			scaling,
+			instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.SMALL),
+		});
 
-    (ec2App.listener.node.defaultChild as CfnListener).sslPolicy =
-      SslPolicy.TLS13_RES;
+		(ec2App.listener.node.defaultChild as CfnListener).sslPolicy =
+			SslPolicy.TLS13_RES;
 
-    new ARecord(this, "AliasRecord", {
-      zone: HostedZone.fromHostedZoneAttributes(this, "hosted-zone", {
-        zoneName: "support.guardianapis.com",
-        hostedZoneId,
-      }),
-      recordName: domainName,
-      target: RecordTarget.fromAlias(
-        new aws_route53_targets.LoadBalancerTarget(ec2App.loadBalancer)
-      ),
-    });
-  }
+		new ARecord(this, 'AliasRecord', {
+			zone: HostedZone.fromHostedZoneAttributes(this, 'hosted-zone', {
+				zoneName: 'support.guardianapis.com',
+				hostedZoneId,
+			}),
+			recordName: domainName,
+			target: RecordTarget.fromAlias(
+				new aws_route53_targets.LoadBalancerTarget(ec2App.loadBalancer),
+			),
+		});
+	}
 }
