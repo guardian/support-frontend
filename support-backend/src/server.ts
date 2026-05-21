95f622aaa4041a4d6ba999618cb2ebeb4f8a413f
Tom Wey <tom.wey@theguardian.com>
2026-05-21 12:17:07 +0100

    Don't log on every healthcheck request
    It's a bit noisy.

--- support-backend/src/server.ts
+++ support-backend/src/server.ts
@@ -5,7 +5,6 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.get('/healthcheck-express', (req, res) => {
-	console.log(`HealthCheck processed from port: ${req.socket.localPort}`);
 	res.json({ status: 'OK' });
 });
 
