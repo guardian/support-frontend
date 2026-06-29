01a501eccd7a20ba9f36a0b4e703d7636126c158
Tom Wey <tom.wey@theguardian.com>
2026-06-29 16:45:15 +0100

    Playing around with the business logic of estimating tax

---
+++ support-frontend/assets/helpers/salesTax/getEstimatedSalesTax.ts
@@ -0,0 +1,88 @@
+import type { CaState } from '@modules/internationalisation/country';
+import { SupportRegionId } from '@modules/internationalisation/countryGroup';
+import type { AppConfig } from 'helpers/globalsAndSwitches/window';
+import type {
+	ActiveProductKey,
+	ActiveRatePlanKey,
+} from 'helpers/productCatalog';
+
+type TaxRateResult =
+	| {
+			type: 'rate';
+			rate: number;
+	  }
+	| {
+			type: 'not_enough_information';
+	  }
+	| {
+			type: 'not_applicable';
+	  }
+	| {
+			type: 'error';
+			message: string;
+	  };
+
+export function getEstimatedSalesTaxRate(
+	productKey: ActiveProductKey,
+	ratePlanKey: ActiveRatePlanKey,
+	appConfig: AppConfig,
+	maybeProvinceCode: CaState | undefined,
+	supportRegionId: SupportRegionId,
+): TaxRateResult {
+	const { productCatalog, taxRates } = appConfig;
+
+	if (supportRegionId !== SupportRegionId.CA) {
+		return { type: 'not_applicable' };
+	}
+
+	if (
+		productCatalog[productKey]?.ratePlans[ratePlanKey]?.taxMode !==
+		'TaxExclusive'
+	) {
+		return {
+			type: 'not_applicable',
+		};
+	}
+
+	if (productKey !== 'DigitalSubscription' && productKey !== 'SupporterPlus') {
+		return {
+			type: 'not_applicable',
+		};
+	}
+
+	if (!maybeProvinceCode) {
+		return {
+			type: 'not_enough_information',
+		};
+	}
+
+	if (!taxRates) {
+		return {
+			type: 'error',
+			message: 'Missing tax rate data',
+		};
+	}
+
+	const maybeProductTaxRates = taxRates[productKey];
+	if (!maybeProductTaxRates) {
+		return {
+			type: 'error',
+			message: 'Missing tax rate data for product',
+		};
+	}
+
+	const maybeRate = maybeProductTaxRates[maybeProvinceCode] as
+		| number
+		| undefined;
+	if (maybeRate === undefined) {
+		return {
+			type: 'error',
+			message: 'Missing tax rate date for province',
+		};
+	}
+
+	return {
+		type: 'rate',
+		rate: maybeRate,
+	};
+}
