39a7de257936b5275fbaf2eca2f6e0c86eed9dd1
Jorge Costa <jorge.costa@theguardian.com>
2026-07-06 13:54:08 +0100

    cleanup bad comment

--- support-e2e/tests/utils/enableTaxExclusiveRatePlans.ts
+++ support-e2e/tests/utils/enableTaxExclusiveRatePlans.ts
@@ -1,7 +1,5 @@
 import type { BrowserContext } from '@playwright/test';
 
-// Force skipping the new onboarding experience. This is WIP and we don't want
-// it to interfere with the Playwright tests
 export const enableCanadaTaxExclusion = async (context: BrowserContext) =>
 	await context.addInitScript(() => {
 		window.sessionStorage.setItem(
