40ea93178909fa39dd58f383ad6e4c652aa50132
Paul Dempsey <76729591+paul-daniel-dempsey@users.noreply.github.com>
2026-07-07 08:37:14 +0100

    One-Time-Checkout: AUD limit increase from $16000 to $25000 (#8088)
    * feat: increase AUD limit from $16000 to $25000
    
    * review: removed max and min in words as unused
    
    * review: removed unused defaults

--- support-frontend/assets/helpers/contributions.ts
+++ support-frontend/assets/helpers/contributions.ts
@@ -47,65 +47,22 @@ type Config = Record<
 	ContributionType,
 	{
 		min: number;
-		minInWords: string;
 		max: number;
-		maxInWords: string;
-		default: number; // TODO - remove this field once old payment flow has gone
 	}
 >;
-
-// ----- Setup ----- //
-
-const numbersInWords = {
-	'1': 'one',
-	'2': 'two',
-	'5': 'five',
-	'6': 'six',
-	'7': 'seven',
-	'10': 'ten',
-	'15': 'fifteen',
-	'20': 'twenty',
-	'25': 'twenty five',
-	'30': 'thirty',
-	'35': 'thirty five',
-	'40': 'forty',
-	'50': 'fifty',
-	'75': 'seventy five',
-	'100': 'one hundred',
-	'125': 'one hundred and twenty five',
-	'166': 'one hundred and sixty six',
-	'200': 'two hundred',
-	'250': 'two hundred and fifty',
-	'275': 'two hundred and seventy five',
-	'500': 'five hundred',
-	'750': 'seven hundred and fifty',
-	'800': 'eight hundred',
-	'2000': 'two thousand',
-	'10000': 'ten thousand',
-	'16000': 'sixteen thousand',
-} as const;
 
 const defaultConfig: Config = {
 	ANNUAL: {
 		min: 10,
-		minInWords: numbersInWords['10'],
 		max: 2000,
-		maxInWords: numbersInWords['2000'],
-		default: 50,
 	},
 	MONTHLY: {
 		min: 2,
-		minInWords: numbersInWords['2'],
 		max: 166,
-		maxInWords: numbersInWords['166'],
-		default: 5,
 	},
 	ONE_OFF: {
 		min: 1,
-		minInWords: numbersInWords['1'],
 		max: 2000,
-		maxInWords: numbersInWords['2000'],
-		default: 50,
 	},
 };
 
@@ -115,27 +72,18 @@ const config: Record<CountryGroupId, Config> = {
 		ANNUAL: defaultConfig.ANNUAL,
 		MONTHLY: {
 			min: 2,
-			minInWords: numbersInWords['2'],
 			max: 200,
-			maxInWords: numbersInWords['200'],
-			default: 20,
 		},
 		ONE_OFF: {
 			min: 1,
-			minInWords: numbersInWords['1'],
-			max: 16000,
-			maxInWords: numbersInWords['16000'],
-			default: 50,
+			max: 25000,
 		},
 	},
 	EURCountries: {
 		ANNUAL: defaultConfig.ANNUAL,
 		MONTHLY: {
 			min: 2,
-			minInWords: numbersInWords['2'],
 			max: 166,
-			maxInWords: numbersInWords['166'],
-			default: 10,
 		},
 		ONE_OFF: defaultConfig.ONE_OFF,
 	},
@@ -143,29 +91,21 @@ const config: Record<CountryGroupId, Config> = {
 		ANNUAL: {
 			...defaultConfig.ANNUAL,
 			max: 10000,
-			maxInWords: numbersInWords['10000'],
 		},
 		MONTHLY: {
 			min: 2,
-			minInWords: numbersInWords['2'],
 			max: 800,
-			maxInWords: numbersInWords['800'],
-			default: 15,
 		},
 		ONE_OFF: {
 			...defaultConfig.ONE_OFF,
 			max: 10000,
-			maxInWords: numbersInWords['10000'],
 		},
 	},
 	International: {
 		ANNUAL: defaultConfig.ANNUAL,
 		MONTHLY: {
 			min: 2,
-			minInWords: numbersInWords['2'],
 			max: 166,
-			maxInWords: numbersInWords['166'],
-			default: 10,
 		},
 		ONE_OFF: defaultConfig.ONE_OFF,
 	},
@@ -173,10 +113,7 @@ const config: Record<CountryGroupId, Config> = {
 		ANNUAL: defaultConfig.ANNUAL,
 		MONTHLY: {
 			min: 2,
-			minInWords: numbersInWords['2'],
 			max: 200,
-			maxInWords: numbersInWords['200'],
-			default: 20,
 		},
 		ONE_OFF: defaultConfig.ONE_OFF,
 	},
@@ -184,10 +121,7 @@ const config: Record<CountryGroupId, Config> = {
 		ANNUAL: defaultConfig.ANNUAL,
 		MONTHLY: {
 			min: 2,
-			minInWords: numbersInWords['2'],
 			max: 166,
-			maxInWords: numbersInWords['166'],
-			default: 10,
 		},
 		ONE_OFF: defaultConfig.ONE_OFF,
 	},
