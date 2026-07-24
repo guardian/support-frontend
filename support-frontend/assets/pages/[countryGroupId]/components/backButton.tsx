cf81030821a6504a58e8372164cf2b5d64c3f610
jorgeCosta <jorge.costa@theguardian.com>
2026-07-24 10:05:42 +0100

    fix accessibility on change button (#8119)

--- support-frontend/assets/pages/[countryGroupId]/components/backButton.tsx
+++ support-frontend/assets/pages/[countryGroupId]/components/backButton.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from '@emotion/react';
-import { Button } from '@guardian/source/react-components';
+import { LinkButton } from '@guardian/source/react-components';
 
 type BackButtonProps = {
 	path: string;
@@ -9,15 +9,14 @@ type BackButtonProps = {
 export function BackButton({ path, buttonText }: BackButtonProps) {
 	const { observerThemeButton } = useTheme();
 	return (
-		<a href={path}>
-			<Button
-				priority="tertiary"
-				size="xsmall"
-				role="link"
-				theme={observerThemeButton}
-			>
-				{buttonText}
-			</Button>
-		</a>
+		<LinkButton
+			href={path}
+			priority="tertiary"
+			size="xsmall"
+			role="link"
+			theme={observerThemeButton}
+		>
+			{buttonText}
+		</LinkButton>
 	);
 }
