7ec499ccc631eb20a16502fd31f70986a0c5be66
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-07-27 16:18:12 +0100

    wip: PayPalScriptProvider and PayPalButtons not loading under playwright tests

--- support-frontend/assets/pages/[countryGroupId]/components/submitButton.tsx
+++ support-frontend/assets/pages/[countryGroupId]/components/submitButton.tsx
@@ -231,7 +231,12 @@ export function SubmitButton({
 							/>
 						</>
 					)}
-					<div>PayPalCompletePayments</div>
+					<div>
+						PayPalCompletePayments paypalClientId:{paypalClientId}{' '}
+						payPalPaymentToken?.token:{payPalPaymentToken?.token}{' '}
+						payPalPaymentToken?.email:{payPalPaymentToken?.email} isTestUser:
+						{isTestUser}
+					</div>
 					<PayPalScriptProvider
 						options={{
 							clientId: paypalClientId,
@@ -241,52 +246,55 @@ export function SubmitButton({
 							disableFunding: paypalSdkFundingBlocklist,
 						}}
 					>
-						<PayPalButtons
-							style={{ color: 'blue', layout: 'horizontal', tagline: false }}
-							createVaultSetupToken={async () => {
-								const setupToken = await createSetupToken(csrf);
-								return setupToken;
-							}}
-							onApprove={async (data) => {
-								const approvedSetupToken = (
-									data as unknown as ApprovedSetupToken
-								).vaultSetupToken;
+						<>
+							ARRIVED
+							<PayPalButtons
+								style={{ color: 'blue', layout: 'horizontal', tagline: false }}
+								createVaultSetupToken={async () => {
+									const setupToken = await createSetupToken(csrf);
+									return setupToken;
+								}}
+								onApprove={async (data) => {
+									const approvedSetupToken = (
+										data as unknown as ApprovedSetupToken
+									).vaultSetupToken;
 
-								const paymentToken = await exchangeSetupTokenForPaymentToken(
-									csrf,
-									approvedSetupToken,
-								);
+									const paymentToken = await exchangeSetupTokenForPaymentToken(
+										csrf,
+										approvedSetupToken,
+									);
 
-								// This will trigger a form submission
-								setPayPalPaymentToken(paymentToken);
-							}}
-							onClick={(_data, actions) => {
-								// The button won't actually submit if the form
-								// isn't valid but we can check here and if invalid
-								// the browser will scroll the user to the first
-								// error if necessary
-								if (!formIsValid) {
-									/** We run this so the form validation happens and focus on errors */
-									formRef.current?.requestSubmit();
-									// We reject here so that the PayPal flow doesn't start.
-									// Unfortunately it does result in a brief flash of the modal.
-									// The PayPalButtons component does support a disabled prop
-									// but when set to true this renders the button in a clearly
-									// disabled state. The UI pattern used on the form is to leave
-									// the button clickable and then scroll to any validation
-									// errors if necessary. Rendering the button in a clearly
-									// disabled state isn't compatible with this pattern.
-									return actions.reject();
-								}
+									// This will trigger a form submission
+									setPayPalPaymentToken(paymentToken);
+								}}
+								onClick={(_data, actions) => {
+									// The button won't actually submit if the form
+									// isn't valid but we can check here and if invalid
+									// the browser will scroll the user to the first
+									// error if necessary
+									if (!formIsValid) {
+										/** We run this so the form validation happens and focus on errors */
+										formRef.current?.requestSubmit();
+										// We reject here so that the PayPal flow doesn't start.
+										// Unfortunately it does result in a brief flash of the modal.
+										// The PayPalButtons component does support a disabled prop
+										// but when set to true this renders the button in a clearly
+										// disabled state. The UI pattern used on the form is to leave
+										// the button clickable and then scroll to any validation
+										// errors if necessary. Rendering the button in a clearly
+										// disabled state isn't compatible with this pattern.
+										return actions.reject();
+									}
 
-								return actions.resolve();
-							}}
-							onError={() => {
-								// TODO: Get proper copy for this:
-								setErrorMessage('Sorry, something went wrong');
-								setErrorContext('Please try again.');
-							}}
-						/>
+									return actions.resolve();
+								}}
+								onError={() => {
+									// TODO: Get proper copy for this:
+									setErrorMessage('Sorry, something went wrong');
+									setErrorContext('Please try again.');
+								}}
+							/>
+						</>
 					</PayPalScriptProvider>
 				</>
 			);
