63a39749b3bdbdee6aab79a95afd67ed20ec3096
Tom Wey <tom.wey@theguardian.com>
2026-07-30 10:34:46 +0100

    Revert unneeded whitespace change

--- support-frontend/assets/pages/[countryGroupId]/components/checkoutForm.tsx
+++ support-frontend/assets/pages/[countryGroupId]/components/checkoutForm.tsx
@@ -1065,6 +1065,7 @@ export default function CheckoutForm({
 									cssOverrides={css``}
 								/>
 							</Legend>
+
 							<RadioGroup
 								role="radiogroup"
 								label="Select payment method"
