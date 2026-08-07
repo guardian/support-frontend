e866375d962364c0f005e4bed8afb303d986c939
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-08-07 13:44:59 +0100

    api router nocaching enabled

--- support-backend/src/routers/apiRouter.ts
+++ support-backend/src/routers/apiRouter.ts
@@ -1,9 +1,15 @@
+import type { RequestHandler } from 'express';
 import { Router } from 'express';
 import { getIdealPostcodeApiKey, getPaperRoundApiConfig } from '../aws/ssm';
 import { buildDeliveryAgentsHandler } from '../handlers/deliveryAgents';
 import { buildPostcodeLookupHandler } from '../handlers/postcodeLookup';
 import { IdealPostcodeService } from '../services/idealPostcodeService';
 import { PaperRoundService } from '../services/paperRoundService';
+
+const noCache: RequestHandler = (_req, res, next) => {
+	res.setHeader('Cache-Control', 'no-cache, private');
+	next();
+};
 
 export const buildApiRouterWithServices = async () => {
 	const apiKey = await getIdealPostcodeApiKey();
@@ -28,11 +34,13 @@ export const buildApiRouter = (
 
 	apiRouter.get(
 		'/postcode-lookup/:postcode',
+		noCache,
 		buildPostcodeLookupHandler(idealPostcodeService),
 	);
 
 	apiRouter.get(
 		'/delivery-agents/:postcode',
+		noCache,
 		buildDeliveryAgentsHandler(paperRoundService),
 	);
 
