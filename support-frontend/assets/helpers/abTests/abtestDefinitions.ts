b1f8eee0b933ba41dd1d24476a8212be31e7ee9a
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-08-14 17:21:15 +0100

    setup abtest deliveryAgentsLookupExpress

--- support-frontend/assets/helpers/abTests/abtestDefinitions.ts
+++ support-frontend/assets/helpers/abTests/abtestDefinitions.ts
@@ -82,6 +82,27 @@ export const tests: Tests = {
 		targetPage: pageUrlRegexes.ukPrintCheckoutOnly,
 		excludeContributionsOnlyCountries: true,
 	},
+	deliveryAgentsLookupExpress: {
+		variants: [
+			{
+				id: 'control',
+			},
+			{
+				id: 'variant',
+			},
+		],
+		audiences: {
+			GBPCountries: {
+				offset: 0,
+				size: 0.1,
+			},
+		},
+		isActive: false,
+		referrerControlled: false, // ab-test name not needed to be in paramURL
+		seed: 9,
+		targetPage: pageUrlRegexes.ukPrintCheckoutOnly,
+		excludeContributionsOnlyCountries: true,
+	},
 	stripePaymentElementTest: {
 		variants: [
 			{
