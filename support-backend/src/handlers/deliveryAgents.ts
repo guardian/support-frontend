3e3542529319d3c872b1e07cecafc282baddd4bb
Tom Wey <tom.wey@theguardian.com>
2026-08-20 09:35:55 +0100

    Send Cloudwatch metrics for delivery agents outcome

--- support-backend/src/handlers/deliveryAgents.ts
+++ support-backend/src/handlers/deliveryAgents.ts
@@ -1,4 +1,5 @@
 import type { RequestHandler } from 'express';
+import { putMetric } from '../aws/cloudwatch';
 import type {
 	AgentCoverage,
 	PaperRoundService,
@@ -38,28 +39,32 @@ export const buildDeliveryAgentsHandler =
 		try {
 			const result = await paperRoundService.coverage(postcode);
 
-			// TODO: emit metrics for success & failure for parity with Scala controller
-
 			switch (result.data.status) {
 				case 'CO':
+					void putMetric('GetDeliveryAgentsSuccess');
 					return res.json({
 						type: 'Covered',
 						agents: result.data.agents.map((a) => mapAgentForResponse(a)),
 					});
 				case 'NC':
+					void putMetric('GetDeliveryAgentsSuccess');
 					return res.json({ type: 'NotCovered' });
 				case 'MP':
+					void putMetric('GetDeliveryAgentsSuccess');
 					return res.status(404).json({ type: 'UnknownPostcode' });
 				case 'IP':
+					void putMetric('GetDeliveryAgentsSuccess');
 					return res.status(400).json({ type: 'ProblemWithInput' });
 				case 'IE':
 					console.error(
 						`Got internal error from PaperRound: ${result.data.message}`,
 					);
+					void putMetric('GetDeliveryAgentsFailure');
 					return res.status(500).send();
 			}
 		} catch (error) {
 			console.error(error);
+			void putMetric('GetDeliveryAgentsFailure');
 			return res.status(500).send();
 		}
 	};
