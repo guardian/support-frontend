5d8b65512f194fbd9d745766517e18bad271ef4f
Tom Wey <tom.wey@theguardian.com>
2026-08-24 14:56:19 +0100

    Turn off Express default behaviour to add etags to all responses (#8181)
    For parity with the Scala Play app which defines the endpoints which the
    Express app replaces.

--- support-backend/src/buildApp.ts
+++ support-backend/src/buildApp.ts
@@ -4,6 +4,9 @@ import { buildApiRouterWithServices } from './routers/apiRouter';
 export const buildApp = async () => {
 	const app = express();
 
+	// Disable Express default behaviour to set an etag header on all responses
+	app.set('etag', false);
+
 	app.use('/api', await buildApiRouterWithServices());
 
 	app.get('/healthcheck-express', (req, res) => {
