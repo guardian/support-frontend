659fb11f916537db4c87d89599a3834e0bae1c3a
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-09-23 20:28:20 +0100

    casting in test still required

--- support-frontend/assets/pages/[countryGroupId]/helpers/isStudentBeansRegionValid.test.ts
+++ support-frontend/assets/pages/[countryGroupId]/helpers/isStudentBeansRegionValid.test.ts
@@ -1,33 +1,33 @@
-import { CountryCode } from '@modules/internationalisation/country';
+import type { CountryCode } from '@modules/internationalisation/country';
 import { SupportRegionId } from '@modules/internationalisation/countryGroup';
 import { isStudentBeansRegionValid } from './isStudentBeansRegionValid';
 
 describe('isStudentBeansRegionValid', () => {
 	it.each`
-		region                  | country            | eurInclude | expected
-		${SupportRegionId.EU}   | ${CountryCode.DE}  | ${true}    | ${true}
-		${SupportRegionId.EU}   | ${CountryCode.FR}  | ${true}    | ${true}
-		${SupportRegionId.EU}   | ${CountryCode.ES}  | ${true}    | ${true}
-		${SupportRegionId.EU}   | ${CountryCode.IE}  | ${true}    | ${true}
-		${SupportRegionId.EU}   | ${CountryCode.NL}  | ${true}    | ${true}
-		${SupportRegionId.EU}   | ${CountryCode.IT}  | ${true}    | ${false}
-		${SupportRegionId.UK}   | ${undefined}       | ${true}    | ${true}
-		${SupportRegionId.US}   | ${undefined}       | ${true}    | ${true}
-		${SupportRegionId.CA}   | ${undefined}       | ${true}    | ${true}
-		${SupportRegionId.AU}   | ${undefined}       | ${true}    | ${false}
-		${SupportRegionId.NZ}   | ${undefined}       | ${true}    | ${false}
-		${SupportRegionId.EU}   | ${CountryCode.DE}  | ${false}   | ${false}
-		${SupportRegionId.EU}   | ${CountryCode.FR}  | ${false}   | ${false}
-		${SupportRegionId.EU}   | ${CountryCode.ES}  | ${false}   | ${false}
-		${SupportRegionId.EU}   | ${CountryCode.IE}  | ${false}   | ${false}
-		${SupportRegionId.EU}   | ${CountryCode.NL}  | ${false}   | ${false}
+		region                | country      | eurInclude | expected
+		${SupportRegionId.EU} | ${'DE'}      | ${true}    | ${true}
+		${SupportRegionId.EU} | ${'FR'}      | ${true}    | ${true}
+		${SupportRegionId.EU} | ${'ES'}      | ${true}    | ${true}
+		${SupportRegionId.EU} | ${'IE'}      | ${true}    | ${true}
+		${SupportRegionId.EU} | ${'NL'}      | ${true}    | ${true}
+		${SupportRegionId.EU} | ${'IT'}      | ${true}    | ${false}
+		${SupportRegionId.UK} | ${undefined} | ${true}    | ${true}
+		${SupportRegionId.US} | ${undefined} | ${true}    | ${true}
+		${SupportRegionId.CA} | ${undefined} | ${true}    | ${true}
+		${SupportRegionId.AU} | ${undefined} | ${true}    | ${false}
+		${SupportRegionId.NZ} | ${undefined} | ${true}    | ${false}
+		${SupportRegionId.EU} | ${'DE'}      | ${false}   | ${false}
+		${SupportRegionId.EU} | ${'FR'}      | ${false}   | ${false}
+		${SupportRegionId.EU} | ${'ES'}      | ${false}   | ${false}
+		${SupportRegionId.EU} | ${'IE'}      | ${false}   | ${false}
+		${SupportRegionId.EU} | ${'NL'}      | ${false}   | ${false}
 	`(
 		`should return $expected for eur included $eurInclude region $region $country`,
 		({ region, country, eurInclude, expected }) => {
 			const isStudentRegionValidResult = isStudentBeansRegionValid(
-				region,
-				eurInclude,
-				country,
+				region as SupportRegionId,
+				eurInclude as boolean,
+				country as CountryCode,
 			);
 			expect(isStudentRegionValidResult).toEqual(expected);
 		},
