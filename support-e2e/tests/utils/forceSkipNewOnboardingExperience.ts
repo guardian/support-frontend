1752d956f7055d56791cbd114f68e4ffedb4d19b
Paul Dempsey <paul.dempsey@guardian.co.uk>
2026-09-23 15:40:23 +0100

    playwright feature switch usage move from session to local storage

--- support-e2e/tests/utils/forceSkipNewOnboardingExperience.ts
+++ support-e2e/tests/utils/forceSkipNewOnboardingExperience.ts
@@ -4,7 +4,7 @@ import type { Page } from '@playwright/test';
 // it to interfere with the Playwright tests
 export const forceSkipNewOnboardingExperience = async (page: Page) =>
 	await page.evaluate(() =>
-		window.sessionStorage.setItem(
+		window.localStorage.setItem(
 			'gu.skipNewOnboardingExperience',
 			JSON.stringify({ value: 'true' }),
 		),
