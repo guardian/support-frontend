be10d9ce2b1ffe6bfda7b3ef2c861c5b3ac2fe5f
(Admin) Jonathan Ruda <admin.jonathan.ruda@J7G4LH7XRF-PT002602.local>
2026-09-25 10:07:46 +0100

    prettier run

--- support-frontend/assets/components/onboarding/sections/summary.tsx
+++ support-frontend/assets/components/onboarding/sections/summary.tsx
@@ -293,10 +293,10 @@ function OnboardingSummary({
 	const paymentMethodCopy = isDirectDebit
 		? 'Direct Debit'
 		: isPaypal
-			? 'PayPal'
-			: isStripeCard
-				? 'Credit/Debit card'
-				: 'Your selected payment method';
+		? 'PayPal'
+		: isStripeCard
+		? 'Credit/Debit card'
+		: 'Your selected payment method';
 
 	const paymentMethod =
 		order?.accountNumber && isDirectDebit
